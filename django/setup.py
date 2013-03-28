4d25b61a92323bebc3f5e2c5f6766e60a9e83414
Brian Knapp <knappador@gmail.com>
2013-03-28 18:44:19 -0500

    Using async gevent worker.  Huge oversight for "CTO Outsourcing."

--- django/setup.py
+++ django/setup.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import sys
 import setup_util
@@ -6,8 +5,15 @@ import os
 
 def start(args):
   setup_util.replace_text("django/hello/hello/settings.py", "HOST': '.*'", "HOST': '" + args.database_host + "'")
-
-  subprocess.Popen("gunicorn hello.wsgi:application -b 0.0.0.0:8080 -w " + str((args.max_threads * 2)) + " --log-level=critical", shell=True, cwd="django/hello")
+  # for the love of all that is good and right in the universe, use gevent worker
+  # if you're going to bother to use gunicorn.  meinheld is faster, but needs some work on 
+  # certain asynch support.
+  # $ easy_install pip
+  # $ pip install gevent
+  # not so difficult, is it?
+  # requires 5 minutes of searching
+  # so much for CTO Outsourcing
+  subprocess.Popen("gunicorn hello.wsgi:application -k gevent  -b 0.0.0.0:8080 -w " + str((args.max_threads * 2)) + " --log-level=critical", shell=True, cwd="django/hello")
   return 0
 def stop():
   p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
