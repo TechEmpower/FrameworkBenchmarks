88255b96d8699ba9854343b59c1f6cc79a2ced95
Ryan Tenney <ryan@10e.us>
2013-03-28 15:35:08 -0400

    Improve controllers
    Let spring handle serializing and writing the response body.

--- spring/src/main/java/hello/web/HelloJsonController.java
+++ spring/src/main/java/hello/web/HelloJsonController.java
@@ -11,27 +11,20 @@ import org.springframework.http.converter.json.*;
 import org.springframework.http.server.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
- 
+
 /**
  * Handles requests for the application home page.
  */
 @Controller
-public class HelloJsonController {
- 
-  @RequestMapping(value = "/json")
-  public Object json(HttpServletResponse response) 
-  {
-    Map<String, String> json = new HashMap<String, String>();
-    json.put("message", "Hello, world");
+public class HelloJsonController
+{
 
-    try {
-      new MappingJackson2HttpMessageConverter().write(
-          json, MediaType.APPLICATION_JSON, new ServletServerHttpResponse(response));
-    } catch (HttpMessageNotWritableException e) {
-        e.printStackTrace();
-    } catch (IOException e) {
-        e.printStackTrace();
-    }
-    return null;
+  @RequestMapping(value = "/json", produces = "application/json")
+  @ResponseBody
+  public Map<String, String> json()
+  {
+    Map<String, String> map = new HashMap<String, String>();
+    map.put("message", "Hello, world");
+    return map;
   }
 }
