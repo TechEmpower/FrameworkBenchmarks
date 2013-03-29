c5208bc2da353051e89d0d5125c14f06d93a00eb
Todd Fredrich <tfredrich@gmail.com>
2013-03-29 13:37:48 -0600

    Introduced RestExpress Java REST framework

---
+++ restexpress/setup.py
@@ -0,0 +1,24 @@
+
+import subprocess
+import sys
+import setup_util
+
+def start(args):
+  setup_util.replace_text("restexpress/config/dev/environment.properties", "mysql:\/\/.*:3306", "mysql://" + args.database_host + ":3306")
+
+  try:
+    subprocess.check_call("mvn clean package", shell=True, cwd="restexpress")
+    subprocess.check_call("mvn assembly:single", shell=True, cwd="restexpress")
+    subprocess.check_call("unzip world-1.0-SNAPSHOT-zip-with-dependencies.zip", shell=True, cwd="restexpress/target")
+    subprocess.Popen("java -jar netty-example-0.1-jar-with-dependencies.jar".rsplit(" "), cwd="restexpress/target/world-1.0-SNAPSHOT")
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'hello.Main' in line:
+      pid = int(line.split(None, 2)[1])
+      os.kill(pid, 9)
+  return 0
