35a01ef99257be09854cbcdd77eb9d8e0c772f29
jeff stresser <play.new.questions[at]gmail.com>
2013-03-30 01:08:47 +0100

    initial commit
    play 2.1 with scala, anorm and async

---
+++ play-scala-anorm/app/controllers/Application.scala
@@ -0,0 +1,36 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import play.api.libs.json.Json
+import play.api.libs.concurrent._
+import java.util.concurrent.ThreadLocalRandom
+import scala.concurrent._
+import models._
+
+object Application extends Controller {
+  
+    private val TEST_DATABASE_ROWS = 10000
+
+    def json() = Action {
+      Ok(Json.obj("message" -> "Hello World!"))   
+    }
+
+    def db(queries: Int) = Action {
+        import play.api.libs.concurrent.Execution.Implicits._
+
+        val random = ThreadLocalRandom.current()
+
+        val worlds = Future {
+            (0 to queries) map {
+                _ => World.findById(random.nextInt(TEST_DATABASE_ROWS) + 1)
+            }
+        }
+
+        Async {
+            worlds map {
+                w => Ok(Json.toJson(w))  
+            }
+        }
+    }
+}
---
+++ play-scala-anorm/project/Build.scala
@@ -0,0 +1,21 @@
+import sbt._
+import Keys._
+import play.Project._
+
+object ApplicationBuild extends Build {
+
+  val appName         = "play-scala-anorm"
+  val appVersion      = "1.0-SNAPSHOT"
+
+  val appDependencies = Seq(
+    // Add your project dependencies here,
+    jdbc,
+    anorm
+  )
+
+
+  val main = play.Project(appName, appVersion, appDependencies).settings(
+    // Add your own project settings here      
+  )
+
+}
