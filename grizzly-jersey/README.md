c31a1300b983043beb6f3b61e97b9b3b47c0cf6b
Chris Vest <mr.chrisvest@gmail.com>
2013-03-30 15:17:55 +0100

    Add a JSON test for using Jersey together with the Grizzly server (note that this should not be taken as representative of using Grizzly directly)

---
+++ grizzly-jersey/README.md
@@ -0,0 +1,18 @@
+# Netty Benchmarking Test
+
+This is the Grizzly+Jersey portion of a [benchmarking test suite](../) comparing a variety of web development platforms.
+
+### JSON Encoding Test
+* [JSON test source](src/main/java/hello/HelloServerHandler.java)
+
+## Versions
+
+* [Java OpenJDK 1.7.0_09](http://openjdk.java.net/)
+* [Jersey 1.17.1](http://jersey.java.net/)
+* [Grizzly 2.2.16](http://grizzly.java.net/)
+
+## Test URLs
+
+### JSON Encoding Test
+
+    http://localhost:8080/json
---
+++ grizzly-jersey/src/main/java/hello/HelloWebServer.java
@@ -0,0 +1,41 @@
+package hello;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import com.sun.jersey.api.container.grizzly2.GrizzlyServerFactory;
+import com.sun.jersey.api.core.PackagesResourceConfig;
+import com.sun.jersey.api.core.ResourceConfig;
+
+public class HelloWebServer {
+
+  private final int port;
+
+  public HelloWebServer(int port) {
+    this.port = port;
+  }
+
+  public void run() throws Exception {
+    URI baseUri = getBaseUrl(port);
+    ResourceConfig rc = new PackagesResourceConfig("hello");
+    GrizzlyServerFactory.createHttpServer(baseUri, rc);
+    
+    System.err.print("Server started. Press ENTER to stop.");
+    System.in.read();
+  }
+
+  private static URI getBaseUrl(int port) {
+    return UriBuilder.fromUri("http://localhost/").port(port).build();
+  }
+
+  public static void main(String[] args) throws Exception {
+    int port;
+    if (args.length > 0) {
+      port = Integer.parseInt(args[0]);
+    } else {
+      port = 8080;
+    }
+    new HelloWebServer(port).run();
+  }
+}
