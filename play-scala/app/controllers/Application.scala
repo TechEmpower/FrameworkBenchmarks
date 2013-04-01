4a5b5d56947bf8c254dbbea339630cc22f32d54a
Skamander <play.new.questions[at]gmail.com>
2013-04-01 17:48:05 +0200

    Changed the model to anorm and fixed the db action
    The model now uses anorm, and the db returns proper json an uses futures

--- play-scala/app/controllers/Application.scala
+++ play-scala/app/controllers/Application.scala
@@ -1,40 +1,34 @@
 package controllers
 
-import play._
-import play.api.libs.concurrent._
+import play.api._
 import play.api.mvc._
-import play.libs.Json
-import org.codehaus.jackson.node.ObjectNode
-import views.html._
-import models._
-import java.util._
+import play.api.libs.json.Json
+import play.api.libs.concurrent._
 import java.util.concurrent.ThreadLocalRandom
 import scala.concurrent._
+import models._
 
 object Application extends Controller {
+  
   private val TEST_DATABASE_ROWS = 10000
 
   def json() = Action {
-    val result = Json.newObject()
-    result.put("message", "Hello World!")
-    Ok(result.toString)
+    Ok(Json.obj("message" -> "Hello World!"))   
   }
 
   def db(queries: Int) = Action {
     import play.api.libs.concurrent.Execution.Implicits._
 
-    val random = ThreadLocalRandom.current()
-
     Async {
-      Future {
-        (1 to queries) map {
-          _ =>
-            World.find.byId(random.nextInt(TEST_DATABASE_ROWS) + 1)
-        }
-      } map {
-        worlds =>
-          Ok(Json.toJson(worlds).toString())
-      }
+      val random = ThreadLocalRandom.current()
+
+      val worlds = Future.sequence( for {
+        _ <- (1 to queries)
+      } yield Future(World.findById(random.nextInt(TEST_DATABASE_ROWS) + 1)))
+
+      worlds map {
+        w => Ok(Json.toJson(w))  
+      } 
     }
-  }
+  }  
 }
