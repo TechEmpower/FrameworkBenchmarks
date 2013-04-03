5f463039c572ec035e414be095e4cec3fd628c5a
Heinz N. Gies <heinz@licenser.net>
2013-04-03 01:24:24 -0700

    Fixed bug in killing erlang.

--- cowboy/setup_erlang.py
+++ cowboy/setup_erlang.py
@@ -17,7 +17,7 @@ def start(args):
     return 1
 def stop():
   try:
-    subprocess.check_call("killall beam", shell=True, cwd="/usr/bin")
+    subprocess.check_call("killall beam.smp", shell=True, cwd="/usr/bin")
     return 0
   except subprocess.CalledProcessError:
     return 1
