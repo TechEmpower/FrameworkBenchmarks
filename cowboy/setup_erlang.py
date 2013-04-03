4c0bd1e1cb56e4d09734b22967690d5dd1267e1c
Heinz N. Gies <heinz@licenser.net>
2013-04-03 08:48:09 -0700

    Removed erlang install form seperate tests.

--- cowboy/setup_erlang.py
+++ cowboy/setup_erlang.py
@@ -7,8 +7,6 @@ def start(args):
 #  setup_util.replace_text("rails/config/database-ruby.yml", "host: .*", "host: " + args.database_host)
   
   try:
-    subprocess.check_call("/usr/bin/curl -klO https://elearning.erlang-solutions.com/couchdb//rbingen_adapter//package_R16B_precise64_1361901944/esl-erlang_16.b-1~ubuntu~precise_amd64.deb", shell=True, cwd="cowboy")
-    subprocess.check_call("sudo /usr/bin/dpkg --install esl-erlang_16.b-1~ubuntu~precise_amd64.deb", shell=True, cwd="cowboy")
     subprocess.check_call("./rebar get-deps", shell=True, cwd="cowboy")
     subprocess.check_call("./rebar compile", shell=True, cwd="cowboy")
     subprocess.check_call("erl -pa ebin deps/*/ebin -s hello_world -noshell -detached", shell=True, cwd="cowboy")
