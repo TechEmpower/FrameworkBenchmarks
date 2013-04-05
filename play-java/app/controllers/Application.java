b8c88f18dc16808fa373c9677f9debeecfa13155
Christopher Hunt <huntchr@gmail.com>
2013-04-06 10:34:51 +1100

    Beginnings of connection pooling configuration.

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.World;
+import play.libs.Akka;
 import play.libs.F;
 import play.libs.Json;
 
@@ -10,6 +11,7 @@ import org.codehaus.jackson.node.ObjectNode;
 import org.codehaus.jackson.map.ObjectMapper;
 import play.mvc.Controller;
 import play.mvc.Result;
+import scala.concurrent.ExecutionContext;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,6 +25,8 @@ public class Application extends Controller {
     private static final int TEST_DATABASE_ROWS = 10000;
     //http://stackoverflow.com/questions/3907929/should-i-make-jacksons-objectmapper-as-static-final
     private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private static final ExecutionContext dbEc = Akka.system().dispatchers().lookup("db");
 
     public static Result json() {
         final ObjectNode result = objectMapper.createObjectNode();
@@ -38,6 +42,7 @@ public class Application extends Controller {
                         final Random random = ThreadLocalRandom.current();
                         final List<F.Promise<? extends World>> promises = new ArrayList<F.Promise<? extends World>>(queries);
                         for (int i = 0; i < queries; ++i) {
+                            //FIXME: How do we express the use of dbEc here?
                             promises.add(future(findWorld(Long.valueOf(random.nextInt(TEST_DATABASE_ROWS) + 1))));
                         }
                         final List<World> worlds = F.Promise.sequence(promises).get(5L * queries, TimeUnit.SECONDS);
--- play-scala/app/controllers/Application.scala
+++ play-scala/app/controllers/Application.scala
@@ -1,6 +1,6 @@
 package controllers
 
-import play.api._
+import play.api.Play.current
 import play.api.mvc._
 import play.api.libs.json.Json
 import play.api.libs.concurrent._
@@ -19,12 +19,14 @@ object Application extends Controller {
   def db(queries: Int) = Action {
     import play.api.libs.concurrent.Execution.Implicits._
 
+    val dbEc: ExecutionContext = Akka.system.dispatchers.lookup("db")
+
     Async {
       val random = ThreadLocalRandom.current()
 
       val worlds = Future.sequence( (for {
         _ <- (1 to queries).par
-      } yield Future(World.findById(random.nextInt(TEST_DATABASE_ROWS) + 1))).toList)
+      } yield Future(World.findById(random.nextInt(TEST_DATABASE_ROWS) + 1))(dbEc)).toList)
 
       worlds map {
         w => Ok(Json.toJson(w))  
