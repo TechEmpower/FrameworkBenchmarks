85840c7744477b1511610d655121883531bb0672
Raphael Javaux <raphaeljavaux@gmail.com>
2013-04-06 14:03:13 +0200

    improved random generator

--- yesod/bench/Application.hs
+++ yesod/bench/Application.hs
@@ -18,22 +18,22 @@ import Yesod.Default.Config
 import Settings
 
 getJsonR :: Handler RepJson
-getJsonR = jsonToRepJson $ object ["message" .= ("Hello, World!" :: Text)]
+getJsonR = jsonToRepJson $ object [("message", "Hello, World!" :: Text)]
 
 getDBR :: Handler RepJson
 getDBR = do
-    i <- liftIO $ randomRIO (1, 10000)
+    !i <- liftIO $ randomRIO (1, 10000)
     Just o <- runDB $ get $ Key $ PersistInt64 i
-    jsonToRepJson $ object ["id" .= i, "randomNumber" .= worldRandomNumber o]
+    jsonToRepJson $ object [("id", i), ("randomNumber", worldRandomNumber o)]
 
 getDB2R :: Int -> Handler RepJson
 getDB2R n = do
-    is <- force . take n . randomRs (1, 10000) <$> liftIO newStdGen
+    !is <- force . take n . randomRs (1, 10000) <$> liftIO newStdGen
 
     os <- runDB $
         forM is $ \i-> do
             Just o <- get $ Key $ PersistInt64 i
-            return $ object ["id" .= i, "randomNumber" .= worldRandomNumber o]
+            return $ object [("id", i), ("randomNumber", worldRandomNumber o)]
 
     jsonToRepJson $ array os
 
