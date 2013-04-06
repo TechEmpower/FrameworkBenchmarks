b354501e7cad9acf1d6f0d4eb75906d1eb719198
Waldecir Santos <waldecir@gmail.com>
2013-04-06 13:47:47 -0300

    tornado 3 with mongodb raw async

---
+++ tornado/setup.py
@@ -0,0 +1,44 @@
+import subprocess
+import sys
+import setup_util
+import os
+from os.path import expanduser
+
+home = expanduser("~")
+cwd = "%s/FrameworkBenchmarks/tornado" % home
+
+
+def start(args):
+    setup_util.replace_text(
+        cwd + "/server.py", "127.0.0.1", args.database_host)
+
+    subprocess.Popen("python %s/FrameworkBenchmarks/tornado/server.py --port=8000" % home, shell=True, cwd=cwd)
+    subprocess.Popen("python %s/FrameworkBenchmarks/tornado/server.py --port=8001" % home, shell=True, cwd=cwd)
+    subprocess.Popen("python %s/FrameworkBenchmarks/tornado/server.py --port=8002" % home, shell=True, cwd=cwd)
+    subprocess.Popen("python %s/FrameworkBenchmarks/tornado/server.py --port=8003" % home, shell=True, cwd=cwd)
+    subprocess.check_call("sudo /usr/local/nginx/sbin/nginx -c " + home + "/FrameworkBenchmarks/php/deploy/nginx.conf", shell=True)
+
+    return 0
+
+
+def stop():
+
+    try:
+
+        subprocess.call("sudo /usr/local/nginx/sbin/nginx -s stop", shell=True)
+
+    except subprocess.CalledProcessError:
+        #TODO: Better handle exception.
+        pass
+
+    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+        if 'server.py' in line:
+            try:
+                pid = int(line.split(None, 2)[1])
+                os.kill(pid, 9)
+            except OSError:
+                pass
+
+    return 0
