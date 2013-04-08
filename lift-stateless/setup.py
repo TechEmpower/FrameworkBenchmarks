b4386c8e67a136e8717857b5c9805f962d0125ed
Patrick Falls <pfalls@techempower.com>
2013-04-08 11:40:18 -0700

    Modified benchmark_config for lift-stateless to reflect correct sort order

--- lift-stateless/setup.py
+++ lift-stateless/setup.py
@@ -15,7 +15,7 @@ def stop():
   p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
   out, err = p.communicate()
   for line in out.splitlines():
-    if './run' in line:
+    if 'lift-stateless-assembly' in line and 'java' in line:
       pid = int(line.split(None, 2)[1])
       os.kill(pid, 9)
   return 0
