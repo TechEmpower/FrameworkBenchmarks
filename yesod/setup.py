a8f924f13a26e2e7eb24b24b79f3cce2b57a2cf9
Steven Smith <guntir@teamclerks.net>
2013-04-07 22:40:00 -0700

    Fixed RTS options for Yesod and Snap, disabled client sessions for Yesod.

--- yesod/setup.py
+++ yesod/setup.py
@@ -7,11 +7,11 @@ import os
 def start(args):
   setup_util.replace_text("yesod/bench/config/mysql.yml", "host: .*", "host: " + args.database_host)
   
-  subprocess.check_call("cabal configure", shell=True, cwd="yesod/bench")
+  subprocess.check_call("cabal update", shell=True, cwd="yesod/bench")
+  subprocess.check_call("cabal install --only-dependencies", shell=True, cwd="yesod/bench")
   subprocess.check_call("cabal build", shell=True, cwd="yesod/bench")
 
-  heap = args.max_threads
-  subprocess.Popen("dist/build/bench/bench Production +RTS -A"+str(heap)+"m -N" + str(args.max_threads) + " > /dev/null", shell=True, cwd="yesod/bench")
+  subprocess.Popen("dist/build/bench/bench Production +RTS -A4M -N -qg2 -I0 -G2 > /dev/null", shell=True, cwd="yesod/bench")
   return 0
 
 def stop():
