957a5032479247e9cd9b4c16c5b4ab1c63493502
pakunoda <bw9999@gmail.com>
2013-04-08 17:50:23 -0500

    Redirecting stderr to /dev/null.

--- unfiltered/setup_unfiltered.py
+++ unfiltered/setup_unfiltered.py
@@ -8,9 +8,16 @@ def start(args):
   setup_util.replace_text("unfiltered/src/main/resources/application.conf", "jdbc:mysql:\/\/.*:3306", "jdbc:mysql://" + args.database_host + ":3306")
   setup_util.replace_text("unfiltered/src/main/resources/application.conf", "maxThreads = \\d+", "maxThreads = " + str(args.max_threads))
 
+  # Shamelessly stolen from stack overflow
+  try:
+    from subprocess import DEVNULL
+  except ImportError:
+    import os
+    DEVNULL = open(os.devnull, 'wb')
+
   subprocess.check_call("chmod u+x sbt", shell=True, cwd="unfiltered")
   subprocess.check_call("./sbt assembly", shell=True, cwd="unfiltered")
-  subprocess.Popen("java -jar bench-assembly-1.0.0.jar", shell=True, cwd="unfiltered/target/scala-2.10")
+  subprocess.Popen("java -jar bench-assembly-1.0.0.jar", stderr=DEVNULL, shell=True, cwd="unfiltered/target/scala-2.10")
 
   return 0
 def stop():
