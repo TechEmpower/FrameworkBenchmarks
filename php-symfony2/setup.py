70e9f409254ddd2f0ccfc1a90db121a46fd564f6
Skamander <play.new.questions[at]gmail.com>
2013-04-09 21:18:56 +0200

    Initial commit

---
+++ php-symfony2/setup.py
@@ -0,0 +1,35 @@
+
+import subprocess
+import sys
+import setup_util
+from os.path import expanduser
+
+home = expanduser("~")
+
+def start(args):
+  setup_util.replace_text("php-symfony2/app/config/parameters.yml", "database_host: .*", "database_host: " + args.database_host)
+  setup_util.replace_text("php-symfony2/deploy/php-symfony2", "\".*\/FrameworkBenchmarks", "\"" + home + "/FrameworkBenchmarks")
+  setup_util.replace_text("php-symfony2/deploy/php-symfony2", "Directory .*\/FrameworkBenchmarks", "Directory " + home + "/FrameworkBenchmarks")
+  setup_util.replace_text("php-symfony2/deploy/nginx.conf", "root .*\/FrameworkBenchmarks", "root " + home + "/FrameworkBenchmarks")
+
+  try:
+    #subprocess.check_call("sudo cp cake/deploy/cake /etc/apache2/sites-available/", shell=True)
+    #subprocess.check_call("sudo a2ensite cake", shell=True)
+    #subprocess.check_call("sudo chown -R www-data:www-data cake", shell=True)
+    #subprocess.check_call("sudo /etc/init.d/apache2 start", shell=True)
+    subprocess.check_call("composer install", shell=True)
+    subprocess.check_call("sudo php-fpm --fpm-config config/php-fpm.conf -g " + home + "/FrameworkBenchmarks/php-symfony2/deploy/php-fpm.pid", shell=True)
+    subprocess.check_call("sudo /usr/local/nginx/sbin/nginx -c " + home + "/FrameworkBenchmarks/php-symfony2/deploy/nginx.conf", shell=True)
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
+def stop():
+  try:
+    subprocess.call("sudo /usr/local/nginx/sbin/nginx -s stop", shell=True)
+    subprocess.call("sudo kill -QUIT $( cat php-symfony2/deploy/php-fpm.pid )", shell=True)
+    #subprocess.check_call("sudo a2dissite cake", shell=True)
+    #subprocess.check_call("sudo /etc/init.d/apache2 stop", shell=True)
+    #subprocess.check_call("sudo chown -R $USER:$USER cake", shell=True)
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
