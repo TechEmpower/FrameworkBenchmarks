867bc585d751edbdb36d6e705ab942b991199a49
Christopher Hunt <huntchr@gmail.com>
2013-04-09 16:12:02 +1000

    Applied a back-pressure check to ensure that the database does not get overloaded

---
+++ play-scala/app/utils/PredicatedAction.scala
@@ -0,0 +1,19 @@
+package utils
+
+import play.api.mvc._
+
+/**
+ * A predicated action is one where a condition must be satisfied in order to proceed with the request. If the
+ * condition is not satisfied then a supplied status result is yielded.
+ */
+class PredicatedActionBuilder {
+  def apply[A](p: => Boolean, failed: Result)(action: Action[A]): Action[A] = new Action[A] {
+    def apply(request: Request[A]): Result = {
+      if (p) action(request) else failed
+    }
+
+    lazy val parser = action.parser
+  }
+}
+
+object PredicatedAction extends PredicatedActionBuilder
