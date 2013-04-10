f3b7a6fcecb67dd3edd1867d4aaefff7bf466dcb
Christopher Hunt <huntchr@gmail.com>
2013-04-10 21:50:23 +1000

    Strengthened the result type to become clearer.

--- play-scala/app/utils/PredicatedAction.scala
+++ play-scala/app/utils/PredicatedAction.scala
@@ -1,13 +1,14 @@
 package utils
 
 import play.api.mvc._
+import play.api.http.Status
 
 /**
  * A predicated action is one where a condition must be satisfied in order to proceed with the request. If the
  * condition is not satisfied then a supplied status result is yielded.
  */
 class PredicatedActionBuilder {
-  def apply[A](p: => Boolean, failed: Result)(action: Action[A]): Action[A] = new Action[A] {
+  def apply[A](p: => Boolean, failed: Results.Status)(action: Action[A]): Action[A] = new Action[A] {
     def apply(request: Request[A]): Result = {
       if (p) action(request) else failed
     }
