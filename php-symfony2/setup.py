a4e3170cae59e94e7c8c41400d45fa5e5671a32a
Patrick Falls <pfalls@techempower.com>
2013-04-10 13:47:02 -0700

    Slightly altered setup script for symfony2

--- php-symfony2/setup.py
+++ php-symfony2/setup.py
@@ -8,16 +8,11 @@ home = expanduser("~")
 
 def start(args):
   setup_util.replace_text("php-symfony2/app/config/parameters.yml", "database_host: .*", "database_host: " + args.database_host)
-  setup_util.replace_text("php-symfony2/deploy/php-symfony2", "\".*\/FrameworkBenchmarks", "\"" + home + "/FrameworkBenchmarks")
-  setup_util.replace_text("php-symfony2/deploy/php-symfony2", "Directory .*\/FrameworkBenchmarks", "Directory " + home + "/FrameworkBenchmarks")
   setup_util.replace_text("php-symfony2/deploy/nginx.conf", "root .*\/FrameworkBenchmarks", "root " + home + "/FrameworkBenchmarks")
 
   try:
-    #subprocess.check_call("sudo cp cake/deploy/cake /etc/apache2/sites-available/", shell=True)
-    #subprocess.check_call("sudo a2ensite cake", shell=True)
-    #subprocess.check_call("sudo chown -R www-data:www-data cake", shell=True)
-    #subprocess.check_call("sudo /etc/init.d/apache2 start", shell=True)
-    subprocess.check_call("composer install", shell=True)
+    subprocess.check_call("composer.phar install", shell=True, cwd="php-symfony2")
+    subprocess.check_call("sudo chown -R www-data:www-data php-symfony2", shell=True)
     subprocess.check_call("sudo php-fpm --fpm-config config/php-fpm.conf -g " + home + "/FrameworkBenchmarks/php-symfony2/deploy/php-fpm.pid", shell=True)
     subprocess.check_call("sudo /usr/local/nginx/sbin/nginx -c " + home + "/FrameworkBenchmarks/php-symfony2/deploy/nginx.conf", shell=True)
     return 0
@@ -27,9 +22,7 @@ def stop():
   try:
     subprocess.call("sudo /usr/local/nginx/sbin/nginx -s stop", shell=True)
     subprocess.call("sudo kill -QUIT $( cat php-symfony2/deploy/php-fpm.pid )", shell=True)
-    #subprocess.check_call("sudo a2dissite cake", shell=True)
-    #subprocess.check_call("sudo /etc/init.d/apache2 stop", shell=True)
-    #subprocess.check_call("sudo chown -R $USER:$USER cake", shell=True)
+    subprocess.check_call("sudo chown -R $USER:$USER php-symfony2", shell=True)
     return 0
   except subprocess.CalledProcessError:
     return 1
