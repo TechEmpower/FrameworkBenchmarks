4ce61db048e9216eddbc489ac9f73dd36c8b1385
Patrick Falls <pfalls@techempower.com>
2013-04-12 09:51:24 -0700

    Modified the setup scripts for play1 and play1siena to deploy to resin

--- play1siena/setup.py
+++ play1siena/setup.py
@@ -7,24 +7,15 @@ import os
 def start(args):
   setup_util.replace_text("play1siena/conf/application.conf", "jdbc:mysql:\/\/.*:3306", "jdbc:mysql://" + args.database_host + ":3306")
   
-  subprocess.check_call("play war -o ../play1siena --zip", shell=True, cwd="play1siena")
-  # TODO deploy to resin
-  #subprocess.check_call("unzip play-java-1.0-SNAPSHOT.zip", shell=True, cwd="play-java/dist")
-  #subprocess.check_call("chmod +x start", shell=True, cwd="play-java/dist/play-java-1.0-SNAPSHOT")
-  # TODO start resin
-  # subprocess.Popen("./start", shell=True, cwd="play-java/dist/play-java-1.0-SNAPSHOT")
+  subprocess.check_call("rm -rf $RESIN_HOME/webapps/*", shell=True)
+  subprocess.check_call("play1 war -o $RESIN_HOME/webapps/play1 --exclude benchmark_config", shell=True, cwd="play1siena")
+  subprocess.check_call("$RESIN_HOME/bin/resinctl start", shell=True)
 
   return 0
 def stop():
-  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  for line in out.splitlines():
-    if './start' in line or ('play' in line and 'java' in line):   
-      pid = int(line.split(None, 2)[1])
-      os.kill(pid, 9)
   try:
-    os.remove("play-java/RUNNING_PID")
-  except OSError:
-    pass
-    
-  return 0
+    subprocess.check_call("$RESIN_HOME/bin/resinctl shutdown", shell=True)
+    subprocess.check_call("rm -rf $RESIN_HOME/webapps/*", shell=True)
+    return 0
+  except subprocess.CalledProcessError:
+    return 1
