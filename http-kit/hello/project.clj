953c060f1ed96fd5d2f095f74264a1456c2e2ccc
feng <feng.shen@onycloud.com>
2013-04-12 20:55:53 +0800

    http-kit 2.0.1 has better performance

--- http-kit/hello/project.clj
+++ http-kit/hello/project.clj
@@ -6,7 +6,7 @@
                  [ring/ring-json "0.2.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [http-kit/dbcp "0.1.0"]
-                 [http-kit "2.0.0"]
+                 [http-kit "2.0.1"]
                  [log4j "1.2.15" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [mysql/mysql-connector-java "5.1.6"]]
   :main hello.handler
