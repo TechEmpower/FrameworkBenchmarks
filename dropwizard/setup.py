6204476abe86624f6b49301fe909349c1dc4c43d
Mike Megally <cmsimike@gmail.com>
2013-04-14 14:53:05 -0700

    trying to run the server with popen but that doesn't seem to help

--- dropwizard/setup.py
+++ dropwizard/setup.py
@@ -9,8 +9,8 @@ def start(args):
     setup_util.replace_text("dropwizard/hello-world.yml", "url: jdbc:mysql://localhost/hello_world", "url: jdbc:mysql://" + args.database_host + ":3306/hello_world")
 
     try:
-        subprocess.check_call("mvn package;", shell=True, cwd="dropwizard")
-        subprocess.check_call("java -jar target/hello-world-0.0.1-SNAPSHOT.jar server hello-world.yml", shell=True, cwd="dropwizard")
+        subprocess.check_call("mvn clean package;", shell=True, cwd="dropwizard")
+        subprocess.Popen("java -jar target/hello-world-0.0.1-SNAPSHOT.jar server hello-world.yml", shell=True, cwd="dropwizard")
         return 0
     except subprocess.CalledProcessError:
         return 1
