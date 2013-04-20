7c52d17c9df105148cc42d69f1af40e5a0ebe164
Skamander <play.new.questions@gmail.com>
2013-04-20 10:06:43 +0300

    Activated apc class loader

--- php-symfony2/web/app.php
+++ php-symfony2/web/app.php
@@ -8,10 +8,10 @@ $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 // Use APC for autoloading to improve performance.
 // Change 'sf2' to a unique prefix in order to prevent cache key conflicts
 // with other applications also using APC.
-/*
+
 $loader = new ApcClassLoader('sf2', $loader);
 $loader->register(true);
-*/
+
 
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
