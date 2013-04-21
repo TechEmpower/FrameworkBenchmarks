b480917040c2bfefd62b7d30fe2da24bf1c41180
Simon Oberhammer <simon@nekapuzer.at>
2013-04-21 11:40:24 +0200

    ringojs as a web framework: ringojs-convinient
    stick (JSGI routing), ringo-sqlstore and reinhardt (templates) are
    my recommended setup for a convinient framework experience with ringo
    
    makes ringojs-stick obsolete

--- ringojs-convinient/ringo-main.js
+++ ringojs-convinient/ringo-main.js
@@ -2,7 +2,7 @@ var {Application} = require("stick");
 
 var app = exports.app = Application();
 app.configure("mount");
-app.mount("/", require("./views"));
+app.mount("/", require("./app/views"));
 
 if (require.main == module) {
     require("ringo/httpserver").main(module.id);
