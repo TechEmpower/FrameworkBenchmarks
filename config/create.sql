4e51c162aa10199c186ee34810532e4eed9f3a80
Skamander <play.new.questions@gmail.com>
2013-04-24 23:15:38 +0300

    Changed fortune within the insert statement to ...
    ... uppercase to match the table name. Didn't work before on Linux.

--- config/create.sql
+++ config/create.sql
@@ -7,10 +7,10 @@ CREATE DATABASE hello_world;
 USE hello_world;
 
 DROP TABLE IF EXISTS World;
-CREATE TABLE  World (
+CREATE TABLE World (
   id int(10) unsigned NOT NULL auto_increment,
   randomNumber int NOT NULL default 0,
-  PRIMARY KEY  (id)
+  PRIMARY KEY (id)
 )
 ENGINE=INNODB;
 
@@ -35,3 +35,24 @@ END #
 DELIMITER ;
 
 CALL load_data();
+
+DROP TABLE IF EXISTS Fortune;
+CREATE TABLE Fortune (
+  id int(10) unsigned NOT NULL auto_increment,
+  message varchar(2048) CHARACTER SET 'utf8' NOT NULL,
+  PRIMARY KEY (id)
+)
+ENGINE=INNODB;
+
+INSERT INTO Fortune (message) VALUES ('fortune: No such file or directory');
+INSERT INTO Fortune (message) VALUES ('A computer scientist is someone who fixes things that aren''t broken.');
+INSERT INTO Fortune (message) VALUES ('After enough decimal places, nobody gives a damn.');
+INSERT INTO Fortune (message) VALUES ('A bad random number generator: 1, 1, 1, 1, 1, 4.33e+67, 1, 1, 1');
+INSERT INTO Fortune (message) VALUES ('A computer program does what you tell it to do, not what you want it to do.');
+INSERT INTO Fortune (message) VALUES ('Emacs is a nice operating system, but I prefer UNIX. — Tom Christaensen');
+INSERT INTO Fortune (message) VALUES ('Any program that runs right is obsolete.');
+INSERT INTO Fortune (message) VALUES ('A list is only as strong as its weakest link. — Donald Knuth');
+INSERT INTO Fortune (message) VALUES ('Feature: A bug with seniority.');
+INSERT INTO Fortune (message) VALUES ('Computers make very fast, very accurate mistakes.');
+INSERT INTO Fortune (message) VALUES ('<script>alert("This should not be displayed in a browser alert box.");</script>');
+INSERT INTO Fortune (message) VALUES ('フレームワークのベンチマーク');
