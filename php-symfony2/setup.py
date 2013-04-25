870374b1ff85013730ab97a3f0bbe0a3deca678d
Marc Weistroff <marc@weistroff.net>
2013-04-25 20:03:39 -0300

    Add "--optimize-autoloader" to `composer.phar install` command

--- php-symfony2/setup.py
+++ php-symfony2/setup.py
@@ -1,4 +1,3 @@
-
 import subprocess
 import sys
 import setup_util
@@ -11,7 +10,7 @@ def start(args):
   setup_util.replace_text("php-symfony2/deploy/nginx.conf", "root .*\/FrameworkBenchmarks", "root " + home + "/FrameworkBenchmarks")
 
   try:
-    subprocess.check_call("composer.phar install", shell=True, cwd="php-symfony2")
+    subprocess.check_call("composer.phar install --optimize-autoloader", shell=True, cwd="php-symfony2")
     subprocess.check_call("sudo chown -R www-data:www-data php-symfony2", shell=True)
     subprocess.check_call("sudo php-fpm --fpm-config config/php-fpm.conf -g " + home + "/FrameworkBenchmarks/php-symfony2/deploy/php-fpm.pid", shell=True)
     subprocess.check_call("sudo /usr/local/nginx/sbin/nginx -c " + home + "/FrameworkBenchmarks/php-symfony2/deploy/nginx.conf", shell=True)
