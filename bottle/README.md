c8005d8e8276ca609a2278f48db52309f1a56a67
seanaedmiston <seane999@gmail.com>
2013-04-29 23:11:42 +1000

    Start on bottle

---
+++ bottle/README.md
@@ -0,0 +1,25 @@
+# Bottle Benchmark Test
+
+Single file test, [app.py](app.py)
+
+
+## Test URLs
+### JSON Encoding 
+
+http://localhost:8080/json
+
+### Single Row Random Query
+
+With ORM:
+    http://localhost:8080/dbs
+
+Without ORM (raw):
+    http://localhost:8080/dbsraw
+
+### Variable Row Query Test 
+
+With ORM:
+    http://localhost:8080/db?queries=2
+
+Without ORM (raw):
+    http://localhost:8080/dbraw?queries=2
---
+++ bottle/setup.py
@@ -0,0 +1,23 @@
+import subprocess
+import sys
+import setup_util
+import os
+
+def start(args):
+  setup_util.replace_text("bottle/app.py", "DBHOSTNAME", args.database_host)
+  subprocess.Popen("gunicorn app:app -k gevent -b 0.0.0.0:8080 -w " + str((args.max_threads * 2)) + " --preload --log-level=critical", shell=True, cwd="bottle")
+  
+  return 0
+
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'gunicorn' in line:
+      try:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+      except OSError:
+        pass
+  
+  return 0
