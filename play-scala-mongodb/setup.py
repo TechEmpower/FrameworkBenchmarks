6755d83893a55627326cc4b0d1c7f544f0c229ea
Patrick Falls <pfalls@techempower.com>
2013-04-29 13:24:42 -0700

    fixed setup file

--- play-scala-mongodb/setup.py
+++ play-scala-mongodb/setup.py
@@ -8,9 +8,9 @@ def start(args):
   setup_util.replace_text("play-scala-mongodb/conf/application.conf", "localhost:27017", "" + args.database_host + ":27017")
 
   subprocess.check_call("play dist", shell=True, cwd="play-scala-mongodb")
-  subprocess.check_call("unzip play-scala-1.0-SNAPSHOT.zip", shell=True, cwd="play-scala-mongodb/dist")
-  subprocess.check_call("chmod +x start", shell=True, cwd="play-scala-mongodb/dist/play-scala-1.0-SNAPSHOT")
-  subprocess.Popen("./start", shell=True, cwd="play-scala-mongodb/dist/play-scala-1.0-SNAPSHOT")
+  subprocess.check_call("unzip play-scala-mongodb-1.0-SNAPSHOT.zip", shell=True, cwd="play-scala-mongodb/dist")
+  subprocess.check_call("chmod +x start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
+  subprocess.Popen("./start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
 
   return 0
 def stop():
