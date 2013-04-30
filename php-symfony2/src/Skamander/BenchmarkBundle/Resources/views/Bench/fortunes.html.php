9ead750c9aa5cc31cf260a080e09f3ac5decb4ef
Skamander <play.new.questions@gmail.com>
2013-04-30 21:05:14 +0200

    Changed the raw fortune test to doctrine orm.
    Now it's possible to "compare" twig with raw php templates. Yeah, the
    raw php template doesn't use includes to mimic the extend which twig
    offers, but I'm too lazy to change that right now. ;o)

--- php-symfony2/src/Skamander/BenchmarkBundle/Resources/views/Bench/fortunes.html.php
+++ php-symfony2/src/Skamander/BenchmarkBundle/Resources/views/Bench/fortunes.html.php
@@ -14,8 +14,8 @@
 
     <?php foreach($fortunes as $fortune): ?>
         <tr>
-            <td><?php echo $fortune['id']; ?></td>
-            <td><?php echo htmlspecialchars($fortune['message'], ENT_QUOTES, "UTF-8", false); ?></td>
+            <td><?php echo $fortune->id; ?></td>
+            <td><?php echo htmlspecialchars($fortune->message, ENT_QUOTES, "UTF-8", false); ?></td>
         </tr>
     <?php endforeach; ?>
 
