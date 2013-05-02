f0caf55c547c1330382a4e5f38a21751a0f65e86
Benjamin Eberlei <kontakt@beberlei.de>
2013-05-02 21:34:21 +0200

    Raw Symfony

--- php-symfony2/app/AppKernel.php
+++ php-symfony2/app/AppKernel.php
@@ -9,16 +9,9 @@ class AppKernel extends Kernel
     {
         $bundles = array(
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
-            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new JMS\AopBundle\JMSAopBundle(),
-            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
-            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new Skamander\BenchmarkBundle\SkamanderBenchmarkBundle(),
         );
 
