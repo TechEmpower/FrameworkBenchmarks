7bed3cc5633bd4231741027d70c04b02301ce887
Simon Elliott <simon@papercreatures.com>
2013-05-02 20:46:02 +0100

    web-simple

---
+++ web-simple/app.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use Web::Simple;
+use JSON::XS;
+use DBI;
+
+my $dsn = "dbi:mysql:database=hello_world;host=localhost;port=3306";
+my $dbh = DBI->connect( $dsn, 'benchmarkdbuser', 'benchmarkdbpass', {} );
+my $sth = $dbh->prepare("SELECT * FROM World where id = ?");
+
+sub dispatch_request {
+ sub (/json) {
+    [ 200, [ 'Content-type' => 'application/json; charset=utf-8', ],
+      [ encode_json({ message => 'Hello, World!' }) ] ];
+  },
+  sub (/db + ?queries~) {
+    my ($self, $queries) = @_;
+    my @response;
+    for ( 1 .. $queries ) {
+        my $id = int rand 10000 + 1;
+        $sth->execute($id);
+        if ( my $row = $sth->fetchrow_hashref ) {
+            push @response,
+              { id => $id, randomNumber => $row->{randomNumber} };
+        }
+    }
+    [ 200, \@response ];
+  }
+}
+
+__PACKAGE__->run_if_script;
