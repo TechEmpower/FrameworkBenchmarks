7250152a1b5bcbd884c031738b4a9c9146aba17c
Pēteris Ņikiforovs <p@mak.id.lv>
2013-05-03 00:05:23 +0000

    ASP.NET Mono JSON & ADO.NET with MySQL tests

---
+++ aspnet-mono/project/Benchmarks.Mono.AspNet/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Benchmarks.Mono.AspNet
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteTable.Routes.MapRoute(
+                name: "JSON",
+                url: "json",
+                defaults: new { controller = "Json", action = "Index" }
+            );
+
+            RouteTable.Routes.MapRoute(
+                name: "MySQL/Raw",
+                url: "mysql-raw",
+                defaults: new { controller = "MySql", action = "Raw" }
+            );
+
+            RouteTable.Routes.MapRoute(
+                name: "MySQL/EF",
+                url: "mysql-ef",
+                defaults: new { controller = "MySql", action = "EntityFramework" }
+            );
+        }
+    }
+}
