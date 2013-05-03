43e8403f76574f43d37c64c7c67811bc42069a58
kenjikobe <kenji.minamoto@gmail.com>
2013-05-02 20:42:04 -0500

    improve code to be more natural with framework guidelines

--- php-phalcon/app/controllers/BenchController.php
+++ php-phalcon/app/controllers/BenchController.php
@@ -1,11 +1,14 @@
 <?php
 
+use Phalcon\Mvc\View,
+    Phalcon\Mvc\Model\Resultset;
+
 class BenchController extends \Phalcon\Mvc\Controller
 {
     public function initialize()
     {
         // views must be renderd explicitly. safes processing time when not needed.
-        $this->view->disable();
+        $this->view->setRenderLevel(View::LEVEL_LAYOUT);
     }
 
     public function jsonAction() {
@@ -15,7 +18,9 @@ class BenchController extends \Phalcon\Mvc\Controller
     }
 
     public function dbAction() {
-        $queries = $this->getQueryOrDefault('queries', 1);
+
+        $queries = $this->request->getQuery('queries', null, 1);
+
         $worlds = array();
 
         for ($i = 0; $i < $queries; ++$i) {
@@ -26,50 +31,40 @@ class BenchController extends \Phalcon\Mvc\Controller
     }
 
     public function fortunesAction() {
+
         // since the resultset is immutable get an array instead
         // so we can add the new fortune
         $fortunes = Fortunes::find()->toArray();
+
         $fortunes[] = array(
             'id' => 0,
             'message' => 'Additional fortune added at request time.'
         );
 
         usort($fortunes, function($left, $right) {
-            if ($left['message'] === $right['message']) {
+            $l = $left['message'];
+            $r = $right['message'];
+            if ($l === $r) {
                 return 0;
-            } else if ($left['message'] > $right['message']) {
-                return 1;
             } else {
-                return -1;
+                if ($l > $r) {
+                    return 1;
+                } else {
+                    return -1;
+                }
             }
         });
 
-        return $this->sendContentAsText(
-            $this->view->getRender('bench', 'fortunes', array(
-                'fortunes' => $fortunes
-            ))
-        );
-    }
+        $this->response->setStatusCode(200, "OK");
+        $this->response->setHeader("Content-Type", "text/html; charset=utf-8");
 
-    private function getQueryOrDefault($query, $default) {
-        return $this->request->getQuery($query) !== null
-            ? $this->request->getQuery($query)
-            : $default;
-    }
-
-    private function sendContentAsText($content) {
-        $response = new Phalcon\Http\Response();
-        $response->setStatusCode(200, "OK");
-        $response->setHeader("Content-Type", "text/html; charset=utf-8");
-        $response->setContent($content);
-        return $response;
+        $this->view->fortunes = $fortunes;
     }
 
     private function sendContentAsJson($content) {
-        $response = new Phalcon\Http\Response();
+        $response = new Phalcon\Http\Response(json_encode($content));
         $response->setStatusCode(200, "OK");
         $response->setHeader("Content-Type", "application/json");
-        $response->setContent(json_encode($content));
         return $response;
     }
 }
