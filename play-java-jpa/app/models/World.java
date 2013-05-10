360bd545a62264001171e73619fd995c3809ffab
Nilanjan Raychaudhuri <nraychaudhuri@gmail.com>
2013-05-10 11:54:59 +1000

    Using JPA

---
+++ play-java-jpa/app/models/World.java
@@ -0,0 +1,24 @@
+package models;
+
+import javax.persistence.*;
+
+import play.db.jpa.JPA;
+import play.db.jpa.Transactional;
+
+@Entity
+public class World {
+
+    @Id
+    public Long id;
+
+    @Column(name = "randomNumber")
+    public Long randomNumber;
+
+    public static World findById(final Long id) throws Throwable {
+      return JPA.withTransaction(new play.libs.F.Function0<World>() {
+        public World apply() {
+            return JPA.em().find(World.class, id);
+        }
+      });
+    }
+}
