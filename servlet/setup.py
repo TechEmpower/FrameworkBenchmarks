0e0fc9eb5aab4714f987602b44698fb26fc56b9e
Pēteris Ņikiforovs <p@mak.id.lv>
2013-05-12 15:23:41 +0300

    Java on Windows

--- servlet/setup.py
+++ servlet/setup.py
@@ -1,6 +1,7 @@
 
 import subprocess
 import sys
+import os
 import setup_util
 
 def start(args):
@@ -8,6 +9,12 @@ def start(args):
 
   try:
     subprocess.check_call("mvn clean compile war:war", shell=True, cwd="servlet")
+    if os.name == 'nt':
+      subprocess.check_call("rmdir /s /q C:\\Java\\resin\\webapps", shell=True)
+      subprocess.check_call("mkdir C:\\Java\\resin\\webapps", shell=True)
+      subprocess.check_call("cp servlet\\target\\servlet.war C:\\Java\\resin\\webapps\\servlet.war", shell=True)
+      subprocess.check_call("C:\\Java\\resin\\bin\\start.bat", shell=True)
+      return 0
     subprocess.check_call("rm -rf $RESIN_HOME/webapps/*", shell=True)
     subprocess.check_call("cp servlet/target/servlet.war $RESIN_HOME/webapps/", shell=True)
     subprocess.check_call("$RESIN_HOME/bin/resinctl start", shell=True)
@@ -16,6 +23,9 @@ def start(args):
     return 1
 def stop():
   try:
+    if os.name == 'nt':
+      subprocess.check_call("C:\\Java\\resin\\bin\\stop.bat", shell=True)
+      return 0
     subprocess.check_call("$RESIN_HOME/bin/resinctl shutdown", shell=True)
     return 0
   except subprocess.CalledProcessError:
