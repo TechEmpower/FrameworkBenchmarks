3b115be15eb3be91f35f2c48d540b9e1b4c57ecc
Patrick Falls <pfalls@techempower.com>
2013-05-13 10:30:20 -0700

    setup wasn't working for me unless i broke out the make and ./run_application commands

--- cpoll_cppsp/setup.py
+++ cpoll_cppsp/setup.py
@@ -5,7 +5,8 @@ import setup_util
 
 def start(args):
   setup_util.replace_text("cpoll_cppsp/www/connectioninfo.H", "\\#define BENCHMARK_DB_HOST \".*\"", "#define BENCHMARK_DB_HOST \"" + args.database_host + "\"")
-  subprocess.Popen("make && ./run_application \"$(pwd)\"/www -g g++-4.8 -m /forcedynamic.cppsm", shell=True, cwd="cpoll_cppsp");
+  subprocess.check_call("make", shell=True, cwd="cpoll_cppsp")
+  subprocess.Popen("./run_application \"$(pwd)\"/www -g g++-4.8 -m /forcedynamic.cppsm", shell=True, cwd="cpoll_cppsp");
   return 0
 
 def stop():
