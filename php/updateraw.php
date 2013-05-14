844f73ea70f6fc69c1515e6c9930b1165d99de5c
Patrick Falls <pfalls@techempower.com>
2013-05-14 11:07:38 -0700

    Error in prepared statment

--- php/updateraw.php
+++ php/updateraw.php
@@ -26,6 +26,7 @@ $statement->bindParam(':id', $id, PDO::PARAM_INT);
 
 $updateStatement = $pdo->prepare('UPDATE World SET randomNumber = :randomNumber WHERE id = :id');
 $updateStatement->bindParam(':id', $id, PDO::PARAM_INT);
+$updateStatement->bindParam(':randomNumber', $randomNumber, PDO::PARAM_INT);
 
 // For each query, store the result set values in the response array
 while (0 < $query_count--) {
