a8d8502e8eb430265ca7cbcae4bae4ac3ab58952
Yogthos <dmitri.sotnikov@gmail.com>
2013-05-19 00:24:55 -0400

    added Luminus benchmark

---
+++ luminus/README.md
@@ -0,0 +1,33 @@
+# Compojure Benchmarking Test
+
+This is the Compojure portion of a [benchmarking test suite](../) comparing a variety of web development platforms.
+
+### JSON Encoding Test
+
+* [JSON test source](hello/src/hello/handler.clj)
+
+### Data-Store/Database Mapping Test
+
+* [Database test source](hello/src/hello/handler.clj)
+
+## Infrastructure Software Versions
+The dependencies are documented in [project.clj](hello/project.clj),
+but the main ones are:
+
+* [Clojure 1.5.1](http://clojure.org/)
+* [Compojure 1.1.5](https://github.com/weavejester/compojure)
+* [Ring-JSON 0.2.0](https://github.com/ring-clojure/ring-json), which in turn uses [Cheshire](https://github.com/dakrone/cheshire), which in turn uses [Jackson](http://jackson.codehaus.org/)
+* [Korma 0.3.0-RC5](http://sqlkorma.com/)
+
+## Test URLs
+### JSON Encoding Test
+
+http://localhost/hello-compojure-standalone/json
+
+### Data-Store/Database Mapping Test
+
+http://localhost/hello-compojure-standalone/db
+
+### Variable Query Test
+
+http://localhost/hello-compojure-standalone/db/2
---
+++ luminus/hello/src/hello/repl.clj
@@ -0,0 +1,34 @@
+(ns hello.repl
+  (:use hello.handler
+        ring.server.standalone
+        [ring.middleware file-info file]))
+
+(defonce server (atom nil))
+
+(defn get-handler []
+  ;; #'app expands to (var app) so that when we reload our code,
+  ;; the server is forced to re-resolve the symbol in the var
+  ;; rather than having its own copy. When the root binding
+  ;; changes, the server picks it up without having to restart.
+  (-> #'app
+    ; Makes static assets in $PROJECT_DIR/resources/public/ available.
+    (wrap-file "resources")
+    ; Content-Type, Content-Length, and Last Modified headers for files in body
+    (wrap-file-info)))
+
+(defn start-server
+  "used for starting the server in development mode from REPL"
+  [& [port]]
+  (let [port (if port (Integer/parseInt port) 8080)]
+    (reset! server
+            (serve (get-handler)
+                   {:port port
+                    :init init
+                    :auto-reload? true
+                    :destroy destroy
+                    :join true}))
+    (println (str "You can view the site at http://localhost:" port))))
+
+(defn stop-server []
+  (.stop @server)
+  (reset! server nil))
