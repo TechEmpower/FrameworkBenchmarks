a4db953080b57ce327f4505bdca0b5c2ba15c215
Kevin Pullin <kevin.pullin@gmail.com>
2013-05-23 22:05:30 -0700

    Added the .NET Nancy framework
    Added the .NET Nancy framework to the benchmark suite.
    
    This is based off the `aspnet` and `servicestack` examples and
    implements the first three benchmark cases (json, single db query, and
    multi db query) using the MySQL backend.

---
+++ nancy/src/DbModule.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using MySql.Data.MySqlClient;
+using Nancy;
+using Dapper;
+
+namespace NancyBenchmark
+{
+    public class DbModule : NancyModule
+    {
+        private static readonly string MYSQL_CONNECTION_STRING;
+        
+        static DbModule()
+        {
+            MYSQL_CONNECTION_STRING = ConfigurationManager.AppSettings["ConnectionString.MySQL"];
+        }
+
+        public DbModule() : base("/db")
+        {
+            Get["/{queries?1}"] = paramz =>
+            {
+                var queries = (int)paramz.queries;
+                
+                var random = new Random();
+                using (var db = new MySqlConnection(MYSQL_CONNECTION_STRING))
+                {
+                    db.Open();
+
+                    if (queries == 1)
+                        return GetRandomWorld(db, random);
+                    else
+                    {
+                        var worldCount = queries > 500 ? 500 : queries;
+                        worldCount = worldCount < 1 ? 1 : worldCount;
+
+                        // NOTE: Experiment with running the DB requests in parallel, on both Mono and Windows CLRs.
+                        var worlds = new World[worldCount];
+
+                        for (int i = 0; i < worldCount; ++i)
+                        {
+                            worlds[i] = GetRandomWorld(db, random);
+                        }
+                        return worlds;
+                    }
+                }
+            };
+        }
+
+        private World GetRandomWorld(IDbConnection db, Random random)
+        {
+            var id = random.Next(1, 10001);
+            return db.Query<World>("SELECT id, randomNumber FROM world WHERE id = @id", new { id = id }).Single();
+        }
+    }
+
+    public class World
+    {
+        public int id { get; set; }
+        public int randomNumber { get; set; }
+    }
+}
