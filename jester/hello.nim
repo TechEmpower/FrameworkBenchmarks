6070ce561ae1ff4d7a4c96505135c6d7f9d238ae
Dominik Picheta <dominikpicheta@googlemail.com>
2013-06-01 20:38:03 +0100

    Improved CPU utilization of the jester benchmark.

--- jester/hello.nim
+++ jester/hello.nim
@@ -1,9 +1,9 @@
-import jester, strtabs, json, asyncio, sockets
+import jester, strtabs, json, asyncio, sockets, os, strutils
 
 get "/json":
   var obj = %{"message": %"Hello, World!"}
   resp($obj, "application/json")
 
 var disp = newDispatcher()
-disp.register(port = TPort(9000), http=false)
+disp.register(port = TPort(paramStr(1).parseInt), http=false)
 while disp.poll(): nil
