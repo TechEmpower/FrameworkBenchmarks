2a582bcedd90bed52f762706bb4720ab0c157f95
INADA Naoki <songofacandy@gmail.com>
2013-06-02 14:31:04 +0000

    Use persistent connection.

--- django-stripped/hello/hello/settings.py
+++ django-stripped/hello/hello/settings.py
@@ -15,8 +15,9 @@ DATABASES = {
         'NAME': 'hello_world',                      # Or path to database file if using sqlite3.
         'USER': 'benchmarkdbuser',                      # Not used with sqlite3.
         'PASSWORD': 'benchmarkdbpass',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'CONN_MAX_AGE': 30,
     }
 }
 
