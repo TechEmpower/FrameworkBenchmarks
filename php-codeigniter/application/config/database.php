b992c9a371538ddf51156a080f53c48a01ed5af1
Arhimandril <nirdala@myopera.com>
2013-07-05 23:11:25 +0200

    Disable CodeIgniter query profiling.
    The 'save_queries' option is used exclusively by the profiler, but is enabled even when the profiler isn't running, adding some unnecessary overhead.

--- php-codeigniter/application/config/database.php
+++ php-codeigniter/application/config/database.php
@@ -63,6 +63,7 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
+$db['default']['save_queries'] = FALSE;
 
 
 /* End of file database.php */
