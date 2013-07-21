64f6e8c724fd9ce982b7b7b5afe442b9c42f123a
INADA Naoki <inada-n@klab.com>
2013-07-21 17:26:34 +0000

    wsgi: Fix setup

--- wsgi/setup.py
+++ wsgi/setup.py
@@ -1,13 +1,18 @@
 import subprocess
 import setup_util
+import multiprocessing
+import os
+
+bin_dir = os.path.expanduser('~/FrameworkBenchmarks/installs/py2/bin')
+NCPU = multiprocessing.cpu_count()
 
 proc = None
 
 
 def start(args):
     proc = subprocess.Popen(
-        "gunicorn hello:app -k meinheld.gmeinheld.MeinheldWorker -b 0.0.0.0:8080 -w " +
-        str((args.max_threads * 2)) + " --preload --log-level=critical", shell=True, cwd="wsgi")
+        bin_dir + "/gunicorn hello:app -k meinheld.gmeinheld.MeinheldWorker -b 0.0.0.0:8080 -w " +
+        str(NCPU) + " --preload --log-level=critical", shell=True, cwd="wsgi")
     return 0
 
 def stop():
