90880d999c8eb234a107b52d6df138431b5ea58d
INADA Naoki <inada-n@klab.com>
2013-07-21 18:04:16 +0900

    wsgi: Add plaintext test.

--- wsgi/setup.py
+++ wsgi/setup.py
@@ -1,22 +1,19 @@
-
 import subprocess
-import sys
 import setup_util
-import os
+
+proc = None
+
 
 def start(args):
-  subprocess.Popen('gunicorn hello:app --worker-class="egg:meinheld#gunicorn_worker" -b 0.0.0.0:8080 -w '
-                   + str((args.max_threads * 2)) + " --log-level=critical", shell=True, cwd="wsgi")
-  return 0
-def stop():
-  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  for line in out.splitlines():
-    if 'gunicorn' in line:
-      try:
-        pid = int(line.split(None, 2)[1])
-        os.kill(pid, 9)
-      except OSError:
-        pass
+    proc = subprocess.Popen(
+        "gunicorn hello:app -k meinheld.gmeinheld.MeinheldWorker -b 0.0.0.0:8080 -w " +
+        str((args.max_threads * 2)) + " --preload --log-level=critical", shell=True, cwd="wsgi")
+    return 0
 
-  return 0
+def stop():
+    global proc
+    if proc is None:
+        return 0
+    proc.terminate()
+    proc = None
+    return 0
