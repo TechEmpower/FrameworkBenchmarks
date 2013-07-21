d9af5c55194c157f835e18391a5f6a848f5bc8f7
INADA Naoki <inada-n@klab.com>
2013-07-21 17:45:52 +0900

    tornado: update setup scripts.

--- tornado/setup_pypy.py
+++ tornado/setup_pypy.py
@@ -1,31 +1,25 @@
 import subprocess
-import sys
 import setup_util
-import os
 from os.path import expanduser
 
-home = expanduser("~")
-cwd = "%s/FrameworkBenchmarks/tornado" % home
-pypy = "%s/FrameworkBenchmarks/installs/pypy-2.0.2/bin/pypy" % home
+python = expanduser('~/FrameworkBenchmarks/installs/pypy/bin/python')
+cwd = expanduser('~/FrameworkBenchmarks/tornado')
+proc = None
 
 
 def start(args):
+    global proc
     setup_util.replace_text(
         cwd + "/server.py", "localhost", args.database_host)
 
-    subprocess.Popen(pypy + " %s/FrameworkBenchmarks/tornado/server.py --port=8080 --logging=error" % home, shell=True, cwd=cwd)
+    proc = subprocess.Popen(
+        python + " server.py --port=8080 --logging=error",
+        shell=True, cwd=cwd)
     return 0
 
-
 def stop():
-    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-    out, err = p.communicate()
-    for line in out.splitlines():
-        if 'server.py' in line:
-            #try:
-            pid = int(line.split(None, 2)[1])
-            os.kill(pid, 9)
-            #except OSError:
-            #    pass
-
+    global proc
+    if proc:
+        proc.terminate()
+        proc = None
     return 0
