eab53a36d06285d43edb81306b2b693f8677e4ed
INADA Naoki <inada-n@klab.com>
2013-07-21 15:34:12 +0900

    Flask: Update setup files.

--- flask/setup_py3.py
+++ flask/setup_py3.py
@@ -5,9 +5,11 @@ proc = None
 
 
 def start(args):
+    global proc
     setup_util.replace_text("flask/app.py", "DBHOSTNAME", args.database_host)
     proc = subprocess.Popen(
-        "gunicorn app:app -k meinheld.gmeinheld.MeinheldWorker -b 0.0.0.0:8080 -w " +
+        "~/FrameworkBenchmarks/installs/python-3.3.2/bin/gunicorn " +
+        "app:app -k meinheld.gmeinheld.MeinheldWorker -b 0.0.0.0:8080 -w " +
         str((args.max_threads * 2)) + " --preload --log-level=critical", shell=True, cwd="flask")
     return 0
 
--- flask/setup_pypy.py
+++ flask/setup_pypy.py
@@ -1,25 +1,22 @@
 import subprocess
-import sys
 import setup_util
-import os
 
 proc = None
 
+
 def start(args):
-  global proc
-  setup_util.replace_text("flask/app.py", "DBHOSTNAME", args.database_host)
-  proc = subprocess.Popen("~/FrameworkBenchmarks/installs/pypy-2.0.2/bin/pypy run_pypy.py --port=8080 --logging=error",
-                          shell=True, cwd="flask")
-  return 0
+    global proc
+    setup_util.replace_text("flask/app.py", "DBHOSTNAME", args.database_host)
+    proc = subprocess.Popen(
+        "~/FrameworkBenchmarks/installs/pypy-2.0.2/bin/gunicorn " +
+        "app:app -k tornado -b 0.0.0.0:8080 -w " +
+        str((args.max_threads * 2)) + " --preload --log-level=critical", shell=True, cwd="flask")
+    return 0
 
 def stop():
-  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  for line in out.splitlines():
-    if 'pypy' in line and 'run-tests' not in line:
-      try:
-        pid = int(line.split(None, 2)[1])
-        os.kill(pid, 9)
-      except OSError:
-        pass
-  return 0 
+    global proc
+    if proc is None:
+        return 0
+    proc.terminate()
+    proc = None
+    return 0
