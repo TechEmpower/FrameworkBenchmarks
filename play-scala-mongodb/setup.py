073afc2387266deb8ed975e65876c60133d28bcf
Kevin Pullin <kevin.pullin@gmail.com>
2013-07-23 06:58:34 +0000

    Add Windows support for Scala & Java frameworks
    Added support for most (all?) remaining Scala & Java based frameworks.

--- play-scala-mongodb/setup.py
+++ play-scala-mongodb/setup.py
@@ -3,23 +3,37 @@ import subprocess
 import sys
 import setup_util
 import os
+from zipfile import ZipFile
 
 def start(args):
   setup_util.replace_text("play-scala-mongodb/conf/application.conf", "localhost:27017", "" + args.database_host + ":27017")
 
   subprocess.check_call("play dist", shell=True, cwd="play-scala-mongodb")
-  subprocess.check_call("unzip play-scala-mongodb-1.0-SNAPSHOT.zip", shell=True, cwd="play-scala-mongodb/dist")
-  subprocess.check_call("chmod +x start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
-  subprocess.Popen("./start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
+  
+  if os.name == 'nt':
+    ZipFile("./play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT.zip").extractall("./play-scala-mongodb/dist")
+    with open("./play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT/start.bat", "w+") as f:
+      f.write("java %1 -cp \"./lib/*;\" play.core.server.NettyServer .")
+    subprocess.Popen("start.bat", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
+  else:
+    subprocess.check_call("unzip play-scala-mongodb-1.0-SNAPSHOT.zip", shell=True, cwd="play-scala-mongodb/dist")
+    subprocess.check_call("chmod +x start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
+    subprocess.Popen("./start", shell=True, cwd="play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT")
 
   return 0
 def stop():
-  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  for line in out.splitlines():
-    if './start' in line or ('play' in line and 'java' in line):
-      pid = int(line.split(None, 2)[1])
+  if os.name == 'nt':
+    with open("./play-scala-mongodb/dist/play-scala-mongodb-1.0-SNAPSHOT/RUNNING_PID") as f:
+      pid = int(f.read())
       os.kill(pid, 9)
+  else:
+    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+      if './start' in line or ('play' in line and 'java' in line):
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+        
   try:
     os.remove("play-scala-mongodb/RUNNING_PID")
   except OSError:
--- spray/setup.py
+++ spray/setup.py
@@ -5,20 +5,27 @@ import time
 import os
 
 def start(args=None):
+  if os.name == 'nt':
+    subprocess.check_call('"..\\sbt\\sbt.bat" assembly', shell=True, cwd="spray")
+  else:
     subprocess.check_call("../sbt/sbt assembly", shell=True, cwd="spray")
-    subprocess.Popen("java -jar target/scala-2.10/spray-benchmark-assembly-1.0.jar", cwd="spray", shell=True)
-    time.sleep(5)
-    return 0
+    
+  subprocess.Popen("java -jar target/scala-2.10/spray-benchmark-assembly-1.0.jar", cwd="spray", shell=True)  
+  time.sleep(5)
+  return 0
 
 def stop():
-  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
-  out, err = p.communicate()
-  for line in out.splitlines():
-    if 'spray-benchmark' in line:
-      try:
-        pid = int(line.split(None, 2)[1])
-        os.kill(pid, 9)
-      except OSError:
-        pass
+  if os.name == 'nt':
+    subprocess.check_call("wmic process where \"CommandLine LIKE '%spray-benchmark%'\" call terminate")
+  else:
+    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+      if 'spray-benchmark' in line:
+        try:
+          pid = int(line.split(None, 2)[1])
+          os.kill(pid, 9)
+        except OSError:
+          pass
   
   return 0
