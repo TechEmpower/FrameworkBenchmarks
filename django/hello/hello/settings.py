22691d2faa140f1347e84c4691e57b9fcee50258
haleyyoung <haleyyoung88@gmail.com>
2013-07-24 14:14:03 -0700

    Add method to get list of frameworks, take out list of frameworks. Add output of commit_count to results.json under rawData.commitCounts

--- django/hello/hello/settings.py
+++ django/hello/hello/settings.py
@@ -15,7 +15,7 @@ DATABASES = {
         'NAME': 'hello_world',                      # Or path to database file if using sqlite3.
         'USER': 'benchmarkdbuser',                      # Not used with sqlite3.
         'PASSWORD': 'benchmarkdbpass',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'CONN_MAX_AGE': 30,
     }
@@ -109,7 +109,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "/home/ubuntu/FrameworkBenchmarks/django/hello/templates",
+    "/home/hyoung/FrameworkBenchmarks/django/hello/templates",
 )
 
 INSTALLED_APPS = (
