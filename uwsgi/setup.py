3e878ecec5107d655de5ccad5024d7b2fcb95a57
Damjan Georgievski <gdamjan@gmail.com>
2013-07-24 19:29:03 +0200

    added uwsgi for python with gevent loop engine

---
+++ uwsgi/setup.py
@@ -0,0 +1,17 @@
+
+import subprocess
+import sys
+import setup_util
+
+
+def start(args):
+  subprocess.Popen('uwsgi --gevent 1000 --http :8080 -w hello --pidfile /tmp/uwsgi.pid', shell=True, cwd="uwsgi")
+  return 0
+
+
+def stop():
+      try:
+        subprocess.Popen('uwsgi --stop /tmp/uwsgi.pid', shell=True, cwd="uwsgi")
+      except OSError:
+        pass
+  return 0
