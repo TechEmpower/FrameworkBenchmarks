000d533595d69034e825c20114cc9be8f9b39c64
INADA Naoki <inada-n@klab.com>
2013-07-25 00:53:10 +0000

    uwsgi: Remove uwsgi-py3 (gevent doesn't support Python 3 yet)

--- uwsgi/setup.py
+++ uwsgi/setup.py
@@ -1,14 +1,19 @@
-
+import multiprocessing
 import subprocess
 import sys
 import setup_util
 import os
+import time
 
 uwsgi = os.path.expanduser('~/FrameworkBenchmarks/installs/py2/bin/uwsgi')
-
+PROCS = multiprocessing.cpu_count()
 
 def start(args):
-    subprocess.Popen(uwsgi + ' -L --gevent 1000 --http :8080 -w hello --pidfile /tmp/uwsgi.pid', shell=True, cwd="uwsgi")
+    subprocess.Popen(
+        uwsgi + ' --master -L --gevent 1000 --http :8080 --http-keepalive ' +
+        '-p ' + str(PROCS) + ' -w hello --add-header "Connection: keep-alive" ' +
+        ' --pidfile /tmp/uwsgi.pid',
+        shell=True, cwd="uwsgi")
     return 0
 
 
@@ -17,4 +22,5 @@ def stop():
         subprocess.Popen(uwsgi + ' --stop /tmp/uwsgi.pid', shell=True, cwd="uwsgi")
     except OSError:
         pass
+    time.sleep(1)
     return 0
