e50af8634bdaf8ffa6c9512b8e4dd50030a1e9a2
roja <roja@vouchercloud.com>
2013-08-19 17:53:51 +0100

    add json.net and servicestack.text serialisers to each endpoint for a richer benchmark set

--- HttpListener/HttpListener/Program.cs
+++ HttpListener/HttpListener/Program.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Data.Common;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Threading;
@@ -13,9 +14,10 @@ using MongoDB.Driver.Builders;
 
 using Benchmarks.AspNet.Models;
 
+
 namespace HttpListener
 {
-    class Program
+    public class Program
     {
         private static void RequestCallback(Object state)
         {
@@ -25,38 +27,54 @@ namespace HttpListener
 
             try
             {
-                string responseString = null;
-                switch (request.Url.LocalPath)
+                var localPath = request.Url.LocalPath;
+
+                if (localPath == "/")
+                {
+                    NotFound(response);
+                    return;
+                }
+
+                switch (localPath)
                 {
                     case "/plaintext":
-                        responseString = Plaintext(response);
-                        break;
-                    case "/json":
-                        responseString = Json(response);
-                        break;
-                    case "/db":
-                        responseString = Db(request, response);
-                        break;
+                        Plaintext(response);
+                        return;
                     case "/fortunes":
-                        responseString = Fortunes(request, response);
-                        break;
-                    case "/updates":
-                        responseString = Updates(request, response);
-                        break;
-                    case "/mongodbdb":
-                        responseString = MongoDBDb(request, response);
-                        break;
+                        Fortunes(request, response);
+                        return;
                     case "/mongodbfortunes":
-                        responseString = MongoDBFortunes(request, response);
-                        break;
-                    case "/mongodbupdates":
-                        responseString = MongoDBUpdates(request, response);
-                        break;
-                    default:
-                        responseString = NotFound(response);
-                        break;
+                        MongoDBFortunes(request, response);
+                        return;
                 }
-                WriteResponse(response, responseString);
+
+                var serializer = GetSerializer(request);
+
+                switch (localPath)
+                {
+                    case "/json":
+                        Json(response, serializer);
+                        return;
+                    case "/db":
+                        Db(request, response, serializer);
+                        return;
+                    case "/updates":
+                        Updates(request, response, serializer);
+                        return;
+                    case "/mongodbdb":
+                        MongoDBDb(request, response, serializer);
+                        return;
+                    case "/mongodbupdates":
+                        MongoDBUpdates(request, response, serializer);
+                        return;
+                    default:
+                        NotFound(response);
+                        return;
+                }
+            }
+            catch (Exception exception)
+            {
+                Error(response, exception);
             }
             finally
             {
@@ -64,11 +82,64 @@ namespace HttpListener
             }
         }
 
-        private static void WriteResponse(HttpListenerResponse response, String responseString)
+        private static Action<object, HttpListenerResponse> GetSerializer(HttpListenerRequest request)
         {
-            response.ContentType = response.ContentType + "; charset=utf-8";
+            string serializerName = request.QueryString["serializer"];
+
+            if (serializerName == null)
+            {
+                throw new ApplicationException("Missing serializer querystring argument");
+            }
+
+            Action<object, HttpListenerResponse> serializer = null;
+            switch (serializerName)
+            {
+                case "jsonnet":
+                    serializer = JsonNetSerialiser;
+                    break;
+                case "servicestack":
+                    serializer = ServiceStackSerialiser;
+                    break;
+                case "default":
+                    serializer = DefaultJsonSerialiser;
+                    break;
+            }
+
+            if (serializer == null)
+            {
+                throw new ApplicationException("Valid serializer required (jsonnet|servicestack|default)");
+            }
+
+            return serializer;
+        }
+
+        private static void JsonNetSerialiser(Object obj, HttpListenerResponse response)
+        {
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(new { message = "Hello, World!" });
+
+            WriteResponse(response, json, "application/json; charset=utf-8");
+        }
+
+        private static void ServiceStackSerialiser(Object obj, HttpListenerResponse response)
+        {
+            var json = ServiceStack.Text.JsonSerializer.SerializeToString(new { message = "Hello, World!" });
+
+            WriteResponse(response, json, "application/json; charset=utf-8");
+        }
+
+        private static void DefaultJsonSerialiser(Object obj, HttpListenerResponse response)
+        {
+            var json = new JavaScriptSerializer().Serialize(new { message = "Hello, World!" });
+
+            WriteResponse(response, json, "application/json; charset=utf-8");
+        }
+
+        private static void WriteResponse(HttpListenerResponse response, String responseString, string contentType)
+        {
+            response.ContentType = contentType;
             byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
             response.ContentLength64 = buffer.Length;
+
             try
             {
                 response.OutputStream.Write(buffer, 0, buffer.Length);
@@ -98,7 +169,7 @@ namespace HttpListener
             }
         }
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Threads();
 
@@ -144,7 +215,7 @@ namespace HttpListener
             }
         }
 
-        public static DbConnection CreateConnection(HttpListenerRequest request)
+        private static DbConnection CreateConnection(HttpListenerRequest request)
         {
             string providerName = request.QueryString["provider"];
             if (providerName == null)
@@ -158,7 +229,7 @@ namespace HttpListener
             return connection;
         }
 
-        public static int GetQueries(HttpListenerRequest request)
+        private static int GetQueries(HttpListenerRequest request)
         {
             int queries = 1;
             string queriesString = request.QueryString["queries"];
@@ -171,26 +242,29 @@ namespace HttpListener
             return queries;
         }
 
-        private static string NotFound(HttpListenerResponse response)
+        private static void NotFound(HttpListenerResponse response)
         {
             response.StatusCode = (int)HttpStatusCode.NotFound;
-            response.ContentType = "text/plain";
-            return "not found";
+            WriteResponse(response, "not found", "text/plain; charset=utf-8");
         }
 
-        private static string Plaintext(HttpListenerResponse response)
+        private static void Error(HttpListenerResponse response, Exception exception)
         {
-            response.ContentType = "text/plain";
-            return "Hello, World!";
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            WriteResponse(response, exception.Message, "text/plain; charset=utf-8");
         }
 
-        private static string Json(HttpListenerResponse response)
+        private static void Plaintext(HttpListenerResponse response)
         {
-            response.ContentType = "application/json";
-            return new JavaScriptSerializer().Serialize(new { message = "Hello, World!" });
+            WriteResponse(response, "Hello, World!", "text/plain; charset=utf-8");
         }
 
-        private static string Db(HttpListenerRequest request, HttpListenerResponse response)
+        private static void Json(HttpListenerResponse response, Action<object, HttpListenerResponse> serializer)
+        {
+            serializer.Invoke(new { message = "Hello, World!" }, response);
+        }
+
+        private static void Db(HttpListenerRequest request, HttpListenerResponse response, Action<object, HttpListenerResponse> serializer)
         {
             Random random = new Random();
 
@@ -233,12 +307,10 @@ namespace HttpListener
                 }
             }
 
-            response.ContentType = "application/json";
-            return new JavaScriptSerializer().Serialize(
-                worlds.Count > 1 ? (Object)worlds : (Object)worlds[0]);
+            serializer(worlds.Count > 1 ? (Object)worlds : (Object)worlds[0], response);
         }
 
-        private static string Fortunes(HttpListenerRequest request, HttpListenerResponse response)
+        private static void Fortunes(HttpListenerRequest request, HttpListenerResponse response)
         {
             List<Fortune> fortunes = new List<Fortune>();
 
@@ -266,13 +338,11 @@ namespace HttpListener
 
             fortunes.Add(new Fortune { ID = 0, Message = "Additional fortune added at request time." });
             fortunes.Sort();
-
-            response.ContentType = "text/html";
             var template = new Fortunes { Model = fortunes };
-            return template.TransformText();
+            WriteResponse(response, template.TransformText(), "text/plain; charset=utf-8");
         }
 
-        private static string Updates(HttpListenerRequest request, HttpListenerResponse response)
+        private static void Updates(HttpListenerRequest request, HttpListenerResponse response, Action<object, HttpListenerResponse> serializer)
         {
             Random random = new Random();
             List<World> worlds = new List<World>(GetQueries(request));
@@ -335,12 +405,10 @@ namespace HttpListener
                 }
             }
 
-            response.ContentType = "application/json";
-            return new JavaScriptSerializer().Serialize(
-                worlds.Count > 1 ? (Object)worlds : (Object)worlds[0]);
+            serializer(worlds.Count > 1 ? (Object)worlds : (Object)worlds[0], response);
         }
 
-        private static string MongoDBDb(HttpListenerRequest request, HttpListenerResponse response)
+        private static void MongoDBDb(HttpListenerRequest request, HttpListenerResponse response, Action<object, HttpListenerResponse> serializer)
         {
             Random random = new Random();
 
@@ -355,12 +423,10 @@ namespace HttpListener
                 worlds.Add(db.Worlds.FindOne(Query<World>.EQ(w => w.id, randomID)));
             }
 
-            response.ContentType = "application/json";
-            return new JavaScriptSerializer().Serialize(
-                worlds.Count > 1 ? (Object)worlds : (Object)worlds[0]);
+            serializer(worlds.Count > 1 ? (Object)worlds : (Object)worlds[0], response);
         }
 
-        private static string MongoDBFortunes(HttpListenerRequest request, HttpListenerResponse response)
+        private static void MongoDBFortunes(HttpListenerRequest request, HttpListenerResponse response)
         {
             Benchmarks.AspNet.Models.MongoDB db = new Benchmarks.AspNet.Models.MongoDB("MongoDB");
 
@@ -369,12 +435,11 @@ namespace HttpListener
             fortunes.Add(new Fortune { ID = 0, Message = "Additional fortune added at request time." });
             fortunes.Sort();
 
-            response.ContentType = "text/html";
             var template = new Fortunes { Model = fortunes };
-            return template.TransformText();
+            WriteResponse(response, template.TransformText(), "text/plain; charset=utf-8");
         }
 
-        private static string MongoDBUpdates(HttpListenerRequest request, HttpListenerResponse response)
+        private static void MongoDBUpdates(HttpListenerRequest request, HttpListenerResponse response, Action<object, HttpListenerResponse> serializer)
         {
             Random random = new Random();
 
@@ -395,9 +460,7 @@ namespace HttpListener
                 db.Worlds.Save(world);
             }
 
-            response.ContentType = "application/json";
-            return new JavaScriptSerializer().Serialize(
-                worlds.Count > 1 ? (Object)worlds : (Object)worlds[0]);
+            serializer(worlds.Count > 1 ? (Object)worlds : (Object)worlds[0], response);
         }
     }
 }
