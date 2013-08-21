ee20502f8b68bd8654c3767d1bdd64478da340a9
INADA Naoki <songofacandy@gmail.com>
2013-08-21 02:30:51 +0000

    Don't use L10N and TZ also.

--- django/hello/hello/settings.py
+++ django/hello/hello/settings.py
@@ -42,10 +42,10 @@ USE_I18N = False
 
 # If you set this to False, Django will not format dates, numbers and
 # calendars according to the current locale.
-USE_L10N = True
+USE_L10N = False
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
