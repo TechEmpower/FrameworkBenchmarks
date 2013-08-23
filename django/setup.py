6b9ee2237779683798dfdce0e21087b43b20c0bf
INADA Naoki <songofacandy@gmail.com>
2013-08-23 10:03:13 +0000

    Fix missign environment variables.

--- django/setup.py
+++ django/setup.py
@@ -23,7 +23,8 @@ def start(args):
         "-b", "0.0.0.0:8080",
         '-w', str(NCPU*3),
         "--log-level=critical"],
-        cwd="django/hello")
+        cwd="django/hello",
+        env=env)
     return 0
 
 def stop():
