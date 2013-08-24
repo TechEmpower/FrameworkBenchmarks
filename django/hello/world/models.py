0c684a55e1ab7bf3bd396f89a8e8f50cd7876218
INADA Naoki <songofacandy@gmail.com>
2013-08-24 09:00:38 +0000

    Fix PostgreSQL table names.

--- django/hello/world/models.py
+++ django/hello/world/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-import os
-MYSQL = os.environ['DJANGO_DB'] == 'mysql'
-
 # Create your models here.
 
 class World(models.Model):
   randomnumber = models.IntegerField()
   class Meta:
-    db_table = 'World' if MYSQL else 'world'
+    db_table = 'World'
 
 class Fortune(models.Model):
   message = models.CharField(max_length=65535)
   class Meta:
-    db_table = 'Fortune' if MYSQL else 'fortune'
+    db_table = 'Fortune'
