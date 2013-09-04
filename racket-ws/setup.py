8f202fa2337c2a7b61caab9766a0588b33ff7075
Jay McCarthy <jay.mccarthy@gmail.com>
2013-09-04 09:58:24 -0600

    Adding Racket Web Server test

---
+++ racket-ws/setup.py
@@ -0,0 +1,24 @@
+
+import subprocess
+import sys
+import setup_util
+import os
+
+def start(args):
+  db_host = args.database_host
+  threads = str(args.max_threads)
+  subprocess.Popen("racket -t bench.rkt -- " + db_host + " > /dev/null", shell=True, cwd="racket-ws/bench")
+  return 0
+
+def stop():
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if 'bench' in line:
+      try:
+        pid = int(line.split(None, 2)[1])
+        os.kill(pid, 9)
+      except OSError:
+        pass
+
+  return 0
