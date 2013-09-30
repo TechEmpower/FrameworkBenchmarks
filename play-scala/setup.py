2120ef0884d0f78c2bffeaa302e43a38d55f00ba
Kevin Pullin <kevin.pullin@gmail.com>
2013-09-30 03:49:12 +0000

    Updated play-scala setup for Windows
    Updated the play-scala setup.py file to work with Windows (i.e. call
    `play.bat`).
    
    Fixed issues with the `stop()` method [Play2 no longer supports the
    `stop` command].

--- play-scala/setup.py
+++ play-scala/setup.py
@@ -1,12 +1,33 @@
 import setup_util
 import subprocess
+import os
 
 def start(args):
+  kill_running_process() # Kill the running process and delete the 
+                         # RUNNING_PID file (if any). With any luck no 
+                         # new process has picked up the same PID.
+
+  play_cmd = "play"
+  if args.os.lower() == "windows":
+    play_cmd = "play.bat"
+  
   setup_util.replace_text("play-scala/conf/application.conf", "jdbc:mysql:\/\/.*:3306", "jdbc:mysql://" + args.database_host + ":3306")
-  subprocess.Popen(["play","start"], stdin=subprocess.PIPE, cwd="play-scala")
+  subprocess.Popen([play_cmd,"start"], stdin=subprocess.PIPE, cwd="play-scala")
   return 0
 
 def stop():
-  p = subprocess.Popen(["play","stop"], cwd="play-scala")
-  p.communicate()
+  kill_running_process()  
   return 0
+
+def kill_running_process():
+  try:
+    with open("./play-scala/RUNNING_PID") as f:
+      pid = int(f.read())
+      os.kill(pid, 9)
+  except:
+  	pass
+
+  try:
+    os.remove("play-scala/RUNNING_PID")
+  except OSError:
+    pass  
