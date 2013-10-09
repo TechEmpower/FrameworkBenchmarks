3c8dbdf464f4da7a7114a7f830a82d56609f5519
Michael Robertson <mrobertson@techempower.com>
2013-10-09 14:46:43 -0700

    Fixed the 404 errors for silex-raw (updated the URLs). Compiled files for php-silex and php-phpixie.

--- php-phpixie/assets/config/db.php
+++ php-phpixie/assets/config/db.php
@@ -7,10 +7,10 @@ return array(
 		'driver' => 'mysql',
 		
 		//'Connection' is required if you use the PDO driver
-		'connection'=>'mysql:host=localhost;dbname=hello_world',
+		'connection'=>'mysql:host=172.16.98.120;dbname=hello_world',
 		
 		// 'db' and 'host' are required if you use Mysqli driver
 		'db' => 'hello_world',
-		'host'=>'localhost'
+		'host'=>'172.16.98.120'
 	)
 );
