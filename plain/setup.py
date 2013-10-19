56b3e31432b6dc1357716be111a83d2d5488bc37
tfb <test@techempower.com>
2013-10-18 17:07:51 -0700

    Fixed plain's setup.py to correctly update the DB hostname

--- plain/setup.py
+++ plain/setup.py
@@ -1,10 +1,11 @@
-
+import setup_util
 import subprocess
 import sys
 import time
 import os
 
 def start(args=None):
+    setup_util.replace_text("plain/src/main/resources/application.conf", "127.0.0.1", args.database_host)
     subprocess.check_call("./sbt assembly", shell=True, cwd="plain")
     subprocess.Popen("java -server -Xnoclassgc -XX:MaxPermSize=1g -XX:ReservedCodeCacheSize=384m -Xmx8g -Xss8m -Xmn4g -jar target/scala-2.10/plain-benchmark-assembly-1.0.1.jar", cwd="plain", shell=True)
     time.sleep(10)
