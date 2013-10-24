fdeb09c376b4b20f9def653551a9cfe64c1860a2
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2013-10-24 12:54:21 +0900

    Updated to treefrog framework v1.7.2.

--- toolset/setup/linux/installer.py
+++ toolset/setup/linux/installer.py
@@ -274,14 +274,14 @@ class Installer:
     # TreeFrog Framework
     #
     self.__run_command("sudo apt-get install qt4-qmake libqt4-dev libqt4-sql-mysql g++", True)
-    self.__download("http://downloads.sourceforge.net/project/treefrog/src/treefrog-1.7.tar.gz")
-    self.__run_command("tar xzf treefrog-1.7.tar.gz")
-    self.__run_command("rm treefrog-1.7.tar.gz")
-    self.__run_command("./configure", cwd="treefrog-1.7")
-    self.__run_command("make", cwd="treefrog-1.7/src")
-    self.__run_command("sudo make install", cwd="treefrog-1.7/src")
-    self.__run_command("make", cwd="treefrog-1.7/tools")
-    self.__run_command("sudo make install", cwd="treefrog-1.7/tools")
+    self.__download("http://downloads.sourceforge.net/project/treefrog/src/treefrog-1.7.2.tar.gz")
+    self.__run_command("tar xzf treefrog-1.7.2.tar.gz")
+    self.__run_command("rm treefrog-1.7.2.tar.gz")
+    self.__run_command("./configure", cwd="treefrog-1.7.2")
+    self.__run_command("make", cwd="treefrog-1.7.2/src")
+    self.__run_command("sudo make install", cwd="treefrog-1.7.2/src")
+    self.__run_command("make", cwd="treefrog-1.7.2/tools")
+    self.__run_command("sudo make install", cwd="treefrog-1.7.2/tools")
 
     #
     # Vert.x
