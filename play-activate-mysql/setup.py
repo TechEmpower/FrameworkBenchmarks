06e21c22643c8e89427f29e6e83a9f8fdbd1461f
Flavio W. Brasil <fwbrasil@gmail.com>
2013-10-24 13:20:35 +0200

    update play-activate-mysql to play 2.2 / update setup.py files to play 2.2

--- play-activate-mysql/setup.py
+++ play-activate-mysql/setup.py
@@ -8,34 +8,34 @@ from zipfile import ZipFile
 def start(args):
   setup_util.replace_text("play-activate-mysql/conf/application.conf", "jdbc:mysql:\/\/.*:3306", "jdbc:mysql://" + args.database_host + ":3306")
 
-  subprocess.check_call("play dist", shell=True, cwd="play-activate-mysql")
+  subprocess.check_call("play clean dist", shell=True, cwd="play-activate-mysql")
 
   if os.name == 'nt':
-    ZipFile("./play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT.zip").extractall("./play-activate-mysql/dist")
-    with open("./play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT/start.bat", "w+") as f:
+    ZipFile("./play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT.zip").extractall("./play-activate-mysql/target/universal")
+    with open("./play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/bin/play-activate-mysql.bat", "w+") as f:
       f.write("java %1 -cp \"./lib/*;\" play.core.server.NettyServer .")
-    subprocess.Popen("start.bat", shell=True, cwd="play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT")
+    subprocess.Popen("play-activate-mysql.bat", shell=True, cwd="play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/bin")
   else:
-    subprocess.check_call("unzip play-activate-mysql-1.0-SNAPSHOT.zip", shell=True, cwd="play-activate-mysql/dist")
-    subprocess.check_call("chmod +x start", shell=True, cwd="play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT")
-    subprocess.Popen("./start", shell=True, cwd="play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT")
+    subprocess.check_call("unzip play-activate-mysql-1.0-SNAPSHOT.zip", shell=True, cwd="play-activate-mysql/target/universal")
+    subprocess.check_call("chmod +x play-activate-mysql", shell=True, cwd="play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/bin")
+    subprocess.Popen("./play-activate-mysql", shell=True, cwd="play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/bin")
 
   return 0
 def stop():
   if os.name == 'nt':
-    with open("./play-activate-mysql/dist/play-activate-mysql-1.0-SNAPSHOT/RUNNING_PID") as f:
+    with open("./play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/RUNNING_PID") as f:
       pid = int(f.read())
       os.kill(pid, 9)
   else:
     p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
     out, err = p.communicate()
     for line in out.splitlines():
-      if './start' in line or ('play' in line and 'java' in line):
+      if 'NettyServer' in line:
         pid = int(line.split(None, 2)[1])
         os.kill(pid, 9)
 
   try:
-    os.remove("play-activate-mysql/RUNNING_PID")
+    os.remove("play-activate-mysql/target/universal/play-activate-mysql-1.0-SNAPSHOT/RUNNING_PID")
   except OSError:
     pass
 
