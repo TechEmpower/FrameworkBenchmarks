e93f0a0ac70b9e93deeffd935ba54c790fc6e2f4
Flavio W. Brasil <fwbrasil@gmail.com>
2013-10-29 17:25:19 +0100

    fall back to find the play process

--- play-activate-mysql/setup.py
+++ play-activate-mysql/setup.py
@@ -30,7 +30,7 @@ def stop():
     p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
     out, err = p.communicate()
     for line in out.splitlines():
-      if 'NettyServer' in line:
+      if 'NettyServer' in line or ('netty' in line and 'java' in line):
         pid = int(line.split(None, 2)[1])
         os.kill(pid, 9)
 
