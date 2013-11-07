5e1af6c96ae5fe0b03441a580664884cca17f81a
Lari Hotari <lari.hotari@sagire.fi>
2013-11-07 20:54:24 +0200

    Use same urls for development

--- grails/hello/grails-app/conf/Config.groovy
+++ grails/hello/grails-app/conf/Config.groovy
@@ -13,6 +13,8 @@
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 
+grails.app.context = '/grails'
+
 // The ACCEPT header will not be used for content negotiation for user agents containing the following strings (defaults to the 4 major rendering engines)
 grails.mime.disable.accept.header.userAgents = ['Gecko', 'WebKit', 'Presto', 'Trident']
 grails.mime.types = [
