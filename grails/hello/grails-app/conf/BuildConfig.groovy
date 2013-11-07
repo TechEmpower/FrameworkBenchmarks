679e9f58209bf52d56be847a4018cfefe088d111
Lari Hotari <lari.hotari@sagire.fi>
2013-11-07 12:36:35 +0200

    Make it easier to profile the app with Yourkit Java Profiler (yjp)

--- grails/hello/grails-app/conf/BuildConfig.groovy
+++ grails/hello/grails-app/conf/BuildConfig.groovy
@@ -21,6 +21,14 @@ grails.project.fork = [
     console: [maxMemory: 768, minMemory: 64, debug: false, maxPerm: 256]
 ]
 
+def yjpConfig = [jvmArgs: [
+        "-agentpath:/opt/yjp/bin/linux-x86-64/libyjpagent.so=delay=30000,disablealloc,disablej2ee,noj2ee,builtinprobes=none,sampling,monitors,onexit=snapshot,telemetryperiod=250"
+    ]]
+if (System.getProperty("grails.yjp")) {
+    grails.project.fork.war += yjpConfig
+    println "Using YJP for run-war"
+}
+
 grails.project.dependency.resolver = "maven" // or ivy
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
