e8f6b725668116bd5a3fe1c91894b30a69a52d5d
Mike Smith <msmith@techempower.com>
2013-11-20 10:39:18 -0800

    kekekeks-evhttp-sharp Updated setup.py for logging
    Added stderr/stdout file handles to the start/stop scripts.

--- evhttp-sharp/setup.py
+++ evhttp-sharp/setup.py
@@ -6,21 +6,21 @@ import os
 root = os.getcwd() + "/evhttp-sharp"
 app = root + "/src"
 
-def start(args):
+def start(args, logfile, errfile):
   if os.name == 'nt':
     return 1
 
   try:
     # build
-    subprocess.check_call("rm -rf bin obj", shell=True, cwd=app)
-    subprocess.check_call("xbuild /p:Configuration=Release", shell=True, cwd=app)
+    subprocess.check_call("rm -rf bin obj", shell=True, cwd=app, stdout=logfile, stderr=errfile)
+    subprocess.check_call("xbuild /p:Configuration=Release", shell=True, cwd=app stdout=logfile, stderr=errfile)
     
-    subprocess.Popen("mono -O=all bin/Release/EvHttpSharpBenchmark.exe 127.0.0.1 8085 " + str(args.max_threads) + " &", shell=True, cwd=app)
+    subprocess.Popen("mono -O=all bin/Release/EvHttpSharpBenchmark.exe 127.0.0.1 8085 " + str(args.max_threads) + " &", shell=True, cwd=app, stdout=logfile, stderr=errfile)
     return 0
   except subprocess.CalledProcessError:
     return 1
 
-def stop():
+def stop(logfile, errfile):
   if os.name == 'nt':
     return 0
   
