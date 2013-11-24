357642ff5300e4442eb00e7b2c2573d1577aaaa9
oleksiys <oleksiys@java.net>
2013-11-24 07:20:36 -0800

    + upgrade to grizzly 2.3.8-beta1 + optimize json HttpHandler: don't create a MAPPER for each request

--- grizzly-bm/src/main/java/org/glassfish/grizzly/bm/JsonHttpHandler.java
+++ grizzly-bm/src/main/java/org/glassfish/grizzly/bm/JsonHttpHandler.java
@@ -1,12 +1,10 @@
 package org.glassfish.grizzly.bm;
 
-import com.fasterxml.jackson.core.JsonFactory;
-import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.*;
 import java.io.IOException;
-import java.util.concurrent.ExecutorService;
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.Request;
+import org.glassfish.grizzly.http.server.RequestExecutorProvider;
 import org.glassfish.grizzly.http.server.Response;
 import org.glassfish.grizzly.http.util.Header;
 
@@ -15,6 +13,8 @@ import org.glassfish.grizzly.http.util.Header;
  */
 public class JsonHttpHandler extends HttpHandler {
 
+    private static final ObjectMapper MAPPER = new ObjectMapper();
+    
     // Response message class.
     public static class HelloMessage {
       public final String message = "Hello, World!";
@@ -25,8 +25,6 @@ public class JsonHttpHandler extends HttpHandler {
             throws Exception {
         response.setContentType("application/json");
         response.setHeader(Header.Server, Server.SERVER_VERSION);
-
-        ObjectMapper MAPPER = new ObjectMapper();
 
         // Write JSON encoded message to the response.
         try
@@ -40,7 +38,7 @@ public class JsonHttpHandler extends HttpHandler {
     }
 
     @Override
-    protected ExecutorService getThreadPool(Request request) {
-        return null;
+    public RequestExecutorProvider getRequestExecutorProvider() {
+        return Server.EXECUTOR_PROVIDER;
     }
 }
