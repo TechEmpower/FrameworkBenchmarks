692bee175bbc7d3f7fbe797d64b7b93ec516bf7e
Mike Smith <msmith@techempower.com>
2013-12-04 07:50:24 -0800

    master Beego, Revel, and Revel-QBS working now
    Huge thanks to @kardianos on github for his involvement
    in making this happen.

--- revel/setup.py
+++ revel/setup.py
@@ -4,6 +4,8 @@ import os
 import setup_util
 import time
 
+
+
 def start(args, logfile, errfile):
   setup_util.replace_text("revel/src/benchmark/conf/app.conf", "tcp\(.*:3306\)", "tcp(" + args.database_host + ":3306)")
   if os.name == 'nt':
@@ -13,6 +15,7 @@ def start(args, logfile, errfile):
     subprocess.call(r"go build -o bin\revel.exe github.com/robfig/revel/revel", shell=True, cwd="revel", env=env, stderr=errfile, stdout=logfile)
     subprocess.Popen(r"bin\revel.exe run benchmark prod".rsplit(" "), shell=True, cwd="revel", env=env, stderr=errfile, stdout=logfile)
     return 0
+  os.environ["GOPATH"] = os.path.expanduser('~/FrameworkBenchmarks/revel')
   subprocess.call("go get -u github.com/robfig/revel/revel", shell=True, cwd="revel", stderr=errfile, stdout=logfile)
   subprocess.call("go build -o bin/revel github.com/robfig/revel/revel", shell=True, cwd="revel", stderr=errfile, stdout=logfile)
   subprocess.Popen("bin/revel run benchmark prod".rsplit(" "), cwd="revel", stderr=errfile, stdout=logfile)
