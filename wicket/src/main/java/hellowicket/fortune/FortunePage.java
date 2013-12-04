7ee8ab3c1334a1eb35d1a572afac28a70e67310f
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2013-12-04 23:12:58 +0100

    Code formatting.
    Revert the change in hibernate.cfg.xml to use data source

--- wicket/src/main/java/hellowicket/fortune/FortunePage.java
+++ wicket/src/main/java/hellowicket/fortune/FortunePage.java
@@ -15,49 +15,48 @@ import org.hibernate.Session;
 import hellowicket.HibernateUtil;
 
 /**
- *
+ * A page that loads all fortune cookies
  */
 public class FortunePage extends WebPage
 {
-	public FortunePage()
-	{
-		final Session session = HibernateUtil.getSessionFactory().openSession();
+  public FortunePage()
+  {
+    Session session = HibernateUtil.getSessionFactory().openSession();
 
-		Query query = session.createQuery("from Fortune");
-		query.setReadOnly(true);
-		List list = query.list();
-		List<Fortune> fortunes = new ArrayList<Fortune>(list);
+    Query query = session.createQuery("from Fortune");
+    query.setReadOnly(true);
+    List list = query.list();
+    List<Fortune> fortunes = new ArrayList<Fortune>(list);
+    session.close();
 
-		session.close();
+    Fortune newFortune = new Fortune();
+    newFortune.message = "Additional fortune added at request time.";
+    fortunes.add(newFortune);
 
-		Fortune newFortune = new Fortune();
-		newFortune.message = "Additional fortune added at request time.";
-		fortunes.add(newFortune);
+    sort(fortunes);
 
-		sort(fortunes);
+    ListView<Fortune> listView = new ListView<Fortune>("fortunes", fortunes)
+    {
+      @Override
+      protected void populateItem(ListItem<Fortune> item)
+      {
+        Fortune fortune = item.getModelObject();
+        item.add(new Label("id", fortune.id));
+        item.add(new Label("message", fortune.message));
+      }
+    };
+    add(listView);
+  }
 
-		ListView<Fortune> listView = new ListView<Fortune>("fortunes", fortunes)
-		{
-			@Override
-			protected void populateItem(ListItem<Fortune> item)
-			{
-				Fortune fortune = item.getModelObject();
-				item.add(new Label("id", fortune.id));
-				item.add(new Label("message", fortune.message));
-			}
-		};
-		add(listView);
-	}
-
-	private void sort(List<Fortune> fortunes)
-	{
-		Collections.sort(fortunes, new Comparator<Fortune>()
-		{
-			@Override
-			public int compare(Fortune f1, Fortune f2)
-			{
-				return f1.message.compareTo(f2.message);
-			}
-		});
-	}
+  private void sort(List<Fortune> fortunes)
+  {
+    Collections.sort(fortunes, new Comparator<Fortune>()
+    {
+      @Override
+      public int compare(Fortune f1, Fortune f2)
+      {
+        return f1.message.compareTo(f2.message);
+      }
+    });
+  }
 }
