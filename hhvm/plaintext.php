ddbd231bf71ecf8b95fb9f788cf487aa21567090
ptarjan <ptarjan@fb.com>
2013-12-17 15:34:28 -0800

    Wrap everything in a function
    HHVM doens't JIT psudomains because there is too much magic that global
    variables can do.

--- hhvm/plaintext.php
+++ hhvm/plaintext.php
@@ -2,5 +2,9 @@
 //
 // 6. Plaintext Test
 //
-header('Content-Type: text/plain; charset=utf-8');
-echo 'Hello, World!';
+
+function main() {
+  header('Content-Type: text/plain; charset=utf-8');
+  echo 'Hello, World!';
+}
+main();
