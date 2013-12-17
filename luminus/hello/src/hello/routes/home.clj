e90429ffc8113365f36421a888f3aa69b78c9d26
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2013-12-17 15:16:34 -0500

    added the plain text benchmark

--- luminus/hello/src/hello/routes/home.clj
+++ luminus/hello/src/hello/routes/home.clj
@@ -6,6 +6,10 @@
 
 (defroutes home-routes
   (GET "/" [] "Hello, World!")
+  (GET "/plaintext" []
+       {:status 200
+        :headers {"Content-Type" "text/plain; charset=utf-8"}
+        :body "Hello, World!"})
   (GET "/json" [] (response/json {:message "Hello, World!"}))
   (GET "/db/:queries" [queries] (response/json (run-queries (get-query-count queries))))
   (GET "/dbraw/:queries" [queries] (response/json (run-queries-raw (get-query-count queries))))  
