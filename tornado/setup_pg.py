24d23e60215026cbeaa00f0e8e78196e78980d0c
Matic Žgur <matic.zgur@zemanta.com>
2013-12-18 18:12:40 +0100

    Added Tornado tests for single and multiple queries on PostgreSQL.

---
+++ tornado/setup_pg.py
@@ -0,0 +1,30 @@
+from os.path import expanduser
+from os import kill
+import subprocess
+import sys
+import time
+
+
+python = expanduser('~/FrameworkBenchmarks/installs/py2/bin/python')
+cwd = expanduser('~/FrameworkBenchmarks/tornado')
+
+
+def start(args, logfile, errfile):
+    subprocess.Popen(
+        python + " server.py --port=8080 --postgres=%s --logging=error" % (args.database_host,),
+        shell=True, cwd=cwd, stderr=errfile, stdout=logfile)
+    return 0
+
+def stop(logfile, errfile):
+    for line in subprocess.check_output(["ps", "aux"]).splitlines():
+        if 'server.py --port=8080' in line:
+            pid = int(line.split(None,2)[1])
+            kill(pid, 9)
+    return 0
+
+if __name__ == '__main__':
+    class DummyArg:
+        database_host = 'localhost'
+    start(DummyArg(), sys.stderr, sys.stderr)
+    time.sleep(1)
+    stop(sys.stderr, sys.stderr)
