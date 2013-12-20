75f9be87c3c8c8530048bcc9c0eceee0083303f6
Brian Clozel <bclozel@gopivotal.com>
2013-12-20 15:11:42 +0100

    disable some filters

--- spring/src/main/java/com/techempower/spring/SampleApplication.java
+++ spring/src/main/java/com/techempower/spring/SampleApplication.java
@@ -1,12 +1,15 @@
 package com.techempower.spring;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @ComponentScan
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = {WebMvcAutoConfiguration.class})
+@EnableWebMvc
 public class SampleApplication extends SpringBootServletInitializer {
 
 	@Override
