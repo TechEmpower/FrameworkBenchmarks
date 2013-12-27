409c6df3ce7a8beed4b93a0f5fab7b2de7e206ec
Tudor Constantin <tudorconstantin@gmail.com>
2013-12-27 13:18:33 +0200

    re-enabled the connection to DB

--- mojolicious/app.pl
+++ mojolicious/app.pl
@@ -4,9 +4,9 @@ use JSON::XS;
 use Memoize;
 use DBI;
 
-my $dsn ;#= "dbi:mysql:database=hello_world;host=localhost;port=3306";
-my $dbh ;#= DBI->connect( $dsn, 'benchmarkdbuser', 'benchmarkdbpass', {} );
-my $sth ;#= $dbh->prepare("SELECT * FROM World where id = ?");
+my $dsn = "dbi:mysql:database=hello_world;host=localhost;port=3306";
+my $dbh = DBI->connect( $dsn, 'benchmarkdbuser', 'benchmarkdbpass', {} );
+my $sth = $dbh->prepare("SELECT * FROM World where id = ?");
 
 
 get '/json' => sub {
