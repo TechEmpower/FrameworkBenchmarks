ff07f2b130ac53588eaf3e41125f1dcc1be25585
Mike Smith <msmith@techempower.com>
2014-01-02 08:59:34 -0800

    verify Fixed django's db test

--- django/hello/world/views.py
+++ django/hello/world/views.py
@@ -43,7 +43,11 @@ def db(request):
   # by creating dicts, we don't need to user the model serializer, which is probably slow and only appropriate
   # for complicated serializations of joins and crazy query sets etc
   # test xrange vs range if the query number is gigantic
-  worlds = uj_dumps([{'id' : r, 'randomNumber' : g(id=r).randomnumber} for r in [rp() for q in xrange(queries)]])  
+  if queries == 1:
+    r = random.randint(1,10000)
+    worlds = uj_dumps({'id' : r, 'randomNumber' : g(id=r).randomnumber})
+  else:
+    worlds = uj_dumps([{'id' : r, 'randomNumber' : g(id=r).randomnumber} for r in [rp() for q in xrange(queries)]])
   return HttpResponse(worlds, mimetype="application/json")
 
 def fortunes(request):
