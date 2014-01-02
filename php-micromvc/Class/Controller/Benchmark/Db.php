c10eb1424927777a1c2daebd16d57e768278ee6c
Mike Smith <msmith@techempower.com>
2014-01-02 11:15:37 -0800

    verify Fixed micromvc's db test

--- php-micromvc/Class/Controller/Benchmark/Db.php
+++ php-micromvc/Class/Controller/Benchmark/Db.php
@@ -15,6 +15,10 @@ class Db extends \MyController
         for ($i = 0; $i < $queries; ++$i) {
             $this->worlds[] = World::row(array('id' => mt_rand(1, 10000)))->to_array();
         }
+
+        if ($queries == 1) {
+            $this->worlds = $this->worlds[0];
+        }
     }
 
     public function send() {
