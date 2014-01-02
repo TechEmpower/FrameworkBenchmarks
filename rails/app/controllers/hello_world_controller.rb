eb0c78e6a561a2aaab24a100eb636169b2a6ff6e
Mike Smith <msmith@techempower.com>
2014-01-02 14:11:18 -0800

    verify Fixed rails' db test

--- rails/app/controllers/hello_world_controller.rb
+++ rails/app/controllers/hello_world_controller.rb
@@ -7,11 +7,16 @@ class HelloWorldController < ApplicationController
   def db
     queries = (params[:queries] || 1).to_i
 
-    results = (1..queries).map do
-      # get a random row from the database, which we know has 10000
-      # rows with ids 1 - 10000
-      World.find(Random.rand(10000) + 1)
+    if queries > 1
+      results = (1..queries).map do
+        # get a random row from the database, which we know has 10000
+        # rows with ids 1 - 10000
+        World.find(Random.rand(10000) + 1)
+      end
+    else
+      results = World.find(Random.rand(10000) + 1)
     end
+
     render :json => results
   end
   
