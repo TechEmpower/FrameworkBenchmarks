1a67ca33b7e6a70556a01369729b3303d13ac79e
Keith R. Gustafson <kgustafson@techempower.com>
2014-01-06 14:07:28 -0800

    Update dependencies to match updates in compojure test

--- luminus/hello/project.clj
+++ luminus/hello/project.clj
@@ -4,16 +4,16 @@
   :dependencies
   [[org.clojure/clojure "1.5.1"]
    [lib-noir "0.6.6"]
-   [compojure "1.1.5"]
-   [ring-server "0.2.8"]
+   [compojure "1.1.6"]
+   [ring-server "0.3.1"]
    [selmer "0.4.0"]
    [com.taoensso/timbre "1.6.0"]
    [com.postspectacular/rotor "0.1.0"]
    [com.taoensso/tower "1.5.1"]
-   [markdown-clj "0.9.29"]
-   [org.clojure/java.jdbc "0.2.3"]
+   [markdown-clj "0.9.38"]
+   [org.clojure/java.jdbc "0.3.0-alpha1"]
    [mysql/mysql-connector-java "5.1.6"]
-   [korma "0.3.0-RC5"]
+   [korma "0.3.0-RC6"]
    [log4j
     "1.2.15"
     :exclusions
@@ -30,11 +30,11 @@
    {:ring
     {:open-browser? false, :stacktraces? false, :auto-reload? false}},
    :dev
-   {:dependencies [[ring-mock "0.1.3"] [ring/ring-devel "1.1.8"]]}}
+   {:dependencies [[ring-mock "0.1.5"] [ring/ring-devel "1.1.8"]]}}
   :url
   "http://example.com/FIXME"
   :plugins
-  [[lein-ring "0.8.5"]]
+  [[lein-ring "0.8.10"]]
   :description
   "FIXME: write description"
   :min-lein-version "2.0.0")
