951bb1d834a286d070ee3983e210cf12319a282d
Adam Chlipala <adamc@csail.mit.edu>
2014-01-14 17:39:23 -0500

    Redid recent Ur/Web changes

--- urweb/world.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE uw_Bench_world AS SELECT uw_id::int8, trunc(random()*9999+1)::int8 AS uw_randomnumber FROM generate_series(1,10000) AS uw_id;
-ALTER TABLE uw_Bench_world ADD PRIMARY KEY (uw_id);
-ALTER TABLE uw_Bench_world ALTER COLUMN uw_randomnumber SET NOT NULL;
