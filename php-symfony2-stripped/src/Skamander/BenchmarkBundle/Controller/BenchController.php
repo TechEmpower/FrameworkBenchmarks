9d8273f84123b473b2783c4d060a82d563a517ce
Arnaud Kleinpeter <arnaud.kleinpeter@gmail.com>
2014-01-16 14:10:03 +0100

    Queries number set between 1 and 500
    Same commit as for symfony2 not stripped.

--- php-symfony2-stripped/src/Skamander/BenchmarkBundle/Controller/BenchController.php
+++ php-symfony2-stripped/src/Skamander/BenchmarkBundle/Controller/BenchController.php
@@ -19,6 +19,8 @@ class BenchController extends Controller
     public function dbAction(Request $request)
     {
         $queries = $request->query->getInt('queries', 1);
+        $queries = max(1, $queries);
+        $queries = min(500, $queries);
 
         // possibility for enhancement is the use of SplFixedArray -> http://php.net/manual/de/class.splfixedarray.php
         $worlds = array();
