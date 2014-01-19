43ee3e72cd9ea6c9aa1815a27e952545529cc215
Richard Nienaber <rjnienaber@gmail.com>
2014-01-19 22:12:09 +0000

    Added raw db tests to rack and additional web servers
    Conflicts:
    	rack/Gemfile-jruby
    	rack/Gemfile-ruby
    	rack/setup_jruby.py
    	rack/setup_ruby.py

---
+++ rack/run_mri_puma.py
@@ -0,0 +1,14 @@
+import helper
+from helper import Command
+
+def start(args, logfile, errfile):
+  helper.set_database_host(args)
+  commands = [
+    Command("rvm jruby-1.7.8 do bundle", True),
+    Command("rvm ruby-2.0.0-p0 do bundle exec puma -t 8:32 -w 8 --preload -b tcp://0.0.0.0:8080 -e production", False)
+  ]
+
+  return helper.run(commands, logfile, errfile)
+
+def stop(logfile, errfile):
+  return helper.stop('puma', logfile, errfile)
