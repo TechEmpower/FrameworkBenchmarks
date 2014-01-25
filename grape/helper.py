9746738ebbcf3d84b43725f98a3351459962862f
Richard Nienaber <rjnienaber@gmail.com>
2014-01-25 07:18:29 +0000

    Fixed bundle gemsets, changed kill service code

--- grape/helper.py
+++ grape/helper.py
@@ -24,4 +24,10 @@ def run(commands, logfile, errfile):
   return 0
 
 def stop(partial_command, logfile, errfile):
-  return run([Command('sudo pkill --signal 15 -f "' + partial_command + '"', True)], logfile, errfile)
+  p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+  for line in out.splitlines():
+    if partial_command in line and 'run-tests' not in line:
+      pid = int(line.split(None, 2)[1])
+      os.kill(pid, 15)
+  return 0
--- grape/run_thin.py
+++ grape/run_thin.py
@@ -4,7 +4,7 @@ from helper import Command
 def start(args, logfile, errfile):
   helper.set_database_host(args)
   commands = [
-    Command("rvm jruby-1.7.8 do bundle", True),
+    Command("rvm ruby-2.0.0-p0 do bundle", True),
     Command("rvm ruby-2.0.0-p0 do bundle exec thin start -C config/thin.yml", False)
   ]
 
