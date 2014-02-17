c749c3af169c72331d326ef308923adcdf8c76e3
Adam Chlipala <adamc@csail.mit.edu>
2014-02-17 17:31:37 -0500

    Upgrade Ur/Web version to handle MySQL databases where table names are not unique, according to case-insensitive comparison

--- toolset/setup/linux/installer.py
+++ toolset/setup/linux/installer.py
@@ -198,7 +198,7 @@ class Installer:
     #
     # Ur/Web
     #
-    self.__run_command("hg clone -r126d24ef6678 http://hg.impredicative.com/urweb")
+    self.__run_command("hg clone -r6bea98c7f736 http://hg.impredicative.com/urweb")
     self.__run_command("./autogen.sh", cwd="urweb")
     self.__run_command("./configure", cwd="urweb")
     self.__run_command("make", cwd="urweb")
