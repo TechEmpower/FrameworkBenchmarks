09569f1d351feb6f4f4e93b39ea0908659b6e563
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-05-04 11:22:17 -0700

    specify -j2 for both ./configure and make to speed up the OpenResty build.

--- toolset/setup/linux/installer.py
+++ toolset/setup/linux/installer.py
@@ -229,8 +229,8 @@ class Installer:
     #
     self.__download("http://openresty.org/download/ngx_openresty-1.5.12.1.tar.gz")
     self.__run_command("tar xzf ngx_openresty-1.5.12.1.tar.gz")
-    self.__run_command("./configure --with-cc-opt=-O2 --with-http_postgres_module", cwd="ngx_openresty-1.5.12.1")
-    self.__run_command("make", cwd="ngx_openresty-1.5.12.1")
+    self.__run_command("./configure --with-cc-opt=-O2 --with-http_postgres_module -j2", cwd="ngx_openresty-1.5.12.1")
+    self.__run_command("make -j2", cwd="ngx_openresty-1.5.12.1")
     self.__run_command("sudo make install", cwd="ngx_openresty-1.5.12.1")
     
     #
