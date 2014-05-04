fd2ea857a5d66e07c88ebdd36b8b83cc01b6014c
Yichun Zhang (agentzh) <agentzh@gmail.com>
2014-05-04 11:17:40 -0700

    upgraded OpenResty to the latest formal release, 1.5.12.1.
    we also remove the --with-luajit option from ./configure because it is
    already the default.

--- toolset/setup/linux/installer.py
+++ toolset/setup/linux/installer.py
@@ -227,11 +227,11 @@ class Installer:
     #
     # Openresty (nginx with lua stuff)
     #
-    self.__download("http://openresty.org/download/ngx_openresty-1.5.8.1.tar.gz")
-    self.__run_command("tar xzf ngx_openresty-1.5.8.1.tar.gz")
-    self.__run_command("./configure --with-luajit --with-http_postgres_module", cwd="ngx_openresty-1.5.8.1")
-    self.__run_command("make", cwd="ngx_openresty-1.5.8.1")
-    self.__run_command("sudo make install", cwd="ngx_openresty-1.5.8.1")
+    self.__download("http://openresty.org/download/ngx_openresty-1.5.12.1.tar.gz")
+    self.__run_command("tar xzf ngx_openresty-1.5.12.1.tar.gz")
+    self.__run_command("./configure --with-http_postgres_module", cwd="ngx_openresty-1.5.12.1")
+    self.__run_command("make", cwd="ngx_openresty-1.5.12.1")
+    self.__run_command("sudo make install", cwd="ngx_openresty-1.5.12.1")
     
     #
     # Lapis
