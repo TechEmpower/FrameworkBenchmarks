f6e8ae890391b3fb974c769db2a31f7298b6a647
Jay McCarthy <jay.mccarthy@gmail.com>
2014-05-10 11:48:45 -0600

    remove deps

--- racket-ws/bench/bench-ev.rkt
+++ racket-ws/bench/bench-ev.rkt
@@ -1,36 +1,48 @@
 #lang racket/base
-(require racket/list
-         racket/port
-         racket/tcp
-         racket/match)
+(require racket/tcp)
 
-(define RESPONSE
-  #"HTTP/1.1 200 OK
-Content-Length: 131
-
-<html>\n<head>\n  <title>An Example Page</title>\n</head>\n<body>\n  Hello World, this is a very simple HTML document.\n</body>\n</html>\n\n")
+(define RESPONSE #"HTTP/1.1 200 OK\r\nContent-Length: 1\r\n\r\n!")
 (define END (bytes-length RESPONSE))
 (define BUFFER-SIZE 64)
 (define BUFFER (make-bytes BUFFER-SIZE))
 
-(struct evt:echo (from to reading writing) #:mutable)
+(struct evt:echo (from to writing) #:mutable)
 
 (define (go! port)
   (define l (tcp-listen port 10 #t #f))
   (printf "Ready\n")
   (flush-output)
-  (let loop ([evts empty])
+  (let loop ([evts null])
     (apply
      sync
      (handle-evt
       (tcp-accept-evt l)
-      (match-lambda
-       [(list from to)
-        (loop (cons (evt:echo from to 0 #f) evts))]))
+      (lambda (l)
+        (define from (car l))
+        (define to (cadr l))
+        (loop (cons (evt:echo from to #f) evts))))
      (for/list ([e (in-list evts)])
-       (match-define (evt:echo from to reading writing) e)
-       (match writing
-         [#f
+       (define from (evt:echo-from e))
+       (define to (evt:echo-to e))
+       (define write-start (evt:echo-writing e))
+       (cond
+         [write-start
+          (handle-evt to
+                      (λ (_)
+                        (define write-k
+                          (write-bytes-avail* RESPONSE to write-start END))
+                        (cond
+                          [(or (not write-k) (zero? write-k))
+                           (loop evts)]
+                          [else
+                           (define new (+ write-start write-k))
+                           (set-evt:echo-writing!
+                            e
+                            (if (= END new)
+                              #f
+                              new))
+                           (loop evts)])))]
+         [else
           (handle-evt from
                       (λ (_)
                         (define read-k
@@ -41,25 +53,7 @@ Content-Length: 131
                            (close-output-port to)
                            (loop (remq e evts))]
                           [else
-                           (define new (+ read-k reading))
-                           (set-evt:echo-reading! e new)
-                           (when (= new 64)
-                             (set-evt:echo-writing! e 0)
-                             (set-evt:echo-reading! e 0))
-                           (loop evts)])))]
-         [start
-          (handle-evt to
-                      (λ (_)
-                        (match (write-bytes-avail* RESPONSE to start END)
-                          [(or #f 0)
-                           (loop evts)]
-                          [more
-                           (define new (+ start more))
-                           (set-evt:echo-writing!
-                            e
-                            (if (= END new)
-                              #f
-                              new))
+                           (set-evt:echo-writing! e 0)
                            (loop evts)])))])))))
 
 (module+ main
