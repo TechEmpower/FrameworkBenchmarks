9816ac9e816f7e38d61a40d4c512f99286e99b45
mcocciaTE <mcoccia@techempower.com>
2014-06-10 18:48:51 -0700

    218 make Kain's changes from the PR comments

--- php-zend-framework/config/autoload/benchmarks.local.php
+++ php-zend-framework/config/autoload/benchmarks.local.php
@@ -3,6 +3,6 @@
 return array(
     'db' => array(
         'driver' => 'Pdo',
-        'dsn'    => 'mysql:dbname=hello_world;host=localhost',
+        'dsn'    => 'mysql:dbname=hello_world;host=127.0.0.1',
     ),
 );
