d66f17874961de75a92cec89fdc8a7cc8cccb1fb
mcocciaTE <mcoccia@techempower.com>
2014-06-12 14:35:36 -0700

    218 Fixed db tests for zend-framework
    Needed to add the username and password credentials
    for connecting to the database.

--- php-zend-framework/config/autoload/benchmarks.local.php
+++ php-zend-framework/config/autoload/benchmarks.local.php
@@ -2,7 +2,9 @@
 
 return array(
     'db' => array(
-        'driver' => 'Pdo',
-        'dsn'    => 'mysql:dbname=hello_world;host=127.0.0.1',
+        'driver'   => 'Pdo',
+        'dsn'      => 'mysql:dbname=hello_world;host=localhost;port=3306',
+        'username' => 'benchmarkdbuser',
+        'password' => 'benchmarkdbpass'
     ),
 );
