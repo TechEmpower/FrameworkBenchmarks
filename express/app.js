85fa9a9ddf27952dd5ea014e7e976cfdc95fb1da
James Yen <jyen@techempower.com>
2014-06-17 14:51:17 -0700

    777 added charset parameter
    app.js file was missing a charset parameter
    that allowed the Japanese characters to be rendered
    properly.

--- express/app.js
+++ express/app.js
@@ -10,7 +10,7 @@ var cluster = require('cluster')
   , mongoose = require('mongoose')
   , async = require('async')
   , conn = mongoose.connect('mongodb://localhost/hello_world')
-  , connMap = { user: 'benchmarkdbuser', password: 'benchmarkdbpass', database: 'hello_world', host: 'localhost' };
+  , connMap = { user: 'benchmarkdbuser', password: 'benchmarkdbpass', database: 'hello_world', host: 'localhost', charset: 'utf8' };
 
 var Schema = mongoose.Schema
   , ObjectId = Schema.ObjectId;
