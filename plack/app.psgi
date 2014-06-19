c2c94a6bd914f41cf82ce0ea943b3070a425781b
Masahiro Nagano <kazeburo@gmail.com>
2014-06-19 12:29:23 +0900

    use nginx as reverse proxy

--- plack/app.psgi
+++ plack/app.psgi
@@ -13,17 +13,19 @@ my $dbh = DBI->connect_cached(
 
 my $query = 'SELECT id, randomNumber FROM World WHERE id = ?';
 my $header = [qw(Content-Type application/json)];
+my $message = { message => 'Hello, World!' };
 
 my $app = sub {
     my $env = shift;
     if ( $env->{PATH_INFO} eq '/json' ) {
-        return [ 200, $header, [ encode_json({ message => 'Hello, World!' }) ]];
+        return [ 200, $header, [ encode_json($message) ]];
     }
     elsif ( $env->{PATH_INFO} eq '/db' ) {
         my ($n) = ($env->{QUERY_STRING} || "" ) =~ m!queries=(\d+)!;
         $n //= 1;
         my @rs = map {{id=>$_->[0]+0,randomNumber=>$_->[1]+0}} 
             map { $dbh->selectrow_arrayref($query,{},int rand 10000 + 1) } 1..$n;
+        return [ 200, $header, [ '{}' ]] unless @rs;
         return [ 200, $header, [ encode_json( @rs > 1 ? \@rs : $rs[0] ) ]];
     }
     [ 404, [], ['not found']];
--- plack/setup.py
+++ plack/setup.py
@@ -1,21 +1,29 @@
 import subprocess
 import sys
 import setup_util
+from os.path import expanduser
 import os
+import getpass
+
+home = expanduser("~")
 
 def start(args, logfile, errfile):
   setup_util.replace_text("plack/app.psgi", "localhost", ""+ args.database_host +"")
+  setup_util.replace_text("plack/nginx.conf", "USR", getpass.getuser())
+  setup_util.replace_text("plack/nginx.conf", "server unix:.*\/FrameworkBenchmarks", "server unix:" + home + "/FrameworkBenchmarks")
   try:
     subprocess.check_call("curl -L http://cpanmin.us | perl - App::cpanminus", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
-    subprocess.check_call("cpanm --notest --no-man-pages --installdeps .", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
-    pid = subprocess.Popen("plackup -E production -s Starlet -l :8080 --max-keepalive-reqs 5000 --max-reqs-per-child 50000 --min-reqs-per-child 40000 --max-workers=" + str(args.max_threads+4) + " app.psgi", shell=True, cwd="plack", stderr=errfile, stdout=logfile).pid
-    open('plack/app.pid', 'w').write(str(pid))
+    subprocess.check_call("cpanm --notest --no-man-pages --installdeps "+home+"/FrameworkBenchmarks/plack", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
+    subprocess.Popen("start_server --backlog=16384 --pid-file="+home+"/FrameworkBenchmarks/plack/app.pid --path="+home+"/FrameworkBenchmarks/plack/app.sock -- plackup -E production -s Starlet --max-keepalive-reqs 1000 --max-reqs-per-child 50000 --min-reqs-per-child 40000 --max-workers=" + str(args.max_threads+128) + " -a "+home+"/FrameworkBenchmarks/plack/app.psgi", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
+    subprocess.check_call("sudo /usr/local/nginx/sbin/nginx -c " + home + "/FrameworkBenchmarks/plack/nginx.conf", shell=True, stderr=errfile, stdout=logfile)
     return 0
   except subprocess.CalledProcessError:
     return 1
 def stop(logfile, errfile):
   try:
-    subprocess.Popen("kill -TERM $(ps --ppid `cat app.pid` -o pid --no-header)", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
+    subprocess.call('kill -TERM $(cat '+home+"/FrameworkBenchmarks/plack/app.pid)", shell=True, cwd="plack", stderr=errfile, stdout=logfile)
+    subprocess.call("sudo /usr/local/nginx/sbin/nginx -s stop", shell=True)
     return 0
   except subprocess.CalledProcessError:
     return 1
+
