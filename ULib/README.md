0e1b0af27eeab75fa9ddf1614e53f43c7bd6130f
stefanocasazza <stefano.casazza@gmail.com>
2014-06-20 17:27:43 +0200

    some enhancement + some fix

---
+++ ULib/README.md
@@ -0,0 +1,29 @@
+#ULib Benchmarking Test
+
+This is the ULib portion of a [benchmarking test suite](../) comparing a variety of web development platforms.
+
+### JSON Encoding Test
+
+* [JSON test source](src/json.usp)
+
+### Data-Store/Database Mapping Test
+
+* [Database test source](src/db.usp)
+
+## Infrastructure Software Versions
+The tests were run with:
+
+* [ULib git Version](https://github.com/stefanocasazza/ULib/tree/master)
+
+## Test URLs
+### JSON Encoding Test
+
+http://localhost/json
+
+### Data-Store/Database Mapping Test
+
+http://localhost/db
+
+### Variable Query Test
+
+http://localhost/queries?queries=5
--- ULib/setup.py
+++ ULib/setup.py
@@ -1,6 +1,7 @@
 import subprocess
 import sys
 import os
+import time
 import setup_util
 
 from os.path import expanduser
@@ -16,24 +17,24 @@ def start(args, logfile, errfile):
   # 2. Compile applicaton
 
   script = """
-  ##############################
-  # Prerequisites
-  ##############################
-  # yes | sudo apt-get update
-  # yes | sudo apt-get install ... 
-  ##############################
+  ############################################################################
+  # Prerequisites (mysql_config)
+  ############################################################################
+  (yes | sudo apt-get install libmariadbclient-dev libssl-dev) >/dev/null 2>&1
+  ############################################################################
   # ULib
-  ##############################
-  git clone https://github.com/stefanocasazza/ULib.git
-  cd ULib
-  ./configure --disable-static --disable-log --without-libz --without-libuuid --without-magic --without-ssl --without-pcre --without-expat --with-mysql --enable-static-orm-driver=mysql --enable-static-server-plugin=http >/dev/null 2>&1
-  make install
-  cd src/ulib/net/server/plugin/usp
-  make install >/dev/null 2>&1
-  make db.la fortunes.la json.la plaintext.la queries.la updates.la >/dev/null 2>&1
-  sudo mkdir -p ~/FrameworkBenchmarks/ULib/www
-  sudo cp .libs/db.so .libs/fortunes.so .libs/json.so .libs/plaintext.so .libs/queries.so .libs/updates.so ~/FrameworkBenchmarks/ULib/www
-  cd ~
+  ###############################################
+  if [ ! -d ~/FrameworkBenchmarks/ULib/www ]; then
+    sudo git clone https://github.com/stefanocasazza/ULib.git
+    cd ULib
+    sudo LIBS="-lssl -lcrypto -lz" ./configure --disable-static --disable-log --without-libz --without-libuuid --without-magic --without-ssl --without-pcre --without-expat --with-mysql --enable-static-orm-driver=mysql --enable-static-server-plugin=http >/dev/null 2>&1
+    sudo make install >/dev/null 2>&1
+    cd src/ulib/net/server/plugin/usp
+    sudo make db.la fortunes.la json.la plaintext.la queries.la updates.la >/dev/null 2>&1
+    sudo mkdir -p ~/FrameworkBenchmarks/ULib/www
+    sudo cp .libs/db.so .libs/fortunes.so .libs/json.so .libs/plaintext.so .libs/queries.so .libs/updates.so ~/FrameworkBenchmarks/ULib/www
+    cd ~
+  fi
   """
 
   p = subprocess.Popen(['sh'], stdin=subprocess.PIPE)
@@ -46,9 +47,9 @@ def start(args, logfile, errfile):
 
     os.putenv("ORM_DRIVER","mysql")
     os.putenv("ORM_OPTION","host=" + args.database_host + " user=benchmarkdbuser password=benchmarkdbpass dbname=hello_world")
-    os.putenv("UMEMPOOL", "1583,1507,-19,34,1033,523,-27,-14,27")
+    os.putenv("UMEMPOOL", "1583,1507,-19,43,1038,523,-27,-14,27")
 
-    subprocess.check_call("/usr/local/bin/userver_tcp -c " + home + "/FrameworkBenchmarks/ULib/benchmark.cfg", shell=True, stderr=errfile, stdout=logfile)
+    subprocess.check_call("sudo /usr/local/bin/userver_tcp -c " + home + "/FrameworkBenchmarks/ULib/benchmark.cfg", shell=True, stderr=errfile, stdout=logfile)
     return 0
   except subprocess.CalledProcessError:
     return 1
@@ -59,6 +60,13 @@ def start(args, logfile, errfile):
 def stop(logfile, errfile):
   try:
     subprocess.check_call("sudo kill -TERM $( cat " + home + "/FrameworkBenchmarks/ULib/userver_tcp.pid )", shell=True, stderr=errfile, stdout=logfile)
+    time.sleep(2);
+    p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
+    out, err = p.communicate()
+    for line in out.splitlines():
+       if 'userver_tcp' in line:
+         pid = int(line.split(None, 2)[1])
+         os.kill(pid, 9)
     return 0
   except subprocess.CalledProcessError:
     return 1
