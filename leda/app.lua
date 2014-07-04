e57c566831977a6ed564e99904e61de81e109df3
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-04 06:40:21 -0700

    updating to use latest api

--- leda/app.lua
+++ leda/app.lua
@@ -2,7 +2,7 @@ local http = require('leda.http')
 
 local server = http.Server(8080, '')
 
-server:request(function(server, request, response)
+server.request = function(server, request, response)
     if request:url():find('/json') then
         response.body =  {message= 'Hello, World!'}
     end
@@ -10,5 +10,4 @@ server:request(function(server, request, response)
     if request:url():find('/plaintext') then
         response.body =  'Hello, World!'
     end
-    
-end)
+end
