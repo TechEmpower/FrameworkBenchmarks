1abebb7d79f72e7fa1fc155a1ed18084286bdce0
stefanocasazza <stefano.casazza@gmail.com>
2014-07-04 16:57:38 +0200

    Setting for Framework Benchmarks participations

--- ULib/setup.py
+++ ULib/setup.py
@@ -14,18 +14,33 @@ home = expanduser("~")
 def start(args, logfile, errfile):
 
   script = """
-  if [ ! -d ~/FrameworkBenchmarks/ULib/www ]; then
+  DIR=$HOME/FrameworkBenchmarks
+  DOCUMENT_ROOT=$DIR/ULib/www
+  if [ ! -d $DOCUMENT_ROOT ]; then
     # 1. Download ULib
-    sudo wget https://github.com/stefanocasazza/ULib/archive/master.zip 
+    wget -nc https://github.com/stefanocasazza/ULib/archive/v1.4.0.tar.gz
     # 2. Compile application (userver_tcp)
-    sudo unzip master.zip
-    cd ULib-master
-    sudo LIBS="-lssl -lcrypto -lz" ./configure --disable-static --disable-log --without-libz --without-libuuid --without-magic --without-ssl --without-pcre --without-expat --with-mysql --enable-static-orm-driver=mysql --enable-static-server-plugin=http >/dev/null 2>&1
-    sudo make install >/dev/null 2>&1
+    tar xzf v1.4.0.tar.gz
+    cd ULib-1.4.0
+    # ======================================================================================================
+    # TO AVOID configure: error: newly created file is older than distributed files! Check your system clock
+    # ======================================================================================================
+    find . -exec touch {} \;
+    # ======================================================================================================
+    PREFIX=$DIR/installs/ulib
+    if [ ! -d $PREFIX ]; then
+       mkdir -p $PREFIX
+    fi
+    DATE=`date '+%Y%m%d'` # 20140117
+    BUILD_OUTPUT=$PREFIX/ULIB_BUILD_OUTPUT-$DATE.txt
+    LIBS="-lssl -lcrypto -lz" ./configure --prefix=$PREFIX --disable-static --without-libz --without-libuuid --without-magic --without-ssl --without-pcre --without-expat --with-mysql --enable-static-orm-driver=mysql --enable-static-server-plugin=http >$BUILD_OUTPUT 2>&1
+    make -j1 install >>$BUILD_OUTPUT 2>&1
     cd src/ulib/net/server/plugin/usp
-    sudo make db.la fortunes.la json.la plaintext.la queries.la updates.la >/dev/null 2>&1
-    sudo mkdir -p ~/FrameworkBenchmarks/ULib/www
-    sudo cp .libs/db.so .libs/fortunes.so .libs/json.so .libs/plaintext.so .libs/queries.so .libs/updates.so ~/FrameworkBenchmarks/ULib/www
+    make -j1 db.la fortunes.la json.la plaintext.la queries.la updates.la >>$BUILD_OUTPUT 2>&1
+    if [ -e .libs/db.so ]; then
+       mkdir -p $DOCUMENT_ROOT
+       cp .libs/db.so .libs/fortunes.so .libs/json.so .libs/plaintext.so .libs/queries.so .libs/updates.so $DOCUMENT_ROOT
+    fi
     cd ~
   fi
   """
@@ -33,7 +48,7 @@ def start(args, logfile, errfile):
   p = subprocess.Popen(['sh'], stdin=subprocess.PIPE)
   p.communicate(script)
 
-  # 3. Start ULib Server
+  # 3. Start ULib Server (userver_tcp)
   try:
     threads = str(args.max_threads)
     setup_util.replace_text(home + "/FrameworkBenchmarks/ULib/benchmark.cfg", "PREFORK_CHILD *", "PREFORK_CHILD " + threads)
@@ -42,7 +57,7 @@ def start(args, logfile, errfile):
     os.putenv("ORM_OPTION","host=" + args.database_host + " user=benchmarkdbuser password=benchmarkdbpass dbname=hello_world")
     os.putenv("UMEMPOOL", "1583,1507,-19,43,1038,523,-27,-14,27")
 
-    subprocess.check_call("sudo /usr/local/bin/userver_tcp -c " + home + "/FrameworkBenchmarks/ULib/benchmark.cfg", shell=True, stderr=errfile, stdout=logfile)
+    subprocess.check_call(home + "/FrameworkBenchmarks/installs/ulib/bin/userver_tcp -c " + home + "/FrameworkBenchmarks/ULib/benchmark.cfg", shell=True, stderr=errfile, stdout=logfile)
     return 0
   except subprocess.CalledProcessError:
     return 1
@@ -52,7 +67,7 @@ def start(args, logfile, errfile):
 ##############
 def stop(logfile, errfile):
   try:
-    subprocess.check_call("sudo kill -TERM $( cat " + home + "/FrameworkBenchmarks/ULib/userver_tcp.pid )", shell=True, stderr=errfile, stdout=logfile)
+    subprocess.check_call("kill -TERM $( cat " + home + "/FrameworkBenchmarks/ULib/userver_tcp.pid )", shell=True, stderr=errfile, stdout=logfile)
     time.sleep(2);
     p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
     out, err = p.communicate()
