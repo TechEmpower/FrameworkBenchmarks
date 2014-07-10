8f11ad4425a3888eaaf748aa42468a14d4e402de
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-10 10:40:29 -0700

    refactored to add caching

--- leda/app.lua
+++ leda/app.lua
@@ -3,9 +3,10 @@ local http = require('leda.http')
 local server = http.Server(8080, '')
 
 server.request = function(server, request, response)
-    if request:url():find('/json') then
+    local url = request:url() 
+    if url:find('/json') then
         response.body =  {message= 'Hello, World!'}
-    elseif request:url():find('/plaintext') then    
+    elseif url:find('/plaintext') then    
         response.body =  'Hello, World!'
     end
 end
