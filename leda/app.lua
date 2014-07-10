dccd8dc99970422d4d11b86fd0ae7921f89c6a3b
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-10 10:33:34 -0700

    refactored to add else clause

--- leda/app.lua
+++ leda/app.lua
@@ -5,9 +5,7 @@ local server = http.Server(8080, '')
 server.request = function(server, request, response)
     if request:url():find('/json') then
         response.body =  {message= 'Hello, World!'}
-    end
-    
-    if request:url():find('/plaintext') then
+    elseif request:url():find('/plaintext') then    
         response.body =  'Hello, World!'
     end
 end
