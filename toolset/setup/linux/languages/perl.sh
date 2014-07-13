ab8160db25817db7a188e6db9fdb3c7c91b0e445
Joel Berger <joel.a.berger@gmail.com>
2014-07-13 15:15:46 -0500

    added Mango to required Perl modules (lost in installer refactoring)

--- toolset/setup/linux/languages/perl.sh
+++ toolset/setup/linux/languages/perl.sh
@@ -8,4 +8,4 @@ perl perl-build.pl -DDEBUGGING=-g 5.18.2 perl-5.18
 
 fw_get http://cpanmin.us -O cpanminus.pl
 perl-5.18/bin/perl cpanminus.pl --notest --no-man-page App::cpanminus
-perl-5.18/bin/cpanm -f --notest --no-man-page DBI DBD::mysql Kelp Dancer Mojolicious Kelp::Module::JSON::XS Dancer::Plugin::Database Starman Plack JSON Web::Simple DBD::Pg JSON::XS EV HTTP::Parser::XS Monoceros EV IO::Socket::IP IO::Socket::SSL Memoize
+perl-5.18/bin/cpanm -f --notest --no-man-page DBI DBD::mysql Kelp Dancer Mojolicious Mango Kelp::Module::JSON::XS Dancer::Plugin::Database Starman Plack JSON Web::Simple DBD::Pg JSON::XS EV HTTP::Parser::XS Monoceros EV IO::Socket::IP IO::Socket::SSL Memoize
