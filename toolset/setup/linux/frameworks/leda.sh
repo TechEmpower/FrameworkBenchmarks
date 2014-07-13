6ce8de1675cb856a4a96162c5cc7d06d1a0f99bb
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-13 05:20:12 -0700

    update setup script

--- toolset/setup/linux/frameworks/leda.sh
+++ toolset/setup/linux/frameworks/leda.sh
@@ -3,8 +3,9 @@
 RETCODE=$(fw_exists leda)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-git clone git@github.com:sergeyzavadski/leda.git
-cd leda
+wget http://sergeyzavadski.github.io/leda/releases/leda-0.4.1/leda-0.4.1.tar.gz
+tar xvf leda-0.4.1.tar.gz
+cd leda-0.4.1
 ./configure
 make 
 sudo make install
