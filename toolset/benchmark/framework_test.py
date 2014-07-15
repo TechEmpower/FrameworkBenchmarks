dcbe28557a24ac79f6f8c122e759c574e84a6f74
Hamilton Turner <hamiltont@gmail.com>
2014-07-15 05:17:21 -0400

    Modify requires_database, includes test type in check

--- toolset/benchmark/framework_test.py
+++ toolset/benchmark/framework_test.py
@@ -910,10 +910,10 @@ class FrameworkTest:
 
   def requires_database(self):
       """Returns True/False if this test requires a database"""
-      return (self.contains_type(self.FORTUNE) or 
-              self.contains_type(self.DB) or 
-              self.contains_type(self.QUERY) or
-              self.contains_type(self.UPDATE))
+      return (self.runTests[self.FORTUNE] or 
+              self.runTests[self.DB] or 
+              self.runTests[self.QUERY] or
+              self.runTests[self.UPDATE])
 
   ##########################################################################################
   # Constructor
