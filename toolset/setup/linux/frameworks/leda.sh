bed4d0021ddacba348c79578d1b3e8753ec0dd1d
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-18 12:57:04 -0700

    updating serup script

--- toolset/setup/linux/frameworks/leda.sh
+++ toolset/setup/linux/frameworks/leda.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists leda-0.5.0)
+RETCODE=$(fw_exists leda-0.5.1)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-sudo apt-get install -y --fix-missing g++
-sudo apt-get install -y --fix-missing luarocks
+sudo apt-get install -y  g++
+sudo apt-get install -y  luarocks
 
 sudo luarocks install lua-cjson
 
-wget http://sergeyzavadski.github.io/leda/releases/leda-0.5.0/leda-0.5.0.tar.gz
-tar xvf leda-0.5.0.tar.gz
-cd leda-0.5.0
+wget http://sergeyzavadski.github.io/leda/releases/leda-0.5.1/leda-0.5.1.tar.gz
+tar xvf leda-0.5.1.tar.gz
+cd leda-0.5.1
 
 ./configure
 make 
