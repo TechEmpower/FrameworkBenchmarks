c8df49491c9396125577265c41b9aa9923d0117a
Alex Schneider <aschneider@techempower>
2014-07-18 13:12:10 -0400

    Fix minor issue with racket's install script. Improve openresty's install. Close #852

--- toolset/setup/linux/languages/racket.sh
+++ toolset/setup/linux/languages/racket.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-RET1=$(fw_exists racket-5.3.6/bin/racket)
-RET2=$(fw_exists racket-5.3.6/collects/racket)
+RET1=$(fw_exists racket-src/bin/racket)
+RET2=$(fw_exists racket-src/collects/racket)
 if [ "$RET1" == 0 ] && [ "$RET2" == 0 ]; then 
   return 0;
 fi
