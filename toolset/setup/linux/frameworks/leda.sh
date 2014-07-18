8633df31f27741ab4330e98ca91a27e6dbfc5fdf
Sergey Zavadski <sergeyzavadski@gmail.com>
2014-07-18 16:35:41 -0700

    making required changes

--- toolset/setup/linux/frameworks/leda.sh
+++ toolset/setup/linux/frameworks/leda.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists leda-0.5.2)
+RETCODE=$(fw_exists leda)
 [ ! "$RETCODE" == 0 ] || { return 0; }
+
+rm -rf leda
 
 sudo apt-get install -y  g++
 sudo apt-get install -y  luarocks
@@ -9,10 +11,13 @@ sudo apt-get install -y  luarocks
 sudo luarocks install lua-cjson
 
 
-wget http://sergeyzavadski.github.io/leda/releases/leda-0.5.2/leda-0.5.2.tar.gz
-tar xvf leda-0.5.2.tar.gz
+wget -O leda.tar.gz http://sergeyzavadski.github.io/leda/releases/leda-0.5.2/leda-0.5.2.tar.gz 
+fw_untar leda.tar.gz
 cd leda-0.5.2
 
 ./configure
 make 
 sudo make install
+cd .. 
+mv leda-0.5.2 leda
+
