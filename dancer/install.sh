f691d2c10221f78b6cce2f3dc6936c58b090c5b9
Joel Berger <joel.a.berger@gmail.com>
2014-08-10 16:05:04 -0500

    dancer plack and web-simple depend on nginx being installed

--- dancer/install.sh
+++ dancer/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fw_depends perl
+fw_depends perl nginx
 
 cpanm --notest --no-man-page Dancer Dancer::Plugin::Database DBI DBD::mysql JSON::XS Plack Starman
 echo installed Dancer app dependencies
