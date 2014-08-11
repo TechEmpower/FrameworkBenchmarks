2cfedb0e0c399507c6205f69e17bcbc62770ffd5
Dmitry Bushmelev <dmitrii.bushmelev@gmail.com>
2014-08-11 16:43:59 +0400

    Add frameworks to travis.yml and fixes

--- erlang-misultin/setup.py
+++ erlang-misultin/setup.py
@@ -8,16 +8,16 @@ def start(args, logfile, errfile):
                             "{db_host, \"" + args.database_host + "\"}")
 
     try:
-        subprocess.check_call("make clean", shell=True, cwd="misultin", stderr=errfile, stdout=logfile)
-        subprocess.check_call("make", shell=True, cwd="misultin", stderr=errfile, stdout=logfile)
-        subprocess.check_call("make run", shell=True, cwd="misultin", stderr=errfile, stdout=logfile)
+        cmd = "erl +K true +sbwt very_long +swt very_low -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -s misultin_bench_app -noshell -detached"
+        subprocess.check_call("./rebar clean get-deps compile", shell=True, cwd="erlang-misultin", stderr=errfile, stdout=logfile)
+        subprocess.check_call(cmd, shell=True, cwd="erlang-misultin", stderr=errfile, stdout=logfile)
         return 0
     except subprocess.CalledProcessError:
         return 1
 
 def stop(logfile, errfile):
     try:
-        subprocess.check_call("make stop", shell=True, cwd="/usr/bin")
+        subprocess.check_call("killall beam.smp", shell=True, cwd="/usr/bin")
         return 0
     except subprocess.CalledProcessError:
         return 1
