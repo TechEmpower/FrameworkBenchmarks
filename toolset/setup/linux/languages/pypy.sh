01e7c045aed8b0dd54a31f5d42107e76e9bea817
Hamilton Turner <hamiltont@gmail.com>
2014-08-12 12:47:46 -0400

    Make fw_untar and fw_unzip clean up the compressed files

--- toolset/setup/linux/languages/pypy.sh
+++ toolset/setup/linux/languages/pypy.sh
@@ -4,7 +4,7 @@ RETCODE=$(fw_exists pypy)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
 fw_get https://bitbucket.org/pypy/pypy/downloads/pypy-2.3.1-linux64.tar.bz2 -O pypy-2.3.1-linux64.tar.bz2
-tar vxf pypy-2.3.1-linux64.tar.bz2
+fw_untar pypy-2.3.1-linux64.tar.bz2
 ln -sf pypy-2.3.1-linux64 pypy
 
 fw_get https://bootstrap.pypa.io/get-pip.py
