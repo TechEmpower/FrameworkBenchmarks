2f3c8c20a64cec0a8b124bbdac0e273e4fb8f77e
Hamilton Turner <hamiltont@gmail.com>
2014-08-12 10:31:44 -0400

    Move mono into prefix folder, check folder contents

--- toolset/setup/linux/languages/mono.sh
+++ toolset/setup/linux/languages/mono.sh
@@ -10,10 +10,12 @@ RETCODE=$(fw_exists mono-3.2.8.installed)
 fw_get http://download.mono-project.com/sources/mono/mono-3.2.8.tar.bz2 -O mono-3.2.8.tar.bz2
 fw_untar mono-3.2.8.tar.bz2
 cd mono-3.2.8 
-./configure --disable-nls --prefix=/usr/local
+./configure --disable-nls --prefix=`pwd`/mono-3.2.8-install
 make get-monolite-latest
 make -j4 EXTERNAL_MCS=${PWD}/mcs/class/lib/monolite/basic.exe
 sudo make install
+
+ls -laR .
 
 sudo apt-get -y install mono-devel
 echo "Installing RootCAs from Mozilla..."; 
