9ae5025995eb6ce4fcbcbae9486dc9b3c1c1aefc
Roel Standaert <roel@emweb.be>
2014-08-14 11:02:24 +0200

    Building boost from source, using g++ 4.8 to make it work on Ubuntu 12.04 and 14.04

--- toolset/setup/linux/frameworks/wt.sh
+++ toolset/setup/linux/frameworks/wt.sh
@@ -3,18 +3,22 @@
 RETCODE=$(fw_exists wt)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-# TODO can this be changed based on the OS we are using? This is not
-# available on 12.04 (libboost-all-dev is available, but requires some
-# apt-get cleaning before it can be installed)
-sudo apt-get -y install libboost1.54-all-dev
+fw_get http://downloads.sourceforge.net/project/boost/boost/1.55.0/boost_1_55_0.tar.gz -O boost_1_55_0.tar.gz
+fw_untar boost_1_55_0.tar.gz
+cd boost_1_55_0
+echo 'using gcc : 4.8 : g++-4.8 ;' > tools/build/v2/user-config.jam
+./bootstrap.sh --prefix=${IROOT}/boost
+./b2 --toolset gcc-4.8 install
+cd ..
 
-fw_get http://downloads.sourceforge.net/witty/wt-3.3.3.tar.gz -O wt.tar.gz
-fw_untar wt.tar.gz
+fw_get http://downloads.sourceforge.net/witty/wt-3.3.3.tar.gz -O wt-3.3.3.tar.gz
+fw_untar wt-3.3.3.tar.gz
 
-mv wt-* wt
-cd wt
+cd wt-3.3.3
 mkdir -p build
 cd build
-cmake .. -DWT_CPP_11_MODE=-std=c++0x -DCMAKE_BUILD_TYPE=Release
+cmake .. -DWT_CPP_11_MODE=-std=c++0x -DCMAKE_BUILD_TYPE=Release \
+	-DBOOST_PREFIX=${IROOT}/boost -DCMAKE_INSTALL_PREFIX=${IROOT}/wt \
+	-DCONFIGDIR=${IROOT}/wt/etc -DCMAKE_CXX_COMPILER=$(which g++-4.8)
 make
-sudo make install
+make install
