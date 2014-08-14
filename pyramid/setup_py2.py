94ea724e1f1066add31f86d4d9a664e3abda0526
INADA Naoki <songofacandy@gmail.com>
2014-08-14 15:27:39 +0900

    Fix typo

--- pyramid/setup_py2.py
+++ pyramid/setup_py2.py
@@ -7,12 +7,12 @@ CWD = os.path.abspath(os.path.dirname(__file__))
 def start(args, logfile, errfile):
     subprocess.Popen(
         "$PY2_GUNICORN wsgi:app -c gunicorn_conf.py",
-        cwd=CWD, sehll=True, stderr=errfile, stdout=logfile)
+        cwd=CWD, shell=True, stderr=errfile, stdout=logfile)
     return 0
 
 
 def stop(logfile, errfile):
     subprocess.call(
         "kill `cat gunicorn.pid`",
-        cwd=CWD, stderr=errfile, stdout=logfile, shell=True)
+        cwd=CWD, shell=True, stderr=errfile, stdout=logfile)
     return 0
