8a80e33c80732fc03ba2293e1b1219407c3c634c
Hamilton Turner <hamiltont@gmail.com>
2014-08-14 20:33:48 -0400

    Update dancer's nginx.conf to find new test root

--- frameworks/Perl/dancer/setup.py
+++ frameworks/Perl/dancer/setup.py
@@ -8,7 +8,7 @@ import getpass
 def start(args, logfile, errfile):
   setup_util.replace_text("dancer/app.pl", "localhost", args.database_host)
   setup_util.replace_text("dancer/nginx.conf", "USR", getpass.getuser())
-  setup_util.replace_text("dancer/nginx.conf", "server unix:.*\/FrameworkBenchmarks", "server unix:" + args.fwroot)
+  setup_util.replace_text("dancer/nginx.conf", "server unix:.*\/FrameworkBenchmarks/dancer", "server unix:" + args.troot)
 
   try:
     subprocess.Popen("plackup -E production -s Starman --workers=" + str(args.max_threads) + " -l $TROOT/frameworks-benchmark.sock -a ./app.pl", shell=True, cwd="dancer", stderr=errfile, stdout=logfile)
