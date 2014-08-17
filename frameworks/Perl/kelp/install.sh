8eb83142cbda61fa7c8d6208b7a3506c62b7c9d3
Stefan G <stefanguen@gmail.com>
2014-08-17 10:55:56 -0700

    Fix all issues with Kelp and add MongoDB tests

--- frameworks/Perl/kelp/install.sh
+++ frameworks/Perl/kelp/install.sh
@@ -2,5 +2,13 @@
 
 fw_depends perl nginx
 
-cpanm --notest --no-man-page Kelp DBI DBD::mysql Kelp::Module::JSON::XS Plack Starman
+cpanm --notest --no-man-page \
+    Kelp \
+    DBI \
+    DBD::mysql \
+    MongoDB \
+    Kelp::Module::JSON::XS \
+    HTML::Escape \
+    HTTP::Parser::XS \
+    Starman
 echo installed Kelp app dependencies
