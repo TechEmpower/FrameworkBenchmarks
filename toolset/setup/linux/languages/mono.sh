062015650d2a5650191159c7f6b0fb26886efd48
Hamilton Turner <hamiltont@gmail.com>
2014-08-17 19:40:01 -0400

    Improve mono installation

--- toolset/setup/linux/languages/mono.sh
+++ toolset/setup/linux/languages/mono.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-RETCODE=$(fw_exists mono-3.2.8.installed)
+RETCODE=$(fw_exists mono.installed)
 [ ! "$RETCODE" == 0 ] || { \
   echo "Installing RootCAs from Mozilla..."; 
   mozroots --import --sync;
@@ -9,16 +9,14 @@ RETCODE=$(fw_exists mono-3.2.8.installed)
 
 fw_get http://download.mono-project.com/sources/mono/mono-3.2.8.tar.bz2 -O mono-3.2.8.tar.bz2
 fw_untar mono-3.2.8.tar.bz2
+
 cd mono-3.2.8 
-./configure --disable-nls --prefix=`pwd`/mono-3.2.8-install
+./configure --disable-nls --prefix=$IROOT/mono-3.2.8-install
 make get-monolite-latest
 make -j4 EXTERNAL_MCS=${PWD}/mcs/class/lib/monolite/basic.exe
-sudo make install
+make install
 
-ls -laR .
-
-sudo apt-get -y install mono-devel
 echo "Installing RootCAs from Mozilla..."; 
 mozroots --import --sync;
 
-touch mono-3.2.8.installed
+touch $IROOT/mono.installed
