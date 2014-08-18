13ffbeff13ef961f20ace348a3e0e4d13afb71d4
Hamilton Turner <hamiltont@gmail.com>
2014-08-17 20:15:22 -0400

    Add xsp installation inside mono installation

--- toolset/setup/linux/languages/xsp.sh
+++ toolset/setup/linux/languages/xsp.sh
@@ -7,7 +7,7 @@ fw_depends mono
 git clone git://github.com/mono/xsp
 cd xsp
 git checkout 8a31bc625727594d42f94173768bee5cf8afd0a4
-./autogen.sh --prefix=$IROOT/xsp-install
+./autogen.sh --prefix=$IROOT/mono-3.2.8-install
 make
 make install
 
