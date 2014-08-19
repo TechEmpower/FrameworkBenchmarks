4b678439882ec8b7fa13fa07ffe262be4e482c5b
Hamilton Turner <hamiltont@gmail.com>
2014-08-18 21:54:28 -0400

    Install mozroot multiple ways to see if this works...

--- frameworks/C#/aspnet/install.sh
+++ frameworks/C#/aspnet/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+echo "Installing RootCAs from Mozilla..."; 
+mozroots --import --sync;
+sudo mozroots --import --sync;
+
 fw_depends nginx mono xsp
+
+echo "Installing RootCAs from Mozilla..."; 
+mozroots --import --sync;
+sudo mozroots --import --sync;
