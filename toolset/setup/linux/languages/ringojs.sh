c08abf257fff9fcdc4167376d8527cb589aa661f
Hamilton Turner <hamiltont@gmail.com>
2014-08-20 18:16:41 -0400

    Update ringojs installer

--- toolset/setup/linux/languages/ringojs.sh
+++ toolset/setup/linux/languages/ringojs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists /usr/share/ringojs)
+RETCODE=$(fw_exists ringo.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
 fw_get http://www.ringojs.org/downloads/ringojs_0.10-1_all.deb
 sudo apt-get install -y jsvc
 sudo dpkg -i ringojs_0.10-1_all.deb
 
-rm ringojs_0.10-1_all.deb
+rm -f ringojs_0.10-1_all.deb
+
+touch $IROOT/ringo.installed
