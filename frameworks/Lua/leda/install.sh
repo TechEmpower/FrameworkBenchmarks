148a18de8f332c416ab9a6652307877d472c9142
Hamilton Turner <hamiltont@gmail.com>
2014-08-20 12:54:03 -0400

    Cleanup leda

--- frameworks/Lua/leda/install.sh
+++ frameworks/Lua/leda/install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-fw_depends leda
+fw_depends lua
+
+sudo luarocks install lua-cjson
+
+wget -O leda.tar.gz http://sergeyzavadski.github.io/leda/releases/leda-0.5.4/leda-0.5.4.tar.gz 
+fw_untar leda.tar.gz
+cd leda-0.5.4
+
+./configure
+make 
+sudo make install
+cd .. 
+mv leda-0.5.4 leda
