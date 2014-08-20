4bb676a315b09ee4fa699b71246473689761d442
Hamilton Turner <hamiltont@gmail.com>
2014-08-19 23:32:31 -0400

    Remove JRuby and Ruby in favor of RVM

--- toolset/setup/linux/languages/jruby.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-RETCODE=$(fw_exists ~/.rvm/rubies/jruby-1.7.8/)
-[ ! "$RETCODE" == 0 ] || { return 0; }
-
-# Instead of installing separately, we install JRuby 
-# whenever we install Ruby
-fw_depends ruby
