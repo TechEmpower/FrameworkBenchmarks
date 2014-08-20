38e3bfce162042c10c1db3344a2a902ec223d0f1
Hamilton Turner <hamiltont@gmail.com>
2014-08-20 13:24:29 -0400

    Cleanup openresty

--- frameworks/Lua/openresty/setup.py
+++ frameworks/Lua/openresty/setup.py
@@ -4,13 +4,13 @@ import setup_util
 import os
 
 def start(args, logfile, errfile):
-  setup_util.replace_text("openresty/nginx.conf", "CWD", os.getcwd())
+  setup_util.replace_text("openresty/nginx.conf", "CWD", args.troot)
   setup_util.replace_text("openresty/app.lua", "DBHOSTNAME", args.database_host)
-  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c `pwd`/nginx.conf -g "worker_processes ' + str((args.max_threads)) + ';"', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
+  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes ' + str((args.max_threads)) + ';"', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
 
   return 0
 
 def stop(logfile, errfile):
-  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c `pwd`/nginx.conf -s stop', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
+  subprocess.Popen('sudo /usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -s stop', shell=True, cwd="openresty", stderr=errfile, stdout=logfile)
 
   return 0
--- toolset/setup/linux/webservers/openresty.sh
+++ toolset/setup/linux/webservers/openresty.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists /usr/local/openresty/nginx/sbin/nginx)
+RETCODE=$(fw_exists openresty.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_depends nginx
+fw_depends nginx lua
+
 fw_get http://openresty.org/download/ngx_openresty-1.5.12.1.tar.gz
 fw_untar ngx_openresty-1.5.12.1.tar.gz
+
 cd ngx_openresty-1.5.12.1
-./configure --with-luajit-xcflags=-DLUAJIT_NUMMODE=2 --with-cc-opt=-O2 --with-http_postgres_module -j2
-make -j2
+./configure --with-luajit-xcflags=-DLUAJIT_NUMMODE=2 --with-cc-opt=-O2 --with-http_postgres_module -j4
+make -j4
 sudo make install
+
+touch $IROOT/openresty.installed
