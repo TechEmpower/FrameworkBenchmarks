6dd081672f11d8fcf6b34963a6db26f610e7fe37
Hamilton Turner <hamiltont@gmail.com>
2014-08-22 13:48:57 -0400

    Add developer mode

--- toolset/deployment/vagrant/bootstrap.sh
+++ toolset/deployment/vagrant/bootstrap.sh
@@ -9,7 +9,17 @@ if [ ! -e "/home/vagrant/.firstboot" ]; then
   echo "export TFB_DATABASE_USER=vagrant" >> ~/.bash_profile
   echo "export TFB_CLIENT_IDENTITY_FILE=/home/vagrant/.ssh/id_rsa" >> ~/.bash_profile
   echo "export TFB_DATABASE_IDENTITY_FILE=/home/vagrant/.ssh/id_rsa" >> ~/.bash_profile
-  echo "export FWROOT=/home/vagrant/FrameworkBenchmarks" >> ~/.bash_profile 
+  if [ "$1" == "dev" ]; then
+    echo "FrameworkBenchmarks: Running in developer mode"
+    echo "export FWROOT=/FrameworkBenchmarks" >> ~/.bash_profile 
+    ln -s /FrameworkBenchmarks ~/FrameworkBenchmarks
+    if [ -e "/FrameworkBenchmarks/benchmark.cfg" ]; then
+      echo "You have a benchmark.cfg file that will interfere with Vagrant, moving"
+      mv /FrameworkBenchmarks/benchmark.cfg /FrameworkBenchmarks/benchmark.cfg.backup
+    fi
+  else
+    echo "export FWROOT=/home/vagrant/FrameworkBenchmarks" >> ~/.bash_profile 
+  fi
   source ~/.bash_profile
 
   # Enable SSH to localhost
@@ -25,7 +35,9 @@ if [ ! -e "/home/vagrant/.firstboot" ]; then
   sudo apt-get install -y python-pip
 
   # Initial project setup
-  git clone https://github.com/TechEmpower/FrameworkBenchmarks.git $FWROOT
+  if [ "$1" != "dev" ]; then
+    git clone https://github.com/TechEmpower/FrameworkBenchmarks.git $FWROOT
+  fi
   sudo pip install -r $FWROOT/config/python_requirements.txt
 
   # Setup databases, client workload generator
