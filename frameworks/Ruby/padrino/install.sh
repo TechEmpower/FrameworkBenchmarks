37027d8b6c563c4fe5be89d0b80b5fcc3cb281cf
Richard Nienaber <rjnienaber@gmail.com>
2014-08-25 16:01:19 +0100

    Added rubinius as a platform (#511)

--- frameworks/Ruby/padrino/install.sh
+++ frameworks/Ruby/padrino/install.sh
@@ -7,3 +7,6 @@ rvm ruby-2.0.0-p0 do bundle install --gemfile=$TROOT/Gemfile
 
 rvm install jruby-1.7.8
 rvm jruby-1.7.8 do bundle install --gemfile=$TROOT/Gemfile
+
+rvm install rbx-2.2.10
+rvm rbx-2.2.10 do bundle install --gemfile=$TROOT/Gemfile
--- frameworks/Ruby/rails/install.sh
+++ frameworks/Ruby/rails/install.sh
@@ -7,3 +7,6 @@ rvm 2.1.2 do bundle install --gemfile=$TROOT/Gemfile
 
 rvm install jruby-1.7.13
 rvm jruby-1.7.13 do bundle install --gemfile=$TROOT/Gemfile
+
+rvm install rbx-2.2.10
+rvm rbx-2.2.10 do bundle install --gemfile=$TROOT/Gemfile
