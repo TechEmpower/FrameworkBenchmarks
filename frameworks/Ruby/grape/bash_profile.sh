5499b4096bbda823195cab3fe1d83e3683ceefe9
Richard Nienaber <rjnienaber@gmail.com>
2014-08-29 00:35:47 +0100

    Fix locale for Rubinius

--- frameworks/Ruby/grape/bash_profile.sh
+++ frameworks/Ruby/grape/bash_profile.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8 
+export LANG=en_US.UTF-8
+
 # We assume single-user installation as 
 # done in our rvm.sh script and 
 # in Travis-CI
--- frameworks/Ruby/rails/install.sh
+++ frameworks/Ruby/rails/install.sh
@@ -8,5 +8,7 @@ rvm 2.1.2 do bundle install --gemfile=$TROOT/Gemfile
 rvm install jruby-1.7.13
 rvm jruby-1.7.13 do bundle install --gemfile=$TROOT/Gemfile
 
+export LC_ALL=en_US.UTF-8 
+export LANG=en_US.UTF-8
 rvm install rbx-2.2.10
 rvm rbx-2.2.10 do bundle install --gemfile=$TROOT/Gemfile
