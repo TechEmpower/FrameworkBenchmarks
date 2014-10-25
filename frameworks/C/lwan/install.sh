a51c049ac587e9692ee0474ad5e109d47e289638
Leandro Pereira <leandro@hardinfo.org>
2014-10-25 14:53:50 -0200

    frameworks/C/lwan: Add MySQL test for Lwan benchmarks as well.

--- frameworks/C/lwan/install.sh
+++ frameworks/C/lwan/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-REV='aa6c15fbdf63d9db722ddc72bd736b23381331be'
+REV='eb96604657dd940ecb70b56fef4279077e3f9c21'
 
 INSTALLED_FILE="${IROOT}/lwan-${REV}.installed"
 RETCODE=$(fw_exists ${INSTALLED_FILE})
@@ -10,7 +10,7 @@ RETCODE=$(fw_exists ${INSTALLED_FILE})
 
 # Lwan is only built during installation as a dependency sanity check.
 sudo apt-get update && \
-	sudo apt-get install libjemalloc-dev && \
+	sudo apt-get install libjemalloc-dev libmysqlclient-dev libsqlite3-dev && \
 	git clone git://github.com/lpereira/lwan.git && \
         cd lwan && \
         git checkout ${REV} && \
