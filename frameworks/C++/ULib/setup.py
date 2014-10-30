feca89a6aa800c85fe733fe83d0857ceaf8f43de
stefanocasazza <stefano.casazza@gmail.com>
2014-10-30 19:00:12 +0100

    add character-set to mysql db

--- frameworks/C++/ULib/setup.py
+++ frameworks/C++/ULib/setup.py
@@ -9,33 +9,27 @@ def get_env_for_database(args):
   database = 'mysql'       if args.database == 'MySQL' else 'sqlite'
   dbname   = 'hello_world' if args.database == 'MySQL' else os.environ['ULIB_ROOT'] + '/db/%.*s'
   return {
+      'UTRACE': '0 50M',
+      'UMEMPOOL': '135,0,0,34,8465,129,-17,-22,41',
       'ORM_DRIVER': database,
-      'ORM_OPTION': "host=" + args.database_host + " user=benchmarkdbuser password=benchmarkdbpass dbname=" + dbname,
-      'UMEMPOOL': '135,0,0,34,8465,129,-17,-22,41'
+      'ORM_OPTION': "host=" + args.database_host + " user=benchmarkdbuser password=benchmarkdbpass character-set=utf8 dbname=" + dbname
     }
 
 def start(args, logfile, errfile):
-  fwroot = args.fwroot
   try:
-    # ulib_root = subprocess.check_output('printf $ULIB_ROOT', shell=True, stderr=errfile)
     ulib_root = os.environ['ULIB_ROOT']
+
     fcfg = ulib_root + "/benchmark.cfg"
+    fprg = ulib_root + "/bin/userver_tcp"
 
     # 1. Change ULib Server configuration
-
-    # I don't understand if the two approach are different...
-    #threads = str(args.max_threads)
-    PROCS = str(multiprocessing.cpu_count())
-    setup_util.replace_text(fcfg, "PREFORK_CHILD .*", "PREFORK_CHILD " + PROCS)
-
-    fprg = ulib_root + "/bin/userver_tcp"
+    setup_util.replace_text(fcfg, "PREFORK_CHILD .*", "PREFORK_CHILD " + str(multiprocessing.cpu_count()))
 
     # 2. Start ULib Server (userver_tcp)
     logfile.write("ULib: trying to start server %s -c %s\n" % (fprg, fcfg))
 
     # Run in the background, but keep stdout/stderr for easy debugging
     subprocess.Popen( "%s -c %s" % (fprg, fcfg), shell=True, stdout=logfile, stderr=errfile, env = get_env_for_database(args))
-    # subprocess.Popen("UTRACE=\"0 50M\" " + fprg + " -c " + fcfg, shell=True, stdout=logfile, stderr=errfile, env = get_env_for_database(args))
 
     logfile.write("ULib: server STARTED\n")
     return 0
