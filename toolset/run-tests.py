61e7146c20c09b25205b13b177c6f2d6cff8fdd9
Alex Schneider <aschneider@techempower.com>
2014-10-31 16:58:24 -0700

    Fix some issues

--- toolset/run-tests.py
+++ toolset/run-tests.py
@@ -185,7 +185,7 @@ def main(argv=None):
     # Run the benchmarker in the specified mode
     #   Do not use benchmarker variables for these checks, 
     #   they are either str or bool based on the python version
-    if (type(args.list_test) is str and args.list_test.lower() == 'true') or (type(args.list_tests) is bool and args.list_tests):
+    if (type(args.list_tests) is str and args.list_tests.lower() == 'true') or (type(args.list_tests) is bool and args.list_tests):
       benchmarker.run_list_tests()
     elif (type(args.list_test_metadata) is str and args.list_test_metadata.lower() == 'true') or (type(args.list_test_metadata) is bool and args.list_test_metadata):
       benchmarker.run_list_test_metadata()
