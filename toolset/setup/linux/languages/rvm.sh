e9f3acd77dda74eeb2f4b545aa542fd0c7858529
msmith-techempower <msmith@techempower.com>
2014-10-31 11:25:53 -0700

    rvmFix Fixed a bug where the GPG signature was bad

--- toolset/setup/linux/languages/rvm.sh
+++ toolset/setup/linux/languages/rvm.sh
@@ -16,6 +16,7 @@ RETCODE=$(fw_exists rvm.installed)
 }
 
 # Run single-user installation and source rvm
+gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 source $HOME/.rvm/scripts/rvm
 
