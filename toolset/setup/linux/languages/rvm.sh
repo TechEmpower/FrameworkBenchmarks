7673dcc00729b6c830dde3c7d93239b7e51ca642
msmith-techempower <msmith@techempower.com>
2014-10-31 12:02:09 -0700

    rvmFix Fixes a bug with configuring RVM

--- toolset/setup/linux/languages/rvm.sh
+++ toolset/setup/linux/languages/rvm.sh
@@ -17,7 +17,7 @@ RETCODE=$(fw_exists rvm.installed)
 
 # Run single-user installation and source rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | SHELL=/bin/bash bash -s stable
 source $HOME/.rvm/scripts/rvm
 
 touch $IROOT/rvm.installed
