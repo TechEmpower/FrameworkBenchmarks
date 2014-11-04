1e13e5dd09e3624a813b78d3f0ea6c7ae926e2ab
Erwan Ameil <wan@idlewan.com>
2014-11-04 03:53:34 +0100

    Document Nimrod commits and fix 'already installed' condition

--- toolset/setup/linux/frameworks/jester.sh
+++ toolset/setup/linux/frameworks/jester.sh
@@ -5,4 +5,5 @@ RETCODE=$(fw_exists jester)
 
 git clone git://github.com/dom96/jester.git jester
 cd jester
+# most recent commit in branch new-async as of 2014-10-31
 git checkout 3e77ce753293b523004dfdaebd2fb195217f30f0
--- toolset/setup/linux/frameworks/nawak.sh
+++ toolset/setup/linux/frameworks/nawak.sh
@@ -5,6 +5,7 @@ RETCODE=$(fw_exists nawak)
 
 git clone git://github.com/idlewan/nawak.git nawak
 cd nawak
+# version 0.3.0 (2014-11-02)
 git checkout b34b0b5077541ae9671957452a70e2578894d3a8
 
 nimble update
