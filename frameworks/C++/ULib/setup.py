67f404d57ac2f6c4e6d9644f82005c42b6938651
stefanocasazza <stefano.casazza@gmail.com>
2014-11-04 19:45:17 +0100

    attempt to start travis

--- frameworks/C++/ULib/setup.py
+++ frameworks/C++/ULib/setup.py
@@ -21,24 +21,18 @@ def get_env_for_database(args):
 def start(args, logfile, errfile):
   try:
     ulib_root = os.environ['ULIB_ROOT']
-
     fcfg = ulib_root + "/benchmark.cfg"
     fprg = ulib_root + "/bin/userver_tcp"
-
     # 1. Change ULib Server configuration
     setup_util.replace_text(fcfg, "PREFORK_CHILD .*", "PREFORK_CHILD " + str(multiprocessing.cpu_count()))
-
     # 2. Start ULib Server (userver_tcp)
     subprocess.Popen( "%s -c %s" % (fprg, fcfg), shell=True, stdout=logfile, stderr=errfile, env = get_env_for_database(args))
-
     return 0
   except subprocess.CalledProcessError:
     return 1
 
 def stop(logfile, errfile):
   try:
-    logfile.write( "ULib: setup.py STOP\n")
-
     # Stop ULib Server (userver_tcp)
     subprocess.check_call("kill -TERM $( cat $ULIB_ROOT/userver_tcp.pid )", shell=True, stderr=errfile, stdout=logfile)
     time.sleep(3);
