c94a64daabfc466c53ac9fc83f14a33ab898186c
Tor Hveem <tor@hveem.no>
2014-11-05 00:54:43 +0100

    search for correct process

--- frameworks/Rust/Iron/setup.py
+++ frameworks/Rust/Iron/setup.py
@@ -14,7 +14,7 @@ def stop(logfile, errfile):
   p = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE)
   out, err = p.communicate()
   for line in out.splitlines():
-    if 'cargo' in line:
+    if 'iron' in line:
       pid = int(line.split(None, 2)[1])
       os.kill(pid, 15)
   return 0
