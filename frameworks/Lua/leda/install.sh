04fb903e85ba7bdd66d9b4fdfef6a22fa2ef09ac
msmith-techempower <msmith@techempower.com>
2014-11-06 07:33:22 -0800

    Kind of important to be in the correct dir
    I don't think this will fix the problem that Travis-CI
    is having, but I cannot reproduce it on our hardware
    and this was my best guess (even though the install
    portion suggests that it succeeded).

--- frameworks/Lua/leda/install.sh
+++ frameworks/Lua/leda/install.sh
@@ -5,8 +5,8 @@ fw_depends lua
 sudo luarocks install lua-cjson
 
 git clone https://github.com/sergeyzavadski/leda.git
-git checkout d1d805877c506e34d23d5aac2b27608c192695d3
 cd leda
+git checkout d1d805877c506e34d23d5aac2b27608c192695d3
 ./configure
 make 
 sudo make install
