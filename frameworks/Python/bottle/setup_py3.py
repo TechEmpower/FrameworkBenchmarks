677cbe8b172f3ed6df67c80044aca55b1111ddf3
Alex Schneider <aschneider@techempower.com>
2014-11-07 16:36:18 -0800

    Fix bottle's database host problem and fix uwsgi

--- frameworks/Python/bottle/setup_py3.py
+++ frameworks/Python/bottle/setup_py3.py
@@ -1,10 +1,12 @@
 import os
 import subprocess
+import setup_util
 
 CWD = os.path.dirname(__file__)
 
 
 def start(args, logfile, errfile):
+    setup_util.replace_text('bottle/app.py', 'database_host', args.database_host)
     subprocess.Popen(
         "$PY3_GUNICORN app:app -c gunicorn_conf.py",
         cwd=CWD, stderr=errfile, stdout=logfile, shell=True)
