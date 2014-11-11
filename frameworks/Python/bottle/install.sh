28d60eeb9c0a42bdfdc4327232eeafbac201de92
msmith-techempower <msmith@techempower.com>
2014-11-11 11:46:34 -0800

    Fixed the bottle installation issues

--- frameworks/Python/bottle/install.sh
+++ frameworks/Python/bottle/install.sh
@@ -5,11 +5,11 @@ export PIP_DOWNLOAD_CACHE=$IROOT/.pip_cache
 
 fw_depends python2 python3 pypy nginx
 
-$IROOT/py2/bin/virtualenv $TROOT/py2
-$TROOT/py2/bin/pip install -r $TROOT/requirements.txt
+$IROOT/py2/bin/virtualenv $IROOT/py2
+$IROOT/py2/bin/pip install -r $TROOT/requirements.txt
 
-$IROOT/py3/bin/python3 -m venv $TROOT/py3
-$TROOT/py3/bin/pip install -r $TROOT/requirements.txt
+$IROOT/py3/bin/python3 -m venv $IROOT/py3
+$IROOT/py3/bin/pip3 install -r $TROOT/requirements.txt
 
-$IROOT/pypy/bin/virtualenv $TROOT/pypy
-$TROOT/pypy/bin/pip install -r $TROOT/requirements-pypy.txt
+$IROOT/pypy/bin/virtualenv $IROOT/pypy
+$IROOT/pypy/bin/pip install -r $TROOT/requirements-pypy.txt
