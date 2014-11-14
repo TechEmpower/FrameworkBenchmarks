c0e91d63420f34e25eb5f1b4dd446240905655a1
msmith-techempower <msmith@techempower.com>
2014-11-14 10:10:44 -0800

    Fixed up lein and updated http-kit

--- frameworks/Clojure/compojure/setup.sh
+++ frameworks/Clojure/compojure/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Path vars must be set here
-export PATH="$JAVA_HOME/bin:$LEIN_HOME:$PATH"
+export PATH="$JAVA_HOME/bin:$PATH"
 
 sed -i 's|:subname "//.*:3306|:subname "//'"${DBHOST}"':3306|g' hello/src/hello/handler.clj
 
 cd hello
-$IROOT/bin/lein clean
-$IROOT/bin/lein ring uberwar
+$LEIN_HOME/bin/lein clean
+$LEIN_HOME/bin/lein ring uberwar
 rm -rf $RESIN_HOME/webapps/*
 cp target/hello-compojure-standalone.war $RESIN_HOME/webapps/compojure.war
 $RESIN_HOME/bin/resinctl start
