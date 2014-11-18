d8e352112ba4981c487574f8d542b84f0ee1cb67
msmith-techempower <msmith@techempower.com>
2014-11-18 10:49:38 -0800

    Naughty - bound to ports as root

--- frameworks/Lua/openresty/setup.sh
+++ frameworks/Lua/openresty/setup.sh
@@ -5,4 +5,4 @@ export PWD=$(pwd)
 sed -i 's|CWD|'"${PWD}"'|g' nginx.conf
 sed -i 's|DBHOSTNAME|'"${DBHOST}"'|g' app.lua
 
-sudo /usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes '$MAX_THREADS';" &
+/usr/local/openresty/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes '$MAX_THREADS';" &
