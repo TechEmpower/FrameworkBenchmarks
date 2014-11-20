bd5b1f3766b9df3be857d08978694b1c371e026e
msmith-techempower <msmith@techempower.com>
2014-11-20 14:47:15 -0800

    Fixed missing openresty setup

--- frameworks/Lua/openresty/setup.sh
+++ frameworks/Lua/openresty/setup.sh
@@ -3,4 +3,4 @@
 sed -i 's|CWD|'"${TROOT}"'|g' nginx.conf
 sed -i 's|DBHOSTNAME|'"${DBHOST}"'|g' app.lua
 
-${OPENRESTY_HOME}/nginx/sbin/nginx -c $TROOT/nginx.conf -g 'worker_processes \x27'"${MAX_THREADS}"'\x27;' &
+${OPENRESTY_HOME}/nginx/sbin/nginx -c $TROOT/nginx.conf -g "worker_processes '"${MAX_THREADS}"';" &
