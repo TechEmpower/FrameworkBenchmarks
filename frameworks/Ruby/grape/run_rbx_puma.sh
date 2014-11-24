ac273657524ed42ab1bc2c2be32f699c0adddd2c
Mike Smith <msmith@techempower.com>
2014-11-24 09:38:57 -0800

    Updated grape

---
+++ frameworks/Ruby/grape/run_rbx_puma.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# We assume single-user installation as 
+# done in our rvm.sh script and 
+# in Travis-CI
+source $HOME/.rvm/scripts/rvm
+
+sed -i 's|  host:.*|  host: '"${DBHOST}"'|g' config/database.yml
+
+rvm rbx-2.2.10 do bundle exec puma -b tcp://0.0.0.0:8080 -e production &
