bd808a10e6ecdae7d707d22e1ecae5861e332f0c
Lita Gratrix <lgratrix@techempower.com>
2014-12-11 14:30:50 -0800

    fix typo in setup.sh for php-fuel

--- frameworks/PHP/php-fuel/setup.sh
+++ frameworks/PHP/php-fuel/setup.sh
@@ -7,4 +7,4 @@ sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 export PATH="$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
 
 $PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
-$NGINX_HOME/sbin/nginx -c $TROOT/deplpy/nginx.conf
+$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
