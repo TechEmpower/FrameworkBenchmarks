2f8c92ffa586f1b7fc32de98af7ff7a47eb16ad8
Lita Gratrix <lgratrix@techempower.com>
2014-12-12 13:13:55 -0800

    Convert setup.py and setup-raw.py to setup.sh and setup-raw.sh. Confirmed that tests ran but was unable to pass tests as was receiving 404s.

---
+++ frameworks/PHP/php-pimf/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed -i 's|127.0.0.1|'"${DBHOST}"'|g' app/config.app.php
+sed -i 's|".*/FrameworkBenchmarks/php-pimf|"'"${TROOT}"'|g' deploy/php-pimf
+sed -i 's|Directory .*/FrameworkBenchmarks/php-pimf|Directory '"${TROOT}"'|g' deploy/php-pimf
+sed -i 's|root .*/FrameworkBenchmarks/php-pimf|root '"${TROOT}"'|g' deploy/php-pimf
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
+
+export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+
+composer.phar install --optimize-autoloader
+$PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
+$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
