4b7b5f2852a416a7c9c09a645cbb0a91acf17c07
Lita Gratrix <lgratrix@techempower.com>
2014-12-12 08:05:42 -0800

    Convert setup.py to setup.sh for php-phalcon-micro

---
+++ frameworks/PHP/php-phalcon-micro/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i 's|localhost|'"${DBHOST}"'|g' public/index.php
+sed -i 's|root .*/FrameworkBenchmarks/php-phalcon-micro|root '"${TROOT}"'|g' deploy/nginx.conf
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
+
+export PATH="$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+
+$PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
+$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
