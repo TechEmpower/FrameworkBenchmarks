1df2cbaf1f308efe60587a719dd8375aab9b129f
Lita Gratrix <lgratrix@techempower.com>
2014-12-15 13:54:58 -0800

    correct typo in setup.sh for php-symfony2-stripped

--- frameworks/PHP/php-symfony2-stripped/setup.sh
+++ frameworks/PHP/php-symfony2-stripped/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sed -i 's|database_host: .*|database_host: '"${DBHOST}"'|g' app/config/parameters.yml
-sed -i 's|root .*/FrameworkBenchmarks/php-symfony2| root '"${TROOT}"'|g' deploy/nginx.conf
+sed -i 's|root .*/FrameworkBenchmarks/php-symfony2-stripped| root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
 export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
