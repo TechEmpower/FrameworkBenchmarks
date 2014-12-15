b6f0ee205d6448e7205ea4b22958e398af0cabbf
Lita Gratrix <lgratrix@techempower.com>
2014-12-15 13:49:46 -0800

    Convert setup.py to setup.sh for php-symfony2-stripped.
    Unable to run test due to error during setup. As it appears related to Apc
    and there was a similar issue on symfony2, left as unresolved

---
+++ frameworks/PHP/php-symfony2-stripped/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sed -i 's|database_host: .*|database_host: '"${DBHOST}"'|g' app/config/parameters.yml
+sed -i 's|root .*/FrameworkBenchmarks/php-symfony2| root '"${TROOT}"'|g' deploy/nginx.conf
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
+
+export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+
+composer.phar install --optimize-autoloader
+php app/console cache:clear --env=prod --no-debug
+php app/console cache:warmup --env=prod --no-debug
+$PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
+$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
