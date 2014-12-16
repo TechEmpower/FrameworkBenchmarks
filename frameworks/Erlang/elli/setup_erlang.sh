18684e3005918acfb8f55faa864029039c094ba0
msmith-techempower <msmith@techempower.com>
2014-12-16 09:15:58 -0800

    Fixed a bug with elli

--- frameworks/Erlang/elli/setup_erlang.sh
+++ frameworks/Erlang/elli/setup_erlang.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i 's|"benchmarkdbpass", ".*", 3306"|"benchmarkdbpass", "'"${DBHOST}"'", 3306"|g' src/elli_bench_sup.erl
+sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/elli_bench_sup.erl
 
 rm -rf deps/*
 $REBAR_HOME/rebar get-deps
