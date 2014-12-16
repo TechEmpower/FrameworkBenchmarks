ba9702b441f89a42ecebb88f4a9b49d2dfaf7e80
msmith-techempower <msmith@techempower.com>
2014-12-16 10:00:37 -0800

    Fixed a bug with rails-stripped

--- frameworks/Ruby/rails-stripped/bash_profile.sh
+++ frameworks/Ruby/rails-stripped/bash_profile.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 
+export NGINX_HOME=${IROOT}/nginx
--- frameworks/Ruby/rails-stripped/setup_ruby.sh
+++ frameworks/Ruby/rails-stripped/setup_ruby.sh
@@ -8,7 +8,9 @@ sed -i 's|host: .*|host: '"${DBHOST}"'|g' config/database.yml
 source $HOME/.rvm/scripts/rvm
 
 rm -f Gemfile
-cp Gemfile-jruby Gemfile
-cp Gemfile-jruby.lock Gemfile.lock
+cp Gemfile-ruby Gemfile
+cp Gemfile-ruby.lock Gemfile.lock
+
+$NGINX_HOME/sbin/nginx -c $TROOT/config/nginx.conf
 
 rvm ruby-2.0.0-p0 do bundle exec unicorn_rails -E production -c $TROOT/config/unicorn.rb &
