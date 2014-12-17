bdb3020b77ec9530de4a2e1a561503a50a50315c
msmith-techempower <msmith@techempower.com>
2014-12-17 08:42:39 -0800

    Fixed Urweb

--- frameworks/Ur/urweb/setup.sh
+++ frameworks/Ur/urweb/setup.sh
@@ -2,4 +2,6 @@
 
 ${URWEB_HOME}/bin/urweb -db "dbname=hello_world user=benchmarkdbuser password=benchmarkdbpass host=${DBHOST}" bench
 
+export LD_LIBRARY_PATH=${URWEB_HOME}/lib
+
 ./bench.exe -q -k -t ${MAX_THREADS} &
