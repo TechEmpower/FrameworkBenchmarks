9564336933d3bba3dc3fca6c5fa6e92ddbede32a
msmith-techempower <msmith@techempower.com>
2014-12-17 15:04:48 -0800

    Fixed play-activate-mysql

--- frameworks/Scala/play-activate-mysql/install.sh
+++ frameworks/Scala/play-activate-mysql/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fw_depends java maven play2
+fw_depends java maven play2 sbt
--- toolset/setup/linux/systools/sbt.sh
+++ toolset/setup/linux/systools/sbt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists sbt/bin/sbt)
+RETCODE=$(fw_exists ${IROOT}/sbt.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
 fw_get http://dl.bintray.com/sbt/native-packages/sbt/0.13.5/sbt-0.13.5.zip -O sbt-0.13.5.zip
 fw_unzip sbt-0.13.5.zip
+
+touch ${IROOT}/sbt.installed
