35a9a461b7cfc685295618a3ad1d24981d8b3b15
Lita Gratrix <lgratrix@techempower.com>
2014-12-19 13:03:57 -0800

    fix web-simple

--- frameworks/Perl/web-simple/install.sh
+++ frameworks/Perl/web-simple/install.sh
@@ -2,5 +2,3 @@
 
 fw_depends perl nginx
 
-cpanm --notest --no-man-page Web::Simple DBI DBD::mysql Plack Starman JSON::XS
-echo installed Web::Simple app dependencies
--- frameworks/Perl/web-simple/setup.sh
+++ frameworks/Perl/web-simple/setup.sh
@@ -5,5 +5,8 @@ sed -i 's|server unix:.*/FrameworkBenchmarks/web-simple|server unix:'"${TROOT}"'
 
 export PATH="$PERL_HOME/bin:$PATH"
 
+cpanm --notest --no-man-page Web::Simple DBI DBD::mysql Plack Starman JSON::XS
+echo installed Web::Simple app dependencies
+
 plackup -E production -s Starman --workers=${MAX_THREADS} -l $TROOT/frameworks-benchmark.sock -a $TROOT/app.pl &
 $NGINX_HOME/sbin/nginx -c $TROOT/nginx.conf
