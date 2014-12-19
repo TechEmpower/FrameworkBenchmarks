3e1754c727563824bd4de632ebb8b88c946ecec1
Lita Gratrix <lgratrix@techempower.com>
2014-12-19 14:02:05 -0800

    fix cakephp for json and fortunes

--- frameworks/PHP/cakephp/setup.sh
+++ frameworks/PHP/cakephp/setup.sh
@@ -5,6 +5,7 @@ sed -i 's|REDISSERVER|'"${DBHOST}"'|g' app/Config/core.php
 sed -i 's|".*/FrameworkBenchmarks/cakephp|"'"${TROOT}"'|g' deploy/cake
 sed -i 's|Directory .*/FrameworkBenchmarks/cakephp|Directory '"${TROOT}"'|g' deploy/cake
 sed -i 's|root .*/FrameworkBenchmarks/cakephp|root '"${TROOT}"'|g' deploy/nginx.conf
+sed -i 's|/usr/local/nginx|'"${NGINX_HOME}"'|g' deploy/nginx.conf
 
 $PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
 
