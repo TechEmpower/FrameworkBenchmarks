9ed168143264fed403ea1724cf422bf99cfd3049
Lita Gratrix <lgratrix@techempower.com>
2014-12-19 10:06:49 -0800

    Fix for express

--- frameworks/JavaScript/express/setup.sh
+++ frameworks/JavaScript/express/setup.sh
@@ -4,6 +4,7 @@ sed -i 's|mongodb://.*/hello_world|mongodb://'"${DBHOST}"'/hello_world|g' app.js
 sed -i 's|localhost|'"${DBHOST}"'|g' app.js
 
 export NODE_ENV=production
+export PATH="$NODE_HOME/bin:$PATH"
 
 ${NODE_HOME}/bin/npm install
 ${NODE_HOME}/bin/node app &
--- toolset/setup/linux/languages/nodejs.sh
+++ toolset/setup/linux/languages/nodejs.sh
@@ -6,4 +6,9 @@ RETCODE=$(fw_exists ${IROOT}/node-v0.10.8.installed)
 fw_get http://nodejs.org/dist/v0.10.8/node-v0.10.8-linux-x64.tar.gz
 fw_untar node-v0.10.8-linux-x64.tar.gz
 
+export PATH="$NODE_HOME/bin:$PATH"
+
+${NODE_HOME}/bin/npm install findup-sync@0.1.2
+${NODE_HOME}/bin/npm install -g npm
+
 touch ${IROOT}/node-v0.10.8.installed
