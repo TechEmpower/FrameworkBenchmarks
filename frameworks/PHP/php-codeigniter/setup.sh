5053a9b4e178b013d8eef975bc146743fc0a9d11
Hamilton Turner <hamiltont@gmail.com>
2014-12-20 18:19:53 -0500

    Remove bash profile

--- frameworks/PHP/php-codeigniter/setup.sh
+++ frameworks/PHP/php-codeigniter/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export PHP_HOME=${IROOT}/php-5.5.17
+
+export PHP_FPM=$PHP_HOME/sbin/php-fpm
+
+export NGINX_HOME=${IROOT}/nginx
+
+
 sed -i 's|localhost|'"${DBHOST}"'|g' application/config/database.php
 sed -i 's|root .*/FrameworkBenchmarks/php-codeigniter|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
