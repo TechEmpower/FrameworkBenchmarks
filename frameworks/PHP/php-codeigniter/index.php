bd1b13a8fda060dfa66ccad47804bd0997b0d886
Hamilton Turner <hamiltont@gmail.com>
2014-12-20 18:19:39 -0500

    Download framework dynamically using composer

--- frameworks/PHP/php-codeigniter/index.php
+++ frameworks/PHP/php-codeigniter/index.php
@@ -56,7 +56,7 @@ if (defined('ENVIRONMENT'))
  * as this file.
  *
  */
-	$system_path = 'system';
+	$system_path = 'vendor/rogeriopradoj/codeigniter/system';
 
 /*
  *---------------------------------------------------------------
