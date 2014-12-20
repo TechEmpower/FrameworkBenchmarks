50d305c87d938eed75e7f45def906e3ca40df918
Hamilton Turner <hamiltont@gmail.com>
2014-12-20 00:23:01 -0500

    Remove bash_profiles

--- frameworks/PHP/lithium/setup.sh
+++ frameworks/PHP/lithium/setup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+export PHP_HOME=${IROOT}/php-5.5.17
+
+export PHP_FPM=$PHP_HOME/sbin/php-fpm
+
+export NGINX_HOME=${IROOT}/nginx
+
+
 sed -i 's|192.168.100.102|'"${DBHOST}"'|g' app/config/bootstrap/connections.php
 sed -i 's|root .*/FrameworkBenchmarks/php-lithium|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
