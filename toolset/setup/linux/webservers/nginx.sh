2edfc0c986e4f5506a1df0ec0f91798905122337
Hamilton Turner <hamiltont@gmail.com>
2014-12-20 14:00:42 -0500

    Quiet Nginx installation
    Removes a few hundred lines of log output for compiling and
    installing nginx

--- toolset/setup/linux/webservers/nginx.sh
+++ toolset/setup/linux/webservers/nginx.sh
@@ -6,8 +6,13 @@ RETCODE=$(fw_exists ${IROOT}/nginx.installed)
 fw_get http://nginx.org/download/nginx-1.4.1.tar.gz
 fw_untar nginx-1.4.1.tar.gz
 cd nginx-1.4.1
-./configure --prefix=$IROOT/nginx
-make
-make install
+
+# There is no --quiet flag that I could find...
+echo "Configuring nginx..."
+./configure --prefix=$IROOT/nginx > /dev/null
+
+echo "Compiling and installing nginx..."
+make --quiet
+make --quiet install
 
 touch ${IROOT}/nginx.installed
