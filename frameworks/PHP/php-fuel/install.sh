86e38bb400e086b500e88b8a4e3c3f929ad3150a
Hamilton Turner <hamiltont@gmail.com>
2014-12-21 12:11:40 -0500

    Install fuel core and packages using composer
    NOTE: This also upgrades our version of fuel from 1.5 to1.7

--- frameworks/PHP/php-fuel/install.sh
+++ frameworks/PHP/php-fuel/install.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-fw_depends php nginx
+export PHP_HOME=${IROOT}/php-5.5.17
+export PHP_FPM=$PHP_HOME/sbin/php-fpm
+export NGINX_HOME=${IROOT}/nginx
+
+
+fw_depends php nginx composer
+
+${PHP_HOME}/bin/php $IROOT/composer.phar install \
+  --no-interaction --working-dir $TROOT \
+  --no-progress --optimize-autoloader 
