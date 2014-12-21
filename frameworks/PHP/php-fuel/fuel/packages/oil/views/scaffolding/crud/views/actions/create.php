4985c86625b4af6d8249e80e865d241c42fd3b0d
Hamilton Turner <hamiltont@gmail.com>
2014-12-21 11:49:23 -0500

    more source removal

--- frameworks/PHP/php-fuel/fuel/packages/oil/views/scaffolding/crud/views/actions/create.php
+++
@@ -1,7 +0,0 @@
-<h2>New <?php echo \Str::ucfirst($singular_name); ?></h2>
-<br>
-
-<?php echo '<?php'; ?> echo render('<?php echo $view_path ?>/_form'); ?>
-
-
-<p><?php echo '<?php'; ?> echo Html::anchor('<?php echo $uri ?>', 'Back'); <?php echo '?>'; ?></p>
