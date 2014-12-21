1330851179d834299781f6150c0ddd526212923b
Hamilton Turner <hamiltont@gmail.com>
2014-12-21 02:46:54 -0500

    Replace python setup with bash setup

--- frameworks/PHP/php-senthot/setup.sh
+++ frameworks/PHP/php-senthot/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sed -i "s|'DB_HOST' => 'localhost'|'DB_HOST' => '\"${DBHOST}\"'|g" app/Conf/config.php
-sed -i 's|root .*/FrameworkBenchmarks/php-senthot|root '"${TROOT}"'|g' deploy/nginx.conf
-sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
+sed -i "s|localhost|${DBHOST}|g" app/Conf/config.php
+sed -i "s|root .*/FrameworkBenchmarks/php-senthot|root ${TROOT}|g" deploy/nginx.conf
+sed -i "s|/usr/local/nginx/|${IROOT}/nginx/|g" deploy/nginx.conf
 
 export PATH="$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
 
