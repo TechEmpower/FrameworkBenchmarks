b0b65290cf425575444e025649b61b5d536a32a8
Pēteris Ņikiforovs <peteris.nikiforovs@tilde.lv>
2014-12-22 13:08:12 +0200

    Fix nginx for ASP.NET

--- frameworks/CSharp/aspnet/setup_nginx.sh
+++ frameworks/CSharp/aspnet/setup_nginx.sh
@@ -2,10 +2,18 @@
 
 sed -i 's|localhost|'"$DBHOST"'|g' src/Web.config
 
+# todo move somewhere else
+wget -N http://nuget.org/nuget.exe -O lib/.nuget/NuGet.exe
+
 # build
-rm -rf bin obj
 cd src
-$MONO_ROOT/bin/xbuild /p:Configuration=Release
+rm -rf bin obj
+xbuild Benchmarks.sln /p:Configuration=Release
+
+# http://stackoverflow.com/questions/4239645/does-the-razor-view-engine-work-for-mono
+rm bin/Microsoft.Web.Infrastructure.dll
+
+cd ..
 
 # nginx
 conf="upstream mono {\n"
@@ -25,6 +33,6 @@ $NGINX_HOME/sbin/nginx -c $TROOT/nginx.conf -g "${MAX_THREADS}"
 current=9001
 end=$(($current+$MAX_THREADS))
 while [ $current -lt $end ]; do
-  MONO_OPTIONS=--gc=sgen fastcgi-mono-server4 --applications=/:$(pwd)/src --socket=tcp:127.0.0.1:$current
+  fastcgi-mono-server4 --applications=/:$(pwd)/src --socket=tcp:127.0.0.1:$current
   let current=current+1
 done
