42029a7b01a60e3ac26320e806edf18ea0d87d49
Pēteris Ņikiforovs <p@mak.id.lv>
2014-12-22 23:30:53 +0200

    Try to fix mono install from snapshots

--- toolset/setup/linux/languages/mono.sh
+++ toolset/setup/linux/languages/mono.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 post_install () {
   . mono-snapshot mono/20141222114925
   
@@ -18,11 +20,12 @@ RETCODE=$(fw_exists $IROOT/monosnap.installed)
   return 0
 }
 
-echo "Installing mono from CI packages"
+echo "Installing mono from packages"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+echo "deb http://download.mono-project.com/repo/debian wheezy main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
 echo "deb http://jenkins.mono-project.com/repo/debian sid main" | sudo tee /etc/apt/sources.list.d/mono-jenkins.list
 sudo apt-get update
-sudo apt-get install -y mono-snapshot-20141222114925
+sudo apt-get install -y -f cli-common mono-snapshot-20141222114925
 
 post_install
 
