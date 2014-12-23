7f83ee0751311b4b9a9fc2dc2a5c7c4aee028e61
INADA Naoki <inada-n@klab.com>
2014-12-23 12:44:00 +0900

    Update Python 2 and PyPy
    * CPython 2.7.9
    * PyPy 2.4.0
    * meinheld==0.5.7, greenlet==0.4.5
    * Fix nginx config.

--- toolset/setup/linux/languages/python2.sh
+++ toolset/setup/linux/languages/python2.sh
@@ -3,17 +3,14 @@
 RETCODE=$(fw_exists ${IROOT}/py2.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://www.python.org/ftp/python/2.7.8/Python-2.7.8.tgz
-fw_untar Python-2.7.8.tgz
-cd Python-2.7.8
+fw_get http://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
+fw_untar Python-2.7.9.tgz
+cd Python-2.7.9
 ./configure --prefix=${IROOT}/py2 --disable-shared --quiet
 make -j4 --quiet
 make install --quiet
 
-cd ..
-if [ ! -f "get-pip.py" ]; then
-fw_get https://bootstrap.pypa.io/get-pip.py -O get-pip.py
-fi
-${IROOT}/py2/bin/python get-pip.py
+${IROOT}/py2/bin/python -m ensurepip -U
+${IROOT}/py2/bin/pip install -U setuptools pip
 
 touch ${IROOT}/py2.installed
