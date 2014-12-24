401ad02c62773aba400c64dd278cbef5529c084e
Keenan Brock <keenan@thebrocks.net>
2014-12-23 22:13:07 -0500

    ngx_mruby cleanup
    - add ngx_mruby to travis
    - merge nginx install into mruby install.sh
    - add config options to nginx.conf
    - remove comments

--- frameworks/Ruby/ngx_mruby/bash_profile.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-export MRUBY_HOME=${IROOT}/nginx_mruby
-source $HOME/.rvm/scripts/rvm
--- frameworks/Ruby/ngx_mruby/install.sh
+++ frameworks/Ruby/ngx_mruby/install.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-#? m_ruby nginx
-fw_depends nginx_mruby
+PREFIX=${IROOT}/nginx_mruby
+
+RETCODE=$(fw_exists ${IROOT}/nginx_mruby.installed)
+[ ! "$RETCODE" == 0 ] || { return 0; }
+
+sudo apt-get install -y libhiredis-dev
+fw_depends rvm
+rvm install ruby-2.0.0-p0
+rvm use ruby-2.0.0-p0
+git clone git://github.com/matsumoto-r/ngx_mruby.git
+cd ngx_mruby
+git submodule init
+git submodule update
+
+NGINX_CONFIG_OPT_ENV="--prefix=${PREFIX} --with-http_stub_status_module" sh build.sh
+sudo make install
+
+touch ${IROOT}/nginx_mruby.installed
