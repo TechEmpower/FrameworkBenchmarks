3a34a186415bc619b1b55fb0ddc9757b31062723
Hamilton Turner <hamiltont@gmail.com>
2014-12-25 02:43:40 -0500

    Remove APC cache (php 5.5 included Zend cache already)

--- frameworks/PHP/php-symfony2/web/app.php
+++ frameworks/PHP/php-symfony2/web/app.php
@@ -1,20 +1,16 @@
 <?php
 
-use Symfony\Component\ClassLoader\ApcClassLoader;
 use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
-// Use APC for autoloading to improve performance.
-// Change 'sf2' to a unique prefix in order to prevent cache key conflicts
-// with other applications also using APC.
-
-$loader = new ApcClassLoader('symfony2Benchmark', $loader);
-$loader->register(true);
-
-
 require_once __DIR__.'/../app/AppKernel.php';
-//require_once __DIR__.'/../app/AppCache.php';
+
+// FrameworkBenchmark rules prohibit using AppCache, 
+// specifically every request for the JSON test type
+// must be handled separately
+// 
+// require_once __DIR__.'/../app/AppCache.php';
 
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
