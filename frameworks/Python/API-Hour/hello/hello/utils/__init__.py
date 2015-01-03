297d7fa6ac7c7ab924f775d480765b0f137329b9
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2015-01-03 22:32:11 +0100

    API-Hour FrameworkBenchmark: Migrate HTTP to aiohttp.web

--- frameworks/Python/API-Hour/hello/hello/utils/__init__.py
+++ frameworks/Python/API-Hour/hello/hello/utils/__init__.py
@@ -1,4 +1,16 @@
 import logging
-import asyncio
+
+from aiohttp.web import Response
+import ujson
 
 LOG = logging.getLogger(__name__)
+
+class JSON(Response):
+    """Serialize response to JSON"""
+
+    def __init__(self, json, status=200,
+                 reason=None, headers=None):
+        body = ujson.dumps(json).encode('utf-8')
+
+        super().__init__(body=body, status=status, reason=reason,
+                         headers=headers, content_type='application/json')
