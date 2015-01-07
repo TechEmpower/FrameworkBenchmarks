a2b252afeaf464070216244d8b5bc37faf7c9fdd
Lita Gratrix <lgratrix@techempower.com>
2015-01-07 12:41:00 -0800

    fix nginx-based ServiceStack tests

--- frameworks/CSharp/servicestack/bash_profile.sh
+++ frameworks/CSharp/servicestack/bash_profile.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-
-export MONO_ROOT=${IROOT}/mono-3.6.0-install
-
-export PATH="$MONO_ROOT/bin:$PATH"
-
-# Needed to find Mono's shared libraries
-export LD_LIBRARY_PATH="$MONO_ROOT/lib"
+export NGINX_HOME=${IROOT}/nginx
--- toolset/setup/linux/languages/xsp.sh
+++ toolset/setup/linux/languages/xsp.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
+set -e
 RETCODE=$(fw_exists ${IROOT}/xsp.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
-
 fw_depends mono
+# mono environment variables
+. ${IROOT}/mono.installed
+# get
 git clone git://github.com/mono/xsp
 cd xsp
-git checkout 8a31bc625727594d42f94173768bee5cf8afd0a4
-
-export PKG_CONFIG_PATH=${IROOT}/mono-3.6.0-install/lib/pkgconfig
-export PATH=${IROOT}/mono-3.6.0-install/bin:$PATH
-
-./autogen.sh --prefix=${IROOT}/mono-3.6.0-install
+git checkout e272a2c006211b6b03be2ef5bbb9e3f8fefd0768
+# build
+./autogen.sh --prefix=${MONO_HOME} --disable-docs
 make
 make install
-
+# cleanup
+cd ..
+rm -rf xsp
 touch ${IROOT}/xsp.installed
