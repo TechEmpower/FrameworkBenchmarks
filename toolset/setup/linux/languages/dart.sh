ad1e7c31788cf289a51dcc99422f4c9d8caa7dfe
msmith-techempower <msmith@techempower.com>
2015-01-09 09:53:14 -0800

    Removed bash_profile from dart

--- toolset/setup/linux/languages/dart.sh
+++ toolset/setup/linux/languages/dart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists dart-sdk)
+RETCODE=$(fw_exists ${IROOT}/dart-sdk.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
 fw_get http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip
 fw_unzip dartsdk-linux-x64-release.zip
+
+touch ${IROOT}/dart-sdk.installed
