0c29f0427c9d4a37e5cd472681c7fe40ced09e09
Lita Gratrix <lgratrix@techempower.com>
2015-01-14 12:08:22 -0800

    correct install.sh for codeigniter

--- frameworks/PHP/codeigniter/install.sh
+++ frameworks/PHP/codeigniter/install.sh
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists ${IROOT}/php-composer.installed)
-[ ! "$RETCODE" == 0 ] || { return 0; }
+export PHP_HOME=${IROOT}/php-5.5.17
+export COMPOSER_HOME=${IROOT}/php-composer
+export PHP_FPM=${PHP_HOME}/sbin/php-fpm
+export NGINX_HOME=${IROOT}/nginx
 
-fw_depends php
+fw_depends php nginx composer
 
-mkdir -p php-composer
-cd php-composer
-
-fw_get https://getcomposer.org/installer -O composer-installer.php
-
-# Use the PHP and composer from our PHP_HOME directory and 
-# COMPOSER_HOME directories (should be specified in frameworks 
-# install.sh file)
-${PHP_HOME}/bin/php composer-installer.php --install-dir=${COMPOSER_HOME}
-
-cd ..
-touch ${IROOT}/php-composer.installed
+${PHP_HOME}/bin/php ${COMPOSER_HOME}/composer.phar install \
+  --no-interaction --working-dir $TROOT \
+  --no-progress --optimize-autoloader 
