9095a3b61499fda0b9375df7b1175bad18b47c17
Lita Gratrix <lgratrix@techempower.com>
2015-01-19 12:28:23 -0800

    remove bash_profile.sh for rack

--- frameworks/Ruby/rack/run_unicorn.sh
+++ frameworks/Ruby/rack/run_unicorn.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
+export NGINX_HOME=${IROOT}/nginx
 
 sed -i 's|  host:.*|  host:'"${DBHOST}"'|g' config/database.yml
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' config/nginx.conf
 
 # We assume single-user installation as 
 # done in our rvm.sh script and 
