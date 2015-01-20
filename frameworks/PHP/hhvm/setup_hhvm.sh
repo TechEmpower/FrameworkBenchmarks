bffce9526375f937f51c2713a636595988822fe7
Joshua Maddux <jmaddux@techempower.com>
2015-01-19 17:27:27 -0800

    Correct a couple of paths that got sed'ed.

--- frameworks/PHP/hhvm/setup_hhvm.sh
+++ frameworks/PHP/hhvm/setup_hhvm.sh
@@ -5,6 +5,8 @@ sed -i 's|SourceRoot = .*/FrameworkBenchmarks/hhvm|SourceRoot = '"${TROOT}"'|g' 
 sed -i 's|Path = .*/.hhvm.hhbc|Path = '"${TROOT}"'/.hhvm.bbhc|g' deploy/config.hdf
 sed -i 's|PidFile = .*/hhvm.pid|PidFile = '"${TROOT}"'/hhvm.pid|g' deploy/config.hdf
 sed -i 's|File = .*/error.log|File = '"${TROOT}"'/error.log|g' deploy/config.hdf
+sed -i "s|/usr/local/nginx/|${IROOT}/nginx/|g" deploy/nginx.conf
+sed -i "s|TEST_ROOT|${TROOT}|g" deploy/nginx.conf
 
 hhvm --config $TROOT/deploy/config.hdf -m daemon
 $NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
