ffd5abf1562a2b77528444e7a42d617e67b3576d
Lita Gratrix <lgratrix@techempower.com>
2015-01-20 07:11:21 -0800

    remove bash_profile.sh from rails

--- frameworks/Ruby/rails/run_unicorn.sh
+++ frameworks/Ruby/rails/run_unicorn.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+export NGINX_HOME=${IROOT}/nginx
 
 # We assume single-user installation as 
 # done in our rvm.sh script and 
 # in Travis-CI
 source $HOME/.rvm/scripts/rvm
+
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' config/nginx.conf
 
 $NGINX_HOME/sbin/nginx -c $TROOT/config/nginx.conf
 
