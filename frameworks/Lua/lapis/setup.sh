184020b995d9c10642f84f21b7d4898da3d53a7e
Lita Gratrix <lgratrix@techempower.com>
2015-01-21 12:15:10 -0800

    remove bash_profile.sh from lapis. Update reference to correct version of openresty

--- frameworks/Lua/lapis/setup.sh
+++ frameworks/Lua/lapis/setup.sh
@@ -4,6 +4,8 @@ sed -i 's|DBHOSTNAME|'"${DBHOST}"'|g' config.lua
 sed -i 's|DBHOSTNAME|'"${DBHOST}"'|g' config.moon
 sed -i 's|DBHOSTNAME|'"${DBHOST}"'|g' nginx.conf
 
+export OPENRESTY_HOME=${IROOT}/openresty-1.7.7.1
+export LAPIS_OPENRESTY=${IROOT}/openresty-1.7.7.1
 export PATH=${OPENRESTY_HOME}/nginx/sbin:$PATH
 
 lapis server production &
