d339f303e9929d20633969925d4f7a0797429245
Brittany Mazza <bmazza@techempower.com>
2015-01-22 11:44:55 -0800

    Update max yesod-core to 1.4.6, Clear sandbox dependencies
    - yesod-core had pool-conduit 0.1.2.3, which was causing an error.
    - cabal sandbox installed dependencies will run instead of indicated version if they're already installed.

--- frameworks/Haskell/yesod/setup.sh
+++ frameworks/Haskell/yesod/setup.sh
@@ -5,6 +5,11 @@ export PATH=${HASKELL_HOME}/bin:$PATH
 cd bench
 
 ${CABAL_HOME}/bin/cabal update
+
+# Run cabal sandbox delete in order to clear old, 
+# previously installed dependencies. 
+${CABAL_HOME}/bin/cabal sandbox delete
+
 ${CABAL_HOME}/bin/cabal sandbox init
 ${CABAL_HOME}/bin/cabal --bindir=${TROOT}/bench/dist/build/bench install
 
