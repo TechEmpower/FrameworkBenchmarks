b9b689ae3d2cb9aa3e5226e7673733eea279d995
Brittany Mazza <bmazza@techempower.com>
2015-01-23 15:03:15 -0800

    Remove unused root path

--- frameworks/PHP/php-laravel/app/routes.php
+++ frameworks/PHP/php-laravel/app/routes.php
@@ -11,13 +11,6 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
-
-// Route::controller(Controller::detect());
-
 Route::get('/json', function()
 {
     return Response::json(array('message' => 'Hello, World!'));
@@ -41,78 +34,3 @@ Route::get('/db', function()
 });
 
 Route::get('/fortunes', 'BenchController@fortunes');
-
-// /*
-// |--------------------------------------------------------------------------
-// | Application 404 & 500 Error Handlers
-// |--------------------------------------------------------------------------
-// |
-// | To centralize and simplify 404 handling, Laravel uses an awesome event
-// | system to retrieve the response. Feel free to modify this function to
-// | your tastes and the needs of your application.
-// |
-// | Similarly, we use an event to handle the display of 500 level errors
-// | within the application. These errors are fired when there is an
-// | uncaught exception thrown in the application. The exception object
-// | that is captured during execution is then passed to the 500 listener.
-// |
-// */
-
-// Event::listen('404', function()
-// {
-// 	return Response::error('404');
-// });
-
-// Event::listen('500', function($exception)
-// {
-// 	return Response::error('500');
-// });
-
-// /*
-// |--------------------------------------------------------------------------
-// | Route Filters
-// |--------------------------------------------------------------------------
-// |
-// | Filters provide a convenient method for attaching functionality to your
-// | routes. The built-in before and after filters are called before and
-// | after every request to your application, and you may even create
-// | other filters that can be attached to individual routes.
-// |
-// | Let's walk through an example...
-// |
-// | First, define a filter:
-// |
-// |		Route::filter('filter', function()
-// |		{
-// |			return 'Filtered!';
-// |		});
-// |
-// | Next, attach the filter to a route:
-// |
-// |		Route::get('/', array('before' => 'filter', function()
-// |		{
-// |			return 'Hello World!';
-// |		}));
-// |
-// */
-
-// Route::filter('before', function()
-// {
-// 	// Do stuff before every request to your application...
-// });
-
-// Route::filter('after', function($response)
-// {
-// 	// Do stuff after every request to your application...
-// });
-
-// Route::filter('csrf', function()
-// {
-// 	if (Request::forged()) return Response::error('500');
-// });
-
-// Route::filter('auth', function()
-// {
-// 	if (Auth::guest()) return Redirect::to('login');
-// });
-
