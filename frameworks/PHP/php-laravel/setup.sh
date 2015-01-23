0eb1e04f179e745078c383d6bf0b707571e2e3ba
Brittany Mazza <bmazza@techempower.com>
2015-01-23 14:47:26 -0800

    Update PHP Laravel to 4.2 with warn/pass tests

--- frameworks/PHP/php-laravel/setup.sh
+++ frameworks/PHP/php-laravel/setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-sed -i 's|localhost|'"${DBHOST}"'|g' application/config/database.php
+export PHP_HOME=${IROOT}/php-5.5.17
+export PHP_FPM=${PHP_HOME}/sbin/php-fpm
+export NGINX_HOME=${IROOT}/nginx
+
+sed -i 's|localhost|'"${DBHOST}"'|g' app/config/database.php
 sed -i 's|root .*/FrameworkBenchmarks/php-laravel|root '"${TROOT}"'|g' deploy/nginx.conf 
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
-export PATH="$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+export PATH="${PHP_HOME}/bin:${PHP_HOME}/sbin:$PATH"
 
-$PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
-$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
+${PHP_FPM} --fpm-config ${FWROOT}/config/php-fpm.conf -g ${TROOT}/deploy/php-fpm.pid
+${NGINX_HOME}/sbin/nginx -c ${TROOT}/deploy/nginx.conf
