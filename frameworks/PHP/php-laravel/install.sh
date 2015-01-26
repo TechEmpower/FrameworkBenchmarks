0f99b1f4cfd4548919d31171c96c21bf8ec626f8
Brittany Mazza <bmazza@techempower.com>
2015-01-26 10:34:25 -0800

    Remove unused export

--- frameworks/PHP/php-laravel/install.sh
+++ frameworks/PHP/php-laravel/install.sh
@@ -3,8 +3,6 @@
 fw_depends php nginx composer
 
 export PHP_HOME=${IROOT}/php-5.5.17
-export PHP_FPM=${PHP_HOME}/bin/php-fpm
-export NGINX_HOME=${IROOT}/nginx
 
 ${PHP_HOME}/bin/php ${IROOT}/composer.phar install \
   --no-interaction --working-dir ${TROOT} \
