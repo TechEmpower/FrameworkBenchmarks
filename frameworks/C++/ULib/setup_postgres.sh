6f451d72386b851cba6ada1ba36af3ad36d28190
stefanocasazza <stefanocasazza@gmail.com>
2015-02-04 19:52:59 +0100

    avoid dependency on bash_profile.sh and doc fix

--- frameworks/C++/ULib/setup_postgres.sh
+++ frameworks/C++/ULib/setup_postgres.sh
@@ -5,7 +5,7 @@ export ORM_DRIVER="pgsql"
 export ORM_OPTION="host=${DBHOST} user=benchmarkdbuser password=benchmarkdbpass dbname=hello_world client_encoding=UTF8"
 
 # 1. Change ULib Server configuration
-sed -i "s|PREFORK_CHILD .*|PREFORK_CHILD ${MAX_THREADS}|g" $ULIB_ROOT/benchmark.cfg
+sed -i "s|PREFORK_CHILD .*|PREFORK_CHILD ${MAX_THREADS}|g" $IROOT/ULib/benchmark.cfg
 
 # 2. Start ULib Server (userver_tcp)
-$ULIB_ROOT/bin/userver_tcp -c $ULIB_ROOT/benchmark.cfg &
+$IROOT/ULib/bin/userver_tcp -c $IROOT/ULib/benchmark.cfg &
