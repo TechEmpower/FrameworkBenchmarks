a1096f09e07df14e54527256632b9dc6e25ef0ad
Joel Berger <joel.a.berger@gmail.com>
2015-02-23 08:24:06 -0600

    less logging during perl build phase

--- toolset/setup/linux/languages/perl.sh
+++ toolset/setup/linux/languages/perl.sh
@@ -5,7 +5,7 @@ RETCODE=$(fw_exists ${IROOT}/perl-5.18.installed)
 
 fw_get https://raw.github.com/tokuhirom/Perl-Build/master/perl-build -O perl-build.pl
 # compile with optimizations, n.b. this does not turn on debugging
-perl perl-build.pl -DDEBUGGING=-g 5.18.2 perl-5.18
+perl perl-build.pl -DDEBUGGING=-g 5.18.2 perl-5.18 2>&1 | tee $IROOT/perl-install.log | awk '{ if (NR%100 == 0) printf "."}'
 
 fw_get http://cpanmin.us -O cpanminus.pl
 perl-5.18/bin/perl cpanminus.pl --notest --no-man-page App::cpanminus
