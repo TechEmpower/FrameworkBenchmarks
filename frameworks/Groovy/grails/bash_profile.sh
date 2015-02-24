c6073b45051cfec7a7e141a6edb06ec1e2b2c7e7
Lari Hotari <lhotari@pivotal.io>
2015-02-24 14:11:00 -0500

    update to Grails 2.4.4

--- frameworks/Groovy/grails/bash_profile.sh
+++ frameworks/Groovy/grails/bash_profile.sh
@@ -1,4 +1,4 @@
 export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
 export RESIN_HOME=${IROOT}/resin-4.0.41
 
-export GRAILS_PATH=${IROOT}/grails-2.4.2/bin/
+export GRAILS_PATH=${IROOT}/grails-2.4.4/bin/
