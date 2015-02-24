cef0561373ac049cbc9198f369c459a88f972c26
Lari Hotari <lhotari@pivotal.io>
2015-02-24 16:29:55 -0500

    remove bash_profile.sh from Grails benchmark

--- frameworks/Groovy/grails/setup.sh
+++ frameworks/Groovy/grails/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-amd64
+export RESIN_HOME=${IROOT}/resin-4.0.41
+
+export GRAILS_PATH=${IROOT}/grails-2.4.4/bin/
 
 sed -i 's|jdbc:mysql://.*:3306|jdbc:mysql://'"${DBHOST}"':3306|g' hello/grails-app/conf/DataSource.groovy
 
