175f75de2a990f1b54b14e2c626b72b4f1af114e
Kevin Moore <kevmoo@google.com>
2015-02-24 17:23:51 -0800

    configure for different dart architectures

--- toolset/setup/linux/languages/dart.sh
+++ toolset/setup/linux/languages/dart.sh
@@ -3,5 +3,5 @@
 RETCODE=$(fw_exists dart-sdk)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-x64-release.zip
-fw_unzip dartsdk-linux-x64-release.zip
+fw_get http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/dartsdk-linux-$DART_ARCH-release.zip
+fw_unzip dartsdk-linux-$DART_ARCH-release.zip
