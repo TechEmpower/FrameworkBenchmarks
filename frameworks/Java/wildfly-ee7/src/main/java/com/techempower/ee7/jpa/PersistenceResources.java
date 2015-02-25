4fcf78893277269e54df9b59289d2624abfaae2b
Cody Lerum <cody.lerum@gmail.com>
2015-02-25 09:33:30 -0700

    update to wildfly 8.2

--- frameworks/Java/wildfly-ee7/src/main/java/com/techempower/ee7/jpa/PersistenceResources.java
+++ frameworks/Java/wildfly-ee7/src/main/java/com/techempower/ee7/jpa/PersistenceResources.java
@@ -1,27 +1,18 @@
 package com.techempower.ee7.jpa;
 
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.inject.Disposes;
+import javax.enterprise.context.Dependent;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.persistence.SynchronizationType;
+import javax.persistence.PersistenceContext;
 
 public class PersistenceResources {
 
-  @PersistenceUnit
-  private EntityManagerFactory entityManagerFactory;
+    @PersistenceContext
+    private EntityManager em;
 
-  @Produces
-  @RequestScoped
-  public EntityManager entityManager() {
-    return entityManagerFactory.createEntityManager(SynchronizationType.UNSYNCHRONIZED);
-  }
-
-  protected void closeEntityManager(@Disposes EntityManager entityManager) {
-    if (entityManager.isOpen()) {
-      entityManager.close();
+    @Produces
+    @Dependent
+    public EntityManager entityManager() {
+        return em;
     }
-  }
 }
