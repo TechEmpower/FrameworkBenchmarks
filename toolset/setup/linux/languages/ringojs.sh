6ee282314d008ceb7d40797097bf2c27466f8173
Simon Oberhammer <simon@nekapuzer.at>
2015-02-28 12:48:31 +0100

    force wget output filename because github sends odd name

--- toolset/setup/linux/languages/ringojs.sh
+++ toolset/setup/linux/languages/ringojs.sh
@@ -3,7 +3,7 @@
 RETCODE=$(fw_exists ${IROOT}/ringojs_0.11.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get https://github.com/ringo/ringojs/releases/download/v0.11.0/ringojs_0.11_all.deb
+fw_get https://github.com/ringo/ringojs/releases/download/v0.11.0/ringojs_0.11_all.deb -O ringojs_0.11_all.deb
 sudo apt-get install -y jsvc
 sudo dpkg -i ringojs_0.11_all.deb
 
