3b6ddac06df0cc3fa0a81ac087d64d4b6f034ea8
root <root@stefano.unirel.intranet>
2015-03-06 15:16:55 +0100

    fix tuning

--- frameworks/C++/ULib/setup.sh
+++ frameworks/C++/ULib/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export UMEMPOOL="176,100,0,37,1110,105,-17,-22,40"
+export UMEMPOOL="176,100,0,37,1160,155,-17,-22,40"
 
 # 1. Change ULib Server configuration
 sed -i "s|PREFORK_CHILD .*|PREFORK_CHILD ${MAX_THREADS}|g" ${IROOT}/ULib/benchmark.cfg
