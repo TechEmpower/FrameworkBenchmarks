9a01be200ec0052c428ecd53bbec02cd265c626f
jamming <juanjoaguililla@gmail.com>
2015-03-12 22:18:32 +0100

    fix(sabina): ignore tests
    Ignore tests when benchmark is building.

--- frameworks/Java/sabina/setup.sh
+++ frameworks/Java/sabina/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 export JAVA_HOME=/opt/java8
-mvn clean package -Ddb.host=${DBHOST}
+mvn clean package -DskipTests -Ddb.host=${DBHOST}
 ${JAVA_HOME}/bin/java -jar target/sabina-1.0.0.jar &
