2c08f2f47b23b912c56173b4265e48173ab44a8a
Keith Newman <knewman@techempower.com>
2015-03-16 12:15:30 -0700

    cleaned up cherrypy install

--- frameworks/Python/cherrypy/install.sh
+++ frameworks/Python/cherrypy/install.sh
@@ -6,13 +6,7 @@ export PIP_DOWNLOAD_CACHE=$IROOT/.pip_cache
 fw_depends python2 
 #python3 pypy
 
-wget https://pypi.python.org/packages/source/C/CherryPy/CherryPy-3.6.0.tar.gz -O cherrypy.tar.gz
-tar -zxvf cherrypy.tar.gz
-cd CherryPy-3.6.0
-sudo python setup.py install
-
-
-#$PY2_PIP install --install-option="--prefix=${PY2_ROOT}" -r $TROOT/requirements.txt
+$PY2_PIP install --install-option="--prefix=${PY2_ROOT}" -r $TROOT/requirements.txt
 
 #$PY3_PIP install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
 
