fe0f135227aea6a9cfa7f962adcb09e926f162ea
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2015-03-16 22:52:33 +0100

    First implementation with the silicon C++ framework.

---
+++ frameworks/C++/silicon/install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt-get install -yq libmicrohttpd-dev libboost-dev
+
+rm -fr silicon;
+git clone https://github.com/matt-42/silicon.git
+cd silicon;
+CXX=/usr/bin/g++-4.9 ./install.sh $IROOT
+
+cd $TROOT
+mkdir -p build
+cd build; cmake .. -DCMAKE_CXX_COMPILER=g++-4.9 && make silicon_app
---
+++ frameworks/C++/silicon/setup.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+$TROOT/build/silicon_app ${DBHOST} 8080 /tmp/silicon_pid_file &
