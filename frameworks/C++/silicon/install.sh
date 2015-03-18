7b242e41b8f6730fec09dae0b665fcfe29668754
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2015-03-18 10:43:18 +0100

    Add the missing microhttpd make install.

--- frameworks/C++/silicon/install.sh
+++ frameworks/C++/silicon/install.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+# Install silicon
 DIR=`pwd`
 rm -fr silicon;
 git clone https://github.com/matt-42/silicon.git
 cd silicon;
 CXX=/usr/bin/g++-4.9 ./install.sh $IROOT
 
+# Install microhttpd
 cd $DIR
 wget http://mirror.ibcp.fr/pub/gnu/libmicrohttpd/libmicrohttpd-0.9.39.tar.gz
 tar xvf libmicrohttpd-0.9.39.tar.gz
 cd libmicrohttpd-0.9.39
 ./configure --prefix=$IROOT
-
+make install
 
 cd $TROOT
 mkdir -p build
