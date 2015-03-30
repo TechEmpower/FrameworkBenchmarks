148b2b18f25bd4a6163236450e9cd8dc2a49a2e5
Brittany Mazza <bmazza@techempower.com>
2015-03-30 14:32:28 -0700

    Fail test verification if required response headers aren't present, Resolve #373

--- toolset/benchmark/test_types/plaintext_type.py
+++ toolset/benchmark/test_types/plaintext_type.py
@@ -7,13 +7,19 @@ class PlaintextTestType(FrameworkTestType):
 
   def verify(self, base_url):
     url = base_url + self.plaintext_url
-    body = self._curl(url)
+    response = self._curl(url)
+    body = self._curl_body(url)
 
     # Empty response
     if body is None:
       return [('fail','No response', url)]
     elif len(body) == 0:
       return [('fail','Empty Response', url)]
+
+    # Ensure required response headers are present
+    if any(v not in response for v in ('Server','Date','Content-Type: text/plain')) \
+       or all(v not in response for v in ('Content-Length','Transfer-Encoding')):
+      return [('fail','Required response header missing.',url)]
 
     # Case insensitive
     orig = body
