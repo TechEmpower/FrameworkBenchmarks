b1496bfc0feb1fcbf22ab6545a8e0010520e2845
ext.bwaite <ext.bwaite@riotgames.com>
2015-04-01 14:13:18 -0700

    Clean up install scripts

--- frameworks/Elixir/phoenix/install.sh
+++ frameworks/Elixir/phoenix/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-fw_depends erlang elixir
+fw_depends elixir
--- toolset/setup/linux/languages/elixir.sh
+++ toolset/setup/linux/languages/elixir.sh
@@ -3,11 +3,7 @@
 RETCODE=$(fw_exists ${IROOT}/elixir.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-sudo dpkg -i erlang-solutions_1.0_all.deb
-
-sudo apt-get -y update
-sudo apt-get install -y esl-erlang
+fw_depends erlang
 sudo apt-get install -y elixir
 
 touch ${IROOT}/elixir.installed
