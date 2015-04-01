3fee6fecf42f8ef62f048caab2bba6be964c0928
Steve Smith <ssmith@techempower.com>
2015-04-01 09:22:14 -0700

    Fixed PHP install and silex-raw #720

--- frameworks/PHP/php-silex/setup.sh
+++ frameworks/PHP/php-silex/setup.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-sed -i 's|192.168.100.102|'"${DBHOST}"'|g' web/index.php
+sed -i 's|localhost|'"${DBHOST}"'|g' web/index.php
 sed -i 's|".*\FrameworkBenchmarks/php-silex|"'"${TROOT}"'|g' deploy/php-silex
 sed -i 's|Directory .*/FrameworkBenchmarks/php-silex|Directory '"${TROOT}"'|g' deploy/php-silex
 sed -i 's|root .*/FrameworkBenchmarks/php-silex|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
+PHP_HOME=${IROOT}/php-5.5.17
+
 export PATH="$COMPOSER_HOME:$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
 
-composer.phar install --optimize-autoloader
+${PHP_HOME}/bin/php ${IROOT}/composer.phar install --optimize-autoloader
 $PHP_FPM --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
 $NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
