14e606209312711fd39dbae1acf3e0bce589c8ee
Zane Kansil <zkansil@techempower.com>
2015-04-02 11:33:14 -0700

    json tests passing

--- frameworks/Go/goji/src/goji/server.go
+++ frameworks/Go/goji/src/goji/server.go
@@ -3,18 +3,31 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 	"flag"
 
 	"github.com/zenazn/goji"
 	"github.com/zenazn/goji/web"
 )
 
+type Message struct {
+	Message string `json:"message"`
+}
+
+// Test 1: Json Serialization
+func serializeJson(c web.C, w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(&Message{"Hello, World!"})
+}
+
+// Test 6: Plaintext
 func plaintext(c web.C, w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!")
 }
 
 func main() {
 	flag.Set("bind", ":8080")
+	goji.Get("/json", serializeJson)
 	goji.Get("/plaintext", plaintext)
 	goji.Serve()
 }
