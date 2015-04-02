4c5abab1e23e00856ea666cc78fe16773aeaa373
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2015-04-02 09:36:56 +0200

    Fix install.sh to compile all the tests.

--- frameworks/C++/silicon/install.sh
+++ frameworks/C++/silicon/install.sh
@@ -9,12 +9,13 @@ CXX=/usr/bin/g++-4.9 ./install.sh $IROOT
 
 # Install microhttpd
 cd $DIR
-wget http://mirror.ibcp.fr/pub/gnu/libmicrohttpd/libmicrohttpd-0.9.39.tar.gz
-tar xvf libmicrohttpd-0.9.39.tar.gz
+fw_get http://mirror.ibcp.fr/pub/gnu/libmicrohttpd/libmicrohttpd-0.9.39.tar.gz
+fw_untar libmicrohttpd-0.9.39.tar.gz
 cd libmicrohttpd-0.9.39
 ./configure --prefix=$IROOT
 make install
 
 cd $TROOT
 mkdir -p build
-cd build; cmake .. -DCMAKE_CXX_COMPILER=g++-4.9 && make silicon_app
+cd build; cmake .. -DCMAKE_CXX_COMPILER=g++-4.9 && make
+
