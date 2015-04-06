a5625e6330ca261ab85702f4ca4fedcf68bf9ab2
Stuart Douglas <stuart.w.douglas@gmail.com>
2015-04-06 14:03:26 +1000

    update setup.sh

--- frameworks/Java/wildfly-ee7/setup.sh
+++ frameworks/Java/wildfly-ee7/setup.sh
@@ -3,4 +3,4 @@
 export JAVA_OPTS="-Xms2g -Xmx2g -XX:MaxPermSize=256m -XX:+UseG1GC -XX:MaxGCPauseMillis=25 -verbosegc -Xloggc:/tmp/wildfly_gc.log"
 
 mvn clean initialize package -Pbenchmark -Ddatabase.host=${DBHOST}
-target/wildfly-8.2.0.Final/bin/standalone.sh -b 0.0.0.0 &
+target/wildfly-9.0.0.Beta2/bin/standalone.sh -b 0.0.0.0 &
