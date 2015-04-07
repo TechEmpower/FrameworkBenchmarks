19e20aa96c104d37a2863b99ebba139647366290
Keith Newman <knewman@techempower.com>
2015-04-07 12:55:58 -0700

    cleaning up web2py

--- frameworks/Python/web2py/setup.sh
+++ frameworks/Python/web2py/setup.sh
@@ -1,4 +1,4 @@
 export PY2_ROOT=$IROOT/py2
 export PY2=$PY2_ROOT/bin/python
 
-sudo chmod a+w web2py/httpserver.pid & $PY2 web2py/web2py.py -a '' -i 127.0.0.1 -p 8080  &
+$PY2 web2py/web2py.py -a '' -i 127.0.0.1 -p 8080  &
