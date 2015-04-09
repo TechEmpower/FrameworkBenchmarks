2137f33ee0e7d8dc86bddcfc87f62e5f8e2f6f66
Edward Bramanti <ebramanti@techempower.com>
2015-04-09 16:07:25 -0700

    Install nvm versioning to frameworks

--- toolset/setup/linux/languages/nodejs.sh
+++ toolset/setup/linux/languages/nodejs.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
 
-RETCODE=$(fw_exists ${IROOT}/nvm-0.24.1.installed)
+RETCODE=$(fw_exists ${IROOT}/nvm.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-# Install nvm (node version manager) v0.24.1
-fw_get https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh | \ 
-	NVM_DIR=${IROOT}/nvm bash
+fw_get https://raw.githubusercontent.com/creationix/nvm/v0.24.1/install.sh
+NVM_DIR=$IROOT/nvm bash install.sh
 
-export NVM_HOME=${IROOT}/nvm
-export PATH=$PATH:$NVM_HOME/nvm.sh
-
-touch ${IROOT}/nvm-0.24.1.installed
+touch ${IROOT}/nvm.installed
