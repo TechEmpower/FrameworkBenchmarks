c3f26d69f1d2089c004125097cff5eb22b84574d
ext.bwaite <ext.bwaite@riotgames.com>
2015-04-13 08:45:16 -0700

    Remove ebin on build

--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -5,7 +5,7 @@ source $IROOT/rebar.installed
 
 sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/hello_world_app.erl
 
-rm -rf deps/*
+rm -rf deps/* ebin/*
 $REBAR_HOME/rebar get-deps
 $REBAR_HOME/rebar compile
 erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
