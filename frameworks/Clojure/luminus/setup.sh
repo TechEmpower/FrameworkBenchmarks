d8c57a4fafebdb4cc99e31cab0e7edc8a085a330
Zane Kansil <zkansil@techempower.com>
2015-04-16 16:52:08 -0700

    Update Luminus start up process

--- frameworks/Clojure/luminus/setup.sh
+++ frameworks/Clojure/luminus/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 source $IROOT/java7.installed
+
+source $IROOT/lein.installed
+
 export RESIN_HOME=${IROOT}/resin-4.0.41
 
 # Path vars must be set here
@@ -9,8 +12,8 @@ export PATH="$JAVA_HOME/bin:$PATH"
 sed -i 's|:subname "//.*:3306|:subname "//'"${DBHOST}"':3306|g' hello/src/hello/models/schema.clj
 
 cd hello
-$IROOT/lein/bin/lein clean
-$IROOT/lein/bin/lein ring uberwar
+lein clean
+lein ring uberwar
 rm -rf $RESIN_HOME/webapps/*
 cp target/hello-luminus-standalone.war $RESIN_HOME/webapps/luminus.war
 $RESIN_HOME/bin/resinctl start
