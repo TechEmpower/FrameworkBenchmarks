52441d086c06c26adcf5af438098349cf672d2b1
Edward Bramanti <ebramanti@techempower.com>
2015-04-16 14:55:03 -0700

    Removed sleep, comments, bash profile.

--- frameworks/JavaScript/express/setup.sh
+++ frameworks/JavaScript/express/setup.sh
@@ -13,12 +13,3 @@ nvm use 0.10.8
 # run app
 npm install
 node app &
-
-# !DO NOT REMOVE!
-#
-# It takes `node app` a few seconds to turn on and 
-# then fork. If you remove this sleep, the parent shell 
-# executing this script will be terminated before the 
-# application has time to awaken and be forked, and 
-# express will fail to be started
-sleep 5
--- frameworks/JavaScript/koa/setup.sh
+++ frameworks/JavaScript/koa/setup.sh
@@ -2,10 +2,6 @@
 
 sed -i 's|mongodb//.*/hello_world|mongodb//'"${DBHOST}"'/hello_world|g' app.js
 
-# export NODE_ENV=production
-# export PATH=$PATH:$NODE_HOME/bin
-
-
 export NVM_HOME=${IROOT}/nvm
 # Used to avoid nvm's return 2 error.
 # Sourcing this functions if 0 is returned.
@@ -13,22 +9,6 @@ source $NVM_HOME/nvm.sh || 0
 nvm install 0.11.16
 nvm use 0.11.16
 
-
 # run app
 npm install
 node --harmony app &
-
-# export NODE_HOME=${IROOT}/node-v0.12.0-linux-x64
-# export PATH=$PATH:$NODE_HOME/bin
-
-# ${NODE_HOME}/bin/npm install
-# ${NODE_HOME}/bin/node --harmony app.js &
-
-# !DO NOT REMOVE!
-#
-# It takes `node app` a few seconds to turn on and 
-# then fork. If you remove this sleep, the parent shell 
-# executing this script will be terminated before the 
-# application has time to awaken and be forked, and 
-# express will fail to be started
-sleep 5
