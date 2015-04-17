7334a922189e75095aa4ca508b36382f54e7d8a5
Edward Bramanti <ebramanti@techempower.com>
2015-04-17 14:23:14 -0700

    Fixed localhost issue (again)

--- frameworks/JavaScript/nodejs/hello.js
+++ frameworks/JavaScript/nodejs/hello.js
@@ -29,12 +29,12 @@ if (!windows) {
     
     Mapper.connect(connMap, {verbose: false, strict: false});
     var World = Mapper.map("World", "id", "randomNumber")
-    libmysql.connectSync('127.0.0.1', 'benchmarkdbuser', 'benchmarkdbpass', 'hello_world');
+    libmysql.connectSync('localhost', 'benchmarkdbuser', 'benchmarkdbpass', 'hello_world');
 }
 
 var collection = null;
 
-MongoClient.connect('mongodb://127.0.0.1/hello_world?maxPoolSize=5', function(err, db) {
+MongoClient.connect('mongodb://localhost/hello_world?maxPoolSize=5', function(err, db) {
   collection = db.collection('world');
 });
 
