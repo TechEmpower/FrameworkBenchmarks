13ecd991f451ddcb991e0cd95f8f09f8630faee2
Robert Gaggl <robert@nomatic.org>
2015-04-19 14:33:57 +0200

    fixed setup of ringojs-convenient and upgraded to ringojs 0.11

--- frameworks/JavaScript/ringojs-convenient/setup.sh
+++ frameworks/JavaScript/ringojs-convenient/setup.sh
@@ -5,9 +5,11 @@ sed -i 's|dbHost = \x27.*\x27;|dbHost = \x27'"${DBHOST}"'\x27|g' app/models.js
 fw_depends java ringojs
 
 rm -rf $RINGOJS_HOME/packages/*
-ringo-admin install oberhamsi/sql-ringojs-client
-ringo-admin install grob/ringo-sqlstore
-ringo-admin install ringo/stick
-ringo-admin install oberhamsi/reinhardt
+$RINGOJS_HOME/bin/ringo-admin install oberhamsi/sql-ringojs-client
+$RINGOJS_HOME/bin/ringo-admin install grob/ringo-sqlstore
+$RINGOJS_HOME/bin/ringo-admin install ringo/stick
+$RINGOJS_HOME/bin/ringo-admin install orfon/reinhardt
+
+cp $RINGOJS_HOME/packages/sql-ringojs-client/jars/mysql.jar $RINGOJS_HOME/packages/ringo-sqlstore/jars/
 
 ringo --production -J-server -J-Xmx1g -J-Xms1g ringo-main.js &
--- toolset/setup/linux/languages/ringojs.sh
+++ toolset/setup/linux/languages/ringojs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VERSION="0.10"
+VERSION="0.11"
 RINGOJS=$IROOT/ringojs_$VERSION
 RINGOJS_HOME=$IROOT/ringojs-$VERSION
 RETCODE=$(fw_exists ${RINGOJS}.installed)
@@ -8,8 +8,8 @@ RETCODE=$(fw_exists ${RINGOJS}.installed)
   source $RINGOJS.installed
   return 0; }
 
-fw_get http://ringojs.org/downloads/ringojs-0.10.tar.gz
-fw_untar ringojs-0.10.tar.gz
+fw_get http://ringojs.org/downloads/ringojs-0.11.tar.gz
+fw_untar ringojs-0.11.tar.gz
 
 echo "export RINGOJS_HOME=${RINGOJS_HOME}" > $RINGOJS.installed
 echo -e "export PATH=${RINGOJS_HOME}/bin:\$PATH" >> $RINGOJS.installed
