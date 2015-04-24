053147c19acbf467bb0e044f2fb58304b759b72d
Chris McDonough <chrism@plope.com>
2015-04-24 07:18:12 -0400

    Fix the path to create-postgres.sql

--- frameworks/Python/pyramid/create_database.py
+++ frameworks/Python/pyramid/create_database.py
@@ -5,7 +5,9 @@ if __name__ == "__main__":
     """
     Initialize database
     """
-    with codecs.open('../config/create-postgres.sql', 'r', encoding='utf-8') as fp:
+    with codecs.open('../../../config/create-postgres.sql',
+                     'r',
+                     encoding='utf-8') as fp:
         sql = fp.read()
     DBSession.execute(sql)
     DBSession.commit()
