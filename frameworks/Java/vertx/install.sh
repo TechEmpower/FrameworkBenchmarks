3d73ccafc8c4519cb3de6cd3176a8037879531fc
victor <github@briz.ws>
2015-04-23 18:48:29 -0700

    Moving install from toolset into vert.x install.sh

--- frameworks/Java/vertx/install.sh
+++ frameworks/Java/vertx/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-fw_depends java7 vertx 
+fw_depends java7
+
+RETCODE=$(fw_exists ${IROOT}/vert.x-2.1.5.installed)
+[ ! "$RETCODE" == 0 ] || { return 0; }
+
+fw_get http://dl.bintray.com/vertx/downloads/vert.x-2.1.5.tar.gz?direct=true -O vert.x-2.1.5.tar.gz
+fw_untar vert.x-2.1.5.tar.gz
+
+touch ${IROOT}/vert.x-2.1.5.installed
