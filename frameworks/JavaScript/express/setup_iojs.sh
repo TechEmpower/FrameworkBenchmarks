be20badeaf0496080ac821eeeff8c4bd0a113f64
Edward Bramanti <ebramanti@techempower.com>
2015-04-24 15:29:21 -0700

    Add iojs support to Express

---
+++ frameworks/JavaScript/express/setup_iojs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sed -i 's|mongodb://.*/hello_world|mongodb://'"${DBHOST}"'/hello_world|g' app.js
+sed -i 's|localhost|'"${DBHOST}"'|g' app.js
+
+export NODE_ENV=production
+export NVM_HOME=${IROOT}/nvm
+# Used to avoid nvm's return 2 error.
+# Sourcing this functions if 0 is returned.
+source $NVM_HOME/nvm.sh || 0
+nvm install iojs-v1.8.1
+nvm use iojs-v1.8.1
+
+# update npm before app init
+npm install -g npm
+
+# run app
+npm install
+iojs app &
