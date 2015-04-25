c9b3c41385efb3ed76946b4ac0bab72b3ccdf170
Hamilton Turner <hamiltont@gmail.com>
2015-04-25 03:32:06 -0400

    Make timeout match TE-msmith branch

--- toolset/benchmark/framework_test.py
+++ toolset/benchmark/framework_test.py
@@ -256,7 +256,7 @@ class FrameworkTest:
       "%s: %s.sh and framework processes have terminated" % (self.name, self.setup_file))
 
     # Set a limit on total execution time of setup.sh
-    timeout = datetime.now() + timedelta(minutes = 20)
+    timeout = datetime.now() + timedelta(minutes = 105)
     time_remaining = timeout - datetime.now()
 
     # Need to print to stdout once every 10 minutes or Travis-CI will abort
