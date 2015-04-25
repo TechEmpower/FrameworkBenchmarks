47c9099440a57a1879e546af83dc2ad5bd4defc8
mfirry <mfirry@gmail.com>
2015-04-25 13:02:53 +0200

    setup.sh

--- frameworks/Scala/http4s/setup.sh
+++ frameworks/Scala/http4s/setup.sh
@@ -3,4 +3,7 @@
 source $IROOT/java8.installed
 export SBT_HOME=${IROOT}/sbt
 
-${SBT_HOME}/bin/sbt update compile
+#${SBT_HOME}/bin/sbt compile
+${SBT_HOME}/bin/sbt 'oneJar'
+
+java -jar target/scala-2.11/http4s*one-jar.jar
