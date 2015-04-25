b8d8526e977e55311d1d983f6d78fde2f32e39c0
mfirry <mfirry@gmail.com>
2015-04-25 15:03:16 +0200

    plaintext added + headers

--- frameworks/Scala/http4s/README.md
+++ frameworks/Scala/http4s/README.md
@@ -9,11 +9,16 @@ The tests were run with:
 
 * [Java Oracle 1.8.0_25](http://www.oracle.com/technetwork/java/javase)
 * [http4s 0.6.2](http://http4s.org/)
+* [blaze 0.6.2](https://github.com/http4s/blaze/)
 
 ## Test URLs
 ### JSON Encoding Test
 
 http://localhost:8080/json
+
+### Plaintext Test
+
+http://localhost:8080/plaintext
 
 ## How to run
 sbt 'oneJar'
--- frameworks/Scala/http4s/src/main/scala/WebServer.scala
+++ frameworks/Scala/http4s/src/main/scala/WebServer.scala
@@ -3,6 +3,7 @@ import org.http4s.server._
 import org.http4s.dsl._
 import org.http4s.argonaut._
 import org.http4s.server.blaze.BlazeBuilder
+import headers._
 
 import _root_.argonaut._, Argonaut._
 
@@ -10,7 +11,15 @@ object WebServer extends App {
 
   val service = HttpService {
     case GET -> Root / "json" =>
+      val dateHeader = Date(DateTime(4))
       Ok(Json("message" -> jString("Hello, World!")).asJson)
+        .withHeaders(dateHeader)
+        .withContentType(Some(`Content-Type`(MediaType.`application/json`, Charset.`UTF-8`)))
+    case GET -> Root / "plaintext" =>
+      val dateHeader = Date(DateTime(4))
+      Ok("Hello, World!")
+        .withHeaders(dateHeader)
+        .withContentType(Some(`Content-Type`(MediaType.`text/plain`, Charset.`UTF-8`)))
   }
 
   BlazeBuilder.bindHttp(8080)
