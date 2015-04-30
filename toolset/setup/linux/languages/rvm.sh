1bcff43e6ac57b9d847b5aa3eb7178ea159ef86a
msmith-techempower <msmith@techempower.com>
2015-04-30 09:49:47 -0700

    Fixed a bug with rvm on travis

--- toolset/setup/linux/languages/rvm.sh
+++ toolset/setup/linux/languages/rvm.sh
@@ -9,17 +9,16 @@ RETCODE=$(fw_exists rvm.installed)
 
 # Never run installation inside of TRAVIS, 
 # just rely on their default RVM installaton
-[ "$TRAVIS" != "true" ] || { \
-  echo "source /home/travis/.rvm/scripts/rvm" > rvm.installed
-  source $IROOT/rvm.installed
-  return 0;
-}
+if [ "$TRAVIS" = "true" ]
+then
+  echo "source /home/travis/.rvm/scripts/rvm" > $IROOT/rvm.installed
+else
+  # Run single-user installation and source rvm
+  export SHELL=/bin/bash
+  gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
+  \curl -sSL https://get.rvm.io | bash -s stable
 
-# Run single-user installation and source rvm
-export SHELL=/bin/bash
-gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable
-
-echo "source ~/.rvm/scripts/rvm" > $IROOT/rvm.installed
+  echo "source ~/.rvm/scripts/rvm" > $IROOT/rvm.installed
+fi
 
 source $IROOT/rvm.installed
