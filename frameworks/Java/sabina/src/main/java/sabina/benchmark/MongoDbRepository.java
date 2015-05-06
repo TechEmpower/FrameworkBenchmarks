44ac73f34b3a4ec198242653b013a025bf15773a
jamming <juanjoaguililla@gmail.com>
2015-05-06 18:50:45 +0200

    Apply updates from code review comments

--- frameworks/Java/sabina/src/main/java/sabina/benchmark/MongoDbRepository.java
+++ frameworks/Java/sabina/src/main/java/sabina/benchmark/MongoDbRepository.java
@@ -27,21 +27,6 @@ import com.mongodb.client.MongoDatabase;
 import org.bson.Document;
 
 final class MongoDbRepository implements Repository {
-    private static final String [] FORTUNES = {
-        "fortune: No such file or directory",
-        "A computer scientist is someone who fixes things that aren't broken.",
-        "After enough decimal places, nobody gives a damn.",
-        "A bad random number generator: 1, 1, 1, 1, 1, 4.33e+67, 1, 1, 1",
-        "A computer program does what you tell it to do, not what you want it to do.",
-        "Emacs is a nice operating system, but I prefer UNIX. — Tom Christaensen",
-        "Any program that runs right is obsolete.",
-        "A list is only as strong as its weakest link. — Donald Knuth",
-        "Feature: A bug with seniority.",
-        "Computers make very fast, very accurate mistakes.",
-        "<script>alert(\"This should not be displayed in a browser alert box.\");</script>",
-        "フレームワークのベンチマーク"
-    };
-
     private MongoCollection<Document> worldCollection;
     private MongoCollection<Document> fortuneCollection;
 
