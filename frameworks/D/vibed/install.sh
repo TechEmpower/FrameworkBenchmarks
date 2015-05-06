5d8399d945706dbcc96cf12e09ff94c3431c6802
jin <nin-jin@ya.ru>
2015-05-06 09:55:29 +0300

    no sudo added to travis

--- frameworks/D/vibed/install.sh
+++ frameworks/D/vibed/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
-sudo apt-get update && sudo apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring && sudo apt-get update
-sudo apt-get install -qq -y dub
+wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
+apt-get update && apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring && apt-get update
+apt-get install -qq -y dub
 dub build
