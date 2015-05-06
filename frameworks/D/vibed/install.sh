7cbf413ca88b58b2d5b5a3104fff0a6308f3017e
jin <nin-jin@ya.ru>
2015-05-06 10:42:17 +0300

    added dependencies: libevent libssl dmd

--- frameworks/D/vibed/install.sh
+++ frameworks/D/vibed/install.sh
@@ -3,4 +3,8 @@
 sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
 sudo apt-get update && sudo apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring && sudo apt-get update
 sudo apt-get install -qq -y dub
+sudo apt-get install -qq -y libevent-dev libssl-dev
+sudo apt-get install -qq -y g++ gcc-multilib xdg-utils
+wget "http://downloads.dlang.org/releases/2014/dmd_2.066.1-0_amd64.deb"
+sudo dpkg -i dmd_2.066.1-0_amd64.deb
 dub build
