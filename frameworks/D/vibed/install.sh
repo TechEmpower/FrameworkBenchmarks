d624dc578e0a95e9d85e5e3aa5b262e90c803cf1
jin <nin-jin@ya.ru>
2015-05-07 16:28:53 +0300

    may be fix path

--- frameworks/D/vibed/install.sh
+++ frameworks/D/vibed/install.sh
@@ -7,5 +7,5 @@ sudo apt-get install -qq -y libevent-dev libssl-dev
 sudo apt-get install -qq -y g++ gcc-multilib xdg-utils
 wget "http://downloads.dlang.org/releases/2014/dmd_2.066.1-0_amd64.deb"
 sudo dpkg -i dmd_2.066.1-0_amd64.deb
-cd $TESTDIR
+cd $FWROOT/frameworks/D/vibed
 dub build --build-mode=singleFile
