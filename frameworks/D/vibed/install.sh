6c240b0f2e396291521dc1772c3ee2eb0557f295
jin <nin-jin@ya.ru>
2015-05-08 10:33:52 +0300

    try to use dmd-bin

--- frameworks/D/vibed/install.sh
+++ frameworks/D/vibed/install.sh
@@ -2,10 +2,8 @@
 
 sudo wget http://master.dl.sourceforge.net/project/d-apt/files/d-apt.list -O /etc/apt/sources.list.d/d-apt.list
 sudo apt-get update && sudo apt-get -y --allow-unauthenticated install --reinstall d-apt-keyring && sudo apt-get update
-sudo apt-get install -qq -y dub
+sudo apt-get install -qq -y dub dmd-bin
 sudo apt-get install -qq -y libevent-dev libssl-dev
 sudo apt-get install -qq -y g++ gcc-multilib xdg-utils
-wget "http://downloads.dlang.org/releases/2015/dmd_2.067.1-0_amd64.deb"
-sudo dpkg -i dmd_2.067.1-0_amd64.deb
 cd $FWROOT/frameworks/D/vibed
 dub build --build-mode=singleFile
