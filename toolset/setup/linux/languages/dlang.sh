98164a699b521db46f28b12c06b37212581643a4
msmith-techempower <msmith@techempower.com>
2015-05-14 10:51:54 -0700

    Trying to fix vibed

--- toolset/setup/linux/languages/dlang.sh
+++ toolset/setup/linux/languages/dlang.sh
@@ -6,8 +6,9 @@ RETCODE=$(fw_exists ${DLANG}.installed)
   source $DLANG.installed
   return 0; }
 
+mkdir $DLANG
 fw_get http://downloads.dlang.org/releases/2.x/2.067.1/dmd_2.067.1-0_amd64.deb
-dpkg -i dmd_2.067.1-0_amd64.deb --instdir=$DLANG
+dpkg-deb dmd_2.067.1-0_amd64.deb $DLANG
 
 echo -e "export PATH=${DLANG}:\$PATH" > $DLANG.installed
 
--- toolset/setup/linux/prerequisites.sh
+++ toolset/setup/linux/prerequisites.sh
@@ -45,7 +45,8 @@ sudo apt-get -qqy install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options:
   libsasl2-dev                      `# Needed by mgo for go-mongodb test` \
   llvm-dev                          `# Required for correct Ruby installation` \
   libboost-dev                      `# Silicon relies on boost::lexical_cast.` \
-  postgresql-server-dev-9.3         `# Needed by cpoll.`
+  postgresql-server-dev-9.3         `# Needed by cpoll.`\
+  xdg-utils                         `# Needed by dlang.`
 
 # Install gcc-4.8 and gcc-4.9
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
