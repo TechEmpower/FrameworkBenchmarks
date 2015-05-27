7ddf4c9b00803a4d0e251ed505aebae0fc1ffa9a
sagenschneider <dsagenschneider@devisd.com>
2015-05-27 23:00:59 +0800

    Providing abstract class for generic testing

--- frameworks/Java/officefloor/src/test/java/net/officefloor/performance/PlainTextTest.java
+++ frameworks/Java/officefloor/src/test/java/net/officefloor/performance/PlainTextTest.java
@@ -1,146 +1,16 @@
 package net.officefloor.performance;
 
-import net.officefloor.plugin.woof.WoofOfficeFloorSource;
-
 import org.apache.http.Header;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.util.EntityUtils;
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Runs application and ensures a page is available.
  */
-public class PlainTextTest {
-
-	@Before
-	public void runApplication() throws Exception {
-		// Start the application
-		WoofOfficeFloorSource.start();
-	}
-
-	@After
-	public void stopApplication() throws Exception {
-		// Stop the application
-		WoofOfficeFloorSource.stop();
-	}
-
-	@Test
-	public void ensurePlainText() throws Exception {
-		try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-			this.doRequestTest(client);
-		}
-	}
-
-	@Test
-	public void performancePlainText() throws Exception {
-
-		final int threadCount = 100;
-		final int iterationCount = 10000;
-
-		// Warm up the server
-		System.out.print("Warming up server ");
-		System.out.flush();
-		try (CloseableHttpClient client = HttpClientBuilder.create().build()) {
-			for (int i = 0; i < 20000; i++) {
-				this.doRequestTest(client);
-				if (i % 1000 == 0) {
-					System.out.print(".");
-					System.out.flush();
-				}
-			}
-		}
-		System.out.println(" warmed up");
-
-		// Create the threads
-		Thread[] threads = new Thread[threadCount];
-		final boolean[] threadComplete = new boolean[threads.length];
-		final Throwable[] failures = new Throwable[threads.length];
-		for (int t = 0; t < threads.length; t++) {
-			final int threadIndex = t;
-			threadComplete[threadIndex] = false; // Not yet complete
-			failures[threadIndex] = null; // no initial failure
-			threads[threadIndex] = new Thread() {
-				@Override
-				public void run() {
-					try {
-						try (CloseableHttpClient client = HttpClientBuilder
-								.create().build()) {
-
-							// Under the request so many times
-							for (int i = 0; i < iterationCount; i++) {
-								PlainTextTest.this
-										.doRequestTest(client);
-							}
-						}
-					} catch (Throwable ex) {
-						// Record failure
-						synchronized (failures) {
-							failures[threadIndex] = ex;
-						}
-					} finally {
-						// Notify complete
-						synchronized (threadComplete) {
-							threadComplete[threadIndex] = true;
-							threadComplete.notify();
-						}
-					}
-				}
-			};
-		}
-
-		// Capture start time
-		long startTime = System.currentTimeMillis();
-
-		// Kick off the threads
-		System.out.print("Running requests ...");
-		System.out.flush();
-		for (int t = 0; t < threads.length; t++) {
-			threads[t].start();
-		}
-
-		// Wait for completion
-		synchronized (threadComplete) {
-
-			// Determine if complete
-			boolean isComplete = false;
-			do {
-				// Determine if all threads a complete
-				isComplete = true;
-				for (int t = 0; t < threads.length; t++) {
-					if (!threadComplete[t]) {
-						isComplete = false;
-					}
-				}
-
-				// Wait some time if not complete
-				if (!isComplete) {
-					threadComplete.wait(1000);
-				}
-
-			} while (!isComplete);
-		}
-
-		// Capture the end time
-		long endTime = System.currentTimeMillis();
-		System.out.println(" requests completed");
-
-		// Determine statistics
-		long duration = endTime - startTime;
-		long totalRequestCount = threadCount * iterationCount;
-		System.out.println();
-		System.out.println("Performance results:");
-		System.out.println(totalRequestCount + " requests (" + threadCount
-				+ " threads by " + iterationCount + " requests) completed in "
-				+ duration + " milliseconds");
-		System.out.println("Effectively 1 request every "
-				+ (duration / (totalRequestCount * 1.0)) + " milliseconds");
-	}
+public class PlainTextTest extends AbstractTestCase {
 
 	/**
 	 * Undertakes a single test.
@@ -150,7 +20,8 @@ public class PlainTextTest {
 	 * @throws Exception
 	 *             If request fails.
 	 */
-	private void doRequestTest(CloseableHttpClient client) throws Exception {
+	@Override
+	protected void doRequestTest(CloseableHttpClient client) throws Exception {
 
 		// Request the plain text
 		HttpResponse response = client.execute(new HttpGet(
