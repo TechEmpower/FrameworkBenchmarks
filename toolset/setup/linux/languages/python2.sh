b24e9afc15dada5156601db60960546380ed7844
INADA Naoki <songofacandy@gmail.com>
2015-05-28 00:22:52 +0900

    Python 2.7.10 and 3.4.3

--- toolset/setup/linux/languages/python2.sh
+++ toolset/setup/linux/languages/python2.sh
@@ -3,9 +3,9 @@
 RETCODE=$(fw_exists ${IROOT}/py2.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://www.python.org/ftp/python/2.7.9/Python-2.7.9.tgz
-fw_untar Python-2.7.9.tgz
-cd Python-2.7.9
+fw_get http://www.python.org/ftp/python/2.7.10/Python-2.7.10.tgz
+fw_untar Python-2.7.10.tgz
+cd Python-2.7.10
 ./configure --prefix=${IROOT}/py2 --disable-shared --quiet
 make -j4 --quiet 2>&1 | tee $IROOT/python-install.log | awk '{ if (NR%100 == 0) printf "."}'
 make install --quiet 2>&1 | tee -a $IROOT/python-install.log | awk '{ if (NR%100 == 0) printf "."}'
--- toolset/setup/linux/languages/python3.sh
+++ toolset/setup/linux/languages/python3.sh
@@ -3,9 +3,9 @@
 RETCODE=$(fw_exists ${IROOT}/py3.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://www.python.org/ftp/python/3.4.2/Python-3.4.2.tar.xz
-fw_untar Python-3.4.2.tar.xz
-cd Python-3.4.2
+fw_get http://www.python.org/ftp/python/3.4.3/Python-3.4.3.tar.xz
+fw_untar Python-3.4.3.tar.xz
+cd Python-3.4.3
 ./configure --prefix=${IROOT}/py3 --disable-shared --with-computed-gotos --quiet
 make -j4 --quiet 2>&1 | tee $IROOT/python3-install.log | awk '{ if (NR%100 == 0) printf "."}'
 make install --quiet 2>&1 | tee -a $IROOT/python3-install.log | awk '{ if (NR%100 == 0) printf "."}'
