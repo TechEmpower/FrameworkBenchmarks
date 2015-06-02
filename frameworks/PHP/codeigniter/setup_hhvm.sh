0544641d55dd4894dbbdde725807e1283bc387ba
Mattie Coccia <mcocciaTE@techempower.com>
2015-06-02 13:36:46 -0700

    Added hhvm version of tests

---
+++ frameworks/PHP/codeigniter/setup_hhvm.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export PHP_HOME=${IROOT}/php-5.5.17
+
+export PHP_FPM=$PHP_HOME/sbin/php-fpm
+
+export NGINX_HOME=${IROOT}/nginx
+
+
+sed -i 's|localhost|'"${DBHOST}"'|g' application/config/database.php
+sed -i 's|SourceRoot = .*/FrameworkBenchmarks/codeigniter|SourceRoot = '"${TROOT}"'|g' deploy/config.hdf
+sed -i 's|Path = .*/.hhvm.hhbc|Path = '"${TROOT}"'/.hhvm.bbhc|g' deploy/config.hdf
+sed -i 's|PidFile = .*/hhvm.pid|PidFile = '"${TROOT}"'/hhvm.pid|g' deploy/config.hdf
+sed -i 's|File = .*/error.log|File = '"${TROOT}"'/error.log|g' deploy/config.hdf
+sed -i 's|root .*/FrameworkBenchmarks/php-codeigniter|root '"${TROOT}"'|g' deploy/nginx.conf
+sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
+
+export PATH="$PHP_HOME/bin:$PHP_HOME/sbin:$PATH"
+
+hhvm --config $TROOT/deploy/config.hdf --user $(whoami) -m daemon
+$NGINX_HOME/sbin/nginx -c $TROOT/deploy/nginx.conf
