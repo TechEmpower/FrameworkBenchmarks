0eb0b884d2623541476b0006d1e477927a5dec36
sagenschneider <dsagenschneider@devisd.com>
2015-06-03 08:37:21 +0800

    Single Database Query test written.

--- frameworks/Java/officefloor/src/test/java/net/officefloor/performance/SingleDatabaseQueryTest.java
+++ frameworks/Java/officefloor/src/test/java/net/officefloor/performance/SingleDatabaseQueryTest.java
@@ -1,16 +1,25 @@
 package net.officefloor.performance;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import lombok.Data;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.hsqldb.jdbc.jdbcDataSource;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Ensures meets criteria for Test type 2: Single database query.
@@ -33,6 +42,11 @@ public class SingleDatabaseQueryTest extends AbstractTestCase {
 	 * Random numbers for validating results.
 	 */
 	private final int[] randomNumbers = new int[10000];
+
+	/**
+	 * {@link ObjectMapper}.
+	 */
+	private final ObjectMapper mapper = new ObjectMapper();
 
 	@Before
 	public void setupDatabase() throws SQLException {
@@ -73,9 +87,43 @@ public class SingleDatabaseQueryTest extends AbstractTestCase {
 				.createStatement().execute("SHUTDOWN IMMEDIATELY");
 	}
 
+	@Data
+	public static class SingleDatabaseResult {
+		private int id;
+		private int randomNumber;
+	}
+
+	@Test
+	public void ensureReadJson() throws IOException {
+		String entity = "{\"id\":3217,\"randomNumber\":2149}";
+		SingleDatabaseResult result = this.mapper.readValue(entity,
+				SingleDatabaseResult.class);
+		Assert.assertEquals("Incorrect id", 3217, result.getId());
+		Assert.assertEquals("Incorrect random number", 2149,
+				result.getRandomNumber());
+	}
+
 	@Override
 	protected void doRequestTest(CloseableHttpClient client) throws Exception {
-		Assert.fail("TODO implement request method");
+
+		// Request the random database value
+		HttpResponse response = client.execute(new HttpGet(
+				"http://localhost:7878/singleQuery-service.woof"));
+
+		// Validate the response
+		Assert.assertEquals("Should be successful", 200, response
+				.getStatusLine().getStatusCode());
+		this.assertHeadersSet(response, "Content-Length", "Content-Type",
+				"Server", "Date", "set-cookie");
+		this.assertHeader(response, "Content-Type",
+				"application/json; charset=UTF-8");
+
+		// Validate the correct random result
+		String entity = EntityUtils.toString(response.getEntity());
+		SingleDatabaseResult result = this.mapper.readValue(entity,
+				SingleDatabaseResult.class);
+		Assert.assertEquals("Incorrect random number",
+				this.randomNumbers[result.id - 1], result.randomNumber);
 	}
 
 }
