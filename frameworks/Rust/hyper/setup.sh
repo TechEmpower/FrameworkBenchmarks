31751dccfb658513364f316b822608fa6ac068fd
Steve Klabnik <steve@steveklabnik.com>
2015-06-04 11:30:15 -0400

    add note in setup.sh

--- frameworks/Rust/hyper/setup.sh
+++ frameworks/Rust/hyper/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/main.rs
+# Something like this will need to be done once we add database tests
+# sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/main.rs
 
 CARGO = $IROOT/rust/bin/cargo
 
