269c3a1200a874a93cb32e6694381afaabb908dd
Steve Klabnik <steve@steveklabnik.com>
2015-06-05 04:31:58 -0400

    bash :cry:

--- frameworks/Rust/hyper/setup.sh
+++ frameworks/Rust/hyper/setup.sh
@@ -3,7 +3,7 @@
 # Something like this will need to be done once we add database tests
 # sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/main.rs
 
-CARGO = $IROOT/rust/bin/cargo
+CARGO=$IROOT/rust/bin/cargo
 
 $CARGO build --release
 
