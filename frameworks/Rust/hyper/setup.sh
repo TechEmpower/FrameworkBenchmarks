dd27eb25f1b577ac49b01823e2eebe80bf36e34b
Steve Klabnik <steve@steveklabnik.com>
2015-06-06 04:41:18 -0400

    Fix up setup.sh

--- frameworks/Rust/hyper/setup.sh
+++ frameworks/Rust/hyper/setup.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Something like this will need to be done once we add database tests
-# sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/main.rs
+source $IROOT/rust.installed
 
-CARGO=$IROOT/rust/bin/cargo
-
-$CARGO build --release
-
-$CARGO run --release &
+cargo clean
+cargo build --release
+cargo run --release &
