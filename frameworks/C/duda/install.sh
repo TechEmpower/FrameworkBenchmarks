7b4c1493e79ff33b7d9522be7db041a1eb72dbe8
Zane Kansil <zkansil@techempower.com>
2015-06-09 09:43:41 -0700

    Update duda install to not depend on http://duda.io

--- frameworks/C/duda/install.sh
+++ frameworks/C/duda/install.sh
@@ -3,10 +3,9 @@
 RETCODE=$(fw_exists ${IROOT}/duda-0.23.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
-fw_get http://duda.io/releases/duda-client/dudac-0.23.tar.gz -O dudac-0.23.tar.gz
-fw_untar dudac-0.23.tar.gz
+git clone https://github.com/monkey/dudac.git
 
-cd dudac-0.23
+cd dudac/
 
 ./dudac -r
 ./dudac -s
