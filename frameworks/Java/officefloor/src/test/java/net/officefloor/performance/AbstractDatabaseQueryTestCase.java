83d31cd52061701bd9b7dc4a8925c456e6e7c88e
sagenschneider <daniel@officefloor.net>
2015-06-10 08:16:27 +0800

    All tests 1-6 written

--- frameworks/Java/officefloor/src/test/java/net/officefloor/performance/AbstractDatabaseQueryTestCase.java
+++ frameworks/Java/officefloor/src/test/java/net/officefloor/performance/AbstractDatabaseQueryTestCase.java
@@ -1,14 +1,21 @@
 package net.officefloor.performance;
 
+import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import lombok.Data;
+
 import org.hsqldb.jdbc.jdbcDataSource;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.ArrayType;
 
 /**
  * Abstract functionality for a database query test.
@@ -32,6 +39,48 @@ public abstract class AbstractDatabaseQueryTestCase extends AbstractTestCase {
 	 */
 	protected final int[] randomNumbers = new int[10000];
 
+	/**
+	 * {@link ObjectMapper}.
+	 */
+	private final ObjectMapper mapper = new ObjectMapper();
+
+	/**
+	 * {@link ArrayType} for parsing the JSON.
+	 */
+	private final ArrayType arrayType = this.mapper.getTypeFactory()
+			.constructArrayType(Result.class);
+
+	/**
+	 * Number of items to request.
+	 */
+	private volatile int requestBatchIndex = 0;
+
+	/**
+	 * Listing of request batch sizes.
+	 */
+	private final String[] queryValues = new String[] { null, "INVALID", "0",
+			"1", "5", "10", "15", "20", "500", "501" };
+
+	/**
+	 * Corresponding list of the resulting request batch size.
+	 */
+	private final int[] requestBatchSizes = new int[] { 1, 1, 1, 1, 5, 10, 15,
+			20, 500, 501 };
+
+	/**
+	 * {@link Connection}.
+	 */
+	protected Connection connection;
+
+	/**
+	 * Result from JSON response.
+	 */
+	@Data
+	public static class Result {
+		private int id;
+		private int randomNumber;
+	}
+
 	@Before
 	public void setupDatabase() throws SQLException {
 
@@ -44,31 +93,133 @@ public abstract class AbstractDatabaseQueryTestCase extends AbstractTestCase {
 		jdbcDataSource dataSource = new jdbcDataSource();
 		dataSource.setDatabase(DATABASE_URL);
 		dataSource.setUser(DATABASE_USER);
-		Connection connection = dataSource.getConnection();
+		this.connection = dataSource.getConnection();
 
 		// Create the table
-		Statement statement = connection.createStatement();
+		Statement statement = this.connection.createStatement();
 		statement
 				.execute("CREATE TABLE World (id int primary key, randomNumber int)");
 
 		// Load the values
-		PreparedStatement insert = connection
+		PreparedStatement insert = this.connection
 				.prepareStatement("INSERT INTO World (id, randomNumber) VALUES (?, ?)");
 		for (int i = 0; i < this.randomNumbers.length; i++) {
 			insert.setInt(1, (i + 1));
 			insert.setInt(2, this.randomNumbers[i]);
 			insert.executeUpdate();
 		}
-
-		// Cleanup setup
-		connection.close();
 	}
 
 	@After
 	public void cleanupDatabase() throws SQLException {
 		// Stop database for new instance each test
-		DriverManager.getConnection(DATABASE_URL, DATABASE_USER, "")
-				.createStatement().execute("SHUTDOWN IMMEDIATELY");
+		this.connection.createStatement().execute("SHUTDOWN IMMEDIATELY");
+	}
+
+	/**
+	 * Obtains the next index.
+	 * 
+	 * @return Next index.
+	 */
+	protected int nextIndex() {
+		// Obtain the next index
+		int index = this.requestBatchIndex;
+
+		// Provide the index for next request (duplicate batch sizes ok)
+		this.requestBatchIndex = (index + 1) % this.requestBatchSizes.length;
+
+		// Return the index
+		return index;
+	}
+
+	/**
+	 * Obtains the query string.
+	 * 
+	 * @param index
+	 *            Batch index.
+	 * @return Query string.
+	 */
+	protected String getQueryString(int index) {
+		String queryString = this.queryValues[index];
+		queryString = (queryString == null ? "" : "?queries=" + queryString);
+		return queryString;
+	}
+
+	/**
+	 * Obtains the expected batch size.
+	 * 
+	 * @param index
+	 *            Batch index.
+	 * @return Expected batch size.
+	 */
+	protected int getBatchSize(int index) {
+		return this.requestBatchSizes[index];
+	}
+
+	/**
+	 * Reads the {@link Result}.
+	 * 
+	 * @param entity
+	 *            Entity content.
+	 * @return {@link Result}.
+	 * @throws IOException
+	 *             If fails to read {@link Result}.
+	 */
+	protected Result readResult(String entity) throws IOException {
+		return this.mapper.readValue(entity, Result.class);
+	}
+
+	@Test
+	public void ensureReadResult() throws IOException {
+		String entity = "{\"id\":3217,\"randomNumber\":2149}";
+		Result result = this.mapper.readValue(entity, Result.class);
+		assertResult(result, 3217, 2149);
+	}
+
+	/**
+	 * Reads the {@link Result} instances from the entity.
+	 * 
+	 * @param entity
+	 *            Entity content.
+	 * @return {@link Result} instances.
+	 * @throws IOException
+	 *             If fails to read {@link Result} instances.
+	 */
+	protected Result[] readResults(String entity) throws IOException {
+		return (Result[]) this.mapper.readValue(entity, this.arrayType);
+	}
+
+	@Test
+	public void ensureReadResults() throws IOException {
+		String entity = "[{\"id\":4174,\"randomNumber\":331}"
+				+ ",{\"id\":51,\"randomNumber\":6544}"
+				+ ",{\"id\":4462,\"randomNumber\":952}"
+				+ ",{\"id\":2221,\"randomNumber\":532}"
+				+ ",{\"id\":9276,\"randomNumber\":3097}"
+				+ ",{\"id\":3056,\"randomNumber\":7293}"
+				+ ",{\"id\":6964,\"randomNumber\":620}"
+				+ ",{\"id\":675,\"randomNumber\":6601}"
+				+ ",{\"id\":8414,\"randomNumber\":6569}"
+				+ ",{\"id\":2753,\"randomNumber\":4065}]";
+		Result[] results = (Result[]) this.mapper.readValue(entity,
+				this.arrayType);
+		Assert.assertEquals("Incorrect number of elements", 10, results.length);
+		assertResult(results[0], 4174, 331);
+		assertResult(results[1], 51, 6544);
+		assertResult(results[2], 4462, 952);
+		assertResult(results[3], 2221, 532);
+		assertResult(results[4], 9276, 3097);
+		assertResult(results[5], 3056, 7293);
+		assertResult(results[6], 6964, 620);
+		assertResult(results[7], 675, 6601);
+		assertResult(results[8], 8414, 6569);
+		assertResult(results[9], 2753, 4065);
+	}
+
+	private static void assertResult(Result result, int id, int randomNumber) {
+		Assert.assertEquals("Incorrect identifier", id, result.getId());
+		Assert.assertEquals("Incorrect random number", randomNumber,
+				result.getRandomNumber());
 	}
 
 }
--- frameworks/Java/officefloor/src/test/java/net/officefloor/performance/SingleDatabaseQueryTest.java
+++ frameworks/Java/officefloor/src/test/java/net/officefloor/performance/SingleDatabaseQueryTest.java
@@ -1,17 +1,10 @@
 package net.officefloor.performance;
 
-import java.io.IOException;
-
-import lombok.Data;
-
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.junit.Assert;
-import org.junit.Test;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * Ensures meets criteria for Test type 2: Single database query.
@@ -19,26 +12,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
  * @author Daniel Sagenschneider.
  */
 public class SingleDatabaseQueryTest extends AbstractDatabaseQueryTestCase {
-
-	/**
-	 * {@link ObjectMapper}.
-	 */
-	private final ObjectMapper mapper = new ObjectMapper();
-
-	@Data
-	public static class Result {
-		private int id;
-		private int randomNumber;
-	}
-
-	@Test
-	public void ensureReadJson() throws IOException {
-		String entity = "{\"id\":3217,\"randomNumber\":2149}";
-		Result result = this.mapper.readValue(entity, Result.class);
-		Assert.assertEquals("Incorrect id", 3217, result.getId());
-		Assert.assertEquals("Incorrect random number", 2149,
-				result.getRandomNumber());
-	}
 
 	@Override
 	protected void doRequestTest(CloseableHttpClient client) throws Exception {
@@ -57,9 +30,10 @@ public class SingleDatabaseQueryTest extends AbstractDatabaseQueryTestCase {
 
 		// Validate the correct random result
 		String entity = EntityUtils.toString(response.getEntity());
-		Result result = this.mapper.readValue(entity, Result.class);
+		Result result = this.readResult(entity);
 		Assert.assertEquals("Incorrect random number",
-				this.randomNumbers[result.id - 1], result.randomNumber);
+				this.randomNumbers[result.getId() - 1],
+				result.getRandomNumber());
 	}
 
 }
