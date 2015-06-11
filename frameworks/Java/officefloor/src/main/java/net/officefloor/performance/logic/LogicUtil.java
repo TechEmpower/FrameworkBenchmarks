fc03f9e273f7d45ea234e793837fa8e6aa395666
sagenschneider <daniel@officefloor.net>
2015-06-11 23:26:54 +0800

    Multiple database queries passing

--- frameworks/Java/officefloor/src/main/java/net/officefloor/performance/logic/LogicUtil.java
+++ frameworks/Java/officefloor/src/main/java/net/officefloor/performance/logic/LogicUtil.java
@@ -25,4 +25,33 @@ public class LogicUtil {
 		return randomNumber;
 	}
 
+	/**
+	 * Obtains the query count.
+	 * 
+	 * @param queries
+	 *            Parameter value for <code>queries</code>.
+	 * @return Query count.
+	 */
+	public static int getQueryCount(String queries) {
+
+		// Obtain the number of queries to make
+		int queryCount;
+		try {
+			queryCount = Integer.parseInt(queries);
+		} catch (NumberFormatException ex) {
+			return 1;
+		} catch (NullPointerException ex) {
+			return 1;
+		}
+
+		// Ensure within bounds
+		if (queryCount < 1) {
+			return 1;
+		} else if (queryCount > 500) {
+			return 500;
+		} else {
+			return queryCount;
+		}
+	}
+
 }
