93891ea37cea99391eb00b12a209fe885babd517
sagenschneider <daniel@officefloor.net>
2015-06-15 18:16:33 +0800

    Fortunes test passing

--- frameworks/Java/officefloor/src/test/java/net/officefloor/performance/FortunesTest.java
+++ frameworks/Java/officefloor/src/test/java/net/officefloor/performance/FortunesTest.java
@@ -1,5 +1,6 @@
 package net.officefloor.performance;
 
+import java.nio.charset.Charset;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -29,17 +30,17 @@ public class FortunesTest extends AbstractTestCase {
 			+ "<tr><td>11</td><td>&lt;script&gt;alert(&quot;This should not be displayed in a browser alert box.&quot;);&lt;/script&gt;</td></tr>\n"
 			+ "<tr><td>4</td><td>A bad random number generator: 1, 1, 1, 1, 1, 4.33e+67, 1, 1, 1</td></tr>\n"
 			+ "<tr><td>5</td><td>A computer program does what you tell it to do, not what you want it to do.</td></tr>\n"
-			+ "<tr><td>2</td><td>A computer scientist is someone who fixes things that aren&apos;t broken.</td></tr>\n"
-			+ "<tr><td>8</td><td>A list is only as strong as its weakest link. — Donald Knuth</td></tr>\n"
+			+ "<tr><td>2</td><td>A computer scientist is someone who fixes things that aren't broken.</td></tr>\n"
+			+ "<tr><td>8</td><td>A list is only as strong as its weakest link. &mdash; Donald Knuth</td></tr>\n"
 			+ "<tr><td>0</td><td>Additional fortune added at request time.</td></tr>\n"
 			+ "<tr><td>3</td><td>After enough decimal places, nobody gives a damn.</td></tr>\n"
 			+ "<tr><td>7</td><td>Any program that runs right is obsolete.</td></tr>\n"
 			+ "<tr><td>10</td><td>Computers make very fast, very accurate mistakes.</td></tr>\n"
-			+ "<tr><td>6</td><td>Emacs is a nice operating system, but I prefer UNIX. — Tom Christaensen</td></tr>\n"
+			+ "<tr><td>6</td><td>Emacs is a nice operating system, but I prefer UNIX. &mdash; Tom Christaensen</td></tr>\n"
 			+ "<tr><td>9</td><td>Feature: A bug with seniority.</td></tr>\n"
 			+ "<tr><td>1</td><td>fortune: No such file or directory</td></tr>\n"
 			+ "<tr><td>12</td><td>フレームワークのベンチマーク</td></tr>\n"
-			+ "</table>\n</body>\n</html>";
+			+ "\n</table>\n</body>\n</html>";
 
 	/**
 	 * URL for the database.
@@ -133,7 +134,8 @@ public class FortunesTest extends AbstractTestCase {
 				"Content-Type", "Server", "Date", "set-cookie");
 		this.assertHeader(response, "Content-Type", "text/html; charset=UTF-8");
 		this.assertHeader(response, "Content-Length",
-				String.valueOf(EXPECTED_RESPONSE.length()));
+				String.valueOf(EXPECTED_RESPONSE.getBytes(Charset
+						.forName("UTF-8")).length));
 	}
 
 }
