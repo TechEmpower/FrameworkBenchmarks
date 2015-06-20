2f61deefd4137cd7931ae0cac211b92ef473d254
Zane Kansil <zkansil@techempower.com>
2015-06-19 17:02:30 -0700

    Don't append to crystal-0.7.1.installed

--- toolset/setup/linux/languages/crystal-0.7.1.sh
+++ toolset/setup/linux/languages/crystal-0.7.1.sh
@@ -11,4 +11,4 @@ fw_get -O $SAVE_AS $URL
 
 fw_untar crystal-0.7.1-1-linux-x86_64.tar.gz
 
-echo "export PATH=$IROOT/crystal-0.7.1-1/bin/:$PATH" >> ${IROOT}/crystal-0.7.1.installed
+echo "export PATH=$IROOT/crystal-0.7.1-1/bin/:$PATH" > ${IROOT}/crystal-0.7.1.installed
