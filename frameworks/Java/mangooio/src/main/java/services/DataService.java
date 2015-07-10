edef7927bde2bacb9427a9f650ed0ea43428ea85
Sven Kubiak <sk@svenkubiak.de>
2015-07-10 14:56:00 +0200

    version bump

--- frameworks/Java/mangooio/src/main/java/services/DataService.java
+++ frameworks/Java/mangooio/src/main/java/services/DataService.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import models.Fortune;
 import models.World;
