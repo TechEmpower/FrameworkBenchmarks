e1462bd4fc09185e3ccede3bc873d9599017682c
Gary Coady <gary@lyranthe.org>
2015-07-12 11:21:39 +0100

    http4s: Code refactoring, add source_code file.

--- frameworks/Scala/http4s/src/main/scala/WebServer.scala
+++ frameworks/Scala/http4s/src/main/scala/WebServer.scala
@@ -12,6 +12,7 @@ import doobie.imports._
 
 import scalaz._
 import scalaz.concurrent.{Task, TaskApp}
+import scalaz.syntax.traverse._
 
 import java.util.concurrent.ThreadLocalRandom
 
@@ -50,23 +51,47 @@ object WebServer extends TaskApp {
   def xaTask(host: String) =
     HikariTransactor[Task]("org.postgresql.Driver", s"jdbc:postgresql://$host/hello_world", "benchmarkdbuser", "benchmarkdbpass")
 
-  def randomWorldId = ThreadLocalRandom.current.nextInt(1, 10001)
+  // Provide a random number between 1 and 10000 (inclusive)
+  val randomWorldId: Task[Int] = Task.delay(ThreadLocalRandom.current.nextInt(1, 10001))
 
-  def selectRandomWorld(xa: Transactor[Task]): Task[World] = {
-    val query = sql"select id, randomNumber from World where id = $randomWorldId".query[World]
+  // Update the randomNumber field with a random number
+  def updateRandomNumber(world: World): Task[World] = {
+    randomWorldId map { id =>
+      world.copy(randomNumber = id)
+    }
+  }
+
+  // Select a World object from the database by ID
+  def selectWorld(xa: Transactor[Task], id: Int): Task[World] = {
+    val query = sql"select id, randomNumber from World where id = $id".query[World]
     query.unique.transact(xa)
   }
 
+  // Select a random World object from the database
+  def selectRandomWorld(xa: Transactor[Task]): Task[World] = {
+    randomWorldId flatMap { id =>
+      selectWorld(xa, id)
+    }
+  }
+
+  // Select a specified number of random World objects from the database
   def getWorlds(xa: Transactor[Task], numQueries: Int): Task[List[World]] =
     Nondeterminism[Task].replicateM(numQueries, selectRandomWorld(xa))
 
-  def updateWorlds(xa: Transactor[Task], worlds: List[World]): Task[List[World]] = {
-    val newWorlds = worlds.map(_.copy(randomNumber = randomWorldId))
-    val sql = "update World set randomNumber = ? where id = ?"
-    val update = Update[(Int, Int)](sql).updateMany(newWorlds.map(w => (w.randomNumber, w.id)))
-    update.transact(xa).map(_ => newWorlds)
+  // Update the randomNumber field with a new random number, for a list of World objects
+  def getNewWorlds(worlds: List[World]): Task[List[World]] = {
+    worlds.traverse(updateRandomNumber)
   }
 
+  // Update the randomNumber column in the database for a specified set of World objects,
+  // this uses a batch update SQL call.
+  def updateWorlds(xa: Transactor[Task], newWorlds: List[World]): Task[Int] = {
+    val sql = "update World set randomNumber = ? where id = ?"
+    val update = Update[(Int, Int)](sql).updateMany(newWorlds.map(w => (w.randomNumber, w.id)))
+    update.transact(xa)
+  }
+
+  // HTTP service definition
   def service(xa: Transactor[Task]) = HttpService {
     case GET -> Root / "json" =>
       Ok(Json("message" -> jString("Hello, World!")))
@@ -83,8 +108,9 @@ object WebServer extends TaskApp {
 
       val updated = for {
         worlds <- getWorlds(xa, numQueries)
-        updatedWorlds <- updateWorlds(xa, worlds)
-      } yield updatedWorlds
+        newWorlds <- getNewWorlds(worlds)
+        _ <- updateWorlds(xa, newWorlds)
+      } yield newWorlds
 
       Ok(updated.map(_.asJson))
 
@@ -93,6 +119,7 @@ object WebServer extends TaskApp {
         .withContentType(Some(`Content-Type`(MediaType.`text/plain`)))
   }
 
+  // Given a fully constructed HttpService, start the server and wait for completion
   def startServer(service: HttpService): Task[Unit] = {
     Task.delay {
       BlazeBuilder.bindHttp(8080, "0.0.0.0")
@@ -102,7 +129,8 @@ object WebServer extends TaskApp {
     }
   }
 
-  override val runc = {
+  // Entry point when starting service
+  override val runc: Task[Unit] = {
     for {
       dbHost <- Task.delay(System.getProperty("DBHOST", "localhost"))
       xa <- xaTask(dbHost)
