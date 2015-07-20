3a69934706f0d3d467de9b3d298a133dc9018d37
circlespainter <fabio@paralleluniverse.co>
2015-07-20 20:47:59 +0300

    Improvements

--- frameworks/Java/comsat-servlet/setup-generic.sh
+++ frameworks/Java/comsat-servlet/setup-generic.sh
@@ -8,4 +8,4 @@ mvn clean compile dependency:copy assembly:single
 cd target
 QUASAR_AGENT=`ls quasar-core-*.jar`
 FATJAR=`ls comsat-servlet-*.jar`
-java -javaagent:$QUASAR_AGENT $PROPS -jar $FATJAR
+java -javaagent:$QUASAR_AGENT $PROPS -Dco.paralleluniverse.fibers.detectRunawayFibers=false -jar $FATJAR
--- frameworks/Java/comsat-servlet/src/main/java/hello/HelloWebServer.java
+++ frameworks/Java/comsat-servlet/src/main/java/hello/HelloWebServer.java
@@ -1,6 +1,9 @@
 package hello;
 
+import co.paralleluniverse.embedded.containers.AbstractEmbeddedServer;
 import co.paralleluniverse.embedded.containers.EmbeddedServer;
+
+import java.io.IOException;
 
 /**
  * An implementation of the TechEmpower benchmark tests using Comsat servlets and the Undertow web server.
@@ -10,18 +13,36 @@ public final class HelloWebServer {
 		final EmbeddedServer server = (EmbeddedServer) Class.forName(System.getProperty("serverClass")).newInstance();
 		server.addServlet("plaintext", PlaintextServlet.class, "/plaintext");
 		server.addServlet("json", JsonServlet.class, "/json");
-		server.start();
-		System.err.println("Server is up");
+
 		Runtime.getRuntime().addShutdownHook(new Thread() {
 			@Override
 			public void run() {
 				try {
 					server.stop();
-					System.err.println("Server is down");
+
+					System.err.println("Server is down.");
 				} catch (Exception e) {
 					throw new RuntimeException(e);
 				}
 			}
 		});
+
+		new Thread() {
+			@Override
+			public void run() {
+				try {
+					AbstractEmbeddedServer.waitUrlAvailable("http://localhost:8080/plaintext");
+					AbstractEmbeddedServer.waitUrlAvailable("http://localhost:8080/json");
+
+					System.err.println("Server test cases are instrumented and bootstrapped.");
+				} catch (InterruptedException | IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}.start();
+
+		server.start();
+
+		System.err.println("Server is up.");
 	}
 }
