12c425c0d07ec8cfb950303dc2e43cbf95fbaeff
sagenschneider <daniel@officefloor.net>
2015-08-13 08:33:11 +0800

    Setup of OfficeFloor to run in TechEmpire environment.

--- frameworks/Java/officefloor/start.sh
+++ frameworks/Java/officefloor/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-mvn clean compile
+# Setup configuration file (normally properties files contains environment specific information but create copy to avoid SCM issues)
+mkdir -p ./production
+cp ./raw/datasource.properties ./production 
+sed -i 's|DATABASE_HOST|'"${DBHOST}"'|g' ./production/datasource.properties
 
-mvn net.officefloor.maven:woof:run
+# Compile and run application
+mvn -DskipTests clean compile
+mvn -Denvironment.properties.directory=production net.officefloor.maven:woof:run
