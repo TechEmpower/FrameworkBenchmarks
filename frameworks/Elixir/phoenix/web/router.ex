15ca7b7f42a3558573ce78f8d27d2a8a1f7dddc0
thousandsofthem <thousandsofthem@gmail.com>
2015-08-23 23:57:41 +0300

    cleanup router

--- frameworks/Elixir/phoenix/web/router.ex
+++ frameworks/Elixir/phoenix/web/router.ex
@@ -1,21 +1,7 @@
 defmodule Hello.Router do
   use Hello.Web, :router
 
-  pipeline :browser do
-    plug :accepts, ["html"]
-    # plug :fetch_session
-    # plug :fetch_flash
-    # plug :protect_from_forgery
-    # plug :put_secure_browser_headers
-  end
-
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", Hello do
-    pipe_through :browser # Use the default browser stack
-
     get "/json", PageController, :_json
     get "/db", PageController, :db
     get "/queries", PageController, :queries
@@ -25,8 +11,4 @@ defmodule Hello.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Hello do
-  #   pipe_through :api
-  # end
 end
