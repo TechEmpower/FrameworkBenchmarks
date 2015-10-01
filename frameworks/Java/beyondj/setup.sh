eac5209a5b3010bc879fbfeb80be75a4710b8959
Nick Kasvosve <KasvosveN@Quindell.com>
2015-10-01 03:16:31 +0100

    clean setup file

--- frameworks/Java/beyondj/setup.sh
+++ frameworks/Java/beyondj/setup.sh
@@ -2,10 +2,8 @@
 
 fw_depends java8 maven
 
-# rm beyondj-launcher/src/main/resources/launchers/webapp-launchers/beyondj-service.war
-sudo chmod -R 777 .
+sudo chmod -R 775 .
+
 mvn package
-# cp beyondj-service/target/beyondj-service.war beyondj-launcher/src/main/resources/launchers/webapp-launchers/
-# mvn clean package
 
 java -jar beyondj-launcher/target/beyondj.jar system.platform.dbserver=${DBHOST} numInstances=10
