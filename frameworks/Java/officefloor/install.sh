e87d919a786d9e2fd6f1d6e247d5e86f3e6729b2
sagenschneider <daniel@officefloor.net>
2015-10-11 10:33:06 +0800

    Tests now running in Vagrant VM

--- frameworks/Java/officefloor/install.sh
+++ frameworks/Java/officefloor/install.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
-fw_depends java8 maven
+# Propagate any failure
+set -e
+
+fw_depends java8
+
+# Need maven 3.1.1 or higher (default for Ubuntu 14.04 is 3.0.5)
+sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
+sudo apt-get update
+sudo apt-get -y --force-yes install maven3
+if [ -e /usr/bin/mvn ]
+then
+    sudo rm -f /usr/bin/mvn
+fi
+sudo ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
--- frameworks/Java/officefloor/start.sh
+++ frameworks/Java/officefloor/start.sh
@@ -3,6 +3,14 @@
 # Propagate any failure
 set -e
 
+# Indicate the user running
+echo `whoami`
+
+# Ensure maven uses appropriate version of Java
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle/
+sudo update-alternatives --set java /usr/lib/jvm/java-8-oracle/jre/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-8-oracle/bin/javac
+
 # Setup configuration file (normally properties files contains environment specific information but create copy to avoid SCM issues)
 echo "Creating configuration for OfficeFloor environment ..."
 mkdir -p ./production
@@ -12,9 +20,10 @@ echo "Configuration created"
 
 # Compile application
 echo "Building OfficeFloor test application ..."
-mvn -DskipTests clean install
+mvn -DskipTests clean package
 echo "OfficeFloor test application built"
 
 # Run application
 echo "Starting OfficeFloor application"
-mvn -DenvDir=production net.officefloor.maven:maven-woof-plugin:run
+mvn -DincludeGWT=false -DenvDir=production net.officefloor.maven:maven-woof-plugin:run
+#mvn net.officefloor.maven:maven-woof-plugin:run
