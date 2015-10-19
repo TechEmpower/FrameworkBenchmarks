fdfaa7805159e0d24acac3d4248cec055f620bb5
jamming <juanjoaguililla@gmail.com>
2015-10-19 23:36:48 +0200

    Pass all arguments to setup script

--- frameworks/Java/sabina/scripts/setup.sh
+++ frameworks/Java/sabina/scripts/setup.sh
@@ -2,7 +2,7 @@
 
 fw_depends java8 maven
 
-gradle/wrapper clean distZip -Ddb.host=${DBHOST}
+gradle/wrapper clean distZip "$@" -Ddb.host=${DBHOST}
 unzip -d build/ build/distributions/sabina-1.0.0.zip
 build/sabina-1.0.0/bin/sabina &
 
