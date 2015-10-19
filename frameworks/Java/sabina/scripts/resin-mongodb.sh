5b21be6825ad271cd1364f4223b68c9cfe912dd7
jamming <juanjoaguililla@gmail.com>
2015-10-19 23:17:21 +0200

    Refactor benchmark scripts

--- frameworks/Java/sabina/scripts/resin-mongodb.sh
+++ frameworks/Java/sabina/scripts/resin-mongodb.sh
@@ -3,9 +3,9 @@
 # load java environment variables
 source $IROOT/java8.installed
 
-mvn clean package -DskipTests -Ddb.host=${DBHOST}
+gradle/wrapper clean war -Ddb.host=${DBHOST}
 
 export RESIN_HOME=${IROOT}/resin-4.0.41
 rm -rf $RESIN_HOME/webapps/*
-cp target/sabina-1.0.0.war $RESIN_HOME/webapps
+cp build/libs/ROOT.war $RESIN_HOME/webapps
 $RESIN_HOME/bin/resinctl start
