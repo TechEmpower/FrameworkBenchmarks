0a9f195ea29d53dde9d72c902312a5522fcc04e3
jamming <juanjoaguililla@gmail.com>
2015-10-20 09:03:29 +0200

    Add traces to script

--- frameworks/Java/sabina/scripts/setup.sh
+++ frameworks/Java/sabina/scripts/setup.sh
@@ -2,7 +2,18 @@
 
 fw_depends java8 maven
 
-gradle/wrapper clean distZip "$@" -Ddb.host=${DBHOST}
+echo "PARAMS: $@"
+
+gradle/wrapper clean distZip $@ -Ddb.host=${DBHOST}
+
+echo "STATUS GRADLE: $#"
+
 unzip -d build/ build/distributions/sabina-1.0.0.zip
+
+echo "STATUS UNZIP: $#"
+
 build/sabina-1.0.0/bin/sabina &
+
+echo "STATUS SABINA: $#"
+
 
