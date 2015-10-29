ec54900508683093888253f62719eee8852802c5
Kamil Endruszkiewicz <kendruszkiewicz@ioki.com.pl>
2015-10-29 15:22:14 +0100

    Fixing faild to listen

--- frameworks/D/vibed/source/app.d
+++ frameworks/D/vibed/source/app.d
@@ -24,9 +24,10 @@ shared static this()
   router.get("/generate-world", &generateWorld);
   router.get("/generate-fortunes", &generateFortunes);
   router.get("/", staticTemplate!"index.dt");
+  router.rebuild();
 
   auto settings = new HTTPServerSettings;
-
+  settings.bindAddresses = ["0.0.0.0"];
   settings.options |= HTTPServerOption.distribute;
   settings.port = 8080;
 
