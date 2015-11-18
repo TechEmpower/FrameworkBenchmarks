7b9c56257ccd2d3236c170e1ed07adda67a58c42
Dennis Meckel <meckel@datenschuppen.de>
2015-11-18 18:28:02 +0100

    Removed the included source code of Fat-Free Framework and updated `setup.sh`

--- frameworks/PHP/fat-free/setup.sh
+++ frameworks/PHP/fat-free/setup.sh
@@ -3,10 +3,17 @@
 fw_depends php nginx
 
 sed -i 's|localhost|'"${DBHOST}"'|g' index.php
-sed -i 's|.*/FrameworkBenchmarks/fat-free|'"${TROOT}"'|g' deploy/fat-free
-sed -i 's|Directory .*/FrameworkBenchmarks/fat-free|Directory '"${TROOT}"'|g' deploy/fat-free
 sed -i 's|root .*/FrameworkBenchmarks/fat-free|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
-php-fpm --fpm-config $FWROOT/config/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
-nginx -c $TROOT/deploy/nginx.conf
+F3DIR="$TROOT/src"
+
+[[ ! -e "$F3DIR" ]] || rm -r "$F3DIR"
+
+git clone "https://github.com/bcosca/fatfree-core.git" "$F3DIR"
+pushd "$F3DIR" > /dev/null
+git checkout -q "b284f0c482e858f0162cab529925e7de14d90746" # v3.5.0
+popd > /dev/null
+
+php-fpm --fpm-config "$FWROOT/config/php-fpm.conf" -g "$TROOT/deploy/php-fpm.pid"
+nginx -c "$TROOT/deploy/nginx.conf"
