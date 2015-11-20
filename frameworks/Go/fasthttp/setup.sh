b85f19f91fddb231bfaad9382293c738cd24efb1
Aliaksandr Valialkin <valyala@gmail.com>
2015-11-20 18:04:44 +0200

    Explicitly enumerate third-party packages used by fasthttp benchmark

--- frameworks/Go/fasthttp/setup.sh
+++ frameworks/Go/fasthttp/setup.sh
@@ -4,6 +4,7 @@ sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/hello/hello.go
 
 fw_depends go
 
-go get ./...
+go get -u github.com/go-sql-driver/mysql
+go get -u github.com/valyala/fasthttp
 
 go run src/hello/hello.go &
