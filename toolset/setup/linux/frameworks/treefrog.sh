b6ef0c258eda2a0fbb3e27a5beef8ccd6313dff1
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2015-12-07 00:37:26 +0900

    Updated to TreeFrog v1.9.2

--- toolset/setup/linux/frameworks/treefrog.sh
+++ toolset/setup/linux/frameworks/treefrog.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+TFVER=treefrog-1.9.2
 RETCODE=$(fw_exists ${IROOT}/treefrog.installed)
 [ ! "$RETCODE" == 0 ] || { \
   source $IROOT/treefrog.installed
   return 0; }
 
-sudo apt-get install -y qt4-qmake libqt4-dev libqt4-sql-mysql libqt4-sql-psql g++
+sudo apt-get install -y qt5-qmake qt5-default qtbase5-dev qtbase5-dev-tools libqt5sql5 libqt5sql5-mysql libqt5sql5-psql g++ 
 
-fw_get -O http://downloads.sourceforge.net/project/treefrog/src/treefrog-1.7.7.tar.gz
-fw_untar treefrog-1.7.7.tar.gz
-cd treefrog-1.7.7
+fw_get -O http://downloads.sourceforge.net/project/treefrog/src/$TFVER.tar.gz
+fw_untar $TFVER.tar.gz
+cd $TFVER
 # TODO: Someday we can try this... I couldn't get it working
 #./configure --prefix=$IROOT/treefrog
 ./configure
