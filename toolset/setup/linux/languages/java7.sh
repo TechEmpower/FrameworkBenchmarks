acf3c36a2606d6ccd727e947781c0dd30913007d
Steven Smith <ssmith@techempower.com>
2015-12-09 10:35:12 -0800

    Changed Java 7 JDK from OpenJDK to Oracle

--- toolset/setup/linux/languages/java7.sh
+++ toolset/setup/linux/languages/java7.sh
@@ -9,13 +9,16 @@ RETCODE=$(fw_exists ${IROOT}/java7.installed)
 JAVA=$IROOT/java7
 
 # TODO: Someday move away from apt-get
+sudo add-apt-repository -y ppa:webupd8team/java
+sudo apt-get update
 # First remove java6
 sudo apt-get remove -y --purge openjdk-6-jre openjdk-6-jre-headless
 # Then install java7
-sudo apt-get install -y openjdk-7-jdk
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
+sudo apt-get install -y oracle-java7-installer
 
 # Setup environment variables
-echo "export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-`dpkg --print-architecture`" > $IROOT/java7.installed
+echo "export JAVA_HOME=/usr/lib/jvm/java-7-oracle" > $IROOT/java7.installed
 echo -e "export PATH=\$JAVA_HOME/bin:\$PATH" >> $IROOT/java7.installed
 
 source $IROOT/java7.installed
