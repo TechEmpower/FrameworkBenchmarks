e6a078a1fb4e1b3182f9b1099a9ba7764b430fa4
Sönke Ludwig <sludwig@rejectedsoftware.com>
2015-12-11 07:44:59 +0100

    Upgrade D and DUB to the latest versions.

--- toolset/setup/linux/systools/dub.sh
+++ toolset/setup/linux/systools/dub.sh
@@ -7,8 +7,8 @@ RETCODE=$(fw_exists ${IROOT}/dub.installed)
 
 mkdir dub
 cd dub
-fw_get -O http://code.dlang.org/files/dub-0.9.23-linux-x86_64.tar.gz
-fw_untar dub-0.9.23-linux-x86_64.tar.gz
+fw_get -O http://code.dlang.org/files/dub-0.9.24-linux-x86_64.tar.gz
+fw_untar dub-0.9.24-linux-x86_64.tar.gz
 
 echo -e "export PATH=${IROOT}/dub:\$PATH" > $IROOT/dub.installed
 
