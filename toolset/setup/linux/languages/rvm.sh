e804dfd535cdaa25224465fb1708312ff1ef4dc6
Steven Smith <ssmith@techempower.com>
2015-12-14 15:27:06 -0800

    More Travis testing

--- toolset/setup/linux/languages/rvm.sh
+++ toolset/setup/linux/languages/rvm.sh
@@ -10,6 +10,12 @@ RETCODE=$(fw_exists rvm.installed)
 export SHELL=/bin/bash
 gpg --keyserver hkp://keys.gnupg.net --recv-keys D39DC0E3
 \curl -sSL https://get.rvm.io | sudo bash -s stable
-echo "source ~/.rvm/scripts/rvm" > $IROOT/rvm.installed
+
+if [ "$TRAVIS" = "true" ]
+then
+  echo "source /usr/local/rvm" > $IROOT/rvm.installed
+else
+  echo "source ~/.rvm/scripts/rvm" > $IROOT/rvm.installed
+fi
 
 source $IROOT/rvm.installed
