a79adde4dfdbe3fb3abc3aa562cfd90df420ef1d
blee-techempower <blee@techempower.com>
2015-12-20 01:23:36 -0800

    Update Crystal/moonshine 'setup-postgres.sh' file

--- frameworks/Crystal/moonshine/setup-postgres.sh
+++ frameworks/Crystal/moonshine/setup-postgres.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-fw_depends crystal
+curl http://dist.crystal-lang.org/apt/setup.sh | sudo bash
+
+sudo apt-get install crystal
 
 crystal deps install
 
-crystal server-postgres.cr &
+crystal server-postgres.cr -- $TFB_SERVER_HOST &
