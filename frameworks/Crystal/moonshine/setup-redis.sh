eb0e59991afcee6aa33a9295fb8c9574b06cf138
blee-techempower <blee@techempower.com>
2015-12-20 03:47:53 -0800

    back to original setup command

--- frameworks/Crystal/moonshine/setup-redis.sh
+++ frameworks/Crystal/moonshine/setup-redis.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#fw_depends crystal
+fw_depends crystal
 
-curl http://dist.crystal-lang.org/apt/setup.sh | sudo bash
+#curl http://dist.crystal-lang.org/apt/setup.sh | sudo bash
 
-sudo apt-get install crystal
+#sudo apt-get install crystal
 
 crystal deps install
 
-crystal server-redis.cr -- $TFB_SERVER_HOST &
+crystal server-redis.cr &
