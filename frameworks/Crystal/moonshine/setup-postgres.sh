d387d52be3538bec587b25c1115f53539e475517
blee-techempower <blee@techempower.com>
2015-12-20 06:39:52 -0800

    clean setup-postgres.sh file

--- frameworks/Crystal/moonshine/setup-postgres.sh
+++ frameworks/Crystal/moonshine/setup-postgres.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-echo "@@@@@@@ Crystal Installation @@@@@@@"
 curl http://dist.crystal-lang.org/apt/setup.sh | sudo bash
 
 sudo apt-get install crystal
 
-echo "@@@@@@@ Dependency Setup @@@@@@@"
-
 crystal deps install
 
-echo "@@@@@@@ Execute Moonshine Server @@@@@@@"
-echo "@@@@@@@ CHECK ENVIRONMENT VARIABLE "
-echo $TFB_SERVER_HOST
-echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
-
-crystal server-postgres.cr -- $TFB_SERVER_HOST &
+crystal server-postgres.cr &
