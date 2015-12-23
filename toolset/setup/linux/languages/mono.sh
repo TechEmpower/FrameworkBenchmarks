94ad7eed5de06b8df917fff0a7307dceef749431
blee-techempower <blee@techempower.com>
2015-12-23 11:46:19 -0800

    STUPID MISTAKE: comment out original code that I need

--- toolset/setup/linux/languages/mono.sh
+++ toolset/setup/linux/languages/mono.sh
@@ -21,32 +21,32 @@ SNAPSHOT=$(apt-cache search 'mono-snapshot-.*-assemblies' | cut -d'-' -f3 | tail
 echo "형님이 나타났다!"$SNAPSHOT
 # save environment
 
-# MONO_HOME=$IROOT/mono-snapshot-$SNAPSHOT
-# echo "export SNAPSHOT=$SNAPSHOT" > $IROOT/mono.installing
-# echo "export MONO_HOME=$MONO_HOME" >> $IROOT/mono.installing
-# echo "export MONO_PATH=$MONO_HOME/lib/mono/4.5" >> $IROOT/mono.installing
-# echo "export MONO_CFG_DIR=$MONO_HOME/etc" >> $IROOT/mono.installing
-# echo -e "export PATH=$MONO_HOME/bin:\$PATH" >> $IROOT/mono.installing
-# echo -e "export LD_LIBRARY_PATH=$MONO_HOME/lib:\$LD_LIBRARY_PATH" >> $IROOT/mono.installing
-# echo -e "export PKG_CONFIG_PATH=$MONO_HOME/lib/pkgconfig:\$PKG_CONFIG_PATH" >> $IROOT/mono.installing
+MONO_HOME=$IROOT/mono-snapshot-$SNAPSHOT
+echo "export SNAPSHOT=$SNAPSHOT" > $IROOT/mono.installing
+echo "export MONO_HOME=$MONO_HOME" >> $IROOT/mono.installing
+echo "export MONO_PATH=$MONO_HOME/lib/mono/4.5" >> $IROOT/mono.installing
+echo "export MONO_CFG_DIR=$MONO_HOME/etc" >> $IROOT/mono.installing
+echo -e "export PATH=$MONO_HOME/bin:\$PATH" >> $IROOT/mono.installing
+echo -e "export LD_LIBRARY_PATH=$MONO_HOME/lib:\$LD_LIBRARY_PATH" >> $IROOT/mono.installing
+echo -e "export PKG_CONFIG_PATH=$MONO_HOME/lib/pkgconfig:\$PKG_CONFIG_PATH" >> $IROOT/mono.installing
 
-# # load environment
-# source $IROOT/mono.installing
+# load environment
+source $IROOT/mono.installing
 
-# # start fresh
-# rm -rf $MONO_HOME && mkdir -p $MONO_HOME
+# start fresh
+rm -rf $MONO_HOME && mkdir -p $MONO_HOME
 
-# # Download and extract debs
-# fw_apt_to_iroot mono-snapshot-$SNAPSHOT
-# fw_apt_to_iroot mono-snapshot-$SNAPSHOT-assemblies mono-snapshot-$SNAPSHOT
+# Download and extract debs
+fw_apt_to_iroot mono-snapshot-$SNAPSHOT
+fw_apt_to_iroot mono-snapshot-$SNAPSHOT-assemblies mono-snapshot-$SNAPSHOT
 
-# # Simplify paths
-# mv $MONO_HOME/opt/mono-*/* $MONO_HOME
-# file $MONO_HOME/bin/* | grep "POSIX shell script" | awk -F: '{print $1}' | xargs sed -i "s|/opt/mono-$SNAPSHOT|$MONO_HOME|g"
-# sed -i "s|/opt/mono-$SNAPSHOT|$MONO_HOME|g" $MONO_HOME/lib/pkgconfig/*.pc $MONO_HOME/etc/mono/config
+# Simplify paths
+mv $MONO_HOME/opt/mono-*/* $MONO_HOME
+file $MONO_HOME/bin/* | grep "POSIX shell script" | awk -F: '{print $1}' | xargs sed -i "s|/opt/mono-$SNAPSHOT|$MONO_HOME|g"
+sed -i "s|/opt/mono-$SNAPSHOT|$MONO_HOME|g" $MONO_HOME/lib/pkgconfig/*.pc $MONO_HOME/etc/mono/config
 
-# echo "mozroots --import --sync" >> $IROOT/mono.installing
+echo "mozroots --import --sync" >> $IROOT/mono.installing
 
-# mv $IROOT/mono.installing $IROOT/mono.installed
+mv $IROOT/mono.installing $IROOT/mono.installed
 
-# source $IROOT/mono.installed
+source $IROOT/mono.installed
