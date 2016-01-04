f8c127a412098cfe858aeb765601c55ff0832239
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2016-01-04 23:34:36 +0100

    Upgrade to Python 3.5.1 and adapt setup_py3.sh for pip3 command

--- frameworks/Python/asyncio/yocto_http/setup.sh
+++ frameworks/Python/asyncio/yocto_http/setup.sh
@@ -2,7 +2,7 @@
 
 fw_depends python3
 
-pip install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
+pip3 install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
 
 cd $TROOT/yocto_http
 api_hour -ac hello:Container &
