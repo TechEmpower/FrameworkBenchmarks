9cd47c383f2310612c4ce2a6567d6d396a0bdc83
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2016-01-04 23:27:26 +0100

    Use pip3 instead of pip

--- frameworks/Python/asyncio/aiohttp.web/setup.sh
+++ frameworks/Python/asyncio/aiohttp.web/setup.sh
@@ -4,7 +4,7 @@ fw_depends python3
 
 sed -i 's|host: 127.0.0.1|host: '${DBHOST}'|g' aiohttp.web/etc/hello/main/main.yaml
 
-$IROOT/py3/bin/pip install --install-option="--prefix=${IROOT}/py3" -r $TROOT/requirements.txt
+pip3 install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
 
 cd $TROOT/aiohttp.web
 api_hour -ac hello:Container &
--- frameworks/Python/flask/setup_py3.sh
+++ frameworks/Python/flask/setup_py3.sh
@@ -2,7 +2,6 @@
 
 fw_depends python3
 
-type pip
-pip install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
+pip3 install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
 
 gunicorn app:app -c gunicorn_conf.py &
