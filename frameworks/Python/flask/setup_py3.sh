5b998db833800a2a5b5799bd41a318de098aa4a3
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2016-01-04 16:17:08 +0100

    Try to find a way to support python 3.5.1

--- frameworks/Python/flask/setup_py3.sh
+++ frameworks/Python/flask/setup_py3.sh
@@ -2,6 +2,7 @@
 
 fw_depends python3
 
+type pip
 pip install --install-option="--prefix=${PY3_ROOT}" -r $TROOT/requirements.txt
 
 gunicorn app:app -c gunicorn_conf.py &
--- toolset/setup/linux/languages/python3.sh
+++ toolset/setup/linux/languages/python3.sh
@@ -3,6 +3,7 @@
 RETCODE=$(fw_exists ${IROOT}/py3.installed)
 [ ! "$RETCODE" == 0 ] || { \
   source $IROOT/py3.installed
+  hash -r
   return 0; }
   
 PY3_ROOT=$IROOT/py3
@@ -15,11 +16,12 @@ make -j4 --quiet 2>&1 | tee $IROOT/python3-install.log | awk '{ if (NR%100 == 0)
 make install --quiet 2>&1 | tee -a $IROOT/python3-install.log | awk '{ if (NR%100 == 0) printf "."}'
 cd ..
 
-#$PY3_ROOT/bin/python3 -m ensurepip -U
-#$PY3_ROOT/bin/pip3 install -U setuptools pip
+$PY3_ROOT/bin/python3 -m ensurepip -U
+$PY3_ROOT/bin/pip3 install -U setuptools pip
 
 echo "export PY3_ROOT=${PY3_ROOT}" > $IROOT/py3.installed
 echo -e "export PYTHONHOME=\$PY3_ROOT" >> $IROOT/py3.installed
 echo -e "export PATH=\$PY3_ROOT/bin:\$PATH" >> $IROOT/py3.installed
 
 source $IROOT/py3.installed
+hash -r
