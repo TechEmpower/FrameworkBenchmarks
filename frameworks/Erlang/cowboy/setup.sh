f65879e797498684b6d24a757ebf20c2f8a846c6
blee-techempower <blee@techempower.com>
2016-01-06 06:56:44 -0800

    Fix Erlang/cowboy travis-ci test fail issue

--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Caution: "line 11 has flaw." reported by 'blee@techempower.com'
+#   Below sed command can cause travis-ci test failing.
+#   Please be aware of testing this on local machine.
+#   If you perform a local test, please apply inverse of   
+#   below command. (hint: ${DBHOST} value must be rollback to
+#   localhost.
+#
+#   I will fix this problem later with better solution. 
+#
 sed -i 's|"benchmarkdbpass", ".*", 3306|"benchmarkdbpass", "'"${DBHOST}"'", 3306|g' src/hello_world_app.erl
 
 fw_depends erlang
