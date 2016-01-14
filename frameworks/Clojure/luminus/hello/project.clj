bb30178abf01283a547dd33bf9df73135e4110b2
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-01-14 18:38:50 -0500

    Update project.clj

--- frameworks/Clojure/luminus/hello/project.clj
+++ frameworks/Clojure/luminus/hello/project.clj
@@ -1,7 +1,7 @@
 (defproject hello "0.1.0-SNAPSHOT"
 
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Luminus framework benchmarks"
+  :url "https://github.com/TechEmpower/FrameworkBenchmarks/tree/master/frameworks/Clojure/luminus"
 
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [selmer "1.0.0"]
