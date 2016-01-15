bb56f9ddadd811e9b09e9c893785aae121a85a5f
stefanocasazza <stefano.casazza@gmail.com>
2016-01-15 17:23:54 +0100

    changes required

--- toolset/setup/linux/frameworks/ulib.sh
+++ toolset/setup/linux/frameworks/ulib.sh
@@ -28,15 +28,15 @@ if [ $? -ne 0 ]; then
 fi
 
 # We need to install mongo-c-driver (we don't have a ubuntu package)
-RETCODE=$(fw_exists ${IROOT}/mongo-c-driver.installed)
-if [ "$RETCODE" != 0 ]; then
-  wget https://github.com/mongodb/mongo-c-driver/releases/download/1.1.10/mongo-c-driver-1.1.10.tar.gz
-  tar -xzf mongo-c-driver-1.1.10.tar.gz
+#RETCODE=$(fw_exists ${IROOT}/mongo-c-driver.installed)
+#if [ "$RETCODE" != 0 ]; then
+  fw_get -O https://github.com/mongodb/mongo-c-driver/releases/download/1.1.10/mongo-c-driver-1.1.10.tar.gz
+  fw_untar mongo-c-driver-1.1.10.tar.gz
   cd mongo-c-driver-1.1.10/
   ./configure --prefix=$IROOT --libdir=$IROOT
-  make && sudo make install
-  touch ${IROOT}/mongo-c-driver.installed
-fi
+  make && make install
+# touch ${IROOT}/mongo-c-driver.installed
+#fi
 
 # Add a simple configuration file to it
 cd $ULIB_ROOT
