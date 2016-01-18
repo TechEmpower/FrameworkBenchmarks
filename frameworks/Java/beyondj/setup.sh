3ce03579e1ef6745bdf3d453bf53fd42898239ba
Nick Kasvosve <nkasvosve@yahoo.com>
2016-01-18 18:14:45 +0000

    remove commented lines in line with PR comments

--- frameworks/Java/beyondj/setup.sh
+++ frameworks/Java/beyondj/setup.sh
@@ -1,15 +1,14 @@
+#!/bin/bash
+
 WORKING_DIR=beyondj-launcher/deploy
-#FILE=beyondj-launcher/deploy/beyondj.jar
 
 if [ ! -d "$WORKING_DIR" ]; then
         mkdir $WORKING_DIR
 fi
 
-#if [ ! -f "$FILE" ]; then
-        cd beyondj-launcher/deploy/
-        wget http://beyondj.com/beyondj.jar
-        cd ../../
-#fi
+cd beyondj-launcher/deploy/
+fw_get -O http://beyondj.com/beyondj.jar
+cd ../../
 
 echo "Launching BeyondJ from location:$PWD"
 java -jar beyondj-launcher/deploy/beyondj.jar system.platform.dbserver=${DBHOST} numInstances=10
