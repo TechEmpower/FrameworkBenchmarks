78d56281a7ee94c6d56da88849f5b11f7e8e338f
Nick Kasvosve <nkasvosve@yahoo.com>
2016-01-19 16:41:30 +0000

    add version to jar^Ceing tested

--- frameworks/Java/beyondj/setup.sh
+++ frameworks/Java/beyondj/setup.sh
@@ -7,9 +7,9 @@ if [ ! -d "$WORKING_DIR" ]; then
 fi
 
 cd beyondj-launcher/deploy/
-fw_get -O http://beyondj.com/beyondj.jar
+fw_get -O http://beyondj.com/beyondj-launcher-1.0-SNAPSHOT.jar
 cd ../../
 
 echo "Launching BeyondJ from location:$PWD"
-java -jar beyondj-launcher/deploy/beyondj.jar system.platform.dbserver=${DBHOST} numInstances=10
+java -jar beyondj-launcher/deploy/beyondj-launcher-1.0-SNAPSHOT.jar system.platform.dbserver=${DBHOST} numInstances=10
 
