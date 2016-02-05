49d1ed3e3d86345034f9e1065fc2399b43032337
sagenschneider <daniel@officefloor.net>
2016-02-06 00:35:33 +0800

    Using latest maven version

--- toolset/setup/linux/systools/maven.sh
+++ toolset/setup/linux/systools/maven.sh
@@ -2,11 +2,18 @@
 
 RETCODE=$(fw_exists ${IROOT}/maven.installed)
 [ ! "$RETCODE" == 0 ] || { \
-  source $IROOT/maven.installed
-  return 0; }
+source $IROOT/maven.installed
+return 0; }
 
-# TODO: Someday remove apt-get
-sudo apt-get -y install maven
+sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
+sudo apt-get update
+sudo apt-get -y --force-yes install maven3
+if [ -e /usr/bin/mvn ]
+then
+    sudo rm -f /usr/bin/mvn
+fi
+sudo ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
+
 mvn -version
 
 touch $IROOT/maven.installed
