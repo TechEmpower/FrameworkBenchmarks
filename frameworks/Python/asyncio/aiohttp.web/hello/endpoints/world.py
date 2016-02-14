165a7c58ec2fc75ad414939fdf36544642f8c4a0
Ludovic Gasc (GMLudo) <git@gmludo.eu>
2016-02-14 21:34:10 +0100

    Add MySQL support for aiohttp.web

--- frameworks/Python/asyncio/aiohttp.web/hello/endpoints/world.py
+++ frameworks/Python/asyncio/aiohttp.web/hello/endpoints/world.py
@@ -7,7 +7,7 @@ import aiohttp_jinja2
 
 from ..services import queries_number
 from ..services.world import get_random_record, get_random_records, update_random_records, get_fortunes
-from ..services import redis
+from ..services import redis, mysql
 
 LOG = logging.getLogger(__name__)
 
@@ -31,6 +31,13 @@ def db_redis(request):
     return JSON((yield from redis.get_random_record(container)))
 
 @asyncio.coroutine
+def db_mysql(request):
+    """Test type 2: Single database query"""
+    container = request.app.ah_container
+
+    return JSON((yield from mysql.get_random_record(container)))
+
+@asyncio.coroutine
 def queries(request):
     """Test type 3: Multiple database queries"""
     container = request.app.ah_container
@@ -45,6 +52,14 @@ def queries_redis(request):
     limit = queries_number(request.GET.get('queries', 1))
 
     return JSON((yield from redis.get_random_records(container, limit)))
+
+@asyncio.coroutine
+def queries_mysql(request):
+    """Test type 3: Multiple database queries"""
+    container = request.app.ah_container
+    limit = queries_number(request.GET.get('queries', 1))
+
+    return JSON((yield from mysql.get_random_records(container, limit)))
 
 @asyncio.coroutine
 def fortunes(request):
@@ -65,6 +80,15 @@ def fortunes_redis(request):
                                           {'fortunes': (yield from redis.get_fortunes(container))})
 
 @asyncio.coroutine
+def fortunes_mysql(request):
+    """Test type 4: Fortunes"""
+    container = request.app.ah_container
+
+    return aiohttp_jinja2.render_template('fortunes.html.j2',
+                                          request,
+                                          {'fortunes': (yield from mysql.get_fortunes(container))})
+
+@asyncio.coroutine
 def updates(request):
     """Test type 5: Database updates"""
     container = request.app.ah_container
@@ -79,6 +103,14 @@ def updates_redis(request):
     limit = queries_number(request.GET.get('queries', 1))
 
     return JSON((yield from redis.update_random_records(container, limit)))
+
+@asyncio.coroutine
+def updates_mysql(request):
+    """Test type 5: Database updates"""
+    container = request.app.ah_container
+    limit = queries_number(request.GET.get('queries', 1))
+
+    return JSON((yield from mysql.update_random_records(container, limit)))
 
 @asyncio.coroutine
 def plaintext(request):
