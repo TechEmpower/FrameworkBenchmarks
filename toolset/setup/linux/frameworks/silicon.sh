3584dc51fd191ebd88e4dfb48d02d901242f247d
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2016-02-16 00:37:25 +0100

    [silicon] clang-3.5

--- toolset/setup/linux/frameworks/silicon.sh
+++ toolset/setup/linux/frameworks/silicon.sh
@@ -11,7 +11,7 @@ SILICON=$IROOT/silicon
 git clone https://github.com/matt-42/silicon.git
 cd silicon;
 git checkout 383282990a0ba12f7a74cb9d2987c7666904b279
-CXX=/usr/bin/clang++ ./install.sh $IROOT
+CXX=/usr/bin/clang++-3.5 ./install.sh $IROOT
 
 echo "" > $IROOT/silicon.installed
 
