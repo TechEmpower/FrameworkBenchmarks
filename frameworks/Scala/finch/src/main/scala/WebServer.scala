c73a375db993fa5c82287761e2cc1fed23e6309c
mfirry <mfirry@gmail.com>
2016-02-20 10:17:48 +0100

    Finch 0.10.0

--- frameworks/Scala/finch/src/main/scala/WebServer.scala
+++ frameworks/Scala/finch/src/main/scala/WebServer.scala
@@ -1,12 +1,9 @@
 import java.net.InetSocketAddress
 
 import io.finch._
-import io.finch.route._
-import io.finch.response._
 
 import com.twitter.finagle.Service
-import com.twitter.finagle.Httpx
-import com.twitter.finagle.httpx.{Response, Request}
+import com.twitter.finagle.Http
 
 import com.twitter.util.Await
 
@@ -14,25 +11,25 @@ import io.circe._
 import io.circe.generic.auto._
 import io.circe.jawn._
 import io.circe.syntax._
-import io.finch.response.EncodeResponse
 import io.circe.{Decoder, Encoder, Json}
 
 object WebServer extends App {
 
+  import io.finch.circe._
   val json = get("json") {
-    import io.finch.circe._
 
     case class Message(message: String)
     Ok(Message("Hello, World!").asJson)
   }
 
-  val plaintext = get("plaintext") {
-    "Hello, World!"
+  val plaintext: Endpoint[String] = get("plaintext") {
+    Ok("Hello, World!")
+      .withContentType(Some("text/plain"))
   }
 
-  val api: Service[Request, Response] = (json :+: plaintext).toService
+  val api = json :+: plaintext
 
   Await.ready(
-    Httpx.serve(":9000", api)
+    Http.serve(":9000", api.toService)
   )
 }
