f38b3aef0cdda68d8e044fb7a7a8ad0a343717fb
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-02-29 13:46:57 +0100

    bench setup

--- frameworks/Haskell/spock/setup.sh
+++ frameworks/Haskell/spock/setup.sh
@@ -5,4 +5,4 @@ fw_depends stack
 ${IROOT}/stack setup
 ${IROOT}/stack build
 
-${IROOT}/stack exec spock-exe ${MAX_THREADS} ${DBHOST} +RTS -A32m -N${MAX_THREADS} &
+${IROOT}/stack exec spock-exe ${MAX_THREADS} +RTS -A32m -N${MAX_THREADS} &
