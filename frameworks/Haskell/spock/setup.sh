d96c452495246b77f81f5e31f028b916d0770a58
Jérôme Mahuet <jerome.mahuet@gmail.com>
2016-03-01 12:17:30 +0100

    RTS settings

--- frameworks/Haskell/spock/setup.sh
+++ frameworks/Haskell/spock/setup.sh
@@ -5,4 +5,4 @@ fw_depends stack
 ${IROOT}/stack --allow-different-user setup
 ${IROOT}/stack --allow-different-user build
 
-${IROOT}/stack --allow-different-user exec spock-exe ${MAX_THREADS} +RTS -A32m -N${MAX_THREADS} &
+${IROOT}/stack --allow-different-user exec spock-exe -- +RTS -A32m -N${MAX_THREADS} &
