db7f57a66dbd69f9e3c9e5b06fa97e60f437e48a
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2016-03-01 11:09:25 +0900

    fix a bug of setup for treefrog.

--- toolset/setup/linux/frameworks/treefrog.sh
+++ toolset/setup/linux/frameworks/treefrog.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-TFVER=treefrog-1.9.2
+TFVER=treefrog-1.10.0
 RETCODE=$(fw_exists ${IROOT}/treefrog.installed)
 [ ! "$RETCODE" == 0 ] || { \
   source $IROOT/treefrog.installed
   return 0; }
 
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo apt-get update
 sudo apt-get install -y qt5-qmake qt5-default qtbase5-dev qtbase5-dev-tools libqt5sql5 libqt5sql5-mysql libqt5sql5-psql g++ 
+sudo add-apt-repository --remove --yes ppa:ubuntu-sdk-team/ppa
 
 fw_get -O http://downloads.sourceforge.net/project/treefrog/src/$TFVER.tar.gz
 fw_untar $TFVER.tar.gz
