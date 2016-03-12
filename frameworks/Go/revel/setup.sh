86b8a5cc0b6ea281add4188f9c34231373495885
INADA Naoki <songofacandy@gmail.com>
2016-03-12 15:16:40 +0900

    Fix revel installation

--- frameworks/Go/revel/setup.sh
+++ frameworks/Go/revel/setup.sh
@@ -4,6 +4,5 @@ fw_depends go
 
 sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/benchmark/conf/app.conf
 
-go get -u github.com/robfig/revel/revel
-go build -o bin/revel github.com/robfig/revel/revel
-bin/revel run benchmark prod &
+go get -u github.com/revel/cmd/revel
+$GOPATH/bin/revel run benchmark prod &
