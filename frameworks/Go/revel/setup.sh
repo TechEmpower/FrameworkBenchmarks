0321e57ac3e9508e85105a81cceacb12194af36e
INADA Naoki <songofacandy@gmail.com>
2016-03-15 02:06:17 +0900

    fix setup.sh

--- frameworks/Go/revel/setup.sh
+++ frameworks/Go/revel/setup.sh
@@ -5,5 +5,4 @@ fw_depends go
 sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/benchmark/conf/app.conf
 
 go get -u github.com/revel/cmd/revel
-revel=$GOPATH/bin/revel
-GOPATH=$GOPATH:`pwd` $revel run benchmark prod &
+bin/revel run benchmark prod &
