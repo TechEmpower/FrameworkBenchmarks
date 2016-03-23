3125b1d4d767ad7384ea04dfc520d9e294f43f67
Julia Nething <jnething@techempower.com>
2016-03-23 15:57:23 -0700

    Change maven and ant installs from creating symbolic links to exporting new PATHs

--- toolset/setup/linux/systools/ant.sh
+++ toolset/setup/linux/systools/ant.sh
@@ -8,14 +8,8 @@ return 0; }
 sudo apt-get update
 sudo apt-get -y --force-yes install ant
 
-if [ -e /usr/bin/ant ]
-then
-    sudo rm -f /usr/bin/ant
-fi
-sudo ln -s /usr/share/ant/bin/ant /usr/bin/ant
+echo "export PATH=/usr/share/ant/bin:\$PATH" > $IROOT/ant.installed
 
 ant -version
-
-touch $IROOT/ant.installed
 
 source $IROOT/ant.installed
--- toolset/setup/linux/systools/maven.sh
+++ toolset/setup/linux/systools/maven.sh
@@ -8,14 +8,9 @@ return 0; }
 sudo add-apt-repository "deb http://ppa.launchpad.net/natecarlson/maven3/ubuntu precise main"
 sudo apt-get update
 sudo apt-get -y --force-yes install maven3
-if [ -e /usr/bin/mvn ]
-then
-    sudo rm -f /usr/bin/mvn
-fi
-sudo ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
+
+echo "export PATH=/usr/share/maven3/bin:\$PATH" > $IROOT/maven.installed
 
 mvn -version
-
-touch $IROOT/maven.installed
 
 source $IROOT/maven.installed
