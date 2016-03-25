f817e89e452fa15a6873c288a04c200e008dd1c1
Tanner Nelson <tanner@bluebite.com>
2016-03-25 01:00:44 -0400

    2.2

--- frameworks/Swift/vapor/setup.sh
+++ frameworks/Swift/vapor/setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/02090c7ede5a637b76e6df1710e83cd0bbe7dcdf/swiftenv-install.sh)"
+# eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/02090c7ede5a637b76e6df1710e83cd0bbe7dcdf/swiftenv-install.sh)"
 
-swift build -c release -v
-.build/release/App &
+git clone https://github.com/qutheory/vapor
+cd vapor
+sudo make install
+
+vapor build --release
+
+# swift build -c release -v
+# .build/release/App &
+
+.build/VaporApp &
