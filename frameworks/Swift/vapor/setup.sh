62e901dbf0b0ad4ca70137d9520378a1f4fa7710
Tanner Nelson <tanner@bluebite.com>
2016-03-25 00:59:33 -0400

    verbose

--- frameworks/Swift/vapor/setup.sh
+++ frameworks/Swift/vapor/setup.sh
@@ -2,7 +2,5 @@
 
 eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/02090c7ede5a637b76e6df1710e83cd0bbe7dcdf/swiftenv-install.sh)"
 
-swift build --fetch
-rm -rf Packages/*/Tests
-swift build -c release
+swift build -c release -v
 .build/release/App &
