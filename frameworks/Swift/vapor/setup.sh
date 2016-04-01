6776ecff94013b9775df631018de4872e3e7550c
Tanner Nelson <tanner@bluebite.com>
2016-04-01 10:43:49 -0400

    update setup.sh

--- frameworks/Swift/vapor/setup.sh
+++ frameworks/Swift/vapor/setup.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
+# install swiftenv for managing Swift versions
 eval "$(curl -sL https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/02090c7ede5a637b76e6df1710e83cd0bbe7dcdf/swiftenv-install.sh)"
 
-# git clone https://github.com/qutheory/vapor
-# cd vapor
-# sudo make install
-
-# vapor build --release
-
-# printenv
-
+# install Swift compiler dependencies
 sudo apt-get -y install clang libicu-dev
 
+# compile the project with optimizations
 swift build -c release
 
-# swift build -c release -v -Xlinker -v
-# export LD_LIBRARY_PATH=/home/travis/build/qutheory/FrameworkBenchmarks/frameworks/Swift/vapor/.build/release
+# run in the background
 .build/release/App &
 
-
-# .build/VaporApp &
