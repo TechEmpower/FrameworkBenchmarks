56806f0c5ede17504222a6b793e088619160df0f
Tanner Nelson <tanner@bluebite.com>
2016-04-06 23:36:18 -0400

    update swift install

--- frameworks/Swift/vapor/setup.sh
+++ frameworks/Swift/vapor/setup.sh
@@ -2,9 +2,10 @@
 
 # install swift
 wget https://swift.org/builds/development/ubuntu1404/swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a/swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a-ubuntu14.04.tar.gz
-tar -zxvf swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a-ubuntu14.04.tar.gz 
+tar -zxf swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a-ubuntu14.04.tar.gz 
 mv swift-DEVELOPMENT-SNAPSHOT-2016-03-24-a-ubuntu14.04 swift
-export PATH=$PWD/swift/usr/bin/:"{$PATH}"
+sudo mv swift /usr/swift
+export PATH=/usr/swift/usr/bin/:"{$PATH}"
 
 # install Swift compiler dependencies
 sudo apt-get -y install clang libicu-dev
