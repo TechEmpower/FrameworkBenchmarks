6f80450d6c3f77e9bfbfd5821250ec5d1b9187f0
Isamu Mogi <isamu@leafytree.jp>
2016-04-24 17:44:05 +0000

    Use --install-ghc instead of 'stack setup'

--- frameworks/Haskell/spock/setup.sh
+++ frameworks/Haskell/spock/setup.sh
@@ -4,7 +4,6 @@ fw_depends stack
 
 sed -i 's|PG.connectHost     = "localhost"|PG.connectHost     = "'"${DBHOST}"'"|g' src/Main.hs
 
-${IROOT}/stack --allow-different-user setup
-${IROOT}/stack --allow-different-user build
+${IROOT}/stack --allow-different-user build --install-ghc
 
 ${IROOT}/stack --allow-different-user exec spock-exe -- +RTS -A32m -N${MAX_THREADS} &
