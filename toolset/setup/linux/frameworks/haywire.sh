478968019b7dad47ba9bcdd1b22aeb52503adafb
Keith Newman <knewman@techempower.com>
2016-06-20 11:23:18 -0700

    update haywire setup to use a specific commit

--- toolset/setup/linux/frameworks/haywire.sh
+++ toolset/setup/linux/frameworks/haywire.sh
@@ -10,6 +10,7 @@ HAYWIRE_HOME=$IROOT/Haywire
  
 git clone https://github.com/kellabyte/Haywire.git
 cd $HAYWIRE_HOME
+git checkout e4d23b533c15ddc51966e439fa455eb764e0f555
 ./build.sh -c release
 
 echo "export HAYWIRE_HOME=${HAYWIRE_HOME}" >> $IROOT/haywire.installed
