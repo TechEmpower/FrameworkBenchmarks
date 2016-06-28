03d476c9778071fe7d40b386374af5544ddc9938
Nate Brady <nbrady@techempower.com>
2016-06-28 11:51:09 -0700

    named all the php frameworks properly

--- frameworks/PHP/lumen/setup.sh
+++ frameworks/PHP/lumen/setup.sh
@@ -4,7 +4,7 @@ sed -i 's|localhost|'"${DBHOST}"'|g' index.php
 sed -i 's|root .*/FrameworkBenchmarks/lumen|root '"${TROOT}"'|g' deploy/nginx.conf
 sed -i 's|/usr/local/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
-fw_depends php nginx composer
+fw_depends php5 nginx composer
 
 rm vendor/illuminate/view/FileViewFinder.php
 cp modifiedVendorFiles/FileViewFinder.php vendor/illuminate/view/
