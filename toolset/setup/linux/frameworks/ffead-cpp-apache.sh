f3abb597ddd2a9bf2348844883a113e4cb9962d1
root <root@TFB-all>
2016-07-29 10:16:47 +0000

    Apache does not start if port 80 is already occupied, also fixed binary location to resolve the failing tests

--- toolset/setup/linux/frameworks/ffead-cpp-apache.sh
+++ toolset/setup/linux/frameworks/ffead-cpp-apache.sh
@@ -8,7 +8,9 @@ sudo apt-get install -y uuid-dev unixodbc unixodbc-dev
 fw_get -o ffead-cpp-2.0.tar.gz https://github.com/sumeetchhetri/ffead-cpp/releases/download/v2.0-Draft-TLV-Fixed-TE_Benchmark/ffead-cpp-2.0-tlfixed-bin.tar.gz
 fw_untar ffead-cpp-2.0.tar.gz
 
-cp -R ffead-cpp-2.0-bin/ ${TROOT}/ffead-cpp-2.0
+rm -rf ${TROOT}/ffead-cpp-2.0-bin
+cp -R ffead-cpp-2.0-bin/ ${TROOT}
+mv ${TROOT}/ffead-cpp-2.0-bin ${TROOT}/ffead-cpp-2.0
 rm -rf ffead-cpp-2.0/
 
 wget https://github.com/mongodb/mongo-c-driver/releases/download/1.1.10/mongo-c-driver-1.1.10.tar.gz
@@ -21,6 +23,7 @@ FFEADROOT=$TROOT/ffead-cpp-2.0
 ETROOT=${FFEADROOT//\//\\/}
 EIROOT=${IROOT//\//\\/}
 
+sudo sed -i 's/.*Listen 80.*/#Listen 80/' /etc/apache2/ports.conf
 sudo sed -i '/^export FFEAD_CPP_PATH=/{h;s/=.*/='"${ETROOT}"'/};${x;/^$/{s//export FFEAD_CPP_PATH='"${ETROOT}"'/;H};x}' /etc/apache2/envvars
 sudo sed -i '/^export LD_LIBRARY_PATH=/{h;s/=.*/='"${EIROOT}"':$FFEAD_CPP_PATH\/lib:$LD_LIBRARY_PATH/};${x;/^$/{s//export LD_LIBRARY_PATH='"${EIROOT}"':$FFEAD_CPP_PATH\/lib:$LD_LIBRARY_PATH/;H};x}' /etc/apache2/envvars
 
@@ -29,12 +32,12 @@ sudo bash -c 'rm -f /etc/apache2/sites-enabled/ffead-site.conf'
 sudo bash -c 'cat > /etc/apache2/sites-enabled/ffead-site.conf <<EOL
 LoadModule ffead_cpp_module '"${FFEADROOT}"'/mod_ffeadcpplib.so
 Listen 8080
-FFEAD_CPP_PATH '"${TROOT}"'/ffead-cpp-2.0
+FFEAD_CPP_PATH '"${FFEADROOT}"'
 <VirtualHost *:8080>
-        DocumentRoot '"${TROOT}"'/ffead-cpp-2.0/web
+        DocumentRoot '"${FFEADROOT}"'/web
         SetHandler ffead_cpp_module
 </VirtualHost>
-<Directory '"${TROOT}"'/ffead-cpp-2.0/web/>
+<Directory '"${FFEADROOT}"'/web/>
         Options Indexes FollowSymLinks
         AllowOverride None
         Require all granted
--- toolset/setup/linux/frameworks/ffead-cpp.sh
+++ toolset/setup/linux/frameworks/ffead-cpp.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sudo /etc/init.d/apache2 stop
-
 RETCODE=$(fw_exists ${IROOT}/ffead-cpp.installed)
 [ ! "$RETCODE" == 0 ] || { return 0; }
 
@@ -10,7 +8,9 @@ sudo apt-get install -y uuid-dev unixodbc unixodbc-dev
 fw_get -o ffead-cpp-2.0.tar.gz https://github.com/sumeetchhetri/ffead-cpp/releases/download/v2.0-Draft-TLV-Fixed-TE_Benchmark/ffead-cpp-2.0-tlfixed-bin.tar.gz
 fw_untar ffead-cpp-2.0.tar.gz
 
-cp -R ffead-cpp-2.0-bin/ ${TROOT}/ffead-cpp-2.0
+rm -rf ${TROOT}/ffead-cpp-2.0-bin
+cp -R ffead-cpp-2.0-bin/ ${TROOT}
+mv ${TROOT}/ffead-cpp-2.0-bin ${TROOT}/ffead-cpp-2.0
 rm -rf ffead-cpp-2.0/
 
 wget https://github.com/mongodb/mongo-c-driver/releases/download/1.1.10/mongo-c-driver-1.1.10.tar.gz
