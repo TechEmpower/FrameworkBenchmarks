961e1715160a796106c9277ed867e7c4d61f94a5
Keith Newman <knewman@techempower.com>
2016-08-05 17:09:41 -0700

    update java spark to v2.3 and add fortune test

--- frameworks/Java/spark/src/main/java/hello/web/HibernateUtil.java
+++ frameworks/Java/spark/src/main/java/hello/web/HibernateUtil.java
@@ -1,6 +1,7 @@
 package hello.web;
 
 import hello.domain.World;
+import hello.domain.Fortune;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -14,10 +15,10 @@ import org.slf4j.LoggerFactory;
 public class HibernateUtil {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(HibernateUtil.class);
-    
+
     private static final SessionFactory SESSION_FACTORY = createSessionFactory();
     private static final ThreadLocal<Session> SESSIONS = new ThreadLocal<>();
-    
+
     public static Session getSession() {
         Session session = SESSIONS.get();
         if (session == null) {
@@ -26,7 +27,7 @@ public class HibernateUtil {
         }
         return session;
     }
-    
+
     public static void closeSession() {
         Session session = SESSIONS.get();
         if (session != null) {
@@ -34,7 +35,7 @@ public class HibernateUtil {
             SESSIONS.remove();
         }
     }
-    
+
     private static SessionFactory createSessionFactory() {
         try {
             Configuration configuration = configuration();
@@ -45,6 +46,7 @@ public class HibernateUtil {
             configuration.setProperty(AvailableSettings.SHOW_SQL, "false");
             configuration.setProperty(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
             configuration.addAnnotatedClass(World.class);
+            configuration.addAnnotatedClass(Fortune.class);
             StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
             return configuration.buildSessionFactory(serviceRegistryBuilder.build());
         } catch (RuntimeException ex) {
@@ -52,11 +54,11 @@ public class HibernateUtil {
             throw ex;
         }
     }
-    
+
     private static Configuration configuration() {
         boolean jndi = Boolean.parseBoolean(System.getProperty("jndi", "true"));
         Configuration configuration = new Configuration();
-        // We're always going to use the -local config now since there were previous 
+        // We're always going to use the -local config now since there were previous
         // problems with the jndi config.
         /*
         if (jndi) {
@@ -68,5 +70,5 @@ public class HibernateUtil {
         configuration.configure("/hibernate-local.cfg.xml");
         return configuration;
     }
-    
+
 }
