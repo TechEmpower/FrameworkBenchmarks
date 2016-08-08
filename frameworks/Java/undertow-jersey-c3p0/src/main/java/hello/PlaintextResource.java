84f56a877199dd1e3d8df3cf3edac582465b1814
Matthew Flickner <mwflickner@gmail.com>
2016-08-08 08:39:52 -0700

    removed unnecessary import statement

--- frameworks/Java/undertow-jersey-c3p0/src/main/java/hello/PlaintextResource.java
+++ frameworks/Java/undertow-jersey-c3p0/src/main/java/hello/PlaintextResource.java
@@ -4,8 +4,6 @@ import javax.inject.*;
 import javax.ws.rs.*;
 import java.util.*;
 
-import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
-
 @Singleton
 @Path("/plaintext")
 public class PlaintextResource
