a500948d3f3f1066b197b17e25790b033eba3abf
Damien Solimando <dsolimando@gmail.com>
2016-08-12 20:19:02 +0200

    Hot framework integration

---
+++ toolset/setup/linux/frameworks/hot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+RETCODE=$(fw_exists ${IROOT}/hot.installed)
+[ ! "$RETCODE" == 0 ] || { \
+  source $IROOT/hot.installed
+  return 0; }
+
+VERSION="0.9.0-SNAPSHOT"
+HOT_HOME=$IROOT/hot-$VERSION
+
+fw_get -O https://github.com/dsolimando/Hot/releases/download/v0.9.0/hot-0.9.0-SNAPSHOT.tar.gz
+fw_untar hot-$VERSION.tar.gz
+
+echo "export HOT_HOME=${HOT_HOME}" > $IROOT/hot.installed
+echo -e "export PATH=\$HOT_HOME:\$PATH" >> $IROOT/hot.installed
+
+source $IROOT/hot.installed
