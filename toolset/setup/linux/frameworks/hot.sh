6f3738d11b7b3a1004d8970f1a1ac81c6864ff5b
Damien Solimando <dsolimando@gmail.com>
2016-08-16 10:43:21 +0200

    switch to version 0.10.0

--- toolset/setup/linux/frameworks/hot.sh
+++ toolset/setup/linux/frameworks/hot.sh
@@ -5,7 +5,7 @@ RETCODE=$(fw_exists ${IROOT}/hot.installed)
   source $IROOT/hot.installed
   return 0; }
 
-VERSION="0.9.0-SNAPSHOT"
+VERSION="0..0-SNAPSHOT"
 HOT_HOME=$IROOT/hot-$VERSION
 
 fw_get -O https://github.com/dsolimando/Hot/releases/download/0.10.0-SNAPSHOT/hot-0.10.0-SNAPSHOT.tar.gz
