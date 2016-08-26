b39a318c89cfcb5cc62b7ec0a4bcf34dff527c59
nathana1 <nathana@microsoft.com>
2016-08-26 16:22:16 -0700

    tweak aspnetcore metadata and configuration (#2230)

--- frameworks/CSharp/aspnetcore/run-windows.ps1
+++ frameworks/CSharp/aspnetcore/run-windows.ps1
@@ -5,4 +5,4 @@ $scenarios = (-split $scenarios) -join ","
 cd Benchmarks
 dotnet restore
 dotnet build -c Release -f netcoreapp1.0
-Start-Process -NoNewWindow dotnet "run -c Release server.urls=http://*:8080 server=$server threadCount=8 NonInteractive=true scenarios=$scenarios"
+Start-Process -NoNewWindow dotnet "run -c Release server.urls=http://*:8080 server=$server threadCount=1 NonInteractive=true scenarios=$scenarios"
