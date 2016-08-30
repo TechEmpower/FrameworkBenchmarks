6bede2d055655eba5e82a0ce2aa7ada545b7ce5a
Matthieu Garrigues <matthieu.garrigues@ensta-paristech.fr>
2016-08-30 10:51:30 +0200

    Fix silicon CC compiler.

--- toolset/setup/linux/frameworks/silicon.sh
+++ toolset/setup/linux/frameworks/silicon.sh
@@ -14,7 +14,7 @@ git clone https://github.com/matt-42/silicon.git
 cd silicon;
 # May 18th, 2016
 git checkout 73dac7f3c8dcd4f9c53713456e8b73165006e968
-CC=clang CXX=clang++-3.8 ./install.sh $IROOT
+CC=clang-3.8 CXX=clang++-3.8 ./install.sh $IROOT
 
 echo "" > $IROOT/silicon.installed
 
