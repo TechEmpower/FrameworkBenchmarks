e332c360126ba658e18f948e747b6d371c3d7220
Daniel Nicoletti <dantti12@gmail.com>
2016-09-09 14:26:23 -0300

    cutelyst fixes for round 13 (#2255)
    Nginx was using only one worker, now with auto it
    will have the same number as processors available,
    and uWSGI HTTP parser does not support keep-alive
    which on poor Azure connection led to 7 req/s
    so replacing it with our own HTTP server which supports
    keep-alive and pipelining, and adding threaded tests.

--- frameworks/C++/cutelyst/setup_thread.sh
+++ frameworks/C++/cutelyst/setup_thread.sh
@@ -21,4 +21,4 @@ make -j $MAX_THREADS
 export LD_LIBRARY_PATH=/opt/qt${QT_VERSION_MM}/lib:${IROOT}/lib/x86_64-linux-gnu/
 export CUTELYST_CONFIG=${TROOT}/config/config.ini
 
-${IROOT}/bin/cutelyst-wsgi --http-socket :8080 -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so -p $MAX_THREADS &
+${IROOT}/bin/cutelyst-wsgi --http-socket :8080 -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so -t $MAX_THREADS &
