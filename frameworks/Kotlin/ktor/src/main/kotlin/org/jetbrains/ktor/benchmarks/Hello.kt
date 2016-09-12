a216a2d14db4390d1f3e5e2e0dab429f7e108fdc
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-09-12 19:29:21 +0300

    ktor: implement json test

--- frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
+++ frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.ktor.benchmarks
 
+import com.google.gson.*
 import org.jetbrains.ktor.features.*
 import org.jetbrains.ktor.features.http.*
 import org.jetbrains.ktor.http.*
@@ -7,12 +8,19 @@ import org.jetbrains.ktor.netty.*
 import org.jetbrains.ktor.routing.*
 import org.jetbrains.ktor.transform.*
 
+data class Message(val message: String = "Hello, World!")
+
 fun main(args: Array<String>) {
+    val gson = GsonBuilder().create()
+
     embeddedNettyServer(9090) {
         application.install(DefaultHeaders)
 
         get("/plaintext") { call ->
             call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Text.Plain, "Hello, World!"))
+        }
+        get("/json") { call ->
+            call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Application.Json, gson.toJson(Message())))
         }
     }.start(true)
 }
