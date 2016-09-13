95e0904a898d0dfd535784c353af75770372a7cc
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2016-09-13 17:13:10 +0300

    ktor: add updates test

--- frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
+++ frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
@@ -4,6 +4,7 @@ import com.google.gson.*
 import com.mchange.v2.c3p0.*
 import com.mysql.cj.jdbc.*
 import kotlinx.support.jdk7.*
+import org.jetbrains.ktor.application.*
 import org.jetbrains.ktor.features.*
 import org.jetbrains.ktor.features.http.*
 import org.jetbrains.ktor.http.*
@@ -15,7 +16,7 @@ import java.sql.ResultSet.*
 import java.util.*
 
 data class Message(val message: String = "Hello, World!")
-data class World(val id: Int, val randomNumber: Int)
+data class World(val id: Int, var randomNumber: Int)
 
 fun main(args: Array<String>) {
     val (a, b) = hex("726f6f743a736563726574").toString(Charsets.ISO_8859_1).split(":")
@@ -41,17 +42,12 @@ fun main(args: Array<String>) {
         }
         get("/db") { call ->
             pool.connection.use { connection ->
-                val queries = try {
-                    call.request.queryParameters["queries"]?.toInt()?.coerceIn(1, 500)
-                } catch (nfe: NumberFormatException) {
-                    1
-                }
-
+                val queries = call.queries()
                 val random = Random()
                 val result = mutableListOf<World>()
 
                 connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
-                    for (i in 1.. (queries ?: 1)) {
+                    for (i in 1..(queries ?: 1)) {
                         statement.setInt(1, random.nextInt(DbRows) + 1)
 
                         statement.executeQuery().use { rs ->
@@ -68,6 +64,48 @@ fun main(args: Array<String>) {
                 }
             }
         }
+        get("/updates") {
+            pool.connection.use { connection ->
+                val queries = call.queries()
+                val random = Random()
+                val result = mutableListOf<World>()
+
+                connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
+                    for (i in 1..(queries ?: 1)) {
+                        statement.setInt(1, random.nextInt(DbRows) + 1)
+
+                        statement.executeQuery().use { rs ->
+                            while (rs.next()) {
+                                result += World(rs.getInt("id"), rs.getInt("randomNumber"))
+                            }
+                        }
+                    }
+
+                }
+
+                result.forEach { it.randomNumber = random.nextInt(DbRows) + 1 }
+
+                connection.prepareStatement("UPDATE World SET randomNumber = ? WHERE id = ?").use { updateStatement ->
+                    for ((id, randomNumber) in result) {
+                        updateStatement.setInt(1, randomNumber)
+                        updateStatement.setInt(2, id)
+
+                        updateStatement.executeUpdate()
+                    }
+                }
+
+                call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Application.Json, gson.toJson(when (queries) {
+                    null -> result.single()
+                    else -> result
+                })))
+            }
+        }
     }.start(true)
+}
+
+fun ApplicationCall.queries() = try {
+    request.queryParameters["queries"]?.toInt()?.coerceIn(1, 500)
+} catch (nfe: NumberFormatException) {
+    1
 }
 
