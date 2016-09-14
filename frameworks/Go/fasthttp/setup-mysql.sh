12df86d7cea32cecc76b437847ce70957cf39ed5
Aliaksandr Valialkin <valyala@gmail.com>
2016-09-14 14:50:14 +0300

    fasthttp: fixed setup scripts

--- frameworks/Go/fasthttp/setup-mysql.sh
+++ frameworks/Go/fasthttp/setup-mysql.sh
@@ -5,7 +5,7 @@ sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/server-mysql/server.go
 fw_depends go
 
 GOPATH=`pwd` go get -u github.com/go-sql-driver/mysql
-GOPATH=`pwd` go get -u github.com/valyala/fasthttp
+GOPATH=`pwd` go get -u github.com/valyala/fasthttp/...
 GOPATH=`pwd` go get -u github.com/valyala/quicktemplate/qtc
 
 rm -f ./server-mysql
