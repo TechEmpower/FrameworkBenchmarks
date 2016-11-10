a67a9cee9ef15b618d0729856cf59f48a6e2d0a9
Daniel Nicoletti <dantti12@gmail.com>
2016-11-10 17:41:04 +0000

    Fix cutelyst threaded tests
    The preview runs raised a segfault when running
    cutelyst in threaded mode, an unsafe use of atribution
    of a static object, the fix got included in the 1.0.0 release

--- frameworks/C++/cutelyst/setup_thread.sh
+++ frameworks/C++/cutelyst/setup_thread.sh
@@ -19,6 +19,5 @@ make clean
 make -j $MAX_THREADS
 
 export LD_LIBRARY_PATH=/opt/qt${QT_VERSION_MM}/lib:${IROOT}/lib/x86_64-linux-gnu/
-export CUTELYST_CONFIG=${TROOT}/config/config.ini
 
-${IROOT}/bin/cutelyst-wsgi --http-socket :8080 -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so -t $MAX_THREADS &
+${IROOT}/bin/cutelyst-wsgi --ini ${TROOT}/config/config.ini --http-socket :8080 -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so -t $MAX_THREADS &
