bd7e10cf55d197f31ff2c6546ec29e2ff4396eee
msmith-techempower <msmith@techempower.com>
2016-11-17 10:52:22 -0800

    Squashing bugs

--- toolset/setup/linux/databases/mysql.sh
+++ toolset/setup/linux/databases/mysql.sh
@@ -5,28 +5,31 @@ RETCODE=$(fw_exists ${IROOT}/mysql.installed)
   source $IROOT/mysql.installed
   return 0; }
 
+# send over the required files
+scp $FWROOT/config/my.conf $DBHOST:~/
+scp $FWROOT/config/usr.sbin.mysqld $DBHOST:~/
+
 # install mysql on database machine
 ssh $DBHOST -t "
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server \
-\
-sudo stop mysql \
-# disable checking of disk size \
-sudo mv mysql /etc/init.d/mysql \
-sudo chmod +x /etc/init.d/mysql \
-sudo mv mysql.conf /etc/init/mysql.conf \
-# use the my.cnf file to overwrite /etc/mysql/my.cnf \
-sudo mv /etc/mysql/my.cnf /etc/mysql/my.cnf.orig \
-sudo mv my.cnf /etc/mysql/my.cnf \
-\
-sudo rm -rf /ssd/mysql \
-sudo rm -rf /ssd/log/mysql \
-sudo cp -R -p /var/lib/mysql /ssd/ \
-sudo cp -R -p /var/log/mysql /ssd/log \
-sudo cp usr.sbin.mysqld /etc/apparmor.d/ \
-sudo /etc/init.d/apparmor reload \
-sudo start mysql \
-\
-# Set root password \
+sudo DEBIAN_FRONTEND=noninteractive apt-get -y install mysql-server
+
+sudo stop mysql
+
+sudo mv mysql /etc/init.d/mysql
+sudo chmod +x /etc/init.d/mysql
+sudo mv mysql.conf /etc/init/mysql.conf
+
+sudo mv /etc/mysql/my.cnf /etc/mysql/my.cnf.orig
+sudo mv my.cnf /etc/mysql/my.cnf
+
+sudo rm -rf /ssd/mysql
+sudo rm -rf /ssd/log/mysql
+sudo cp -R -p /var/lib/mysql /ssd/
+sudo cp -R -p /var/log/mysql /ssd/log
+sudo cp usr.sbin.mysqld /etc/apparmor.d/
+sudo /etc/init.d/apparmor reload
+sudo start mysql
+
 sudo mysqladmin -u root password secret"
 
 # Install the mysql client
