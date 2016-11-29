94fea0f9237e06464b805a0ec5803f7c403ba9f4
msmith-techempower <msmith@techempower.com>
2016-11-29 11:59:23 -0800

    ssh+bash making me sad

--- toolset/setup/linux/databases/postgresql.sh
+++ toolset/setup/linux/databases/postgresql.sh
@@ -9,10 +9,10 @@ RETCODE=$(fw_exists ${IROOT}/postgresql.installed)
 
 # delete any old required files that do not belong to us as
 # scp will fail otherwise
-ssh $DBHOST -t "
+ssh $DBHOST 'bash' <<EOF
   sudo rm -rf create-postgres-database.sql
   sudo rm -rf create-postgres.sql
-"
+EOF
 
 # send over the required files
 scp $FWROOT/config/postgresql.conf $DBHOST:~/
@@ -27,11 +27,7 @@ sudo apt-get -y update
 sudo apt-get -y install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" postgresql
 
 # This will support all 9.* versions depending on the machine
-service postgresql status &> /dev/null
-if [ $? -eq 0 ]; then
-  sudo service postgresql stop
-fi
-sudo killall -s 9 -u postgres
+service postgresql status &> /dev/null || sudo service postgresql stop
 
 # Make sure all the configuration files in main belong to postgres
 PG_VERSION=`pg_config --version | grep -oP '\d\.\d'`
@@ -47,13 +43,12 @@ sudo mv 60-postgresql-shm.conf /etc/sysctl.d/60-postgresql-shm.conf
 sudo chown postgres:postgres /etc/sysctl.d/60-postgresql-shm.conf
 sudo chown postgres:postgres create-postgres*
 
-service postgresql status &> /dev/null
-if [ $? -ne 0 ]; then
-  sudo service postgresql start
-fi
+service postgresql status &> /dev/null && sudo service postgresql start
 EOF
 
-echo -e "ssh \$DBHOST -t 'sudo -u postgres psql -q template1 < create-postgres-database.sql
-  sudo -u postgres psql -q hello_world < create-postgres.sql'" > $IROOT/postgresql.installed
+echo -e "ssh \$DBHOST <<EOF" > $IROOT/postgresql.installed
+echo "sudo -u postgres psql -q template1 < create-postgres-database.sql" >> $IROOT/postgresql.installed
+echo "sudo -u postgres psql -q hello_world < create-postgres.sql" >> $IROOT/postgresql.installed
+echo "EOF" > $IROOT/postgresql.installed" >> $IROOT/postgresql.installed
 
 source $IROOT/postgresql.installed
