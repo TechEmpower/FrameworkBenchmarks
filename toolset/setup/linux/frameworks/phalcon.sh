f786bd67d9ba05a042c983450bed66617c54b975
knewmanTE <knewman@techempower.com>
2016-12-15 13:59:00 -0800

    Update PHP/phalcon to v3 (#2179)
    * upgrade phalcon to v3
    
    * install php7-compatible mongodb library for phalcon with composer
    
    * MongoDB working with Phalcon incubator

--- toolset/setup/linux/frameworks/phalcon.sh
+++ toolset/setup/linux/frameworks/phalcon.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-fw_depends php5
+fw_depends php7
 
 RETCODE=$(fw_exists ${IROOT}/phalcon.installed)
 [ ! "$RETCODE" == 0 ] || { \
@@ -10,17 +10,12 @@ RETCODE=$(fw_exists ${IROOT}/phalcon.installed)
 # Enable the PHP phalcon extension
 sed -i 's|;extension=phalcon.so|extension=phalcon.so|g' $PHP_HOME/lib/php.ini
 
-fw_get -O https://github.com/phalcon/cphalcon/archive/phalcon-v2.0.13.tar.gz
-fw_untar phalcon-v2.0.13.tar.gz
-cd cphalcon-phalcon-v2.0.13/build/64bits 
-$PHP_HOME/bin/phpize
-# For some reason we have to point to php-config 
-# explicitly, it's not found by the prefix settings
-./configure --prefix=$PHP_HOME --exec-prefix=$PHP_HOME \
-  --with-php-config=$PHP_HOME/bin/php-config \
-  --enable-phalcon --quiet
-make --quiet
-make install
+VERSION="3.0.2"
+
+fw_get -O https://github.com/phalcon/cphalcon/archive/v${VERSION}.tar.gz
+fw_untar v${VERSION}.tar.gz
+cd cphalcon-${VERSION}/build
+./install
 
 echo "" > $IROOT/phalcon.installed
 
