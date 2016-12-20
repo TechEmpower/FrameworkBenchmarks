f699f850a70080bdfea68e45eb54a887dba6b812
Nate Brady <nbrady@techempower.com>
2016-12-20 12:54:07 -0800

    force toolset change

--- toolset/travis/travis_clean.sh
+++ toolset/travis/travis_clean.sh
@@ -8,8 +8,8 @@ export PATH=/home/travis/bin:/home/travis/.local/bin:/usr/local/sbin:/usr/local/
 # Remove services/installs that travis gives us
 sudo apt-get purge ruby* -yqq
 
-# Travis adds sources for the extra services they provide. When doing apt
-# updates this can interfere with the standard ubuntu installs so we'll reset the sources.
+# Travis adds sources for the extra services they provide. When doing
+# apt updates this can interfere with the standard ubuntu installs so we'll reset the sources.
 sudo mv ./toolset/travis/sources.list /etc/apt/sources.list
 sudo rm -rf /etc/apt/sources.list.d
 sudo mkdir /etc/apt/sources.list.d
