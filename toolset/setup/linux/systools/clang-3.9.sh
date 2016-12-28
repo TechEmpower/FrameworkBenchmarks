a3139680ca40b4696c9ccc832fd31235b4d24b2f
nate <nate>
2016-12-28 14:43:05 +0000

    add the gpg keys

--- toolset/setup/linux/systools/clang-3.9.sh
+++ toolset/setup/linux/systools/clang-3.9.sh
@@ -7,6 +7,7 @@ RETCODE=$(fw_exists ${IROOT}/clang-3.9.installed)
   return 0; }
 
 sudo add-apt-repository -s "deb http://apt.llvm.org/`lsb_release -cs`/ llvm-toolchain-`lsb_release -cs`-3.9 main"
+wget -O - http://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get -yq update
 sudo apt-get install -qqy clang-3.9 lldb-3.9
 
