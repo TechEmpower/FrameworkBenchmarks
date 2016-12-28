b657c8d5f2fc0ab417b68e27d9157c284add996a
Nate Brady <nbrady@techempower.com>
2016-12-28 07:42:15 -0800

    Java/dropwizard fix

--- frameworks/Java/dropwizard/setup_postgresql.sh
+++ frameworks/Java/dropwizard/setup_postgresql.sh
@@ -4,6 +4,6 @@ fw_depends postgresql java maven
 
 sed -i 's|url: jdbc:postgresql://.*/hello_world|url: jdbc:postgresql://'"${DBHOST}"':5432/hello_world|g' hello-world-postgres.yml
 
-mvn -P postgresql clean package
+mvn -P postgres clean package
 
 java -jar target/hello-world-0.0.1-SNAPSHOT.jar server hello-world-postgres.yml &
