8957b1ab216a6148d75ce35ae9977260d3fce2e1
nate <nate>
2016-12-28 14:37:26 +0000

    update clang

---
+++ toolset/setup/linux/systools/clang-3.9.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+RETCODE=$(fw_exists ${IROOT}/clang-3.9.installed)
+[ ! "$RETCODE" == 0 ] || { \
+  source $IROOT/clang-3.9.installed
+  return 0; }
+
+sudo add-apt-repository -s "deb http://apt.llvm.org/`lsb_release -cs`/ llvm-toolchain-`lsb_release -cs`-3.9 main"
+sudo apt-get -yq update
+sudo apt-get install -qqy clang-3.9 lldb-3.9
+
+touch $IROOT/clang-3.9.installed
+
