0889d7f42acce88deea580eee4d0bfa1c74120c0
Nate <nathan.brady@yahoo.com>
2017-01-06 11:45:21 -0800

    Remove redis from nodejs test (#2480)

--- frameworks/JavaScript/nodejs/setup.sh
+++ frameworks/JavaScript/nodejs/setup.sh
@@ -5,7 +5,6 @@ fw_depends mongodb nodejs
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/mongodb-raw.js
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/mongoose.js
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/mysql-raw.js
-sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/redis.js
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/sequelize.js
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' handlers/sequelize-postgres.js
 
