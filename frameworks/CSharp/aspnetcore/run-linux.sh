399a99a724647eee42ddd3c3a54e31be3fbbaac2
Mike Harder <mharder@microsoft.com>
2017-01-10 06:37:34 -0800

    CSharp/aspnetcore update (#2483)
    * aspnetcore: Remove dependency on net451 and mono
    
    * aspnetcore: Update PostgreSQL connection string for latest npgsql
    - Prepared statements are now persisted by default, so the PersistPrepared option has been removed.
    - https://github.com/npgsql/npgsql/issues/1237
    
    * aspnetcore: Make shell scripts executable
    
    * aspnetcore: Change scripts to use `dotnet Benchmarks.dll`
    - Scripts currently use `dotnet run` which is intended for iterative development rather than production.
    - Should not cause a measurable change in performance.
    
    * aspnetcore: Add DB scenarios to README.md
    
    * aspnetcore: Update framework and packages to 1.1
    
    * aspnetcore: Remove unnecessary import from project.json
    
    * aspnetcore: Make dotnetcore.sh executable
    
    * aspnetcore: Update dotnetcore.sh to 1.1

--- frameworks/CSharp/aspnetcore/run-linux.sh
+++ frameworks/CSharp/aspnetcore/run-linux.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-fw_depends mono dotnetcore
+fw_depends dotnetcore
 
 threadCount=$2
 if [ "$threadCount" -lt "1" ]
@@ -11,6 +11,6 @@ cd Benchmarks
 cp appsettings.postgresql.json appsettings.json
 sed -i 's|{db_server_placeholder}|'"${DBHOST}"'|g' appsettings.json
 dotnet restore
-dotnet build -c Release -f netcoreapp1.0
+dotnet build -c Release
 
-dotnet run -c Release server.urls=http://*:8080 scenarios=$1 server=kestrel threadCount=$threadCount NonInteractive=true &
+dotnet bin/Release/netcoreapp1.1/Benchmarks.dll server.urls=http://*:8080 scenarios=$1 server=kestrel threadCount=$threadCount NonInteractive=true &
--- frameworks/CSharp/aspnetcore/run-windows.ps1
+++ frameworks/CSharp/aspnetcore/run-windows.ps1
@@ -4,5 +4,5 @@ $scenarios = (-split $scenarios) -join ","
 
 cd Benchmarks
 dotnet restore
-dotnet build -c Release -f netcoreapp1.0
-Start-Process -NoNewWindow dotnet "run -c Release server.urls=http://*:8080 server=$server threadCount=1 NonInteractive=true scenarios=$scenarios"
+dotnet build -c Release
+Start-Process -NoNewWindow dotnet "bin\Release\netcoreapp1.1\Benchmarks.dll server.urls=http://*:8080 server=$server threadCount=1 NonInteractive=true scenarios=$scenarios"
