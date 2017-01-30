95a16408f3454efaded71f043e227ae36e7784c1
Samuel Colvin <samcolvin@gmail.com>
2017-01-30 14:40:40 +0000

    Python aiohttp (#2529)
    * starting work on python/aiohttp
    
    * adding more test endpoints
    
    * completing default tests
    
    * raw SQL endpoints
    
    * improve performance of raw updates for aiohttp
    
    * use prepared statements for repeated queries
    
    * setting port to 8080 to match others
    
    * adding aiohttp to travis
    
    * fix response with 'foo' queries and escaping

---
+++ frameworks/Python/aiohttp/gunicorn_conf.py
@@ -0,0 +1,14 @@
+import multiprocessing
+import os
+
+if os.environ.get('TRAVIS') == 'true':
+    workers = 2
+else:
+    workers = multiprocessing.cpu_count()
+
+bind = '0.0.0.0:8080'
+keepalive = 120
+errorlog = '-'
+pidfile = 'gunicorn.pid'
+
+worker_class = 'aiohttp.worker.GunicornUVLoopWebWorker'
