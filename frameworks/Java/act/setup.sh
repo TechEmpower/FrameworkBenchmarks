78ba074f7ccd9d7fdd132f6f07cb6ee59751ec7f
Gelin Luo <greenlaw110@gmail.com>
2017-01-30 13:30:55 +1100

    update actframework setup.sh to fix CI issue

--- frameworks/Java/act/setup.sh
+++ frameworks/Java/act/setup.sh
@@ -5,4 +5,16 @@ fw_depends mysql postgresql mongodb java maven
 mvn clean package
 cd target/dist
 unzip *.zip
-./start -Dmongo.host=${DBHOST} -Dmysql.host=${DBHOST} -Dpgsql.host=${DBHOST}
+
+
+APP_ENTRY=com.techempower.act.AppEntry
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BASE=$DIR
+CP=$BASE/classes:$BASE/lib/*
+echo CLASSPATH: $CP
+echo
+
+JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Dawt.useSystemAAFontSettings=on -server -Xms512m -Xmx2g"
+
+java -server $JAVA_OPTS -Dapp.mode=prod -Dapp.nodeGroup=$GROUP -Dprofile=prod -Dmongo.host=${DBHOST} -Dmysql.host=${DBHOST} -Dpgsql.host=${DBHOST} -cp "$CP" $APP_ENTRY &
