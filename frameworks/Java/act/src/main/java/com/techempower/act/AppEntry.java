dd3d049afcfccb2895f744096d0e14a7f9f4a553
Gelin Luo <greenlaw110@gmail.com>
2017-02-01 20:02:00 +1100

    use spark style API for /json endpoint

--- frameworks/Java/act/src/main/java/com/techempower/act/AppEntry.java
+++ frameworks/Java/act/src/main/java/com/techempower/act/AppEntry.java
@@ -1,10 +1,35 @@
 package com.techempower.act;
 
 
+import act.Act;
 import act.Version;
 import act.boot.app.RunApp;
+import act.job.OnAppStart;
+import com.alibaba.fastjson.JSON;
 
 public class AppEntry {
+
+	private static final String HELLO_WORLD = "Hello, World!";
+
+	public static final class Message {
+
+		private final String message;
+
+		private Message(String message) {
+			this.message = message;
+		}
+
+		public String getMessage() {
+			return message;
+		}
+
+	}
+
+
+	@OnAppStart
+	public void routing() {
+		Act.get("/json", context -> context.resp().writeContent(JSON.toJSONString(new Message(HELLO_WORLD))));
+	}
 
 	public static void main(String[] args) throws Exception {
 		RunApp.start("Act Test", Version.appVersion(), AppEntry.class);
