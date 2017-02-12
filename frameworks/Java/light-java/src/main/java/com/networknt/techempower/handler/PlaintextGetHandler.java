2817d149c785cab2581692506d0ccb8114823c7e
Steve Hu <stevehu@gmail.com>
2017-02-12 07:19:01 -0500

    upgrade to the latest undertow server and update plaintext

--- frameworks/Java/light-java/src/main/java/com/networknt/techempower/handler/PlaintextGetHandler.java
+++ frameworks/Java/light-java/src/main/java/com/networknt/techempower/handler/PlaintextGetHandler.java
@@ -12,23 +12,11 @@ import java.util.Map;
 import org.apache.commons.lang3.StringEscapeUtils;
 
 public class PlaintextGetHandler implements HttpHandler {
-    private static final ByteBuffer buffer;
     private static final String MESSAGE = "Hello, World!";
-
-    static {
-        buffer = ByteBuffer.allocateDirect(MESSAGE.length());
-        try {
-            buffer.put(MESSAGE.getBytes("US-ASCII"));
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-        buffer.flip();
-    }
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        exchange.getResponseHeaders().put(
-                Headers.CONTENT_TYPE, "text/plain");
-        exchange.getResponseSender().send(buffer.duplicate());
+        exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+        exchange.getResponseSender().send(MESSAGE);
     }
 }
