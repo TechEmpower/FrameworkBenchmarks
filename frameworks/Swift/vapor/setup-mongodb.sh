caf961a676ddc16d0e7a62e8546f0de752850994
Evgeny Kazakov <azeffin@gmail.com>
2017-02-16 01:25:20 +0300

    No need to insall libmysqlclient-dev package in setup scripts, since it's already specified in prerequisites.sh

--- frameworks/Swift/vapor/setup-mongodb.sh
+++ frameworks/Swift/vapor/setup-mongodb.sh
@@ -2,8 +2,6 @@
 
 fw_depends swift3 mongodb mysql postgresql
 
-sudo apt-get install -y libmysqlclient-dev
-
 swift build -Xswiftc -DNOJSON -c release
 
 .build/release/vapor-tfb-mongodb --env=production &
