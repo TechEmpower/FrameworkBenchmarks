b82f570f1b9dd8ed40608db42b57d70a32bfe01a
Steve Hu <stevehu@gmail.com>
2017-02-14 19:29:30 -0500

    hard coded database host

--- frameworks/Java/light-java/setup.sh
+++ frameworks/Java/light-java/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-sed -i 's|DATABASE_HOST|'"${DBHOST}"'|g' src/main/resources/config/mysql.json
-sed -i 's|DATABASE_HOST|'"${DBHOST}"'|g' src/main/resources/config/postgres.json
-
 fw_depends postgresql mysql java maven
 
 mvn clean package
