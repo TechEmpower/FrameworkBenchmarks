9e85717cc33babab69ae1129826fab39b9fb6248
Daniel Nicoletti <dantti12@gmail.com>
2017-02-17 13:30:07 -0200

    Update cutelyst 1 4 0 (#2571)
    * cutelyst: Use a mutex locker for all database types
    
    QSqlDatabase::addDatabase is not thread-safe, so
    using a QMutexLocker for all databases is safer and
    is only used when connecting the first time to the
    database.
    
    * cutelyst: update to cutelyst 1.4.0 and enable jemalloc
    
    * cutelyst: Adjust cutelyst-wsgi to 1.4.0 and add tests with Epoll

--- toolset/setup/linux/frameworks/cutelyst.sh
+++ toolset/setup/linux/frameworks/cutelyst.sh
@@ -2,17 +2,26 @@
 
 fw_installed cutelyst && return 0
 
-CUTELYST_VER=r1.3.0
+CUTELYST_VER=r1.4.0
 QT_VERSION_MM=56
 QT_VERSION_FULL=562-trusty
 
 sudo apt-add-repository --yes ppa:george-edison55/cmake-3.x
 sudo apt-add-repository --yes ppa:beineri/opt-qt$QT_VERSION_FULL
 sudo apt-get update -qq
-sudo apt-get install -qqy cmake
-sudo apt-get install -qqy uwsgi uuid-dev libcap-dev libzmq3-dev
-sudo apt-get install -qqy clearsilver-dev 
-sudo apt-get install -qqy qt${QT_VERSION_MM}base qt${QT_VERSION_MM}script qt${QT_VERSION_MM}tools
+
+sudo apt-get install -qqy \
+cmake \
+uwsgi \
+uuid-dev \
+libcap-dev \
+libzmq3-dev \
+clearsilver-dev \
+libjemalloc-dev \
+qt${QT_VERSION_MM}base \
+qt${QT_VERSION_MM}script \
+qt${QT_VERSION_MM}tools
+
 export CMAKE_PREFIX_PATH=/opt/qt${QT_VERSION_MM};
 
 fw_get -O https://github.com/cutelyst/cutelyst/archive/$CUTELYST_VER.tar.gz
@@ -21,7 +30,7 @@ fw_untar $CUTELYST_VER.tar.gz
 cd cutelyst-$CUTELYST_VER
 mkdir build && cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$IROOT
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$IROOT -DUSE_JEMALLOC=on
 
 make -j $MAX_THREADS && sudo make install
 
