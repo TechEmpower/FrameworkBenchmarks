fc7cd876fb336099fcecf74576c9037bbc231d12
Michael Hixson <mhixson@techempower.com>
2017-03-06 14:05:41 -0800

    Simplify post-run steps for continuous benchmarking
    Put the entire results directory into a zip archive on the local disk.
    Preserve the directory structure in the archive (previously the archive
    was flat) and actually compress the contents (previous they were not
    compressed).  Do not attempt to send an email.

--- toolset/lifecycle/post-run-tests/keep-logs.py
+++ toolset/lifecycle/post-run-tests/keep-logs.py
@@ -16,29 +16,19 @@ import shutil
 #
 path_in = os.path.abspath(os.path.normpath(os.path.expanduser(os.path.join( \
     os.environ['TFB_REPOPARENT'], os.environ['TFB_REPONAME'], \
-    'results/latest/logs'))))
+    'results'))))
 date_time = datetime.datetime.now()
 dt_folder = date_time.strftime('%Y%m%d%H%M%S')
 path_out = os.path.abspath(os.path.join(os.environ['TFB_LOGSFOLDER'], \
     dt_folder))
-# Step through each folder in the TFB log folder...
-for folder in os.listdir(path_in):
-  if not os.path.exists(path_out):
-    os.makedirs(path_out)
-  zip_file = zipfile.ZipFile(path_out + '/' + folder + '.zip', 'w')
-# ... walk the folder structure ...
-  for root, directories, files in os.walk(os.path.join(path_in, folder), 'w', \
-        zipfile.ZIP_DEFLATED):
-# ... and add to the zip file.   
-    for file in files:
-      try:
-        zip_file.write(os.path.abspath(os.path.join(root, file)), \
-            arcname=file)
-      except OSError as err:
-        print "An OSError occurred while writing to a log zip file for {0}: \
-            {1}".format(file, err)
-  zip_file.close()
-path_results_in = os.path.abspath(os.path.normpath(os.path.expanduser(os.path.join( \
-    os.environ['TFB_REPOPARENT'], os.environ['TFB_REPONAME'], \
-    'results'))))
-shutil.copytree(path_results_in, path_out + '/results')
+
+if not os.path.exists(path_out):
+  os.makedirs(path_out)
+
+zip_file = zipfile.ZipFile(path_out + '/results.zip', 'w', zipfile.ZIP_DEFLATED)
+
+for root, dirs, files in os.walk(path_in):
+  for file in files:
+    zip_file.write(os.path.join(root, file))
+
+zip_file.close()
