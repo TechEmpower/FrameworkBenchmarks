78cddf9637fd8094e4eb2670c1fbbd29aad57ced
Sergey Lanzman <Sergeylanz@gettaxi.com>
2017-03-23 15:59:29 +0200

    add glide.lock

--- frameworks/Go/beego/setup.sh
+++ frameworks/Go/beego/setup.sh
@@ -7,5 +7,9 @@ fw_depends mysql go
 go get -u github.com/astaxie/beego
 go get -u github.com/go-sql-driver/mysql
 
+curl https://glide.sh/get | sh
+glide -v
+
 cd src/hello
+glide install
 go run main.go &
