c23e0222ecf30547f6bc267b4668a3757db5e60e
Michael Hixson <mhixson@techempower.com>
2017-03-23 12:38:16 -0700

    Isolate cutelyst install to installs/cutelyst, let it use sudo again
    The use of sudo was breaking other frameworks because it created
    root-owned directories in installs that the other frameworks could not
    use.  However, removing sudo broke the cutelyst install.
    
    This change lets cutelyst use sudo again but puts all of the installed
    files in installs/cutelyst, meaning that it should no longer break other
    frameworks (tested by running "toolset/run-tests.py --mode verify --test
    cutelyst ffead-cpp").

--- frameworks/C++/cutelyst/config.sh
+++ frameworks/C++/cutelyst/config.sh
@@ -21,40 +21,41 @@ elif [ "${DRIVER}" == "QPSQL" ]; then
   fw_depends postgresql
 fi
 
+CROOT=${IROOT}/cutelyst
 
-mkdir ${IROOT}/cutelyst-benchmarks || true
-cd ${IROOT}/cutelyst-benchmarks
+mkdir -p ${CROOT}/benchmarks || true
+cd ${CROOT}/benchmarks
 
 # build
-export CMAKE_PREFIX_PATH=/opt/qt${QT_VERSION_MM}:${IROOT}
-cmake $TROOT -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$IROOT
+export CMAKE_PREFIX_PATH=/opt/qt${QT_VERSION_MM}:${CROOT}
+cmake $TROOT -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CROOT
 make -j $MAX_THREADS
 
 if [ -n "${UWSGI}" ]; then
-  cp -v ${TROOT}/config/config_socket.ini ${IROOT}/config.ini
+  cp -v ${TROOT}/config/config_socket.ini ${CROOT}/config.ini
   SEND_DATE=true
 else
-  cp -v ${TROOT}/config/config.ini ${IROOT}/config.ini
+  cp -v ${TROOT}/config/config.ini ${CROOT}/config.ini
   SEND_DATE=false
 fi
 
-sed -i "s|Driver=.*|Driver=${DRIVER}|g" ${IROOT}/config.ini
-sed -i "s|DatabaseHostName=.*|DatabaseHostName=${DBHOST}|g" ${IROOT}/config.ini
-sed -i "s|SendDate=.*|SendDate=${SEND_DATE}|g" ${IROOT}/config.ini
+sed -i "s|Driver=.*|Driver=${DRIVER}|g" ${CROOT}/config.ini
+sed -i "s|DatabaseHostName=.*|DatabaseHostName=${DBHOST}|g" ${CROOT}/config.ini
+sed -i "s|SendDate=.*|SendDate=${SEND_DATE}|g" ${CROOT}/config.ini
 
-export LD_LIBRARY_PATH=/opt/qt${QT_VERSION_MM}/lib:${IROOT}/lib/x86_64-linux-gnu/
+export LD_LIBRARY_PATH=/opt/qt${QT_VERSION_MM}/lib:${CROOT}/lib/x86_64-linux-gnu/
 
 if [ -n "${UWSGI}" ]; then
-  uwsgi --ini ${IROOT}/config.ini --cutelyst-app ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $MAX_THREADS &
+  uwsgi --ini ${CROOT}/config.ini --cutelyst-app ${CROOT}/benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $MAX_THREADS &
 else
-  ${IROOT}/bin/cutelyst-wsgi --ini ${IROOT}/config.ini -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $MAX_THREADS --socket-timeout 0 &
+  ${CROOT}/bin/cutelyst-wsgi --ini ${CROOT}/config.ini -a ${CROOT}/benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $MAX_THREADS --socket-timeout 0 &
 fi
 
 # configure Nginx
 if [ -n "${NGINX}" ]; then
   fw_depends nginx
-  cp -v ${TROOT}/nginx.conf ${IROOT}/nginx.conf
-  sed -i "s|include .*/conf/uwsgi_params;|include ${NGINX_HOME}/conf/uwsgi_params;|g" ${IROOT}/nginx.conf
-  nginx -c ${IROOT}/nginx.conf
+  cp -v ${TROOT}/nginx.conf ${CROOT}/nginx.conf
+  sed -i "s|include .*/conf/uwsgi_params;|include ${NGINX_HOME}/conf/uwsgi_params;|g" ${CROOT}/nginx.conf
+  nginx -c ${CROOT}/nginx.conf
 fi
 
