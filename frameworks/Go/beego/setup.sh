3c8d2765e94a816be9964fc9809e551b81bca3e3
Sergey Lanzman <Sergeylanz@gettaxi.com>
2017-03-24 12:02:24 +0300

    code review fix

--- frameworks/Go/beego/setup.sh
+++ frameworks/Go/beego/setup.sh
@@ -4,9 +4,6 @@ sed -i 's|tcp(.*:3306)|tcp('"${DBHOST}"':3306)|g' src/hello/models/init.go
 
 fw_depends mysql go
 
-go get -u github.com/astaxie/beego
-go get -u github.com/go-sql-driver/mysql
-
 curl https://glide.sh/get | sh
 glide -v
 
