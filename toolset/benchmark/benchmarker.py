2d075e05ce5351efa4531c1cd998f6bf507ba858
Michael Hixson <mhixson@techempower.com>
2017-03-28 11:05:48 -0700

    Add a startTime field to results.json
    We'd like to use this for computing the elapsed duration and estimated
    completion time of a run during continuous benchmarking.

--- toolset/benchmark/benchmarker.py
+++ toolset/benchmark/benchmarker.py
@@ -1042,6 +1042,7 @@ class Benchmarker:
             self.results['uuid'] = str(uuid.uuid4())
             self.results['name'] = datetime.now().strftime(self.results_name)
             self.results['environmentDescription'] = self.results_environment
+            self.results['startTime'] = int(round(time.time() * 1000))
             self.results['completionTime'] = None
             self.results['concurrencyLevels'] = self.concurrency_levels
             self.results['queryIntervals'] = self.query_levels
