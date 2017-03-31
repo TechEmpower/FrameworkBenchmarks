84d97ea28a8b11d8da9932adf75a1b4cf220c4c7
Michael Hixson <michael.hixson@gmail.com>
2017-03-31 06:26:08 -0700

    Upgrade to wt 3.3.6, fixing the wt tests (#2639)
    The sourceforge.net link for wt 3.3.3 is broken, causing the wt tests to
    fail.
    
    Adding the "-lboost_system" flag in the setup files seemed to be
    necessary.  Without it, the wt framework would install, but the tests
    would fail.  I found out about that flag by googling for the error
    message; I don't actually know what the flag does.
    
    I removed the trailing "&" from the final line of the setup files as
    I've been doing with other frameworks, since it has become unnecessary
    (and even harmful sometimes) due to other changes in our toolset.

--- frameworks/C++/wt/setup_postgres.sh
+++ frameworks/C++/wt/setup_postgres.sh
@@ -4,6 +4,6 @@ fw_depends postgresql apache wt
 
 sed -i 's|INSERT_DB_HOST_HERE|'"${DBHOST}"'|g' benchmark.cpp
 
-g++-4.8 -O3 -DNDEBUG -DBENCHMARK_USE_POSTGRES -std=c++0x -L${BOOST_LIB} -I${BOOST_INC} -L${WT_LIB} -I${WT_INC} -o benchmark_postgres.wt benchmark.cpp -lwt -lwthttp -lwtdbo -lwtdbopostgres -lboost_thread
+g++-4.8 -O3 -DNDEBUG -DBENCHMARK_USE_POSTGRES -std=c++0x -L${BOOST_LIB} -I${BOOST_INC} -L${WT_LIB} -I${WT_INC} -o benchmark_postgres.wt benchmark.cpp -lwt -lwthttp -lwtdbo -lwtdbopostgres -lboost_thread -lboost_system
 
-./benchmark_postgres.wt -c wt_config.xml -t ${MAX_THREADS} --docroot . --http-address 0.0.0.0 --http-port 8080 --accesslog=- --no-compression &
+./benchmark_postgres.wt -c wt_config.xml -t ${MAX_THREADS} --docroot . --http-address 0.0.0.0 --http-port 8080 --accesslog=- --no-compression
