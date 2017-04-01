3834c50279cc908f793edb01561cd5ebde6a5149
zloster <more@edno.moe>
2017-04-01 14:03:01 +0300

    Add common JVM settings to toolset->java.sh Modify Dropwizard, Jetty and Undertow start scripts to use them

--- frameworks/Java/dropwizard/setup_mongo.sh
+++ frameworks/Java/dropwizard/setup_mongo.sh
@@ -6,4 +6,4 @@ sed -i 's|host: 127.0.0.1|host: '"${DBHOST}"'|g' hello-world-mongo.yml
 
 mvn -P mongo clean package
 
-java -jar target/hello-world-0.0.1-SNAPSHOT.jar server hello-world-mongo.yml &
+java -server $JAVA_OPTS -jar target/hello-world-0.0.1-SNAPSHOT.jar server hello-world-mongo.yml &
