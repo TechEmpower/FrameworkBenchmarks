2e28aff10002882b57d35b870b9d8d5bfd45c623
Sergey Mashkov <sergey.mashkov@jetbrains.com>
2017-04-09 19:25:51 +0300

    ktor: upgrade to latest Kotlin and ktor

--- frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
+++ frameworks/Kotlin/ktor/src/main/kotlin/org/jetbrains/ktor/benchmarks/Hello.kt
@@ -3,16 +3,16 @@ package org.jetbrains.ktor.benchmarks
 import com.google.gson.*
 import com.mchange.v2.c3p0.*
 import com.mysql.cj.jdbc.*
-import kotlinx.support.jdk7.*
 import org.jetbrains.ktor.application.*
+import org.jetbrains.ktor.content.*
 import org.jetbrains.ktor.features.*
-import org.jetbrains.ktor.features.http.*
+import org.jetbrains.ktor.host.*
 import org.jetbrains.ktor.http.*
 import org.jetbrains.ktor.netty.*
 import org.jetbrains.ktor.routing.*
-import org.jetbrains.ktor.transform.*
 import org.jetbrains.ktor.util.*
-import java.sql.ResultSet.*
+import java.sql.ResultSet.CONCUR_READ_ONLY
+import java.sql.ResultSet.TYPE_FORWARD_ONLY
 import java.util.*
 
 data class Message(val message: String = "Hello, World!")
@@ -31,73 +31,74 @@ fun main(args: Array<String>) {
         password = b
     }
 
-    embeddedNettyServer(9090) {
-        application.install(DefaultHeaders)
+    embeddedServer(Netty, 9090) {
+        install(DefaultHeaders)
+        routing {
+            get("/plaintext") { call ->
+                call.respond(TextContent("Hello, World!", ContentType.Text.Plain, HttpStatusCode.OK))
+            }
+            get("/json") { call ->
+                call.respond(TextContent(gson.toJson(Message()), ContentType.Application.Json, HttpStatusCode.OK))
+            }
+            get("/db") { call ->
+                pool.connection.use { connection ->
+                    val queries = call.queries()
+                    val random = Random()
+                    val result = mutableListOf<World>()
 
-        get("/plaintext") { call ->
-            call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Text.Plain, "Hello, World!"))
-        }
-        get("/json") { call ->
-            call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Application.Json, gson.toJson(Message())))
-        }
-        get("/db") { call ->
-            pool.connection.use { connection ->
-                val queries = call.queries()
-                val random = Random()
-                val result = mutableListOf<World>()
+                    connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
+                        for (i in 1..(queries ?: 1)) {
+                            statement.setInt(1, random.nextInt(DbRows) + 1)
 
-                connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
-                    for (i in 1..(queries ?: 1)) {
-                        statement.setInt(1, random.nextInt(DbRows) + 1)
-
-                        statement.executeQuery().use { rs ->
-                            while (rs.next()) {
-                                result += World(rs.getInt("id"), rs.getInt("randomNumber"))
+                            statement.executeQuery().use { rs ->
+                                while (rs.next()) {
+                                    result += World(rs.getInt("id"), rs.getInt("randomNumber"))
+                                }
                             }
                         }
-                    }
 
-                    call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Application.Json, gson.toJson(when (queries) {
-                        null -> result.single()
-                        else -> result
-                    })))
+                        call.respond(TextContent(gson.toJson(when (queries) {
+                            null -> result.single()
+                            else -> result
+                        }), ContentType.Application.Json, HttpStatusCode.OK))
+                    }
                 }
             }
-        }
-        get("/updates") {
-            pool.connection.use { connection ->
-                val queries = call.queries()
-                val random = Random()
-                val result = mutableListOf<World>()
+            get("/updates") {
+                pool.connection.use { connection ->
+                    val queries = call.queries()
+                    val random = Random()
+                    val result = mutableListOf<World>()
 
-                connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
-                    for (i in 1..(queries ?: 1)) {
-                        statement.setInt(1, random.nextInt(DbRows) + 1)
+                    connection.prepareStatement("SELECT * FROM World WHERE id = ?", TYPE_FORWARD_ONLY, CONCUR_READ_ONLY).use { statement ->
+                        for (i in 1..(queries ?: 1)) {
+                            statement.setInt(1, random.nextInt(DbRows) + 1)
 
-                        statement.executeQuery().use { rs ->
-                            while (rs.next()) {
-                                result += World(rs.getInt("id"), rs.getInt("randomNumber"))
+                            statement.executeQuery().use { rs ->
+                                while (rs.next()) {
+                                    result += World(rs.getInt("id"), rs.getInt("randomNumber"))
+                                }
                             }
+                        }
+
+                    }
+
+                    result.forEach { it.randomNumber = random.nextInt(DbRows) + 1 }
+
+                    connection.prepareStatement("UPDATE World SET randomNumber = ? WHERE id = ?").use { updateStatement ->
+                        for ((id, randomNumber) in result) {
+                            updateStatement.setInt(1, randomNumber)
+                            updateStatement.setInt(2, id)
+
+                            updateStatement.executeUpdate()
                         }
                     }
 
+                    call.respond(TextContent(gson.toJson(when (queries) {
+                        null -> result.single()
+                        else -> result
+                    }), ContentType.Application.Json, HttpStatusCode.OK))
                 }
-
-                result.forEach { it.randomNumber = random.nextInt(DbRows) + 1 }
-
-                connection.prepareStatement("UPDATE World SET randomNumber = ? WHERE id = ?").use { updateStatement ->
-                    for ((id, randomNumber) in result) {
-                        updateStatement.setInt(1, randomNumber)
-                        updateStatement.setInt(2, id)
-
-                        updateStatement.executeUpdate()
-                    }
-                }
-
-                call.respond(TextContentResponse(HttpStatusCode.OK, ContentType.Application.Json, gson.toJson(when (queries) {
-                    null -> result.single()
-                    else -> result
-                })))
             }
         }
     }.start(true)
