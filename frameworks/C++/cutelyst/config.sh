084eadfcb584c3d578e120304e1eee78499f9b54
Nate Brady <nbrady@techempower.com>
2017-04-13 13:08:22 -0700

    some fixes

--- frameworks/C++/cutelyst/config.sh
+++ frameworks/C++/cutelyst/config.sh
@@ -28,10 +28,9 @@ mkdir -p ${CROOT}/benchmarks || true
 cd ${CROOT}/benchmarks
 
 # build
-export CMAKE_PREFIX_PATH=/opt/qt${QT_VERSION_MM}:${IROOT}
-cmake $TROOT -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$IROOT
+export CMAKE_PREFIX_PATH=/opt/qt${QT_VERSION_MM}:${CROOT}
+cmake $TROOT -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$CROOT
 make -j $CPU_COUNT
-
 
 if [ -n "${UWSGI}" ]; then
   cp -v ${TROOT}/config/config_socket.ini ${CROOT}/config.ini
@@ -48,9 +47,9 @@ sed -i "s|SendDate=.*|SendDate=${SEND_DATE}|g" ${CROOT}/config.ini
 export LD_LIBRARY_PATH=/opt/qt${QT_VERSION_MM}/lib:${CROOT}/lib/x86_64-linux-gnu/
 
 if [ -n "${UWSGI}" ]; then
-  uwsgi --ini ${IROOT}/config.ini --cutelyst-app ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $CPU_COUNT &
+  uwsgi --ini ${CROOT}/config.ini --plugin ${CROOT}/lib/uwsgi/plugins/cutelyst_plugin.so --cutelyst-app ${CROOT}/benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $CPU_COUNT &
 else
-  ${IROOT}/bin/cutelyst-wsgi --ini ${IROOT}/config.ini -a ${IROOT}/cutelyst-benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $CPU_COUNT --socket-timeout 0 &
+  ${CROOT}/bin/cutelyst-wsgi --ini ${CROOT}/config.ini -a ${CROOT}/benchmarks/src/libcutelyst_benchmarks.so ${PROCESS_OR_THREAD} $CPU_COUNT --socket-timeout 0 ${BALANCER} &
 fi
 
 # configure Nginx
@@ -60,4 +59,3 @@ if [ -n "${NGINX}" ]; then
   sed -i "s|include .*/conf/uwsgi_params;|include ${NGINX_HOME}/conf/uwsgi_params;|g" ${CROOT}/nginx.conf
   nginx -c ${CROOT}/nginx.conf
 fi
-
