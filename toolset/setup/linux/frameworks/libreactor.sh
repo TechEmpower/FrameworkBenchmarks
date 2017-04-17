b702c46ea10d8e9a9ae50c23b7e2dc85c7f6f755
Fredrik Widlund <fredrik.widlund@gmail.com>
2017-04-17 21:59:15 +0000

    Fix serious socket leakage introduced by optimization

--- toolset/setup/linux/frameworks/libreactor.sh
+++ toolset/setup/linux/frameworks/libreactor.sh
@@ -9,7 +9,7 @@ LIBREACTOR_HOME=$IROOT/libreactor_techempower
 git clone https://github.com/fredrikwidlund/libreactor_techempower
 cd $LIBREACTOR_HOME
 # 4/14/2017
-git checkout 675ccca0fb74a5161f8c0eae806d9a275b63a50c
+git checkout c96da1fa46d3de3618755d8b578b6b4db48ccd3b
 export CC=gcc-4.9
 export RANLIB=gcc-ranlib-4.9
 export AR=gcc-ar-4.9
