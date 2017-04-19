92f4c1d4bbf20164be6e4f0c2644a1f5eb65efa5
Don Goodman-Wilson <don@slack-corp.com>
2017-04-19 15:41:37 +0200

    Fix the luna setup.sh file to point to the correctl binary. Unsure how this slipped past Travis testing the first time!

--- frameworks/C++/luna/setup.sh
+++ frameworks/C++/luna/setup.sh
@@ -6,4 +6,4 @@ CC=gcc-4.9 CXX=g++-4.9 conan install --build=missing -s compiler="gcc" -s compil
 cmake . -DCMAKE_CXX_COMPILER=g++-4.9 -DCMAKE_CC_COMPILER=gcc-4.9
 cmake --build .
 
-$TROOT/bin/lunabench 8080 $MAX_THREADS
+$TROOT/bin/lunabench_default 8080 $MAX_THREADS
