3d5f6c341eff647dbab0c98defe2a04d7e477d69
Radoslav Petrov <rrpetrov@gmail.com>
2017-04-22 22:27:08 +0300

    Revert "Adjusting JVM parameters for Dropwizard, Jetty, Resin, Undertow (#2652)" (#2707)
    This reverts commit 18e0797cd82438d4d37e0bc964dcc22dc48c0cb0.

--- frameworks/Java/jetty-servlet/setup.sh
+++ frameworks/Java/jetty-servlet/setup.sh
@@ -5,5 +5,4 @@ fw_depends java maven
 mvn clean compile assembly:single
 
 cd target
-
-java -server $JAVA_OPTS_TFB -jar jetty-servlet-example-0.2-jar-with-dependencies.jar &
+java -jar jetty-servlet-example-0.2-jar-with-dependencies.jar &
--- frameworks/Java/jetty/setup.sh
+++ frameworks/Java/jetty/setup.sh
@@ -5,5 +5,4 @@ fw_depends java maven
 mvn clean compile assembly:single
 
 cd target
-
-java -server $JAVA_OPTS_TFB -jar jetty-example-0.1-jar-with-dependencies.jar
+java -jar jetty-example-0.1-jar-with-dependencies.jar
