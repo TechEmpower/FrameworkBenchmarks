6bcc3a439014c90e29ff70d0d650350fcc199801
joanhey <joanhey@kumbiaphp.com>
2017-04-27 21:46:33 +0200

    Update yaf

--- toolset/setup/linux/frameworks/yaf.sh
+++ toolset/setup/linux/frameworks/yaf.sh
@@ -6,7 +6,7 @@ fw_installed yaf && return 0
 
 # Enable the PHP yaf extension
 sed -i 's|;extension=yaf.so|extension=yaf.so|g' $PHP_HOME/lib/php.ini
-printf "\n" | $PHP_HOME/bin/pecl -q install -f yaf-2.2.9
+printf "\n" | $PHP_HOME/bin/pecl -q install -f yaf-2.3.3
 
 echo "" > $IROOT/yaf.installed
 
