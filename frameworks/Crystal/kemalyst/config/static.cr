ae54f49985555c28bf0aa6f3b315d3d8b7dcca51
Dru Jensen <drujensen@gmail.com>
2017-05-15 09:42:27 -0700

    remove unneeded files

--- frameworks/Crystal/kemalyst/config/static.cr
+++
@@ -1,4 +0,0 @@
-Kemalyst::Handler::Static.config do |config|
-  # set the public folder. Defaults to `/public`
-  # config.public_folder = "./public"
-end
--- frameworks/Crystal/kemalyst/spec/models/fortune_spec.cr
+++
@@ -1,8 +0,0 @@
-require "./spec_helper"
-require "../../src/models/fortune.cr"
-
-describe Fortune do
-  Spec.before_each do
-    Fortune.clear
-  end
-end
