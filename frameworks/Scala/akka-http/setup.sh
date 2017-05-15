be728f0a8d9c5958a8ce876fe5a7b827bd825ae1
Nate Brady <nbrady@techempower.com>
2017-05-15 09:03:31 -0700

    merge conflicts

--- frameworks/Scala/akka-http/setup.sh
+++ frameworks/Scala/akka-http/setup.sh
@@ -4,4 +4,4 @@ fw_depends mysql java sbt
 
 sbt 'assembly' -batch
 
-java -server -jar target/scala-2.11/akka-http-benchmark.jar &
+java -server -Dakka.http.benchmark.mysql.dbhost=$DBHOST -jar target/scala-2.12/akka-http-benchmark.jar &
