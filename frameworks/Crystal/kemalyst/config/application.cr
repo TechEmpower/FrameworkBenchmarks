fd2a004324fc2b5b9082aab2f739d974b13734b2
Dru Jensen <drujensen@gmail.com>
2017-05-15 20:00:42 -0700

    Kemalyst crystal pg full (#2784)
    * initial checkin of kemalyst
    
    * passing version
    
    * add kemalyst to travis.yml
    
    * remove unneeded files
    
    * cleanup routes
    
    * replace 127.0.0.1 with TFB-database

---
+++ frameworks/Crystal/kemalyst/config/application.cr
@@ -0,0 +1,19 @@
+require "../src/controllers/test_controller"
+
+Kemalyst::Application.config do |config|
+  # Set the binding host ip address.  Defaults to "0.0.0.0"
+  # config.host = "0.0.0.0"
+
+  # Set the port.  Defaults to 3000.
+  # config.port = 3000
+
+  # Configure reuse_port option
+  config.reuse_port = true
+
+  # Disable unused middleware
+  config.handlers = [] of HTTP::Handler
+  config.handlers << Kemalyst::Handler::Error.instance
+  config.handlers << Kemalyst::Handler::Params.instance
+  config.handlers << Kemalyst::Handler::Router.instance
+  config.handlers << TestController.instance
+end
