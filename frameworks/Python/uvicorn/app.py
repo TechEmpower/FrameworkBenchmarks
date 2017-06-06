02870c4bcb62e66c751d8c42f266c184f6fa632e
Tom Christie <tom@tomchristie.com>
2017-06-06 13:57:27 +0100

    Added uvicorn, an asyncio server for Python 3.

---
+++ frameworks/Python/uvicorn/app.py
@@ -0,0 +1,48 @@
+import ujson as json
+
+
+def json_endpoint(message):
+    content = json.dumps({'message': 'Hello, world!'}).encode('utf-8')
+    response = {
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+        'content': content
+    }
+    message['reply_channel'].send(response)
+
+
+def plaintext_endpoint(message):
+    content = b'Hello, world!'
+    response = {
+        'status': 200,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+        'content': content
+    }
+    message['reply_channel'].send(response)
+
+
+def handle_404(message):
+    content = b'Not found'
+    response = {
+        'status': 404,
+        'headers': [
+            [b'content-type', b'text/plain'],
+        ],
+        'content': content
+    }
+    message['reply_channel'].send(response)
+
+
+routes = {
+    '/json': json_endpoint,
+    '/plaintext': plaintext_endpoint
+}
+
+
+def main(message):
+    path = message['content']['path']
+    routes.get(path, handle_404)(message)
