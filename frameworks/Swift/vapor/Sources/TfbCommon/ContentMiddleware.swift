a8ad5c09ab8412304fbd5098ebe45bad78441f52
Kim de Vo <kv@idealdev.dk>
2017-06-07 09:12:29 +0200

    ContentMiddleware fixes

--- frameworks/Swift/vapor/Sources/TfbCommon/ContentMiddleware.swift
+++ frameworks/Swift/vapor/Sources/TfbCommon/ContentMiddleware.swift
@@ -8,11 +8,7 @@ public final class ContentMiddleware: Middleware {
     public func respond(to request: Request, chainingTo next: Responder) throws -> Response {
         let response = try next.respond(to: request)
         
-        if response.headers[HeaderKey.contentType] == "text/plain; charset=utf-8" {
-            response.headers[HeaderKey.contentType] = "text/plain"
-        } else if response.headers[HeaderKey.contentType] == "application/json; charset=utf-8" {
-            response.headers[HeaderKey.contentType] = "application/json"
-        }
+        response.headers[HeaderKey.contentType] = response.headers[HeaderKey.contentType]?.replacingOccurrences(of: "; charset=utf-8", with: "")
         
         return response
     } 
