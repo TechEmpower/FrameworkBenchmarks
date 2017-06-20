76a4c7919f274045b83932488bda56c53da87de8
zloster <rado@edno.moe>
2017-06-20 23:26:19 +0300

    Call correct JAR name

--- frameworks/Java/jetty/setup_servlet.sh
+++ frameworks/Java/jetty/setup_servlet.sh
@@ -5,4 +5,4 @@ fw_depends java maven
 mvn -P servlet clean compile assembly:single
 
 cd target
-java -XX:+UseNUMA -XX:+UseParallelGC -jar jetty-servlet-example-0.2-jar-with-dependencies.jar &
+java -XX:+UseNUMA -XX:+UseParallelGC -jar jetty-example-0.1-jar-with-dependencies.jar &
