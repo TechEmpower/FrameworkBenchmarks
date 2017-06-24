42a7e1d0edbc30b0682a17b0b5c45c7430b55c6c
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2017-06-24 21:39:35 +0530

    Adding tests for ffead-cpp (mongodb, mysql, postgresql) on - Embedded server - Apache - Nginx Resolves https://github.com/TechEmpower/FrameworkBenchmarks/issues/2554

---
+++ frameworks/C++/ffead-cpp/setup-apache-mysql.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fw_depends ffead-cpp-apache-mysql
+
+export FFEAD_CPP_PATH=$IROOT/ffead-cpp-2.0
+export LD_LIBRARY_PATH=$IROOT:$FFEAD_CPP_PATH/lib:$LD_LIBRARY_PATH
+echo $FFEAD_CPP_PATH
+echo $LD_LIBRARY_PATH
+rm -f $FFEAD_CPP_PATH/*.cntrl
+rm -f $FFEAD_CPP_PATH/tmp/*.sess
+${IROOT}/httpd/bin/apachectl restart > ffead.log 2>&1
---
+++ toolset/setup/linux/frameworks/ffead-cpp-framework.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+fw_installed ffead-cpp-framework && return 0
+
+fw_get -o ffead-cpp-src.zip https://github.com/sumeetchhetri/ffead-cpp/archive/master.zip
+rm -rf ffead-cpp-src
+rm -rf ffead-cpp-master
+unzip ffead-cpp-src.zip
+mv ffead-cpp-master ffead-cpp-src
+cd ffead-cpp-src/
+chmod 755 *.sh resources/*.sh rtdcf/autotools/*.sh
+./autogen.sh
+./configure --enable-apachemod=yes --enable-mod_sdormmongo=yes --enable-mod_sdormsql=yes CPPFLAGS="$CPPFLAGS -I${IROOT}/include/libmongoc-1.0 -I${IROOT}/include/libbson-1.0 -I${IROOT}/include/" LDFLAGS="$LDFLAGS -L${IROOT}"
+make install
+rm -rf web/default web/oauthApp web/flexApp web/markers
+rm -rf ${IROOT}/ffead-cpp-2.0
+cp -rf ffead-cpp-2.0-bin ${IROOT}/ffead-cpp-2.0
+
+sed -i 's|localhost|'${DBHOST}'|g' web/te-benchmark/config/sdorm.xml
+sed -i 's|localhost|'${DBHOST}'|g' web/te-benchmark/config/sdormmongo.xml
+sed -i 's|localhost|'${DBHOST}'|g' web/te-benchmark/config/sdormmysql.xml
+sed -i 's|localhost|'${DBHOST}'|g' web/te-benchmark/config/sdormpostgresql.xml
+sed -i 's|localhost|'${DBHOST}'|g' resources/sample-odbcinst.ini
+sed -i 's|localhost|'${DBHOST}'|g' resources/sample-odbc.ini
+
+cp resources/sample-odbcinst.ini ${IROOT}/odbcinst.ini
+cp resources/sample-odbc.ini ${IROOT}/odbc.ini
+
+cd ${IROOT}
+
+touch ${IROOT}/ffead-cpp-framework.installed
