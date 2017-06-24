b000f70448f515dc598ef887a404ec0886e9da5f
Dru Jensen <drujensen@gmail.com>
2017-06-24 08:04:57 -0700

    update to latest kemalyst

--- frameworks/Crystal/kemalyst/config/routes.cr
+++ frameworks/Crystal/kemalyst/config/routes.cr
@@ -2,9 +2,16 @@ require "../src/controllers/*"
 
 include Kemalyst::Handler
 
-get "/plaintext", test, plaintext
-get "/json", test, json
-get "/db", test, db
-get "/queries", test, queries
-get "/updates", test, updates
-get "/fortunes", test, fortunes
+get "/plaintext", TestController::Plaintext.new
+get "/json", TestController::Json.new
+get "/db", TestController::Db.new
+get "/queries", TestController::Queries.new
+get "/updates", TestController::Updates.new
+get "/fortunes", TestController::Fortunes.new
+
+# get "/plaintext", "benchmark", "plaintext"
+# get "/json", "benchmark", "json"
+# get "/db", "benchmark", "db"
+# get "/queries", "benchmark", "queries"
+# get "/updates", "benchmark", "updates"
+# get "/fortunes", "benchmark", "fortunes"
--- frameworks/Crystal/kemalyst/src/controllers/test_controller.cr
+++ frameworks/Crystal/kemalyst/src/controllers/test_controller.cr
@@ -1,63 +1,75 @@
 require "../models/*"
 
-class TestController < Kemalyst::Controller
-  def plaintext
-    text "Hello, World!"
-  end
-
-  def json
-    results = {message: "Hello, World!"}
-    json results.to_json
-  end
-
-  def db
-    results = {} of Symbol => Int32
-    if world = World.find rand(1..10_000)
-      results = {id: world.id, randomNumber: world.randomNumber}
+module TestController 
+  class Plaintext < Kemalyst::Handler::Base
+    def call(context)
+      text "Hello, World!"
     end
-    json results.to_json
   end
 
-  def queries
-    queries = params["queries"].as(String)
-    queries = queries.to_i? || 1
-    queries = queries.clamp(1..500)
+  class Json < Kemalyst::Handler::Base
+    def call(context)
+      results = {message: "Hello, World!"}
+      json results.to_json
+    end
+  end
 
-    results = (1..queries).map do
+  class Db < Kemalyst::Handler::Base
+    def call(context)
+      results = {} of Symbol => Int32
       if world = World.find rand(1..10_000)
-        {id: world.id, randomNumber: world.randomNumber}
+        results = {id: world.id, randomNumber: world.randomNumber}
       end
+      json results.to_json
     end
-
-    json results.to_json
   end
 
-  def updates
-    queries = params["queries"].as(String)
-    queries = queries.to_i? || 1
-    queries = queries.clamp(1..500)
+  class Queries < Kemalyst::Handler::Base
+    def call(context)
+      queries = context.params["queries"].as(String)
+      queries = queries.to_i? || 1
+      queries = queries.clamp(1..500)
 
-    updated = (1..queries).map do
-      world = World.find rand(1..10_000)
-      if world
-        world.randomNumber = rand(1..10_000)
-        world.save
-        {id: world.id, randomNumber: world.randomNumber}
+      results = (1..queries).map do
+        if world = World.find rand(1..10_000)
+          {id: world.id, randomNumber: world.randomNumber}
+        end
       end
-    end
 
-    json updated.to_json
+      json results.to_json
+    end
   end
 
-  def fortunes
-    fortune = Fortune.new
-    fortune.id = 0
-    fortune.message = "Additional fortune added at request time."
+  class Updates < Kemalyst::Handler::Base
+    def call(context)
+      queries = context.params["queries"].as(String)
+      queries = queries.to_i? || 1
+      queries = queries.clamp(1..500)
 
-    fortunes = Fortune.all
-    fortunes << fortune
-    fortunes.sort_by! { |fortune| fortune.message.not_nil! }
+      updated = (1..queries).map do
+        world = World.find rand(1..10_000)
+        if world
+          world.randomNumber = rand(1..10_000)
+          world.save
+          {id: world.id, randomNumber: world.randomNumber}
+        end
+      end
 
-    html render("fortune/index.slang", "main.slang")
+      json updated.to_json
+    end
+  end
+
+  class Fortunes < Kemalyst::Handler::Base
+    def call(context)
+      fortune = Fortune.new
+      fortune.id = 0
+      fortune.message = "Additional fortune added at request time."
+
+      fortunes = Fortune.all
+      fortunes << fortune
+      fortunes.sort_by! { |fortune| fortune.message.not_nil! }
+
+      html render("fortune/index.slang", "main.slang")
+    end
   end
 end
