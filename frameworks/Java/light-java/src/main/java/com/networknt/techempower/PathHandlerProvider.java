16c8a266693a9a449b768290cce2b9f736c3173f
Steve Hu <stevehu@gmail.com>
2017-06-25 07:13:12 -0400

    update queries and updates to remove future

--- frameworks/Java/light-java/src/main/java/com/networknt/techempower/PathHandlerProvider.java
+++ frameworks/Java/light-java/src/main/java/com/networknt/techempower/PathHandlerProvider.java
@@ -5,6 +5,7 @@ import com.networknt.server.HandlerProvider;
 import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.BlockingHandler;
 import io.undertow.util.Methods;
 import com.networknt.techempower.handler.*;
 
@@ -12,16 +13,12 @@ public class PathHandlerProvider implements HandlerProvider {
     @Override
     public HttpHandler getHandler() {
         return Handlers.path()
-            //.addExactPath("/db/mysql", new DbMysqlGetHandler())
             .addExactPath("/plaintext", new PlaintextGetHandler())
             .addExactPath("/json", new JsonGetHandler())
-            .addExactPath("/db", new DbPostgresqlGetHandler())
-            //.addExactPath("/fortunes/mysql", new FortunesMysqlGetHandler())
-            .addExactPath("/fortunes", new FortunesPostgresqlGetHandler())
-            //.addExactPath("/queries/mysql", new QueriesMysqlGetHandler())
-            .addExactPath("/queries", new QueriesPostgresqlGetHandler())
-            //.addExactPath("/updates/mysql", new UpdatesMysqlGetHandler())
-            .addExactPath("/updates", new UpdatesPostgresqlGetHandler())
+            .addExactPath("/db", new BlockingHandler(new DbPostgresqlGetHandler()))
+            .addExactPath("/fortunes", new BlockingHandler(new FortunesPostgresqlGetHandler()))
+            .addExactPath("/queries", new BlockingHandler(new QueriesPostgresqlGetHandler()))
+            .addExactPath("/updates", new BlockingHandler(new UpdatesPostgresqlGetHandler()))
         ;
     }
 }
