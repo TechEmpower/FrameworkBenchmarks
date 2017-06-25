e4f59e5f2d7a3eb719fd752fa84806d4345a0b16
Dru Jensen <drujensen@gmail.com>
2017-06-25 09:39:53 -0700

    use new controller

--- frameworks/Crystal/kemalyst/config/routes.cr
+++ frameworks/Crystal/kemalyst/config/routes.cr
@@ -2,16 +2,9 @@ require "../src/controllers/*"
 
 include Kemalyst::Handler
 
-get "/plaintext", TestController::Plaintext.new
-get "/json", TestController::Json.new
-get "/db", TestController::Db.new
-get "/queries", TestController::Queries.new
-get "/updates", TestController::Updates.new
-get "/fortunes", TestController::Fortunes.new
-
-# get "/plaintext", "benchmark", "plaintext"
-# get "/json", "benchmark", "json"
-# get "/db", "benchmark", "db"
-# get "/queries", "benchmark", "queries"
-# get "/updates", "benchmark", "updates"
-# get "/fortunes", "benchmark", "fortunes"
+get "/plaintext", "benchmark", "plaintext"
+get "/json", "benchmark", "json"
+get "/db", "benchmark", "db"
+get "/queries", "benchmark", "queries"
+get "/updates", "benchmark", "updates"
+get "/fortunes", "benchmark", "fortunes"
