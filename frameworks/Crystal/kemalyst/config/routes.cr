a90f2bbc116d06181b42a972ff62bfde4ec40d16
Dru Jensen <drujensen@gmail.com>
2017-06-25 09:52:53 -0700

    remove unneeded quotes

--- frameworks/Crystal/kemalyst/config/routes.cr
+++ frameworks/Crystal/kemalyst/config/routes.cr
@@ -2,9 +2,9 @@ require "../src/controllers/*"
 
 include Kemalyst::Handler
 
-get "/plaintext", "benchmark", "plaintext"
-get "/json", "benchmark", "json"
-get "/db", "benchmark", "db"
-get "/queries", "benchmark", "queries"
-get "/updates", "benchmark", "updates"
-get "/fortunes", "benchmark", "fortunes"
+get "/plaintext", benchmark, plaintext
+get "/json", benchmark, json
+get "/db", benchmark, db
+get "/queries", benchmark, queries
+get "/updates", benchmark, updates
+get "/fortunes", benchmark, fortunes
