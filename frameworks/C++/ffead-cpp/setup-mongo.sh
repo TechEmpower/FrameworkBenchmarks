afe25ef606b21ef93141e7214744f65a46e59731
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2017-06-26 20:31:26 +0530

    Fixed failing mysql/postgresql tests

--- frameworks/C++/ffead-cpp/setup-mongo.sh
+++ frameworks/C++/ffead-cpp/setup-mongo.sh
@@ -2,10 +2,8 @@
 
 fw_depends ffead-cpp-emb-mongo
 
-export FFEAD_CPP_PATH=$IROOT/ffead-cpp-2.0
-export LD_LIBRARY_PATH=$IROOT:$FFEAD_CPP_PATH/lib:$LD_LIBRARY_PATH
-echo $FFEAD_CPP_PATH
-echo $LD_LIBRARY_PATH
+chmod 755 $IROOT/ffead-cpp-2.0/*.sh
 rm -f $FFEAD_CPP_PATH/*.cntrl
 rm -f $FFEAD_CPP_PATH/tmp/*.sess
-$IROOT/ffead-cpp-2.0/CHS $FFEAD_CPP_PATH > ffead.log 2>&1
+cd $IROOT/ffead-cpp-2.0
+./server.sh
--- frameworks/C++/ffead-cpp/setup-nginx-postgresql.sh
+++ frameworks/C++/ffead-cpp/setup-nginx-postgresql.sh
@@ -4,6 +4,8 @@ fw_depends ffead-cpp-nginx-postgresql
 
 export FFEAD_CPP_PATH=$IROOT/ffead-cpp-2.0
 export LD_LIBRARY_PATH=$IROOT:$FFEAD_CPP_PATH/lib:$LD_LIBRARY_PATH
+export ODBCINI=${IROOT}/odbc.ini
+export ODBCSYSINI=${IROOT}
 echo $FFEAD_CPP_PATH
 echo $LD_LIBRARY_PATH
 rm -f $FFEAD_CPP_PATH/*.cntrl
