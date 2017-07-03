8ab7a8c7c4aa01867f585f49448a4bd6d531e1d8
Radoslav Petrov <rrpetrov@gmail.com>
2017-07-04 01:51:07 +0300

    Java/servlet Caching implementation and updates (#2906)
    * Update dependencies. Minor code cleanup
    
    * cache2k cashing implementation
    
    * Update benchmark_config.json
    
    * Update the cached query URL
    
    * More clean up
    
    * Add Maven build profiles for modular webapp assembly
    
    * Maven clean install
    
    * Use correct database hostname
    
    * Add missing dependencies for the shell scripts
    
    * Updated documentation. Removed duplicated servlet code. Minor code clean up

--- frameworks/Java/servlet/setup.sh
+++ frameworks/Java/servlet/setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-sed -i 's|localhost|'"${DBHOST}"'|g' src/main/webapp/WEB-INF/resin-web.xml
-
 fw_depends java resin maven
 
-mvn clean compile war:war
+mvn clean install
 rm -rf $RESIN_HOME/webapps/*
 cp target/servlet.war $RESIN_HOME/webapps/
 resinctl start
--- frameworks/Java/servlet/src/main/java/hello/World.java
+++ frameworks/Java/servlet/src/main/java/hello/World.java
@@ -3,30 +3,25 @@ package hello;
 /**
  * Simple World entity.
  */
-public class World
-{
+public class World {
 
-  private final int id;
-  private int randomNumber;
-  
-  public World(int id, int randomNumber)
-  {
-    this.id = id;
-    this.randomNumber = randomNumber;
-  }
-  
-  public int getId()
-  {
-    return this.id;
-  }
-  
-  public int getRandomNumber()
-  {
-    return this.randomNumber;
-  }      
- 
-  public void setRandomNumber(int randomNumber)
-  {
-    this.randomNumber = randomNumber;
-  } 
+	private final int id;
+	private int randomNumber;
+
+	public World(int id, int randomNumber) {
+		this.id = id;
+		this.randomNumber = randomNumber;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getRandomNumber() {
+		return randomNumber;
+	}
+
+	public void setRandomNumber(int randomNumber) {
+		this.randomNumber = randomNumber;
+	}
 }
