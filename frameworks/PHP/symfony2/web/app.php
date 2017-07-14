85ed491c19b8358c681f30cf1d70f12085ba215c
Thomas Lallement <thomas.lallement@9online.fr>
2017-07-14 22:08:14 +0200

    Revert changes for HHVM support

--- frameworks/PHP/symfony2/web/app.php
+++ frameworks/PHP/symfony2/web/app.php
@@ -4,8 +4,14 @@ use Symfony\Component\HttpFoundation\Request;
 
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
+if (PHP_VERSION_ID < 70000) {
+    include_once __DIR__.'/../var/bootstrap.php.cache';
+}
 
 $kernel = new AppKernel('prod', false);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 
 // When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
 //Request::enableHttpMethodParameterOverride();
