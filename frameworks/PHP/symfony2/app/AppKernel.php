78b67c704aacb0ff05030d8186d8e09af6c3e51c
Thomas Lallement <thomas.lallement@9online.fr>
2017-07-14 21:33:12 +0200

    Code cleanup & small improvements

--- frameworks/PHP/symfony2/app/AppKernel.php
+++ frameworks/PHP/symfony2/app/AppKernel.php
@@ -9,12 +9,8 @@ class AppKernel extends Kernel
     {
         $bundles = [
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
-            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\MonologBundle\MonologBundle(),
-            //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Skamander\BenchmarkBundle\SkamanderBenchmarkBundle(),
         ];
         
