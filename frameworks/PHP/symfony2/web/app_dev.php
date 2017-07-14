8416f02c88faaf870b80e80bfef5549c7b51c66d
Thomas Lallement <thomas.lallement@9online.fr>
2017-07-14 22:08:14 +0200

    Revert changes for HHVM support

--- frameworks/PHP/symfony2/web/app_dev.php
+++ frameworks/PHP/symfony2/web/app_dev.php
@@ -23,6 +23,9 @@ $loader = require __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 
 $kernel = new AppKernel('dev', true);
+if (PHP_VERSION_ID < 70000) {
+    $kernel->loadClassCache();
+}
 
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
