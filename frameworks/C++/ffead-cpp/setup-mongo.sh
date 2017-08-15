e8b2062e1fafb708d39bfa41889e4f6a418e8ce8
Sumeet Chhetri <sumeet.chhetri@gmail.com>
2017-08-15 19:19:17 +0530

    Adding tests for ffead-cpp (mongodb, mysql, postgresql) on (#2893)
    * Adding tests for ffead-cpp (mongodb, mysql, postgresql) on
    - Embedded server
    - Apache
    - Nginx
    Resolves https://github.com/TechEmpower/FrameworkBenchmarks/issues/2554
    
    * All test implementation code now sits inside the test folder for ffead-cpp
    
    * Fixed issue with server startup
    
    * Fixed failing mysql/postgresql tests
    
    * Fixed scripts as per comments from Mike

---
+++ frameworks/C++/ffead-cpp/setup-mongo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+fw_depends ffead-cpp-emb-mongo
+
+chmod 755 $FFEAD_CPP_PATH/*.sh
+rm -f $FFEAD_CPP_PATH/*.cntrl
+rm -f $FFEAD_CPP_PATH/tmp/*.sess
+cd $FFEAD_CPP_PATH
+./server.sh
---
+++ frameworks/C++/ffead-cpp/te-benchmark/sql-src/TeBkWorldmongo.h
@@ -0,0 +1,33 @@
+/*
+ * TeBkWorld.h
+ *
+ *  Created on: 11-Mar-2015
+ *      Author: sumeetc
+ */
+
+#ifndef TEBKWORLD_H_
+#define TEBKWORLD_H_
+#include "string"
+
+
+#pragma @Entity
+#pragma @Table name="world"
+class TeBkWorld {
+	#pragma @Id dbf="_id"
+	int id;
+	#pragma @Column dbf="randomNumber"
+	int randomNumber;
+	#pragma @IgnoreSer
+	#pragma @Column dbf="id"
+	int anotherId;
+public:
+	virtual ~TeBkWorld();
+	int getId() const;
+	void setId(int id);
+	int getRandomNumber() const;
+	void setRandomNumber(int randomNumber);
+	int getAnotherId() const;
+	void setAnotherId(int anotherId);
+};
+
+#endif /* TEBKWORLD_H_ */
