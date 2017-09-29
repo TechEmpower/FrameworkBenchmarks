2954fde0915bd27cc0ada40464913bc26edffbe8
joshua bauer <josh@wurrly.com>
2017-09-29 16:14:01 -0700

    Proteus: Updated framework.

--- frameworks/Java/proteus/src/main/java/io/sinistral/ExampleApplication.java
+++ frameworks/Java/proteus/src/main/java/io/sinistral/ExampleApplication.java
@@ -1,15 +1,27 @@
 package io.sinistral;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import static io.undertow.util.Headers.CONTENT_TYPE;
+
+import java.nio.ByteBuffer;
+
+import com.jsoniter.output.EncodingMode;
+import com.jsoniter.output.JsonStream;
 
 import io.sinistral.controllers.Benchmarks;
+import io.sinistral.models.Message;
 import io.sinistral.proteus.ProteusApplication;
 import io.sinistral.proteus.services.AssetsService;
 import io.sinistral.proteus.services.SwaggerService;
 import io.sinistral.services.MySqlService;
 import io.sinistral.services.PostgresService;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.UndertowOptions;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.server.handlers.SetHeaderHandler;
+import io.undertow.util.Headers;
 
 /**
  * Hello world!
@@ -17,6 +29,16 @@ import io.sinistral.services.PostgresService;
  */
 public class ExampleApplication extends ProteusApplication
 {
+ 
+    static {
+   
+    	
+    	JsonStream.setMode(EncodingMode.STATIC_MODE);
+    	 
+    	 
+    }
+    
+    
     public static void main( String[] args )
     {
         
@@ -33,6 +55,10 @@ public class ExampleApplication extends ProteusApplication
 		app.addController(Benchmarks.class);  
 		
 		app.start();
-        
+		
+		 
+		
+	 
     }
+    
 }
