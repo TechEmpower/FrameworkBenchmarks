07c4c4c59c7e6cc5bbb83a9f8dea35f65586a75f
Robert Husák <robert@husak.cloud>
2017-10-06 12:19:51 +0200

    Add fortunes Peachpie test

---
+++ frameworks/PHP/peachpie/Website/fortune.php
@@ -0,0 +1,46 @@
+<?php
+//
+// Database Test
+//
+
+// Database connection (TODO: When it works, use PDO instead)
+$link = mysql_connect('TFB-database', 'benchmarkdbuser', 'benchmarkdbpass');
+mysql_select_db('hello_world', $link);
+  
+// Store result in array.
+$result = mysql_query('SELECT id, message FROM Fortune', $link);
+$arr = array();
+while ($row = mysql_fetch_array($result)) {
+  $arr[$row['id']] = $row['message'];
+}
+
+$arr[0] = 'Additional fortune added at request time.';
+
+asort($arr);
+
+// Set content type
+header("Content-type: text/html; charset=utf-8");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Fortunes</title>
+</head>
+<body>
+<table>
+<tr>
+<th>id</th>
+<th>message</th>
+</tr>
+<?php
+foreach ( $arr as $id => $fortune ) {
+?>
+<tr>
+<td><?php echo htmlspecialchars($id, ENT_QUOTES, 'UTF-8'); ?></td>  
+<td><?php echo htmlspecialchars($fortune, ENT_QUOTES, 'UTF-8'); ?></td>
+</tr>
+<?php } ?>
+</table>
+</body>
+</html>
