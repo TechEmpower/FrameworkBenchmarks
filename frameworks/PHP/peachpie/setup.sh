a91a75f70881c23a66bbfae0981c43ee6c52b12d
Robert Husák <robert@husak.cloud>
2017-10-27 17:21:35 +0200

    Simplify Peachpie setup
    Remove (now unnecessary) installation of .NET Core 1.0.4 and dotnet
    restore. Run the website in the Release configuration.

--- frameworks/PHP/peachpie/setup.sh
+++ frameworks/PHP/peachpie/setup.sh
@@ -1,7 +1,3 @@
 fw_depends mysql dotnetcore
 
-# dotnet-compile-php is a .NET Core 1.0.4 application
-sudo apt-get install -y dotnet-dev-1.0.4
-
-dotnet restore
-dotnet run -p Server -c Runtime
+dotnet run -p Server -c Release
