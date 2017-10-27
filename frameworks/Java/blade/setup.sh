a839d24995a3564a175d4a21f41cd018d481de42
王爵nice <biezhi.me@gmail.com>
2017-10-27 04:15:15 -0500

    Update setup.sh

--- frameworks/Java/blade/setup.sh
+++ frameworks/Java/blade/setup.sh
@@ -1,4 +1,8 @@
-mvn clean package		  mvn clean package
-  cd target/dist/hello-blade-1.0.0-BUILD-SNAPSHOT		  cd target/dist/hello-blade-1.0.0-BUILD-SNAPSHOT
-  		  
- java -Xms2G -Xmx2G -server -XX:+UseNUMA -XX:+UseParallelGC -XX:+AggressiveOpts -jar hello-blade-1.0.0-BUILD-SNAPSHOT.jar & 
+#!/bin/bash
+
+fw_depends mysql java maven
+
+mvn clean package
+cd target/dist/hello-blade-1.0.0-BUILD-SNAPSHOT
+
+java -Xms2G -Xmx2G -server -XX:+UseNUMA -XX:+UseParallelGC -XX:+AggressiveOpts -jar hello-blade-1.0.0-BUILD-SNAPSHOT.jar &
