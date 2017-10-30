5db11a5858f02bec45ad790e49a640a8d850a3b9
Walther Lalk <dakota@users.noreply.github.com>
2017-10-30 14:25:00 +0200

    Update setup.sh

--- frameworks/PHP/cakephp/setup.sh
+++ frameworks/PHP/cakephp/setup.sh
@@ -5,7 +5,7 @@ fw_depends mysql php7 composer nginx
 sed -i "s|'host' => '.*'|'host' => '${DBHOST}'|g" app/Config/database.php
 sed -i 's|REDISSERVER|'${DBHOST}'|g' app/Config/core.php
 sed -i 's|root .*/FrameworkBenchmarks/cakephp|root '${TROOT}'|g' deploy/nginx.conf
-sed -i 's|/usr/local/nginx/|'${IROOT}'/nginx/|g' deploy/nginx.conf
+sed -i 's|/home/vagrant/FrameworkBenchmarks/installs/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
 
 php-fpm --fpm-config $FWROOT/toolset/setup/linux/languages/php/php-fpm.conf -g $TROOT/deploy/php-fpm.pid
 
