5cfe61a57c9bfd78021250f09fdf41350273f3a5
Mike Pastore <mwpastore@users.noreply.github.com>
2017-11-10 11:27:36 -0600

    Ruby runtime and framework updates (#3058)
    * Ruby runtime updates
    
    * Ruby framework updates
    
    * Sinatra 2 (final)
    * Roda 3
    * ActiveRecord 5.1
    * Sequel 5
    * and other goodies

--- toolset/setup/linux/languages/ruby/jruby-9.0.sh
+++ toolset/setup/linux/languages/ruby/jruby-9.0.sh
@@ -4,13 +4,12 @@ fw_depends rvm java
 
 fw_installed jruby-9.0 && return 0
 
-# rvm stable [typically] only provides one version of jruby-9.0
-# update this when it changes
+# TODO: JRuby 9.0 is EOL. Remove as soon as possible.
 JRUBY_VERSION="9.0.5.0"
 
 rvm install jruby-$JRUBY_VERSION
 
-rvm jruby-$JRUBY_VERSION do gem install bundler -v 1.14.3
+rvm jruby-$JRUBY_VERSION do gem install bundler -v 1.15.4
 
 echo "export JRUBY_VERSION=${JRUBY_VERSION}" > $IROOT/jruby-9.0.installed
 
