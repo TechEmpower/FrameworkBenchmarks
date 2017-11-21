737bb102127da1a1ace1371f9f124f44240244b0
Mickey248 <mickeynaja246@gmail.com>
2017-11-21 11:37:15 +0700

    Update setup_php5.sh
    comment a important

--- frameworks/PHP/laravel/setup_php5.sh
+++ frameworks/PHP/laravel/setup_php5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 fw_depends mysql php5 nginx composer
-
+//localhost id pass
 sed -i 's|127.0.0.1|'"${DBHOST}"'|g' app/config/database.php
 sed -i 's|root .*/FrameworkBenchmarks/frameworks/PHP/php-laravel|root '"${TROOT}"'|g' deploy/nginx.conf 
 sed -i 's|/home/vagrant/FrameworkBenchmarks/installs/nginx/|'"${IROOT}"'/nginx/|g' deploy/nginx.conf
