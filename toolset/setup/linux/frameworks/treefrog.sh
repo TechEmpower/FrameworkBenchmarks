46b23ed67e22e2450cf910a62023993effb06792
AOYAMA Kazuharu <a.kazuharu@gmail.com>
2017-11-26 15:15:03 +0900

    fix a setup error for treefrog in round 15 preview.

--- toolset/setup/linux/frameworks/treefrog.sh
+++ toolset/setup/linux/frameworks/treefrog.sh
@@ -5,7 +5,7 @@ fw_installed treefrog && return 0
 TFVER=1.18.0
 
 sudo apt-get update -qq
-sudo apt-get install -y qt5-qmake qt5-default qtbase5-dev qtbase5-dev-tools libqt5sql5 libqt5sql5-mysql libqt5sql5-psql libqt5qml5 libqt5xml5 qtdeclarative5-dev g++ libjemalloc-dev gcc
+sudo apt-get install -y qt5-qmake qt5-default qtbase5-dev qtbase5-dev-tools libqt5sql5 libqt5sql5-mysql libqt5sql5-psql libqt5qml5 libqt5xml5 qtdeclarative5-dev libqt5quick5 libqt5quickparticles5 g++ libjemalloc-dev gcc
 
 fw_get -O https://github.com/treefrogframework/treefrog-framework/archive/v${TFVER}.tar.gz
 fw_untar v${TFVER}.tar.gz
