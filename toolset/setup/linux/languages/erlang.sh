130f89d79f57fc7aea474f2ba90831826408cbca
Supreet Sethi <supreet.sethi@gmail.com>
2017-11-29 19:16:46 +0700

    fixing url for erlang

--- toolset/setup/linux/languages/erlang.sh
+++ toolset/setup/linux/languages/erlang.sh
@@ -5,8 +5,8 @@ fw_installed erlang && return 0
 VERSION="20.1-1"
 RELEASE="trusty"
 ARCH="amd64"
-
-fw_get -O http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_2_esl/esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb
+#https://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_20.1-1~ubuntu~trusty_amd64.deb
+fw_get -O http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_esl/esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb
 dpkg -x esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb $IROOT/erlang
 $IROOT/erlang/usr/lib/erlang/Install -minimal $IROOT/erlang/usr/lib/erlang
 
