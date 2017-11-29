a5091d17fc6f1575511c83aacf0ce2cfc8b20b7b
Supreet Sethi <supreet.sethi@gmail.com>
2017-11-29 14:05:04 +0700

    release sequence

--- frameworks/Elixir/phoenix/setup.sh
+++ frameworks/Elixir/phoenix/setup.sh
@@ -7,9 +7,10 @@ rm -rf _build deps
 export MIX_ENV=prod
 mix local.hex --force
 mix local.rebar --force
-mix release.init
+
 mix deps.get --force --only prod
 mix compile --force
+mix release.init
 mix release
 ERL_AFLAGS="+K true +sbwt very_long +swt very_low" _build/prod/rel/hello/bin/hello foreground
 #elixir --erl "+K true +sbwt very_long +swt very_low" --detached -S mix phoenix.server
