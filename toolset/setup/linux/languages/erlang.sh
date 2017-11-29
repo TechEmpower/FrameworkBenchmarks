6fee14af3f5cb938f01ebe2c11e974d967bac98b
Supreet Sethi <supreet.sethi@gmail.com>
2017-11-29 16:10:36 +0700

    update erlang version to 20.1

--- toolset/setup/linux/languages/erlang.sh
+++ toolset/setup/linux/languages/erlang.sh
@@ -2,10 +2,10 @@
 
 fw_installed erlang && return 0
 
-VERSION="18.2-2"
+VERSION="20.1-1"
 RELEASE="trusty"
 ARCH="amd64"
-
+http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_1_general/esl-erlang_20.1-1~ubuntu~trusty_amd64.deb
 fw_get -O http://packages.erlang-solutions.com/site/esl/esl-erlang/FLAVOUR_2_esl/esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb
 dpkg -x esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb $IROOT/erlang
 $IROOT/erlang/usr/lib/erlang/Install -minimal $IROOT/erlang/usr/lib/erlang
