7baa068c1130c459e8bcb8fdc2db087c0022cea1
Supreet Sethi <supreet.sethi@gmail.com>
2017-11-30 11:37:40 +0700

    using rebar3 instead of rebar

--- frameworks/Erlang/chicagoboss/setup.sh
+++ frameworks/Erlang/chicagoboss/setup.sh
@@ -3,7 +3,7 @@
 fw_depends erlang
 
 rm -rf deps/* ebin/*
-rebar get-deps
-rebar compile
+rebar3 get-deps
+rebar3 compile
 
 erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -config boss -s boss -sname chicagoboss -noshell -detached
--- frameworks/Erlang/cowboy/setup.sh
+++ frameworks/Erlang/cowboy/setup.sh
@@ -3,7 +3,7 @@
 fw_depends mysql erlang
 
 rm -rf deps/* ebin/*
-rebar get-deps
-rebar compile
+rebar3 get-deps
+rebar3 compile
 
 erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s hello_world -noshell -detached
