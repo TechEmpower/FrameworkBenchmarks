35ac47c42810e580f443681efb35af2381f14ce0
Supreet Sethi <supreet.sethi@gmail.com>
2017-12-06 18:12:58 +0700

    Land the url to file

--- toolset/setup/linux/languages/erlang.sh
+++ toolset/setup/linux/languages/erlang.sh
@@ -10,7 +10,7 @@ ARCH="amd64"
 fw_get -O http://packages.erlang-solutions.com/erlang/esl-erlang/FLAVOUR_1_general/esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb
 dpkg -x esl-erlang_${VERSION}~ubuntu~${RELEASE}_${ARCH}.deb $IROOT/erlang
 $IROOT/erlang/usr/lib/erlang/Install -minimal $IROOT/erlang/usr/lib/erlang
-fw_get https://s3.amazonaws.com/rebar3/rebar3 && mv rebar3 $IROOT/erlang/usr/bin/rebar && chmod +x $IROOT/erlang/usr/bin/rebar
+fw_get -O https://s3.amazonaws.com/rebar3/rebar3 && mv rebar3 $IROOT/erlang/usr/bin/rebar && chmod +x $IROOT/erlang/usr/bin/rebar
 echo -e "export PATH=${IROOT}/erlang/usr/bin:\$PATH" > $IROOT/erlang.installed
 
 source $IROOT/erlang.installed
