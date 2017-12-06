7251247cd15e96899360e743e75eab8659a13643
Supreet Sethi <supreet.sethi@gmail.com>
2017-12-06 18:27:10 +0700

    moved all frameworks to rebar3

--- frameworks/Erlang/elli/setup.sh
+++ frameworks/Erlang/elli/setup.sh
@@ -3,7 +3,7 @@
 fw_depends erlang mysql
 
 rm -rf deps/* ebin/*
-rebar get-deps
-rebar compile
+rebar3 get-deps
+rebar3 compile
 
 erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s elli_bench -noshell -detached
--- frameworks/Erlang/mochiweb/setup.sh
+++ frameworks/Erlang/mochiweb/setup.sh
@@ -3,7 +3,7 @@
 fw_depends erlang mysql
 
 rm -rf deps/* ebin/*
-rebar get-deps
-rebar compile
+rebar3 get-deps
+rebar3 compile
 
 erl +K true +sbwt very_long +swt very_low -pa ebin deps/*/ebin -boot start_sasl -config priv/app.config -s mochiweb_bench_app
