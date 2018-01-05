f840b49bf61c9dc2775e2eb69b4a8837e43928e5
Schien Dong <schien@antradar.com>
2018-01-05 12:59:46 -0500

    Corrected unsafe character
    The double dash was apparently converted to a unicode long dash. Although this is in a comment, the mere existence of these characters triggers editors on Windows such as Notepad to insert extra characters at the beginning of file, which breaks the entire script

--- toolset/setup/linux/prerequisites.sh
+++ toolset/setup/linux/prerequisites.sh
@@ -32,7 +32,7 @@ sudo apt-get -qqy install -o Dpkg::Options::="--force-confdef" -o Dpkg::Options:
 
 sudo pip install colorama==0.3.1
 # Version 2.3 has a nice Counter() and other features
-# but it requires —-allow-external and -—allow-unverified
+# but it requires --allow-external and --allow-unverified
 sudo pip install progressbar==2.2
 sudo pip install requests
 
