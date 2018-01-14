19846fc0f69473bbcf504df352dcd826c38925c8
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-01-14 16:42:43 +0100

    Make ./setup.sh executable

--- frameworks/Scala/colossus/setup.sh
+++ frameworks/Scala/colossus/setup.sh
@@ -4,4 +4,4 @@ fw_depends java sbt
 
 sbt 'oneJar' -batch
 
-java -jar target/scala-2.11/colossus*one-jar.jar
+java -server -Xms1g -Xmx1g -XX:NewSize=512m -XX:+UseG1GC -XX:MaxGCPauseMillis=30 -XX:-UseBiasedLocking -XX:+AlwaysPreTouch -jar target/scala-2.11/colossus*one-jar.jar
