0648304e4dfc0a7d591a25118a52e4383df418fa
Andriy Plokhotnyuk <plokhotnyuk@gmail.com>
2018-01-14 18:07:03 +0100

    Fix JSON response

--- frameworks/Scala/colossus/src/main/scala/example/Main.scala
+++ frameworks/Scala/colossus/src/main/scala/example/Main.scala
@@ -13,7 +13,7 @@ import com.github.plokhotnyuk.jsoniter_scala.macros._
 
 import scala.concurrent.duration.Duration
 
-case class Message(message: String = "Hello, World!")
+case class Message(message: String)
 
 object Main extends App {
   def toHttpBodyEncoder[T](codec: JsonCodec[T]): HttpBodyEncoder[T] = new HttpBodyEncoder[T] {
@@ -41,7 +41,7 @@ object Main extends App {
     override def onConnect: RequestHandlerFactory = serverContext => new RequestHandler(serverContext, serviceConfig) {
       override def handle: PartialHandler[Http] = {
         case req if req.head.url == "/plaintext" => req.ok("Hello, World!")
-        case req if req.head.url == "/json" => req.ok(Message())
+        case req if req.head.url == "/json" => req.ok(Message("Hello, World!"))
       }
     }
   })
