83e1de9cbaf74dcc7555cf97c7c10cb900046985
Green Luo <greenlaw110@gmail.com>
2018-01-16 03:27:00 +1100

    [ci fw-only Java/act] Update to act-1.6.4 (#3179)
    * add act-ebean-pgsql-rythm to compare rythm vs mustache template engine
    
    * update to act-1.6.4
    
    * act: add rythm template for fortunes

--- frameworks/Java/light-java/setup.sh
+++ frameworks/Java/light-java/setup.sh
@@ -4,4 +4,4 @@ fw_depends postgresql mysql java maven
 
 mvn clean package
 cd target
-java -server -Xms512m -Xmx2g -jar techempower-1.0.0.jar &
+java -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 -server -Xms512m -Xmx2g -jar techempower-1.0.0.jar &
