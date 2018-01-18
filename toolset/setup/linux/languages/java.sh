33c008ab57470d06f9bbf47f2a4780bf5f1bd31b
Michael Hixson <mhixson@techempower.com>
2018-01-18 14:06:48 -0800

    Attempt to fix Maven build errors under OpenJDK
    We're seeing a bunch of this in the Travis build:
    
      java.security.InvalidAlgorithmParameterException: the trustAnchors
      parameter must be non-empty
    
    Googling led me to this bug report and solution:
    
      https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
    
    Indeed, in the Java/act installation, we see the same messages as in
    the bug report:
    
      Setup actframework: Setting up ca-certificates-java (20130815ubuntu1) ...
      Setup actframework: /var/lib/dpkg/info/ca-certificates-java.postinst: line 53: java: command not found
      Setup actframework: /var/lib/dpkg/info/ca-certificates-java.postinst: line 66: java: command not found

--- toolset/setup/linux/languages/java.sh
+++ toolset/setup/linux/languages/java.sh
@@ -7,6 +7,9 @@ sudo add-apt-repository -y ppa:openjdk-r/ppa
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk
 
+# https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
+sudo /var/lib/dpkg/info/ca-certificates-java.postinst configure
+
 # Setup environment variables
 JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
 echo "export JAVA_HOME=${JAVA_HOME}" > $IROOT/java.installed
