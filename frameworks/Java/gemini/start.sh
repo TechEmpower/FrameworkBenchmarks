dd2221890cecba35cb6af4d83091fe905dfe7f17
Mike Smith <msmith@techempower.com>
2018-01-22 15:53:12 -0800

    Exploring docker

--- frameworks/Java/gemini/start.sh
+++ frameworks/Java/gemini/start.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-fw_depends mysql java resin maven ant
-
 sed -i 's|db.ConnectString = .*/|db.ConnectString = '"$DBHOST"':3306/|g' Docroot/WEB-INF/GeminiHello.conf
-sed -i 's|root-directory=".*/FrameworkBenchmarks/frameworks/Java/gemini|root-directory="'"$TROOT"'|g' Docroot/WEB-INF/resin.xml
 sed -i 's|db.Driver.Class = .*|db.Driver.Class = com.mysql.jdbc.Driver|g' Docroot/WEB-INF/GeminiHello.conf
 sed -i 's|db.Driver.UrlPrefix = .*|db.Driver.UrlPrefix = jdbc:mysql://|g' Docroot/WEB-INF/GeminiHello.conf
-mkdir -p Docroot/WEB-INF/classes
-ant resolve
-ant compile
-resinctl -conf $TROOT/Docroot/WEB-INF/resin.xml start
+
+docker build -t gemini .
+
+docker run -d --rm -p 8080:8080 gemini
