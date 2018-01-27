f35245b477bc79b5e262235232f939719aae5c13
Gelin Luo <greenlaw110@gmail.com>
2018-01-27 18:01:42 +1100

    add hibernate tests

--- frameworks/Java/act/setup-hibernate-pgsql.sh
+++ frameworks/Java/act/setup-hibernate-pgsql.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-fw_depends mysql java maven
+fw_depends postgresql java maven
 
-mvn -Pebean_mysql clean package
+mvn -Pjpa_pgsql clean package
 cd target/dist
 unzip *.zip
 
@@ -21,4 +21,4 @@ echo
 
 JAVA_OPTS="-Djava.security.egd=file:/dev/./urandom -Xms1G -Xmx1G -Xss320k -XX:+UseNUMA -XX:+UseParallelGC -XX:+AggressiveOpts"
 
-java -server $JAVA_OPTS -Dapp.mode=prod -Dapp.nodeGroup=$GROUP -Dprofile=ebean_mysql -Dxio.worker_threads.max=256 -Dmysql.host=${DBHOST} -cp "$CP" $APP_ENTRY &
+java -server $JAVA_OPTS -Dapp.mode=prod -Dapp.nodeGroup=$GROUP -Dprofile=jpa_pgsql -Dxio.worker_threads.max=256 -Dpgsql.host=${DBHOST} -cp "$CP" $APP_ENTRY &
