8e5459446e0e0dfd512af7273953f45e3a266c7f
Faustino Aguilar <faustinoaq@gmail.com>
2018-01-31 03:38:53 -0500

    Update amber

--- frameworks/Crystal/amber/config/application.cr
+++ frameworks/Crystal/amber/config/application.cr
@@ -2,10 +2,3 @@ require "amber"
 
 require "../src/models/*"
 require "../src/controllers/*"
-
-Amber::Server.configure do |app|
-  app.name = "TFB test app"
-  app.color = false
-  app.port = 8080
-  app.host = "0.0.0.0"
-end
