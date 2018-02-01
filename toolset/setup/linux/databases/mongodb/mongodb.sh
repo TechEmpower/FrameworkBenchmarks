d1443cebf987cc36d4628a8d7ccedd3a78eb5e1d
Michael Hixson <mhixson@techempower.com>
2018-02-01 15:22:55 -0800

    Upgrade mongdb from 3.2 to 3.6
    The vapor tests rely on a feature that was added in mongodb 3.4, so we
    might as well take this opportunity to upgrade mongodb to the latest
    release.

--- toolset/setup/linux/databases/mongodb/mongodb.sh
+++ toolset/setup/linux/databases/mongodb/mongodb.sh
@@ -11,20 +11,30 @@ scp $FWROOT/toolset/setup/linux/databases/mongodb/create.js $DBHOST:~/
 # install mongo on database machine
 ssh $DBHOST 'bash' <<"EOF"
 echo "Setting up MongoDB database"
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
-echo 'deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
+
+if service --status-all | grep -Fq 'mongod'; then
+  sudo service mongod stop
+fi
+sudo apt-get -y remove --purge mongodb-org
+sudo apt-get -y autoremove
+sudo find /etc/apt/sources.list.d -type f -name 'mongodb-org-*.list' -delete -maxdepth 1
+sudo find /etc -type f -name 'mongo*.conf*' -delete -maxdepth 1
+sudo rm -rf /var/lib/mongodb
+sudo rm -rf /var/log/mongodb
+
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
+echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.6 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.6.list
 sudo apt-get -y update
-sudo apt-get -y remove mongodb-clients
 sudo apt-get -y install mongodb-org
 
 sudo service mongod stop
-sudo mv /etc/mongodb.conf /etc/mongodb.conf.orig
-sudo cp mongodb.conf /etc/mongodb.conf
 sudo mv mongodb.conf /etc/mongod.conf
 sudo rm -rf /ssd/mongodb
 sudo rm -rf /ssd/log/mongodb
-sudo cp -R -p /var/lib/mongodb /ssd/
-sudo cp -R -p /var/log/mongodb /ssd/log/
+sudo mkdir -p /ssd/mongodb
+sudo mkdir -p /ssd/log/mongodb
+sudo chown -R mongodb:mongodb /ssd/mongodb
+sudo chown -R mongodb:mongodb /ssd/log/mongodb
 sudo service mongod start
 
 for i in {1..15}; do
