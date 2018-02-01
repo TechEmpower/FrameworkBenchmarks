d645c639cd8078a5fc0f6a960c4bfe50e0367a4d
Faustino Aguilar <faustinoaq@gmail.com>
2018-02-01 10:04:03 -0500

    Use shards build on amber

--- frameworks/Crystal/amber/setup.sh
+++ frameworks/Crystal/amber/setup.sh
@@ -2,18 +2,16 @@
 
 fw_depends postgresql crystal
 
-shards install
-
-crystal build --release --no-debug src/amber.cr
-
 export GC_MARKERS=1
 
 export AMBER_ENV=production
 
 export DATABASE_URL=postgres://benchmarkdbuser:benchmarkdbpass@TFB-database:5432/hello_world
 
+shards build amber --release --no-debug
+
 for i in $(seq 1 $(nproc --all)); do
-  ./amber &
+  ./bin/amber &
 done
 
 wait
