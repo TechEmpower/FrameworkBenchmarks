5db2852547bc11b9f987516e570bb0eca23a001e
INADA Naoki <songofacandy@gmail.com>
2018-02-02 20:38:27 +0900

    Update Flask

--- frameworks/Python/falcon/setup.sh
+++ frameworks/Python/falcon/setup.sh
@@ -2,6 +2,6 @@
 
 fw_depends python2
 
-pip install --install-option="--prefix=${PY2_ROOT}" -r $TROOT/requirementsa.lock
+pip install --install-option="--prefix=${PY2_ROOT}" -r $TROOT/requirements.lock
 
 gunicorn app:app -c gunicorn_conf.py &
