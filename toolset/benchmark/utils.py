978cc93d666b7d244649d10961670e82afeca958
Mike Smith <msmith@techempower.com>
2018-02-22 13:43:59 -0800

    Split up lang/systools

--- toolset/benchmark/utils.py
+++ toolset/benchmark/utils.py
@@ -28,17 +28,16 @@ def gather_docker_dependencies(docker_file):
             line = fp.readline()
             if line:
                 tokens = line.strip().split(' ')
-                if tokens[0] == "FROM":
-                    # This is magic that our base image points to
-                    if tokens[1] != "ubuntu:16.04":
-                        depTokens = tokens[1].strip().split(':')
-                        deps.append(depTokens[0])
-                        dep_docker_file = os.path.join(os.path.dirname(docker_file), depTokens[0] + ".dockerfile")
-                        if not os.path.exists(dep_docker_file):
-                            # dep_docker_file = os.path.join(setup_util.get_fwroot(),
-                            #     "toolset", "setup", "linux", "docker", depTokens[0] + ".dockerfile")
-                            dep_docker_file = find_docker_file(docker_dir, depTokens[0] + ".dockerfile")
-                        deps.extend(gather_docker_dependencies(dep_docker_file))
+                for token in tokens:
+                    if token == "FROM":
+                        # This is magic that our base image points to
+                        if tokens[1] != "ubuntu:16.04":
+                            depTokens = tokens[1].strip().split(':')
+                            deps.append(depTokens[0])
+                            dep_docker_file = os.path.join(os.path.dirname(docker_file), depTokens[0] + ".dockerfile")
+                            if not os.path.exists(dep_docker_file):
+                                dep_docker_file = find_docker_file(docker_dir, depTokens[0] + ".dockerfile")
+                            deps.extend(gather_docker_dependencies(dep_docker_file))
 
     return deps
 
--- toolset/setup/linux/docker/languages/java.dockerfile
+++ toolset/setup/linux/docker/languages/java.dockerfile
@@ -3,7 +3,7 @@ FROM tfb:latest
 RUN add-apt-repository -y ppa:openjdk-r/ppa
 RUN apt-get update
 RUN apt-get install -qqy -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" \
-    openjdk-8-jdk ant
+    openjdk-8-jdk
 
 # https://bugs.launchpad.net/ubuntu/+source/ca-certificates-java/+bug/1396760
 RUN /var/lib/dpkg/info/ca-certificates-java.postinst configure
