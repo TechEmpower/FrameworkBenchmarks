0f98b3af161146194772e41703ef1949afda3016
zloster <more@edno.moe>
2018-02-27 17:20:59 +0200

    Fix setup scripts

---
+++ frameworks/Java/comsat/setup-servlet-jetty.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+MODE="servlet-jetty"
+
+. $TROOT/setup-servlet-generic.sh
---
+++ frameworks/Java/comsat/setup-webactors-netty.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+MODE="webactors-netty"
+
+. $TROOT/setup-webactors-generic.sh
