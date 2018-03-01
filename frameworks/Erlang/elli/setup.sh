281ff29d5a950a91813c08401c30064551f56f03
Nate <nathan.brady@yahoo.com>
2018-03-01 08:10:45 -0800

    Erlang and Elixir tests for docker (#3341)

--- frameworks/Erlang/elli/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-fw_depends erlang mysql
-
-rm -rf deps/* ebin/*
-rebar get-deps
-rebar compile
-
-erl -pa ebin deps/*/ebin +sbwt very_long +swt very_low -s elli_bench -noshell -detached
