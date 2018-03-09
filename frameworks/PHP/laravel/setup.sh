16ca6455a40f5ae715cc2973d06635359eb7b498
Francis Turmel <fturmel@gmail.com>
2018-03-09 20:57:31 +0100

    Laravel tuning: added config and route caching as part of setup

--- frameworks/PHP/laravel/setup.sh
+++ frameworks/PHP/laravel/setup.sh
@@ -7,5 +7,8 @@ sed -i 's|__DBHOST__|'${DBHOST}'|g' .env
 sed -i 's|__IROOT__|'${IROOT}'|g' deploy/nginx.conf
 sed -i 's|__TROOT__|'${TROOT}'|g' deploy/nginx.conf
 
+php artisan config:cache
+php artisan route:cache
+
 php-fpm --fpm-config ${FWROOT}/toolset/setup/linux/languages/php/php-fpm.conf -g ${TROOT}/deploy/php-fpm.pid
 nginx -c ${TROOT}/deploy/nginx.conf
