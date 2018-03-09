a38cb0a2351be4d4645c7c739625815c2605c116
Francis Turmel <fturmel@gmail.com>
2018-03-09 18:55:08 +0100

    All Laravel tests implemented

--- frameworks/PHP/laravel/routes/web.php
+++ frameworks/PHP/laravel/routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/json', 'Controller@json');
+Route::get('/db', 'Controller@db');
+Route::get('/queries/{queries?}', 'Controller@queries');
+Route::get('/fortunes', 'Controller@fortunes');
+Route::get('/updates/{queries?}', 'Controller@updates');
+Route::get('/plaintext', 'Controller@plaintext');
---
+++ frameworks/PHP/laravel/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+fw_depends mysql php7 nginx composer
+
+sed -i 's|__DBHOST__|'${DBHOST}'|g' .env
+
+sed -i 's|__IROOT__|'${IROOT}'|g' deploy/nginx.conf
+sed -i 's|__TROOT__|'${TROOT}'|g' deploy/nginx.conf
+
+php-fpm --fpm-config ${FWROOT}/toolset/setup/linux/languages/php/php-fpm.conf -g ${TROOT}/deploy/php-fpm.pid
+nginx -c ${TROOT}/deploy/nginx.conf
