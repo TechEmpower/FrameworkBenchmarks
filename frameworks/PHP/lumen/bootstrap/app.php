af5342dbc3104e03f924f3a4a2bb991646b35964
Francis Turmel <fturmel@gmail.com>
2018-03-10 13:20:10 +0100

    Implemented all tests for Lumen (pretty much identical to Laravel using Eloquent and Blade)

--- frameworks/PHP/lumen/bootstrap/app.php
+++ frameworks/PHP/lumen/bootstrap/app.php
@@ -25,7 +25,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
--- frameworks/PHP/lumen/routes/web.php
+++ frameworks/PHP/lumen/routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->get('/json', 'Controller@json');
+$router->get('/db', 'Controller@db');
+$router->get('/queries[/{queries}]', 'Controller@queries');
+$router->get('/fortunes', 'Controller@fortunes');
+$router->get('/updates[/{queries}]', 'Controller@updates');
+$router->get('/plaintext', 'Controller@plaintext');
