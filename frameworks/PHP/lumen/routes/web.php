feb27cac9d4f133a9224ef1703c51f8f6f19c5ea
Francis Turmel <fturmel@gmail.com>
2018-03-10 12:25:50 +0100

    Starting fresh with a Lumen 5.6 default skeleton

---
+++ frameworks/PHP/lumen/routes/web.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
