ee4187c4d5bb8d0739f348d65a8ef49cf23dc66d
Nate <nathan.brady@yahoo.com>
2018-03-16 16:13:37 -0700

    Revert "add Nim's web framework "mofuw" on Docker (#3417)"
    This reverts commit 7aa103e2e9d0d7758303544bec2133b4dd8845a2.

--- frameworks/Nim/mofuw/techempower.nim
+++
@@ -1,9 +0,0 @@
-import mofuw
-
-proc h(req: mofuwReq, res: mofuwRes) {.async.} =
-  if req.getPath == "/plaintext":
-    mofuwResp(HTTP200, "text/plain", "Hello, World!")
-  else:
-    mofuwResp(HTTP200, "text/plain", "404 NOT FOUND")
-
-h.mofuwRun(port = 8080, bufSize = 512)
