1bede0a20caa60da0fa4473ece240919ecaf868f
Michael Hixson <mhixson@techempower.com>
2018-03-23 09:27:01 -0700

    Try to get more useful info from travis about cakephp failures

--- frameworks/PHP/cakephp/app/Config/core.php
+++ frameworks/PHP/cakephp/app/Config/core.php
@@ -32,7 +32,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-	Configure::write('debug', 0);
+	Configure::write('debug', 2);
 
 /**
  * Configure the Error handler used to handle errors for your application.  By default
