4d916064d15499e28cb0efd3e402293fa28a54a2
maximelkin <maxelkin@list.ru>
2018-03-27 21:47:24 +0300

    Refactor koa (#3433)
    * refactoring koa
    
    * fix sequelize-postgres
    
    * make database-layer optional
    
    * fix benchmark config
    
    * fix wrong middleware
    add getting from db as plain objects
    remove __v key for mongoose
    
    * fixing database layer
    (wtf mongo use lowercase table name)
    
    * minify back fortunes view
    
    * postgres fix

--- frameworks/JavaScript/koa/create-server.js
+++ frameworks/JavaScript/koa/create-server.js
@@ -3,40 +3,45 @@ const Router = require('koa-router');
 const hbs = require('koa-hbs');
 const bodyParser = require('koa-bodyparser');
 const handlebars = require('handlebars');
-
-const Handler = require(`./handlers/${process.env.NODE_HANDLER}`);
+const handler = require('./handlers/handler');
 
 const app = new Koa();
 const router = new Router();
-app.use(bodyParser());
-app.use(hbs.middleware({
-  handlebars: handlebars,
-  viewPath: __dirname + '/views'
-}));
 
-function JsonSerialization(ctx, next) {
-  ctx.set('Server', 'Koa');
-  ctx.type = 'application/json';
-  ctx.body = { message: 'Hello, World!' };
-  return next();
+app
+  .use(async (ctx, next) => {
+    ctx.set('Server', 'Koa');
+    await next();
+  });
+
+router
+  .get('/json', (ctx) => {
+    ctx.body = {message: 'Hello, World!'};
+  })
+  .get('/plaintext', (ctx) => {
+    ctx.body = 'Hello, World!';
+  });
+
+const handlerName = process.env.NODE_HANDLER;
+
+if (handlerName) {
+  const dbLayer = require(`./handlers/${handlerName}`);
+
+  const routerHandler = handler(dbLayer);
+
+  router
+    .use(bodyParser())
+    .use(hbs.middleware({
+      handlebars: handlebars,
+      viewPath: __dirname + '/views'
+    }))
+    .get('/db', routerHandler.SingleQuery)
+    .get('/queries', routerHandler.MultipleQueries)
+    .get('/fortunes', routerHandler.Fortunes)
+    .get('/updates', routerHandler.Updates);
 }
-
-function Plaintext(ctx, next) {
-  ctx.set('Server', 'Koa');
-  ctx.type = 'text/plain';
-  ctx.body = 'Hello, World!';
-  return next();
-}
-
-router.get('/json', JsonSerialization);
-router.get('/plaintext', Plaintext);
-router.get('/db', Handler.SingleQuery);
-router.get('/queries', Handler.MultipleQueries);
-router.get('/fortunes', Handler.Fortunes);
-router.get('/updates', Handler.Updates);
-
 
 app.use(router.routes());
-const server = app.listen(8080);
-console.log('Worker started and listening on http://0.0.0.0:8080 ' 
-  + new Date().toISOString(" "));
+
+app.listen(8080);
+console.log(`Worker started and listening on http://0.0.0.0:8080 ${new Date().toISOString()}`);
