d84cebf2fd64a08dbac20ccc5c6b1bec11e4d632
Michael Hixson <mhixson@techempower.com>
2018-04-03 15:48:19 -0700

    [ci skip] Delete docker crap more aggressively

--- toolset/continuous/tfb-script-for-systemd.sh
+++ toolset/continuous/tfb-script-for-systemd.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-# Stop any leftover docker containers on this server and on the
-# database server, which might not have been cleaned up if the
-# previous run was forcibly stopped.
-docker stop $(docker ps -aq)
-ssh techempower@$TFB_DATABASE_HOST << \EOF
-  docker stop $(docker ps -aq)
-EOF
-ssh techempower@$TFB_CLIENT_HOST << \EOF
-  docker stop $(docker ps -aq)
-EOF
-
 while true
 do
+  docker rm -f $(docker ps -a -q)
+  ssh techempower@$TFB_DATABASE_HOST << \EOF
+    docker rm -f $(docker ps -a -q)
+EOF
+  ssh techempower@$TFB_CLIENT_HOST << \EOF
+    docker rm -f $(docker ps -a -q)
+EOF
+
   cd $TFB_REPOPARENT
   if [ -d "$TFB_REPOPARENT/$TFB_REPONAME" ]; then
     sudo rm -rf $TFB_REPOPARENT/$TFB_REPONAME
   fi
   git clone -b $TFB_REPOBRANCH $TFB_REPOURI $TFB_REPOPARENT/$TFB_REPONAME --depth 1
   cd $TFB_REPOPARENT/$TFB_REPONAME
-
-  docker run \
-    --network=host \
-    --mount type=bind,source=$TFB_REPOPARENT/$TFB_REPONAME,target=/FrameworkBenchmarks \
-    techempower/tfb \
-    --server-host $TFB_SERVER_HOST \
-    --client-host $TFB_CLIENT_HOST \
-    --database-host $TFB_DATABASE_HOST \
-    --network-mode host \
-    --clean
 
   docker run \
     --network=host \
