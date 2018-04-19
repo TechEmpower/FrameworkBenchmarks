1710aeaf4ca8eed01c2456ade55457950da7e8e9
Tommi Reiman <tommi@iki.fi>
2018-04-19 22:04:48 +0300

    aleph 0.4.5-alpha6

--- frameworks/Clojure/aleph/project.clj
+++ frameworks/Clojure/aleph/project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [clj-tuple "0.2.2"]
                  [org.clojure/tools.cli "0.3.6"]
-                 [aleph "0.4.4"]
+                 [aleph "0.4.5-alpha6"]
                  [javax.xml.bind/jaxb-api "2.3.0"]
                  [metosin/jsonista "0.1.1"]]
   :main hello.handler
