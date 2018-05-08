135f4738ff0f3088e2dfff56cff1b25cac7df09f
bre7 <bre7@users.noreply.github.com>
2018-05-08 19:13:04 -0300

    Fixed json test & middlewares comment

--- frameworks/Swift/vapor/Sources/vapor-tfb/main.swift
+++ frameworks/Swift/vapor/Sources/vapor-tfb/main.swift
@@ -5,18 +5,29 @@ var services = Services.default()
 
 // Router
 let router = EngineRouter.default()
+
 // Routes
-let json = Message(message: "Hello, world!")
-router.get("json") { req in
-    return json.encode(status: .ok, headers: ["Server": "Vapor"], for: req)
+
+// JSON test
+var jsonRes = HTTPResponse(status: .ok, headers: ["Server": "Vapor"])
+let jsonEncoder = JSONEncoder()
+
+router.get("json") { req -> Response in
+    let res = Response(http: jsonRes, using: req.sharedContainer)
+    try res.content.encode(json: Message(message: "Hello, world!"), using: jsonEncoder)
+    return res
 }
+
+// Plaintext test
 let plaintextRes = HTTPResponse(status: .ok, headers: ["Server": "Vapor", "Content-Type": "text/plain"], body: "Hello, world!")
+
 router.get("plaintext") { req in
     return plaintextRes
 }
+
 services.register(router, as: Router.self)
 
-// Middlewares (remove default mws)
+// Middlewares (remove unused ErrorMiddleware)
 var middlewares = MiddlewareConfig()
 services.register(middlewares)
 
