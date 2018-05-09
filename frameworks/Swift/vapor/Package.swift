f0f2d1aac28c44fbe996bc9aef49858190051f4a
Nathan <bre7@users.noreply.github.com>
2018-05-08 21:06:56 -0300

    Vapor 3 (#3683)
    * Vapor 3
    
    * Linux random() fix
    
    * New target for non-Fluent tests
    
    * Use req as DatabaseConnectable
    
    * Fixed JSON typo
    
    * random fix
    
    * Added new dockerfile
    
    * Remove old import
    
    * Added missing (and required) Middlewares
    
    * Use 0.0.0.0 instead of localhost
    
    * Change hostname in mysql target also 🤪
    
    * Dockerfile fixes (remove old flags)
    
    * Remove legacy/unused code
    
    * Updated packages
    
    * Use Vapor's ducker images
    
    * Route fixes, updated readme
    
    * Revert "Use Vapor's ducker images"
    
    This reverts commit a6bdeef910c0ca6dee35216fd925266a82390220.
    
    * Use IN instead of multiple requests
    
    * Use append instead of insert(_, at: 0)
    
    * Refactor query
    
    * Update to 3.0.0
    
    * Use transactions (awaiting for PR merge)
    
    * param decoding fixes
    
    * Removed mysql source data for the time being
    
    * Trimmed fat
    
    * Added Server-MW
    
    * perf
    
    * Removed toolbox from dockerfile
    
    * Removed legacy file
    
    * fix
    
    * Updated deps
    
    * Fixed json test & middlewares comment

--- frameworks/Swift/vapor/Package.swift
+++ frameworks/Swift/vapor/Package.swift
@@ -1,24 +1,12 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
   name: "tfb",
-  targets: [
-    Target(name: "vapor-tfb-mysql", dependencies: ["TfbCommon"]),
-    Target(name: "vapor-tfb-postgresql", dependencies: ["TfbCommon"]),
-    Target(name: "vapor-tfb-mongodb", dependencies: ["TfbCommon"])
-  ],
   dependencies: [
-    .Package(url: "https://github.com/vapor/vapor.git", "2.4.4"),
-    .Package(url: "https://github.com/vapor/postgresql-provider.git", majorVersion:2),
-    .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 2),
-    .Package(url: "https://github.com/vapor/mongo-provider.git", majorVersion: 2),
-    .Package(url: "https://github.com/vapor/leaf-provider.git", majorVersion: 1)
+    .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
   ],
-  exclude: [
-    "Config",
-    "Database",
-    "Localization",
-    "Public",
-    "Resources",
+  targets: [
+    .target(name: "vapor-tfb", dependencies: ["Vapor"], exclude: ["Resources"]),
   ]
 )
