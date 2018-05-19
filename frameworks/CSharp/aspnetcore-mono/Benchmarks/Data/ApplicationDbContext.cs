0232c4fac54ba7309db89140cd2fdf1d99593186
Ben Adams <thundercat@illyriad.co.uk>
2018-05-19 23:05:53 +0100

    Add aspnetcore-mono (ASP.NET Core on Mono)

---
+++ frameworks/CSharp/aspnetcore-mono/Benchmarks/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.EntityFrameworkCore;
+
+namespace Benchmarks.Data
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+            ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+            ChangeTracker.AutoDetectChangesEnabled = false;
+        }
+
+        public DbSet<World> World { get; set; }
+
+        public DbSet<Fortune> Fortune { get; set; }
+    }
+}
