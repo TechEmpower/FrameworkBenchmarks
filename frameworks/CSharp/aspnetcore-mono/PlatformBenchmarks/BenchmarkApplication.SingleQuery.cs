6904053fed6796397a8922eb77a74ddc511e0dc1
Ben Adams <thundercat@illyriad.co.uk>
2018-05-24 18:27:27 +0100

    Mono - Add Query scenerios

--- frameworks/CSharp/aspnetcore-mono/PlatformBenchmarks/BenchmarkApplication.SingleQuery.cs
+++ frameworks/CSharp/aspnetcore-mono/PlatformBenchmarks/BenchmarkApplication.SingleQuery.cs
@@ -1,21 +1,21 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
+using Utf8Json;
 
 namespace PlatformBenchmarks
 {
     public partial class BenchmarkApplication
     {
-        private async Task Fortunes(PipeWriter pipeWriter)
+        private async Task SingleQuery(PipeWriter pipeWriter)
         {
-            OutputFortunes(pipeWriter, await Db.LoadFortunesRows());
+            OutputSingleQuery(pipeWriter, await Db.LoadSingleQueryRow());
         }
 
-        private static void OutputFortunes(PipeWriter pipeWriter, List<Fortune> model)
+        private static void OutputSingleQuery(PipeWriter pipeWriter, World row)
         {
             var writer = GetWriter(pipeWriter);
 
@@ -29,31 +29,18 @@ namespace PlatformBenchmarks
             writer.Write(DateHeader.HeaderBytes);
 
             // Content-Type header
-            writer.Write(_headerContentTypeHtml);
+            writer.Write(_headerContentTypeJson);
 
             // Content-Length header
             writer.Write(_headerContentLength);
-
-            var lengthWriter = writer;
-            writer.Write(_contentLengthGap);
+            var jsonPayload = JsonSerializer.SerializeUnsafe(row);
+            writer.WriteNumeric((uint)jsonPayload.Count);
 
             // End of headers
             writer.Write(_eoh);
 
-            var bodyStart = writer.Buffered;
             // Body
-            writer.Write(_fortunesTableStart);
-            foreach (var item in model)
-            {
-                writer.Write(_fortunesRowStart);
-                writer.WriteNumeric((uint)item.Id);
-                writer.Write(_fortunesColumn);
-                writer.WriteUtf8HtmlString(item.Message);
-                writer.Write(_fortunesRowEnd);
-            }
-            writer.Write(_fortunesTableEnd);
-            lengthWriter.WriteNumeric((uint)(writer.Buffered - bodyStart));
-
+            writer.Write(jsonPayload);
             writer.Commit();
         }
     }
