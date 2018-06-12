3d3823af934a9375f45f7a6c60ea1167d44b2394
Dmitry Kushnir <dmytro.kushnir@just-eat.com>
2018-06-12 16:51:15 +0300

    react to code review

--- frameworks/FSharp/giraffe/src/App/Program.fs
+++ frameworks/FSharp/giraffe/src/App/Program.fs
@@ -1,27 +1,17 @@
 module App.App
 
-open System
-open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.Extensions.Logging
-open Microsoft.Extensions.DependencyInjection
-open Giraffe
 open Microsoft.AspNetCore.Http
+open Giraffe
 
-// ---------------------------------
-// Models
-// ---------------------------------
 [<CLIMutable>]
 type JsonMessage = { message : string }
 
-// ---------------------------------
-// Web app
-// ---------------------------------
-
-let jsonUtf8 (data:obj) : HttpHandler =
-    fun (next : HttpFunc) (ctx : HttpContext) ->
+let jsonutf8 (data:obj) : HttpHandler =
+    fun (_ : HttpFunc) (ctx : HttpContext) ->
         let bytes = Utf8Json.JsonSerializer.Serialize(data)
         ctx.SetContentType "application/json"
+        ctx.Response.ContentLength <- new System.Nullable<int64>( int64 bytes.Length )
         ctx.WriteBytesAsync bytes
 
 let webApp =
@@ -30,43 +20,17 @@ let webApp =
             choose [
                 route "/plaintext" >=> text "Hello, World!"
                 route "/json" >=> json { message = "Hello, World!" }
-                route "/jsonutf8" >=> jsonUtf8 { message = "Hello, World!" }
+                route "/jsonutf8" >=> jsonutf8 { message = "Hello, World!" }
             ]
-        setStatusCode 404 >=> text "Not Found" ]
-
-// ---------------------------------
-// Error handler
-// ---------------------------------
-
-let errorHandler (ex : Exception) (logger : ILogger) =
-    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
-    clearResponse >=> setStatusCode 500 >=> text ex.Message
-
-// ---------------------------------
-// Config and Main
-// ---------------------------------
-
-let configureApp (app : IApplicationBuilder) =
-    let env = app.ApplicationServices.GetService<IHostingEnvironment>()
-    (match env.IsDevelopment() with
-    | true  -> app.UseDeveloperExceptionPage()
-    | false -> app.UseGiraffeErrorHandler errorHandler)      
-        .UseGiraffe(webApp)
-
-let configureServices (services : IServiceCollection) =    
-    services.AddGiraffe() |> ignore
-
-let configureLogging (builder : ILoggingBuilder) =
-    let filter (l : LogLevel) = l.Equals LogLevel.Error
-    builder.AddFilter(filter).AddConsole().AddDebug() |> ignore
-
+        setStatusCode 404 >=> text "Not Found" 
+    ]
+            
 [<EntryPoint>]
 let main _ =    
     WebHostBuilder()
         .UseKestrel()        
-        .Configure(Action<IApplicationBuilder> configureApp)
-        .ConfigureServices(configureServices)
-        .ConfigureLogging(configureLogging)
+        .Configure(fun app -> app.UseGiraffe(webApp))
+        .ConfigureServices(fun services -> services.AddGiraffe() |> ignore)
         .Build()
         .Run()
     0
