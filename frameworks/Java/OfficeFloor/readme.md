870922e328ba170605ea3f04c6ef4db9ad30cb8d
Daniel Sagenschneider <daniel.sagenschneider@projix.com.au>
2018-06-18 21:09:26 +0800

    Providing readme placeholder for OfficeFloor

---
+++ frameworks/Java/OfficeFloor/readme.md
@@ -0,0 +1,1 @@
+OfficeFloor
--- frameworks/Java/officefloor/external/run_comparison.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Find tfb directory
-while [ ! -f tfb ]; do
-	cd ..
-done
-
-# Run comparision
-./tfb --test h2o actix-raw rapidoid-http-fast vertx-postgres vertx-web-postgres
