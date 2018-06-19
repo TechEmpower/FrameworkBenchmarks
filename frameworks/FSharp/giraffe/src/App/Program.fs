047245a511d6e3fea975f2411b0dfdd392d89b5c
Dmitry Kushnir <dmykush@gmail.com>
2018-06-20 00:07:55 +0300

    Fsharp giraffe fortunes (#3863)

--- frameworks/FSharp/giraffe/src/App/Program.fs
+++ frameworks/FSharp/giraffe/src/App/Program.fs
@@ -2,61 +2,27 @@ module App.App
 
 open Microsoft.AspNetCore.Hosting
 open Giraffe
-
-[<CLIMutable>] 
-type JsonMessage = { message : string }
-
-[<CLIMutable>][<Struct>] 
-type JsonStructMessage = { message : string }
-
-type Implementation = Stock | Custom
-
-module Routes =
-
-    let stock : HttpHandler list = 
-        [ route "/plaintext" >=> text "Hello, World!"
-          route "/json" >=> json { JsonMessage.message = "Hello, World!" } ]
-
-    let custom : HttpHandler list = 
-        let inline contentLength (x:int32) = new System.Nullable<int64>( int64 x )
-
-        let json data : HttpHandler =
-            let bytes = Utf8Json.JsonSerializer.Serialize(data)
-            fun _ ctx ->
-                ctx.Response.ContentLength <- contentLength ( bytes.Length )
-                ctx.Response.ContentType <- "application/json"
-                ctx.Response.StatusCode <- 200
-                ctx.WriteBytesAsync bytes
-
-        let bytes = System.Text.Encoding.UTF8.GetBytes "Hello, World!"
-        let text : HttpHandler = 
-            fun _ ctx ->
-                ctx.Response.ContentLength <- contentLength ( bytes.Length )
-                ctx.Response.ContentType <- "text/plain"
-                ctx.Response.StatusCode <- 200
-                ctx.WriteBytesAsync bytes
-    
-        [ route "/plaintext" >=> text
-          route "/json" >=> json { JsonStructMessage.message = "Hello, World!" } ]
-
-let webApp implementation = 
-    match implementation with
-    | Stock -> GET >=> choose Routes.stock
-    | Custom -> GET >=> choose Routes.custom
+open Models
 
 [<EntryPoint>]
 let main args = 
     let implementation = 
         match args with
-        | [| "stock" |]  -> Stock
-        | _ -> Custom
+        | [| "stock" |] -> Implementation.Stock
+        | _ -> Implementation.Custom
 
     printfn "Running with %A implementation" implementation
 
+    let webApp = function
+    | Implementation.Custom -> Custom.application
+    | Implementation.Stock -> Stock.application
+
+    let app = webApp implementation
+
     WebHostBuilder()
         .UseKestrel()
-        .Configure(fun app -> app.UseGiraffe (webApp implementation))
-        .ConfigureServices(fun services -> services.AddGiraffe() |> ignore)
+        .Configure(fun b -> b.UseGiraffe app)
+        .ConfigureServices(fun s -> s.AddGiraffe() |> ignore)
         .Build()
         .Run()
     0
