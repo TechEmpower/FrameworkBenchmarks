6c1e54b129e16c317b787f79aff21a9715f16158
getong <getong@users.noreply.github.com>
2018-06-20 16:05:16 +0800

    add public_key

--- frameworks/Erlang/cowboy/src/hello_world.erl
+++ frameworks/Erlang/cowboy/src/hello_world.erl
@@ -9,6 +9,7 @@
 
 start() ->
 	ok = application:start(crypto),
+	ok = application:start(public_key),
 	ok = application:start(ssl),
 	ok = application:start(ranch),
 	ok = application:start(cowboy),
