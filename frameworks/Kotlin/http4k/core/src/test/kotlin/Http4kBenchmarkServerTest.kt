c2c0e345418b88be6218393e5acb1f1a4e909520
David Denton <denton.david@gmail.com>
2018-06-30 12:41:58 +0100

    added more tests

--- frameworks/Kotlin/http4k/core/src/test/kotlin/Http4kBenchmarkServerTest.kt
+++ frameworks/Kotlin/http4k/core/src/test/kotlin/Http4kBenchmarkServerTest.kt
@@ -1,9 +1,15 @@
+
 import com.natpryce.hamkrest.should.shouldMatch
+import io.mockk.every
 import io.mockk.mockk
 import org.http4k.core.Method.GET
 import org.http4k.core.Request
+import org.http4k.format.Jackson.number
+import org.http4k.format.Jackson.obj
 import org.http4k.hamkrest.hasBody
 import org.junit.jupiter.api.Test
+import java.sql.Connection
+import java.sql.PreparedStatement
 
 class Http4kBenchmarkServerTest {
 
@@ -19,4 +25,32 @@ class Http4kBenchmarkServerTest {
     fun `plaintext`() {
         app(Request(GET, "/plaintext")) shouldMatch hasBody("Hello, World!")
     }
+
+    @Test
+    fun `single`() {
+        connectionReturns(obj("id" to number(1), "randomNumber" to number(2)))
+        app(Request(GET, "/db")) shouldMatch hasBody("""{"id":1,"randomNumber":2}""")
+    }
+
+    @Test
+    fun `queries`() {
+        connectionReturns(listOf(obj("id" to number(1), "randomNumber" to number(2))))
+        app(Request(GET, "/queries").query("queries", "500")) shouldMatch hasBody("""[{"id":1,"randomNumber":2}]""")
+    }
+
+    @Test
+    fun `updates`() {
+        connectionReturns(listOf(obj("id" to number(1), "randomNumber" to number(2))))
+        app(Request(GET, "/updates").query("queries", "500")) shouldMatch hasBody("""[{"id":1,"randomNumber":2}]""")
+    }
+
+    @Test
+    fun `fortunes`() {
+        statementReturns(listOf(Fortune(1, "hello world")))
+        app(Request(GET, "/fortunes")) shouldMatch hasBody(this.javaClass.getResourceAsStream("/expectedfortunes.html").reader().readText())
+    }
+
+    private fun <T> connectionReturns(t: T) = every { database.withConnection(any<(Connection) -> T>()) } returns t
+    private fun <T> statementReturns(t: T) = every { database.withStatement(any(), any<(PreparedStatement) -> T>()) } returns t
+
 }
