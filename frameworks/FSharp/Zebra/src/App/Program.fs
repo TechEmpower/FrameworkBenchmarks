1571b7e2f44187812ea1d423279225d538433670
Gerard O'Connor <gerardtoconnor@gmail.com>
2018-07-17 09:05:06 +0100

    fixed up custom operations

--- frameworks/FSharp/Zebra/src/App/Program.fs
+++ frameworks/FSharp/Zebra/src/App/Program.fs
@@ -3,36 +3,14 @@ module App.App
 open State
 open Router
 open ExecNodes
+open Middleware
+open Giraffe
 open System.Threading.Tasks
 open System.Runtime.CompilerServices
 open Microsoft.AspNetCore.Http
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Builder
-open Giraffe
 
-type ZebraMiddleware<'T>(
-                        next          : RequestDelegate,
-                        Dependencies  : 'T,
-                        failAction    : State<'T> -> unit,
-                        AppBuilder    : PipeLine<'T>
-                        ) =
-    let finishNode = FinishNode() :> INode<'T>
-    let failNode = FailNode<'T>(failAction,finishNode)
-    let appNode = AppBuilder(finishNode,failNode) // build App node
-
-    do System.GC.Collect() // AppBuilder creates alot of garbage
-    
-    member __.Invoke (ctx : HttpContext) = 
-        
-        let amb = AsyncTaskMethodBuilder()
-        let state = State<'T>(ctx,Dependencies,amb)
-        appNode.Apply state
-        amb.Task
-
-
-type IApplicationBuilder with
-    member x.UseZebraMiddleware<'T>(dependencies:'T,fallback:Zapp<'T>,app:PipeLine<'T>) = 
-        x.UseMiddleware<ZebraMiddleware<'T>> [|box dependencies;box fallback;box app|] 
 
 // [<CLIMutable>] 
 // type JsonMessage = { message : string }
@@ -45,14 +23,14 @@ type JsonStructMessage = { message : string }
 let main _ = 
     
     let fallback : Zapp<_> = (fun ctx -> ctx {
-        ctx.Text "Url Not Found"
-        ctx.SetStatus 404        
+        text "Url Not Found"
+        status 404        
     })
 
     let webapp = 
         router [
-            get "/plaintext" => (fun ctx -> ctx { ctx.Text "Hello, World!" } )
-            get "/json"      => (fun ctx -> ctx { ctx.Json {JsonStructMessage.message = "Hello, World!"} } )
+            get "/plaintext" => (fun ctx -> ctx { text "Hello, World!" } )
+            get "/json"      => (fun ctx -> ctx { json {JsonStructMessage.message = "Hello, World!"} } )
         ]
 
     WebHostBuilder()
--- frameworks/FSharp/Zebra/src/App/State.fs
+++ frameworks/FSharp/Zebra/src/App/State.fs
@@ -6,9 +6,11 @@ open System.Runtime.CompilerServices
 open System.Threading
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Primitives
+open System.Xml.Serialization
 open StreamBuffer
 open System.IO
 open EncodeHelper
+open System.Net.Mime
 
 [<Struct>]
 type GenericStateAwaiter<'T>(awaiter:TaskAwaiter<'T> ,continuation:'T -> unit,methodBuilder:AsyncTaskMethodBuilder) =
@@ -24,31 +26,23 @@ type PlainStateAwaiter(continuation:unit -> unit,methodBuilder:AsyncTaskMethodBu
                 continuation ()  // runs cont, will update awaiter & continuation
         member __.SetStateMachine sm = methodBuilder.SetStateMachine sm
 
-
 [<Struct>]
-type FinishStateAwaiter(methodBuilder:AsyncTaskMethodBuilder,ctx:HttpContext) =
-    interface IAsyncStateMachine with
-        member __.MoveNext() =         
-            //ctx.Response.Body.Flush()
-            methodBuilder.SetResult()
-            
-            ctx.Abort()
-        member __.SetStateMachine sm = methodBuilder.SetStateMachine sm    
-
-[<Struct>]
-type FinishStateMemoryAwaiter(methodBuilder:AsyncTaskMethodBuilder,ms:MemoryStream) =
+type FinishStateAwaiter(methodBuilder:AsyncTaskMethodBuilder,ms:MemoryStream) =
     interface IAsyncStateMachine with
         member __.MoveNext() =         
             MemoryStreamCache.Release ms
             methodBuilder.SetResult()
-        member __.SetStateMachine sm = methodBuilder.SetStateMachine sm
- 
+        member __.SetStateMachine sm = methodBuilder.SetStateMachine sm  
+
 
 ////////////////
 // nodes are embedded in list so on each node load there is no additional calls to fucntions
 type ContentType =
 | Text = 0uy
 | Json = 1uy
+| Xml = 2uy
+| Custom = 3uy
+
 
 type INode<'T> =
     abstract member Next  : INode<'T> with get, set
@@ -56,9 +50,9 @@ type INode<'T> =
     abstract member Apply : State<'T> -> unit
 
 and State<'T>(hctx: HttpContext, deps: 'T,amb: AsyncTaskMethodBuilder) =
-        //let buffer = MemoryStreamCache.Get()
+        let buffer = MemoryStreamCache.Get()
         
-        member val MethodBuilder : AsyncTaskMethodBuilder = amb with get,set
+        member val MethodBuilder : AsyncTaskMethodBuilder = amb with get
         member val HttpContext              = hctx with get,set
         member val Dependencies             = deps with get // alternative dependency injection system
         member val DNode : INode<'T>        = Unchecked.defaultof<INode<'T>> with get, set // <<<TEMP
@@ -66,7 +60,7 @@ and State<'T>(hctx: HttpContext, deps: 'T,amb: AsyncTaskMethodBuilder) =
         member val PathPosition             = 0 with get, set // pos starts 0 and mutates via routers
 
         member val Disposables              = List.empty<IDisposable> with get, set
-        //member __.Buffer with get () = buffer
+        member __.Buffer with get () = buffer
         member val ContentType = ContentType.Text with get, set
 
         member inline x.Next   () = x.DNode.Next.Apply x
@@ -74,15 +68,25 @@ and State<'T>(hctx: HttpContext, deps: 'T,amb: AsyncTaskMethodBuilder) =
                    
         // Computaion Expresssion Members
         /////////////////////////////////
+        member inline x.Run(_:unit) = () // runs last on customOperations, runs after first bind
+
+        member inline x.Run(n:INode<'T>) = // runs last on customOperations, runs after first bind
+            n.Apply x
+
+
         member inline x.Zero() =
             // Zero presumes a true, failing must be explicit, progressing is implicit 
             x.DNode.Next.Apply x
-        member x.Return(_:unit) = x.DNode.Next.Apply x
-        member x.Return(result: bool) = 
+
+        member inline x.Return(_:unit) = x.DNode.Next 
+        member inline x.Return(result: bool) = // runs after last bind
             if result then
                 x.DNode.Next.Apply x
             else
                 x.DNode.Fail.Apply x
+        member inline x.Return(a) = () // passthrough bound values for now
+
+        member inline x.Bind<'a,'b>(n:INode<'T>,b:unit -> unit) = b () // input is output of last custom op
 
         member inline x.Bind(task:Task, continuation : unit -> unit) : unit =
             let awt : TaskAwaiter = task.GetAwaiter()
@@ -91,7 +95,7 @@ and State<'T>(hctx: HttpContext, deps: 'T,amb: AsyncTaskMethodBuilder) =
             else
                 let mutable awtref = awt
                 let mutable smref = PlainStateAwaiter(continuation,x.MethodBuilder)
-                x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)    
+                x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
 
         member inline x.Bind(configurableTaskLike:Task< ^inp>, continuation : ^inp -> unit) : unit =
             let awt : TaskAwaiter< ^inp> = configurableTaskLike.GetAwaiter() 
@@ -100,109 +104,104 @@ and State<'T>(hctx: HttpContext, deps: 'T,amb: AsyncTaskMethodBuilder) =
             else
                 let mutable awtref = awt
                 let mutable smref =  GenericStateAwaiter< ^inp>(awt,continuation,x.MethodBuilder)
-                x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
-                        
+                x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)          
 
         member inline x.Using(disp : #IDisposable, continuation : #IDisposable -> unit) =
             x.Disposables <- disp :> IDisposable :: x.Disposables
             continuation disp
                
-        member x.SetComplete() =            
-            // if x.Buffer.Length > 0L then
-            //     x.HttpContext.Response.Headers.["Content-Length"] <- StringValues(x.Buffer.Length.ToString())
-            //     x.HttpContext.Response.StatusCode <- 200
-            //     x.HttpContext.Response.Headers.["Content-Encoding"] <- StringValues("identity")
-            //     x.HttpContext.Response.Headers.["Content-Type"] <- 
-            //         match x.ContentType with
-            //         | ContentType.Text -> "text/plain" 
-            //         | ContentType.Json -> "application/json"
-            //         | _ -> "text/plain"
-            //         |> StringValues
+        member inline x.SetComplete() =
+            let inline contType (v:string) = x.HttpContext.Response.Headers.["Content-Type"] <- StringValues v
+            
+            if x.Buffer.Length > 0L then
+                x.HttpContext.Response.Headers.["Content-Length"] <- StringValues(x.Buffer.Length.ToString())
+                x.HttpContext.Response.StatusCode <- 200
+                match x.ContentType with
+                | ContentType.Custom -> ()
+                | ContentType.Text -> contType "text/plain" 
+                | ContentType.Json -> contType "application/json"
+                | ContentType.Xml  -> contType "application/xml"
+                | _                -> contType "text/plain"
 
-            //     x.Buffer.Seek(0L,SeekOrigin.Begin) |> ignore
+                x.Buffer.Seek(0L,SeekOrigin.Begin) |> ignore
 
-            //     let t = x.Buffer.CopyToAsync(x.HttpContext.Response.Body)
-            //     let awt = t.GetAwaiter()
-            //     let mutable awtref = awt
-            //     let mutable smref = FinishStateAwaiter(x.MethodBuilder,x.Buffer)
-            //     x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
+                let t = x.Buffer.CopyToAsync(x.HttpContext.Response.Body)
+                let awt = t.GetAwaiter()
+                let mutable awtref = awt
+                let mutable smref = FinishStateAwaiter(x.MethodBuilder,x.Buffer)
+                x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
 
-            // else
-
-            //     x.HttpContext.Response.Headers.["Content-Length"] <- StringValues("0")
-            //     x.HttpContext.Response.StatusCode <- 404
-            //     x.HttpContext.Response.Body.Flush()
-            //     x.MethodBuilder.SetResult()
+            else                
+                x.HttpContext.Response.Headers.["Content-Length"] <- StringValues("0")
+                x.HttpContext.Response.StatusCode <- 404
+                x.MethodBuilder.SetResult()
 
             // at end of pipeline when buffer written, we can start disposing waiting for Async copy to finish                         
-            for disp in x.Disposables do disp.Dispose()            
-
+            for disp in x.Disposables do disp.Dispose()   // may need to be pushed out to FinishStateAwaiter
         
         // Custom Ops
         /////////////////////////////
-         
         
+
         // Text
-        member inline x.Text(text: string ) =
-            let ctx = x.HttpContext
-            x.HttpContext <- Unchecked.defaultof<HttpContext>
-            ctx.Response.Headers.["Content-Length"] <- StringValues(text.Length.ToString())            
-            ctx.Response.Headers.["Content-Type"] <- StringValues "text/plain"
-            ctx.Response.Headers.["Content-Encoding"] <- StringValues "identity"
+        ////////
+        member inline x.Text(text: string ) = StreamWrite(text,x.Buffer) ;    
 
-            ctx.Response.StatusCode <- 200
-            let t = ctx.Response.WriteAsync(text)
-            let awt = t.GetAwaiter()
-            let mutable awtref = awt
-            let mutable smref = FinishStateAwaiter(x.MethodBuilder,ctx)
-            x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
-                        
-            for disp in x.Disposables do disp.Dispose()
-
-        // [<CustomOperation("text",MaintainsVariableSpaceUsingBind=true)>] //,MaintainsVariableSpaceUsingBind = true)>]
+        [<CustomOperation("text",MaintainsVariableSpaceUsingBind=true)>] //,MaintainsVariableSpaceUsingBind = true)>]
         
-        // /// **Description**
-        // ///   Writes text to the state buffer that will be flushed at the end  
-        // /// **Parameters**
-        // ///   * `
-        // ///   * `text` - parameter of type `string`
+        /// **Description**
+        ///   Writes text to the state buffer that will be flushed at the end  
+        /// **Parameters**
+        ///   * `
+        ///   * `text` - parameter of type `string`
 
-        // member inline x.Text<'a>(_:unit, text: string ) = x.Text(text)
-
+        member inline x.Text<'a>(n:INode<'T>, text: string ) =  x.Text(text); n
 
         // Json
-        member inline x.Json< ^a>(value: ^a ) =
-            let buffer = MemoryStreamCache.Get()
-            x.HttpContext.Response.Headers.["Content-Type"] <- StringValues "application/json"
-            
-            
-            Utf8Json.JsonSerializer.Serialize< ^a>(buffer,value)
-            x.HttpContext.Response.Headers.["Content-Length"] <- StringValues(buffer.Length.ToString())            
-            buffer.Seek(0L,SeekOrigin.Begin) |> ignore
-            let t = buffer.CopyToAsync(x.HttpContext.Response.Body)
-            let awt = t.GetAwaiter()
-            let mutable awtref = awt
-            let mutable smref = FinishStateMemoryAwaiter(x.MethodBuilder,buffer)
-            x.MethodBuilder.AwaitUnsafeOnCompleted(&awtref,&smref)
-            
-            for disp in x.Disposables do disp.Dispose()
+        //////////
         
-        // [<CustomOperation("json",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
-        // member inline x.Json< ^a>(_:unit, value: ^a ) = x.Json(value)
+        member inline x.Json< ^a>(value: ^a ) =
+            Utf8Json.JsonSerializer.Serialize< ^a>(x.Buffer,value)
+            x.ContentType <- ContentType.Json
+        [<CustomOperation("json",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
+        member inline x.Json< ^a>(n:INode<'T>, value: ^a ) =  x.Json< ^a>(value: ^a); n
 
 
-        // SetHeader
+        // XML
+        /////////
+        member inline x.Xml< ^a>(value: ^a ) =
+            let ser = XmlSerializer(typeof< ^a>).Serialize(x.Buffer,value)   // <<< should find better serialiser
+            x.ContentType <- ContentType.Xml
+
+        [<CustomOperation("xml",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
+        member inline x.Xml< ^a>(n:INode<'T>, value: ^a ) = x.Xml< ^a>(value: ^a) ; n
+        
+        // setHeader
+        ////////////
         member inline x.SetHeader(header: string, value:string ) =
-            x.HttpContext.Response.Headers.[header] <- StringValues(value)    
+            x.HttpContext.Response.Headers.[header] <- StringValues(value)  
 
-        // [<CustomOperation("setHeader",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
-        // member inline x.SetHeader(_:unit, header: string, value:string ) = x.SetHeader(header,value)
-    
+        [<CustomOperation("setHeader",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
+        member inline x.SetHeader(n:INode<'T>, header: string, value:string ) = x.SetHeader(header, value) ; n
+
+        // status code
+        ///////////////
         member inline x.SetStatus(value:int ) =
-            x.HttpContext.Response.StatusCode <- value  
+            x.HttpContext.Response.StatusCode <- value
 
-        // [<CustomOperation("status",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
-        // member inline x.SetStatus(_:unit, value:int ) = x.SetStatus(value)
+        [<CustomOperation("status",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
+        member inline x.SetStatus(n:INode<'T>, value:int ) = x.SetStatus(value) ; n
+
+        // content_type
+        ////////////////////
+        member inline x.SetContentType(value:string ) =
+            x.ContentType <- ContentType.Custom
+            x.HttpContext.Response.Headers.["Content-Type"] <- StringValues value
+
+        
+        [<CustomOperation("content_type",MaintainsVariableSpaceUsingBind=true)>]//,MaintainsVariableSpaceUsingBind = true)>]
+        member inline x.SetContentType(_:unit, value:string ) = x.SetContentType(value)
+
 
 
 and Zapp<'T> = State<'T> -> unit
