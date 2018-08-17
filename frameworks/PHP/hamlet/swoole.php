e902ee04081076e10167f5a2800019020e3489dd
Vasily Kartashov <vasily.kartashov@hunterindustries.com>
2018-08-17 15:01:31 +1000

    Adding Hamlet 3 Adding Hamlet 3 / Swoole Integration

--- frameworks/PHP/hamlet/swoole.php
+++ frameworks/PHP/hamlet/swoole.php
@@ -2,6 +2,6 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-\Hamlet\Bootstraps\ServerBootstrap::run(function () {
+\Hamlet\Bootstraps\SwooleBootstrap::run('0.0.0.0', 8080, function () {
     return new \Benchmark\Application;
 });
