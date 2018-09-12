691eadcc504422acca169345d82e380d757f2e6d
jcoleman-techempower <jcoleman@techempower.com>
2018-09-12 16:21:14 -0700

    Adds an Nginx configuration for Hapi and Postgres

---
+++ frameworks/JavaScript/hapi/start-servers.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PORT=3000 node create-server.js &
+PORT=3001 node create-server.js &
+PORT=3002 node create-server.js &
+PORT=3003 node create-server.js &
+PORT=3004 node create-server.js &
+PORT=3005 node create-server.js &
+PORT=3006 node create-server.js &
+PORT=3007 node create-server.js &
