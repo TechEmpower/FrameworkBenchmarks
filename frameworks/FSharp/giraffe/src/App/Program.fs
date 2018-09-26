b6f01696a9d090c3638fdeb2ae7e387316fb8151
Dmitry Kushnir <dmytro.kushnir@just-eat.com>
2018-09-26 15:04:47 +0300

    use utf8json as a default serializer

--- frameworks/FSharp/giraffe/src/App/Program.fs
+++ frameworks/FSharp/giraffe/src/App/Program.fs
@@ -3,6 +3,8 @@ module App.App
 open Microsoft.AspNetCore.Hosting
 open Giraffe
 open Models
+open Microsoft.Extensions.DependencyInjection
+open Giraffe.Serialization
 
 [<EntryPoint>]
 let main args = 
@@ -22,7 +24,10 @@ let main args =
     WebHostBuilder()
         .UseKestrel()
         .Configure(fun b -> b.UseGiraffe app)
-        .ConfigureServices(fun s -> s.AddGiraffe() |> ignore)
+        .ConfigureServices(fun s ->
+            s.AddGiraffe() |> ignore
+            s.AddSingleton<IJsonSerializer>(Utf8JsonSerializer(Utf8JsonSerializer.DefaultResolver)) |> ignore
+        )
         .Build()
         .Run()
     0
