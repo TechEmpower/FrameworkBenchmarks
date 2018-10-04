958e5278b095032622bbf87fabb360f2ee624fda
zloster <more@edno.moe>
2018-10-04 18:36:03 +0300

    Dependencies update - PostgreSQL JDBC, MySQL Connector/J

--- frameworks/Clojure/compojure/project.clj
+++ frameworks/Clojure/compojure/project.clj
@@ -7,7 +7,7 @@
                  [ring/ring-json "0.4.0"]
                  [korma "0.4.2"]
                  [log4j "1.2.15" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
-                 [mysql/mysql-connector-java "5.1.38"]
+                 [mysql/mysql-connector-java "5.1.47"]
                  [com.mchange/c3p0 "0.9.5.2"]
                  [org.clojure/java.jdbc "0.3.7"]
                  [hikari-cp "1.5.0"]
