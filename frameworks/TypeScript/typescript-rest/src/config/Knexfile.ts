7cdc99a5e394ee13b515d6d3cabd75e8f3b3643c
jcoleman-techempower <jcoleman@techempower.com>
2018-10-10 17:36:07 -0700

    Final push for the day

--- frameworks/TypeScript/typescript-rest/src/config/Knexfile.ts
+++ frameworks/TypeScript/typescript-rest/src/config/Knexfile.ts
@@ -1,13 +1,15 @@
+// tslint:disable:max-line-length
 import { Config, ConnectionConfig, PoolConfig } from "knex";
 
-const client: string = "postgresql"; // can also be "pg"
+const client: string = "pg"; // can also be "pg"
 const pool: Readonly<PoolConfig> = { min: 2, max: 10 };
-const connection: Readonly<ConnectionConfig> = {
-  host: "tfb-database",
-  user: "benchmarkdbuser",
-  password: "benchmarkdbpass",
-  database: "hello_world",
-};
+// const connection: Readonly<ConnectionConfig> = {
+//   host: "tfb-database",
+//   user: "benchmarkdbuser",
+//   password: "benchmarkdbpass",
+//   database: "hello_world",
+// };
+const connection: string = "postgres://benchmarkdbuser:benchmarkdbpass@tfb-database:5432/hello_world";
 
 export default <Config> {
   client,
