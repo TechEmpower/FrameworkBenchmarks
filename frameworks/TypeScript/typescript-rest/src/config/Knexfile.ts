c9a716751eed403d29a4b8b06f213e75b1294ffa
jcoleman-techempower <jcoleman@techempower.com>
2018-10-11 11:35:07 -0700

    Fixes rebase merge conflicts

--- frameworks/TypeScript/typescript-rest/src/config/Knexfile.ts
+++ frameworks/TypeScript/typescript-rest/src/config/Knexfile.ts
@@ -1,45 +1,9 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 // tslint:disable:max-line-length
-import { Config, ConnectionConfig, PoolConfig } from "knex";
+import { Config, PoolConfig } from "knex";
 
 const client: string = "pg"; // can also be "postgresql"
-<<<<<<< HEAD
 const pool: Readonly<PoolConfig> = { min: 2, max: 10 };
-// const connection: Readonly<ConnectionConfig> = {
-//   host: "tfb-database",
-//   user: "benchmarkdbuser",
-//   password: "benchmarkdbpass",
-//   database: "hello_world",
-// };
 const connection: string = "postgres://benchmarkdbuser:benchmarkdbpass@tfb-database:5432/hello_world";
-=======
-=======
-// tslint:disable:max-line-length
->>>>>>> Final push for the day
-import { Config, ConnectionConfig, PoolConfig } from "knex";
-
-const client: string = "pg"; // can also be "pg"
-=======
->>>>>>> Fixes broken tests
-const pool: Readonly<PoolConfig> = { min: 2, max: 10 };
-<<<<<<< HEAD
-const connection: Readonly<ConnectionConfig> = {
-  host: "tfb-database",
-  user: "benchmarkdbuser",
-  password: "benchmarkdbpass",
-  database: "hello_world",
-};
->>>>>>> Wraps up initial server configuration
-=======
-// const connection: Readonly<ConnectionConfig> = {
-//   host: "tfb-database",
-//   user: "benchmarkdbuser",
-//   password: "benchmarkdbpass",
-//   database: "hello_world",
-// };
-const connection: string = "postgres://benchmarkdbuser:benchmarkdbpass@tfb-database:5432/hello_world";
->>>>>>> Final push for the day
 
 export default <Config> {
   client,
