a1438ec1b50f774fafb262b0f4f0865a71be3676
vka <kartashov.vasily@googlemail.com>
2018-10-14 05:30:16 +1100

    Updating to Hamlet 3.1 (#4113)

--- frameworks/PHP/hamlet/react.php
+++ frameworks/PHP/hamlet/react.php
@@ -2,6 +2,5 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-\Hamlet\Bootstraps\ReactBootstrap::run('0.0.0.0:8080', function () {
-    return new \Benchmark\Application;
-});
+$application = new \Benchmark\Application;
+\Hamlet\Bootstraps\ReactBootstrap::run('0.0.0.0', 8080, $application);
