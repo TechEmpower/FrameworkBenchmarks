04cc2ff04d6c0a22b31edb581751a06ce9fe32ac
Nathan Tippy <tippyn@objectcomputing.com>
2018-11-15 17:19:06 -0600

    [ci fw-only Java/greenlightning]
    Was using 16-20G which may cause paging and slow results
    
    Reduced Pipe lenghts
    
    Reduced concurrent connections
    
    Combined behaviors to reduce Pipe counts for more memory.
    
    Lowered limit to 16G to know it is enforced.

--- frameworks/Java/greenlightning/src/main/java/com/ociweb/gl/benchmark/GreenLightning.java
+++ frameworks/Java/greenlightning/src/main/java/com/ociweb/gl/benchmark/GreenLightning.java
@@ -6,6 +6,8 @@ public class GreenLightning {
 
 	public static void main(String[] args) {
 		
+		//System.setProperty("pronghorn.processors", "28"); //this is what server will see in production test
+		//less stack per thread??
 		GreenRuntime.run(new FrameworkTest(),args);
 	
 	}
