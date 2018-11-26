988ff35922a62e9f912804632aab2b8ed1ec27e5
Nathan Tippy <tippyn@objectcomputing.com>
2018-11-25 20:26:28 -0600

    [ci fw-only Java/greenlightning]
    remove dead code

--- frameworks/Java/greenlightning/src/main/java/com/ociweb/gl/benchmark/GreenLightning.java
+++ frameworks/Java/greenlightning/src/main/java/com/ociweb/gl/benchmark/GreenLightning.java
@@ -5,11 +5,7 @@ import com.ociweb.gl.api.GreenRuntime;
 public class GreenLightning {
 
 	public static void main(String[] args) {
-		
-		//PipeConfig.showConfigsCreatedLargerThan = 1<<23;
-		///GraphManager.showThreadIdOnTelemetry = true;
-		
-		//System.setProperty("pronghorn.processors", "28"); 
+
 		GreenRuntime.run(new FrameworkTest(),args);
 	
 	}
