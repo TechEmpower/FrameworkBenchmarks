55f42f34058ae8aee9109b8c0b2a524ab382a0e6
IKende <henryfan@msn.com>
2018-12-14 10:43:21 +0800

    Merge to pr

--- frameworks/CSharp/beetlex/Benchmarks/Program.cs
+++ frameworks/CSharp/beetlex/Benchmarks/Program.cs
@@ -4,20 +4,12 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Threading;
-<<<<<<< HEAD
-=======
 using System.Text;
-
->>>>>>> master
 namespace Benchmarks
 {
     [BeetleX.FastHttpApi.Controller]
     class Program
     {
-<<<<<<< HEAD
-        public static void Main(string[] args)
-        {
-=======
         private static readonly byte[] _helloWorldPayload = Encoding.UTF8.GetBytes("Hello, World!");
 
         private static StringBytes plaintextResult;
@@ -27,7 +19,6 @@ namespace Benchmarks
         public static void Main(string[] args)
         {
             plaintextResult = new StringBytes(_helloWorldPayload);
->>>>>>> master
             var builder = new HostBuilder()
                 .ConfigureServices((hostContext, services) =>
                 {
@@ -39,34 +30,21 @@ namespace Benchmarks
         public object plaintext(IHttpContext context)
         {
             context.Response.Header[HeaderTypeFactory.DATE] = DateTime.Now.ToUniversalTime().ToString("r");
-<<<<<<< HEAD
-            return new TextResult("Hello, World!");
-=======
             return plaintextResult;
->>>>>>> master
         }
 
         public object json(IHttpContext context)
         {
             context.Response.Header[HeaderTypeFactory.DATE] = DateTime.Now.ToUniversalTime().ToString("r");
-<<<<<<< HEAD
-            return new JsonResult(new JsonMessage { message = "Hello, World!" });
-=======
             return new JsonResult(jsonMessage);
->>>>>>> master
         }
-
         public class JsonMessage
         {
             public string message { get; set; }
         }
     }
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> master
     public class BeetleXHttpServer : IHostedService
     {
         private HttpApiServer mApiServer;
