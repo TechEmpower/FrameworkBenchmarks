67f3469aa12e80c290ec63fdd98c70b5f97a6fc8
noboomu <noboomu@users.noreply.github.com>
2018-12-19 20:30:45 -0800

    Update blocking endpoints. Improve JVM params. (#4275)

--- frameworks/Java/proteus/run.sh
+++ frameworks/Java/proteus/run.sh
@@ -2,12 +2,13 @@
 java \
   -Dlogback.configurationFile="conf/logback.xml" \
   -Dconfig.file="conf/application.conf" \
-  -Xms2g \
-  -Xmx2g \
+  -Xms4g \
+  -Xmx4g \
   -XX:+AggressiveOpts \
   -server \
   -XX:-UseBiasedLocking \
   -XX:+UseStringDeduplication \
   -Djava.net.preferIPv4Stack=true \
+  -XX:+UseNUMA \
   -classpath "/proteus/target/proteus-techempower-1.0.0.jar:/proteus/target/lib/*" \
   io.sinistral.ExampleApplication
