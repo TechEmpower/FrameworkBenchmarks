5504c09a9139f164fba92948ba0a478b59d6e492
an-tao <antao2002@gmail.com>
2018-12-23 23:53:31 +0800

    Add benchmark tests for the Drogon framework

---
+++ frameworks/C++/drogon/drogon_benchmark/main.cc
@@ -0,0 +1,11 @@
+#include <drogon/drogon.h>
+int main() {
+    //Set HTTP listener address and port
+    app().setLogPath("./");
+    app().setLogLevel(trantor::Logger::WARN);
+    app().addListener("0.0.0.0", 8080);
+    app().setThreadNum(std::thread::hardware_concurrency());
+    //app().enableRunAsDaemon();
+    app().run();
+    return 0;
+}
