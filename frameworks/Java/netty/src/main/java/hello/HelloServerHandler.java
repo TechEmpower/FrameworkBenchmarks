a6d65f288f562093ece63dbdea1e1bff682292ef
Luis Neves <luis.neves@gmail.com>
2018-12-26 16:23:03 +0000

    remove unnecessary call to fireChannelRead()

--- frameworks/Java/netty/src/main/java/hello/HelloServerHandler.java
+++ frameworks/Java/netty/src/main/java/hello/HelloServerHandler.java
@@ -95,8 +95,6 @@ public class HelloServerHandler extends ChannelInboundHandlerAdapter {
 			} finally {
 				ReferenceCountUtil.release(msg);
 			}
-		} else {
-			ctx.fireChannelRead(msg);
 		}
 	}
 
