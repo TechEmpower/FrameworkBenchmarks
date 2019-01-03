f2ca74c315ffe79f3d3bc69a94da55adb5612616
David Kiss <davidakiss@gmail.com>
2019-01-03 13:14:03 -0500

    Add new benchmarks for spring-webflux using rxjava and r2dbc (#4314)

---
+++ frameworks/Java/spring-webflux/src/main/java/benchmark/config/R2dbcConfig.java
@@ -0,0 +1,59 @@
+package benchmark.config;
+
+import io.r2dbc.postgresql.PostgresqlConnectionConfiguration;
+import io.r2dbc.postgresql.PostgresqlConnectionFactory;
+import io.r2dbc.spi.ConnectionFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.r2dbc.function.DatabaseClient;
+
+@Configuration
+@Profile("r2dbc")
+@ConfigurationProperties(prefix = "database")
+public class R2dbcConfig {
+    private String name;
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Bean
+    public PostgresqlConnectionFactory connectionFactory() {
+        PostgresqlConnectionConfiguration configuration = PostgresqlConnectionConfiguration
+                .builder()
+                .host(host)
+                .port(port)
+                .database(name)
+                .username(username)
+                .password(password)
+                .build();
+        return new PostgresqlConnectionFactory(configuration);
+    }
+
+    @Bean
+    public DatabaseClient databaseClient(ConnectionFactory connectionFactory) {
+        return DatabaseClient.create(connectionFactory);
+    }
+}
