9a53e16ff32baa689c8389553c74879ff8188dca
Tuomas Lappeteläinen <tuomas.lappetelainen@gmail.com>
2019-01-12 20:45:46 +0200

    Rust/rocket/src/main.rs: update prelude for rocket
    As instructed in https://github.com/SergioBenitez/Rocket/blob/master/CHANGELOG.md#codegen-rewrite.

--- frameworks/Rust/rocket/src/main.rs
+++ frameworks/Rust/rocket/src/main.rs
@@ -1,8 +1,7 @@
-#![feature(plugin, custom_derive)]
-#![plugin(rocket_codegen)]
+#![feature(proc_macro_hygiene, decl_macro)]
 
 extern crate rand;
-extern crate rocket;
+#[macro_use] extern crate rocket;
 extern crate rocket_contrib;
 #[macro_use] extern crate diesel;
 #[macro_use] extern crate serde_derive;
