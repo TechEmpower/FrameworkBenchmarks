ca9dd77405862be7c4b7bacc0d551a8d212ac3b8
Carlos Donderis <cdonderis@gmail.com>
2019-02-22 10:30:29 +0900

    adds Vlad changes, and some fixes

--- frameworks/Crystal/onyx/src/server_postgres.cr
+++ frameworks/Crystal/onyx/src/server_postgres.cr
@@ -1,90 +1,110 @@
 require "pg"
-require "json"
+require "onyx/sql"
 require "onyx/rest"
-require "onyx-sql"
 
-
-BENCH_DB = DB.open(ENV["DATABASE_URL"])
-
-class CONTENT
-  JSON  = "application/json"
-  PLAIN = "text/plain"
-end
-
-# Models
 class World
   include Onyx::SQL::Model
 
   schema world do
     pkey id : Int32
-    type randomnumber : Int32
+    type random_number : Int32, key: "randomnumber"
   end
 end
 
-class Fortune
-  include Onyx::SQL::Model
-
-  schema fortune do
-    pkey id : Int32
-    type message : String
-  end
-end
-
-# Helper methods
 def random_id
   Random.rand(10_000).succ
 end
 
-def get_world
-  rs = BENCH_DB.query("SELECT * FROM world WHERE id = $1", random_id)
-  world = World.from_rs(rs).first
-end
+struct Views::World
+  include Onyx::REST::View
 
-struct QueriesParams
-  include HTTP::Params::Serializable
-  getter queries : Int32
-end
-
-def sanitized_query_count(env)
-  query = env.request.query
-  queries = (query ? QueriesParams.from_query(query).queries.to_i : 1) rescue 1
-  queries.clamp(1..500)
-end
-
-Onyx.draw do
-  get "/json" do |env|
-    env.response << { message: "Hello, World!" }.to_json
+  def initialize(@world : ::World)
   end
 
-  get "/db" do |env|
-    world = get_world
-    env.response << { id: world.id, randomnumber: world.randomnumber }.to_json
-  end
-
-  get "/queries" do |env|
-    results = (1..sanitized_query_count(env)).map do
-      world = get_world
-      { id: world.id, randomnumber: world.randomnumber }
+  json do
+    object do
+      field "id", @world.id
+      field "randomnumber", @world.random_number
     end
-    env.response << results.to_json
+  end
+end
+
+struct Views::Worlds
+  include Onyx::REST::View
+
+  def initialize(@worlds : Enumerable(::World))
   end
 
-  get "/updates" do |env|
-    updated = (1..sanitized_query_count(env)).map do
-      random_number = random_id
-      world = get_world
-      changeset = world.changeset
-      changeset.update(randomnumber: random_number)
-
-      BENCH_DB.exec(*world.update(changeset).build(true))
-      { id: world.id, randomnumber: random_number }
+  json do
+    array do
+      @worlds.each do |world|
+        Views::World.new(world).to_json(itself)
+      end
     end
-    env.response << updated.to_json
+  end
+end
+
+struct Actions::RandomWorld
+  include Onyx::REST::Action
+
+  QUERY = World.where(id: 0).build(true)[0]
+
+  def call
+    world = Onyx.query(World, QUERY, random_id).first
+    return Views::World.new(world)
+  end
+end
+
+struct Actions::ManyWorlds
+  include Onyx::REST::Action
+
+  QUERY = World.where("id = ANY(?)", 0).build(true)[0]
+
+  params do
+    query do
+      type queries : String | Int32 = 1
+    end
   end
 
-  get "/plaintext" do |env|
-    env.response.content_type = CONTENT::PLAIN
-    env.response << "Hello, World!"
+  def call
+    queries = params.query.queries.is_a?(String) ? 1 : params.query.queries.as(Int32)
+    ids = queries.clamp(1..500).times.map do
+      random_id
+    end.join(',').try do |s|
+      "{#{s}}"
+    end
+
+    worlds = Onyx.query(World, QUERY, ids)
+    return Views::Worlds.new(worlds)
+  end
+end
+
+struct Actions::UpdateWorlds
+  include Onyx::REST::Action
+
+  QUERY = World.update.set(random_number: 0).where(id: 0).build[0]
+
+  params do
+    query do
+      type queries : String | Int32 = 1
+    end
+  end
+
+  def call
+    queries = params.query.queries.is_a?(String) ? 1 : params.query.queries.as(Int32)
+
+    worlds = Array(World).new
+
+    queries.clamp(1..500).times.each do
+      id, number = {random_id, random_id}
+
+      world = World.new(id: id, random_number: number)
+      worlds << world
+
+      Onyx.exec(QUERY, world.random_number, world.id)
+    end
+
+    return Views::Worlds.new(worlds)
   end
 end
 
@@ -94,13 +114,28 @@ class CustomHandler
   def call(context)
     context.response.headers["Server"] = "Onyx"
     context.response.headers["Date"] = HTTP.format_time(Time.now)
-    context.response.content_type = CONTENT::JSON
     call_next(context)
   end
 end
 
+Onyx.draw do
+  get "/json" do |env|
+    env.response.content_type = "application/json"
+    {message: "Hello, World!"}.to_json(env.response)
+  end
+
+  get "/db", Actions::RandomWorld
+  get "/queries", Actions::ManyWorlds
+  get "/updates", Actions::UpdateWorlds
+
+  get "/plaintext" do |env|
+    env.response.content_type = "text/plain"
+    env.response << "Hello, World!"
+  end
+end
+
+Onyx.render(:json)
+
 Onyx.listen(ENV["TEST_HOST"], 8080) do
   handlers.insert(2, CustomHandler.new)
 end
-
-
