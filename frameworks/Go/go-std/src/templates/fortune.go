37990866adfc7aaf4963dac11ffcc1bb15772517
Andrew Chunariov <chunariov23@gmail.com>
2019-02-23 22:49:17 +0200

    fix iris 2

---
+++ frameworks/Go/go-std/src/templates/fortune.go
@@ -0,0 +1,39 @@
+package templates
+
+import "html/template"
+
+//go:generate qtc
+
+const (
+	fortuneHTML = `<!DOCTYPE html>
+<html>
+<head>
+<title>Fortunes</title>
+</head>
+<body>
+<table>
+<tr>
+<th>id</th>
+<th>message</th>
+</tr>
+{{range .}}
+<tr>
+<td>{{.Id}}</td>
+<td>{{.Message}}</td>
+</tr>
+{{end}}
+</table>
+</body>
+</html>`
+)
+
+var (
+	// FortuneTemplate ...
+	FortuneTemplate = template.Must(template.New("fortune.html").Parse(fortuneHTML))
+)
+
+// Fortune struct
+type Fortune struct {
+	ID      int    `json:"id,omitempty"`
+	Message string `json:"message,omitempty"`
+}
