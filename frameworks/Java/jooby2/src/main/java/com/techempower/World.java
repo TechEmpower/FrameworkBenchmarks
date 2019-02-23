fc87c77a56c1b58a085109ca02aa29c441dd9f97
Edgar Espina <espina.edgar@gmail.com>
2019-02-23 10:04:36 -0300

    Jooby 2.0.0.M1 (#4470)

---
+++ frameworks/Java/jooby2/src/main/java/com/techempower/World.java
@@ -0,0 +1,13 @@
+package com.techempower;
+
+public class World {
+
+  public final int id;
+  public final int randomNumber;
+
+  public World(int id, int randomNumber) {
+    this.id = id;
+    this.randomNumber = randomNumber;
+  }
+
+}
