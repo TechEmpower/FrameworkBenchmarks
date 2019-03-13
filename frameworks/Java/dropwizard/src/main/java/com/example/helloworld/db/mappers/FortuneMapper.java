1bd9b95ec4d5bfaa4ad80be672f731c2a3f85f72
Fred Deschenes <frederick.deschenes@gmail.com>
2019-03-13 15:58:43 -0400

    Dropwizard JDBI update (#4546)
    * Update Dropwizard to 1.3.9
    
    * Update JDBI to v3 in Dropwizard test

--- frameworks/Java/dropwizard/src/main/java/com/example/helloworld/db/mappers/FortuneMapper.java
+++ frameworks/Java/dropwizard/src/main/java/com/example/helloworld/db/mappers/FortuneMapper.java
@@ -3,14 +3,14 @@ package com.example.helloworld.db.mappers;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.skife.jdbi.v2.StatementContext;
-import org.skife.jdbi.v2.tweak.ResultSetMapper;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
 
 import com.example.helloworld.db.model.Fortune;
 
-public class FortuneMapper implements ResultSetMapper<Fortune> {
-	public Fortune map(int index, ResultSet r, StatementContext ctx)
-			throws SQLException {
-		return new Fortune(r.getInt("id"), r.getString("message"));
+public class FortuneMapper implements RowMapper<Fortune> {
+	@Override
+	public Fortune map(ResultSet rs, StatementContext ctx) throws SQLException {
+		return new Fortune(rs.getInt("id"), rs.getString("message"));
 	}
 }
