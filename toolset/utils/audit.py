74a2e06e25c84858c14dc0cc4f55f1ba1bed82ea
Nate <nathan.brady@yahoo.com>
2019-03-27 10:10:52 -0700

    Revert " Squash duplicate lines in logs when writing to file" (#4584)

--- toolset/utils/audit.py
+++ toolset/utils/audit.py
@@ -1,3 +1,4 @@
+from toolset.utils.output_helper import log
 from colorama import Fore
 
 
@@ -8,7 +9,6 @@ class Audit:
 
     def __init__(self, benchmarker):
         self.benchmarker = benchmarker
-        self.log = benchmarker.config.log
 
     def start_audit(self):
         for lang in self.benchmarker.metadata.gather_languages():
@@ -18,13 +18,13 @@ class Audit:
 
     def audit_test_dir(self, test_dir):
         warnings = 0
-        self.log('Auditing %s:' % test_dir, color=Fore.BLUE)
+        log('Auditing %s:' % test_dir, color=Fore.BLUE)
 
         if not self.benchmarker.metadata.has_file(test_dir, 'README.md'):
-            self.log('README.md file is missing')
+            log('README.md file is missing')
             warnings += 1
 
         if warnings:
-            self.log('(%s) warning(s)' % warnings, color=Fore.YELLOW)
+            log('(%s) warning(s)' % warnings, color=Fore.YELLOW)
         else:
-            self.log('No problems to report', color=Fore.GREEN)
+            log('No problems to report', color=Fore.GREEN)
