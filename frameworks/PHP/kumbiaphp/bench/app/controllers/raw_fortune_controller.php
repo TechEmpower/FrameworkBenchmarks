e8e2b396d2f42345ec33dd043c54a78f409db878
Joanhey <joanhey@kumbiaphp.com>
2019-04-01 18:10:30 +0200

    Simplify fortune-raw

--- frameworks/PHP/kumbiaphp/bench/app/controllers/raw_fortune_controller.php
+++ frameworks/PHP/kumbiaphp/bench/app/controllers/raw_fortune_controller.php
@@ -5,11 +5,10 @@ class RawFortuneController extends AppController
     public function index()
     {
         View::select(null,'raw');
-        $pdo = new PDO('mysql:host=tfb-database;dbname=hello_world', 'benchmarkdbuser', 'benchmarkdbpass', array(
+        $pdo = new PDO('mysql:host=tfb-database;dbname=hello_world', 'benchmarkdbuser', 'benchmarkdbpass', [
             PDO::ATTR_PERSISTENT => true
-        ));
-        $res = $pdo->query('SELECT id, message FROM Fortune', PDO::FETCH_KEY_PAIR);
-        $data = $res->fetchAll();
+        ]);
+        $data = $pdo->query('SELECT id, message FROM Fortune', PDO::FETCH_KEY_PAIR)->fetchAll();
 
         $data[0] = 'Additional fortune added at request time.';
         asort($data);       
