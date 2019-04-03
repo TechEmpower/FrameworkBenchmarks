d415bb2a9a5235f015b1f432ae712e89465059c4
Joan Miquel <joanhey@kumbiaphp.com>
2019-04-03 16:21:34 +0200

    Simplify php activerecords (#4606)
    * Simplify php-activerecord
    
    * Simplify php-eloquent
    
    * Simplify php-laravel-query-builder
    
    * Change require_once for require
    and delete commented code
    
    * Small change for travis

--- frameworks/PHP/php/eloquent/db-laravel-query-builder.php
+++ frameworks/PHP/php/eloquent/db-laravel-query-builder.php
@@ -1,38 +1,30 @@
 <?php
+// Set content type
+header('Content-type: application/json');
 
-require_once __DIR__ . '/init-capsule.php';
+require __DIR__.'/init-capsule.php';
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+if (! isset($_GET['queries'])) {
+    echo json_encode(
+        Capsule::table('World')->where('id', mt_rand(1, 10000))->get()->first()
+    );
+    return;
+}
+
 // Read number of queries to run from URL parameter
 $query_count = 1;
-$query_param = isset($_GET['queries']);
-if ($query_param && $_GET['queries'] > 0) {
+if ($_GET['queries'] > 1) {
     $query_count = $_GET['queries'] > 500 ? 500 : $_GET['queries'];
 }
-
 // Create an array with the response string.
-$arr = array();
-
+$arr = [];
 // For each query, store the result set values in the response array
-$query_counter = $query_count;
-while (0 < $query_counter--) {
-    // Choose a random row
-    // http://www.php.net/mt_rand
-    $id = mt_rand(1, 10000);
-
-    $world = Capsule::table('World')->where('id',$id)->get()->first();
-
+while (0 < $query_count--) {
     // Store result in array.
-    $arr[] = $world;
+    $arr[] = Capsule::table('World')->where('id', mt_rand(1, 10000))->get()->first();
 }
-
-if ($query_count === 1 && !$query_param) {
-    $arr = $arr[0];
-}
-
-// Set content type
-header("Content-type: application/json");
 
 // Use the PHP standard JSON encoder.
 // http://www.php.net/manual/en/function.json-encode.php
