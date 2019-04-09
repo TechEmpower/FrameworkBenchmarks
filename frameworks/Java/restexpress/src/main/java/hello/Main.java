5d2387b4cffd1978ef53b2337635e1a02f9305db
zloster <more@edno.moe>
2019-04-09 16:43:52 +0300

    Add separate implementations for db and query tests for MySQL

--- frameworks/Java/restexpress/src/main/java/hello/Main.java
+++ frameworks/Java/restexpress/src/main/java/hello/Main.java
@@ -14,44 +14,39 @@ import org.jboss.netty.handler.codec.http.HttpMethod;
 import com.strategicgains.restexpress.RestExpress;
 import com.strategicgains.restexpress.util.Environment;
 
-public class Main
-{
-	public static void main(String[] args) throws Exception
-	{
+public class Main {
+	public static void main(String[] args) throws Exception {
 		Configuration config = loadEnvironment(args);
-		RestExpress server = new RestExpress()
-			.setName("RestExpress Benchmark")
-		    .setExecutorThreadCount(config.getExecutorThreadPoolSize())
-		    .alias("HelloWorld", HelloWorld.class);
+		RestExpress server = new RestExpress().setName("RestExpress Benchmark")
+				.setExecutorThreadCount(config.getExecutorThreadPoolSize())
+				.alias("HelloWorld", HelloWorld.class);
 
-		server.uri("/restexpress/json", config.getJsonController())
-			.action("helloWorld", HttpMethod.GET);
+		server.uri("/restexpress/json", config.getJsonController()).action("helloWorld",
+				HttpMethod.GET);
 
-                server.uri("/restexpress/plaintext", config.getPlaintextController())
-                        .action("helloWorld", HttpMethod.GET)
-                        .noSerialization();
+		server.uri("/restexpress/plaintext", config.getPlaintextController())
+				.action("helloWorld", HttpMethod.GET).noSerialization();
 
-		server.uri("/restexpress/mysql", config.getMysqlController())
-			.method(HttpMethod.GET);
+		server.uri("/restexpress/mysql/db", config.getDbMysqlController()).method(HttpMethod.GET);
 
-		server.uri("/restexpress/mongodb", config.getMongodbController())
-		    .method(HttpMethod.GET);
+		server.uri("/restexpress/mysql/query", config.getQueriesMysqlController()).method(
+				HttpMethod.GET);
+
+		server.uri("/restexpress/mongodb", config.getMongodbController()).method(HttpMethod.GET);
 
 		server.addPostprocessor((request, response) -> {
 			response.addHeader("Server", "RestExpress");
-			response.addHeader("Date", DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)));
+			response.addHeader("Date",
+					DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now(ZoneOffset.UTC)));
 		});
 
 		server.bind(config.getPort());
 		server.awaitShutdown();
 	}
 
-	private static Configuration loadEnvironment(String[] args)
-	throws FileNotFoundException,
-	    IOException
-	{
-		if (args.length > 0)
-		{
+	private static Configuration loadEnvironment(String[] args) throws FileNotFoundException,
+			IOException {
+		if (args.length > 0) {
 			return Environment.from(args[0], Configuration.class);
 		}
 
--- frameworks/Java/restexpress/src/main/java/hello/config/Configuration.java
+++ frameworks/Java/restexpress/src/main/java/hello/config/Configuration.java
@@ -1,10 +1,11 @@
 package hello.config;
 
+import hello.controller.DbMysqlController;
 import hello.controller.JsonController;
 import hello.controller.MongodbController;
-import hello.controller.MysqlController;
-import hello.controller.persistence.WorldsMongodbRepository;
 import hello.controller.PlaintextController;
+import hello.controller.QueriesMysqlController;
+import hello.controller.persistence.WorldsMongodbRepository;
 
 import java.util.Properties;
 
@@ -13,9 +14,7 @@ import com.strategicgains.repoexpress.exception.InvalidObjectIdException;
 import com.strategicgains.restexpress.Format;
 import com.strategicgains.restexpress.util.Environment;
 
-public class Configuration
-extends Environment
-{
+public class Configuration extends Environment {
 	private static final String DEFAULT_EXECUTOR_THREAD_POOL_SIZE = "20";
 
 	private static final String PORT_PROPERTY = "port";
@@ -29,76 +28,72 @@ extends Environment
 	private int executorThreadPoolSize;
 
 	private JsonController jsonController;
-	private MysqlController mysqlController;
+	private DbMysqlController dbMysqlController;
+	private QueriesMysqlController queriesMysqlController;
 	private MongodbController mongodbController;
-        private PlaintextController plaintextController;
+	private PlaintextController plaintextController;
 
 	@Override
-	protected void fillValues(Properties p)
-	{
+	protected void fillValues(Properties p) {
 		this.port = Integer.parseInt(p.getProperty(PORT_PROPERTY, "8080"));
 		this.defaultFormat = p.getProperty(DEFAULT_FORMAT_PROPERTY, Format.JSON);
 		this.baseUrl = p.getProperty(BASE_URL_PROPERTY, "http://localhost:" + String.valueOf(port));
-		this.executorThreadPoolSize = Integer.parseInt(p.getProperty(EXECUTOR_THREAD_POOL_SIZE, DEFAULT_EXECUTOR_THREAD_POOL_SIZE));
+		this.executorThreadPoolSize = Integer.parseInt(p.getProperty(EXECUTOR_THREAD_POOL_SIZE,
+				DEFAULT_EXECUTOR_THREAD_POOL_SIZE));
 		MongoConfig mongoSettings = new MongoConfig(p);
 		MysqlConfig mysqlSettings = new MysqlConfig(p);
 		initialize(mysqlSettings, mongoSettings);
 	}
 
-	private void initialize(MysqlConfig mysqlSettings, MongoConfig mongo)
-	{
+	private void initialize(MysqlConfig mysqlSettings, MongoConfig mongo) {
 		jsonController = new JsonController();
-                plaintextController = new PlaintextController();
-		mysqlController = new MysqlController(mysqlSettings.getDataSource());
-        WorldsMongodbRepository worldMongodbRepository = new WorldsMongodbRepository(mongo.getClient(), mongo.getDbName());
-        worldMongodbRepository.setIdentifierAdapter(new IdentiferAdapter<Long>()
-		{
+		plaintextController = new PlaintextController();
+		dbMysqlController = new DbMysqlController(mysqlSettings.getDataSource());
+		queriesMysqlController = new QueriesMysqlController(mysqlSettings.getDataSource());
+		WorldsMongodbRepository worldMongodbRepository = new WorldsMongodbRepository(
+				mongo.getClient(), mongo.getDbName());
+		worldMongodbRepository.setIdentifierAdapter(new IdentiferAdapter<Long>() {
 			@Override
-			public Long convert(String id) throws InvalidObjectIdException
-			{
+			public Long convert(String id) throws InvalidObjectIdException {
 				return Long.valueOf(id);
 			}
 		});
 		mongodbController = new MongodbController(worldMongodbRepository);
 	}
 
-	public String getDefaultFormat()
-	{
+	public String getDefaultFormat() {
 		return defaultFormat;
 	}
 
-	public int getPort()
-	{
+	public int getPort() {
 		return port;
 	}
-	
-	public String getBaseUrl()
-	{
+
+	public String getBaseUrl() {
 		return baseUrl;
 	}
-	
-	public int getExecutorThreadPoolSize()
-	{
+
+	public int getExecutorThreadPoolSize() {
 		return executorThreadPoolSize;
 	}
 
-	public JsonController getJsonController()
-	{
+	public JsonController getJsonController() {
 		return jsonController;
 	}
 
-	public MysqlController getMysqlController()
-	{
-		return mysqlController;
+	public DbMysqlController getDbMysqlController() {
+		return dbMysqlController;
 	}
 
-	public MongodbController getMongodbController()
-	{
+	public QueriesMysqlController getQueriesMysqlController() {
+		return queriesMysqlController;
+	}
+
+	public MongodbController getMongodbController() {
 		return mongodbController;
 	}
 
-        public PlaintextController getPlaintextController()
-        {
-            return plaintextController;
-        }
+	public PlaintextController getPlaintextController() {
+		return plaintextController;
+	}
 }
