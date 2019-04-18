0b3383b1a9a8d48539757c706682f19a166e8099
Matthias Kurz <m.kurz@irregular.at>
2019-04-18 19:22:37 +0200

    Revert "Play2: Use thread-pool-executor for all database benchmarks"
    This reverts commit 0fcdb3509357158d30430f2abb07bbce372b1384.

--- frameworks/Java/play2-java/play2-java-ebean-hikaricp/app/controllers/Application.java
+++ frameworks/Java/play2-java/play2-java-ebean-hikaricp/app/controllers/Application.java
@@ -4,41 +4,56 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ThreadLocalRandom;
+
+import javax.inject.Inject;
 
 import models.Fortune;
 import models.World;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import utils.DatabaseExecutionContext;
 
 public class Application extends Controller {
 
-    public Result db() {
-        return ok(Json.toJson(getRandomWorlds(1).get(0)));
+    private final DatabaseExecutionContext dbEc;
+
+    @Inject
+    public Application(final DatabaseExecutionContext dbEc) {
+        this.dbEc = dbEc;
     }
 
-    public Result queries(final String queries) {
-        return ok(Json.toJson(getRandomWorlds(queryCount(queries))));
+    public CompletionStage<Result> db() {
+        return getRandomWorlds(1).thenApply(worlds -> ok(Json.toJson(worlds.get(0))));
     }
 
-    public Result fortunes() {
-        final List<Fortune> fortunes = Fortune.findAll();
-        fortunes.add(new Fortune("Additional fortune added at request time."));
-        Collections.sort(fortunes, (f1, f2) -> f1.message.compareTo(f2.message));
-
-        return ok(views.html.fortunes.render(fortunes));
+    public CompletionStage<Result> queries(final String queries) {
+        return getRandomWorlds(queryCount(queries)).thenApply(worlds -> ok(Json.toJson(worlds)));
     }
 
-    public Result update(final String queries) {
-        final List<World> worlds = getRandomWorlds(queryCount(queries));
-        final Random random = ThreadLocalRandom.current();
-        for (final World world : worlds) {
-            world.randomNumber = (long) (random.nextInt(10000) + 1);
-        }
+    public CompletionStage<Result> fortunes() {
+        return CompletableFuture.supplyAsync(() -> {
+            final List<Fortune> fortunes = Fortune.findAll();
+            fortunes.add(new Fortune("Additional fortune added at request time."));
+            Collections.sort(fortunes, (f1, f2) -> f1.message.compareTo(f2.message));
 
-        final List<World> updatedWorlds = World.save(worlds);
-        return ok(Json.toJson(updatedWorlds));
+            return ok(views.html.fortunes.render(fortunes));
+        }, dbEc);
+    }
+
+    public CompletionStage<Result> update(final String queries) {
+        return getRandomWorlds(queryCount(queries)).thenApplyAsync(worlds -> {
+            final Random random = ThreadLocalRandom.current();
+            for (final World world : worlds) {
+                world.randomNumber = (long) (random.nextInt(10000) + 1);
+            }
+
+            final List<World> updatedWorlds = World.save(worlds);
+            return ok(Json.toJson(updatedWorlds));
+        }, dbEc);
     }
 
     private int queryCount(final String queryCountString) {
@@ -57,15 +72,17 @@ public class Application extends Controller {
         return queryCount;
     }
 
-    private List<World> getRandomWorlds(final int n) {
-        final Random random = ThreadLocalRandom.current();
-        final List<World> worlds = new ArrayList<>(n);
-        for (int i = 0; i < n; ++i) {
-            long randomId = random.nextInt(10000) + 1;
-            final World world = World.find(randomId);
-            worlds.add(world);
-        }
-        return worlds;
+    private CompletionStage<List<World>> getRandomWorlds(final int n) {
+        return CompletableFuture.supplyAsync(() -> {
+            final Random random = ThreadLocalRandom.current();
+            final List<World> worlds = new ArrayList<>(n);
+            for (int i = 0; i < n; ++i) {
+                long randomId = random.nextInt(10000) + 1;
+                final World world = World.find(randomId);
+                worlds.add(world);
+            }
+            return worlds;
+        }, dbEc);
     }
 
 }
--- frameworks/Scala/play2-scala/play2-scala-anorm/app/utils/DbOperation.scala
+++ frameworks/Scala/play2-scala/play2-scala-anorm/app/utils/DbOperation.scala
@@ -12,18 +12,20 @@ import scala.concurrent.Future
 
 @Singleton
 class DbOperation @Inject() (protected val db: Database,
-  configuration: Configuration) {
+  configuration: Configuration, dbEc: DatabaseExecutionContext) {
 
   /**
    * Run a DB operation in the DB context.
    */
-  def syncDbOp[T](op: Connection => T): T = {
+  def asyncDbOp[T](op: Connection => T): Future[T] = {
     // If the thread-pool queue used by the database grows too large then our server
     // is probably struggling, and we should start dropping requests. If we don't
     // then we'll just slow everything down and it will fail anyway. Better to fail
     // quickly rather than slowly. Set the max size of our queue something above the
     // number of concurrent connections that we expect to be handling.
-    db.withConnection { connection => op(connection) }
+    Future {
+      db.withConnection { connection => op(connection) }
+    }(dbEc)
   }
 
 }
