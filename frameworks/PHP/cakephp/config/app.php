555866ce041e3f8b8d2a31558978aad5e5c47f64
Matthias Vanooteghem <matthias@ns347102.ip-91-121-175.eu>
2019-05-01 15:55:59 +0200

    webroot deploy.conf

--- frameworks/PHP/cakephp/config/app.php
+++ frameworks/PHP/cakephp/config/app.php
@@ -296,7 +296,6 @@ return [
             'className' => 'Cake\Database\Connection',
             'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => true,
-            //'host' => '172.30.0.2',
             'host' => 'tfb-database',
             /*
              * CakePHP will use the default DB port based on the driver selected
