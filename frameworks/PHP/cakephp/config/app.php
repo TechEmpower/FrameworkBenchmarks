58292671476ea15c2d90da6532575e1d168c661a
Matthias Vanooteghem <matthias@ns347102.ip-91-121-175.eu>
2019-05-01 16:27:15 +0200

    debug false

--- frameworks/PHP/cakephp/config/app.php
+++ frameworks/PHP/cakephp/config/app.php
@@ -9,7 +9,7 @@ return [
      * Development Mode:
      * true: Errors and warnings shown.
      */
-    'debug' => filter_var(env('DEBUG', true), FILTER_VALIDATE_BOOLEAN),
+    'debug' => filter_var(env('DEBUG', false), FILTER_VALIDATE_BOOLEAN),
 
     /**
      * Configure basic information about the application.
