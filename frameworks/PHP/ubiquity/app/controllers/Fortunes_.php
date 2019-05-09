f257cd718e832295b4fde9624dfa5e1e49a4f037
jcheron <myaddressmail@gmail.com>
2019-05-09 16:08:50 +0200

    update urls

---
+++ frameworks/PHP/ubiquity/app/controllers/Fortunes_.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace controllers;
+
+use Ubiquity\controllers\Controller;
+use Ubiquity\controllers\Startup;
+use Ubiquity\orm\DAO;
+use Ubiquity\views\engine\micro\MicroTemplateEngine;
+use models\Fortune;
+
+class Fortunes extends Controller {
+	public function initialize(){
+		Startup::$templateEngine=new MicroTemplateEngine();
+		$config=Startup::getConfig();
+		\Ubiquity\orm\DAO::startDatabase($config);
+	}
+	
+	public function index() {
+		$fortunes = DAO::getAll(Fortune::class,'',false);
+		$fortunes[] = (new Fortune())->setId(0)->setMessage('Additional fortune added at request time.');
+		usort($fortunes, function($left, $right) {
+			return $left->message<=>$right->message;
+		});
+		$this->loadView('Fortunes/index.php',['fortunes' => $fortunes]);
+	}
+}
+
--- frameworks/PHP/ubiquity/index.php
+++ frameworks/PHP/ubiquity/index.php
@@ -5,5 +5,5 @@ define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', __DIR__.DS.'app'.DS);
 $config=include_once ROOT.'config/config.php';
 require_once ROOT.'./../vendor/autoload.php';
-require_once ROOT.'config/services.php';
+require_once ROOT.'config/services_.php';
 \Ubiquity\controllers\Startup::run($config);
