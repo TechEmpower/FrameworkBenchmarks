f38cfc86c248c958a0e4faebd807d22c32e39681
vka <kartashov.vasily@googlemail.com>
2019-05-11 17:12:17 +1000

    Removing PeachPie, clean up, cacheing resources (#4757)
    * Updating Hamlet framework, using entities instead of raw ORM, removing roadrunner, react and amp.
    
    * adding missing template file
    
    * updates
    
    * using type safe parser for query params
    
    * removing peachpie
    
    * Updating Hamlet framework, using entities instead of raw ORM, removing roadrunner, react and amp.
    
    * updates
    
    * using type safe parser for query params
    
    * removing peachpie
    
    * using ubuntu instead of debian, pre-initializing resources
    
    * storing prepared queries in the constructor
    
    * compare hamlet-swoole to swoole for framework overhead

--- frameworks/PHP/hamlet/Benchmark/Resources/UpdateResource.php
+++ frameworks/PHP/hamlet/Benchmark/Resources/UpdateResource.php
@@ -4,59 +4,57 @@ namespace Benchmark\Resources;
 
 use Benchmark\Entities\RandomNumber;
 use Hamlet\Database\Database;
+use Hamlet\Database\Procedure;
 use Hamlet\Http\Entities\JsonEntity;
 use Hamlet\Http\Requests\Request;
-use Hamlet\Http\Resources\HttpResource;
 use Hamlet\Http\Responses\Response;
 use Hamlet\Http\Responses\SimpleOKResponse;
 
-class UpdateResource implements HttpResource
+class UpdateResource extends DbResource
 {
-    private $database;
+    /** @var Procedure */
+    private $selectProcedure;
+
+    /** @var Procedure */
+    private $updateProcedure;
 
     public function __construct(Database $database)
     {
-        $this->database = $database;
-    }
-
-    public function getResponse(Request $request): Response
-    {
-        $count = $request->parameter('queries');
-        if ($count !== null && $count > 0) {
-            $count = min($count, 500);
-        } else {
-            $count = 1;
-        }
-
+        parent::__construct($database);
         $selectQuery = '
             SELECT id,
                    randomNumber 
               FROM World
              WHERE id = ?
         ';
-        $selectProcedure = $this->database->prepare($selectQuery);
-
+        $this->selectProcedure = $this->database->prepare($selectQuery);
         $updateQuery = '
             UPDATE World 
                SET randomNumber = ? 
              WHERE id = ?
         ';
-        $updateProcedure = $this->database->prepare($updateQuery);
+        $this->updateProcedure = $this->database->prepare($updateQuery);
+    }
+
+    public function getResponse(Request $request): Response
+    {
+        $count = $this->getQueriesCount($request);
 
         $payload = [];
         while ($count-- > 0) {
             $id = mt_rand(1, 10000);
             $randomNumber = mt_rand(1, 10000);
 
-            $selectProcedure->bindInteger($id);
+            $this->selectProcedure->bindInteger($id);
             /** @var RandomNumber $entry */
-            $entry = $selectProcedure->processOne()->selectAll()->cast(RandomNumber::class)->collectHead();
-
+            $entry = $this->selectProcedure->processOne()
+                ->selectAll()->cast(RandomNumber::class)
+                ->collectHead();
             $modifiedEntry = $entry->withNumber($randomNumber);
 
-            $updateProcedure->bindInteger($modifiedEntry->number());
-            $updateProcedure->bindInteger($modifiedEntry->id());
-            $updateProcedure->execute();
+            $this->updateProcedure->bindInteger($modifiedEntry->number());
+            $this->updateProcedure->bindInteger($modifiedEntry->id());
+            $this->updateProcedure->execute();
 
             $payload[] = $modifiedEntry;
         }
