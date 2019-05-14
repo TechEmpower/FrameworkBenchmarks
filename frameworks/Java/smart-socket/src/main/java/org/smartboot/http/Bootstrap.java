46c7c0a966837e39510e8392b841f15bdda41279
三刀 <junwei.zheng@hipac.cn>
2019-05-14 11:03:30 +0800

    update smart-socket to 1.4.1

--- frameworks/Java/smart-socket/src/main/java/org/smartboot/http/Bootstrap.java
+++ frameworks/Java/smart-socket/src/main/java/org/smartboot/http/Bootstrap.java
@@ -55,10 +55,10 @@ public class Bootstrap {
         AioQuickServer<Http11Request> server = new AioQuickServer<>(8080, new HttpRequestProtocol(), processor);
         server.setReadBufferSize(1024 * 4);
         int cpuNum = Runtime.getRuntime().availableProcessors();
-        int shareNum = Runtime.getRuntime().availableProcessors() * 3 / 4;
+        int shareNum = Runtime.getRuntime().availableProcessors() * 7 / 8;
         server.setBossThreadNum(cpuNum);
         server.setBossShareToWorkerThreadNum(shareNum);
-        server.setWorkerThreadNum(cpuNum - shareNum);
+        server.setWorkerThreadNum((cpuNum - shareNum) * 2);
         try {
             server.start();
         } catch (IOException e) {
--- frameworks/Java/smart-socket/src/main/java/org/smartboot/http/JSON.java
+++ frameworks/Java/smart-socket/src/main/java/org/smartboot/http/JSON.java
@@ -2,12 +2,22 @@ package org.smartboot.http;
 
 import com.alibaba.fastjson.JSONObject;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author 三刀
  * @version V1.0 , 2018/8/12
  */
 public class JSON {
-    public static byte[] toJson(Object obj) {
-        return JSONObject.toJSONBytes(obj);
+    private static Map<String, byte[]> cacheMap = new HashMap<>();
+
+    public static byte[] toJson(Message obj) {
+        byte[] cache = cacheMap.get(obj.getMessage());
+        if (cache == null) {
+            cache = JSONObject.toJSONBytes(obj);
+            cacheMap.put(obj.getMessage(), cache);
+        }
+        return cache;
     }
 }
