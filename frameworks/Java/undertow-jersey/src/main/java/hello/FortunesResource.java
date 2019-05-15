908b1492fa5766321ea2e6f0f13132d038427543
zloster <more@edno.moe>
2019-05-15 16:57:34 +0300

    Update undertow-jersey also

--- frameworks/Java/undertow-jersey/src/main/java/hello/FortunesResource.java
+++ frameworks/Java/undertow-jersey/src/main/java/hello/FortunesResource.java
@@ -1,44 +1,34 @@
 package hello;
 
-import hello.domain.*;
-import org.glassfish.jersey.server.mvc.*;
-import org.hibernate.*;
-
-import javax.inject.*;
-import javax.ws.rs.*;
-import java.util.*;
-
 import static javax.ws.rs.core.MediaType.TEXT_HTML;
+import hello.domain.Fortune;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import javax.persistence.EntityManagerFactory;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+import org.glassfish.jersey.server.mvc.Template;
 
 @Singleton
 @Path("/fortunes")
-public class FortunesResource
-{
+public class FortunesResource {
+	@Inject
+	private EntityManagerFactory emf;
 
-  @Inject
-  private SessionFactory sessionFactory;
-
-  @GET
-  @Produces(TEXT_HTML + "; charset=utf-8")
-  public Viewable fortunes()
-  {
-    final Session session = sessionFactory.openSession();
-    final List<Fortune> fortunes = new ArrayList<>(
-        session.createCriteria(Fortune.class).list());
-    fortunes.add(new Fortune(0, "Additional fortune added at request time."));
-    Collections.sort(fortunes);
-
-    session.close();
-    return new Viewable("/fortunes.mustache", new Scope(fortunes));
-  }
-
-  public static class Scope
-  {
-    public List fortunes;
-
-    public Scope(final List fortunes)
-    {
-      this.fortunes = fortunes;
-    }
-  }
+	@GET
+	@Produces(TEXT_HTML + "; charset=utf-8")
+	@Template(name = "/fortunes.mustache")
+	public List<Fortune> fortunes() {
+		List<Fortune> fortunes = new ArrayList<>(emf.createEntityManager()
+				.createQuery("SELECT f FROM Fortune f", Fortune.class).getResultList());
+		fortunes.add(new Fortune(0, "Additional fortune added at request time."));
+		fortunes.sort(null);
+		return fortunes;
+	}
 }
