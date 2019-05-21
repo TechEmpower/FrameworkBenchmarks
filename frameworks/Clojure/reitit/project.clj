c9773fcc42c94eb9a8a60dd9c3764fe9bf0e0087
Tommi Reiman <tommi@iki.fi>
2019-05-22 01:14:43 +0300

    Remove Clojure/immutant entry (#4797)
    * Remove Clojure/Immutant entry
    
    This is superseded by Clojure/reitit
    
    * Faster plain-text handling with reitit

--- frameworks/Clojure/reitit/project.clj
+++ frameworks/Clojure/reitit/project.clj
@@ -1,12 +1,12 @@
 (defproject hello "reitit"
   :description "Immutant-nio, Reitit, Jsonista & Porsas"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [ikitommi/immutant-web "3.0.0-alpha2"]
+                 [ikitommi/immutant-web "3.0.0-alpha3"]
                  [metosin/jsonista "0.2.2"]
                  [hikari-cp "2.7.1"]
                  [org.postgresql/postgresql "42.2.5"]
                  [metosin/porsas "0.0.1-alpha4"]
-                 [metosin/reitit "0.3.3"]]
+                 [metosin/reitit "0.3.4"]]
   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]
   :main hello.handler
   :aot :all)
