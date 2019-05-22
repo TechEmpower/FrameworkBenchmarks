fc5a398cb1c35f5c9c54809cff7609ebb92cd7da
Xueping <bitworld@qq.com>
2019-05-22 12:59:46 +0800

    [D/Hunt] Improve performance (#4781)
    * Using PicoHTTPParser
    
    * Update  docker  files to clone picohttpparser
    
    * Update docker files again
    
    * Update config file
    
    * Strip unnecessary test version
    
    * Update libmysqlclient
    
    * Test latest docker for D

--- frameworks/D/hunt/build.sh
+++ frameworks/D/hunt/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rm -rf http-parser
-git clone https://github.com/nodejs/http-parser.git
-cd http-parser
+rm -rf picohttpparser
+git clone https://github.com/h2o/picohttpparser.git
+cp patches/Makefile picohttpparser
+cd picohttpparser
 make package
