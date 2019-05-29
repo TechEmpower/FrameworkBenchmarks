459e0758c502be98ba5a12f191f0ab3d72304f5b
zloster <more@edno.moe>
2019-05-29 15:35:30 +0300

    Fix the name of SpringData Fortune repository so the discovery to work

--- frameworks/Java/officefloor/src/woof_benchmark_spring/src/main/java/net/officefloor/benchmark/FortuneRepository.java
+++ frameworks/Java/officefloor/src/woof_benchmark_spring/src/main/java/net/officefloor/benchmark/FortuneRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FortunueRepository extends CrudRepository<Fortune, Integer> {
+public interface FortuneRepository extends CrudRepository<Fortune, Integer> {
 }
