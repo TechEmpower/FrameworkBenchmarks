7b22bb862b1d1510c18887b3f310a86e96c14ab5
jcheron <myaddressmail@gmail.com>
2019-05-30 00:36:59 +0200

    Update Json.php

--- frameworks/PHP/ubiquity/app/controllers/Json.php
+++ frameworks/PHP/ubiquity/app/controllers/Json.php
@@ -6,12 +6,16 @@ use Ubiquity\utils\http\UResponse;
 
 /**
  * Json controller.
- **/
-class Json extends Controller{
+ */
+class Json extends Controller {
+
 	public function initialize() {
-		UResponse::setContentType( 'application/json' );
+		UResponse::setContentType('application/json');
 	}
-	public function index(){
-		echo \json_encode(['message' => 'Hello, World!']);
+
+	public function index() {
+		echo \json_encode([
+			'message' => 'Hello, World!'
+		]);
 	}
 }
