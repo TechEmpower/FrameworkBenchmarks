e3864ee623bb7c80d4515310dc4b6776b7aafda0
jcheron <myaddressmail@gmail.com>
2019-06-08 03:17:50 +0200

    Remove transactions in ubiquity(update)

--- frameworks/PHP/ubiquity/app/controllers/Db_.php
+++ frameworks/PHP/ubiquity/app/controllers/Db_.php
@@ -4,14 +4,26 @@ namespace controllers;
 use Ubiquity\controllers\Startup;
 use Ubiquity\utils\http\UResponse;
 
-
 /**
  * Bench controller.
- **/
-class Db_ extends Db{
-	public function initialize(){
-		UResponse::setContentType( 'application/json' );
-		$config=Startup::getConfig();
+ */
+class Db_ extends Db {
+
+	public function initialize() {
+		UResponse::setContentType('application/json');
+		$config = Startup::getConfig();
 		\Ubiquity\orm\DAO::startDatabase($config);
+	}
+
+	public function update($queries = 1) {
+		$worlds = [];
+		$queries = is_numeric($queries) ? min(max($queries, 1), 500) : 1;
+		for ($i = 0; $i < $queries; ++ $i) {
+			$world = DAO::getById(World::class, mt_rand(1, 10000), false);
+			$world->setRandomNumber(mt_rand(1, 10000));
+			DAO::update($world);
+			$worlds[] = $world->_rest;
+		}
+		echo \json_encode($worlds);
 	}
 }
