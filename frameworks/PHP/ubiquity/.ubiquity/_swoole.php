108f483f20b1e9d9ac11e998a854878cd00cfddb
jcheron <myaddressmail@gmail.com>
2019-06-12 20:31:05 +0200

    Add Ubiquity-swoole (#4840)
    * Add ubiquity-swoole server
    
    * Update ubiquity-swoole.dockerfile
    
    * Update ubiquity-swoole.dockerfile
    
    * Remove transactions in ubiquity(update)
    
    * Update Db_.php
    
    * update php-pm (react) to 64 workers
    
    * Update Db_.php
    
    * add pcntl ext

---
+++ frameworks/PHP/ubiquity/.ubiquity/_swoole.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+// swoole.php
+
+if (! defined ( 'DS' )) {
+	define ( 'DS', DIRECTORY_SEPARATOR );
+	define ( 'ROOT', __DIR__ . \DS .'..'.\DS. 'app' . \DS );
+}
+$config=include ROOT.'config/config.php';
+$sConfig= include __DIR__.\DS.'swoole-config.php';
+$config["sessionName"]=$sConfig["sessionName"];
+$address=$sConfig['host'].':'.$sConfig['port'];
+$config ["siteUrl"] = 'http://'.$address;
+require ROOT . './../vendor/autoload.php';
+require ROOT.'config/services.php';
+$reactServer=new \Ubiquity\servers\swoole\SwooleServer();
+$reactServer->init($config, __DIR__);
+$reactServer->run($sConfig['host'],$sConfig['port']);
