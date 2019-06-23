add74d486b1f2ca76b9a5eb3b95516c7033a176f
Tommi Reiman <tommi@iki.fi>
2019-06-23 21:56:12 +0300

    Fix async pool size, update deps

--- frameworks/Clojure/reitit/project.clj
+++ frameworks/Clojure/reitit/project.clj
@@ -1,9 +1,9 @@
 (defproject hello "reitit"
   :description "Immutant-nio, Reitit, Jsonista & Porsas"
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [ikitommi/immutant-web "3.0.0-alpha4"]
-                 [metosin/reitit "0.3.7"]
-                 [metosin/jsonista "0.2.2"]
+                 [metosin/reitit "0.3.9"]
+                 [metosin/jsonista "0.2.3"]
                  [metosin/porsas "0.0.1-alpha10"]
                  [hikari-cp "2.7.1"]]
   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]
