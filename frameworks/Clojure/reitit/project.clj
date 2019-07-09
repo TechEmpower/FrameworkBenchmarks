2c92910eb896b85f9bbcfa65815843297140c24c
Tommi Reiman <tommi@iki.fi>
2019-07-09 17:57:14 +0300

    Change clojure/reitit webserver to metosin/pohjavirta

--- frameworks/Clojure/reitit/project.clj
+++ frameworks/Clojure/reitit/project.clj
@@ -1,10 +1,10 @@
 (defproject hello "reitit"
-  :description "Immutant-nio, Reitit, Jsonista & Porsas"
+  :description "pohjavirta, reitit, jsonista & porsas"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [ikitommi/immutant-web "3.0.0-alpha4"]
-                 [metosin/reitit "0.3.9"]
+                 [metosin/pohjavirta "0.0.1-alpha1"]
+                 [metosin/porsas "0.0.1-alpha12"]
                  [metosin/jsonista "0.2.3"]
-                 [metosin/porsas "0.0.1-alpha10"]
+                 [metosin/reitit "0.3.9"]
                  [hikari-cp "2.7.1"]]
   :jvm-opts ^:replace ["-Dclojure.compiler.direct-linking=true"]
   :main hello.handler
