21bad95735197016d88d9f033927a15edbc41aae
Michal Strehovský <michals@microsoft.com>
2019-07-11 22:14:01 +0200

    Add CoreRT with Red Hat Linux transport
    Copies a couple more things over from the aspnetcore benchmark to build with the CoreRT runtime.

--- frameworks/CSharp/aspnetcore-corert/PlatformBenchmarks/BenchmarkConfigurationHelpers.cs
+++ frameworks/CSharp/aspnetcore-corert/PlatformBenchmarks/BenchmarkConfigurationHelpers.cs
@@ -29,6 +29,25 @@ namespace PlatformBenchmarks
                 theadCount = value;
             }
 
+            if (string.Equals(webHost, "LinuxTransport", StringComparison.OrdinalIgnoreCase))
+            {
+                builder.ConfigureServices(services =>
+                {
+                    services.Configure<KestrelServerOptions>(options =>
+                    {
+                        // Run callbacks on the transport thread
+                        options.ApplicationSchedulingMode = SchedulingMode.Inline;
+                    });
+                })
+                .UseLinuxTransport(options =>
+                {
+                    if (theadCount.HasValue)
+                    {
+                        options.ThreadCount = theadCount.Value;
+                    }
+                });
+            }
+
             return builder;
         }
         
