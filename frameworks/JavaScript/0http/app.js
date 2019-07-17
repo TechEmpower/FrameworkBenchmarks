ed2e64a265a71c3b52d8fb6d95b374794bfcafb1
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-07-17 16:29:58 +0200

    adding required headers

--- frameworks/JavaScript/0http/app.js
+++ frameworks/JavaScript/0http/app.js
@@ -5,10 +5,16 @@ const cero = require('0http')
 const { router, server } = cero()
 
 router.on('GET', '/json', (req, res) => {
+  res.setHeader('server', '0http')
+  res.setHeader('content-type', 'application/json')
+
   res.end(JSON.stringify({ message: 'Hello, World!' }))
 })
 
 router.on('GET', '/plaintext', (req, res) => {
+  res.setHeader('server', '0http')
+  res.setHeader('content-type', 'text/plain')
+
   res.end('Hello, World!')
 })
 
--- frameworks/JavaScript/restana/app.js
+++ frameworks/JavaScript/restana/app.js
@@ -4,10 +4,16 @@ const cpus = require('os').cpus()
 const service = require('restana')()
 
 service.get('/json', (req, res) => {
+  res.setHeader('server', 'restana')
+  res.setHeader('content-type', 'application/json')
+  
   res.end(JSON.stringify({ message: 'Hello, World!' }))
 })
 
 service.get('/plaintext', (req, res) => {
+  res.setHeader('server', 'restana')
+  res.setHeader('content-type', 'text/plain')
+
   res.end('Hello, World!')
 })
 
