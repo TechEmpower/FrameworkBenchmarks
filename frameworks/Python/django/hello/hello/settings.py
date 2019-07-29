87b99a58102e35f78169778d430c0c9a8b737675
Anthony Monthe <anthony.monthe@gmail.com>
2019-07-29 04:08:41 +0100

    Disabled django loggers (#4958)

--- frameworks/Python/django/hello/hello/settings.py
+++ frameworks/Python/django/hello/hello/settings.py
@@ -54,38 +54,9 @@ INSTALLED_APPS = (
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': True,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        },
-        'console':{
-            'level': 'ERROR',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins', 'console'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    }
+    'handlers': {},
+    'loggers': {},
+
 }
 
 ALLOWED_HOSTS = ['*']
