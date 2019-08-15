9725161287fad3a728f18a630d1074af29146d46
黄朝晖 <huangzhwork@gmail.com>
2019-08-16 02:40:20 +0800

    Add Hyperf v1.0 (#4993)
    * Add Hyperf
    
    * Revert
    
    * Update composer.json
    
    * Translate Readme.md
    
    * Removed useless files
    
    * Optimized
    
    * Optimized
    
    * Add gitignore
    
    * Update the timezone to America/Los_Angeles
    
    * Remove apk update
    
    * Add missing files
    
    * Removed all useless files

---
+++ frameworks/PHP/hyperf/config/autoload/logger.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * This file is part of Hyperf.
+ *
+ * @link     https://www.hyperf.io
+ * @document https://doc.hyperf.io
+ * @contact  group@hyperf.io
+ * @license  https://github.com/hyperf-cloud/hyperf/blob/master/LICENSE
+ */
+
+return [
+    'default' => [
+        'handler' => [
+            'class' => Monolog\Handler\StreamHandler::class,
+            'constructor' => [
+                'stream' => BASE_PATH . '/runtime/logs/hyperf.log',
+                'level' => Monolog\Logger::DEBUG,
+            ],
+        ],
+        'formatter' => [
+            'class' => Monolog\Formatter\LineFormatter::class,
+            'constructor' => [
+                'format' => null,
+                'dateFormat' => null,
+                'allowInlineLineBreaks' => true,
+            ],
+        ],
+    ],
+];
