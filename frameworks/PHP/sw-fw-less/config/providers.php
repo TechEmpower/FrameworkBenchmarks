05c8a95836a277f43b68b7a40dfcbd6a20120339
luoxiaojun <luoxiaojun@dodoca.com>
2019-09-10 22:26:38 +0800

    remove useless files

--- frameworks/PHP/sw-fw-less/config/providers.php
+++ frameworks/PHP/sw-fw-less/config/providers.php
@@ -7,21 +7,21 @@ return [
 
     //App Providers
     \SwFwLess\components\swoole\SwooleProvider::class,
-    \SwFwLess\components\chaos\ChaosProvider::class,
+//    \SwFwLess\components\chaos\ChaosProvider::class,
 
     //Worker Providers
     \SwFwLess\components\datetime\DatetimeProvider::class,
     \SwFwLess\components\event\EventProvider::class,
     \SwFwLess\components\log\LogProvider::class,
-    \SwFwLess\components\ratelimit\RatelimitProvider::class,
-    \SwFwLess\components\cache\CacheProvider::class,
+//    \SwFwLess\components\ratelimit\RatelimitProvider::class,
+//    \SwFwLess\components\cache\CacheProvider::class,
     \SwFwLess\components\mysql\MysqlProvider::class,
-    \SwFwLess\components\es\EsProvider::class,
-    \SwFwLess\components\storage\StorageProvider::class,
-    \SwFwLess\components\amqp\AmqpProvider::class,
-    \SwFwLess\components\hbase\HbaseProvider::class,
+//    \SwFwLess\components\es\EsProvider::class,
+//    \SwFwLess\components\storage\StorageProvider::class,
+//    \SwFwLess\components\amqp\AmqpProvider::class,
+//    \SwFwLess\components\hbase\HbaseProvider::class,
     \SwFwLess\components\di\ContainerProvider::class,
-    \SwFwLess\components\auth\jwt\JwtProvider::class,
+//    \SwFwLess\components\auth\jwt\JwtProvider::class,
 
     //Request Providers
 
