db452b5efc6ce3a0886f660e3ce3d0182717e183
Joanhey <joanhey@kumbiaphp.com>
2019-09-12 15:02:29 +0200

    Reuse the db statement

--- frameworks/PHP/workerman/fortune.php
+++ frameworks/PHP/workerman/fortune.php
@@ -1,13 +1,16 @@
 <?php
-function fortune($pdo) {
-    $statement = $pdo->query( 'SELECT id,message FROM Fortune' );
-    $arr = $statement->fetchAll(PDO::FETCH_KEY_PAIR); 
-    $arr[0] = 'Additional fortune added at request time.';
+function fortune($pdo)
+{
+    static $statement;
+    $statement = $statement ?? $pdo->prepare('SELECT id,message FROM Fortune');
+    $statement->execute();
+    $arr       = $statement->fetchAll(PDO::FETCH_KEY_PAIR);
+    $arr[0]    = 'Additional fortune added at request time.';
     asort($arr);
 ?>
 <!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>
-<?php foreach ( $arr as $id => $fortune ) : ?>
-<tr><td><?= $id ?></td><td><?= htmlspecialchars($fortune, ENT_QUOTES, 'UTF-8'); ?></td></tr>
-<?php endforeach ?></table></body></html>
+<?php foreach ($arr as $id => $fortune): ?>
+<tr><td><?=$id?></td><td><?=htmlspecialchars($fortune, ENT_QUOTES, 'UTF-8');?></td></tr>
+<?php endforeach?></table></body></html>
 <?php
 }
