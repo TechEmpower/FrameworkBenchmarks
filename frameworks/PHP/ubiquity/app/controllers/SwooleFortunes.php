ff91a34de13b2526e7f5a60c87d0f6739b865da2
jcheron <myaddressmail@gmail.com>
2019-09-13 19:19:08 +0200

    remove pooling from SwooleFortunes

--- frameworks/PHP/ubiquity/app/controllers/SwooleFortunes.php
+++ frameworks/PHP/ubiquity/app/controllers/SwooleFortunes.php
@@ -7,9 +7,7 @@ use Ubiquity\orm\DAO;
 class SwooleFortunes extends \Ubiquity\controllers\Controller {
 
 	public function index() {
-		$dbInstance=DAO::pool();
 		$fortunes = DAO::getAll(Fortune::class, '', false);
-		DAO::freePool($dbInstance);
 		$fortunes[] = (new Fortune())->setId(0)->setMessage('Additional fortune added at request time.');
 		\usort($fortunes, function ($left, $right) {
 			return $left->message <=> $right->message;
