51b06382f4897461a2eeec98abceebd5a470ffe5
jcheron <myaddressmail@gmail.com>
2019-09-14 00:39:10 +0200

    Update raw for multidb

--- frameworks/PHP/ubiquity/app/controllers/RawFortunes.php
+++ frameworks/PHP/ubiquity/app/controllers/RawFortunes.php
@@ -8,7 +8,7 @@ class RawFortunes extends \Ubiquity\controllers\Controller {
 	}
 	
 	public function index() {
-		$fortunes = \Ubiquity\db\Database::start()->fetchAll('SELECT id,message FROM Fortune',\PDO::FETCH_KEY_PAIR);
+		$fortunes = \Ubiquity\db\Database::start('default')->fetchAll('SELECT id,message FROM Fortune',\PDO::FETCH_KEY_PAIR);
 		$fortunes[0] = 'Additional fortune added at request time.';
 		\asort($fortunes);
 		$this->loadView('Fortunes/raw-index.php', [
