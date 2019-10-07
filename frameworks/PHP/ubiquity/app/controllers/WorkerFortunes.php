ed1440163076fa7d75e7bdbd90ca22a9a2f81c54
jcheron <myaddressmail@gmail.com>
2019-10-07 04:09:16 +0200

    Add ubiquity-workerman-pgsql (#5119)
    * Add workerman-pgsql
    
    * add persistent connection

--- frameworks/PHP/ubiquity/app/controllers/WorkerFortunes.php
+++ frameworks/PHP/ubiquity/app/controllers/WorkerFortunes.php
@@ -6,6 +6,10 @@ use Ubiquity\orm\DAO;
 
 class WorkerFortunes extends \Ubiquity\controllers\Controller {
 
+	public function initialize() {
+		\Ubiquity\utils\http\UResponse::setContentType('text/html','utf-8');
+	}
+	
 	public function index() {
 		$fortunes = DAO::getAll(Fortune::class, '', false);
 		$fortunes[] = (new Fortune())->setId(0)->setMessage('Additional fortune added at request time.');
