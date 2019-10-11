3413cd2b83a1dcf15b2f3a117b9b47a777668982
jcheron <myaddressmail@gmail.com>
2019-10-11 17:19:50 +0200

    [ci skip] Add check methods to Mysql Database

--- toolset/databases/mysql/mysql.py
+++ toolset/databases/mysql/mysql.py
@@ -8,6 +8,11 @@ from toolset.utils.output_helper import log
 class Database:
 
     @staticmethod
+    def get_connection(config):
+        return MySQLdb.connect(config.database_host, "benchmarkdbuser",
+                                 "benchmarkdbpass", "hello_world")
+
+    @staticmethod
     def get_current_world_table(config):
         '''
         Return a JSON object containing all 10,000 World items as they currently
@@ -17,9 +22,7 @@ class Database:
         results_json = []
 
         try:
-            db = MySQLdb.connect(config.database_host,
-                                 "benchmarkdbuser", "benchmarkdbpass",
-                                 "hello_world")
+            db = Database.get_connection(config)
             cursor = db.cursor()
             cursor.execute("SELECT * FROM World")
             results = cursor.fetchall()
@@ -36,8 +39,7 @@ class Database:
     @staticmethod
     def test_connection(config):
         try:
-            db = MySQLdb.connect(config.database_host, "benchmarkdbuser",
-                                 "benchmarkdbpass", "hello_world")
+            db = Database.get_connection(config)
             cursor = db.cursor()
             cursor.execute("SELECT 1")
             cursor.fetchall()
@@ -45,3 +47,27 @@ class Database:
             return True
         except:
             return False
+
+    @staticmethod
+    def get_queries(config):
+        db = Database.get_connection(config)
+        cursor = db.cursor()
+        cursor.execute("Show session status like 'Queries'")
+        record = cursor.fetchone()
+        return record[1]
+
+    @staticmethod
+    def get_rows(config):
+        db = Database.get_connection(config)
+        cursor = db.cursor()
+        cursor.execute("show session status like 'Innodb_rows_read'")
+        record = cursor.fetchone()
+        return record[1]
+
+    @staticmethod
+    def reset_cache(config):
+        #No more in Mysql 8.0
+        #cursor = self.db.cursor()
+        #cursor.execute("RESET QUERY CACHE")
+        #self.db.commit()
+        return
