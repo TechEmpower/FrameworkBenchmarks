c666e120b12248eff22b61ed79ef53680c949989
jcheron <myaddressmail@gmail.com>
2019-10-12 11:22:26 +0200

    [ci skip] cls attr in abstract_database

--- toolset/databases/abstract_database.py
+++ toolset/databases/abstract_database.py
@@ -11,12 +11,12 @@ class AbstractDatabase:
         Verify queries and rows for table_name
         '''
         cls.tbl_name=table_name # only for Postgres
-        queries=int(cls.get_queries(config))
-        rows=int(cls.get_rows(config))
-        cls.reset_cache(config)
+        queries=int(cls.get_queries(cls, config))
+        rows=int(cls.get_rows(cls, config))
+        cls.reset_cache(cls, config)
         
         os.system("ab -k -c %s -n %s %s" % (concurrency, count, url))
         
-        queries=int(cls.get_queries(config))-queries
-        rows=int(cls.get_rows(config))-rows
+        queries=int(cls.get_queries(cls, config))-queries
+        rows=int(cls.get_rows(cls, config))-rows
         return queries, rows
