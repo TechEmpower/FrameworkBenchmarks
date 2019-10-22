44ce831bc15d9f5a1b510caad026bef6b0603434
jcheron <myaddressmail@gmail.com>
2019-10-22 19:12:07 +0200

    move swoole to pdo Mysql

--- frameworks/PHP/ubiquity/app/config/services.php
+++ frameworks/PHP/ubiquity/app/config/services.php
@@ -1,7 +1,11 @@
 <?php
 \Ubiquity\cache\CacheManager::startProd($config);
 \Ubiquity\controllers\Startup::$templateEngine = new \Ubiquity\views\engine\micro\MicroTemplateEngine();
-\Ubiquity\orm\DAO::setModelsDatabases(["models\\Fortune"=>"swoole","models\\World"=>"swoole"]);
-$swooleServer->on('workerStart',function($srv) use($config){
-	\Ubiquity\orm\DAO::initPooling($config,'swoole',\intdiv(512,$srv->setting['worker_num']));
+\Ubiquity\orm\DAO::setModelsDatabases([
+	"models\\Fortune" => "default",
+	"models\\World" => "default"
+]);
+$swooleServer->on('workerStart', function ($srv) use (&$config) {
+	\Ubiquity\orm\DAO::startDatabase($config, 'default');
 });
+	
