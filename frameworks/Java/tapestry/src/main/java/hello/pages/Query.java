c263453ed8c02ade14b675898ae5d2817a4c58a9
Nate Brady <nathan.brady@yahoo.com>
2019-10-25 11:58:10 -0700

    add comment

--- frameworks/Java/tapestry/src/main/java/hello/pages/Query.java
+++ frameworks/Java/tapestry/src/main/java/hello/pages/Query.java
@@ -50,6 +50,9 @@ public class Query
       queries = 500;
     }
 
+    // Since we're using hibernate ORM which has a 1st level cache
+    // that can't be turned off, we instead pick distinct random numbers
+    // to avoid failing query verification
     final World[] worlds = ThreadLocalRandom
             .current()
             .ints(1, DB_ROWS + 1)
