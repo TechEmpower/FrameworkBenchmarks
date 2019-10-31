f97f5894cd8603d14e95d16faa606568210f6a1b
Joan Miquel <joanhey@kumbiaphp.com>
2019-10-31 15:58:03 +0100

    Php-ngx bulk update (#5198)
    * Php-ngx bulk update
    
    * php-ngx async bulk update
    
    * Small change to rerun travis

--- frameworks/PHP/php-ngx/app.php
+++ frameworks/PHP/php-ngx/app.php
@@ -46,21 +46,20 @@ function update()
         $query_count = min($params, 500);
     }
 
-    $statement       = $pdo->prepare('SELECT randomNumber FROM World WHERE id=?');
-    $updateStatement = $pdo->prepare('UPDATE World SET randomNumber=? WHERE id=?');
+    $statement = $pdo->prepare('SELECT randomNumber FROM World WHERE id=?');
+    $update    = '';
 
     while ($query_count--) {
         $id = mt_rand(1, 10000);
         $statement->execute([$id]);
 
         $world = ['id' => $id, 'randomNumber' => $statement->fetchColumn()];
-        $updateStatement->execute(
-            [$world['randomNumber'] = mt_rand(1, 10000), $id]
-        );
+        $world['randomNumber'] = mt_rand(1, 10000);
+        $update .= "UPDATE World SET randomNumber={$world['randomNumber']} WHERE id=$id;";
 
         $arr[] = $world;
     }
-
+    $pdo->exec($update);
     echo json_encode($arr, JSON_NUMERIC_CHECK);
 }
 
