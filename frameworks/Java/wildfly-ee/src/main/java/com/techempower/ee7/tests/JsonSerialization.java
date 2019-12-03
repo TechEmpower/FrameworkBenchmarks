6efdd2ce991001a0920d223c71b5984bda459ea9
Cody Lerum <cody.lerum@gmail.com>
2019-12-02 17:26:15 -0700

    Update Wildfly to use ZGC and a larger heap and formatting

--- frameworks/Java/wildfly-ee/src/main/java/com/techempower/ee7/tests/JsonSerialization.java
+++ frameworks/Java/wildfly-ee/src/main/java/com/techempower/ee7/tests/JsonSerialization.java
@@ -4,32 +4,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
 
 @Path("/json")
 public class JsonSerialization {
 
-  private static final String MESSAGE = "Hello, World!";
+    private static final String MESSAGE = "Hello, World!";
 
-  @GET
-  @Produces(MediaType.APPLICATION_JSON)
-  public JsonResponse get() {
-    return new JsonResponse(MESSAGE);
-  }
-
-  @XmlRootElement
-  public class JsonResponse {
-
-    private final String message;
-
-    public JsonResponse(String message) {
-      this.message = message;
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public JsonResponse get() {
+        return new JsonResponse(MESSAGE);
     }
 
-    @XmlElement
-    public String getMessage() {
-      return message;
+    public class JsonResponse {
+
+        private final String message;
+
+        public JsonResponse(String message) {
+            this.message = message;
+        }
+
+        public String getMessage() {
+            return message;
+        }
     }
-  }
 }
