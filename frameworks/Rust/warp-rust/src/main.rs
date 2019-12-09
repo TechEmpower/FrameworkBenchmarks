4daf0a9a3f818ff173313097c01d1f312b196cae
Konrad Borowski <konrad@borowski.pw>
2019-12-09 07:44:50 +0100

    Add new framework - warp-rust

---
+++ frameworks/Rust/warp-rust/src/main.rs
@@ -0,0 +1,21 @@
+use serde::Serialize;
+use warp::http::header;
+use warp::Filter;
+
+#[derive(Serialize)]
+struct Message {
+    message: &'static str,
+}
+
+fn main() {
+    let json = warp::path("json").map(|| {
+        warp::reply::json(&Message {
+            message: "Hello, world!",
+        })
+    });
+    let plaintext = warp::path("plaintext").map(|| "Hello, World!");
+    let routes = json
+        .or(plaintext)
+        .map(|reply| warp::reply::with_header(reply, header::SERVER, "warp"));
+    warp::serve(routes).run(([0, 0, 0, 0], 8080))
+}
