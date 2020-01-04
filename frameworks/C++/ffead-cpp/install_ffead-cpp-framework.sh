e517a284ec5cd37e86e7f3ba06a7297ec8b7f6c8
sumeetchhetri <sumeet.chhetri@gmail.com>
2020-01-04 17:07:31 +0000

    Added fix for failing nginx/apache tests

--- frameworks/C++/ffead-cpp/install_ffead-cpp-framework.sh
+++ frameworks/C++/ffead-cpp/install_ffead-cpp-framework.sh
@@ -93,6 +93,13 @@ cd ${IROOT}/ffead-cpp-src/
 cp -rf ffead-cpp-3.0-bin ${IROOT}/ffead-cpp-3.0
 rm -rf ffead-cpp-3.0-bin
 
+ln -s ${IROOT}/ffead-cpp-3.0/lib/libte_benchmark.so /usr/local/lib/libte_benchmark.so
+ln -s ${IROOT}/ffead-cpp-3.0/lib/libffead-modules.so /usr/local/lib/libffead-modules.so
+ln -s ${IROOT}/ffead-cpp-3.0/lib/libffead-framework.so /usr/local/lib/libffead-framework.so
+ln -s ${IROOT}/ffead-cpp-3.0/lib/libinter.so /usr/local/lib/libinter.so
+ln -s ${IROOT}/ffead-cpp-3.0/lib/libdinter.so /usr/local/lib/libdinter.so
+ldconfig
+
 cd ${IROOT}/ffead-cpp-3.0
 cp -f ${TROOT}/run_ffead.sh ./
 
