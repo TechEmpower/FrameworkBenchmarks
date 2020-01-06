b0eb8156431dd46037b6246819a6c2bbd8362a1a
jcheron <myaddressmail@gmail.com>
2020-01-06 01:03:24 +0100

    spec pk for queries

--- frameworks/PHP/ubiquity/app/controllers/Db.php
+++ frameworks/PHP/ubiquity/app/controllers/Db.php
@@ -14,25 +14,31 @@ class Db extends \Ubiquity\controllers\Controller {
 		\Ubiquity\cache\CacheManager::startProd(\Ubiquity\controllers\Startup::$config);
 		DAO::setModelDatabase(World::class);
 	}
-	
+
 	public function index() {
-		echo \json_encode((DAO::getById(World::class, \mt_rand(1, 10000), false))->_rest);
+		echo \json_encode((DAO::getById(World::class, [
+			'id' => \mt_rand(1, 10000)
+		], false))->_rest);
 	}
-	
+
 	public function query($queries = 1) {
 		$worlds = [];
 		$queries = \min(\max($queries, 1), 500);
 		for ($i = 0; $i < $queries; ++ $i) {
-			$worlds[] = (DAO::getById(World::class, \mt_rand(1, 10000), false))->_rest;
+			$worlds[] = (DAO::getById(World::class, [
+				'id' => \mt_rand(1, 10000)
+			], false))->_rest;
 		}
 		echo \json_encode($worlds);
 	}
-	
+
 	public function update($queries = 1) {
 		$worlds = [];
 		$queries = \min(\max($queries, 1), 500);
 		for ($i = 0; $i < $queries; ++ $i) {
-			$world = DAO::getById(World::class, \mt_rand(1, 10000), false);
+			$world = DAO::getById(World::class, [
+				'id' => \mt_rand(1, 10000)
+			], false);
 			$world->randomNumber = \mt_rand(1, 10000);
 			DAO::update($world);
 			$worlds[] = $world->_rest;
