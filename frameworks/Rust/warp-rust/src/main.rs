9e1fba6d1396e1ca758bfe9e9f865ddc79c27f2a
Konrad Borowski <konrad@borowski.pw>
2020-01-17 16:17:50 +0100

    warp-rust 0.2 (#5407)

--- frameworks/Rust/warp-rust/src/main.rs
+++ frameworks/Rust/warp-rust/src/main.rs
@@ -7,15 +7,16 @@ struct Message {
     message: &'static str,
 }
 
-fn main() {
-    let json = warp::path("json").map(|| {
+#[tokio::main]
+async fn main() {
+    let json = warp::path!("json").map(|| {
         warp::reply::json(&Message {
             message: "Hello, world!",
         })
     });
-    let plaintext = warp::path("plaintext").map(|| "Hello, World!");
+    let plaintext = warp::path!("plaintext").map(|| "Hello, World!");
     let routes = json
         .or(plaintext)
         .map(|reply| warp::reply::with_header(reply, header::SERVER, "warp"));
-    warp::serve(routes).run(([0, 0, 0, 0], 8080))
+    warp::serve(routes).run(([0, 0, 0, 0], 8080)).await;
 }
