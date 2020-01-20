fd3632a193ffeb0f5dfb83dc27888435c4a45ff5
jcheron <myaddressmail@gmail.com>
2020-01-20 18:38:51 +0100

    Add DAO prepared queries for async platforms (#5417)

--- frameworks/PHP/ubiquity/app/config/workerServices.php
+++ frameworks/PHP/ubiquity/app/config/workerServices.php
@@ -12,5 +12,7 @@
 ]);
 $workerServer->onWorkerStart = function () use ($config) {
 	\Ubiquity\orm\DAO::startDatabase($config, 'pgsql');
+	\Ubiquity\orm\DAO::prepareGetById('world', 'models\\World');
+	\Ubiquity\orm\DAO::prepareGetAll('fortune', 'models\\Fortune');
 };
 
