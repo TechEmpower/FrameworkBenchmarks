61dc1f06dc978334168b83945d1f293461f77768
Arthur Ryabtsev <aryabtsev@techempower.com>
2020-02-04 15:02:36 -0800

    [fix] Remove try/catch block

--- frameworks/Java/servicetalk/src/main/java/servicetalk/Server.java
+++ frameworks/Java/servicetalk/src/main/java/servicetalk/Server.java
@@ -45,45 +45,42 @@ public final class Server {
 
         DefaultHttpHeadersFactory headersFactory = new DefaultHttpHeadersFactory(false,false);
 
-        try{
-            HttpSerializationProvider serializer = HttpSerializationProviders.jsonSerializer(new JacksonSerializationProvider());
-//            Create a custom server builder with performance enhancements
-            HttpServers
-              .forPort(8080)
-              .executionStrategy(HttpExecutionStrategies.noOffloadsStrategy())
-              .ioExecutor(ioExecutor)
-              .disableDrainingRequestPayloadBody()
-              .protocols(HttpProtocolConfigs.h1().headersFactory(headersFactory).build())
-              .appendConnectionAcceptorFilter(delegate -> new ConnectionAcceptor() {
-                  @Override
-                  public Completable accept(ConnectionContext context) {
-                      ((NettyConnectionContext)context).updateFlushStrategy((current, isOrig) -> FlushStrategies.flushOnEnd());
-                      return delegate.accept(context);
-                  }
-              })
-              .listenAndAwait((ctx, request, responseFactory) -> {
-                  ((NettyConnectionContext)ctx).updateFlushStrategy(((current, isCurrentOriginal) -> FlushStrategies.flushOnEach()));
-                  if(request.path().equals("/json")) {
-                      Map<String, String> obj = new HashMap<String, String>();
-                      obj.put("message", "Hello, World!");
-                      return succeeded(responseFactory.ok()
-                        .payloadBody(obj, serializer.serializerFor(Map.class))
-                        .addHeader("Date", GetCurrentTime())
-                        .addHeader("Server", "TFB"));
-                  }
 
-                  if(request.path().equals("/plaintext")) {
-                      return succeeded(responseFactory.ok()
-                        .payloadBody("Hello, World!", HttpSerializationProviders.textSerializer())
-                        .addHeader("Date", GetCurrentTime())
-                        .addHeader("Server", "TFB"));
-                    };
-                    return null;
-                })
-                .awaitShutdown();
-        } catch (Exception e) {
-//            logging is not allowed
-        }
+        HttpSerializationProvider serializer = HttpSerializationProviders.jsonSerializer(new JacksonSerializationProvider());
+//            Create a custom server builder with performance enhancements
+        HttpServers
+          .forPort(8080)
+          .executionStrategy(HttpExecutionStrategies.noOffloadsStrategy())
+          .ioExecutor(ioExecutor)
+          .disableDrainingRequestPayloadBody()
+          .protocols(HttpProtocolConfigs.h1().headersFactory(headersFactory).build())
+          .appendConnectionAcceptorFilter(delegate -> new ConnectionAcceptor() {
+              @Override
+              public Completable accept(ConnectionContext context) {
+                  ((NettyConnectionContext)context).updateFlushStrategy((current, isOrig) -> FlushStrategies.flushOnEnd());
+                  return delegate.accept(context);
+              }
+          })
+          .listenAndAwait((ctx, request, responseFactory) -> {
+              ((NettyConnectionContext)ctx).updateFlushStrategy(((current, isCurrentOriginal) -> FlushStrategies.flushOnEach()));
+              if(request.path().equals("/json")) {
+                  Map<String, String> obj = new HashMap<String, String>();
+                  obj.put("message", "Hello, World!");
+                  return succeeded(responseFactory.ok()
+                    .payloadBody(obj, serializer.serializerFor(Map.class))
+                    .addHeader("Date", GetCurrentTime())
+                    .addHeader("Server", "TFB"));
+              }
+
+              if(request.path().equals("/plaintext")) {
+                  return succeeded(responseFactory.ok()
+                    .payloadBody("Hello, World!", HttpSerializationProviders.textSerializer())
+                    .addHeader("Date", GetCurrentTime())
+                    .addHeader("Server", "TFB"));
+                };
+                return null;
+            })
+            .awaitShutdown();
     }
 
     public static String GetCurrentTime() {
