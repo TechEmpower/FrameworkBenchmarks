238170267d6346c11ce70ec063cfe0dbcbb93683
jcheron <myaddressmail@gmail.com>
2020-02-07 15:06:01 +0100

    Fix typo

--- frameworks/PHP/ubiquity/deploy/swoole/swooleMysqlAsyncServices.php
+++ frameworks/PHP/ubiquity/deploy/swoole/swooleMysqlAsyncServices.php
@@ -9,7 +9,7 @@
 	'controllers\\SwooleFortunesAsync'
 ]);
 $swooleServer->on('workerStart', function ($srv) use (&$config) {
-	\Ubiquity\orm\DAO::initPooling($config, 'async', \intdiv(512, $srv->settings['worker_num']));
+	\Ubiquity\orm\DAO::initPooling($config, 'async', \intdiv(512, $srv->setting['worker_num']));
 	\Ubiquity\orm\DAO::prepareGetById('world', 'models\\World');
 	\Ubiquity\orm\DAO::prepareGetAll('fortune', 'models\\Fortune');
 });
