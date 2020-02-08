57b5dba0e2e2f049aeec3c10dfc03a12f8ee73ec
Trustin Lee <trustin@linecorp.com>
2020-02-08 20:45:54 +0900

    Update Armeria to 0.98.0
    - `new ServerBuilder()` has been deprecated in favor or `Server.builder()`.
    - `Server` and `Date` headers are now added by default.
    - There's no need to return a `CompletableFuture` to run in an event loop.

--- frameworks/Java/armeria/src/main/java/hello/App.java
+++ frameworks/Java/armeria/src/main/java/hello/App.java
@@ -1,10 +1,5 @@
 package hello;
 
-import java.time.ZoneOffset;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-
-import com.linecorp.armeria.common.HttpHeaderNames;
 import com.linecorp.armeria.server.Server;
 import com.linecorp.armeria.server.ServerBuilder;
 
@@ -14,19 +9,12 @@ import hello.services.PostgresFortunesService;
 
 public final class App {
   public static void main(String[] args) {
-    ServerBuilder sb = new ServerBuilder();
+    ServerBuilder sb = Server.builder();
 
     sb.http(8080)
       .annotatedService(new HelloService())
       .annotatedService(new PostgresDbService())
-      .annotatedService(new PostgresFortunesService())
-      .decorator((delegate, ctx, req) -> {
-        ctx.addAdditionalResponseHeader(HttpHeaderNames.SERVER, "armeria");
-        ctx.addAdditionalResponseHeader(HttpHeaderNames.DATE,
-                                        DateTimeFormatter.RFC_1123_DATE_TIME.format(
-                                                ZonedDateTime.now(ZoneOffset.UTC)));
-        return delegate.serve(ctx, req);
-      });
+      .annotatedService(new PostgresFortunesService());
 
     Server server = sb.build();
     server.start().join();
