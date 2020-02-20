95ff8ed564ffd83c2ef59ab5bd2f73b7eddad33f
Joanhey <joanhey@kumbiaphp.com>
2020-02-20 18:25:36 +0100

    Fix initial date header

--- frameworks/PHP/workerman/server.php
+++ frameworks/PHP/workerman/server.php
@@ -9,6 +9,7 @@ use Workerman\Lib\Timer;
 $http_worker                = new Worker('http://0.0.0.0:8080');
 $http_worker->count         = (int) shell_exec('nproc') * 4;
 $http_worker->onWorkerStart = function () {
+    Header::$date = 'Date: '.gmdate('D, d M Y H:i:s').' GMT';
     Timer::add(1, function() {
         Header::$date = 'Date: '.gmdate('D, d M Y H:i:s').' GMT';
     });
