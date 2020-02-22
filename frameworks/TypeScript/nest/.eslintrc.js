f5caa199e8085bade07f0b8fd3a04d3aff701de5
Patryk Strugacz <pstrugacz@pm.me>
2020-02-22 11:42:56 +0100

    Merge nestjs frameworks, update deps

--- frameworks/TypeScript/nest/.eslintrc.js
+++ frameworks/TypeScript/nest/.eslintrc.js
@@ -14,7 +14,6 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    jest: true,
   },
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
--- frameworks/TypeScript/nest/src/app.module.ts
+++ frameworks/TypeScript/nest/src/app.module.ts
@@ -1,13 +1,16 @@
-import { Module } from '@nestjs/common';
+import { CacheModule, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BenchModule } from './bench/bench.module';
-
+import { Fortune } from './fortune.entity';
+import { World } from './world.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), BenchModule],
+  imports: [
+    TypeOrmModule.forRoot(),
+    TypeOrmModule.forFeature([World, Fortune]),
+    CacheModule.register(),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
