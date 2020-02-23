974889e0d458055ebcf10680e634b45a0e46c271
Andreas Nägeli <andr.naegeli@gmail.com>
2020-02-23 23:12:49 +0100

    Switch to GenHTTP 2.8 and simplify program (#5505)

--- frameworks/CSharp/genhttp/Benchmarks/Program.cs
+++ frameworks/CSharp/genhttp/Benchmarks/Program.cs
@@ -16,35 +16,14 @@ namespace Benchmarks
 
         public static int Main(string[] args)
         {
-            try
-            {
-                var waitEvent = new AutoResetEvent(false);
+            var tests = Layout.Create()
+                              .Add("plaintext", Content.From("Hello, World!"))
+                              .Add<JsonResource>("json");
 
-                AppDomain.CurrentDomain.ProcessExit += (s, e) =>
-                {
-                    waitEvent.Set();
-                };
-
-                var tests = Layout.Create()
-                                  .Add("plaintext", Content.From("Hello, World!"))
-                                  .Add<JsonResource>("json");
-
-                var server = Server.Create()
-                                   .Router(tests)
-                                   .Compression(false);
-
-                using (var instance = server.Build())
-                {
-                    waitEvent.WaitOne();
-                }
-
-                return 0;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                return -1;
-            }
+            return Host.Create()
+                       .Router(tests)
+                       .Compression(false)
+                       .Run();
         }
 
     }
