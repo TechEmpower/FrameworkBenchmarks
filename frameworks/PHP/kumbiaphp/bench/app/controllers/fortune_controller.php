875e2e5c8bf2ff4f2a74a72371bda1438a56a24f
Joanhey <joanhey@kumbiaphp.com>
2020-02-25 12:31:34 +0100

    KumbiaPHP with Workerman

--- frameworks/PHP/kumbiaphp/bench/app/controllers/fortune_controller.php
+++ frameworks/PHP/kumbiaphp/bench/app/controllers/fortune_controller.php
@@ -5,7 +5,7 @@ class FortuneController extends AppController
     public function index()
     {
         View::select(null,'fortune');
-        $data = World::all('SELECT id, message FROM Fortune');
+        $data = Fortune::all('SELECT id, message FROM Fortune');
         $data[] = (object) ['id' => 0,'message' => 'Additional fortune added at request time.'];
         usort($data, function($a, $b){return strcmp($a->message, $b->message);});
         $this->data = $data;
