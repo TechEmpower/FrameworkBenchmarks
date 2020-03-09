049f230c8c4266e8583cb9beb2c46e5fb6d1d1a7
walkor <walkor@users.noreply.github.com>
2020-03-10 00:03:22 +0800

    Upgrade workerman to 4.x (#5535)
    * upgrade workerman to 4.0.0
    
    * fix server-async.php
    
    * fix server-async.php
    
    Co-authored-by: walkor <walkor@workerman.net>

--- frameworks/PHP/workerman/server.php
+++ frameworks/PHP/workerman/server.php
@@ -2,25 +2,22 @@
 require_once __DIR__.'/vendor/autoload.php';
 require_once __DIR__.'/app.php';
 
-use Workerman\Protocols\Http;
 use Workerman\Worker;
-use Workerman\Lib\Timer;
+use Workerman\Timer;
 
 $http_worker                = new Worker('http://0.0.0.0:8080');
 $http_worker->count         = (int) shell_exec('nproc') * 4;
 $http_worker->onWorkerStart = function () {
-    Header::$date = 'Date: '.gmdate('D, d M Y H:i:s').' GMT';
+    Header::$date = gmdate('D, d M Y H:i:s').' GMT';
     Timer::add(1, function() {
-        Header::$date = 'Date: '.gmdate('D, d M Y H:i:s').' GMT';
+        Header::$date = gmdate('D, d M Y H:i:s').' GMT';
     });
     init();
 };
 
-$http_worker->onMessage = static function ($connection) {
+$http_worker->onMessage = static function ($connection, $request) {
 
-    Http::header(Header::$date);
-
-    $connection->send(router());
+    $connection->send(router($request));
     
 };
 
