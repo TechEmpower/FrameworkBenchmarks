20abbbc40f39c435c91ebb6b0ba1d4e4ee4dc774
Andy Pan <panjf2000@gmail.com>
2020-03-21 20:41:26 +0800

    Refine HTTP parser of gnet

--- frameworks/Go/gnet/src/main.go
+++ frameworks/Go/gnet/src/main.go
@@ -1,29 +1,21 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"log"
 	"runtime"
 	"time"
-	"unsafe"
 
 	"github.com/panjf2000/gnet"
 )
-
-type request struct {
-	proto, method string
-	path, query   string
-	head, body    string
-	remoteAddr    string
-}
 
 type httpServer struct {
 	*gnet.EventServer
 }
 
 type httpCodec struct {
-	req request
 }
 
 func (hc *httpCodec) Encode(c gnet.Conn, buf []byte) (out []byte, err error) {
@@ -37,7 +29,7 @@ func (hc *httpCodec) Decode(c gnet.Conn) (out []byte, err error) {
 	// process the pipeline
 	var leftover []byte
 pipeline:
-	if leftover, _ = parseReq(buf, &hc.req); len(leftover) == len(buf) {
+	if leftover = parseReq(buf); len(leftover) == len(buf) {
 		// request not ready, yet
 		return
 	}
@@ -83,71 +75,21 @@ func main() {
 // The head parameter should be a series of lines ending with "\r\n" or empty.
 func appendResp(b []byte) []byte {
 	b = append(b, "HTTP/1.1 200 OK\r\nServer: gnet\r\nContent-Type: text/plain\r\nDate: "...)
-
 	b = time.Now().AppendFormat(b, "Mon, 02 Jan 2006 15:04:05 GMT")
-
 	b = append(b, "\r\nContent-Length: 13\r\n\r\nHello, World!"...)
-
 	return b
 }
 
-func b2s(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
-}
+var brn = []byte("\r\n\r\n")
 
 // parseReq is a very simple http request parser. This operation
 // waits for the entire payload to be buffered before returning a
 // valid request.
-func parseReq(data []byte, req *request) (leftover []byte, err error) {
-	sdata := b2s(data)
-	dlen := len(sdata)
-	var (
-		i, s int
-		head string
-		q    = -1
-	)
-	// method, path, proto line
-	for ; i < dlen; i++ {
-		if sdata[i] == ' ' {
-			req.method = sdata[s:i]
-			for i, s = i+1, i+1; i < dlen; i++ {
-				if sdata[i] == '?' && q == -1 {
-					q = i - s
-				} else if sdata[i] == ' ' {
-					if q != -1 {
-						req.path = sdata[s:q]
-						req.query = req.path[q+1 : i]
-					} else {
-						req.path = sdata[s:i]
-					}
-					for i, s = i+1, i+1; i < dlen; i++ {
-						if sdata[i] == '\n' && sdata[i-1] == '\r' {
-							req.proto = sdata[s:i]
-							i, s = i+1, i+1
-							break
-						}
-					}
-					break
-				}
-			}
-			break
-		}
+func parseReq(data []byte) []byte {
+	if i := bytes.Index(data, brn); i != -1 {
+		return data[i+4:]
 	}
-	if req.proto == "" {
-		return data, fmt.Errorf("malformed request")
-	}
-	head = sdata[:s]
-	for ; i < dlen; i++ {
-		if i > 1 && sdata[i] == '\n' && sdata[i-1] == '\r' {
-			line := sdata[s : i-1]
-			s = i + 1
-			if line == "" {
-				req.head = sdata[len(head)+2 : i+1]
-				i++
-				return data[i:], nil
-			}
-		}
-	}
+
 	// not enough data
-	return data, nil
+	return data
 }
