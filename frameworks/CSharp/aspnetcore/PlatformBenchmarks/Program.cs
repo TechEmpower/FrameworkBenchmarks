0e0a0e5edbfc3064fd623f587bb96003776a745c
Ben Adams <thundercat@illyriad.co.uk>
2020-04-06 16:30:21 +0100

    ASP.NET Core Platform Sync (#5579)
    * ASP.NET Core Platform Sync
    
    * Remove redundant read
    
    * Combine preamble
    
    * fix default

--- frameworks/CSharp/aspnetcore/PlatformBenchmarks/Program.cs
+++ frameworks/CSharp/aspnetcore/PlatformBenchmarks/Program.cs
@@ -4,10 +4,11 @@
 using System;
 using System.Net;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core.Internal.Http;
 using Microsoft.Extensions.Configuration;
+#if DATABASE
 using Npgsql;
 using MySql.Data.MySqlClient;
+#endif
 
 namespace PlatformBenchmarks
 {
@@ -20,12 +21,15 @@ namespace PlatformBenchmarks
             Args = args;
 
             Console.WriteLine(BenchmarkApplication.ApplicationName);
+#if !DATABASE
             Console.WriteLine(BenchmarkApplication.Paths.Plaintext);
             Console.WriteLine(BenchmarkApplication.Paths.Json);
+#else
             Console.WriteLine(BenchmarkApplication.Paths.Fortunes);
             Console.WriteLine(BenchmarkApplication.Paths.SingleQuery);
             Console.WriteLine(BenchmarkApplication.Paths.Updates);
             Console.WriteLine(BenchmarkApplication.Paths.MultipleQueries);
+#endif
             DateHeader.SyncDateTimer();
 
             var host = BuildWebHost(args);
@@ -43,6 +47,7 @@ namespace PlatformBenchmarks
                 .Build();
 
             var appSettings = config.Get<AppSettings>();
+#if DATABASE
             Console.WriteLine($"Database: {appSettings.Database}");
 
             if (appSettings.Database == DatabaseServer.PostgreSql)
@@ -53,12 +58,13 @@ namespace PlatformBenchmarks
             {
                 BenchmarkApplication.Db = new RawDb(new ConcurrentRandom(), MySqlClientFactory.Instance, appSettings);
             }
+#endif
 
             var host = new WebHostBuilder()
                 .UseBenchmarksConfiguration(config)
                 .UseKestrel((context, options) =>
                 {
-                    IPEndPoint endPoint = context.Configuration.CreateIPEndPoint();
+                    var endPoint = context.Configuration.CreateIPEndPoint();
 
                     options.Listen(endPoint, builder =>
                     {
