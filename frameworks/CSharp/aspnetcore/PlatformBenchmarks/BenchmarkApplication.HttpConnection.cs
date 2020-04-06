11ca70ef62b9d958213dfd224731c43c2d85f7f9
Ben Adams <thundercat@illyriad.co.uk>
2020-04-06 16:30:21 +0100

    ASP.NET Core Platform Sync (#5579)
    * ASP.NET Core Platform Sync
    
    * Remove redundant read
    
    * Combine preamble
    
    * fix default

--- frameworks/CSharp/aspnetcore/PlatformBenchmarks/BenchmarkApplication.HttpConnection.cs
+++ frameworks/CSharp/aspnetcore/PlatformBenchmarks/BenchmarkApplication.HttpConnection.cs
@@ -48,23 +48,67 @@ namespace PlatformBenchmarks
             return HtmlEncoder.Create(settings);
         }
 
+#if !DATABASE
         private async Task ProcessRequestsAsync()
         {
             while (true)
             {
-                var task = Reader.ReadAsync();
+                var readResult = await Reader.ReadAsync();
 
-                if (!task.IsCompleted)
+                if (!HandleRequest(readResult))
                 {
-                    // No more data in the input
-                    await OnReadCompletedAsync();
+                    return;
                 }
 
-                var result = await task;
-                var buffer = result.Buffer;
+                await Writer.FlushAsync();
+            }
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private bool HandleRequest(in ReadResult result)
+        {
+            var buffer = result.Buffer;
+            var writer = GetWriter(Writer);
+
+            while (true)
+            {
+                if (!ParseHttpRequest(ref buffer, result.IsCompleted, out var examined))
+                {
+                    return false;
+                }
+
+                if (_state == State.Body)
+                {
+                    ProcessRequest(ref writer);
+
+                    _state = State.StartLine;
+
+                    if (!buffer.IsEmpty)
+                    {
+                        // More input data to parse
+                        continue;
+                    }
+                }
+
+                // No more input or incomplete data, Advance the Reader
+                Reader.AdvanceTo(buffer.Start, examined);
+                break;
+            }
+
+            writer.Commit();
+            return true;
+        }
+#else
+        private async Task ProcessRequestsAsync()
+        {
+            while (true)
+            {
+                var readResult = await Reader.ReadAsync();
+
+                var buffer = readResult.Buffer;
                 while (true)
                 {
-                    if (!ParseHttpRequest(ref buffer, result.IsCompleted, out var examined))
+                    if (!ParseHttpRequest(ref buffer, readResult.IsCompleted, out var examined))
                     {
                         return;
                     }
@@ -86,18 +130,19 @@ namespace PlatformBenchmarks
                     Reader.AdvanceTo(buffer.Start, examined);
                     break;
                 }
+
+                await Writer.FlushAsync();
             }
         }
-
+#endif
         private bool ParseHttpRequest(ref ReadOnlySequence<byte> buffer, bool isCompleted, out SequencePosition examined)
         {
             examined = buffer.End;
-
-            var consumed = buffer.Start;
             var state = _state;
 
             if (!buffer.IsEmpty)
             {
+                SequencePosition consumed;
                 if (state == State.StartLine)
                 {
                     if (Parser.ParseRequestLine(new ParsingAdapter(this), buffer, out consumed, out examined))
@@ -141,18 +186,30 @@ namespace PlatformBenchmarks
             return true;
         }
 
+#if NETCOREAPP5_0
+
+        public void OnStaticIndexedHeader(int index)
+        {
+        }
+
+        public void OnStaticIndexedHeader(int index, ReadOnlySpan<byte> value)
+        {
+        }
+
+        public void OnHeader(ReadOnlySpan<byte> name, ReadOnlySpan<byte> value)
+        {
+        }
+        public void OnHeadersComplete(bool endStream)
+        {
+        }
+#else
         public void OnHeader(Span<byte> name, Span<byte> value)
         {
         }
-
         public void OnHeadersComplete()
         {
         }
-
-        public async ValueTask OnReadCompletedAsync()
-        {
-            await Writer.FlushAsync();
-        }
+#endif
 
         private static void ThrowUnexpectedEndOfData()
         {
@@ -194,14 +251,27 @@ namespace PlatformBenchmarks
             public ParsingAdapter(BenchmarkApplication requestHandler)
                 => RequestHandler = requestHandler;
 
+#if NETCOREAPP5_0
+            public void OnStaticIndexedHeader(int index) 
+                => RequestHandler.OnStaticIndexedHeader(index);
+
+            public void OnStaticIndexedHeader(int index, ReadOnlySpan<byte> value)
+                => RequestHandler.OnStaticIndexedHeader(index, value);
+
+            public void OnHeader(ReadOnlySpan<byte> name, ReadOnlySpan<byte> value)
+                => RequestHandler.OnHeader(name, value);
+
+            public void OnHeadersComplete(bool endStream)
+                => RequestHandler.OnHeadersComplete(endStream);
+#else
             public void OnHeader(Span<byte> name, Span<byte> value)
                 => RequestHandler.OnHeader(name, value);
+            public void OnHeadersComplete()
+                => RequestHandler.OnHeadersComplete();
+#endif
 
             public void OnStartLine(HttpMethod method, HttpVersion version, Span<byte> target, Span<byte> path, Span<byte> query, Span<byte> customMethod, bool pathEncoded)
                 => RequestHandler.OnStartLine(method, version, target, path, query, customMethod, pathEncoded);
-
-            public void OnHeadersComplete()
-                => RequestHandler.OnHeadersComplete();
         }
     }
 }
