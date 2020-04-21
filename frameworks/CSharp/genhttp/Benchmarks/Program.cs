f4d8ae64f44c2b5eaf6cf490a6cf93fc7c8898ba
Andreas Nägeli <andreas.naegeli@nexmart.com>
2020-04-21 16:19:43 +0200

    Switch to GenHTTP 3.0.0

--- frameworks/CSharp/genhttp/Benchmarks/Program.cs
+++ frameworks/CSharp/genhttp/Benchmarks/Program.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-
-using GenHTTP.Core;
+﻿using GenHTTP.Core;
 using GenHTTP.Modules.Core;
 using GenHTTP.Modules.Webservices;
 
@@ -21,8 +17,7 @@ namespace Benchmarks
                               .Add<JsonResource>("json");
 
             return Host.Create()
-                       .Router(tests)
-                       .Compression(false)
+                       .Handler(tests)
                        .Run();
         }
 
