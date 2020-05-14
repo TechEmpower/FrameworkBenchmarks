c931d0bbf743653441a8f2695a4d2def79ff5f8b
Marcelo Barbosa <marcelo.barbosa@dazn.com>
2020-05-14 18:16:25 +0200

    use Headers constructor

--- frameworks/TypeScript/deno/src/handlers.ts
+++ frameworks/TypeScript/deno/src/handlers.ts
@@ -1,33 +1,39 @@
-import { ServerRequest } from "https://deno.land/std@v0.50.0/http/server.ts";
+import {
+  ServerRequest,
+  Response
+} from "https://deno.land/std@v0.50.0/http/server.ts";
 
 interface IRequestHandler {
-  (request: ServerRequest): void
+  (request: ServerRequest): Promise<void>
 }
 
-const HELLO_WORLD = new TextEncoder().encode("Hello, World!");
+const SERVER: string = "Deno";
+const HELLO_WORLD: Uint8Array = new TextEncoder().encode("Hello, World!");
 
-const json: IRequestHandler = (req: ServerRequest) => {
-  const headers = new Headers();
-  headers.set("server", "Deno");
-  headers.set("content-type", "application/json");
-  headers.set("date", (new Date()).toUTCString());
+let date: string = new Date().toUTCString();
+setInterval(() => { date = new Date().toUTCString(); }, 1000);
+
+const json: IRequestHandler = async (req: ServerRequest): Promise<void> => {
+  const headers = new Headers([
+    ["server", SERVER],
+    ["content-type", "application/json"],
+    ["date", date]
+  ]);
 
   req.respond({
     headers,
     body: JSON.stringify({ message: "Hello, World!" })
-  });
+  } as Response);
 };
 
-const plaintext: IRequestHandler = (req: ServerRequest) => {
-  const headers = new Headers();
-  headers.set("server", "Deno");
-  headers.set("content-type", "text/plain; charset=UTF-8");
-  headers.set("date", (new Date()).toUTCString());
+const plaintext: IRequestHandler = async (req: ServerRequest): Promise<void> => {
+  const headers = new Headers([
+    ["server", SERVER],
+    ["content-type", "text/plain; charset=UTF-8"],
+    ["date", date]
+  ]);
 
-  req.respond({
-    headers,
-    body: HELLO_WORLD
-  });
+  req.respond({ headers, body: HELLO_WORLD } as Response);
 };
 
 const handlers: { [Key: string]: IRequestHandler } = {};
@@ -37,4 +43,4 @@ handlers["/plaintext"] = plaintext;
 
 export {
   handlers
-}
+};
