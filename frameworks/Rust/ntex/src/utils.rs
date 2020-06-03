0644179edd23dd436c6430e9893a4e32f67b231b
Nikolay Kim <fafhrd91@gmail.com>
2020-06-03 22:31:43 +0600

    ntex framework (#5710)
    * add ntex framework
    
    * update travis config
    
    * use vec instead of hashmap
    
    * use /json path

---
+++ frameworks/Rust/ntex/src/utils.rs
@@ -0,0 +1,65 @@
+#![allow(dead_code)]
+use std::{cmp, io};
+
+use atoi::FromRadix10;
+use bytes::BytesMut;
+use serde_derive::Serialize;
+use yarte::Template;
+
+#[allow(non_snake_case)]
+#[derive(Serialize, Debug)]
+pub struct Fortune {
+    pub id: i32,
+    pub message: String,
+}
+
+pub const SIZE: usize = 27;
+
+pub struct Writer<'a>(pub &'a mut BytesMut);
+
+impl<'a> io::Write for Writer<'a> {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.0.extend_from_slice(buf);
+        Ok(buf.len())
+    }
+    fn flush(&mut self) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+pub fn get_query_param(query: &str) -> u16 {
+    let q = if let Some(pos) = query.find("q") {
+        u16::from_radix_10(query.split_at(pos + 2).1.as_ref()).0
+    } else {
+        1
+    };
+    cmp::min(500, cmp::max(1, q))
+}
+
+markup::define! {
+    FortunesTemplate(fortunes: Vec<Fortune>) {
+        {markup::doctype()}
+        html {
+            head {
+                title { "Fortunes" }
+            }
+            body {
+                table {
+                    tr { th { "id" } th { "message" } }
+                    @for item in {fortunes} {
+                        tr {
+                            td { {item.id} }
+                            td { {markup::raw(v_htmlescape::escape(&item.message))} }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[derive(Template)]
+#[template(path = "fortune.hbs")]
+pub struct FortunesYarteTemplate {
+    pub fortunes: Vec<Fortune>,
+}
