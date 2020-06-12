1ffc661aebc23bc2e745093d51a1cce87de7555b
Ryan Riley <ryanriley@live.com>
2020-06-11 21:32:28 -0500

    Production-like HTML rendering and OOB JSON rendering

--- frameworks/FSharp/frank/src/App/Program.fs
+++ frameworks/FSharp/frank/src/App/Program.fs
@@ -2,72 +2,42 @@ module App.App
 
 open System
 open System.IO
+open System.Text
+open System.Text.Json.Serialization
+open System.Text.Json
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Hosting
 open Microsoft.AspNetCore.Http
 open Microsoft.Extensions.Logging
 open Dapper
+open Giraffe
 open Frank.Builder
 open FSharp.Control.Tasks
 open Npgsql
-open App
 open Models
-
-let [<Literal>] private DefaultCapacity = 1386
-let private MaxBuilderSize = DefaultCapacity * 3
-
-type MemoryStreamCache = 
-    
-    [<ThreadStatic>]
-    [<DefaultValue>]
-    static val mutable private instance: MemoryStream
-
-    static member Get() = MemoryStreamCache.Get(DefaultCapacity)
-    static member Get(capacity:int) = 
-        
-        if capacity <= MaxBuilderSize then
-            let ms = MemoryStreamCache.instance;
-            let capacity = max capacity DefaultCapacity
-            
-            if ms <> null && capacity <= ms.Capacity then
-                MemoryStreamCache.instance <- null;
-                ms.SetLength 0L
-                ms
-            else
-                new MemoryStream(capacity)
-        else
-            new MemoryStream(capacity)
-
-    static member Release(ms:MemoryStream) = 
-        if ms.Capacity <= MaxBuilderSize then
-            MemoryStreamCache.instance <- ms
 
 let inline contentLength x = new Nullable<int64> ( int64 x )
 
-let json' data : HttpContext -> Task<unit> =
+let json' data : HttpContext -> Task =
+    let options = JsonSerializerOptions()
+    options.Converters.Add(JsonFSharpConverter())
     fun ctx ->
-        let bytes = Utf8Json.JsonSerializer.Serialize(data)
-        ctx.Response.ContentLength <- contentLength bytes.Length
         ctx.Response.ContentType <- "application/json"
         ctx.Response.StatusCode <- 200
-        task {
-            do! ctx.Response.Body.WriteAsync(bytes, 0, bytes.Length)
-        }
+        JsonSerializer.SerializeAsync(ctx.Response.Body, data)
 
-let text' (msg:string): HttpContext -> Task<unit> =
-    let bytes = System.Text.Encoding.UTF8.GetBytes(msg)
+let text' (msg:string): HttpContext -> Task =
+    let bytes = Encoding.UTF8.GetBytes(msg)
     fun ctx ->
         ctx.Response.ContentLength <- contentLength bytes.Length
         ctx.Response.ContentType <- "text/plain"
         ctx.Response.StatusCode <- 200
-        task {
-            do! ctx.Response.Body.WriteAsync(bytes, 0, bytes.Length)
-        }
+        ctx.Response.Body.WriteAsync(bytes, 0, bytes.Length)
 
 // Pulled from Giraffe example
-let fortunes' : HttpContext -> Task<unit> = 
+let fortunes' = 
     let extra = { id = 0; message = "Additional fortune added at request time." }
-    fun ctx ->
+    fun next (ctx: HttpContext) ->
         let conn = new NpgsqlConnection(ConnectionString)
         ctx.Response.RegisterForDispose conn
         task {
@@ -79,16 +49,7 @@ let fortunes' : HttpContext -> Task<unit> =
                 xs.Sort FortuneComparer
                 xs
 
-            let html = MemoryStreamCache.Get()
-            let view = fortunes |> HtmlViews.fortunes 
-            StetefullRendering.renderHtmlToStream html view
-
-            ctx.Response.ContentType <- "text/html;charset=utf-8"
-            ctx.Response.ContentLength <- contentLength html.Length
-            ctx.Response.StatusCode <- 200
-            do! html.CopyToAsync ctx.Response.Body
-
-            MemoryStreamCache.Release html
+            return! htmlView (HtmlViews.fortunes fortunes) next ctx
         }
 
 // Resources
@@ -102,7 +63,7 @@ let plaintext =
 let json =
     resource "/json" {
         name "JSON"
-        get (json' struct {| message = "Hello, World!" |})
+        get (json' struct {|message="Hello, World!"|})
     }
 
 let fortunes =
