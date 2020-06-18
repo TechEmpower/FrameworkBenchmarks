f26be1c79eeb548656665e3d6675bcfce0da1fe5
Sergio Andrés Virviescas Santana <developersavsgio@gmail.com>
2020-06-18 22:34:22 +0200

    [Go/fasthttp] Upgrade fasthttp to v1.14.0, remove some tests and refactor (#5774)
    * Upgrade fasthttp to v1.14.0, remove some tests and refactor
    
    * Fix typo

--- frameworks/Go/fasthttp/src/handlers/message.go
+++ frameworks/Go/fasthttp/src/handlers/message.go
@@ -2,41 +2,19 @@ package handlers
 
 import (
 	"sync"
-
-	"github.com/tidwall/sjson"
 )
 
-// Message struct
-type Message struct {
-	Message string `json:"message"`
-}
-
-var messageJSONStr = []byte(`{"message": ""}`)
-
-// MessagePool ...
-var MessagePool = sync.Pool{
+var messagePool = &sync.Pool{
 	New: func() interface{} {
 		return new(Message)
 	},
 }
 
-// AcquireMessage returns new message from pool
-func AcquireMessage() *Message {
-	return MessagePool.Get().(*Message)
+func acquireMessage() *Message {
+	return messagePool.Get().(*Message)
 }
 
-// ReleaseMessage resets the message and return it to the pool
-func ReleaseMessage(m *Message) {
+func releaseMessage(m *Message) {
 	m.Message = ""
-	MessagePool.Put(m)
-}
-
-// IsNil returns true if the object is nil
-func (m *Message) IsNil() bool {
-	return m == nil
-}
-
-// MarshalSJSON marshals the object as json
-func (m Message) MarshalSJSON() ([]byte, error) {
-	return sjson.SetBytesOptions(messageJSONStr, "message", m.Message, &sjson.Options{Optimistic: true})
+	messagePool.Put(m)
 }
