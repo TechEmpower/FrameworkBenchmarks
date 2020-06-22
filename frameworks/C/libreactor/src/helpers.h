b362d0f77ce2c8dae329c431223d99554f052277
Marc Richards <talawahtech@users.noreply.github.com>
2020-06-22 10:07:32 -0500

    Libreactor Refactor (#5771)
    * libreactor - Upgrade to ubunut 20.04 and gcc 9
    
    * libreactor - Refactor response handling
    
    * libreactor - Fix getaddrinfo() error handling
    
    * libreactor - Simplify setup.c

--- frameworks/C/libreactor/src/helpers.h
+++ frameworks/C/libreactor/src/helpers.h
@@ -1,9 +1,11 @@
 #ifndef HELPERS_H_INCLUDED
 #define HELPERS_H_INCLUDED
 
-void update_date();
+reactor_vector http_date_header(int update);
 
-void copy_date(char *target, size_t position);
+reactor_vector http_content_length_header(uint32_t n);
+
+void write_response(reactor_stream *stream, reactor_vector preamble, reactor_vector body);
 
 reactor_status custom_reactor_net_bind(reactor_net *net, char *node, char *service);
 
