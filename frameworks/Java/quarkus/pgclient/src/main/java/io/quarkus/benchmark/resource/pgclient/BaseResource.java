c483a6f806ec7ffa4a29871428adb8786aa31965
John OHara <johara@redhat.com>
2020-06-24 12:23:31 +0100

    Quarkus tuning
    Switch template engine to Rocker for quarkus-pgclient
    
    Bump quarkus version from 1.4.2.Final to 1.5.2.Final

--- frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/BaseResource.java
+++ frameworks/Java/quarkus/pgclient/src/main/java/io/quarkus/benchmark/resource/pgclient/BaseResource.java
@@ -1,24 +1,15 @@
 package io.quarkus.benchmark.resource.pgclient;
 
-import javax.inject.Inject;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
+import io.vertx.core.http.HttpHeaders;
+import io.vertx.core.json.Json;
 import io.vertx.ext.web.RoutingContext;
 
 public abstract class BaseResource {
-    
-    @Inject
-    ObjectMapper mapper;
 
     void sendJson(RoutingContext rc, Object value) {
-        try {
-            rc.response().putHeader("Content-Type", "application/json");
-            rc.response().end(mapper.writeValueAsString(value));
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }   
+        rc.response()
+                .putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
+                .end(Json.encodeToBuffer(value));
     }
 
     Void handleFail(RoutingContext rc, Throwable t) {
