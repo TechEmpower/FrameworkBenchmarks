ff336576ebfa49559a9cac196e0d3a9d1bd04e03
pim <pimbrouwers@users.noreply.github.com>
2020-07-05 12:39:11 -0400

    🐦[FSharp] Falco - Add benchmark for Falco v1.2.* (#5828)
    * falco v1.2.3
    
    * Server header, removed meta tag from head
    
    * inline json message to meet spec
    
    Co-authored-by: pimbrouwers <pimnation@gmail.com>

---
+++ frameworks/FSharp/falco/src/App/UI.fs
@@ -0,0 +1,11 @@
+﻿module App.UI
+
+open Falco.ViewEngine
+    
+let layout pageTitle content = 
+    html [] [
+        head [] [                
+                title [] [ raw pageTitle ]                                                                
+            ]
+        body [] content
+    ] 
---
+++ frameworks/FSharp/falco/src/App/Value.fs
@@ -0,0 +1,16 @@
+﻿module App.Value
+
+open Falco 
+
+type JsonOutputModel = { message : string }
+    
+module Controller =
+    let defaultMsg = "Hello, World!"
+
+    let handleJson : HttpHandler =
+        fun next ctx ->
+            let json = { message = defaultMsg }
+            (jsonOut json) next ctx
+        
+    let handlePlainText : HttpHandler =        
+        textOut defaultMsg
