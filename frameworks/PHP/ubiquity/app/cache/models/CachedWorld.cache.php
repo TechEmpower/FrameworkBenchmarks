e6cdef422048310d220c17b5d73c69a430f44f44
jcheron <myaddressmail@gmail.com>
2020-07-16 15:43:08 +0200

    add CachedWorld model

---
+++ frameworks/PHP/ubiquity/app/cache/models/CachedWorld.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("#tableName"=>"World","#primaryKeys"=>array("id"=>"id"),"#manyToOne"=>array(),"#fieldNames"=>array("id"=>"id","randomNumber"=>"randomnumber"),"#fieldTypes"=>array("id"=>"int(11)","randomNumber"=>"int(11)"),"#nullable"=>array(),"#notSerializable"=>array(),"#transformers"=>array(),"#accessors"=>array("id"=>"setId","randomNumber"=>"setRandomNumber"));
