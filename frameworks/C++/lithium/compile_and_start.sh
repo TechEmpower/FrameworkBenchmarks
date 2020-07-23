467594dbfb222edba6c22c21b562ea978715cb15
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2020-07-23 16:05:08 +0200

    Lithium: profile on  8081 port.

--- frameworks/C++/lithium/compile_and_start.sh
+++ frameworks/C++/lithium/compile_and_start.sh
@@ -15,7 +15,7 @@ fi
 wget https://raw.githubusercontent.com/matt-42/lithium/$COMMIT/single_headers/lithium_http_backend.hh
 
 clang++ -fprofile-instr-generate=./profile.prof -DPROFILE_MODE -DNDEBUG -D$DB_FLAG -O3 -march=native -std=c++17 ./lithium.cc $CXX_FLAGS -lpthread -lboost_context -lssl -lcrypto -o /lithium_tbf
-/lithium_tbf tfb-database 8080
+/lithium_tbf tfb-database 8081
 llvm-profdata-10 merge -output=./profile.pgo  ./profile.prof
 clang++ -fprofile-instr-use=./profile.pgo -DNDEBUG -D$DB_FLAG -O3 -march=native -std=c++17 ./lithium.cc $CXX_FLAGS -lpthread -lboost_context -lssl -lcrypto -o /lithium_tbf
 
