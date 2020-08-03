7f70ec9de383f76c16d1be9f962c23946401b68d
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2020-08-03 16:38:33 +0200

    Lithium: stop benchmarking sql connections. (#5913)

--- frameworks/C++/lithium/compile_and_start_gcc.sh
+++ frameworks/C++/lithium/compile_and_start_gcc.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 DB_FLAG=$1
-N_SQL_CONNECTIONS=$2
 COMMIT=2ea53ab949627baeaaad77e7662da74f9a9d0a31
 
 if [ $DB_FLAG = "TFB_MYSQL" ]; then
@@ -15,6 +14,6 @@ fi
 
 wget https://raw.githubusercontent.com/matt-42/lithium/$COMMIT/single_headers/lithium_http_backend.hh
 
-g++ -flto -DNDEBUG -DN_SQL_CONNECTIONS=$N_SQL_CONNECTIONS -D$DB_FLAG -O3 -march=native -std=c++17 ./lithium.cc $CXX_FLAGS -lpthread -lboost_context -lssl -lcrypto -o /lithium_tbf
+g++ -flto -DNDEBUG -D$DB_FLAG -O3 -march=native -std=c++17 ./lithium.cc $CXX_FLAGS -lpthread -lboost_context -lssl -lcrypto -o /lithium_tbf
 
 /lithium_tbf tfb-database 8080
--- frameworks/C++/lithium/lithium.cc
+++ frameworks/C++/lithium/lithium.cc
@@ -71,22 +71,24 @@ int main(int argc, char* argv[]) {
     s::id(s::auto_increment, s::primary_key) = int(),
     s::randomNumber = int());
 
-// #if TFB_MYSQL
-//   int db_nconn = 4;
-//   int queries_nconn = 2;
-//   int fortunes_nconn = 4;
-//   int updates_nconn = 1;
-// #elif TFB_PGSQL
-//   int db_nconn = 7;
-//   int queries_nconn = 4;
-//   int fortunes_nconn = 7;
-//   int updates_nconn = 3;
-// #endif
-
+#ifndef N_SQL_CONNECTIONS
+  #if TFB_MYSQL
+    int db_nconn = 5;
+    int queries_nconn = 4;
+    int fortunes_nconn = 5;
+    int updates_nconn = 2;
+  #elif TFB_PGSQL
+    int db_nconn = 5;
+    int queries_nconn = 3;
+    int fortunes_nconn = 7;
+    int updates_nconn = 3;
+  #endif
+#else
   int db_nconn = N_SQL_CONNECTIONS;
   int queries_nconn = N_SQL_CONNECTIONS;
   int fortunes_nconn = N_SQL_CONNECTIONS;
   int updates_nconn = N_SQL_CONNECTIONS;
+#endif
 
   http_api my_api;
 
