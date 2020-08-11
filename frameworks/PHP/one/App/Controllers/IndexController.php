b20ae21d4a7d7b6b1abdc6c2a54b8251f1d86db0
lzc <1018595261@qq.com>
2020-08-11 16:46:07 +0800

    Using bulk updates

--- frameworks/PHP/one/App/Controllers/IndexController.php
+++ frameworks/PHP/one/App/Controllers/IndexController.php
@@ -53,11 +53,13 @@ class IndexController extends Controller
     {
         $count = max(min(intval($count), 500), 1);
         $list  = [];
+        $update = [];
         while ($count--) {
             $row    = World::repeatStatement()->find(mt_rand(1, 10000));
             $list[] = $row;
-            $row->repeatStatement()->update(['randomNumber' => mt_rand(1, 10000)]);
+            $update[] = "({$row->id},".mt_rand(1, 10000).")";
         }
+        World::exec('replace into '.World::TABLE.' (id,randomNumber) values '.implode(',',$update));
         return $this->json($list);
     }
 
