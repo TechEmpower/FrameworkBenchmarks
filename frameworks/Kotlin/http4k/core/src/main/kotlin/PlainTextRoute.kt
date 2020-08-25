86f2b43ea547594f20d650e2d886126a259e4285
David Denton <denton.david@gmail.com>
2020-08-25 20:27:20 +0100

    replace plaintext route with simpler implementation

--- frameworks/Kotlin/http4k/core/src/main/kotlin/PlainTextRoute.kt
+++ frameworks/Kotlin/http4k/core/src/main/kotlin/PlainTextRoute.kt
@@ -4,15 +4,14 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
+import org.http4k.lens.Header.CONTENT_TYPE
 import org.http4k.lens.binary
 import org.http4k.routing.bind
 
 object PlainTextRoute {
-    private val preAllocatedHelloWorldText = "Hello, World!".toByteArray()
+    private const val preAllocatedHelloWorldText = "Hello, World!"
 
-    private val plainTextBody = Body.binary(TEXT_PLAIN).toLens()
-
-    operator fun invoke() = "/plaintext" bind GET to { Response(OK).with(plainTextBody of
-        preAllocatedHelloWorldText.inputStream() )
+    operator fun invoke() = "/plaintext" bind GET to {
+        Response(OK).body(preAllocatedHelloWorldText).with(CONTENT_TYPE of TEXT_PLAIN)
     }
 }
