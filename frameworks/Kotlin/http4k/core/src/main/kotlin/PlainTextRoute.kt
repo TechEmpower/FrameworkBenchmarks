f5045eb26bd7b7f5c5b504c72f284a698e8430fb
David Denton <daviddenton@users.noreply.github.com>
2020-09-08 03:43:41 +0100

    http4k - tidy, adding virtual hosting to apache server implementation (#5980)
    * tidy imports and code
    adding virtual hosting to apache server implementation
    
    * upgrade http4k
    
    * readded missed project directory

--- frameworks/Kotlin/http4k/core/src/main/kotlin/PlainTextRoute.kt
+++ frameworks/Kotlin/http4k/core/src/main/kotlin/PlainTextRoute.kt
@@ -1,11 +1,9 @@
-import org.http4k.core.Body
 import org.http4k.core.ContentType.Companion.TEXT_PLAIN
 import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
 import org.http4k.lens.Header.CONTENT_TYPE
-import org.http4k.lens.binary
 import org.http4k.routing.bind
 
 object PlainTextRoute {
