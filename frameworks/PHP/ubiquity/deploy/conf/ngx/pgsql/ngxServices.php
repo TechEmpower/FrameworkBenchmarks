f7aee1565dd1366bb4fe104666804471c3ee9362
jcheron <myaddressmail@gmail.com>
2020-09-27 00:34:26 +0200

    add other db urls

--- frameworks/PHP/ubiquity/deploy/conf/ngx/pgsql/ngxServices.php
+++ frameworks/PHP/ubiquity/deploy/conf/ngx/pgsql/ngxServices.php
@@ -8,10 +8,10 @@
 \Ubiquity\cache\CacheManager::warmUpControllers([
 	\controllers\Plaintext_::class,
 	\controllers\Json_::class,
-	\controllers\Db_::class
-	// \controllers\Fortunes_::class
+	\controllers\Db_::class,
+	\controllers\Fortunes_::class
 ]);
 
 \Ubiquity\orm\DAO::startDatabase($config, 'pgsql');
 \controllers\Db_::warmup();
-//\controllers\Fortunes_::warmup();
+\controllers\Fortunes_::warmup();
