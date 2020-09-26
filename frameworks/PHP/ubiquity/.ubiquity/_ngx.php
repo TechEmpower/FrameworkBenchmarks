c75c5b6b6d0d3cfad53445002e71820484bed281
jcheron <myaddressmail@gmail.com>
2020-09-26 20:06:27 +0200

    try to fix composer autoloading pb

--- frameworks/PHP/ubiquity/.ubiquity/_ngx.php
+++ frameworks/PHP/ubiquity/.ubiquity/_ngx.php
@@ -1,5 +1,6 @@
 <?php
 // ngx.php
+use Ubiquity\servers\ngx\NgxServer;
 if (! defined('DS')) {
 	define('DS', DIRECTORY_SEPARATOR);
 	define('ROOT', __DIR__ . \DS . '..' . \DS . 'app' . \DS);
@@ -7,9 +8,9 @@ if (! defined('DS')) {
 $config = include ROOT . 'config/config.php';
 $config["siteUrl"] = 'http://0.0.0.0:8080';
 require ROOT . './../vendor/autoload.php';
-\Ubiquity\servers\ngx\NgxServer::init($config);
+NgxServer::init($config);
 require ROOT . 'config/ngxServices.php';
 
 function handle() {
-	\Ubiquity\servers\ngx\NgxServer::handleRequest();
+	NgxServer::handleRequest();
 }
