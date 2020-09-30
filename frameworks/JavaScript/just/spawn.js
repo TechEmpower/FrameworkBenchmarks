6f5c51558caea4f9a45eaf1080c59f1f40ddddf3
Andrew Johnston <apjohnsto@gmail.com>
2020-09-30 13:43:51 +0100

    remove debug logging and use processes instead of threads

--- frameworks/JavaScript/just/spawn.js
+++ frameworks/JavaScript/just/spawn.js
@@ -1,26 +1,6 @@
-const { readFile } = require('fs')
-
-function spawn (source) {
-  const shared = new SharedArrayBuffer(4)
-  const u32 = new Uint32Array(shared)
-  const tid = just.thread.spawn(source, just.args.slice(1), shared)
-  const thread = { tid, u32 }
-  threads.push(thread)
-  return thread
-}
-
-function onTimer () {
-  const { user, system } = just.cpuUsage()
-  const { rss } = just.memoryUsage()
-  let total = 0
-  for (const thread of threads) {
-    total += Atomics.load(thread.u32, 0)
-  }
-  just.error(`threads ${threads.length} total ${total} mem ${rss} cpu (${user.toFixed(2)}/${system.toFixed(2)}) ${(user + system).toFixed(2)} qps/core ${(total / (user + system)).toFixed(2)}`)
-}
-
-const source = readFile(just.args[2] || 'test.js')
+const { STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO } = just.sys
+const path = just.sys.cwd()
+const [...args] = just.args.slice(2)
 const cpus = parseInt(just.env().CPUS || just.sys.cpus, 10)
-const threads = []
-for (let i = 0; i < cpus; i++) spawn(source)
-just.setInterval(onTimer, 1000)
+for (let i = 0; i < cpus; i++) just.sys.spawn('./just', path, args, STDIN_FILENO, STDOUT_FILENO, STDERR_FILENO)
+while (1) just.sys.sleep(1)
