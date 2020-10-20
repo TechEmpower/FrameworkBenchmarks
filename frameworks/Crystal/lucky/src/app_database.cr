28ee5000c3c5302fb5552bf93897773351fad460
Matthew McGarvey <matthewmcgarvey14@gmail.com>
2020-10-20 12:25:58 -0500

    Add mutex lock around opening database connection

--- frameworks/Crystal/lucky/src/app_database.cr
+++ frameworks/Crystal/lucky/src/app_database.cr
@@ -1,2 +1,7 @@
 class AppDatabase < Avram::Database
+  @@lock = Mutex.new
+
+  private def db
+    @@db ||= @@lock.synchronize { Avram::Connection.new(url, database_class: self.class).open }
+  end
 end
