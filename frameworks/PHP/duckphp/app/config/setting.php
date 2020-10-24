888420088401bdfca7160f05bacd28a819424c87
dvaknheo <dvaknheo@gmail.com>
2020-10-24 12:25:10 +0800

    fix last local test version

--- frameworks/PHP/duckphp/app/config/setting.php
+++ frameworks/PHP/duckphp/app/config/setting.php
@@ -9,8 +9,8 @@ return [
     'duckphp_platform' => 'default',
     'database' => [
         //'dsn' => 'mysql:host=172.19.0.3;dbname=hello_world;',
-        'dsn' => 'mysql:host=127.0.0.1;dbname=hello_world;',
-        //'dsn' => 'mysql:host=tfb-database;dbname=hello_world;',
+        //'dsn' => 'mysql:host=127.0.0.1;dbname=hello_world;',
+        'dsn' => 'mysql:host=tfb-database;dbname=hello_world;',
         'username' => 'benchmarkdbuser',
         'password' => 'benchmarkdbpass',
         'driver_options' => [ 
