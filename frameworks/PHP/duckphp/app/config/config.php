a55a2f97ae7fe42f4960d8b639e3f2873718f61b
dvaknheo <dvaknheo@gmail.com>
2020-10-24 12:22:57 +0800

    ci again

--- frameworks/PHP/duckphp/app/config/config.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-/**
- * DuckPHP
- * From this time, you never be alone~
- */
-
-return [];
--- frameworks/PHP/duckphp/app/config/setting.php
+++ frameworks/PHP/duckphp/app/config/setting.php
@@ -5,12 +5,12 @@
  */
 
 return [
-    'duckphp_is_debug' => false, //er ,don't turn on the debug again
+    'duckphp_is_debug' => true, // turn on ,for test er ,don't turn on the debug again
     'duckphp_platform' => 'default',
     'database' => [
         //'dsn' => 'mysql:host=172.19.0.3;dbname=hello_world;',
-        //'dsn' => 'mysql:host=127.0.0.1;dbname=hello_world;',
-        'dsn' => 'mysql:host=tfb-database;dbname=hello_world;',
+        'dsn' => 'mysql:host=127.0.0.1;dbname=hello_world;',
+        //'dsn' => 'mysql:host=tfb-database;dbname=hello_world;',
         'username' => 'benchmarkdbuser',
         'password' => 'benchmarkdbpass',
         'driver_options' => [ 
