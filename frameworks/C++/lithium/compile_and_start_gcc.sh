5e98353c75e6eaf0f29d19bf319aff010a1b412d
Matthieu Garrigues <matthieu.garrigues@gmail.com>
2020-10-30 16:46:55 +0100

    Lithium update. (#6100)
    * Lithium update.
    
    * Lithium update.

--- frameworks/C++/lithium/compile_and_start_gcc.sh
+++ frameworks/C++/lithium/compile_and_start_gcc.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 
 DB_FLAG=$1
-COMMIT=58334f62653a330863cfff633b50f3cfc567e527
+COMMIT=17737fb9473e0111d5ab235a13b6a143b5ca1673
 
 if [ $DB_FLAG = "TFB_MYSQL" ]; then
   CXX_FLAGS="-I /usr/include/mariadb  -lmariadbclient "
@@ -12,7 +12,7 @@ elif [ $DB_FLAG = "TFB_PGSQL" ]; then
 fi
 
 
-wget https://raw.githubusercontent.com/matt-42/lithium/$COMMIT/single_headers/lithium_http_backend.hh
+wget https://raw.githubusercontent.com/matt-42/lithium/$COMMIT/single_headers/lithium_http_server.hh
 
 g++ -flto -DNDEBUG -D$DB_FLAG -O3 -march=native -std=c++17 ./lithium.cc $CXX_FLAGS -lpthread -lboost_context -lssl -lcrypto -o /lithium_tbf
 
