3d669ea5fe6a44c59e0babb30f2a7d45e70178d1
Pim Brouwers <pimnation@gmail.com>
2020-11-07 07:00:49 -0500

    code collapse

--- frameworks/FSharp/falco/src/App/Program.fs
+++ frameworks/FSharp/falco/src/App/Program.fs
@@ -6,14 +6,19 @@ open App
 [<Literal>]
 let connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=1024;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3"
 
+[<Literal>]
+let defaultMsg = "Hello, World!"
+
+type JsonModel = { message : string }
+
 [<EntryPoint>]
 let main args =        
     Host.startWebHost 
         args        
-        (Host.configure connectionString)
+        (Server.configure connectionString)
         [
-            get "/plaintext"  Value.handlePlainText
-            get "/json"       Value.handleJson
+            get "/plaintext"  (Response.ofPlainText defaultMsg)
+            get "/json"       (Response.ofJson { message = defaultMsg })
             get "/fortunes"   Fortune.handleIndex
         ]    
     0
