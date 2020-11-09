6ec2f10e12d9ebe93a750814fa16d812255d1c05
Henry <henryfan@msn.com>
2020-11-09 22:28:30 +0800

    [c#/beetlex] update db connection string (#6126)
    * update beetlex 1.4.3
    
    update beetlex 1.4.3
    
    * docker add COMPlus_ReadyToRun variable
    update beetlex
    
    * update beetlex, enabled thread queue
    
    * beetlex framework add db and queries cases
    
    * add db code
    
    * change result json data
    
    * update query url
    
    * beetlex framework add fortunes cases
    
    * change Content-Type
    
    * add beetlex core cases
    
    * fix queries cases
    
    * update config
    
    * change try readline
    
    * update benchmark config
    
    * Update README.md
    
    * Update README.md
    
    * change versus property
    
    * beetlex-core update .net core to v3.0
    
    * change beetlex-core project file
    
    * beetlex update raw db class
    
    * beetlex update raw db
    
    * beetlex debug plaintext
    
    * change debug docker file
    
    * update beetlex to 1.4.0
    
    * update
    
    * beetlex update core 3.1
    
    * [c#/beetlex] add updates cases
    
    * [c#/beetlex] change Server: TFB, change custom connection pool, add update docker
    
    * fix errors
    
    * change pool init
    
    * change connection pool maxsize
    
    * fix fortunes errors
    
    * clear DBRaw _connectionString value.
    
    * [c#beetlex] change update dbconnection pool size
    
    * [c#/beetlex] udpate spanjson to v3.0.1, Npgsql v5.0.0
    
    * [c#/beetlex] add caching sample
    
    * set connectionstring multiplexing
    
    * remove connection multiplexing setting
    
    * [c#/beetlex]change NpgsqlParameter to  NpgsqlParameter<T>
    
    * [c#/beetlex] update dbraw
    
    * [c#/beetlex] change connection string

--- frameworks/CSharp/beetlex/PlatformBenchmarks/HttpServer.cs
+++ frameworks/CSharp/beetlex/PlatformBenchmarks/HttpServer.cs
@@ -27,13 +27,13 @@ namespace PlatformBenchmarks
             ApiServer.Open();
             if (!Program.UpDB)
             {
-               RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
+               RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Delay Us=500;Write Coalescing Buffer Threshold Bytes=1000";
                 //RawDb._connectionString = "Server=192.168.2.19;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
             }
             else
             {
                // RawDb._connectionString = "Server=192.168.2.19;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
-               RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
+               RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3;Multiplexing=true;Write Coalescing Delay Us=500;Write Coalescing Buffer Threshold Bytes=1000";
             }           
             ApiServer.Log(LogType.Info, null, $"Debug mode [{Program.Debug}]");
             return Task.CompletedTask;
