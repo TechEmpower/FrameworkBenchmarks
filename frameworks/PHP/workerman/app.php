f886f4936ece629e08542b5067b0929c96a633af
Joanhey <joanhey@kumbiaphp.com>
2020-11-15 00:59:33 +0100

    Fix for php8

--- frameworks/PHP/workerman/app.php
+++ frameworks/PHP/workerman/app.php
@@ -47,11 +47,11 @@ function router(Request $request)
         case '/update':
             return updateraw($request);
 
-       case '/info':
+/*        case '/info':
             ob_start();
             phpinfo();
             return new Response(200, ['Content-Type' => 'text/plain'], ob_get_clean());
-
+ */
         default:
             return new Response(404, [], 'Error 404');
     }
@@ -74,7 +74,7 @@ function query($request)
     global $world;
 
     $query_count = 1;
-    $q = $request->get('q');
+    $q = (int) $request->get('q');
     if ($q > 1) {
         $query_count = min($q, 500);
     }
@@ -95,7 +95,7 @@ function updateraw($request)
     global $world, $update;
 
     $query_count = 1;
-    $q = $request->get('q');
+    $q = (int) $request->get('q');
     if ($q > 1) {
         $query_count = min($q, 500);
     }
