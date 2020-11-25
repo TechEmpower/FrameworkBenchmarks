a5011ad108176291fc3cbd8f1cbd85c655a9ccd5
Fred Leitz <fred.leitz@gmail.com>
2020-11-25 03:59:26 -0800

    add stuff

--- frameworks/Haskell/postgrest/postgrest.dockerfile
+++ frameworks/Haskell/postgrest/postgrest.dockerfile
@@ -1,6 +1,5 @@
 FROM postgrest/postgrest:latest
 
-ADD ./ /postgrest
 ENV PGRST_DB_URI=postgres://tfb:password@localhost/tfb
 ENV PGRST_DB_SCHEMA=public
 ENV PGRST_DB_ANON_ROLE=tfb
--- frameworks/Haskell/postgrest/src/00-init.sql
+++ frameworks/Haskell/postgrest/src/00-init.sql
@@ -1,8 +1,16 @@
-DROP TABLE IF EXISTS "World";
-CREATE TABLE "World" (id serial primary key not null, randomNumber int not null);
-INSERT INTO "World" SELECT nextval('"World_id_seq"'::regclass), (((random()*9999)::int+1))::int as randomNumber from generate_series(1, 10000) as id;
+create or replace function postgrest_init() returns void as $$
+BEGIN
+  CREATE TABLE IF NOT EXISTS "World" (id serial primary key not null, randomNumber int not null);
+  IF NOT EXISTS (SELECT * FROM "World") THEN 
+    INSERT INTO "World" SELECT nextval('"World_id_seq"'::regclass), (((random()*9999)::int+1))::int as randomNumber from generate_series(1, 10000) as id;
+  END IF;
 
-DROP TABLE IF EXISTS "Fortunes";
+  CREATE TABLE IF NOT EXISTS "Fortunes" (id serial primary key not null, message text);
+  IF NOT EXISTS (SELECT * FROM "Fortunes") THEN 
+    INSERT INTO "Fortunes" (message) VALUES ('A'), ('B'), ('C'), ('D'), ('E'), ('F'), ('G'),('H'),('I'),('J'),('K'),('<script>alert(''foo'');</script>');
+  END IF;
+END
+$$ language plpgsql volatile;
+SELECT postgrest_init();
 
-CREATE TABLE "Fortunes" (id serial primary key not null, message text);
-INSERT INTO "Fortunes" (message) VALUES ('A'), ('B'), ('C'), ('D'), ('E'), ('F'), ('G'),('H'),('I'),('J'),('K'),('<script>alert(''foo'');</script>');
+
