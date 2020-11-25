eb3530418043a51a087fa0db4838a303438f9ef8
Fred Leitz <fred.leitz@gmail.com>
2020-11-25 13:02:12 -0800

    remove setup files

--- frameworks/Haskell/postgrest/src/prerequest.sql
+++
@@ -1,9 +0,0 @@
-create or replace function custom_headers() returns void as $$
-declare
-  user_agent text := current_setting('request.header.user-agent', true);
-begin
-  if user_agent similar to '%MSIE (6.0|7.0)%' then
-    perform set_config('response.headers',
-      '[{"Cache-Control": "no-cache, no-store, must-revalidate"}]', false);
-  end if;
-end; $$ language plpgsql;
