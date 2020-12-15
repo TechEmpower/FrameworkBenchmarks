db3ec363d2f28bfa53d00ba18fa7d73b82d12536
Marc Richards <talawahtech@users.noreply.github.com>
2020-12-15 14:18:50 -0500

    Update to libreactor 2.0 (#6204)
    * libreactor: Standardize on Ubuntu 20.04 and GCC 10
    
    * libreactor: upgrade to v2.0
    
    Stream code now uses recv/send instead of read/write if stream_is_socket is true. This gives a small speed boost.
    
    Network bind code no longer needs pthread so I deleted my custom_reactor_net_bind.
    
    The http_date functions is now visible so I am using it directly instead of reimplementing it.

--- frameworks/C/libreactor/src/main.c
+++ frameworks/C/libreactor/src/main.c
@@ -21,102 +21,59 @@
                       "Server: libreactor\r\n"\
                       "Content-Type: text/plain\r\n"
 
-struct http_server
+void plaintext(server_context *context, char *response)
 {
-  reactor_net    net;
-  list           connections;
-};
-
-static void plaintext(reactor_stream *stream, char *response)
-{
-  static const reactor_vector text_preamble = { .base = TEXT_PREAMBLE, .size = sizeof(TEXT_PREAMBLE) - 1 };
-  write_response(stream, text_preamble, reactor_vector_string(response));
+  static const segment text_preamble = { .base = TEXT_PREAMBLE, .size = sizeof(TEXT_PREAMBLE) - 1 };
+  write_response(&context->session->stream, text_preamble, segment_string(response));
 }
 
-static void json(reactor_stream *stream, clo *json_object)
+void json(server_context *context, clo *json_object)
 {
-  static const reactor_vector json_preamble = { .base = JSON_PREAMBLE, .size = sizeof(JSON_PREAMBLE) - 1 };
+  static const segment json_preamble = { .base = JSON_PREAMBLE, .size = sizeof(JSON_PREAMBLE) - 1 };
   static char json_string[4096];
 
   (void) clo_encode(json_object, json_string, sizeof(json_string));
-  write_response(stream, json_preamble, reactor_vector_string(json_string));
+  write_response(&context->session->stream, json_preamble, segment_string(json_string));
 }
 
-static reactor_status http_handler(reactor_event *event)
+static core_status server_handler(core_event *event)
 {
   static char hello_string[] = "Hello, World!";
   static char default_string[] = "Hello from libreactor!\n";
   static clo_pair json_pair[] = {{ .string = "message", .value = { .type = CLO_STRING, .string = "Hello, World!" }}};
   static clo json_object[] = {{ .type = CLO_OBJECT, .object = json_pair }};
 
-  reactor_http *http_connection = event->state;
-  reactor_http_request *request;
+  server *server = event->state;
+  server_context *context = (server_context *) event->data;
 
-  if (event->type == REACTOR_HTTP_EVENT_REQUEST){
-    request = (reactor_http_request *) event->data;
-
-    if (reactor_vector_equal(request->target, reactor_vector_string("/json"))){
-      json(&http_connection->stream, json_object);
+  if (event->type == SERVER_REQUEST){
+    if (segment_equal(context->request.target, segment_string("/json"))){
+      json(context, json_object);
     }
-    else if (reactor_vector_equal(request->target, reactor_vector_string("/plaintext"))){
-      plaintext(&http_connection->stream, hello_string);
+    else if (segment_equal(context->request.target, segment_string("/plaintext"))){
+      plaintext(context, hello_string);
     }
     else{
-      plaintext(&http_connection->stream, default_string);
+      plaintext(context, default_string);
     }
-    return REACTOR_OK;
+    return CORE_OK;
   }
   else {
-    reactor_http_destruct(http_connection);
-    list_erase(http_connection, NULL);
-    return REACTOR_ABORT;
+    warn("error");
+    server_destruct(server);
+    return CORE_ABORT;
   }
-}
-
-static reactor_status net_handler(reactor_event *event)
-{
-  struct http_server *server = event->state;
-  reactor_http connection_initializer = (reactor_http) {0};
-  reactor_http *http_connection;
-
-  switch (event->type)
-    {
-    case REACTOR_NET_EVENT_ACCEPT:
-      http_connection = list_push_back(&server->connections, &connection_initializer, sizeof (reactor_http));
-      reactor_http_construct(http_connection, http_handler, http_connection);
-      reactor_http_set_mode(http_connection, REACTOR_HTTP_MODE_REQUEST);
-      reactor_http_open(http_connection, event->data);
-      return REACTOR_OK;
-    default:
-      reactor_net_destruct(&server->net);
-      return REACTOR_ABORT;
-    }
-}
-
-static reactor_status http_date_timer_handler(reactor_event *event)
-{
-  (void) event;
-  http_date_header(1); // update the date header
-  return REACTOR_OK;
 }
 
 int main()
 {
-  struct http_server server = {0};
-  reactor_timer timer;
+  server s;
 
   setup();
-  list_construct(&server.connections);
-  reactor_core_construct();
+  core_construct(NULL);
+  server_construct(&s, server_handler, &s);
+  server_open(&s, 0, 8080);
 
-  // Set the correct date before the server starts. Timer then updates it every second
-  http_date_header(1);
-  reactor_timer_construct(&timer, http_date_timer_handler, &timer);
-  reactor_timer_set(&timer, 1, 1000000000);
-
-  reactor_net_construct(&server.net, net_handler, &server);
-  (void) custom_reactor_net_bind(&server.net, "0.0.0.0", "8080");
-
-  reactor_core_run();
-  reactor_core_destruct();
+  core_loop(NULL);
+  core_destruct(NULL);
 }
