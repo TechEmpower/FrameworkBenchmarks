5386e4aba0423b8e668a328e272d72aa4657d9c3
mcmcgrath13 <m.c.mcgrath13@gmail.com>
2020-12-16 18:28:46 -0500

    fix: use project.toml, make multi-threaded and multi-core

--- frameworks/Julia/Http.jl/server.jl
+++ frameworks/Julia/Http.jl/server.jl
@@ -1,7 +1,13 @@
-using HTTP
-using JSON
-using Dates
+using Pkg
 
+Pkg.activate(@__DIR__)
+
+using HTTP
+import JSON3
+using Dates
+using Sockets
+
+@info "starting listener"
 HTTP.listen("0.0.0.0", 8080, reuseaddr=true) do http
    HTTP.setheader(http, "Server" => "Julia-HTTP")
    HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
@@ -14,10 +20,37 @@ HTTP.listen("0.0.0.0", 8080, reuseaddr=true) do http
       HTTP.setheader(http, "Content-Type" => "application/json")
       HTTP.setstatus(http, 200)
       startwrite(http)
-      write(http, JSON.json(Dict(:message => "Hello, World!")))
+      JSON3.write(http, (;message ="Hello, World!"))
    else
        HTTP.setstatus(http, 404)
        startwrite(http)
        write(http, "Not Found")
    end
 end
+
+# function getHeaders(contentType::String)
+#    return [
+#       "Server" => "Julia-HTTP",
+#       "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT",
+#       "Content-Type" => contentType
+#    ]
+# end
+#
+# # "service" functions to actually do the work
+# function helloWorldPlain(req::HTTP.Request)
+#    return HTTP.Response(200, getHeaders("text/plain"), bytes("Hello, World!"))
+# end
+#
+# function helloWorldJSON(req::HTTP.Request)
+#    res = HTTP.Response(JSON3.write(Dict(:message => "Hello, World!")))
+#    res.headers = HTTP.mkheaders(getHeaders("application/json"))
+#    return res
+# end
+#
+# # define REST endpoints to dispatch to "service" functions
+# const TBM_ROUTER = HTTP.Router()
+# HTTP.@register(TBM_ROUTER, "GET", "/plaintext", helloWorldPlain)
+# HTTP.@register(TBM_ROUTER, "GET", "/json", helloWorldJSON)
+#
+# @info "Starting server on port 8080"
+# HTTP.serve(TBM_ROUTER, ip"0.0.0.0", 8080, reuseaddr=true, verbose=true)
