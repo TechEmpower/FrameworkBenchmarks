549e7330f2f320dae6180edd21f8fa088392d120
Henry <henryfan@msn.com>
2020-12-20 20:45:50 +0800

    [c#/beetlex] Optimized actions

--- frameworks/CSharp/beetlex/PlatformBenchmarks/plaintext.cs
+++ frameworks/CSharp/beetlex/PlatformBenchmarks/plaintext.cs
@@ -9,7 +9,7 @@ namespace PlatformBenchmarks
 {
     public partial class HttpHandler
     {
-        public Task Plaintext(ReadOnlySpan<byte> url, PipeStream stream, HttpToken token, ISession session)
+        public Task Plaintext(PipeStream stream, HttpToken token, ISession session)
         {
             stream.Write(_result_plaintext.Data, 0, _result_plaintext.Length);
             OnCompleted(stream, session, token);
