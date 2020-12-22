f7dcfdc1a5629b32f992365ffe14b16d6edaf6aa
Daniel Sagenschneider <daniel@officefloor.net>
2020-12-22 10:23:23 +0800

    Passing benchmark tests

--- frameworks/Java/officefloor/src/woof_benchmark_raw/src/main/java/net/officefloor/benchmark/RawOfficeFloorMain.java
+++ frameworks/Java/officefloor/src/woof_benchmark_raw/src/main/java/net/officefloor/benchmark/RawOfficeFloorMain.java
@@ -118,8 +118,7 @@ public class RawOfficeFloorMain {
 		System.out.println("Starting server on port " + port + " talking to database " + server);
 
 		// Increase the buffer size (note: too high and cause OOM issues)
-		// 20 + 1 to cover initial update query load
-		System.setProperty("reactor.bufferSize.small", String.valueOf(QUERY_BUFFER_SIZE * (20 + 1)));
+		System.setProperty("reactor.bufferSize.small", String.valueOf(QUERY_BUFFER_SIZE));
 
 		// Build the connection pool
 		ConnectionFactoryOptions factoryOptions = ConnectionFactoryOptions.builder()
