0e9a0b37306fbd564e6f77b99138b1ab83695214
Daniel Sagenschneider <daniel@officefloor.net>
2020-12-22 08:26:02 +0800

    Fixing for 3.30.1

--- frameworks/Java/officefloor/src/woof_benchmark_raw/src/main/java/net/officefloor/benchmark/RawOfficeFloorMain.java
+++ frameworks/Java/officefloor/src/woof_benchmark_raw/src/main/java/net/officefloor/benchmark/RawOfficeFloorMain.java
@@ -20,8 +20,6 @@ package net.officefloor.benchmark;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.Writer;
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
 import java.nio.ByteBuffer;
 import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
@@ -643,61 +641,18 @@ public class RawOfficeFloorMain {
 
 		private final Scheduler writeScheduler;
 
-		private final Object socketListener;
-
-		private final Method disableRead;
-
-		private final Method enableRead;
-
-		private boolean isReadEnabled = true;
-
 		private RateLimits(RequestHandler<HttpRequestParser> requestHandler, AtomicInteger db, AtomicInteger queries,
 				AtomicInteger fortunes, AtomicInteger updates) {
-			this.db = new RateLimit(db, this);
-			this.queries = new RateLimit(queries, this);
-			this.fortunes = new RateLimit(fortunes, this);
-			this.updates = new RateLimit(updates, this);
+			this.db = new RateLimit(db);
+			this.queries = new RateLimit(queries);
+			this.fortunes = new RateLimit(fortunes);
+			this.updates = new RateLimit(updates);
 
 			// Create the write scheduler
 			this.socketExecutor = (runnable) -> requestHandler.execute(() -> {
 				runnable.run();
 			});
 			this.writeScheduler = Schedulers.fromExecutor(this.socketExecutor);
-
-			// Obtain the socket listener
-			try {
-				Field socketListenerField = requestHandler.getClass().getDeclaredField("socketListener");
-				socketListenerField.setAccessible(true);
-				this.socketListener = socketListenerField.get(requestHandler);
-				this.disableRead = this.socketListener.getClass().getDeclaredMethod("disableReading");
-				this.disableRead.setAccessible(true);
-				this.enableRead = this.socketListener.getClass().getDeclaredMethod("enableReading");
-				this.enableRead.setAccessible(true);
-			} catch (Exception ex) {
-				throw new IllegalStateException("Unable to obtain enable/disable read methods", ex);
-			}
-		}
-
-		private void disableReading() {
-			if (this.isReadEnabled) {
-				try {
-					this.disableRead.invoke(this.socketListener);
-					this.isReadEnabled = false;
-				} catch (Exception ex) {
-					System.err.println("Failed to disable read");
-				}
-			}
-		}
-
-		private void enableReading(boolean isForce) {
-			if (isForce || !this.isReadEnabled) {
-				try {
-					this.enableRead.invoke(this.socketListener);
-					this.isReadEnabled = true;
-				} catch (Exception ex) {
-					System.err.println("Failed to enable read");
-				}
-			}
 		}
 	}
 
@@ -707,15 +662,12 @@ public class RawOfficeFloorMain {
 
 		private final AtomicInteger activeQueries;
 
-		private final RateLimits rateLimits;
-
 		private int requestCount = 0;
 
 		private boolean isActiveLimit = false;
 
-		public RateLimit(AtomicInteger activeQueries, RateLimits rateLimits) {
+		private RateLimit(AtomicInteger activeQueries) {
 			this.activeQueries = activeQueries;
-			this.rateLimits = rateLimits;
 		}
 
 		private boolean isLimit(int queryCount) {
@@ -740,11 +692,6 @@ public class RawOfficeFloorMain {
 				return false; // within limit
 			}
 
-			// Determine if read limit hit
-			if (this.isActiveLimit) {
-				this.rateLimits.disableReading();
-			}
-
 			// Return whether limit
 			return this.isActiveLimit;
 		}
@@ -753,9 +700,6 @@ public class RawOfficeFloorMain {
 
 			// Update the active queries
 			this.activeQueries.updateAndGet((count) -> count - queryCount);
-
-			// Some processed, so allow further reading
-			this.rateLimits.enableReading(false);
 		}
 	}
 
