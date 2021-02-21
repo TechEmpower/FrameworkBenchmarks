d23784e107693a17435e7b06cb152f4fb26679c3
Michael Spiz <mike@spiz.us>
2021-02-20 18:19:25 -0800

    remove middlewares not required for this benchmark

--- frameworks/Ruby/rails/config/application.rb
+++ frameworks/Ruby/rails/config/application.rb
@@ -28,5 +28,28 @@ module Hello
     # the framework and any gems in your application.
 
     config.action_dispatch.default_headers.merge!('Server' => 'WebServer')
+
+    config.middleware.delete ActionDispatch::HostAuthorization
+    config.middleware.delete Rack::Sendfile
+    config.middleware.delete ActionDispatch::Static
+    config.middleware.delete ActionDispatch::Executor
+    config.middleware.delete Rack::Runtime
+    config.middleware.delete Rack::MethodOverride
+    config.middleware.delete ActionDispatch::RequestId
+    config.middleware.delete ActionDispatch::RemoteIp
+    config.middleware.delete Rails::Rack::Logger
+    config.middleware.delete ActionDispatch::ShowExceptions
+    config.middleware.delete ActionDispatch::DebugExceptions
+    config.middleware.delete ActionDispatch::ActionableExceptions
+    config.middleware.delete ActionDispatch::Reloader
+    config.middleware.delete ActiveRecord::Migration::CheckPending
+    config.middleware.delete ActionDispatch::Cookies
+    config.middleware.delete ActionDispatch::Session::CookieStore
+    config.middleware.delete ActionDispatch::Flash
+    config.middleware.delete ActionDispatch::ContentSecurityPolicy::Middleware
+    config.middleware.delete ActionDispatch::PermissionsPolicy::Middleware
+    config.middleware.delete Rack::Head
+    config.middleware.delete Rack::ConditionalGet
+    config.middleware.delete Rack::ETag
   end
 end
