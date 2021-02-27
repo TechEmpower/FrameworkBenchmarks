b9ff905e0f4f481b15e714cfa6b18e78044bcd24
yuye <631941527@qq.com>
2021-02-27 17:33:00 +0800

    update martian

--- frameworks/Java/martian/src/main/java/com/text/service/TestService.java
+++ frameworks/Java/martian/src/main/java/com/text/service/TestService.java
@@ -3,13 +3,8 @@ package com.text.service;
 import com.mars.common.annotation.bean.MarsBean;
 import com.mars.iserver.constant.HttpConstant;
 import com.mars.iserver.server.impl.MarsHttpExchange;
-import com.mars.iserver.server.model.HttpHeaders;
-import com.mars.server.server.request.HttpMarsRequest;
 import com.mars.server.server.request.HttpMarsResponse;
-import com.sun.org.apache.bcel.internal.generic.FADD;
 import com.text.api.vo.MessageVO;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.Date;
 
