666cdb17b15c4fc05528f3276bf02af8a3701df4
yuye <631941527@qq.com>
2021-02-27 17:16:42 +0800

    update martian

--- frameworks/Java/martian/src/main/java/com/text/service/TestService.java
+++ frameworks/Java/martian/src/main/java/com/text/service/TestService.java
@@ -1,10 +1,12 @@
 package com.text.service;
 
 import com.mars.common.annotation.bean.MarsBean;
+import com.mars.iserver.constant.HttpConstant;
 import com.mars.iserver.server.impl.MarsHttpExchange;
 import com.mars.iserver.server.model.HttpHeaders;
 import com.mars.server.server.request.HttpMarsRequest;
 import com.mars.server.server.request.HttpMarsResponse;
+import com.sun.org.apache.bcel.internal.generic.FADD;
 import com.text.api.vo.MessageVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,15 +16,22 @@ import java.util.Date;
 @MarsBean
 public class TestService {
 
-    private Logger logger = LoggerFactory.getLogger(TestService.class);
-
     public MessageVO json(HttpMarsResponse response){
+        /*
+            Because this is a purely front-end and back-end separation framework,
+            the response header will include cross-domain by default.
+            In order to adapt to the tfb test, these default headers are removed here.
+         */
+        response.geNativeResponse(MarsHttpExchange.class).getResponseHeaders().clear();
+
+        // Add the header required by tfb
+        response.setHeader("Content-Type", HttpConstant.RESPONSE_CONTENT_TYPE);
+        response.setHeader("Server","Martian");
+        response.setHeader("Date", new Date().toGMTString());
+
+
         MessageVO messageVO = new MessageVO();
         messageVO.setMessage("Hello, World!");
-
-        response.setHeader("Server","Martian");
-        response.setHeader("Date", "Wed, "+new Date().toGMTString());
-
         return messageVO;
     }
 }
