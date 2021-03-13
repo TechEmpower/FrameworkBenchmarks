525464f23a97fdbba5a6db1ccb3498032393c483
John Towler <john.towler@contrastsecurity.com>
2021-03-12 17:18:22 -0800

    SDET-282: Add script for running Python agent benchmarks

---
+++ contrast/benchmark-python.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+cd "${0%/*}"
+
+# Use files in the script directory by default
+AGENT_FILE=${1-contrast-agent.tar.gz}
+
+if [[ $(basename -- "$AGENT_FILE") != "contrast-agent.tar.gz" ]];
+    then echo "First argument must be a path to contrast-agent.tar.gz" && exit 1;
+fi;
+
+if [[ ! -f $AGENT_FILE ]];
+    then echo "Could not find $AGENT_FILE" && exit 1;
+fi;
+
+# Copy files into correct place
+./copy-files.sh Python contrast-agent.tar.gz
+
+# Create tfb network if it's not already up
+if ! docker network inspect tfb >/dev/null 2>&1; then
+  docker network create tfb >/dev/null
+fi
+
+# Start contrast-service container
+docker build . -f contrast-service.dockerfile -t contrast-service   
+docker run --rm -d --network tfb --name contrast-service contrast-service
+
+# Run tests
+../tfb --tag contrast --test-lang Python --type fortune --duration 60
+
+# Stop contrast-service container
+docker stop contrast-service
