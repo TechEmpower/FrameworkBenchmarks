1c6865daa73d6717762f1c0de296e2da0c709b5a
test <amit.singh@dream11.com>
2021-03-16 19:16:03 +0530

    fix(zio-http): update zhttp version and change example

--- frameworks/Scala/zio-http/src/main/scala/Main.scala
+++ frameworks/Scala/zio-http/src/main/scala/Main.scala
@@ -1,7 +1,6 @@
 import zhttp.http._
 import zhttp.service.Server
 import zio.{App, ExitCode, URIO}
-import io.netty.handler.codec.http.{HttpHeaderNames => JHttpHeaderNames, HttpHeaderValues => JHttpHeaderValues}
 import com.github.plokhotnyuk.jsoniter_scala.macros._
 import com.github.plokhotnyuk.jsoniter_scala.core._
 import java.time.ZonedDateTime
@@ -14,24 +13,8 @@ object WebApp extends App {
   implicit val codec: JsonValueCodec[Message] = JsonCodecMaker.make
 
   val app = Http.collect[Request] {
-    case Method.GET -> Root / "plaintext" =>
-      Response.http(
-        headers = List(
-          Header(JHttpHeaderNames.SERVER, "zio-http"),
-          Header(JHttpHeaderNames.DATE, s"$createDate"),
-          Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.TEXT_PLAIN),
-        ),
-        content = HttpContent.Complete("Hello, World!"),
-      )
-    case Method.GET -> Root / "json"      =>
-      Response.http(
-        headers = List(
-          Header(JHttpHeaderNames.SERVER, "zio-http"),
-          Header(JHttpHeaderNames.DATE, s"$createDate"),
-          Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.APPLICATION_JSON),
-        ),
-        content = HttpContent.Complete(writeToString(Message(message))),
-      )
+    case Method.GET -> Root / "plaintext" => Response.text(message)
+    case Method.GET -> Root / "json"      => Response.jsonString(writeToString(Message(message)))
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = Server.start(8080, app).exitCode
