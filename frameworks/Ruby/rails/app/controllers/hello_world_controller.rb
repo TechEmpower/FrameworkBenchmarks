1928d6af4cfd65af99536e39c0e977246e946989
Michael Spiz <mike@spiz.us>
2021-03-15 21:48:00 -0700

    move cached_query

--- frameworks/Ruby/rails/app/controllers/hello_world_controller.rb
+++ frameworks/Ruby/rails/app/controllers/hello_world_controller.rb
@@ -23,6 +23,16 @@ class HelloWorldController < ApplicationController
     render json: results
   end
 
+  def cached_query
+    results = QUERY_RANGE.sample(query_count).map do |id|
+      Rails.cache.fetch("world-#{id}") do
+        World.find(id)
+      end
+    end
+
+    render json: results
+  end
+
   def fortune
     @fortunes = Fortune.all.to_a
     @fortunes << Fortune.new(id: 0, message: 'Additional fortune added at request time.')
@@ -34,25 +44,18 @@ class HelloWorldController < ApplicationController
       # get a random row from the database, which we know has 10000
       # rows with ids 1 - 10000
       world = World.select(:id, :randomNumber).find(id)
+      rn = world.randomNumber
+
       loop do
         rn = Random.rand(1..10_000)
         break if rn != world.randomNumber
       end
+      
       world.update_columns(randomNumber: rn)
       world
     end
 
     render json: worlds
-  end
-
-  def cached_query
-    results = QUERY_RANGE.sample(query_count).map do |id|
-      Rails.cache.fetch("world-#{id}") do
-        World.find(id)
-      end
-    end
-
-    render json: results
   end
 
   def query_count
