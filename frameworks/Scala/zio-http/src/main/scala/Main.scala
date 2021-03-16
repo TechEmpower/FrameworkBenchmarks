d9e5d5b68578137d2f29b2c2e8cb7bae62f8024c
test <amit.singh@dream11.com>
2021-03-16 17:38:55 +0530

    style(zio-http): scalafmt

--- frameworks/Scala/zio-http/src/main/scala/Main.scala
+++ frameworks/Scala/zio-http/src/main/scala/Main.scala
@@ -8,28 +8,30 @@ import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
 object WebApp extends App {
-
-  val message: String = "Hello, World!"
-def createDate: String = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now)
+  val message: String                         = "Hello, World!"
+  def createDate: String                      = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now)
   case class Message(message: String)
   implicit val codec: JsonValueCodec[Message] = JsonCodecMaker.make
 
   val app = Http.collect[Request] {
-    case Method.GET -> Root / "plaintext" =>  Response.http(
-      headers = List(
-        Header(JHttpHeaderNames.SERVER, "zio-http"),
-        Header(JHttpHeaderNames.DATE, s"$createDate"),
-        Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.TEXT_PLAIN),
-      ),
-      content = HttpContent.Complete("Hello, World!"),
-    )
-    case Method.GET -> Root / "json" => Response.http(
-      headers = List(
-        Header(JHttpHeaderNames.SERVER, "zio-http"),
-        Header(JHttpHeaderNames.DATE, s"$createDate"),
-        Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.APPLICATION_JSON),
-      ),
-      content = HttpContent.Complete(writeToString(Message(message))))
+    case Method.GET -> Root / "plaintext" =>
+      Response.http(
+        headers = List(
+          Header(JHttpHeaderNames.SERVER, "zio-http"),
+          Header(JHttpHeaderNames.DATE, s"$createDate"),
+          Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.TEXT_PLAIN),
+        ),
+        content = HttpContent.Complete("Hello, World!"),
+      )
+    case Method.GET -> Root / "json"      =>
+      Response.http(
+        headers = List(
+          Header(JHttpHeaderNames.SERVER, "zio-http"),
+          Header(JHttpHeaderNames.DATE, s"$createDate"),
+          Header(JHttpHeaderNames.CONTENT_TYPE, JHttpHeaderValues.APPLICATION_JSON),
+        ),
+        content = HttpContent.Complete(writeToString(Message(message))),
+      )
   }
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = Server.start(8080, app).exitCode
