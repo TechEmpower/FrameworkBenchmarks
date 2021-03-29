846a3c0a095844a911385a29606a1139299ba284
John Towler <john.towler@contrastsecurity.com>
2021-03-29 14:28:34 -0700

    SDET-310: Add script for running .NET Core agent benchmarks

---
+++ contrast/benchmark-dot-net-core.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+
+cd "${0%/*}"
+
+# Use files in the script directory by default
+AGENT_FILE=${1-Contrast.NET.Core.zip}
+
+if [[ $(basename -- "$AGENT_FILE") != "Contrast.NET.Core.zip" ]];
+    then echo "First argument must be a path to Contrast.NET.Core.zip" && exit 1;
+fi;
+
+if [[ ! -f $AGENT_FILE ]];
+    then echo "Could not find $AGENT_FILE" && exit 1;
+fi;
+
+# Copy files into correct place
+./copy-files.sh CSharp $AGENT_FILE
+./copy-files.sh VB $AGENT_FILE
+
+../tfb --tag contrast --test-lang CSharp VB --type fortune --duration 60
