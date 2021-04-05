26ab4804f967fb7af1d8ebdf99dbc18c9a1e39e4
Michael Spiz <mike@spiz.us>
2021-04-04 17:01:28 -0700

    Rails tweaks (#6463)
    * move common code out to a function
    
    * update_columns is more commonly used
    
    * update gems and add redis
    
    * set up redis in container for caching
    
    * move cached_query
    
    * update readme
    
    * add rails gitignores
    
    * ignore pyc files in toolset dir
    
    * use bash
    
    * clean up app controller
    
    * be noisy if redis is down
    
    * simplify update and cached query
    
    * update mysql dockerfile
    
    * oops, set mysql as rails env
    
    * remove rails gitignore as this lives in a docker container

--- frameworks/Ruby/rails/app/controllers/application_controller.rb
+++ frameworks/Ruby/rails/app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
+# frozen_string_literal: true
 class ApplicationController < ActionController::Base
-
   before_action :add_header
 
-protected
+  private
 
   def add_header
     response.set_header('Date', Time.now.httpdate)
   end
-
 end
