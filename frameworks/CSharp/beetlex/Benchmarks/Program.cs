1b191dff39e9deebcb0632b70c2e2ea281f3ab56
Henry <henryfan@msn.com>
2021-05-16 20:03:13 +0800

    update

--- frameworks/CSharp/beetlex/Benchmarks/Program.cs
+++ frameworks/CSharp/beetlex/Benchmarks/Program.cs
@@ -84,8 +84,12 @@ namespace Benchmarks
 
         private HttpApiServer mApiServer;
 
+        private System.Threading.Tasks.TaskCompletionSource<object> mComplete = new TaskCompletionSource<object>();
+
         public async virtual Task StartAsync(CancellationToken cancellationToken)
         {
+
+
             plaintextResult = new StringBytes(_helloWorldPayload);
             mApiServer = new HttpApiServer();
             mApiServer.Options.Port = 8080;
@@ -100,10 +104,14 @@ namespace Benchmarks
             {
                 e.Session["DB"] = new RawDb(new ConcurrentRandom(), Npgsql.NpgsqlFactory.Instance);
             };
+            mApiServer.Started += (o, e) =>
+            {
+                mComplete.TrySetResult(new object());
+            };
             mApiServer.Open();
             RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Delay Us=500;Write Coalescing Buffer Threshold Bytes=1000";
             //RawDb._connectionString = "Server=192.168.2.19;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
-
+            await mComplete.Task;
         }
 
         public virtual Task StopAsync(CancellationToken cancellationToken)
