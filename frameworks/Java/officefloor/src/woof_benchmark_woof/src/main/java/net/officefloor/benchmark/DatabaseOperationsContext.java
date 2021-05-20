e9b36667c14d9ea7cf9c95915f91f558846ba158
Daniel Sagenschneider <daniel@officefloor.net>
2021-05-20 13:21:56 +0800

    Abstract WoOF from database driver
    This will allow introducing Vertx SQL Client

---
+++ frameworks/Java/officefloor/src/woof_benchmark_woof/src/main/java/net/officefloor/benchmark/DatabaseOperationsContext.java
@@ -0,0 +1,64 @@
+package net.officefloor.benchmark;
+
+import java.util.List;
+
+import net.officefloor.server.http.HttpResponse;
+import net.officefloor.server.http.ServerHttpConnection;
+
+/**
+ * @author Daniel Sagenschneider
+ */
+public interface DatabaseOperationsContext {
+
+	/**
+	 * Obtains {@link Exception} for transient overload of resource.
+	 * 
+	 * @return {@link Exception} for transient overload of resource.
+	 */
+	Exception getTransientResourceException();
+
+	/**
+	 * Sends db response.
+	 * 
+	 * @param response   {@link HttpResponse}.
+	 * @param connection {@link ServerHttpConnection}.
+	 * @param world      {@link World} to send.
+	 */
+	void dbSend(HttpResponse response, ServerHttpConnection connection, World world);
+
+	/**
+	 * Sends queries response.
+	 * 
+	 * @param response   {@link HttpResponse}.
+	 * @param connection {@link ServerHttpConnection}.
+	 * @param worlds     {@link World} instances to send.
+	 */
+	void queriesSend(HttpResponse response, ServerHttpConnection connection, List<World> worlds);
+
+	/**
+	 * Sends fortunes response.
+	 * 
+	 * @param response   {@link HttpResponse}.
+	 * @param connection {@link ServerHttpConnection}.
+	 * @param fortune    {@link Fortune} instances to send.
+	 */
+	void fortunesSend(HttpResponse response, ServerHttpConnection connection, List<Fortune> fortunes);
+
+	/**
+	 * Sends update response.
+	 * 
+	 * @param response   {@link HttpResponse}.
+	 * @param connection {@link ServerHttpConnection}.
+	 * @param worlds     {@link World} instances to send.
+	 */
+	void updateSend(HttpResponse response, ServerHttpConnection connection, List<World> worlds);
+
+	/**
+	 * Sends error.
+	 * 
+	 * @param connection {@link ServerHttpConnection}.
+	 * @param failure    Cause..
+	 */
+	void sendError(ServerHttpConnection connection, Throwable failure);
+
+}
---
+++ frameworks/Java/officefloor/src/woof_benchmark_woof/src/main/java/net/officefloor/benchmark/RawWoofThreadAffinity.java
@@ -0,0 +1,54 @@
+package net.officefloor.benchmark;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.ThreadFactory;
+import java.util.function.Supplier;
+
+import net.officefloor.server.stream.impl.ThreadLocalStreamBufferPool;
+import net.officefloor.web.executive.CpuCore;
+import net.openhft.affinity.Affinity;
+
+/**
+ * Encapsulating the {@link Thread} affinity.
+ * 
+ * @author Daniel Sagenschneider
+ */
+public class RawWoofThreadAffinity {
+
+	/**
+	 * Obtains the {@link ThreadFactory} with {@link Thread} affinity.
+	 * 
+	 * @param bufferPoolSupplier Obtains the {@link ThreadLocalStreamBufferPool}.
+	 * @return {@link ThreadFactory} with {@link Thread} affinity.
+	 */
+	public static ThreadFactory[] createThreadFactories(Supplier<ThreadLocalStreamBufferPool> bufferPoolSupplier) {
+		List<ThreadFactory> threadFactories = new LinkedList<>();
+		for (CpuCore cpuCore : CpuCore.getCores()) {
+			for (CpuCore.LogicalCpu logicalCpu : cpuCore.getCpus()) {
+
+				// Create thread factory for logical CPU
+				ThreadFactory boundThreadFactory = (runnable) -> new Thread(() -> {
+					ThreadLocalStreamBufferPool bufferPool = bufferPoolSupplier.get();
+					try {
+						// Bind thread to logical CPU
+						Affinity.setAffinity(logicalCpu.getCpuAffinity());
+
+						// Set up for thread local buffer pooling
+						bufferPool.activeThreadLocalPooling();
+
+						// Run logic for thread
+						runnable.run();
+					} finally {
+						bufferPool.threadComplete();
+					}
+				});
+
+				// Add the thread factory
+				threadFactories.add(boundThreadFactory);
+			}
+		}
+		return threadFactories.toArray(new ThreadFactory[0]);
+	}
+
+}
