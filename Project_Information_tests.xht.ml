9e38e32530cda01a32a4f59856d592767e000431
MoneyMan573 <pimpneasily@yahoo.com>
2021-06-12 11:03:56 -0500

    Update Project_Information_tests.xht.ml

--- Project_Information_tests.xht.ml
+++ Project_Information_tests.xht.ml
@@ -4,4 +4,4 @@
  Cookie: uid=12345678901234567890; __utma=1.1234567890.1234567890.1234567890.1234567890.12; wd=2560x1600
  Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
  Accept-Language: en-US,en;q=0.5
- Connection: keep-alive
+ Connection: keep-alive/ GET results.json
