7f4eccf7325564f1944ccb5eb679f6fd0dd02ad1
Sam Bull <aa6bs0@sambull.org>
2021-06-18 21:54:55 +0100

    Update views.py

--- frameworks/Python/aiohttp/app/views.py
+++ frameworks/Python/aiohttp/app/views.py
@@ -128,15 +128,14 @@ async def updates(request):
     Test 5 ORM
     """
     num_queries = get_num_queries(request)
-    ids = [randint(1, 10000) for _ in range(num_queries)]
     updates = [(randint(1, 10000), randint(1, 10000)) for _ in range(num_queries)]
+    updates.sort()
     worlds = [{'id': row_id, 'randomNumber': number} for row_id, number in updates]
 
     async with request.app['db_session'].begin() as sess:
-        for id_ in ids:
-            rand_new = randint(1, 10000)
+        for id_, number in updates:
             world = await sess.get(World, id_, populate_existing=True)
-            world.randomnumber = rand_new
+            world.randomnumber = number
     return json_response(worlds)
 
 async def updates_raw(request):
