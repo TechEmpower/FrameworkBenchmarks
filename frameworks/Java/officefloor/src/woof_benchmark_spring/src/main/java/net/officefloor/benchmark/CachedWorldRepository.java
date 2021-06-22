7c29fc6282bde80ccc2eacbc7f06d507ecd9c4cb
Daniel Sagenschneider <daniel@officefloor.net>
2021-06-22 16:44:35 +0800

    Only require World able

--- frameworks/Java/officefloor/src/woof_benchmark_spring/src/main/java/net/officefloor/benchmark/CachedWorldRepository.java
+++
@@ -1,8 +0,0 @@
-package net.officefloor.benchmark;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CachedWorldRepository extends CrudRepository<CachedWorld, Integer> {
-}
