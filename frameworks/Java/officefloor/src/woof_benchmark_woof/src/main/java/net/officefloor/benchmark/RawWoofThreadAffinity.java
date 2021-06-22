06b59cee0474f236f27cf61ff47ee5c8b61bffc9
Daniel Sagenschneider <daniel@officefloor.net>
2021-06-22 09:34:34 +0800

    Re-enabling thread affinity

--- frameworks/Java/officefloor/src/woof_benchmark_woof/src/main/java/net/officefloor/benchmark/RawWoofThreadAffinity.java
+++ frameworks/Java/officefloor/src/woof_benchmark_woof/src/main/java/net/officefloor/benchmark/RawWoofThreadAffinity.java
@@ -6,6 +6,8 @@ import java.util.concurrent.ThreadFactory;
 import java.util.function.Supplier;
 
 import net.officefloor.server.stream.impl.ThreadLocalStreamBufferPool;
+import net.officefloor.web.executive.CpuCore;
+import net.openhft.affinity.Affinity;
 
 /**
  * Encapsulating the {@link Thread} affinity.
@@ -41,30 +43,30 @@ public class RawWoofThreadAffinity {
 
 		} else {
 			// Provide socket per thread with thread affinity
-//			for (CpuCore cpuCore : CpuCore.getCores()) {
-//				for (CpuCore.LogicalCpu logicalCpu : cpuCore.getCpus()) {
-//
-//					// Create thread factory for logical CPU
-//					ThreadFactory boundThreadFactory = (runnable) -> new Thread(() -> {
-//						ThreadLocalStreamBufferPool bufferPool = bufferPoolSupplier.get();
-//						try {
-//							// Bind thread to logical CPU
-//							Affinity.setAffinity(logicalCpu.getCpuAffinity());
-//
-//							// Set up for thread local buffer pooling
-//							bufferPool.activeThreadLocalPooling();
-//
-//							// Run logic for thread
-//							runnable.run();
-//						} finally {
-//							bufferPool.threadComplete();
-//						}
-//					});
-//
-//					// Add the thread factory
-//					threadFactories.add(boundThreadFactory);
-//				}
-//			}
+			for (CpuCore cpuCore : CpuCore.getCores()) {
+				for (CpuCore.LogicalCpu logicalCpu : cpuCore.getCpus()) {
+
+					// Create thread factory for logical CPU
+					ThreadFactory boundThreadFactory = (runnable) -> new Thread(() -> {
+						ThreadLocalStreamBufferPool bufferPool = bufferPoolSupplier.get();
+						try {
+							// Bind thread to logical CPU
+							Affinity.setAffinity(logicalCpu.getCpuAffinity());
+
+							// Set up for thread local buffer pooling
+							bufferPool.activeThreadLocalPooling();
+
+							// Run logic for thread
+							runnable.run();
+						} finally {
+							bufferPool.threadComplete();
+						}
+					});
+
+					// Add the thread factory
+					threadFactories.add(boundThreadFactory);
+				}
+			}
 		}
 
 		return threadFactories.toArray(new ThreadFactory[0]);
