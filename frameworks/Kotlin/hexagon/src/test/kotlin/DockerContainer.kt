cdbf826150a41d287f6f2b1f99de74fc37dfed5c
jaguililla <juanjoaguililla@gmail.com>
2021-06-28 08:17:04 +0200

    Delete tests

--- frameworks/Kotlin/hexagon/src/test/kotlin/DockerContainer.kt
+++
@@ -1,6 +0,0 @@
-package com.hexagonkt
-
-import org.testcontainers.containers.GenericContainer
-import org.testcontainers.utility.DockerImageName
-
-class DockerContainer(imageName: String) : GenericContainer<DockerContainer>(DockerImageName.parse(imageName))
