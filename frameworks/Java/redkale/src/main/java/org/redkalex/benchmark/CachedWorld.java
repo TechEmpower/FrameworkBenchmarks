be0bec23db3492f3b94cad18c0f3db0d7b0323a6
Redkale <redkale@qq.com>
2021-07-13 13:09:13 +0800

    add graalvm (#6695)
    * Update redkale-graalvm.dockerfile
    
    * Update pom.xml
    
    * Update redkale-graalvm.dockerfile
    
    * Update redkale-graalvm.dockerfile
    
    * Update redkale-graalvm.dockerfile
    
    * Create redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update config.toml
    
    * Update benchmark_config.json
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update config.toml
    
    * Update benchmark_config.json
    
    * Update redkale-native.dockerfile
    
    * Update config.toml
    
    * Update redkale-native.dockerfile
    
    * Update benchmark_config.json
    
    * Update config.toml
    
    * Update redkale-native.dockerfile
    
    * Update config.toml
    
    * Update benchmark_config.json
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update redkale-native.dockerfile
    
    * Update CachedWorld.java
    
    * Update Fortune.java
    
    * Update World.java
    
    * Update Message.java
    
    * update graalvm version

--- frameworks/Java/redkale/src/main/java/org/redkalex/benchmark/CachedWorld.java
+++ frameworks/Java/redkale/src/main/java/org/redkalex/benchmark/CachedWorld.java
@@ -14,6 +14,7 @@ import org.redkale.source.*;
  * @author zhangjx
  */
 //@Cacheable(direct = true)
+@Entity
 @Table(name = "World")
 public final class CachedWorld implements Comparable<CachedWorld> {
 
