34abb9c768337c04d60391040b27b6d1d529f849
黑小马 <heixiaoma@gmail.com>
2021-07-13 20:25:12 +0800

    Required response header missing: Date fix

--- frameworks/Java/hserver/src/main/java/com/test/hserver/controller/TestController.java
+++ frameworks/Java/hserver/src/main/java/com/test/hserver/controller/TestController.java
@@ -1,8 +1,11 @@
 package com.test.hserver.controller;
 
 import com.test.hserver.bean.Message;
+import top.hserver.core.interfaces.HttpResponse;
 import top.hserver.core.ioc.annotation.Controller;
 import top.hserver.core.ioc.annotation.GET;
+
+import java.util.Date;
 
 /**
  * @author hxm
@@ -11,12 +14,14 @@ import top.hserver.core.ioc.annotation.GET;
 public class TestController {
 
     @GET("/json")
-    public Message json() {
+    public Message json(HttpResponse response) {
+        response.setHeader("Date", new Date().toString());
         return new Message();
     }
 
     @GET("/plaintext")
-    public String plaintext() {
+    public String plaintext(HttpResponse response) {
+        response.setHeader("Date", new Date().toString());
         return "Hello, World!";
     }
 }
