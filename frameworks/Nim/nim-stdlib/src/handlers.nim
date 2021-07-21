9b4c1df637980c27073a16e153bbb18e1db1350b
Rishav Sharan <rishav.sharan@live.com>
2021-07-22 01:15:08 +0530

    Create handlers.nim

---
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -0,0 +1,17 @@
+import asynchttpserver, asyncdispatch, times
+
+proc handle404Error*(req: Request) {.async.} =
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
+    await req.respond(Http404, "URL doesn't exists", headers.newHttpHeaders())
+
+proc handle405Error*(req: Request) {.async.} =
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
+    await req.respond(Http405, "Method not allowed", headers.newHttpHeaders())
+
+proc handlePlaintext*(req: Request) {.async.} =
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
+    await req.respond(Http200, "Hello World", headers.newHttpHeaders())
+
+proc handleJson*(req: Request) {.async.} =
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
+    await req.respond(Http200, """{"message":"Hello, World!"}""", headers.newHttpHeaders())
