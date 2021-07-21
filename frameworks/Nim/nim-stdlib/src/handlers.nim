17726724d8f930abb9579799d221549aa9fdc16e
Rishav Sharan <rishav.sharan@live.com>
2021-07-22 02:19:07 +0530

    Update handlers.nim

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -9,7 +9,7 @@ proc handle405Error*(req: Request) {.async.} =
     await req.respond(Http405, "Method not allowed", headers.newHttpHeaders())
 
 proc handlePlaintext*(req: Request) {.async.} =
-    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8", "Server": "Example"}
     await req.respond(Http200, "Hello, World!", headers.newHttpHeaders())
 
 proc handleJson*(req: Request) {.async.} =
