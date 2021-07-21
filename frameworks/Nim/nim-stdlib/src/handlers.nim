2a644aa907bf0857615addfb62fbb2d9c5df4310
Rishav Sharan <rishav.sharan@live.com>
2021-07-22 02:10:15 +0530

    i might as well commit sudoku now. yet another copy paste error.

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -10,7 +10,7 @@ proc handle405Error*(req: Request) {.async.} =
 
 proc handlePlaintext*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
-    await req.respond(Http200, 'Hello, World!', headers.newHttpHeaders())
+    await req.respond(Http200, "Hello, World!", headers.newHttpHeaders())
 
 proc handleJson*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
