ff6df3ad07786c310d003227f74b930c417a4b2e
Rishav Sharan <rishav.sharan@live.com>
2021-07-22 01:15:28 +0530

    Update nim-stdlib.nim

--- frameworks/Nim/nim-stdlib/src/nim-stdlib.nim
+++ frameworks/Nim/nim-stdlib/src/nim-stdlib.nim
@@ -1,35 +1,24 @@
-import asynchttpserver, asyncdispatch, times
+import asynchttpserver, asyncdispatch
 
-import src.handlers
+import handlers
+
+const port = 8080
 
 proc main {.async.} =
     var server = newAsyncHttpServer()
-    
 
     proc cb(req: Request) {.async.} =
-        let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}            
         case req.reqMethod
         of HttpGet:
             case req.url.path
             of "/plaintext":    await handlePlaintext(req)
             of "/json":         await handleJson(req)
-            else:
-                await req.respond(Http404, "404 Error", headers.newHttpHeaders())
-
-        of HttpPost:
-            case req.url.path
-            of "/posty":
-                await req.respond(Http200, "POsted something", headers.newHttpHeaders())
-            else:
-                await req.respond(Http404, "404 Error", headers.newHttpHeaders())
-
-        else:
-            await req.respond(Http405, "Method not allowed", headers.newHttpHeaders())
+            else:               await handle404Error(req)
+        else:                   await handle405Error(req)
 
 
-
-    
-    server.listen Port(8080)
+    server.listen Port(port)
+    echo "Starting server on port: " & $port
     while true:
         if server.shouldAcceptRequest():
             await server.acceptRequest(cb)
