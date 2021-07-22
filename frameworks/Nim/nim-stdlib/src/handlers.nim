fb95658917e8c9feeddae596b8f80cf3ef8989fb
Rishav Sharan <rishav.sharan@live.com>
2021-07-22 12:20:11 +0530

    Adding db test

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -1,4 +1,6 @@
-import asynchttpserver, asyncdispatch, times
+import asynchttpserver, asyncdispatch, times, random, db_postgres
+
+import defs
 
 proc handle404Error*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8"}
@@ -9,9 +11,14 @@ proc handle405Error*(req: Request) {.async.} =
     await req.respond(Http405, "Method not allowed", headers.newHttpHeaders())
 
 proc handlePlaintext*(req: Request) {.async.} =
-    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8", "Server": "Example"}
-    await req.respond(Http200, "Hello, World!", headers.newHttpHeaders())
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/plain; charset=utf-8",  "Server": "Example"}
+    await req.respond(Http200, "Hello World", headers.newHttpHeaders())
 
 proc handleJson*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
     await req.respond(Http200, """{"message":"Hello, World!"}""", headers.newHttpHeaders())
+
+proc handleDB*(req: Request) {.async.} =
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
+    let queryResult = db.getRow(sql"""select * from "public"."World" where id = ?""", rand(1..10000))
+    await req.respond(Http200, """ {"id":""" & queryResult[0] & ""","randomNumber":""" & queryResult[1] & """}""", headers.newHttpHeaders())
