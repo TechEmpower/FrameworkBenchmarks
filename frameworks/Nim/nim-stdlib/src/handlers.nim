d86dd1d37047dd27a492686da24ecc60f33ef095
Rishav Sharan <rishav.sharan@live.com>
2021-07-24 15:45:04 +0530

    Adding updates test

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -64,3 +64,43 @@ proc handleFortunes*(req: Request) {.async.} =
 
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "text/html; charset=UTF-8", "Server": "Example"}
     await req.respond(Http200, fortunesView, headers.newHttpHeaders())
+    
+    
+proc handleUpdates*(req: Request) {.async.} =
+    let queryObj = readData(req.url.query)
+    var count = 1
+    try:
+        count = clamp(parseInt(queryObj["queries"]), 1, 500)
+    except KeyError, ValueError:
+        count = 1
+    except:
+        await handleHTTPErrors(req, Http502, "Something is wrong")
+
+    var insertStatement = """
+update "public"."World" as w set
+    randomNumber = tmp.randomNumber
+from (values """
+
+    var jsonList: seq[DBQueryObj]
+    for i in 1..count:
+        let randId = rand(1..10000)
+        let randVal = rand(1..10000)
+        let queryResult = db.getRow(sql"""select * from "public"."World" where id = ?""", randId)
+
+        insertStatement.add("(" & $randId & "," & $randVal & ")")
+
+        if i < count :
+            insertStatement.add(",")
+
+        let jsonNode = DBQueryObj(id: queryResult[0], randomNumber: $randVal)
+        jsonList.add(jsonNode)
+
+    insertStatement.add("""
+) as tmp(id, randomNumber)
+where tmp.id = w.id;
+""")
+
+    db.exec(sql insertStatement)
+
+    let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
+    await req.respond(Http200, $(%*jsonList), headers.newHttpHeaders())
