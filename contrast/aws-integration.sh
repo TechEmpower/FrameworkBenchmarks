e1df1bae6afaebb1487cef8c5e45eb131bcae7d9
John Towler <john.towler@contrastsecurity.com>
2021-07-26 16:01:03 -0700

    Update aws integration script to grab Teamserver auth info from AWS Secrets Manager

--- contrast/aws-integration.sh
+++ contrast/aws-integration.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
-# aws.sh <bucket-name> <language> <hash> <agent filename>
+# aws-integration.sh <bucket-name> <language> <hash> <agent filename>
+
+CURRENT_REGION=`curl -s http://169.254.169.254/latest/dynamic/instance-identity/document | jq .region -r`
+
+CONTRAST_API_SECRETS=`aws secretsmanager get-secret-value --secret-id tech-empower-teamserver-api --region $CURRENT_REGION --query SecretString --output text`
+
+export CONTRAST__API__URL=`echo $CONTRAST_API_SECRETS | jq -r .CONTRAST__API__URL`
+export CONTRAST__API__API_KEY=`echo $CONTRAST_API_SECRETS | jq -r .CONTRAST__API__API_KEY`
+export CONTRAST__API__SERVICE_KEY=`echo $CONTRAST_API_SECRETS | jq -r .CONTRAST__API__SERVICE_KEY`
+export CONTRAST__API__USER_NAME=`echo $CONTRAST_API_SECRETS | jq -r .CONTRAST__API__USER_NAME`
 
 # Get the agent
 aws s3 cp s3://$1/$2/$3/$4 ./
