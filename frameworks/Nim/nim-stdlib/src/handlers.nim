e9a90807a1a66af0f802e0b60fc5978d8bfb3698
Rishav Sharan <rishav.sharan@live.com>
2021-07-28 01:59:01 +0530

    updated /db handler to use json serializer as well
    also fixed a missing character

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -12,12 +12,12 @@ proc handlePlaintext*(req: Request) {.async.} =
 
 proc handleJson*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
-    await req.respond(Http200, %*{"message":"Hello, World!"}, headers.newHttpHeaders())
+    await req.respond(Http200, $(%*{"message":"Hello, World!"}), headers.newHttpHeaders())
 
 proc handleDB*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
     let queryResult = db.getRow(sql"""select * from "public"."World" where id = ?""", rand(1..10000))
-    await req.respond(Http200, """ {"id":""" & queryResult[0] & ""","randomNumber":""" & queryResult[1] & """}""", headers.newHttpHeaders())
+    await req.respond(Http200, $(%*{"id": queryResult[0] , "randomNumber": queryResult[1] }), headers.newHttpHeaders())
 
 proc handleQueries*(req: Request) {.async.} =
     let queryObj = readData(req.url.query)
