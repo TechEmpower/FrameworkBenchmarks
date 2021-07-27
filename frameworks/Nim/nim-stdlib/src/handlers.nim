3feec025874d63ebfa17750dfcfd94bde2a881ad
Rishav Sharan <rishav.sharan@live.com>
2021-07-28 01:31:13 +0530

    changing json handler to actually use the json serializer
    %*{"message":"Hello, World!"}

--- frameworks/Nim/nim-stdlib/src/handlers.nim
+++ frameworks/Nim/nim-stdlib/src/handlers.nim
@@ -12,7 +12,7 @@ proc handlePlaintext*(req: Request) {.async.} =
 
 proc handleJson*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
-    await req.respond(Http200, """{"message":"Hello, World!"}""", headers.newHttpHeaders())
+    await req.respond(Http200, %*{"message":"Hello, World!"}, headers.newHttpHeaders())
 
 proc handleDB*(req: Request) {.async.} =
     let headers = {"Date": now().utc.format("ddd, dd MMM yyyy HH:mm:ss") & " GMT", "Content-type": "application/json; charset=utf-8", "Server": "Example"}
