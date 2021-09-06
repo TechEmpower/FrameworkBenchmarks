690486af8d8f59d462f29e480aa3b51caecccffa
LIU JIAN <onanying@126.com>
2021-09-07 00:15:36 +0800

    feat: add mixphp (#6724)
    * feat: add mixphp
    
    * update /fortunes views and mixphp-workerman.dockerfile
    
    * Add bin/*.php
    
    * update dockerfile
    
    * update mixphp-workerman.dockerfile
    
    * Add mixphp-workerman-pgsql
    
    * update views/fortunes.php
    
    * update fortunes()
    
    * update conn pool $maxOpen
    
    * orm switch to object
    
    * update orm switch to object
    
    * Remove useless code
    
    * debug build docker
    
    * build dockerfile remove > /dev/null
    
    * handle false & update routes
    
    * Add 500 handle, close swoole coroutine
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update dockerfile
    
    * Update readme
    
    * Enable swoole coroutine
    
    * disable swoole coroutine
    
    * enable swoole coroutine
    
    * Update pool
    
    * disable swoole coroutine
    
    * Update ubuntu version
    
    * Update composer install
    
    * Remove workerman-mysql json_url plaintext_url
    
    * Update start params
    
    * Add phpfpm
    
    * Add phpfpm
    
    * Add phpfpm
    
    * Update mixphp.dockerfile
    
    * Update mixphp.dockerfile
    
    * Update mixphp.dockerfile
    
    * Update dockerfile
    
    * Update swoole.php

---
+++ frameworks/PHP/mixphp/src/Controller/Benchmark.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controller;
+
+use App\Container\DB;
+use Mix\Vega\Context;
+
+class Benchmark
+{
+
+    /**
+     * @param Context $ctx
+     */
+    public function json(Context $ctx)
+    {
+        $ctx->setHeader('Content-Type', 'application/json');
+        $ctx->string(200, json_encode(['message' => 'Hello, World!']));
+    }
+
+    /**
+     * @param Context $ctx
+     */
+    public function plaintext(Context $ctx)
+    {
+        $ctx->setHeader('Content-Type', 'text/plain; charset=utf-8');
+        $ctx->string(200, 'Hello, World!');
+    }
+
+    /**
+     * @param Context $ctx
+     */
+    public function db(Context $ctx)
+    {
+        $ctx->setHeader('Content-Type', 'application/json');
+        $ret = DB::instance()->raw('SELECT id,randomNumber FROM World WHERE id=?', mt_rand(1, 10000))->first();
+        $ctx->string(200, json_encode($ret));
+    }
+
+    /**
+     * @param Context $ctx
+     */
+    public function query(Context $ctx)
+    {
+        $queryCount = 1;
+        $q = (int)$ctx->query('q');
+        if ($q > 1) {
+            $queryCount = min($q, 500);
+        }
+
+        $arr = [];
+        while ($queryCount--) {
+            $id = mt_rand(1, 10000);
+            $ret = DB::instance()->raw('SELECT id,randomNumber FROM World WHERE id=?', $id)->first();
+            if (!$ret) {
+                continue;
+            }
+            $arr[] = $ret;
+        }
+
+        $ctx->setHeader('Content-Type', 'application/json');
+        $ctx->string(200, json_encode($arr));
+    }
+
+    /**
+     * @param Context $ctx
+     */
+    public function fortunes(Context $ctx)
+    {
+        $rows = DB::instance()->raw('SELECT id,message FROM Fortune')->get();
+        $rows[] = (object)['id' => 0, 'message' => 'Additional fortune added at request time.'];
+        usort($rows, function ($left, $right) {
+            return $left->message <=> $right->message;
+        });
+        $ctx->HTML(200, 'fortunes', ['rows' => $rows]);
+    }
+
+    /**
+     * @param Context $ctx
+     */
+    public function update(Context $ctx)
+    {
+        $queryCount = 1;
+        $q = (int)$ctx->query('q');
+        if ($q > 1) {
+            $queryCount = min($q, 500);
+        }
+
+        $arr = [];
+        while ($queryCount--) {
+            $id = mt_rand(1, 10000);
+            $ret = DB::instance()->raw('SELECT id,randomNumber FROM World WHERE id=?', $id)->first();
+            if (!$ret) {
+                continue;
+            }
+            DB::instance()->exec('UPDATE World SET randomNumber=? WHERE id=?', $ret->randomNumber = mt_rand(1, 10000), $id);
+            $arr[] = $ret;
+        }
+
+        $ctx->setHeader('Content-Type', 'application/json');
+        $ctx->string(200, json_encode($arr));
+    }
+
+}
---
+++ frameworks/PHP/mixphp/src/Vega.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App;
+
+use App\Container\Logger;
+use Mix\Vega\Abort;
+use Mix\Vega\Context;
+use Mix\Vega\Engine;
+use Mix\Vega\Exception\NotFoundException;
+
+class Vega
+{
+
+    /**
+     * @return Engine
+     */
+    public static function new(): Engine
+    {
+        $vega = new Engine();
+
+        // 500
+        $vega->use(function (Context $ctx) {
+            try {
+                $ctx->next();
+            } catch (\Throwable $ex) {
+                if ($ex instanceof Abort || $ex instanceof NotFoundException) {
+                    throw $ex;
+                }
+                Logger::instance()->error(sprintf('%s in %s on line %d', $ex->getMessage(), $ex->getFile(), $ex->getLine()));
+                $ctx->string(500, 'Internal Server Error');
+                $ctx->abort();
+            }
+        });
+
+        // views
+        $vega->withHTMLRoot(__DIR__ . '/../views');
+
+        // routes
+        $routes = require __DIR__ . '/../routes/index.php';
+        $routes($vega);
+
+        return $vega;
+    }
+
+}
