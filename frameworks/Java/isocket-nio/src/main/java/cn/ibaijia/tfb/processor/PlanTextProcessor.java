d0c79e804f93269c4c0a9a09793c7967be8949cb
longzl <longzhili2005@126.com>
2021-09-07 00:19:13 +0800

    optimiz protocol and mem pool (#6792)

--- frameworks/Java/isocket-nio/src/main/java/cn/ibaijia/tfb/processor/PlanTextProcessor.java
+++ frameworks/Java/isocket-nio/src/main/java/cn/ibaijia/tfb/processor/PlanTextProcessor.java
@@ -2,9 +2,7 @@ package cn.ibaijia.tfb.processor;
 
 import cn.ibaijia.isocket.processor.Processor;
 import cn.ibaijia.isocket.session.Session;
-import cn.ibaijia.isocket.session.SessionManager;
-import cn.ibaijia.isocket.util.BufferPool;
-import cn.ibaijia.isocket.util.BufferState;
+import cn.ibaijia.tfb.Consts;
 import cn.ibaijia.tfb.http.HttpEntity;
 import cn.ibaijia.tfb.http.HttpRequestEntity;
 import cn.ibaijia.tfb.http.HttpResponseEntity;
@@ -12,62 +10,33 @@ import com.alibaba.fastjson.JSON;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.concurrent.locks.ReentrantLock;
-
 public class PlanTextProcessor implements Processor<HttpEntity> {
     private static final Logger logger = LoggerFactory.getLogger(PlanTextProcessor.class);
-    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z", Locale.ENGLISH);//Fri, 09 Jul 2021 09:10:42 UTC
-    private static final String SERVER_NAME = "isocket-nio-tfb";
-    private static final String TEXT_TYPE = "text/plain; charset=UTF-8";
-    private static final String JSON_TYPE = "application/json; charset=UTF-8";
-    private static ReentrantLock lock = new ReentrantLock();
-    private static String dateHeader = dateFormat.format(new Date());
-
-    private String getDateHeader() {
-        //TODO
-        if (lock.tryLock()) {
-            dateHeader = dateFormat.format(new Date());
-        }
-        return dateHeader;
-    }
 
     @Override
     public boolean process(final Session session, final HttpEntity httpEntity) {
         HttpRequestEntity httpRequestEntity = (HttpRequestEntity) httpEntity;
         String url = httpRequestEntity.url;
         logger.trace("url:{}", url);
-        if (url.equals("/plaintext")) {
+        if ("/plaintext".equals(url)) {
             HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-            httpResponseEntity.setHeader("Content-Type", TEXT_TYPE);
-            httpResponseEntity.setHeader("Server", SERVER_NAME);
-            httpResponseEntity.setHeader("Date", getDateHeader());
+            httpResponseEntity.setContentType(Consts.TEXT_TYPE);
             httpResponseEntity.body = "Hello, World!";
             session.write(httpResponseEntity);
-        } else if (url.equals("/json")) {
+        } else if ("/json".equals(url)) {
             HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-            httpResponseEntity.setHeader("Content-Type", JSON_TYPE);
-            httpResponseEntity.setHeader("Server", SERVER_NAME);
-            httpResponseEntity.setHeader("Date", getDateHeader());
+            httpResponseEntity.setContentType(Consts.JSON_TYPE);
             httpResponseEntity.body = JSON.toJSONString(new Message("Hello, World!"));
             session.write(httpResponseEntity);
-        } else if (url.equals("/state")) {
+        } else if ("/state".equals(url)) {
             HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-            httpResponseEntity.setHeader("Content-Type", JSON_TYPE);
-            httpResponseEntity.setHeader("Server", SERVER_NAME);
-            httpResponseEntity.setHeader("Date", getDateHeader());
+            httpResponseEntity.setContentType(Consts.JSON_TYPE);
             State state = new State();
-            state.sessionCount = SessionManager.getSessionCount();
-            state.bufferState = new BufferState();
             httpResponseEntity.body = JSON.toJSONString(state);
             session.write(httpResponseEntity);
         } else {
             HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-            httpResponseEntity.setHeader("Content-Type", TEXT_TYPE);
-            httpResponseEntity.setHeader("Server", SERVER_NAME);
-            httpResponseEntity.setHeader("Date", getDateHeader());
+            httpResponseEntity.setContentType(Consts.TEXT_TYPE);
             httpResponseEntity.body = "hi";
             session.write(httpResponseEntity);
         }
@@ -78,12 +47,8 @@ public class PlanTextProcessor implements Processor<HttpEntity> {
     public void processError(Session session, HttpEntity httpEntity, Throwable throwable) {
         logger.error("processError:", throwable);
         HttpResponseEntity httpResponseEntity = new HttpResponseEntity();
-        httpResponseEntity.setHeader("Content-Type", TEXT_TYPE);
-        httpResponseEntity.setHeader("Server", SERVER_NAME);
-        httpResponseEntity.setHeader("Date", getDateHeader());
+        httpResponseEntity.setContentType(Consts.TEXT_TYPE);
         httpResponseEntity.body = "hi";
-        httpResponseEntity.statusCode = 500;
-        httpResponseEntity.status = "Internal Server Error";
         session.write(httpResponseEntity);
     }
 }
