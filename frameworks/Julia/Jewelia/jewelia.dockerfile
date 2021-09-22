3b6c48ba33effd7b658e2abdcec2405badd399c8
donavindebartolo <donavin.debartolo@gmail.com>
2021-09-22 18:40:13 -0500

    Working Docker build and plaintext/json tests implemented and passing..

--- frameworks/Julia/Jewelia/jewelia.dockerfile
+++ frameworks/Julia/Jewelia/jewelia.dockerfile
@@ -1,21 +1,22 @@
 FROM ubuntu:latest
 
-ENV DEBIAN_FRONTEND interactive
+ENV DEBIAN_FRONTEND noninteractive
 
 RUN apt-get update -yqq && apt-get install -y
 RUN apt-get update -yqq && apt-get install -y wget
 
-RUN mkdir /usr/Downloads
-
-WORKDIR /usr/Downloads
-
 RUN wget https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.2-linux-x86_64.tar.gz
 RUN tar zxvf julia-1.6.2-linux-x86_64.tar.gz
+ENV PATH="$PATH:/julia-1.6.2/bin"
 
-ENV PATH="$PATH:/usr/Downloads/julia-1.6.2/bin"
+RUN rm -f julia-1.6.2-linux-x86_64.tar.gz
 
-RUN mkdir /jewelia
-ADD ./ /jewelia
+COPY ./ ./
 
 RUN julia -e 'import Pkg; Pkg.activate(@__DIR__); Pkg.instantiate()' && julia -e 'import Pkg; Pkg.activate(@__DIR__); Pkg.precompile()'
 
+RUN chmod +x run.sh
+
+EXPOSE 8080
+
+CMD ./run.sh
---
+++ frameworks/Julia/Jewelia/julia_server.jl
@@ -0,0 +1,31 @@
+Pkg.activate(@__DIR__)
+
+using Dates
+using HTTP
+
+import JSON3
+
+
+HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
+   if endswith(http.message.target, "/plaintext")
+        HTTP.setstatus(http, 200)
+        HTTP.setheader(http, "Content-Type" => "text/plain")
+        HTTP.setheader(http, "Server" => "Julia-HTTP")
+        HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
+        HTTP.startwrite(http)
+        write(http, "Hello, World!")
+
+   elseif endswith(http.message.target, "/json")
+        HTTP.setheader(http, "Content-Type" => "application/json")
+        HTTP.setheader(http, "Server" => "Julia-HTTP")
+        HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
+        HTTP.setstatus(http, 200)
+        startwrite(http)
+        JSON3.write(http, (;message = "Hello, World!"))
+
+   else
+        HTTP.setstatus(http, 404)
+           startwrite(http)
+           write(http, "Not Found")
+   end
+end
