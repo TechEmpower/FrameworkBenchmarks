e55c13b9a48fe319a0cfa8008270f93f47fcd7dd
donavindebartolo <donavin.debartolo@gmail.com>
2021-09-23 21:40:34 -0500

    Testing multiple db queries test

--- frameworks/Julia/Jewelia/julia_server.jl
+++ frameworks/Julia/Jewelia/julia_server.jl
@@ -7,7 +7,9 @@ using MySQL
 using JSON3
 
 HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
-    if endswith(http.message.target, "/plaintext")
+    target = http.message.target
+
+    if endswith(target, "/plaintext")
         HTTP.setstatus(http, 200)
         HTTP.setheader(http, "Content-Type" => "text/plain")
         HTTP.setheader(http, "Server" => "Julia-HTTP")
@@ -16,7 +18,7 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
         HTTP.startwrite(http)
         write(http, "Hello, World!")
 
-    elseif endswith(http.message.target, "/json")
+    elseif endswith(target, "/json")
         HTTP.setheader(http, "Content-Type" => "application/json")
         HTTP.setheader(http, "Server" => "Julia-HTTP")
         HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
@@ -25,7 +27,7 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
         startwrite(http)
         JSON3.write(http, (;message = "Hello, World!"))
 
-    elseif endswith(http.message.target, "/db")
+    elseif endswith(target, "/db")
         randNum = rand(1:10000)
 
         HTTP.setheader(http, "Content-Type" => "application/json")
@@ -42,6 +44,28 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
 
         startwrite(http)
         JSON3.write(http, (JSON3.read(jsonString)))
+
+    elseif occursin("/queries", target)
+        HTTP.setheader(http, "Content-Type" => "application/json")
+        HTTP.setheader(http, "Server" => "Julia-HTTP")
+        HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
+        HTTP.setstatus(http, 200)
+
+        numQueries = parse(Int64, (split(target, "="))[2])
+        randNumList = rand(Int64, 1:10000, numQueries)
+        conn = DBInterface.connect(MySQL.Connection, "tfb-database", "benchmarkdbuser", "benchmarkdbpass", db="hello_world")
+
+        responseArray = Array{String, numQueries}
+        for i in 1:numQueries
+            sqlQuery = "SELECT * FROM World WHERE id = $randNum"
+            results = DBInterface.execute(conn, sqlQuery)
+            row = first(results)
+            dbNumber = row[2]
+            responseArray[i] = "{\"id\":$randNum,\"randomNumber\":$dbNumber}"
+        end
+
+        startwrite(http)
+        JSON3.write(http, (JSON3.read(responseArray)))
 
     else
         HTTP.setstatus(http, 404)
