e1199af5e8e119fe409d8537f1a880859a026050
donavindebartolo <donavin.debartolo@gmail.com>
2021-09-24 01:08:29 -0500

    Fixed response headers

--- frameworks/Julia/Jewelia/julia_server.jl
+++ frameworks/Julia/Jewelia/julia_server.jl
@@ -7,7 +7,6 @@ using MySQL
 using JSON3
 using StructTypes
 
-
 struct jsonObj
     id::Int
     randomNumber::Int
@@ -19,19 +18,21 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
     target = http.message.target
 
     HTTP.setstatus(http, 200)
-    HTTP.setheader(http, "Content-Type" => "text/plain")
     HTTP.setheader(http, "Server" => "Julia-HTTP")
     HTTP.setheader(http, "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT")
 
     if endswith(target, "/plaintext")
+        HTTP.setheader(http, "Content-Type" => "text/plain")
         HTTP.startwrite(http)
         write(http, "Hello, World!")
 
     elseif endswith(target, "/json")
+        HTTP.setheader(http, "Content-Type" => "application/json")
         startwrite(http)
         JSON3.write(http, (;message = "Hello, World!"))
 
     elseif endswith(target, "/db")
+        HTTP.setheader(http, "Content-Type" => "application/json")
         randNum = rand(1:10000)
 
         conn = DBInterface.connect(MySQL.Connection, "tfb-database", "benchmarkdbuser", "benchmarkdbpass", db="hello_world")
@@ -45,6 +46,7 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
         JSON3.write(http, (JSON3.read(jsonString)))
 
     elseif occursin("/queries", target)
+        HTTP.setheader(http, "Content-Type" => "application/json")
         numQueries = -1
 
         try
@@ -79,6 +81,7 @@ HTTP.listen("0.0.0.0" , 8080, reuseaddr = true) do http
 
         startwrite(http)
         JSON3.write(http, responseArray)
+        # JSON3.write(http, (JSON3.read(responseArray, JSON3.Array)))
 
     else
         HTTP.setstatus(http, 404)
