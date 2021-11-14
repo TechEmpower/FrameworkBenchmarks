33c780fb8b3cd884903c245fd21e184feec51c64
amitksingh1490 <amitksingh1490@gmail.com>
2021-11-14 18:41:45 +0530

    fix(main): servertime and memoize

--- frameworks/Scala/zio-http/build.sbt
+++ frameworks/Scala/zio-http/build.sbt
@@ -3,7 +3,7 @@ name := "zio-http"
 version := "1.0.0"
 scalaVersion := "2.13.6"
 
-lazy val zhttp = ProjectRef(uri(s"https://github.com/tusharmath/zio-http.git#b13f7b9c8827f7ff8e795fe9965c5d6435893279"), "zhttp")
+lazy val zhttp = ProjectRef(uri(s"https://github.com/dream11/zio-http.git#7bd1f2903129c3bd23806663a9b0200341a06a71"), "zhttp")
 lazy val root  = (project in file("."))
   .settings(
     name := "helloExample",
--- frameworks/Scala/zio-http/src/main/scala/Main.scala
+++ frameworks/Scala/zio-http/src/main/scala/Main.scala
@@ -6,35 +6,22 @@ import com.github.plokhotnyuk.jsoniter_scala.core._
 import zhttp.http.Response
 import zhttp.service.server.ServerChannelFactory
 
-import java.time.format.DateTimeFormatter
-import java.time.{Instant, ZoneOffset}
-
 case class Message(message: String)
 
 object Main extends App {
   val message: String                         = "Hello, World!"
   val messageLength: Long = message.size
   implicit val codec: JsonValueCodec[Message] = JsonCodecMaker.make
-  val plaintextResp = Response(data= HttpData.fromText(message), headers = Header.contentTypeTextPlain :: headers())
-  val jsonResp = Response(data = HttpData.fromText(writeToString(Message(message))), headers = Header.contentTypeJson :: headers())
+  val plaintextResp = Response.text(message).addHeader("server", "zio-http")
+  val jsonResp = Response.jsonString(writeToString(Message(message))).addHeader("server", "zio-http")
 
   val app = HttpApp.collect{
     case Method.GET -> !! / "plaintext" => plaintextResp
     case Method.GET -> !! / "json"      => jsonResp
 
   }
-  val server = Server.app(app.silent) ++ Server.port(8080) ++ Server.keepAlive ++ Server.disableLeakDetection
+  val server = Server.app(app.silent) ++ Server.port(8080) ++ Server.keepAlive ++ Server.disableLeakDetection ++ Server.memoize ++ Server.serverTime
 
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = server.make.useForever.provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto()).exitCode
 
-  val formatter: DateTimeFormatter                = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneOffset.UTC)
-  val constantHeaders: List[Header]               = Header("server", "zio-http") :: Nil
-  var lastHeaders: (Long, List[Header]) = (0, Nil)
-
-  def headers(): List[Header] = {
-    val t = System.currentTimeMillis()
-    if (t - lastHeaders._1 >= 1000)
-      lastHeaders = (t, Header("date", formatter.format(Instant.ofEpochMilli(t))) :: constantHeaders)
-    lastHeaders._2
-  }
 }
