15870a2e9972c76cf86bf80527d4259672daeb5b
Austin Brown <80549596+abrowao@users.noreply.github.com>
2021-11-15 20:00:35 -0600

    Update julia_server.jl

--- frameworks/Julia/Jewelia/julia_server.jl
+++ frameworks/Julia/Jewelia/julia_server.jl
@@ -27,9 +27,8 @@ StructTypes.StructType(::Type{jsonObj}) = StructTypes.Struct()
                     "Server" => "Julia-HTTP",
                     "Date" => Dates.format(Dates.now(), Dates.RFC1123Format) * " GMT" ]
     
-        jsonString = """{"Message": "Hello, World!"}"""
-        hello_world = JSON3.read(jsonString)
-        body = JSON3.write(hello_world)
+        jsonString = {"Message": "Hello, World!"}
+        body = JSON3.write(jsonString)
  
         return HTTP.Response(200, headers, body = body)
     end
