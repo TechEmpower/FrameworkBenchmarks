ea6f5581148a1454a4d50688c64ec7b6d576a495
Vojtech Rylko <vojta.rylko@gmail.com>
2021-11-22 20:02:43 +0100

    [vapor-postgres] Add database updates test

--- frameworks/Swift/vapor/vapor-postgres/Sources/main.swift
+++ frameworks/Swift/vapor/vapor-postgres/Sources/main.swift
@@ -70,6 +70,47 @@ app.get("queries") { req async throws -> [World] in
     return worlds
 }
 
+// Database Updates test
+//
+app.get("updates") { req async throws -> [World] in
+    let queries = (req.query["queries"] ?? 1).bounded(to: 1...500)
+
+    var worlds: [World] = []
+
+    let db = req.db(pools)
+
+    for _ in queries {
+        // Get
+        //
+        let rows = try await db.query(
+            "SELECT id, randomnumber FROM World WHERE id = $1 LIMIT 1",
+            [PostgresData(int32: .random(in: 1...10_000))]
+        ).get()
+
+        guard let row = rows.first else {
+            throw Abort(.notFound)
+        }
+
+        var world =  World(
+            id: row.column("id")!.int32!,
+            randomnumber: row.column("randomnumber")!.int!
+        )
+
+        // Update
+        //
+        world.randomnumber = .random(in: 1...10_000)
+
+        _ = try await db.query(
+            "UPDATE World SET randomnumber = $1 WHERE id = $2",
+            [PostgresData(int: world.randomnumber), PostgresData(int32: world.id!)]
+        ).get()
+
+        worlds.append(world)
+    }
+
+    return worlds
+}
+
 extension Int: Sequence {
     public func makeIterator() -> CountableRange<Int>.Iterator {
         return (0..<self).makeIterator()
