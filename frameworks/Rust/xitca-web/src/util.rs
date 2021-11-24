f403892ce52e33c40fed16762647a699f60f61b5
fakeshadow <24548779@qq.com>
2021-11-25 02:31:24 +0800

    update dep

--- frameworks/Rust/xitca-web/src/util.rs
+++ frameworks/Rust/xitca-web/src/util.rs
@@ -7,18 +7,14 @@ use std::{
     io,
 };
 
-use serde::Serialize;
 use xitca_web::{
     dev::bytes::{Bytes, BytesMut},
     http::{
-        header::{HeaderValue, CONTENT_TYPE, SERVER},
+        header::{HeaderValue, SERVER},
         StatusCode,
     },
-    request::WebRequest,
     response::{WebResponse, WebResponseBuilder},
 };
-
-use super::ser::Message;
 
 pub(super) type HandleResult = Result<WebResponse, Infallible>;
 
@@ -92,36 +88,6 @@ pub const HTML_HEADER_VALUE: HeaderValue = HeaderValue::from_static("text/html; 
 pub const TEXT_HEADER_VALUE: HeaderValue = HeaderValue::from_static("text/plain");
 
 pub const JSON_HEADER_VALUE: HeaderValue = HeaderValue::from_static("application/json");
-
-pub(super) fn plain_text<D>(req: &mut WebRequest<'_, D>) -> HandleResult {
-    let mut res = req.as_response(Bytes::from_static(b"Hello, World!"));
-
-    res.headers_mut().append(SERVER, SERVER_HEADER_VALUE);
-    res.headers_mut().append(CONTENT_TYPE, TEXT_HEADER_VALUE);
-
-    Ok(res)
-}
-
-#[inline(always)]
-pub(super) fn json<D>(req: &mut WebRequest<'_, AppState<D>>) -> HandleResult {
-    json_response(req, &Message::new())
-}
-
-#[inline]
-pub(super) fn json_response<S, D>(req: &mut WebRequest<'_, AppState<D>>, value: &S) -> HandleResult
-where
-    S: ?Sized + Serialize,
-{
-    let mut writer = req.state().writer();
-    simd_json::to_writer(&mut writer, value).unwrap();
-    let body = writer.take();
-
-    let mut res = req.as_response(body);
-    res.headers_mut().append(SERVER, SERVER_HEADER_VALUE);
-    res.headers_mut().append(CONTENT_TYPE, JSON_HEADER_VALUE);
-
-    Ok(res)
-}
 
 macro_rules! error {
     ($error: ident, $code: path) => {
