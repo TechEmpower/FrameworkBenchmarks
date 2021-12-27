17d5437b26a1ab4734500a36cbfd4a48c30d2f35
Michael Spiz <mike@spiz.us>
2021-12-27 10:17:34 -0800

    Rails updates (#6997)
    * update gems
    
    * use array.new
    
    * remove deprecation
    
    * attempt to fix run error on TFB server

--- frameworks/Ruby/rails/app/controllers/hello_world_controller.rb
+++ frameworks/Ruby/rails/app/controllers/hello_world_controller.rb
@@ -41,8 +41,8 @@ class HelloWorldController < ApplicationController
   end
 
   def update
-    worlds = query_count.times.map { random_id }.map do |id|
-      world = World.find(id)
+    worlds = Array.new(query_count) do
+      world = World.find(random_id)
       new_value = random_id
       new_value = random_id until new_value != world.randomNumber
       world.update_columns(randomNumber: new_value)
