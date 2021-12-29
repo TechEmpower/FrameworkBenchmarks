2810b23c5715acd72817abb9d3e4209a149441d8
Damith G <djnitehawk83@gmail.com>
2021-12-29 11:26:12 +0530

    Add FastEndpoints framework to CSharp

---
+++ frameworks/CSharp/fastendpoints/Benchmarks/Endpoints/JsonEndpoint.cs
@@ -0,0 +1,21 @@
+﻿namespace Benchmarks.Endpoints;
+
+public class Response
+{
+    public string message => "Hello, World!";
+}
+
+public class JsonEndpoint : Endpoint<EmptyRequest, Response>
+{
+    public override void Configure()
+    {
+        Get("/json");
+        AllowAnonymous();
+    }
+
+    public override Task HandleAsync(EmptyRequest r, CancellationToken ct)
+    {
+        HttpContext.Response.ContentLength = 27;
+        return SendAsync(Response);
+    }
+}
---
+++ frameworks/CSharp/fastendpoints/Benchmarks/Program.cs
@@ -0,0 +1,9 @@
+global using FastEndpoints;
+
+var builder = WebApplication.CreateBuilder();
+builder.Services.AddFastEndpoints();
+
+var app = builder.Build();
+app.UseAuthorization();
+app.UseFastEndpoints();
+app.Run();
