04814eabe5dea7baeffddccf6d65fe9a4d2316f6
黑小马 <1417262058@qq.com>
2022-01-12 04:18:39 +0800

    update hserver version (#7015)
    * hserver framework submit
    
    * hserver framework submit
    
    * lowercase fix
    
    * Required response header missing: Date fix
    
    * Date format fix
    
    * update hserver version
    
    * update hserver threadPool
    
    * update hserver
    
    * update hserver
    
    Co-authored-by: 黑小马 <heixiaoma@gmail.com>

--- frameworks/Java/hserver/src/main/java/com/test/hserver/StartApp.java
+++ frameworks/Java/hserver/src/main/java/com/test/hserver/StartApp.java
@@ -2,6 +2,7 @@ package com.test.hserver;
 
 import top.hserver.HServerApplication;
 import top.hserver.core.ioc.annotation.HServerBoot;
+import top.hserver.core.server.context.ConstConfig;
 
 /**
  * @author hxm
@@ -10,6 +11,7 @@ import top.hserver.core.ioc.annotation.HServerBoot;
 public class StartApp {
 
     public static void main(String[] args) {
+        ConstConfig.workerPool=Runtime.getRuntime().availableProcessors();
         HServerApplication.run(StartApp.class, 8888, args);
     }
 }
