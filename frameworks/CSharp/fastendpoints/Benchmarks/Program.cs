203a65172507b7a48486e84cd96c7b8e08f327ce
Dĵ ΝιΓΞΗΛψΚ <dj-nitehawk@users.noreply.github.com>
2022-01-28 21:59:40 +0530

    upgrade `fastendpoints` to v3.1 and match with `aspcore-mvc` (#7033)
    * upgrade `fastendpoints` to v3.1.0 and match with `aspcore-mvc`
    
    * change fastendpoints package patch version
    
    * disable default logging providers

--- frameworks/CSharp/fastendpoints/Benchmarks/Program.cs
+++ frameworks/CSharp/fastendpoints/Benchmarks/Program.cs
@@ -1,9 +1,9 @@
 global using FastEndpoints;
 
 var builder = WebApplication.CreateBuilder();
+builder.Logging.ClearProviders();
 builder.Services.AddFastEndpoints();
 
 var app = builder.Build();
-app.UseAuthorization();
 app.UseFastEndpoints();
 app.Run();
