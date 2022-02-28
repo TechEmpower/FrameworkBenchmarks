8dd61b75eb841a0376b3e74ca3b7a498aadd1b25
Yurun <admin@yurunsoft.com>
2022-03-01 00:19:10 +0800

    [imi] Update imi 2.0 (#6866)
    * Update imi 2.0
    
    * optimize
    
    * optimize
    
    * update php 8.1
    
    * Fix
    
    * Fix

--- frameworks/PHP/imi/Model/PgSql/World.php
+++ frameworks/PHP/imi/Model/PgSql/World.php
@@ -1,13 +1,13 @@
 <?php
 declare(strict_types=1);
 
-namespace ImiApp\Model;
+namespace ImiApp\Model\PgSql;
 
 use Imi\Bean\Annotation\Inherit;
-use ImiApp\Model\Base\WorldBase;
+use ImiApp\Model\PgSql\Base\WorldBase;
 
 /**
- * world
+ * World
  * @Inherit
  */
 class World extends WorldBase
