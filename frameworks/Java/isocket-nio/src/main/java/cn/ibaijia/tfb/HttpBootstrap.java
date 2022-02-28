9117e145dba445df805c834f82d6b94359dcf8c8
longzhili <longzhili@126.com>
2022-02-28 16:54:32 +0800

    fix

--- frameworks/Java/isocket-nio/src/main/java/cn/ibaijia/tfb/HttpBootstrap.java
+++ frameworks/Java/isocket-nio/src/main/java/cn/ibaijia/tfb/HttpBootstrap.java
@@ -2,9 +2,10 @@ package cn.ibaijia.tfb;
 
 import cn.ibaijia.isocket.Server;
 import cn.ibaijia.isocket.listener.SessionProcessErrorListener;
+import cn.ibaijia.isocket.protocol.SimpleHttpProtocol;
+import cn.ibaijia.isocket.protocol.http.DateUtil;
 import cn.ibaijia.isocket.session.Session;
 import cn.ibaijia.tfb.processor.PlanTextProcessor;
-import cn.ibaijia.tfb.protocol.SimpleHttpProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -19,17 +20,12 @@ public class HttpBootstrap {
         Server server = new Server("0.0.0.0", 8080);
         server.addProtocol(new SimpleHttpProtocol());
         server.setProcessor(new PlanTextProcessor());
-        server.setSessionProcessErrorListener(new SessionProcessErrorListener() {
-            @Override
-            public void run(Session session, Object o, Throwable throwable) {
-                logger.error("session on process error.", throwable);
-            }
-        });
+        server.setSessionProcessErrorListener((session, o, throwable) -> logger.error("session on process error.", throwable));
         server.setUseDirectBuffer(true);
         server.setUsePool(true);
-        server.setPoolPageSize(32 * 1024);
+        server.setPoolSize(64 * 1024);
         server.setBuffSize(1 * 1024);
-        server.setBacklog(16 * 1024);
+        server.setBacklog(4 * 1024);
         server.start();
     }
 
