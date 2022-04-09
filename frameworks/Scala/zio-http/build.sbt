ddb6fb6f6acd7550be69055aaad5afc52b0bb66d
shrutiverma97 <shruti@dream11.com>
2022-04-09 13:16:46 +0530

    zhttp version updated

--- frameworks/Scala/zio-http/build.sbt
+++ frameworks/Scala/zio-http/build.sbt
@@ -7,7 +7,13 @@ lazy val root = (project in file("."))
       Seq(
         "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-core"   % "2.9.1",
         "com.github.plokhotnyuk.jsoniter-scala" %% "jsoniter-scala-macros" % "2.9.1" % "compile-internal",
-        "io.d11"                                 % "zhttp"                 % "1.0.0-RC5",
+        "io.d11"                                %% "zhttp"                 % "1.0.0.0-RC27",
       ),
     testFrameworks += new TestFramework("zio.test.sbt.ZTestFramework"),
+    assembly / assemblyMergeStrategy  := {
+      case x if x.contains("io.netty.versions.properties") => MergeStrategy.discard
+      case x =>
+        val oldStrategy = (assembly / assemblyMergeStrategy).value
+        oldStrategy(x)
+    }
   )
--- frameworks/Scala/zio-http/src/main/scala/Main.scala
+++ frameworks/Scala/zio-http/src/main/scala/Main.scala
@@ -1,42 +1,58 @@
-import zhttp.http._
-import zhttp.service.Server
-import zio.{App, ExitCode, URIO}
-import com.github.plokhotnyuk.jsoniter_scala.macros._
-import com.github.plokhotnyuk.jsoniter_scala.core._
-import zhttp.http.Response
-
 import java.time.format.DateTimeFormatter
 import java.time.{Instant, ZoneOffset}
-
-case class Message(message: String)
+import zhttp.http._
+import zio._
+import zhttp.service._
+import zhttp.service.server.ServerChannelFactory
+import com.github.plokhotnyuk.jsoniter_scala.core._
+import com.github.plokhotnyuk.jsoniter_scala.macros._
+import io.netty.util.AsciiString
 
 object Main extends App {
-  val message: String                         = "Hello, World!"
-  implicit val codec: JsonValueCodec[Message] = JsonCodecMaker.make
+  private val plainTextMessage: String = "Hello, World!"
+  private val jsonMessage: String      = """{"message":"Hello, World!"}"""
 
-  val app: Http[Any, HttpError, Request, Response] = Http.collect[Request] {
-    case Method.GET -> Root / "plaintext" =>
-      Response.http(
-        content = HttpContent.Complete(message),
-        headers = Header.contentTypeTextPlain :: headers(),
-      )
-    case Method.GET -> Root / "json"      =>
-      Response.http(
-        content = HttpContent.Complete(writeToString(Message(message))),
-        headers = Header.contentTypeJson :: headers(),
-      )
-  }
+  private val plaintextPath = "/plaintext"
+  private val jsonPath      = "/json"
 
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = Server.start(8080, app).exitCode
+  private val STATIC_SERVER_NAME = AsciiString.cached("zio-http")
 
-  val formatter: DateTimeFormatter                = DateTimeFormatter.RFC_1123_DATE_TIME.withZone(ZoneOffset.UTC)
-  val constantHeaders: List[Header]               = Header("server", "zio-http") :: Nil
-  @volatile var lastHeaders: (Long, List[Header]) = (0, Nil)
+  private val frozenJsonResponse = Response
+    .json(jsonMessage)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
 
-  def headers(): List[Header] = {
-    val t = System.currentTimeMillis()
-    if (t - lastHeaders._1 >= 1000)
-      lastHeaders = (t, Header("date", formatter.format(Instant.ofEpochMilli(t))) :: constantHeaders)
-    lastHeaders._2
-  }
+  private val frozenPlainTextResponse = Response
+    .text(plainTextMessage)
+    .withServerTime
+    .withServer(STATIC_SERVER_NAME)
+    .freeze
+
+  private def plainTextApp(response: Response) = Http.fromHExit(HExit.succeed(response)).whenPathEq(plaintextPath)
+
+  private def jsonApp(json: Response) = Http.fromHExit(HExit.succeed(json)).whenPathEq(jsonPath)
+
+  private def app = for {
+    plainTextResponse <- frozenPlainTextResponse
+    jsonResponse      <- frozenJsonResponse
+  } yield plainTextApp(plainTextResponse) ++ jsonApp(jsonResponse)
+
+  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] =
+    app
+      .flatMap(server(_).make.useForever)
+      .provideCustomLayer(ServerChannelFactory.auto ++ EventLoopGroup.auto(8))
+      .exitCode
+
+  private def server(app: HttpApp[Any, Nothing]) =
+    Server.app(app) ++
+      Server.port(8080) ++
+      Server.error(_ => UIO.unit) ++
+      Server.disableLeakDetection ++
+      Server.consolidateFlush ++
+      Server.disableFlowControl
 }
+
+
+
+
