a8fc62bf40768a01abbe371c97b0519d7be92a00
Chrislearn Young <chris@acroidea.com>
2022-05-15 09:42:25 +0800

    Salvo: upgrade version 0.23

--- frameworks/Rust/salvo/src/main.rs
+++ frameworks/Rust/salvo/src/main.rs
@@ -4,7 +4,9 @@ static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 use std::sync::Arc;
 use std::thread::available_parallelism;
 
+use bytes::BytesMut;
 use salvo::http::header::{self, HeaderValue};
+use salvo::http::response::Body;
 use salvo::prelude::*;
 use serde::Serialize;
 
@@ -18,18 +20,22 @@ pub struct Message {
 
 #[fn_handler]
 async fn json(res: &mut Response) {
-    res.headers_mut().insert(header::SERVER, HeaderValue::from_static("S"));
-    res.render(Json(Message {
+    let headers = res.headers_mut();
+    headers.insert(header::SERVER, HeaderValue::from_static("S"));
+    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    let data = serde_json::to_vec(&Message {
         message: "Hello, World!",
-    }));
+    })
+    .unwrap();
+    res.set_body(Body::Bytes(BytesMut::from(data.as_slice())));
 }
 
 #[fn_handler]
 async fn plaintext(res: &mut Response) {
-    res.headers_mut().insert(header::SERVER, HeaderValue::from_static("S"));
-    res.headers_mut()
-        .insert(header::CONTENT_TYPE, HeaderValue::from_static("text/plain"));
-    res.write_body(HELLO_WORLD).ok();
+    let headers = res.headers_mut();
+    headers.insert(header::SERVER, HeaderValue::from_static("S"));
+    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+    res.set_body(Body::Bytes(BytesMut::from(HELLO_WORLD)));
 }
 
 fn main() {
