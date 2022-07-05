378e4532249cc0689cbdca6174eecb2c8900c089
John Towler <john.towler@contrastsecurity.com>
2022-07-05 13:12:29 -0700

    QE-899: Update tech-empower-contrast to accept node-protect agents

---
+++ contrast/benchmark-node-protect.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+source ./benchmark-variables.sh
+
+set -e
+
+cd "${0%/*}"
+
+# Use files in the script directory by default
+AGENT_FILE=${1-node-contrast.tgz}
+
+if [[ $(basename -- "$AGENT_FILE") != "node-contrast.tgz" ]];
+    then echo "First argument must be a path to node-contrast.tgz" && exit 1;
+fi;
+
+if [[ ! -f $AGENT_FILE ]];
+    then echo "Could not find $AGENT_FILE" && exit 1;
+fi;
+
+# Copy files into correct place
+./copy-files.sh JavaScript $AGENT_FILE
+
+# Start contrast-service
+./start-contrast-service.sh
+
+# Run tests
+../tfb --tag "contrast-unattached" --tag "contrast-off" --tag "contrast-protect" --test-lang JavaScript --type fortune --duration $DURATION --concurrency-levels $CONCURRENCY_LEVELS
+
+# Stop contrast-service container
+docker stop contrast-service
