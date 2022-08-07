ffbbf4b94bfbde61d98c1543abc6ca4ae1756fd2
Vladyslav Zolotarov <zolotaryov.v.g@gmail.com>
2022-08-07 18:01:04 +0300

    Workings updates

--- frameworks/Julia/genie/routes.jl
+++ frameworks/Julia/genie/routes.jl
@@ -32,7 +32,7 @@ struct World
   randomNumber::Int
 end
 
-function withConnection(operation)
+function with_connection(operation)
   connection = DBInterface.connect(MySQL.Connection, "tfb-database", "benchmarkdbuser", "benchmarkdbpass", db="hello_world")
   try
     return operation(connection)
@@ -41,7 +41,7 @@ function withConnection(operation)
   end
 end
 
-function queryWorld(connection, id::Int)::World
+function query_world(connection, id::Int)::World
   sqlQuery = "SELECT id, randomNumber FROM World WHERE id = $id"
   results = DBInterface.execute(connection, sqlQuery)
   row = first(results)
@@ -49,38 +49,74 @@ function queryWorld(connection, id::Int)::World
 end
 
 route("/db") do
-  withConnection(function (connection)
-    randomNumber = rand(1:10000)
-    world = queryWorld(connection, randomNumber)
+  with_connection(function (connection)
+  randomnumber = rand(1:10000)
+    world = query_world(connection, randomnumber)
     json(world, headers=Genie.Renderer.HTTPHeaders(date_now_header()))
   end)
 end
 
-route("/queries") do  
-  numQueriesParam = params(:queries, "")
+function numbers_of_queries(paramname::Symbol; default="")
+  number_of_queries_param = params(paramname, default)
 
-  numQueries = 1  
+  number_of_queries = 1  
   try
-    numQueries = parse(Int64, numQueriesParam)
-  catch ArgumentError
-    numQueries = 1
+    number_of_queries = parse(Int64, number_of_queries_param)
+  catch _
+    number_of_queries = 1
   end
 
-  if numQueries > 500
-    numQueries = 500
+  if number_of_queries > 500
+    number_of_queries = 500
   end
 
-  if numQueries < 1
-    numQueries = 1
+  if number_of_queries < 1
+    number_of_queries = 1
   end
+
+  return number_of_queries
+end
+
+route("/queries") do  
+  number_of_queries = numbers_of_queries(:queries)
   
   worlds = World[]
-  withConnection(function (connection)
-    for _ in 1:numQueries
-      randomNumber = rand(1:10000)
-      world = queryWorld(connection, randomNumber)
+  with_connection(function (connection)
+    for _ in 1:number_of_queries
+      randomnumber = rand(1:10000)
+      world = query_world(connection, randomnumber)
       push!(worlds, world)
     end
+  end)
+
+  json(worlds, headers=Genie.Renderer.HTTPHeaders(date_now_header()))
+end
+
+route("/updates") do  
+  number_of_queries = numbers_of_queries(:queries)
+  
+  worlds = World[]
+  with_connection(function (connection)
+    for _ in 1:number_of_queries
+      randomnumber = rand(1:10000)
+      world = query_world(connection, randomnumber)
+      push!(worlds, world)
+    end
+
+    worlds = map(world -> World(world.id, rand(1:10000)), worlds)
+    
+    ids = Int[]
+    random_numbers = Int[]
+    [(push!(ids, world.id); push!(random_numbers, world.randomNumber)) for world in worlds]
+
+    DBInterface.transaction(function ()
+      update_statement = DBInterface.prepare(connection, "UPDATE world SET randomNumber=? WHERE id=?") 
+      try
+        DBInterface.executemany(update_statement, (randomNumber=random_numbers, id=ids))
+      finally
+        DBInterface.close!(update_statement)
+      end
+    end, connection)
   end)
 
   json(worlds, headers=Genie.Renderer.HTTPHeaders(date_now_header()))
