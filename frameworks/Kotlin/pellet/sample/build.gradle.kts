5240fe5ebbd9b3de0219ac3113557aaeac4b48cc
Carrot <149851+CarrotCodes@users.noreply.github.com>
2022-08-15 18:31:37 +0100

    Update Pellet to 0.0.15 (#7540)

--- frameworks/Kotlin/pellet/sample/build.gradle.kts
+++ frameworks/Kotlin/pellet/sample/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     application
     id("com.github.johnrengelman.shadow") version "7.1.0"
-    kotlin("jvm") version "1.7.0"
-    kotlin("plugin.serialization") version "1.7.0"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.serialization") version "1.7.10"
     id("nu.studer.rocker") version "3.0.4"
 }
 
@@ -25,19 +25,20 @@ rocker {
 }
 
 dependencies {
-    implementation(platform("dev.pellet:pellet-bom:0.0.14"))
+    implementation(platform("dev.pellet:pellet-bom:0.0.15"))
     implementation("dev.pellet:pellet-server")
     implementation("dev.pellet:pellet-logging")
     implementation("org.slf4j:slf4j-api:1.7.36")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.0-RC")
     implementation(platform(kotlin("bom")))
     implementation(kotlin("stdlib-jdk8"))
-    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.2"))
+    implementation(platform("org.jetbrains.kotlinx:kotlinx-coroutines-bom:1.6.4"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8")
-    implementation("io.vertx:vertx-pg-client:4.3.1")
-    implementation("io.vertx:vertx-lang-kotlin:4.3.1")
-    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.3.1")
+    implementation("io.vertx:vertx-pg-client:4.3.2")
+    implementation("com.ongres.scram:client:2.1")
+    implementation("io.vertx:vertx-lang-kotlin:4.3.2")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines:4.3.2")
 }
 
 java {
