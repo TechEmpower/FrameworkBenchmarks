f441b7a1159e83e49ab32943ffd9004dc94ed667
pgjones <philip.graham.jones@googlemail.com>
2022-09-02 17:15:29 +0100

    Ensure FastAPI apps differ only by OrJSON usage
    This will help maintain these files as now a diff between them only
    shows the OrJSON related changes as expected.

--- frameworks/Python/fastapi/app.py
+++ frameworks/Python/fastapi/app.py
@@ -6,12 +6,11 @@ from fastapi.templating import Jinja2Templates
 from starlette.responses import HTMLResponse, JSONResponse, PlainTextResponse
 from random import randint, sample
 from operator import itemgetter
-from urllib.parse import parse_qs
 
-
-READ_ROW_SQL = 'SELECT "randomnumber", "id" FROM "world" WHERE id = $1'
+READ_ROW_SQL = 'SELECT "id", "randomnumber" FROM "world" WHERE id = $1'
 WRITE_ROW_SQL = 'UPDATE "world" SET "randomnumber"=$1 WHERE id=$2'
-ADDITIONAL_ROW = [0, 'Additional fortune added at request time.']
+ADDITIONAL_ROW = [0, "Additional fortune added at request time."]
+
 
 def get_num_queries(queries):
     try:
@@ -38,30 +37,29 @@ templates = Jinja2Templates(directory="templates")
 async def setup_database():
     global connection_pool
     connection_pool = await asyncpg.create_pool(
-        user=os.getenv('PGUSER', 'benchmarkdbuser'),
-        password=os.getenv('PGPASS', 'benchmarkdbpass'),
-        database='hello_world',
-        host='tfb-database',
-        port=5432
+        user=os.getenv("PGUSER", "benchmarkdbuser"),
+        password=os.getenv("PGPASS", "benchmarkdbpass"),
+        database="hello_world",
+        host="tfb-database",
+        port=5432,
     )
 
 
-@app.get('/json')
+@app.get("/json")
 async def json_serialization():
-    return JSONResponse({'message': 'Hello, world!'})
+    return JSONResponse({"message": "Hello, world!"})
 
 
-@app.get('/db')
+@app.get("/db")
 async def single_database_query():
     row_id = randint(1, 10000)
-
     async with connection_pool.acquire() as connection:
         number = await connection.fetchval(READ_ROW_SQL, row_id)
 
-    return JSONResponse({'id': row_id, 'randomNumber': number})
+    return JSONResponse({"id": row_id, "randomNumber": number})
 
 
-@app.get('/queries')
+@app.get("/queries")
 async def multiple_database_queries(queries = None):
     num_queries = get_num_queries(queries)
     row_ids = sample(range(1, 10000), num_queries)
@@ -71,26 +69,26 @@ async def multiple_database_queries(queries = None):
         statement = await connection.prepare(READ_ROW_SQL)
         for row_id in row_ids:
             number = await statement.fetchval(row_id)
-            worlds.append({'id': row_id, 'randomNumber': number})
+            worlds.append({"id": row_id, "randomNumber": number})
 
     return JSONResponse(worlds)
 
 
-@app.get('/fortunes')
+@app.get("/fortunes")
 async def fortunes(request: Request):
     async with connection_pool.acquire() as connection:
-        fortunes = await connection.fetch('SELECT * FROM Fortune')
+        fortunes = await connection.fetch("SELECT * FROM Fortune")
 
     fortunes.append(ADDITIONAL_ROW)
     fortunes.sort(key=sort_fortunes_key)
     return templates.TemplateResponse("fortune.html", {"fortunes": fortunes, "request": request})
 
 
-@app.get('/updates')
+@app.get("/updates")
 async def database_updates(queries = None):
     num_queries = get_num_queries(queries)
     updates = [(row_id, randint(1, 10000)) for row_id in sample(range(1, 10000), num_queries)]
-    worlds = [{'id': row_id, 'randomNumber': number} for row_id, number in updates]
+    worlds = [{"id": row_id, "randomNumber": number} for row_id, number in updates]
 
     async with connection_pool.acquire() as connection:
         statement = await connection.prepare(READ_ROW_SQL)
@@ -101,6 +99,6 @@ async def database_updates(queries = None):
     return JSONResponse(worlds)
 
 
-@app.get('/plaintext')
+@app.get("/plaintext")
 async def plaintext():
-    return PlainTextResponse(b'Hello, world!')
+    return PlainTextResponse(b"Hello, world!")
--- frameworks/Python/fastapi/app_orjson.py
+++ frameworks/Python/fastapi/app_orjson.py
@@ -6,22 +6,10 @@ from fastapi.responses import HTMLResponse, ORJSONResponse, PlainTextResponse
 from fastapi.templating import Jinja2Templates
 from random import randint, sample
 from operator import itemgetter
-from functools import partial
 
 READ_ROW_SQL = 'SELECT "id", "randomnumber" FROM "world" WHERE id = $1'
 WRITE_ROW_SQL = 'UPDATE "world" SET "randomnumber"=$1 WHERE id=$2'
 ADDITIONAL_ROW = [0, "Additional fortune added at request time."]
-
-
-async def setup_database():
-    global connection_pool
-    connection_pool = await asyncpg.create_pool(
-        user=os.getenv("PGUSER", "benchmarkdbuser"),
-        password=os.getenv("PGPASS", "benchmarkdbpass"),
-        database="hello_world",
-        host="tfb-database",
-        port=5432,
-    )
 
 
 def get_num_queries(queries):
@@ -39,13 +27,22 @@ def get_num_queries(queries):
 
 connection_pool = None
 sort_fortunes_key = itemgetter(1)
-loop = asyncio.get_event_loop()
-loop.run_until_complete(setup_database())
-
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+
+
+@app.on_event("startup")
+async def setup_database():
+    global connection_pool
+    connection_pool = await asyncpg.create_pool(
+        user=os.getenv("PGUSER", "benchmarkdbuser"),
+        password=os.getenv("PGPASS", "benchmarkdbpass"),
+        database="hello_world",
+        host="tfb-database",
+        port=5432,
+    )
 
 
 @app.get("/json")
@@ -55,14 +52,15 @@ async def json_serialization():
 
 @app.get("/db")
 async def single_database_query():
+    row_id = randint(1, 10000)
     async with connection_pool.acquire() as connection:
-        record = await connection.fetchrow(READ_ROW_SQL, randint(1, 10000))
+        number = await connection.fetchval(READ_ROW_SQL, row_id)
 
-    return ORJSONResponse({"id": record['id'], "randomNumber": record['randomnumber']})
+    return ORJSONResponse({"id": row_id, "randomNumber": number})
 
 
 @app.get("/queries")
-async def multiple_database_queries(queries=None):
+async def multiple_database_queries(queries = None):
     num_queries = get_num_queries(queries)
     row_ids = sample(range(1, 10000), num_queries)
     worlds = []
@@ -71,7 +69,7 @@ async def multiple_database_queries(queries=None):
         statement = await connection.prepare(READ_ROW_SQL)
         for row_id in row_ids:
             number = await statement.fetchval(row_id)
-            worlds.append({'id': row_id, 'randomNumber': number})
+            worlds.append({"id": row_id, "randomNumber": number})
 
     return ORJSONResponse(worlds)
 
@@ -87,7 +85,7 @@ async def fortunes(request: Request):
 
 
 @app.get("/updates")
-async def database_updates(queries=None):
+async def database_updates(queries = None):
     num_queries = get_num_queries(queries)
     updates = [(row_id, randint(1, 10000)) for row_id in sample(range(1, 10000), num_queries)]
     worlds = [{"id": row_id, "randomNumber": number} for row_id, number in updates]
