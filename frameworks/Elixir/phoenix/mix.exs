67498c18c2b1a4fd841317420c1b1e66040a1e64
Chris Garvis <cgarvis@gmail.com>
2022-09-21 09:36:40 -0400

    build(runtime): upgrade elixir to 1.14 and erlang to 24.4

--- frameworks/Elixir/phoenix/mix.exs
+++ frameworks/Elixir/phoenix/mix.exs
@@ -5,7 +5,7 @@ defmodule Hello.Mixfile do
     [
       app: :hello,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
