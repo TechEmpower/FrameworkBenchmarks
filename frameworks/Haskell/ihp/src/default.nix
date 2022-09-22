aa4a0d80d6fb5e1d4799b09a2521fd7f28516c56
Marc Scholten <marc@digitallyinduced.com>
2022-09-22 16:06:56 +0200

    Fixed haskell/ihp (#7585)

--- frameworks/Haskell/ihp/src/default.nix
+++ frameworks/Haskell/ihp/src/default.nix
@@ -1,23 +1,21 @@
 let
     ihp = builtins.fetchGit {
         url = "https://github.com/digitallyinduced/ihp.git";
-        ref = "refs/tags/v0.9.0";
+        ref = "refs/tags/v0.20.0";
     };
     haskellEnv = import "${ihp}/NixSupport/default.nix" {
         ihp = ihp;
         haskellDeps = p: with p; [
-            cabal-install
             base
             wai
             text
-            hlint
             p.ihp
         ];
         otherDeps = p: with p; [
-	    git
             # Native dependencies, e.g. imagemagick
         ];
         projectPath = ./.;
+        optimized = true;
     };
 in
     haskellEnv
