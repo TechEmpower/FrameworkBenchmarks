2af0ad607e4d50d3bb7afa8904b9602351e94726
Ivan Trofimov <i.trofimow@yandex.ru>
2022-10-02 05:05:07 +0300

    + db

--- frameworks/C++/userver/userver.dockerfile
+++ frameworks/C++/userver/userver.dockerfile
@@ -4,7 +4,7 @@ COPY userver_benchmark/src/* ./
 RUN mkdir third_party && git clone https://github.com/userver-framework/userver.git third_party/userver
 RUN mkdir build && cd build && \
     cmake -DUSERVER_OPEN_SOURCE_BUILD=1 -DUSERVER_FEATURE_CRYPTOPP_BLAKE2=0 -DUSERVER_FEATURE_REDIS_HI_MALLOC=1 -DCMAKE_BUILD_TYPE=Release \
-          -DUSERVER_FEATURE_REDIS=0 -DUSERVER_FEATURE_CLICKHOUSE=0 -DUSERVER_FEATURE_MONGODB=0 -DUSERVER_FEATURE_RABBITMQ=0 -DUSERVER_FEATURE_GRPC=0  .. && \
+          -DUSERVER_FEATURE_REDIS=0 -DUSERVER_FEATURE_CLICKHOUSE=0 -DUSERVER_FEATURE_MONGODB=0 -DUSERVER_FEATURE_RABBITMQ=0 -DUSERVER_FEATURE_GRPC=0 .. && \
     make -j5
 
 COPY userver_benchmark/configs/* ./
--- frameworks/C++/userver/userver_benchmark/src/main.cpp
+++ frameworks/C++/userver/userver_benchmark/src/main.cpp
@@ -4,6 +4,10 @@
 #include <userver/server/handlers/tests_control.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
+#include <userver/utils/rand.hpp>
+
+#include <userver/storages/postgres/postgres.hpp>
+#include <userver/storages/secdist/component.hpp>
 
 namespace plaintext {
 
@@ -45,10 +49,58 @@ class Handler final : public userver::server::handlers::HttpHandlerJsonBase {
 
 }  // namespace json
 
+namespace single_query {
+
+struct TableRow final {
+  int id;
+  int random_number;
+};
+
+constexpr int kMaxWorldRows = 10000;
+
+class Handler final : public userver::server::handlers::HttpHandlerJsonBase {
+ public:
+  static constexpr std::string_view kName = "single-query-handler";
+
+  Handler(const userver::components::ComponentConfig& config,
+          const userver::components::ComponentContext& context)
+      : userver::server::handlers::HttpHandlerJsonBase{config, context},
+        pg_{context
+                .FindComponent<userver::components::Postgres>("hello-world-db")
+                .GetCluster()} {}
+
+  userver::formats::json::Value HandleRequestJsonThrow(
+      const userver::server::http::HttpRequest&,
+      const userver::formats::json::Value&,
+      userver::server::request::RequestContext&) const override {
+    const auto row =
+        pg_->Execute(userver::storages::postgres::ClusterHostType::kMaster,
+                     "SELECT id, randomNumber from World where id = $1",
+                     userver::utils::RandRange(1, kMaxWorldRows + 1))
+            .AsSingleRow<TableRow>(userver::storages::postgres::kRowTag);
+
+    userver::formats::json::ValueBuilder builder{
+        userver::formats::json::Type::kObject};
+    builder["id"] = row.id;
+    builder["randomNumber"] = row.random_number;
+    return builder.ExtractValue();
+  }
+
+ private:
+  userver::storages::postgres::ClusterPtr pg_;
+};
+
+}  // namespace single_query
+
 int main(int argc, char* argv[]) {
-  auto component_list = userver::components::MinimalServerComponentList()
-                            .Append<plaintext::Handler>()
-                            .Append<json::Handler>();
+  auto component_list =
+      userver::components::MinimalServerComponentList()
+          .Append<userver::components::Secdist>()
+          .Append<userver::components::TestsuiteSupport>()
+          .Append<plaintext::Handler>()
+          .Append<json::Handler>()
+          .Append<userver::components::Postgres>("hello-world-db")
+          .Append<single_query::Handler>();
 
   return userver::utils::DaemonMain(argc, argv, component_list);
 }
