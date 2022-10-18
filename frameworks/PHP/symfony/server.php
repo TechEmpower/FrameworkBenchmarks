55577beb5ba337155416e34a89c90cb3db4ff3e6
Joanhey <joanhey@kumbiaphp.com>
2022-10-18 10:11:34 +0200

    [ci skip] Fix typo

--- frameworks/PHP/symfony/server.php
+++ frameworks/PHP/symfony/server.php
@@ -10,7 +10,7 @@ Adapterman::init();
 
 $http_worker                = new Worker('http://0.0.0.0:8080');
 $http_worker->count         = (int) shell_exec('nproc') * 4;
-$http_worker->name          = 'AdapterMan-Laravel';
+$http_worker->name          = 'AdapterMan-Symfony';
 $http_worker->onWorkerStart = function () {
     Header::$date = gmdate('D, d M Y H:i:s').' GMT';
     Timer::add(1, function() {
