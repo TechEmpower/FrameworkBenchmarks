c5032986abee13e0131c1d47617272e21e7df4e8
David Denton <denton.david@gmail.com>
2022-11-04 18:42:56 +0000

    upgrade libraries, replace Jackson with Argo

--- frameworks/Kotlin/http4k/core/src/main/kotlin/JsonRoute.kt
+++ frameworks/Kotlin/http4k/core/src/main/kotlin/JsonRoute.kt
@@ -3,13 +3,14 @@ import org.http4k.core.Method.GET
 import org.http4k.core.Response
 import org.http4k.core.Status.Companion.OK
 import org.http4k.core.with
-import org.http4k.format.Jackson.json
-import org.http4k.format.Jackson.obj
-import org.http4k.format.Jackson.string
+import org.http4k.format.Argo
+import org.http4k.format.Argo.json
 import org.http4k.routing.bind
 
 object JsonRoute {
     private val jsonBody = Body.json().toLens()
 
-    operator fun invoke() = "/json" bind GET to { Response(OK).with(jsonBody of obj("message" to string("Hello, World!"))) }
+    operator fun invoke() = "/json" bind GET to {
+        Response(OK).with(jsonBody of Argo { obj("message" to string("Hello, World!")) })
+    }
 }
