6c7702fe10ee9d3d22e5a7cedf229c3108a91b4c
Joan Miquel <joanhey@kumbiaphp.com>
2022-11-28 00:27:50 +0100

    [php] Codeigniter 4 remove database connect from BaseController (#7723)
    * Remove Database::connect() from BaseController
    
    * Clean index.php

--- frameworks/PHP/codeigniter/app/Controllers/BaseController.php
+++ frameworks/PHP/codeigniter/app/Controllers/BaseController.php
@@ -48,6 +48,6 @@ abstract class BaseController extends Controller
         // Preload any models, libraries, etc, here.
 
         // E.g.: $this->session = \Config\Services::session();
-        $this->db = \Config\Database::connect();
+        
     }
 }
