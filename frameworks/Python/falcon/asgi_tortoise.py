ced100137f99814805e88736f7c4fb081d5026df
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2022-12-15 04:59:48 +0800

    Add asgi+tortoise version

---
+++ frameworks/Python/falcon/asgi_tortoise.py
@@ -0,0 +1,82 @@
+import falcon.asgi
+from helpers import load_template, FortuneTuple, generate_ids, sanitize
+from operator import attrgetter
+from random import randint
+from tortoise import Tortoise
+from tortoise_models import World, Fortune
+
+
+class TortoiseInit:
+    async def process_startup(self, scope, event):
+        await Tortoise.init(
+            db_url="postgres://benchmarkdbuser:benchmarkdbpass@tfb-database:5432/hello_world",
+            modules={"models": ["tortoise_models"]}
+        )
+        await Tortoise.generate_schemas(safe=True)
+
+    async def process_shutdown(self, scopre, event):
+        await Tortoise.close_connections()
+
+
+tortoise_init = TortoiseInit()
+
+
+# resource endpoints
+class JSONResource(object):
+    async def on_get(self, request, response):
+        response.media = {'message': "Hello, world!"}
+
+
+class SingleQuery(object):
+    async def on_get(self, request, response):
+        resp = await World.get(id=randint(1, 10000))
+        response.media = resp.to_dict()
+
+
+class MultipleQueries(object):
+    async def on_get(self, request, response, num):
+        num = sanitize(num)
+        worlds = await World.filter(id__in=[idx for idx in generate_ids(num)]).values()
+        response.media = worlds
+
+
+class UpdateQueries(object):
+    async def on_get(self, request, response, num):
+        num = sanitize(num)
+        ids = generate_ids(num)
+        ids.sort()
+        worlds = []
+        for item in ids:
+            world = await World.get(id=item)
+            world.randomNumber = randint(1, 10000)
+            worlds.append({"id": world.id, "randomNumber": world.randomNumber})
+        response.media = worlds
+
+
+class Fortunes(object):
+    _template = load_template()
+
+    # @in_transaction()
+    async def on_get(self, request, response):
+        fortunes = [FortuneTuple(id=f.id, message=f.message) for f in await Fortune.all()]
+        fortunes.append(FortuneTuple(id=0, message="Additional fortune added at request time."))
+        fortunes.sort(key=attrgetter("message"))
+        content = self._template.render(fortunes=fortunes)
+        response.content_type = falcon.MEDIA_HTML
+        response.text = content
+
+
+class PlaintextResource(object):
+    async def on_get(self, request, response):
+        response.content_type = falcon.MEDIA_TEXT
+        response.text = 'Hello, world!'
+
+
+app = falcon.asgi.App(middleware=[tortoise_init])
+# register resources
+app.add_route("/json", JSONResource())
+app.add_route("/db", SingleQuery())
+app.add_route("/queries/{num}", MultipleQueries())
+app.add_route("/updates/{num}", UpdateQueries())
+app.add_route("/fortunes", Fortunes())
+app.add_route("/plaintext", PlaintextResource())
---
+++ frameworks/Python/falcon/falcon-asgi.dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.8-buster
+
+RUN apt-get update; apt-get install libpq-dev python3-dev -y
+COPY ./ /falcon
+WORKDIR /falcon
+RUN pip3 install -U pip; \
+    pip3 install cython==0.29.26; \
+    pip3 install -r /falcon/requirements.txt; \
+    pip3 install falcon==3.1.1 --no-binary :all:;
+
+EXPOSE 8080
+
+CMD ["uvicorn", "asgi_tortoise:asgi", "--host", "0.0.0.0", "--port", "8080", "--workers", "2"]
