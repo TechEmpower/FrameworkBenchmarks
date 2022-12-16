42b03f49f75a6353a673c805f163bc1966667f7a
Nik Peric <nik@nikperic.com>
2022-12-16 16:45:42 +0100

    update gitignore to match guidelines of PR

--- frameworks/Clojure/kit/test/clj/io/github/kit_clj/te_bench/core_test.clj
+++
@@ -1,8 +0,0 @@
-(ns io.github.kit-clj.te-bench.core-test
-  (:require
-    [io.github.kit-clj.te-bench.test-utils :as utils]
-    [clojure.test :refer :all]))
-
-(deftest example-test
-  (is (= 1 2)))
-
