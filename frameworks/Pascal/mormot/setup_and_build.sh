f1204759eba6dc4da70a7514a33c7e29218f491d
pavel.mash <pavel.mash@inbase.com.ua>
2023-01-17 14:03:07 +0200

    [mORMot] improved HTTP header parser

--- frameworks/Pascal/mormot/setup_and_build.sh
+++ frameworks/Pascal/mormot/setup_and_build.sh
@@ -40,7 +40,7 @@ echo "Unpacking to ./libs/mORMot/static ..."
 rm -rf ./mormot2static.7z
 
 # uncomment for fixed commit URL
-URL=https://github.com/synopse/mORMot2/tarball/5439a291fbb43bb8b5804982c5f843420ce932e1
+URL=https://github.com/synopse/mORMot2/tarball/dbe44a75014ac01f3afb4fbf1880dbfd18ae0d2c
 #URL="https://api.github.com/repos/synopse/mORMot2/tarball/$USED_TAG"
 echo "Download and unpacking mORMot sources from $URL ..."
 wget -qO- "$URL" | tar -xz -C ./libs/mORMot  --strip-components=1
