227976f9cd8e6523b7b35b753c168316ffa9502c
Shreck Ye <ShreckYe@gmail.com>
2023-01-24 19:42:37 +0800

    Add some needed Gradle plugins and dependencies, and an empty main function

--- frameworks/Kotlin/vertx-web-kotlin/build.gradle.kts
+++ frameworks/Kotlin/vertx-web-kotlin/build.gradle.kts
@@ -1,3 +1,23 @@
 tasks.wrapper {
     distributionType = Wrapper.DistributionType.ALL
 }
+
+plugins {
+    kotlin("jvm") version "1.8.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+val vertxVersion = "4.3.7"
+dependencies {
+    implementation(platform("io.vertx:vertx-stack-depchain:$vertxVersion"))
+    implementation("io.vertx:vertx-web")
+    implementation("io.vertx:vertx-pg-client")
+    implementation("io.vertx:vertx-lang-kotlin")
+    implementation("io.vertx:vertx-lang-kotlin-coroutines")
+}
+
+application.mainClass.set("MainKt")
