40854c7a312e33814edf27e39ef255645a92e27e
Ehsan Noureddin Moosa <ehsannm@users.noreply.github.com>
2023-03-08 07:32:28 +0300

    fix conflicts

--- frameworks/Go/ronykit/src/main.go
+++ frameworks/Go/ronykit/src/main.go
@@ -5,39 +5,24 @@ import (
     "os"
 
     "github.com/clubpay/ronykit/kit"
-    "github.com/clubpay/ronykit/std/gateways/fasthttp"
     "github.com/clubpay/ronykit/std/gateways/silverhttp"
 )
 
 func main() {
-
     opts := []kit.Option{
         kit.RegisterService(serviceDesc.Generate()),
+        kit.RegisterGateway(
+            silverhttp.MustNew(
+                silverhttp.Listen(":8080"),
+                silverhttp.WithServerName("ronykit"),
+            ),
+        ),
     }
 
     for i := range os.Args[1:] {
         switch os.Args[1:][i] {
         case "-prefork":
             opts = append(opts, kit.WithPrefork())
-        case "silverhttp":
-            opts = append(opts,
-                kit.RegisterGateway(
-                    silverhttp.MustNew(
-                        silverhttp.Listen(":8080"),
-                        silverhttp.WithServerName("ronykit"),
-                    ),
-                ),
-            )
-
-        case "fasthttp":
-            opts = append(opts,
-                kit.RegisterGateway(
-                    fasthttp.MustNew(
-                        fasthttp.Listen(":8080"),
-                        fasthttp.WithServerName("ronykit"),
-                    ),
-                ),
-            )
         }
     }
 
