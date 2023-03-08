11ffaeb0b95a8747455c79ed042e0518273a6883
pavel.mash <pavel.mash@inbase.com.ua>
2023-03-08 10:16:45 +0200

    - thread pool auto-tuning: use 1 listening socket per logical CPU and 8 working thread pes listener socket - for /updates with count <=20 use 'case .. when .. then' pattern [mORMot] update to mORMot 2.0.stable [mORMot] improved cached queries performance

--- frameworks/Pascal/mormot/setup_and_build.sh
+++ frameworks/Pascal/mormot/setup_and_build.sh
@@ -24,23 +24,18 @@ script_aborted() {
 set -o pipefail
 
 rm -rf ./libs
-
+mkdir -p ./libs/mORMot/static
 # echo "Getting the latest pre-release URL..."
 # USED_TAG=$(wget -qO- https://api.github.com/repos/synopse/mORMot2/releases/latest | jq -r '.tag_name')
-USED_TAG="2.0.4383"
+USED_TAG="2.0.stable"
 
 echo "Used release tag $USED_TAG"
-URL="https://github.com/synopse/mORMot2/releases/download/$USED_TAG/mormot2static.7z"
+URL="https://github.com/synopse/mORMot2/releases/download/$USED_TAG/mormot2static.tgz"
 echo "Download statics from $URL ..."
-wget -q -O./mormot2static.7z "$URL"
-
-mkdir -p ./libs/mORMot/static
-echo "Unpacking to ./libs/mORMot/static ..."
-7za x ./mormot2static.7z -o./libs/mORMot/static
-rm -rf ./mormot2static.7z
+wget -qO- "$URL" | tar -xz -C ./libs/mORMot/static
 
 # uncomment for fixed commit URL
-URL=https://github.com/synopse/mORMot2/tarball/0eb1a70da04481a6d478acff5183742eed1882f7
+URL=https://github.com/synopse/mORMot2/tarball/46f5360a668ccf7a7c4d538fb319b449da8a232f
 #URL="https://api.github.com/repos/synopse/mORMot2/tarball/$USED_TAG"
 echo "Download and unpacking mORMot sources from $URL ..."
 wget -qO- "$URL" | tar -xz -C ./libs/mORMot  --strip-components=1
@@ -72,7 +67,6 @@ fi
 # Warning: (5089) Local variable XXX of a managed type does not seem to be initialized
 # Warning: (5090) Variable XXX of a managed type does not seem to be initialized
 SUPRESS_WARN=-vm11047,6058,5092,5091,5060,5058,5057,5028,5024,5023,4081,4079,4055,3187,3124,3123,5059,5036,5089,5090
-
 echo "Start compiling..."
 fpc -MDelphi -Sci -Ci -O4 -g -gl -gw2 -Xg -k'-rpath=$ORIGIN' -k-L$BIN \
   -T$TARGET -P$ARCH \
@@ -82,7 +76,7 @@ fpc -MDelphi -Sci -Ci -O4 -g -gl -gw2 -Xg -k'-rpath=$ORIGIN' -k-L$BIN \
   -Fu"$MSRC/core" -Fu"$MSRC/db" -Fu"$MSRC/rest" -Fu"$MSRC/crypt" \
     -Fu"$MSRC/app" -Fu"$MSRC/net" -Fu"$MSRC/lib" -Fu"$MSRC/orm" -Fu"$MSRC/soa" \
   -FU"$BIN/fpc-$ARCH_TG/.dcu" -FE"$BIN/fpc-$ARCH_TG" -o"$BIN/fpc-$ARCH_TG/$dest_fn" \
-  -dFPC_LIBCMM -dNOSYNDBZEOS -dNOSYNDBIBX \
+  -dFPC_LIBCMM \
   -B -Se1 "./src/raw.pas" | grep "[Warning|Error|Fatal]:"
 
 script_successful
