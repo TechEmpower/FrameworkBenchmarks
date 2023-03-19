21022624f749519434e3f636adba99cf1928d246
Giovanni Barillari <g@baro.dev>
2023-03-19 22:51:12 +0100

    [Python] Bump granian to 0.3

--- frameworks/Python/granian/run.py
+++ frameworks/Python/granian/run.py
@@ -6,15 +6,23 @@ from granian import Granian
 
 if __name__ == '__main__':
     interface = sys.argv[1]
+    threading_mode = sys.argv[2]
+    if threading_mode == "runtime":
+        workers = multiprocessing.cpu_count()
+        threads = 2
+    else:
+        workers = multiprocessing.cpu_count() // 2
+        threads = 1
 
     Granian(
         f"app_{interface}:main",
         address="0.0.0.0",
         port=8080,
-        workers=multiprocessing.cpu_count(),
-        threading_mode="runtime",
-        threads=2,
+        workers=workers,
+        threading_mode=threading_mode,
+        threads=threads,
         backlog=2048,
         interface=interface,
+        http="1",
         websockets=False
     ).serve()
