77be03995163f387aa50c89f6fc3f3db2cdf126b
Redkale <redkale@qq.com>
2023-03-29 11:15:16 +0800

    Update cached-worlds

---
+++ frameworks/Java/redkale/src/main/java/org/redkalex/benchmark/CachedWorld.java
@@ -0,0 +1,43 @@
+/*
+ *
+ */
+package org.redkalex.benchmark;
+
+import org.redkale.convert.json.*;
+import org.redkale.persistence.*;
+
+/**
+ *
+ * @author zhangjx
+ */
+@Entity
+@Cacheable(direct = true)
+@Table(name = "world")
+public final class CachedWorld {
+
+    @Id
+    private int id;
+
+    private int randomNumber;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getRandomNumber() {
+        return randomNumber;
+    }
+
+    public void setRandomNumber(int randomNumber) {
+        this.randomNumber = randomNumber;
+    }
+
+    @Override
+    public String toString() {
+        return JsonConvert.root().convertTo(this);
+    }
+}
