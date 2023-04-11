77158e6c368360df9830b140401e9818a600359d
Tim Uckun <timuckun@gmail.com>
2023-04-12 00:34:02 +1200

    upgraded to ruby 3.2 and the latest versions of all the gems. Also switched out the json gem for OJ

--- frameworks/Ruby/roda-sequel/hello_world.rb
+++ frameworks/Ruby/roda-sequel/hello_world.rb
@@ -3,11 +3,11 @@
 # Our Rack application to be executed by rackup
 class HelloWorld < Roda
   plugin :hooks
-  plugin :render, escape: true, layout_opts: { cache_key: 'default_layout' }
+  plugin :render, escape: true, layout_opts: { cache_key: "default_layout" }
   plugin :static_routing
 
   def bounded_queries
-    queries = request['queries'].to_i
+    queries = request.params["queries"].to_i
     return QUERIES_MIN if queries < QUERIES_MIN
     return QUERIES_MAX if queries > QUERIES_MAX
 
@@ -20,43 +20,42 @@ class HelloWorld < Roda
   end
 
   after do
-    response['Date'] = Time.now.httpdate
-    response['Server'] = SERVER_STRING if SERVER_STRING
+    response["Date"] = Time.now.httpdate
+    response["Server"] = SERVER_STRING if SERVER_STRING
   end
 
   # Test type 1: JSON serialization
-  static_get '/json' do |_|
-    response['Content-Type'] = 'application/json'
+  static_get "/json" do |_|
+    response["Content-Type"] = "application/json"
 
-    { message: 'Hello, World!' }.to_json
+    { message: "Hello, World!" }.to_json
   end
 
   # Test type 2: Single database query
-  static_get '/db' do |_|
-    response['Content-Type'] = 'application/json'
+  static_get "/db" do |_|
+    response["Content-Type"] = "application/json"
 
     World.with_pk(rand1).values.to_json
   end
 
   # Test type 3: Multiple database queries
-  static_get '/queries' do |_|
-    response['Content-Type'] = 'application/json'
-    worlds = DB.synchronize do
-      Array.new(bounded_queries) do
-        World.with_pk(rand1).values
+  static_get "/queries" do |_|
+    response["Content-Type"] = "application/json"
+    worlds =
+      DB.synchronize do
+        Array.new(bounded_queries) { World.with_pk(rand1).values }
       end
-    end
 
     worlds.to_json
   end
 
   # Test type 4: Fortunes
-  static_get '/fortunes' do |_|
-    response['Content-Type'] = 'text/html; charset=utf-8'
+  static_get "/fortunes" do |_|
+    response["Content-Type"] = "text/html; charset=utf-8"
     @fortunes = Fortune.all
     @fortunes << Fortune.new(
       id: 0,
-      message: 'Additional fortune added at request time.'
+      message: "Additional fortune added at request time."
     )
     @fortunes.sort_by!(&:message)
 
@@ -64,26 +63,27 @@ class HelloWorld < Roda
   end
 
   # Test type 5: Database updates
-  static_get '/updates' do |_|
-    response['Content-Type'] = 'application/json'
-    worlds = DB.synchronize do
-      Array.new(bounded_queries) do
-        world = World.with_pk(rand1)
-        new_value = rand1
-        new_value = rand1 while new_value == world.randomnumber
-        world.update(randomnumber: new_value)
-        world.values
+  static_get "/updates" do |_|
+    response["Content-Type"] = "application/json"
+    worlds =
+      DB.synchronize do
+        Array.new(bounded_queries) do
+          world = World.with_pk(rand1)
+          new_value = rand1
+          new_value = rand1 while new_value == world.randomnumber
+          world.update(randomnumber: new_value)
+          world.values
+        end
       end
-    end
 
     worlds.to_json
   end
 
   # Test type 6: Plaintext
-  static_get '/plaintext' do |_|
-    response['Content-Type'] = 'text/plain'
+  static_get "/plaintext" do |_|
+    response["Content-Type"] = "text/plain"
 
-    'Hello, World!'
+    "Hello, World!"
   end
 
   # Even though we don't have any non-static routes, this is still required.
