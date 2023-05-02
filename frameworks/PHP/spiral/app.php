0e204cf8e293e4f4d81495fa1a07bc21025b02c7
Maxim Smakouz <m.smakouz@gmail.com>
2023-05-02 17:39:24 +0300

    Spiral Framework v3.7 (#8157)
    * Update Spiral Framework
    
    * Call run outside of the cycle
    
    * Fix queries default value

--- frameworks/PHP/spiral/app.php
+++ frameworks/PHP/spiral/app.php
@@ -1,23 +1,22 @@
 <?php
-/**
- * Spiral Framework.
- *
- * @license   MIT
- * @author    Anton Titov (Wolfy-J)
- */
+
 declare(strict_types=1);
 
-mb_internal_encoding('UTF-8');
-error_reporting(E_ALL | E_STRICT);
-ini_set('display_errors', 'stderr');
+use App\App;
 
-//Composer
+\mb_internal_encoding('UTF-8');
+\error_reporting(E_ALL | E_STRICT ^ E_DEPRECATED);
+\ini_set('display_errors', 'stderr');
+
+// Register Composer's auto loader.
 require __DIR__ . '/vendor/autoload.php';
 
-//Initiating shared container, bindings, directories and etc
-$app = \App\App::init(['root' => __DIR__]);
+// Initialize shared container, bindings, directories and etc.
+$app = App::create(directories: ['root' => __DIR__])->run();
 
-if ($app !== null) {
-    $code = (int)$app->serve();
-    exit($code);
+if ($app === null) {
+    exit(255);
 }
+
+$code = (int)$app->serve();
+exit($code);
