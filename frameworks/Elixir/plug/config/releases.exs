8c340c4cbca332c446560e0678cae90c42d3424b
Chris Garvis <cgarvis@gmail.com>
2023-06-05 12:11:02 -0400

    feat(elixir/plug) upgrade to latest elixir/erlang and use jason (#8244)

--- frameworks/Elixir/plug/config/releases.exs
+++ frameworks/Elixir/plug/config/releases.exs
@@ -1,6 +1,11 @@
 import Config
 
-config :logger, level: :error
+config :logger,
+  compile_time_purge_matching: [
+    [level_lower_than: :error]
+  ],
+  level: :error,
+  backends: []
 
 config :framework_benchmarks, FrameworkBenchmarks.Repo,
   username: "benchmarkdbuser",
