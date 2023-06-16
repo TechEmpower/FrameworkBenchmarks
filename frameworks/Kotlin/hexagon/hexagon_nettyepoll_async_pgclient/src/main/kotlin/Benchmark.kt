267c4b626513161721edfce382629d5c79ce7d83
jaguililla <juanjoaguililla@gmail.com>
2023-06-16 11:26:16 +0200

    Replace Vert.x adapter by Netty Epoll Async adapter

--- frameworks/Kotlin/hexagon/hexagon_nettyepoll_async_pgclient/src/main/kotlin/Benchmark.kt
+++ frameworks/Kotlin/hexagon/hexagon_nettyepoll_async_pgclient/src/main/kotlin/Benchmark.kt
@@ -2,7 +2,7 @@ package com.hexagonkt
 
 import com.hexagonkt.async.Benchmark
 import com.hexagonkt.async.store.BenchmarkPgClientStore
-import com.hexagonkt.http.server.vertx.VertxServerAdapter
+import com.hexagonkt.http.server.netty.epoll.async.NettyEpollServerAdapter
 import com.hexagonkt.templates.rocker.RockerAdapter
 import io.netty.util.ResourceLeakDetector
 import io.netty.util.ResourceLeakDetector.Level.DISABLED
@@ -26,7 +26,7 @@ fun main() {
     val store = BenchmarkPgClientStore("postgresql")
     val templateEngine = RockerAdapter()
     val templateUrl = URL("classpath:fortunes.rocker.html")
-    val engine = VertxServerAdapter(preferNativeTransport = true)
+    val engine = NettyEpollServerAdapter(bossGroupThreads = 48)
 
     val benchmark = Benchmark(engine, store, templateEngine, templateUrl, settings)
     benchmark.server.start()
