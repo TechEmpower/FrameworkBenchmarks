3b053f41613114925dcb8973536360aa52e6fcfb
jaguililla <juanjoaguililla@gmail.com>
2023-06-17 22:16:45 +0200

    Add Jasync store

---
+++ frameworks/Kotlin/hexagon/hexagon-nettyepoll-async-jasync.dockerfile
@@ -0,0 +1,23 @@
+#
+# BUILD
+#
+FROM docker.io/gradle:8.1.1-jdk17-alpine AS build
+USER root
+WORKDIR /hexagon
+
+ADD . .
+RUN gradle --quiet classes
+RUN gradle --quiet -x test installDist
+
+#
+# RUNTIME
+#
+FROM docker.io/eclipse-temurin:17-jre-alpine
+ARG PROJECT=hexagon_nettyepoll_async_jasync
+
+ENV POSTGRESQL_DB_HOST tfb-database
+ENV JDK_JAVA_OPTIONS -XX:+AlwaysPreTouch -XX:+UseParallelGC -XX:+UseNUMA
+
+COPY --from=build /hexagon/$PROJECT/build/install/$PROJECT /opt/$PROJECT
+
+ENTRYPOINT [ "/opt/hexagon_nettyepoll_async_jasync/bin/hexagon_nettyepoll_async_jasync" ]
--- frameworks/Kotlin/hexagon/hexagon_nettyepoll_async_jasync/src/main/kotlin/Benchmark.kt
+++ frameworks/Kotlin/hexagon/hexagon_nettyepoll_async_jasync/src/main/kotlin/Benchmark.kt
@@ -1,7 +1,7 @@
 package com.hexagonkt
 
 import com.hexagonkt.async.Benchmark
-import com.hexagonkt.async.store.BenchmarkPgClientStore
+import com.hexagonkt.async.store.BenchmarkJasyncStore
 import com.hexagonkt.http.server.netty.epoll.async.NettyEpollServerAdapter
 import com.hexagonkt.templates.rocker.RockerAdapter
 import io.netty.util.ResourceLeakDetector
@@ -11,19 +11,11 @@ import java.net.URL
 fun main() {
     ResourceLeakDetector.setLevel(DISABLED)
 
-    System.setProperty("vertx.disableMetrics", "true")
-    System.setProperty("vertx.disableH2c", "true")
-    System.setProperty("vertx.disableWebsockets", "true")
-    System.setProperty("vertx.flashPolicyHandler", "false")
-    System.setProperty("vertx.threadChecks", "false")
-    System.setProperty("vertx.disableContextTimings", "true")
-    System.setProperty("vertx.disableTCCL", "true")
-    System.setProperty("vertx.disableHttpHeadersValidation", "true")
     System.setProperty("io.netty.buffer.checkBounds", "false")
     System.setProperty("io.netty.buffer.checkAccessible", "false")
 
     val settings = Settings()
-    val store = BenchmarkPgClientStore("postgresql")
+    val store = BenchmarkJasyncStore("postgresql")
     val templateEngine = RockerAdapter()
     val templateUrl = URL("classpath:fortunes.rocker.html")
     val engine = NettyEpollServerAdapter(bossGroupThreads = 48)
