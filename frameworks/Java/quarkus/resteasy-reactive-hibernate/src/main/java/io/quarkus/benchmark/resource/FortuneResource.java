06c85efce5df863b04349766c668d3e8580f8d7d
Francesco Nigro <nigro.fra@gmail.com>
2023-06-26 18:11:18 +0200

    Few Quarkus Tweaks (#8265)
    * Disable Arc context propagation
    
    * Disable Arc context propagation on Reactive side
    
    * Change from ApplicationScoped to Singleton
    
    * Reactive Filter is singleton
    
    * Singleton Filter
    
    * Optimized Fortune Rocker
    
    * Leverage on modern compact Strings intrinsics
    
    * Use unpooled heap Buffers for plaintext

--- frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/FortuneResource.java
@@ -1,9 +1,13 @@
 package io.quarkus.benchmark.resource;
 
+import com.fizzed.rocker.RenderingException;
+import com.fizzed.rocker.runtime.DefaultHtmlStringify;
+import com.fizzed.rocker.runtime.DefaultRockerTemplate;
+import com.fizzed.rocker.runtime.GuavaHtmlStringify;
 import io.quarkus.benchmark.model.Fortune;
 import io.quarkus.benchmark.repository.FortuneRepository;
+import io.quarkus.benchmark.utils.rocker.HtmlUtf8BufferRockerOutput;
 import io.vertx.core.buffer.Buffer;
-import io.vertx.ext.web.templ.rocker.impl.VertxBufferOutput;
 
 import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
@@ -12,6 +16,8 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import views.Fortunes;
+
 import java.util.Comparator;
 import java.util.List;
 
@@ -20,6 +26,16 @@ import java.util.List;
 @Produces(MediaType.TEXT_HTML+"; charset=UTF-8")
 @Consumes(MediaType.APPLICATION_JSON)
 public class FortuneResource {
+
+    private static final DefaultHtmlStringify STRINGIFY = new GuavaHtmlStringify();
+
+    private static class CustomFortunesTemplate extends Fortunes.Template {
+
+        public CustomFortunesTemplate(Fortunes model) {
+            super(model);
+            __internal.setStringify(STRINGIFY);
+        }
+    }
 
     @Inject
     FortuneRepository repository;
@@ -32,9 +48,15 @@ public class FortuneResource {
         List<Fortune> fortunes = repository.findAllStateless();
         fortunes.add(new Fortune(0, "Additional fortune added at request time."));
         fortunes.sort(fortuneComparator);
-        return views.Fortunes.template(fortunes)
-                .render(VertxBufferOutput.FACTORY)
-                .getBuffer();
+        return new Fortunes() {
+
+            @Override
+            protected DefaultRockerTemplate buildTemplate() throws RenderingException {
+                return new CustomFortunesTemplate(this);
+            }
+        }.fortunes(fortunes)
+                .render(HtmlUtf8BufferRockerOutput.threadLocalFactory())
+                .buffer();
     }
 
 }
--- frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/PlaintextResource.java
+++ frameworks/Java/quarkus/resteasy-reactive-hibernate/src/main/java/io/quarkus/benchmark/resource/PlaintextResource.java
@@ -1,7 +1,5 @@
 package io.quarkus.benchmark.resource;
 
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.ByteBufAllocator;
 import io.smallrye.common.annotation.NonBlocking;
 import io.vertx.core.buffer.Buffer;
 
@@ -13,14 +11,9 @@ import java.nio.charset.StandardCharsets;
 
 @Path("/plaintext")
 public class PlaintextResource {
-    private static final String HELLO_WORLD = "Hello, world!";
-    private static final Buffer HELLO_WORLD_BUFFER;
 
-    static {
-        ByteBuf nettyBuffer = ByteBufAllocator.DEFAULT.directBuffer();
-        nettyBuffer.writeBytes(HELLO_WORLD.getBytes(StandardCharsets.UTF_8));
-        HELLO_WORLD_BUFFER = Buffer.buffer(nettyBuffer);
-    }
+    // We prefer an heap Buffer, because Resteasy-Reactive would perform Buffer::getBytes on it
+    private static final Buffer HELLO_WORLD_BUFFER = Buffer.buffer("Hello, world!".getBytes(StandardCharsets.UTF_8));
 
     @Produces(MediaType.TEXT_PLAIN)
     @GET
