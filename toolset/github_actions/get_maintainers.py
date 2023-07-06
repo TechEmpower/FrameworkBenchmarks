774277d479d135a5226c6264f13c4c526fe670f9
ecruz-te <ecruz@techempower.com>
2023-07-05 17:24:22 -0700

    update description

--- toolset/github_actions/get_maintainers.py
+++ toolset/github_actions/get_maintainers.py
@@ -6,8 +6,6 @@
 # @description: This script is only for use within Github Actions. It is meant
 # to get a list of maintainers to ping for a PR whenever their framework
 # is updated.
-# This script is meant to be used in conjunction with the github_actions_diff.py
-# to determine which frameworks have been updated.
 
 import os
 import json
