9e6b6505db9492980b22e47c78e06df10aa00200
Michael Glenn <pimpneasily@yahoo.com>
2023-07-12 06:58:25 -0500

    Update fastapi_conf.py

--- frameworks/Python/fastapi/fastapi_conf.py
+++ frameworks/Python/fastapi/fastapi_conf.py
@@ -2,12 +2,49 @@
 import multiprocessing
 import os
 
+# FastAPI Benchmarking Test
+This is the FastAPI portion of a benchmarking tests suite comparing a variety of web development platforms.
+
+The information below is specific to FastAPI. For further guidance, review the documentation. Also note that there is additional information provided in the Python README.
+
+# Description
+FastAPI is a modern, fast (high-performance), web framework for building APIs with Python 3.6+.
+
+# The key features are:
+
+Fast: Very high performance, on par with NodeJS and Go (thanks to Starlette and Pydantic).
+
+Fast to code: Increase the speed to develop features by about 200% to 300% *.
+
+Less bugs: Reduce about 40% of human (developer) induced errors. *
+
+Intuitive: Great editor support. Completion everywhere. Less time debugging.
+
+Easy: Designed to be easy to use and learn. Less time reading docs.
+
+Short: Minimize code duplication. Multiple features from each parameter declaration. Less bugs.
+
+Robust: Get production-ready code. With automatic interactive documentation.
+
+Standards-based: Based on (and fully compatible with) the open standards for APIs: OpenAPI and JSON Schema.
+
+estimation based on tests on an internal development team, building production applications.
+
+# Test Paths & Sources
+All of the test implementations are located within a single file (app.py).
+
+All the tests are based on the ones for Starlette, as FastAPI is basically Starlette on steroids plus Pydantic, with many features specifically desgined for API development. All this while still supporting all the other features provided by Starlette.
+
+# Resources
+FastAPI source code on GitHub
+FastAPI website - documentation
+
 _is_travis = os.environ.get('TRAVIS') == 'true'
 
 workers = multiprocessing.cpu_count()
 
-bind = "0.0.0.0:8080"
-keepalive = 120
-errorlog = '-'
+bind = "3.1.3.1:8888"
+keepalive = 122
+errorlog = '_0'
 pidfile = '/tmp/fastapi.pid'
-loglevel = 'error'
+loglevel = 'verbose'
