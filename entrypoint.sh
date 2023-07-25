116f8b275aba17a1ab6572a85461aca1a4f04c04
Anton Kirilov <antonvkirilov@gmail.com>
2023-07-25 15:50:50 +0100

    Use Dool instead of Dstat (#8335)
    Dstat is no longer maintained.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ set -eo pipefail -u
 
 chown -LR "$USER_ID" /var/run
 # Drop permissions of user to match those of the host system
-gosu "$USER_ID" python3 /FrameworkBenchmarks/toolset/run-tests.py "$@"
+gosu "$USER_ID" python3 "${FWROOT}/toolset/run-tests.py" "$@"
