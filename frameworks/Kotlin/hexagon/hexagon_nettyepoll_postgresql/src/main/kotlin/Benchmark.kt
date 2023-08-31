10d635c350417c23d3f6344b124a652b3e6f2669
jaguililla <juanjoaguililla@gmail.com>
2023-08-31 18:44:25 +0200

    Update dependencies

--- frameworks/Kotlin/hexagon/hexagon_nettyepoll_postgresql/src/main/kotlin/Benchmark.kt
+++ frameworks/Kotlin/hexagon/hexagon_nettyepoll_postgresql/src/main/kotlin/Benchmark.kt
@@ -1,11 +1,11 @@
 package com.hexagonkt
 
+import com.hexagonkt.core.urlOf
 import com.hexagonkt.http.server.netty.epoll.NettyEpollServerAdapter
 import com.hexagonkt.store.BenchmarkSqlStore
 import com.hexagonkt.templates.rocker.RockerAdapter
 import io.netty.util.ResourceLeakDetector
 import io.netty.util.ResourceLeakDetector.Level.DISABLED
-import java.net.URL
 
 fun main() {
     ResourceLeakDetector.setLevel(DISABLED)
@@ -16,7 +16,7 @@ fun main() {
     val settings = Settings()
     val store = BenchmarkSqlStore("postgresql")
     val templateEngine = RockerAdapter()
-    val templateUrl = URL("classpath:fortunes.rocker.html")
+    val templateUrl = urlOf("classpath:fortunes.rocker.html")
     val engine = NettyEpollServerAdapter()
 
     val benchmark = Benchmark(engine, store, templateEngine, templateUrl, settings)
