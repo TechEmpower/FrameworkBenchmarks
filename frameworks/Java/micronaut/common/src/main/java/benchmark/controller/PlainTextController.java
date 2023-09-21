3132d1488529efd1036dd2a3ef37f5a1d758a537
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-09-21 11:05:25 +0200

    Correct

--- frameworks/Java/micronaut/common/src/main/java/benchmark/controller/PlainTextController.java
+++ frameworks/Java/micronaut/common/src/main/java/benchmark/controller/PlainTextController.java
@@ -3,18 +3,16 @@ package benchmark.controller;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
 
 import java.nio.charset.StandardCharsets;
 
 @Controller("/plaintext")
 public class PlainTextController {
 
-    private static final ByteBuf TEXT = Unpooled.unreleasableBuffer(Unpooled.copiedBuffer("Hello, World!".getBytes(StandardCharsets.UTF_8)));
+    private static final byte[] TEXT = "Hello, World!".getBytes(StandardCharsets.UTF_8);
 
     @Get(value = "/", produces = MediaType.TEXT_PLAIN)
-    public ByteBuf getPlainText() {
+    public byte[] getPlainText() {
         return TEXT;
     }
 }
