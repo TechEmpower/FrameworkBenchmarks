0e1b591cc4044dc09d0b075c14f182f5f4122ad6
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-09-21 08:37:47 +0200

    Improve plain text response + bump version

--- frameworks/Java/micronaut/common/src/main/java/benchmark/controller/PlainTextController.java
+++ frameworks/Java/micronaut/common/src/main/java/benchmark/controller/PlainTextController.java
@@ -3,14 +3,18 @@ package benchmark.controller;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+
+import java.nio.charset.StandardCharsets;
 
 @Controller("/plaintext")
 public class PlainTextController {
 
-    private static final String TEXT = "Hello, World!";
+    private static final ByteBuf TEXT = Unpooled.copiedBuffer("Hello, World!".getBytes(StandardCharsets.UTF_8));
 
     @Get(value = "/", produces = MediaType.TEXT_PLAIN)
-    public String getPlainText() {
+    public ByteBuf getPlainText() {
         return TEXT;
     }
 }
