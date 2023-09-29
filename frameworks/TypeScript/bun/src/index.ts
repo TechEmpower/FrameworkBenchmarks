9c5800eaa78ee4fd4aab46953dac34ae3aec143b
Imam Fahrur Rofi <92277627+masfahru@users.noreply.github.com>
2023-09-29 23:08:38 +0700

    [Bun] use Bun.spawn to increase performance (#8454)
    * [Bun] use Bun.spawn to increase performance
    
    * [Bun] Simple way to get pathname

--- frameworks/TypeScript/bun/src/index.ts
+++ frameworks/TypeScript/bun/src/index.ts
@@ -3,8 +3,9 @@ const options: ResponseInit = { headers: { "Server": "Bun" } };
 
 const server = Bun.serve({
   port: 8080,
+  reusePort: true,
   fetch(req: Request) {
-    const { pathname } = new URL(req.url);
+    const pathname = req.url.slice(req.url.indexOf("/", 8));
 
     if (pathname === "/json") {
       return Response.json({ message: HELLO_WORLD_STR }, options);
