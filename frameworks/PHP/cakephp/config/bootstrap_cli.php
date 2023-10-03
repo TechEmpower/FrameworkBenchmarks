023286f90b3fc55645c07eda5572ef5c70ffa05d
Joanhey <joanhey@kumbiaphp.com>
2023-10-03 17:29:56 +0200

    Add extra files

---
+++ frameworks/PHP/cakephp/config/bootstrap_cli.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+
+use Cake\Core\Configure;
+
+/*
+ * Additional bootstrapping and configuration for CLI environments should
+ * be put here.
+ */
+
+// Set the fullBaseUrl to allow URLs to be generated in shell tasks.
+// This is useful when sending email from shells.
+//Configure::write('App.fullBaseUrl', php_uname('n'));
+
+// Set logs to different files so they don't have permission conflicts.
+if (Configure::check('Log.debug')) {
+    Configure::write('Log.debug.file', 'cli-debug');
+}
+if (Configure::check('Log.error')) {
+    Configure::write('Log.error.file', 'cli-error');
+}
