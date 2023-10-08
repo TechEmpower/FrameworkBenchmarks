c1dd453bb4d2093f23d0e5fe0b0e60ab1475e2d6
KostyaTretyak <ktretiak.in.ua@gmail.com>
2023-10-08 10:46:22 +0300

    feat(ditsmod): added db tests with simplifieduse of DI.

--- frameworks/TypeScript/ditsmod/src/app/modules/routed/simple/simple.module.ts
+++ frameworks/TypeScript/ditsmod/src/app/modules/routed/simple/simple.module.ts
@@ -6,9 +6,18 @@ import { WithoutDbController } from './without-db.controller.js';
 import { DbController } from './db.controller.js';
 import { FortuneController } from './fortune.controller.js';
 import { SingletonController } from './singleton.controller.js';
+import { DbController2 } from './db.controller2.js';
+import { FortuneController2 } from './fortune.controller2.js';
 
 @featureModule({
   imports: [RoutingModule, DbModule],
-  controllers: [WithoutDbController, DbController, FortuneController, SingletonController],
+  controllers: [
+    WithoutDbController,
+    DbController,
+    DbController2,
+    FortuneController,
+    FortuneController2,
+    SingletonController,
+  ],
 })
 export class SimpleModule {}
