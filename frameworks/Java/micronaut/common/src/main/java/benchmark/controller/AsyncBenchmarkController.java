c4a1f769dc7ab6a36f2fd1d86340c75bd3fd2725
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-10-24 16:03:00 +0200

    Improve Vertx code. Bump Gradle, Micronaut platform (#8478)
    * Improve Vertx code. Bump Gradle, Micronaut platform
    
    * Correct
    
    * Switch to a record
    
    * Update frameworks/Java/micronaut/gradle.properties
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>
    
    * Trigger build
    
    ---------
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

--- frameworks/Java/micronaut/common/src/main/java/benchmark/controller/AsyncBenchmarkController.java
+++ frameworks/Java/micronaut/common/src/main/java/benchmark/controller/AsyncBenchmarkController.java
@@ -2,54 +2,49 @@ package benchmark.controller;
 
 import benchmark.model.Fortune;
 import benchmark.model.World;
-import benchmark.repository.ReactiveFortuneRepository;
-import benchmark.repository.ReactiveWorldRepository;
+import benchmark.repository.AsyncFortuneRepository;
+import benchmark.repository.AsyncWorldRepository;
 import io.micronaut.context.annotation.Requires;
-import io.micronaut.core.async.annotation.SingleResult;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
-import org.reactivestreams.Publisher;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import views.fortunes;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.concurrent.CompletionStage;
 
 import static java.util.Comparator.comparing;
 
-@Requires(beans = {ReactiveWorldRepository.class, ReactiveFortuneRepository.class})
+@Requires(beans = {AsyncWorldRepository.class, AsyncFortuneRepository.class})
 @Controller
-public class ReactiveBenchmarkController extends AbstractBenchmarkController {
+public class AsyncBenchmarkController extends AbstractBenchmarkController {
 
-    private final ReactiveWorldRepository worldRepository;
-    private final ReactiveFortuneRepository fortuneRepository;
+    private final AsyncWorldRepository worldRepository;
+    private final AsyncFortuneRepository fortuneRepository;
 
-    public ReactiveBenchmarkController(ReactiveWorldRepository worldRepository,
-                                       ReactiveFortuneRepository fortuneRepository) {
+    public AsyncBenchmarkController(AsyncWorldRepository worldRepository,
+                                    AsyncFortuneRepository fortuneRepository) {
         this.worldRepository = worldRepository;
         this.fortuneRepository = fortuneRepository;
     }
 
     @Get("/prepare-data-for-test")
-    public Mono<Void> prepareDataForTest() {
-        return Mono.from(worldRepository.initDb(createWords())).then(Mono.from(fortuneRepository.initDb(createFortunes())));
+    public CompletionStage<?> prepareDataForTest() {
+        return worldRepository.initDb(createWords()).thenCompose(ignore -> fortuneRepository.initDb(createFortunes()));
     }
 
     // https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#single-database-query
     @Get("/db")
-    @SingleResult
-    public Publisher<World> db() {
+    public CompletionStage<World> db() {
         return worldRepository.findById(randomId());
     }
 
     // https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#multiple-database-queries
     @Get("/queries")
-    @SingleResult
-    public Publisher<List<World>> queries(@QueryValue String queries) {
+    public CompletionStage<List<World>> queries(@QueryValue String queries) {
         int count = parseQueryCount(queries);
         List<Integer> ids = new ArrayList<>(count);
         for (int i = 0; i < count; i++) {
@@ -60,9 +55,8 @@ public class ReactiveBenchmarkController extends AbstractBenchmarkController {
 
     // https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#fortunes
     @Get(value = "/fortunes", produces = "text/html;charset=utf-8")
-    @SingleResult
-    public Mono<HttpResponse<String>> fortune() {
-        return Mono.from(fortuneRepository.findAll()).map(fortuneList -> {
+    public CompletionStage<HttpResponse<String>> fortune() {
+        return fortuneRepository.findAll().thenApply(fortuneList -> {
             List<Fortune> all = new ArrayList<>(fortuneList.size() + 1);
             all.add(new Fortune(0, "Additional fortune added at request time."));
             all.addAll(fortuneList);
@@ -74,14 +68,13 @@ public class ReactiveBenchmarkController extends AbstractBenchmarkController {
 
     // https://github.com/TechEmpower/FrameworkBenchmarks/wiki/Project-Information-Framework-Tests-Overview#database-updates
     @Get("/updates")
-    @SingleResult
-    public Publisher<List<World>> updates(@QueryValue String queries) {
-        return Flux.from(queries(queries)).flatMap(worlds -> {
+    public CompletionStage<List<World>> updates(@QueryValue String queries) {
+        return queries(queries).thenCompose(worlds -> {
             for (World world : worlds) {
                 world.setRandomNumber(randomWorldNumber());
             }
             worlds.sort(Comparator.comparingInt(World::getId)); // Avoid deadlock
-            return Mono.from(worldRepository.updateAll(worlds)).thenReturn(worlds);
+            return worldRepository.updateAll(worlds).thenApply(ignore -> worlds);
         });
     }
 
