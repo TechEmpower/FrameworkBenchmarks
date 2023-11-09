e525febafbd53543dc63f10575568b3c87d88a69
David Denton <denton.david@gmail.com>
2023-11-09 19:10:31 +0000

    upgrade kotlin and http4k

--- frameworks/Kotlin/http4k/build.gradle.kts
+++ frameworks/Kotlin/http4k/build.gradle.kts
@@ -3,7 +3,7 @@ import org.gradle.api.JavaVersion.*
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.9.10"
+    kotlin("jvm") version "1.9.20"
     application
 }
 
