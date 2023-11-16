72db1be788df213898015ecf4c1f7eea6554ebba
Luís Cobucci <lcobucci@gmail.com>
2023-11-16 22:20:44 +0100

    [php] Update ReactPHP usage
    This makes us rely on the automatic execution of the Loop and removes
    usage of deprecated classes.
    
    Signed-off-by: Luís Cobucci <lcobucci@gmail.com>

--- frameworks/PHP/reactphp/server.php
+++ frameworks/PHP/reactphp/server.php
@@ -1,20 +1,18 @@
 <?php
 
+use React\EventLoop\Loop;
+use React\Http\HttpServer;
+use React\Socket\SocketServer;
+
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__.'/app.php';
 
 init();
 
-$loop = React\EventLoop\Loop::get();
-
-$server = new React\Http\Server($loop, function (Psr\Http\Message\ServerRequestInterface $request) {
-    return router($request);
-});
-
-$socket = new React\Socket\Server('0.0.0.0:8080', $loop);
+$server = new HttpServer(router(...));
+$socket = new SocketServer('0.0.0.0:8080');
 $server->listen($socket);
 
 echo "React Server running at http://0.0.0.0:8080\n";
-echo "EventLoop: ", $loop::class, "\n";
+echo "EventLoop: ", Loop::get()::class, "\n";
 
-$loop->run();
