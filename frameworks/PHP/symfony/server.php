0b8566bce4f457050842cb6004ceca7472aab837
Joan Miquel <joanhey@kumbiaphp.com>
2023-11-29 22:50:10 +0100

    [php] Symfony update to PHP  8.3 (#8583)

--- frameworks/PHP/symfony/server.php
+++ frameworks/PHP/symfony/server.php
@@ -12,7 +12,7 @@ $http_worker                = new Worker('http://0.0.0.0:8080');
 $http_worker->count         = (int) shell_exec('nproc') * 4;
 $http_worker->name          = 'AdapterMan-Symfony';
 
-$http_worker->onWorkerStart = function () {
+$http_worker->onWorkerStart = static function () {
     Header::$date = gmdate('D, d M Y H:i:s').' GMT';
     Timer::add(1, function() {
          Header::$date = gmdate('D, d M Y H:i:s').' GMT';
