8209f2c96519c20246dc73202f38f9cd8a10e2d9
Dragos Varovici <dvarovici.work@gmail.com>
2023-12-05 12:03:37 -0700

    Amber database fix v2

--- frameworks/Crystal/amber/config/initializers/database.cr
+++ frameworks/Crystal/amber/config/initializers/database.cr
@@ -1,7 +1,7 @@
 require "granite/adapter/pg"
 
 cpu_count = System.cpu_count
-pool_size = 112 // cpu_count
+pool_size = 56 // cpu_count
 database_url = ENV["DATABASE_URL"]
 url = "#{database_url}?initial_pool_size=#{pool_size}&max_idle_pool_size=#{pool_size}"
 
