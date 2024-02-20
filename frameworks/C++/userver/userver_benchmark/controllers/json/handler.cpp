366cbcd234d510791a8b03286202fc01c7e14f92
itrofimow <i.trofimow@yandex.ru>
2024-02-21 00:05:29 +0400

    [C++] [userver] Bump userver commit, switch to SAX-serialization, tune postgres a bit (#8737)

--- frameworks/C++/userver/userver_benchmark/controllers/json/handler.cpp
+++ frameworks/C++/userver/userver_benchmark/controllers/json/handler.cpp
@@ -1,16 +1,25 @@
 #include "handler.hpp"
 
+#include <userver/formats/json/string_builder.hpp>
+#include <userver/http/common_headers.hpp>
+
 namespace userver_techempower::json {
 
-userver::formats::json::Value Handler::HandleRequestJsonThrow(
-    const userver::server::http::HttpRequest&,
-    const userver::formats::json::Value&,
+std::string Handler::HandleRequestThrow(
+    const userver::server::http::HttpRequest& request,
     userver::server::request::RequestContext&) const {
+  request.GetHttpResponse().SetHeader(userver::http::headers::kContentType,
+                                      "application/json");
   return GetResponse();
 }
 
-userver::formats::json::Value Handler::GetResponse() {
-  return userver::formats::json::MakeObject("message", "Hello, World!");
+std::string Handler::GetResponse() {
+  const auto json =
+      userver::formats::json::MakeObject("message", "Hello, World!");
+
+  userver::formats::json::StringBuilder sb{};
+  sb.WriteValue(json);
+  return sb.GetString();
 }
 
 }  // namespace userver_techempower::json
