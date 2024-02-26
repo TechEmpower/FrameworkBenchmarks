bff79d6c57bd668084a68b89014774c717d2dd8b
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-02-26 15:16:14 +0100

    Upgrade spring-webflux test to Spring Boot 3.3.1
    - Reactivate the test
    - Remove unmaintained variants to keep only r2dbc and mongo
    - Upgrade to Spring Boot 3.3.1
    - Upgrade to Java 21 with Liberica JDK
    - Dependency cleanup
    - Various related updates
    - Add JSON and textplain endpoints
    - Properly exclude autoconfigurations based on profiles
    - Use Spring Boot 3.3 executable JAR unpack feature
    - Add -Dreactor.netty.http.server.lastFlushWhenNoRead=true property

--- frameworks/Java/spring-webflux/src/main/java/benchmark/repository/MongoDbRepository.java
+++ frameworks/Java/spring-webflux/src/main/java/benchmark/repository/MongoDbRepository.java
@@ -5,7 +5,7 @@ import benchmark.model.World;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
+import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -18,22 +18,21 @@ import static org.springframework.data.mongodb.core.query.Update.update;
 @Component
 @Profile("mongo")
 public class MongoDbRepository implements DbRepository {
-    private final Logger log = LoggerFactory.getLogger(getClass());
-    private final ReactiveMongoTemplate mongoTemplate;
 
-    public MongoDbRepository(ReactiveMongoTemplate mongoTemplate) {
-        this.mongoTemplate = mongoTemplate;
+    private final ReactiveMongoOperations operations;
+
+    public MongoDbRepository(ReactiveMongoOperations operations) {
+        this.operations = operations;
     }
 
     @Override
     public Mono<World> getWorld(int id) {
-        log.debug("getWorld({})", id);
-        return mongoTemplate.findById(id, World.class);
+        return operations.findById(id, World.class);
     }
 
     @Override
     public Mono<World> findAndUpdateWorld(int id, int randomNumber) {
-        return mongoTemplate.findAndModify(
+        return operations.findAndModify(
                 query(where("id").is(id)),
                 update("randomNumber", randomNumber),
                 options().returnNew(true),
@@ -42,6 +41,6 @@ public class MongoDbRepository implements DbRepository {
 
     @Override
     public Flux<Fortune> fortunes() {
-        return mongoTemplate.findAll(Fortune.class);
+        return operations.findAll(Fortune.class);
     }
 }
