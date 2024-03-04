c823e8e8a76aae86155166ed90429da3ca9a9760
Sébastien Ros <sebastienros@gmail.com>
2024-03-04 10:22:45 -0800

    Add startup message for Spring

--- frameworks/Java/spring/src/main/java/hello/App.java
+++ frameworks/Java/spring/src/main/java/hello/App.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.EventListener;
 
 import com.zaxxer.hikari.HikariDataSource;
 
@@ -18,6 +20,11 @@ public class App {
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
+
+	@EventListener(ApplicationReadyEvent.class)
+    public void runAfterStartup() {
+        System.out.println("Application is ready");
+    }
 
 	@Bean
 	@Profile({ "jdbc", "jpa" })
