cc785c07d79de103f4a300031c3b2d2ad7d3e984
Joanhey <joanhey@kumbiaphp.com>
2024-04-27 12:31:55 +0200

    [php] Add Laravel Octnae with Frankenphp

--- frameworks/PHP/laravel/config/app.php
+++ frameworks/PHP/laravel/config/app.php
@@ -142,7 +142,7 @@ return [
 		// Illuminate\Redis\RedisServiceProvider::class,
 		// Illuminate\Auth\Passwords\PasswordResetServiceProvider::class,
 		// Illuminate\Session\SessionServiceProvider::class,
-		// Illuminate\Translation\TranslationServiceProvider::class,
+		Illuminate\Translation\TranslationServiceProvider::class,
 		// Illuminate\Validation\ValidationServiceProvider::class,
 		Illuminate\View\ViewServiceProvider::class,
 
