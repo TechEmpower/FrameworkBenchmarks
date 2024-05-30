c66f01a6074581f2185ac173a8f5c5ac3424bae1
pavel.mash <pavel.mash@inbase.com.ua>
2024-05-30 14:43:17 +0300

    [mORMot] - migrate to Ubuntu24.04; mORMot@2.2.7565; return to glibc MM

--- frameworks/Pascal/mormot/setup_and_build.sh
+++ frameworks/Pascal/mormot/setup_and_build.sh
@@ -35,7 +35,7 @@ echo "Download statics from $URL ..."
 wget -qO- "$URL" | tar -xz -C ./libs/mORMot/static
 
 # uncomment for fixed commit URL
-URL=https://github.com/synopse/mORMot2/tarball/527b3fb11cb4dad5f2c03ace293b550f85504420
+URL=https://github.com/synopse/mORMot2/tarball/d6b92b1f6b678d50449f870cbbe8ef4e1613c6b1
 #URL="https://api.github.com/repos/synopse/mORMot2/tarball/$USED_TAG"
 echo "Download and unpacking mORMot sources from $URL ..."
 wget -qO- "$URL" | tar -xz -C ./libs/mORMot  --strip-components=1
