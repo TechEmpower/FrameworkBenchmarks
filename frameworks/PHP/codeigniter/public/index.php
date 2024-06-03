31e4007d3606ec11142fd8094e7a5ee1c52935b1
Joanhey <joanhey@kumbiaphp.com>
2024-06-03 17:30:53 +0200

    Update index.php

--- frameworks/PHP/codeigniter/public/index.php
+++ frameworks/PHP/codeigniter/public/index.php
@@ -1,9 +1,17 @@
 <?php
+/*
+*---------------------------------------------------------------
+* SET THE CURRENT DIRECTORY
+*---------------------------------------------------------------
+*/
+
 // Path to the front controller (this file)
 define('FCPATH', __DIR__ . DIRECTORY_SEPARATOR);
 
 // Ensure the current directory is pointing to the front controller's directory
-chdir(FCPATH);
+if (getcwd() . DIRECTORY_SEPARATOR !== FCPATH) {
+   chdir(FCPATH);
+}
 
 /*
  *---------------------------------------------------------------
@@ -14,7 +22,7 @@ chdir(FCPATH);
  * and fires up an environment-specific bootstrapping.
  */
 
-const CI_ENVIRONMENT = "production";
+const ENVIRONMENT = "production";
 const CI_DEBUG = false;
 
 
@@ -25,52 +33,7 @@ require FCPATH . '../app/Config/Paths.php';
 
 $paths = new Config\Paths();
 
-// Location of the framework bootstrap file.
-require rtrim($paths->systemDirectory, '\\/ ') . DIRECTORY_SEPARATOR . 'bootstrap.php';
+// LOAD THE FRAMEWORK BOOTSTRAP FILE
+require $paths->systemDirectory . '/Boot.php';
 
-// Load environment settings from .env files into $_SERVER and $_ENV
-require_once SYSTEMPATH . 'Config/DotEnv.php';
-(new CodeIgniter\Config\DotEnv(ROOTPATH))->load();
-
-// Define ENVIRONMENT
-if (! defined('ENVIRONMENT')) {
-    define('ENVIRONMENT', env('CI_ENVIRONMENT', 'production'));
-}
-
-// Load Config Cache
-// $factoriesCache = new \CodeIgniter\Cache\FactoriesCache();
-// $factoriesCache->load('config');
-// ^^^ Uncomment these lines if you want to use Config Caching.
-
-/*
- * ---------------------------------------------------------------
- * GRAB OUR CODEIGNITER INSTANCE
- * ---------------------------------------------------------------
- *
- * The CodeIgniter class contains the core functionality to make
- * the application run, and does all the dirty work to get
- * the pieces all working together.
- */
-
-$app = Config\Services::codeigniter();
-$app->initialize();
-//$context = is_cli() ? 'php-cli' : 'web';
-$app->setContext('web');
-
-/*
- *---------------------------------------------------------------
- * LAUNCH THE APPLICATION
- *---------------------------------------------------------------
- * Now that everything is set up, it's time to actually fire
- * up the engines and make this app do its thang.
- */
-
-$app->run();
-
-// Save Config Cache
-// $factoriesCache->save('config');
-// ^^^ Uncomment this line if you want to use Config Caching.
-
-// Exits the application, setting the exit code for CLI-based applications
-// that might be watching.
-exit(EXIT_SUCCESS);
+exit(CodeIgniter\Boot::bootWeb($paths));
