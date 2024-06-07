TechEmpower/FrameworkBenchmarks

commit f2b0b607acb50c71890f00693bc2f467d217ba4f

Micronaut to 4.4.2 and Upgrades (#8778)
* Micronaut Upgrades

- Micronaut to 4.3.4
- Micronaut Gradle plugin to latest
- Unpin micronaut-data-mongodb
- Update Gradle to 8.6

* Update frameworks/Java/micronaut/gradle.properties

Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

* Apply suggestions from code review

Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

* Tie Graal docker images to 21

I don't think the Gradle we're using uses 22 yet

* Micronaut 4.4.1, Gradle 8.7, update dockerfiles for graal

* Gradle 8.7

* Update gradle plugin

* Try and trigger pipelines on my fork

* Update frameworks/Java/micronaut/gradle.properties

Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---------

Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

frameworks/Java/micronaut/micronaut-data-jdbc-graalvm.dockerfile
@@ -1,11 +1,10 @@
-FROM ghcr.io/graalvm/graalvm-community:latest as build
+FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
+RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew  --no-daemon
-RUN ./gradlew micronaut-data-jdbc:nativeBuild -x test --no-daemon
+RUN ./gradlew micronaut-data-jdbc:nativeCompile -x test --no-daemon
 
-FROM frolvlad/alpine-glibc:glibc-2.34
-RUN apk --no-cache update && apk add libstdc++
+FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-jdbc/build/native/nativeCompile/micronaut-data-jdbc micronaut
 

frameworks/Java/micronaut/micronaut-data-jdbc.dockerfile
@@ -1,4 +1,4 @@
-FROM gradle:8.1.0-jdk17 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle micronaut-data-jdbc:build -x test --no-daemon

frameworks/Java/micronaut/micronaut-data-mongodb-graalvm.dockerfile
@@ -1,11 +1,10 @@
-FROM ghcr.io/graalvm/graalvm-community:latest as build
+FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
+RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew  --no-daemon
-RUN ./gradlew micronaut-data-mongodb:nativeBuild -x test --no-daemon
+RUN ./gradlew micronaut-data-mongodb:nativeCompile -x test --no-daemon
 
-FROM frolvlad/alpine-glibc:glibc-2.34
-RUN apk --no-cache update && apk add libstdc++
+FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-mongodb/build/native/nativeCompile/micronaut-data-mongodb micronaut
 

frameworks/Java/micronaut/micronaut-data-mongodb.dockerfile
@@ -1,4 +1,4 @@
-FROM gradle:8.1.0-jdk17 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle micronaut-data-mongodb:build -x test --no-daemon

frameworks/Java/micronaut/micronaut-data-r2dbc-graalvm.dockerfile
@@ -1,11 +1,10 @@
-FROM ghcr.io/graalvm/graalvm-community:latest as build
+FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
+RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew  --no-daemon
-RUN ./gradlew micronaut-data-r2dbc:nativeBuild -x test --no-daemon
+RUN ./gradlew micronaut-data-r2dbc:nativeCompile -x test --no-daemon
 
-FROM frolvlad/alpine-glibc:glibc-2.34
-RUN apk --no-cache update && apk add libstdc++
+FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-r2dbc/build/native/nativeCompile/micronaut-data-r2dbc micronaut
 

frameworks/Java/micronaut/micronaut-data-r2dbc.dockerfile
@@ -1,4 +1,4 @@
-FROM gradle:8.1.0-jdk17 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
 RUN gradle micronaut-data-r2dbc:build -x test --no-daemon

frameworks/Java/micronaut/micronaut-graalvm.dockerfile
@@ -1,10 +1,10 @@
-FROM ghcr.io/graalvm/graalvm-community:latest as build
+FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
+RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew  --no-daemon
-RUN ./gradlew micronaut-vertx-pg-client:nativeBuild -x test --no-daemon
+RUN ./gradlew micronaut-vertx-pg-client:nativeCompile -x test --no-daemon
 
-FROM frolvlad/alpine-glibc:glibc-2.34
+FROM cgr.dev/chainguard/wolfi-base:latest
 RUN apk --no-cache update && apk add libstdc++
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-vertx-pg-client/build/native/nativeCompile/micronaut-vertx-pg-client micronaut

frameworks/Java/micronaut/micronaut-jdbc-graalvm.dockerfile
@@ -1,11 +1,10 @@
-FROM ghcr.io/graalvm/graalvm-community:latest as build
+FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
+RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew  --no-daemon
-RUN ./gradlew micronaut-jdbc:nativeBuild -x test --no-daemon
+RUN ./gradlew micronaut-jdbc:nativeCompile -x test --no-daemon
 
-FROM frolvlad/alpine-glibc:glibc-2.34
-RUN apk --no-cache update && apk add libstdc++
+FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-jdbc/build/native/nativeCompile/micronaut-jdbc micronaut
 

commit e50dde3cddd740c5e04c6e9bb1ec8685bfb865d0

Disable testresources and downgrade docker images (#9084)
This is an attempt to fix the JIT-only regression introduced by f2b0b607acb50c71890f00693bc2f467d217ba4f .

frameworks/Java/micronaut/micronaut-data-jdbc-graalvm.dockerfile
@@ -2,7 +2,7 @@ FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
 RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew micronaut-data-jdbc:nativeCompile -x test --no-daemon
+RUN ./gradlew micronaut-data-jdbc:nativeCompile -x test -x internalStartTestResourcesService --no-daemon
 
 FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut

frameworks/Java/micronaut/micronaut-data-jdbc.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-data-jdbc:build -x test --no-daemon
+RUN gradle micronaut-data-jdbc:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-jdbc/build/libs/micronaut-data-jdbc-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh

frameworks/Java/micronaut/micronaut-data-mongodb-graalvm.dockerfile
@@ -2,7 +2,7 @@ FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
 RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew micronaut-data-mongodb:nativeCompile -x test --no-daemon
+RUN ./gradlew micronaut-data-mongodb:nativeCompile -x test -x internalStartTestResourcesService --no-daemon
 
 FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut

frameworks/Java/micronaut/micronaut-data-mongodb.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-data-mongodb:build -x test --no-daemon
+RUN gradle micronaut-data-mongodb:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-mongodb/build/libs/micronaut-data-mongodb-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh

frameworks/Java/micronaut/micronaut-data-r2dbc-graalvm.dockerfile
@@ -2,7 +2,7 @@ FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
 RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew micronaut-data-r2dbc:nativeCompile -x test --no-daemon
+RUN ./gradlew micronaut-data-r2dbc:nativeCompile -x test -x internalStartTestResourcesService --no-daemon
 
 FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut

frameworks/Java/micronaut/micronaut-data-r2dbc.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-data-r2dbc:build -x test --no-daemon
+RUN gradle micronaut-data-r2dbc:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-data-r2dbc/build/libs/micronaut-data-r2dbc-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh

frameworks/Java/micronaut/micronaut-graalvm.dockerfile
@@ -2,7 +2,7 @@ FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
 RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew micronaut-vertx-pg-client:nativeCompile -x test --no-daemon
+RUN ./gradlew micronaut-vertx-pg-client:nativeCompile -x test -x internalStartTestResourcesService --no-daemon
 
 FROM cgr.dev/chainguard/wolfi-base:latest
 RUN apk --no-cache update && apk add libstdc++

frameworks/Java/micronaut/micronaut-jdbc-graalvm.dockerfile
@@ -2,7 +2,7 @@ FROM ghcr.io/graalvm/native-image-community:21-ol9 as build
 RUN microdnf install findutils # Gradle 8.7 requires xargs
 COPY . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN ./gradlew micronaut-jdbc:nativeCompile -x test --no-daemon
+RUN ./gradlew micronaut-jdbc:nativeCompile -x test -x internalStartTestResourcesService --no-daemon
 
 FROM cgr.dev/chainguard/wolfi-base:latest
 WORKDIR /micronaut

frameworks/Java/micronaut/micronaut-jdbc.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-jdbc:build -x test --no-daemon
+RUN gradle micronaut-jdbc:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-jdbc/build/libs/micronaut-jdbc-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh

frameworks/Java/micronaut/micronaut-r2dbc.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-r2dbc:build -x test --no-daemon
+RUN gradle micronaut-r2dbc:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-r2dbc/build/libs/micronaut-r2dbc-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh

frameworks/Java/micronaut/micronaut.dockerfile
@@ -1,9 +1,9 @@
-FROM gradle:8.7.0-jdk21 as build
+FROM gradle:8.7.0-jdk17 as build
 COPY --chown=gradle:gradle . /home/gradle/src
 WORKDIR /home/gradle/src
-RUN gradle micronaut-vertx-pg-client:build -x test --no-daemon
+RUN gradle micronaut-vertx-pg-client:build -x test -x internalStartTestResourcesService --no-daemon
 
-FROM openjdk:22
+FROM openjdk:21
 WORKDIR /micronaut
 COPY --from=build /home/gradle/src/micronaut-vertx-pg-client/build/libs/micronaut-vertx-pg-client-all.jar micronaut.jar
 COPY run_benchmark.sh run_benchmark.sh