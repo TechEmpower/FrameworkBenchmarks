0e1316c3ce7d9b5522b282a97c08aa6ed1c4c079
Chrislearn Young <chris@acroidea.com>
2024-06-10 07:19:45 +0800

    cargo fmt

--- frameworks/Rust/salvo/src/main.rs
+++ frameworks/Rust/salvo/src/main.rs
@@ -1,15 +1,21 @@
-// #[global_allocator]
-// static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use std::sync::Arc;
+use std::thread::available_parallelism;
 
 use bytes::Bytes;
+use salvo::conn::tcp::TcpAcceptor;
 use salvo::http::body::ResBody;
 use salvo::http::header::{self, HeaderValue};
 use salvo::prelude::*;
 use serde::Serialize;
 
 mod utils;
+
+pub const HEADER_SERVER: HeaderValue = HeaderValue::from_static("salvo");
+pub const HEADER_JSON: HeaderValue = HeaderValue::from_static("application/json");
+pub const HEADER_TEXT: HeaderValue = HeaderValue::from_static("text/plain");
 
 #[derive(Serialize)]
 pub struct Message {
@@ -19,8 +25,8 @@ pub struct Message {
 #[handler]
 fn json(res: &mut Response) {
     let headers = res.headers_mut();
-    headers.insert(header::SERVER, HeaderValue::from_static("salvo"));
-    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("application/json"));
+    headers.insert(header::SERVER, HEADER_SERVER.clone());
+    headers.insert(header::CONTENT_TYPE,HEADER_JSON.clone());
     let data = serde_json::to_vec(&Message {
         message: "Hello, World!",
     })
@@ -31,12 +37,11 @@ fn json(res: &mut Response) {
 #[handler]
 fn plaintext(res: &mut Response) {
     let headers = res.headers_mut();
-    headers.insert(header::SERVER, HeaderValue::from_static("salvo"));
-    headers.insert(header::CONTENT_TYPE, HeaderValue::from_static("text/plain"));
+    headers.insert(header::SERVER, HEADER_SERVER.clone());
+    headers.insert(header::CONTENT_TYPE,HEADER_TEXT.clone());
     res.body(ResBody::Once(Bytes::from_static(b"Hello, world!")));
 }
-#[tokio::main]
-async 
+
 fn main() {
     let router = Arc::new(
         Router::new()
@@ -44,13 +49,28 @@ fn main() {
             .push(Router::with_path("json").get(json)),
     );
 
+    let thread_count = available_parallelism().map(|n| n.get()).unwrap_or(16);
+    let rt = tokio::runtime::Builder::new_current_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    for _ in 1..thread_count {
+        let router = router.clone();
+        std::thread::spawn(move || {
+            let rt = tokio::runtime::Builder::new_current_thread()
+                .enable_all()
+                .build()
+                .unwrap();
+            rt.block_on(serve(router));
+        });
+    }
     println!("Started http server: 127.0.0.1:8080");
-    serve(router).await;
+    rt.block_on(serve(router));
 }
 
 async fn serve(router: Arc<Router>) {
     // let acceptor: TcpAcceptor = utils::reuse_listener().unwrap().try_into().unwrap();
-    let acceptor = TcpListener::new("0.0.0.0:8080").bind().await;
+    let acceptor: TcpAcceptor = utils::reuse_listener().unwrap().try_into().unwrap();
     let mut server = Server::new(acceptor);
     let http1 = server.http1_mut();
     http1.pipeline_flush(true);
