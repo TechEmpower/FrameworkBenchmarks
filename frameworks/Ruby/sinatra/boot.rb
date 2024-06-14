9c19af78b79fe37de52a6bed3aaa9eccdef58cee
Petrik <petrik@deheus.net>
2024-06-14 21:12:06 +0200

    [ruby|sinatra] Use batch update on postgres

--- frameworks/Ruby/sinatra/boot.rb
+++ frameworks/Ruby/sinatra/boot.rb
@@ -58,8 +58,18 @@ connect ENV.fetch('DBTYPE').to_sym
 class World < ActiveRecord::Base
   self.table_name = name
 
-  alias_attribute(:randomnumber, :randomNumber) \
-    if connection.adapter_name.downcase.start_with?('mysql')
+  alias_attribute(:randomNumber, :randomnumber) \
+    if connection.adapter_name.downcase.start_with?('postgres')
+
+  if connection.adapter_name.downcase.start_with?('mysql')
+    def self.upsert_all(attributes, on_duplicate: :update, update_only: nil, returning: nil, unique_by: nil, record_timestamps: nil)
+      # On MySQL Batch updates verification isn't supported yet by TechEmpower.
+      # https://github.com/TechEmpower/FrameworkBenchmarks/issues/5983
+      attributes.each do |attrs|
+        where(id: attrs[:id]).update_all(randomNumber: attrs[:randomNumber])
+      end
+    end
+  end
 end
 
 class Fortune < ActiveRecord::Base
