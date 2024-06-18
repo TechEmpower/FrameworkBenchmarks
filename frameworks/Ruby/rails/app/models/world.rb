b835f28ff8bfcc629c62fee2e2918d19be36d737
Petrik <petrik@deheus.net>
2024-06-18 13:59:13 +0200

    [rails] Don't bulk update on MySQL
    The benchmarks currently don't take into account batch updates in MySQL.
    This fixes the flaky MySQL updates verification.

--- frameworks/Ruby/rails/app/models/world.rb
+++ frameworks/Ruby/rails/app/models/world.rb
@@ -3,4 +3,14 @@ class World < ApplicationRecord
 
   alias_attribute(:randomNumber, :randomnumber) \
     if connection.adapter_name.downcase.start_with?('postgres')
+
+  if connection.adapter_name.downcase.start_with?('trilogy')
+    def self.upsert_all(attributes, on_duplicate: :update, update_only: nil, returning: nil, unique_by: nil, record_timestamps: nil)
+      # On MySQL Batch updates verification isn't supported yet by TechEmpower.
+      # https://github.com/TechEmpower/FrameworkBenchmarks/issues/5983
+      attributes.each do |attrs|
+        where(id: attrs[:id]).update_all(randomNumber: attrs[:randomNumber])
+      end
+    end
+  end
 end
