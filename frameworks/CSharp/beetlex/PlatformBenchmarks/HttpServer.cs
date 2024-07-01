3c91fd5b639be1383bdeef90ed69005a72b87d2a
Henry <henryfan@msn.com>
2024-07-02 01:50:14 +0800

    beetlex update  RawDb (#9117)
    * update beetleX to BeetleX.Light
    
    * update beetlex to beetlex.light
    
    * fix fortunes error
    
    * fix error
    
    * remove beetlex-core-updb
    
    * update
    
    * update benchmark_config
    
    * update
    
    * update
    
    * update
    
    * update RawDb
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update dbraw

--- frameworks/CSharp/beetlex/PlatformBenchmarks/HttpServer.cs
+++ frameworks/CSharp/beetlex/PlatformBenchmarks/HttpServer.cs
@@ -13,13 +13,19 @@ namespace PlatformBenchmarks
     {
         private static NetServer<HttpNetApplication, HttpHandler> _apiServer;
 
-        public virtual Task StartAsync(CancellationToken cancellationToken)
+        public static string _connectionString;
+
+        public virtual async Task StartAsync(CancellationToken cancellationToken)
         {
+            _connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;SSL Mode=Disable;Maximum Pool Size=16;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Buffer Threshold Bytes=1000";
+            //_connectionString = "Server=localhost;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;SSL Mode=Disable;Maximum Pool Size=16;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Buffer Threshold Bytes=1000";
             ThreadPool.SetMinThreads(Environment.ProcessorCount * 2, Environment.ProcessorCount * 2);
-            Constants.MemorySegmentMinSize = 1024 * 8;
-            Constants.MemorySegmentMaxSize = 1024 * 8;
+            Constants.MemorySegmentMinSize = 1024 * 16;
+            Constants.MemorySegmentMaxSize = 1024 * 16;
             Constants.InitMemoryBlock();
-            ArraySegment<byte> date = GMTDate.Default.DATE;
+            var date = GMTDate.Default.DATE;
+            HttpHandler.DB = new RawDb(new ConcurrentRandom(), HttpServer._connectionString);
+            await HttpHandler.DB.PopulateCache();
             _apiServer = new NetServer<HttpNetApplication, HttpHandler>();
             _apiServer.Options.LogLevel = BeetleX.Light.Logs.LogLevel.Error;
             _apiServer.Options.AddLogOutputHandler<LogOutputToConsole>();
@@ -29,20 +35,6 @@ namespace PlatformBenchmarks
             });
             _apiServer.Start();
 
-
-            if (!Program.UpDB)
-            {
-                RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;SSL Mode=Disable;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Buffer Threshold Bytes=1000";
-                //RawDb._connectionString = "Server=127.0.0.1;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
-            }
-            else
-            {
-
-                 RawDb._connectionString = "Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;SSL Mode=Disable;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4;Multiplexing=true;Write Coalescing Buffer Threshold Bytes=1000";
-                //RawDb._connectionString = "Server=127.0.0.1;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=64;NoResetOnClose=true;Enlist=false;Max Auto Prepare=3";
-            }
-            // ApiServer.Log(LogType.Info, null, $"Debug mode [{Program.Debug}]");
-            return Task.CompletedTask;
 
         }
 
