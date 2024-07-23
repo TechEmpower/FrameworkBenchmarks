4d9a9e945d41da5c774187c88d543de519ff86e5
LLT21 <>
2024-07-23 20:46:37 +0200

    mac dylibs

--- frameworks/CSharp/appmpower/src/appMpower/NativeMethods.cs
+++ frameworks/CSharp/appmpower/src/appMpower/NativeMethods.cs
@@ -3,15 +3,14 @@ using System.Runtime.InteropServices;
 public unsafe partial class NativeMethods
 {
 #if DEBUG
-    [DllImport("nativeAOT.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+    [DllImport("nativeAOT.dylib", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
 #else
     [DllImport("nativeAOT.so", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
 #endif   
     public static extern char* HelloWorld(); 
 
-
 #if DEBUG
-    [DllImport("nativeAOT.dll", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
+    [DllImport("nativeAOT.dylib", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
 #else
     [DllImport("nativeAOT.so", CallingConvention = CallingConvention.Cdecl, CharSet = CharSet.Unicode)]
 #endif   
