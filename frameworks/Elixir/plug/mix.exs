f64609459609c0ad0c1f8fd6f4ae9f55a03b2d6b
Aaron Tavistock <aaron@geekshow.com>
2024-08-07 17:46:06 -0700

    Update plug libraries

--- frameworks/Elixir/plug/mix.exs
+++ frameworks/Elixir/plug/mix.exs
@@ -22,6 +22,7 @@ defmodule FrameworkBenchmarks.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:plug_cowboy, "~> 2.5"},
       {:jason, "~> 1.4"},
       {:ecto_sql, "~> 3.10"},
       {:postgrex, ">= 0.0.0"},
