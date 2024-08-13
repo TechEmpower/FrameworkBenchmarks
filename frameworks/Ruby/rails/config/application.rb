e05436e96c606be703cd75e52b8c5c192e3f0d7b
Petrik de Heus <petrik@deheus.net>
2024-08-13 21:43:37 +0200

    [rails] Upgrade to Rails 7.2 (#9209)
    Update to latest release.
    - Remove YJIT initializer as YJIT is enabled by Rails by default.
    - Updated config with bin/rails app:update

--- frameworks/Ruby/rails/config/application.rb
+++ frameworks/Ruby/rails/config/application.rb
@@ -8,9 +8,10 @@ require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 # require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -20,12 +21,12 @@ Bundler.require(*Rails.groups)
 module Hello
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.1
+    config.load_defaults 7.2
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
     config.action_dispatch.default_headers.merge!('Server' => 'WebServer')
 
