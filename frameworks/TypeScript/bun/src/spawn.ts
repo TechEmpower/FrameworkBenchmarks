8ab41808b2b18f1405278835664916e1aa007bb9
zoto_ff <zoto_ff@vk.com>
2024-08-14 03:01:28 +0300

    kill children on parent exit

--- frameworks/TypeScript/bun/src/spawn.ts
+++ frameworks/TypeScript/bun/src/spawn.ts
@@ -1,7 +1,18 @@
-const numCPUs = navigator.hardwareConcurrency;
-for (let i = 0; i < numCPUs; i++) {
-  Bun.spawn(["./server"], {
+const cpus = navigator.hardwareConcurrency;
+const buns = new Array(cpus);
+
+for (let i = 0; i < cpus; i++) {
+  buns[i] = Bun.spawn(["./server"], {
     stdio: ["inherit", "inherit", "inherit"],
     env: { ...process.env },
   });
 }
+
+function kill() {
+  for (const bun of buns) {
+    bun.kill();
+  }
+}
+
+process.on("SIGINT", kill);
+process.on("exit", kill);
