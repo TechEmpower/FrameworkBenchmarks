ee8aa7fc16eb588d166d3904208af7fe4d1ef1aa
saltyaom <saltyaom@gmail.com>
2024-09-05 18:57:14 +0700

    feat(elysia): use imperative for-loop instead of map to optimize performance

--- frameworks/TypeScript/elysia/src/postgres.ts
+++ frameworks/TypeScript/elysia/src/postgres.ts
@@ -16,11 +16,15 @@ export const find = (id: number) =>
     (arr) => arr[0],
   );
 
-export const bulkUpdate = (worlds: World[]) =>
-  sql`UPDATE world SET randomNumber = (update_data.randomNumber)::int
-  FROM (VALUES ${sql(
-    worlds
-      .map((world) => [world.id, world.randomNumber])
-      .sort((a, b) => (a[0] < b[0] ? -1 : 1)),
-  )}) AS update_data (id, randomNumber)
-  WHERE world.id = (update_data.id)::int`;
+export const bulkUpdate = (worlds: World[]) => {
+  worlds = worlds.toSorted((a, b) => a.id - b.id);
+
+  const values = new Array(worlds.length);
+  for (let i = 0; i < worlds.length; i++) {
+    values[i] = [worlds[i].id, worlds[i].randomNumber];
+  }
+
+  return sql`UPDATE world SET randomNumber = (update_data.randomNumber)::int
+	FROM (VALUES ${sql(values)}) AS update_data (id, randomNumber)
+	WHERE world.id = (update_data.id)::int`;
+};
