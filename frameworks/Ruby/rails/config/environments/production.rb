afdc756dd8f5023143d0005620c5a47a43fc73f8
Petrik <petrik@deheus.net>
2024-09-06 21:22:02 +0200

    [rails] Delete remaining unused middleware
    The following middleware is unused for the benchmark:
    - Callbacks
    - Rack::TempfileReaper
    
    The following middleware is already disabled in production by default,
    so they don't need to be deleted:
    - ActionDispatch::HostAuthorization
    - ActionDispatch::Static
    
    The remaining middleware are:
    - ActiveSupport::Cache::Strategy::LocalCache::Middleware
    - Hello::Application.routes
    
    Also sort the middleware.
    
    The middleware can be checked using the following command:
    
       SECRET_KEY_BASE_DUMMY=1  RAILS_ENV=production_mysql bin/rails middleware

--- frameworks/Ruby/rails/config/environments/production.rb
+++ frameworks/Ruby/rails/config/environments/production.rb
@@ -21,7 +21,8 @@ Rails.application.configure do
   # config.require_master_key = true
 
   # Disable serving static files from `public/`, relying on NGINX/Apache to do so instead.
-  # config.public_file_server.enabled = false
+  # This disables the ActionDispatch::Static middleware.
+  config.public_file_server.enabled = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
