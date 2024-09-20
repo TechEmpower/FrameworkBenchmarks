3cca47dd37bffdfcbfec5789357a1ec2dcdf6d63
Sébastien Deleuze <sebastien.deleuze@broadcom.com>
2024-09-20 10:51:08 +0200

    New round of spring optimizations
    - Upgrade to Spring Boot 3.3.4
    - Remove the JPA variant that is not competitive and
      introduces additional processing not needed in
      other variants
    - Use batched update in the JDBC variant (compliant with
      the test requirements)
    - Set Hikari maximum pool size to 256
    - Switch to jstachio for view rendering

--- frameworks/Java/spring/src/main/java/hello/UpdateWorldService.java
+++
@@ -1,9 +0,0 @@
-package hello;
-
-import hello.model.World;
-
-public interface UpdateWorldService {
-
-	World updateWorld(int worldId);
-
-}
--- frameworks/Java/spring/src/main/java/hello/model/World.java
+++ frameworks/Java/spring/src/main/java/hello/model/World.java
@@ -1,26 +1,22 @@
 package hello.model;
 
-import jakarta.persistence.Entity;
-
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
 @Document
-@Entity
 public final class World {
 
 	@Id
-	@jakarta.persistence.Id
 	public int id;
+
 	@Field("randomNumber")
-	public int randomnumber;
+	public int randomNumber;
 
-	protected World() {
-	}
 
-	public World(int id, int randomnumber) {
+	public World(int id, int randomNumber) {
 		this.id = id;
-		this.randomnumber = randomnumber;
+		this.randomNumber = randomNumber;
 	}
+
 }
