42acadfb4143d9a92fef09660503af6495f3d52c
James R. Perkins <jperkins@redhat.com>
2024-10-22 07:47:50 -0700

    Update WildFly to Jakarta EE 10 and the latest version of WildFly. (#9266)
    Signed-off-by: James R. Perkins <jperkins@redhat.com>

--- frameworks/Java/wildfly-ee/src/main/java/com/techempower/ee/tests/MultipleQueries.java
+++ frameworks/Java/wildfly-ee/src/main/java/com/techempower/ee/tests/MultipleQueries.java
@@ -3,13 +3,13 @@ package com.techempower.ee7.tests;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.MediaType;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 
 import com.techempower.ee7.model.World;
 import com.techempower.ee7.util.Helpers;
