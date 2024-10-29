9ad4a99a36aebe123cfd514904a5f93a40f7a666
noear <noear@live.cn>
2024-10-29 15:46:11 +0800

    Add FilterImpl

---
+++ frameworks/Java/solon-vertx/src/main/java/hello/controller/FilterImpl.java
@@ -0,0 +1,22 @@
+package hello.controller;
+
+import org.noear.solon.annotation.Component;
+import org.noear.solon.core.handle.Context;
+import org.noear.solon.core.handle.Filter;
+import org.noear.solon.core.handle.FilterChain;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class FilterImpl implements Filter {
+    private static DateFormat DATE_FORMAT = new SimpleDateFormat("EEE, dd MMM yyyyy HH:mm:ss z");
+
+    @Override
+    public void doFilter(Context ctx, FilterChain chain) throws Throwable {
+        String dateString = DATE_FORMAT.format(new Date());
+        ctx.headerSet("Date", dateString);
+        chain.doFilter(ctx);
+    }
+}
