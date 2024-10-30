bfdcf67641c86d18ac611e0da260d897042512c0
Shreck Ye <ShreckYe@gmail.com>
2024-10-30 08:39:30 +0800

    Simply the code introduced in the previous commit by making `MainVerticle` implement `CoroutineRouterSupport`
    I didn't go through [the docs](https://vertx.io/docs/vertx-lang-kotlin-coroutines/kotlin/#_vert_x_web) thoroughly before implementing this.

--- frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/MainVerticle.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/MainVerticle.kt
@@ -11,7 +11,6 @@ import io.vertx.kotlin.core.http.httpServerOptionsOf
 import io.vertx.kotlin.coroutines.CoroutineRouterSupport
 import io.vertx.kotlin.coroutines.CoroutineVerticle
 import io.vertx.kotlin.coroutines.coAwait
-import io.vertx.kotlin.coroutines.coroutineRouter
 import io.vertx.kotlin.pgclient.pgConnectOptionsOf
 import io.vertx.pgclient.PgConnection
 import io.vertx.sqlclient.PreparedQuery
@@ -30,7 +29,7 @@ import kotlinx.serialization.json.io.encodeToSink
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 
-class MainVerticle(val hasDb: Boolean) : CoroutineVerticle() {
+class MainVerticle(val hasDb: Boolean) : CoroutineVerticle(), CoroutineRouterSupport {
     // `PgConnection`s as used in the "vertx" portion offers better performance than `PgPool`s.
     lateinit var pgConnection: PgConnection
     lateinit var date: String
@@ -105,47 +104,45 @@ class MainVerticle(val hasDb: Boolean) : CoroutineVerticle() {
         putHeader(HttpHeaders.CONTENT_TYPE, "application/json")
     }
 
-    inner /*value*/ class CoroutineRouterSupportExt(val coroutineRouterSupport: CoroutineRouterSupport) {
-        fun Route.coHandlerUnconfined(requestHandler: suspend (RoutingContext) -> Unit): Route =
-            with(coroutineRouterSupport) {
-                /* Some conclusions from the Plaintext test results with trailing `await()`s:
-                   1. `launch { /*...*/ }` < `launch(start = CoroutineStart.UNDISPATCHED) { /*...*/ }` < `launch(Dispatchers.Unconfined) { /*...*/ }`.
-                   1. `launch { /*...*/ }` without `context` or `start` lead to `io.netty.channel.StacklessClosedChannelException` and `io.netty.channel.unix.Errors$NativeIoException: sendAddress(..) failed: Connection reset by peer`. */
-                coHandler(Dispatchers.Unconfined, requestHandler)
-            }
 
-        inline fun <reified T : Any> Route.jsonResponseCoHandler(
-            serializer: SerializationStrategy<T>,
-            crossinline requestHandler: suspend (RoutingContext) -> @Serializable T
-        ) =
-            coHandlerUnconfined {
-                it.response().run {
-                    putJsonResponseHeader()
+    fun Route.coHandlerUnconfined(requestHandler: suspend (RoutingContext) -> Unit): Route =
+        /* Some conclusions from the Plaintext test results with trailing `await()`s:
+           1. `launch { /*...*/ }` < `launch(start = CoroutineStart.UNDISPATCHED) { /*...*/ }` < `launch(Dispatchers.Unconfined) { /*...*/ }`.
+           1. `launch { /*...*/ }` without `context` or `start` lead to `io.netty.channel.StacklessClosedChannelException` and `io.netty.channel.unix.Errors$NativeIoException: sendAddress(..) failed: Connection reset by peer`. */
+        coHandler(Dispatchers.Unconfined, requestHandler)
 
-                    /*
-                    // approach 1
-                    end(Json.encodeToString(serializer, requestHandler(it)))/*.coAwait()*/
-                    */
+    inline fun <reified T : Any> Route.jsonResponseCoHandler(
+        serializer: SerializationStrategy<T>,
+        crossinline requestHandler: suspend (RoutingContext) -> @Serializable T
+    ) =
+        coHandlerUnconfined {
+            it.response().run {
+                putJsonResponseHeader()
 
-                    /*
-                    // approach 2
-                    // java.lang.IllegalStateException: You must set the Content-Length header to be the total size of the message body BEFORE sending any data if you are not using HTTP chunked encoding.
+                /*
+                // approach 1
+                end(Json.encodeToString(serializer, requestHandler(it)))/*.coAwait()*/
+                */
+
+                /*
+                // approach 2
+                // java.lang.IllegalStateException: You must set the Content-Length header to be the total size of the message body BEFORE sending any data if you are not using HTTP chunked encoding.
+                toRawSink().buffered().use { bufferedSink ->
+                    @OptIn(ExperimentalSerializationApi::class)
+                    Json.encodeToSink(serializer, requestHandler(it), bufferedSink)
+                }
+                */
+
+                // approach 3
+                end(Buffer.buffer().apply {
                     toRawSink().buffered().use { bufferedSink ->
                         @OptIn(ExperimentalSerializationApi::class)
                         Json.encodeToSink(serializer, requestHandler(it), bufferedSink)
                     }
-                    */
-
-                    // approach 3
-                    end(Buffer.buffer().apply {
-                        toRawSink().buffered().use { bufferedSink ->
-                            @OptIn(ExperimentalSerializationApi::class)
-                            Json.encodeToSink(serializer, requestHandler(it), bufferedSink)
-                        }
-                    })
-                }
+                })
             }
-    }
+        }
+
 
     suspend fun selectRandomWorlds(queries: Int): List<World> {
         val rowSets = List(queries) {
@@ -154,85 +151,83 @@ class MainVerticle(val hasDb: Boolean) : CoroutineVerticle() {
         return rowSets.map { it.single().toWorld() }
     }
 
-    fun Router.routes() = coroutineRouter {
-        with(CoroutineRouterSupportExt(this)) {
-            get("/json").jsonResponseCoHandler(Serializers.message) {
-                jsonSerializationMessage
-            }
+    fun Router.routes() {
+        get("/json").jsonResponseCoHandler(Serializers.message) {
+            jsonSerializationMessage
+        }
 
-            get("/db").jsonResponseCoHandler(Serializers.world) {
-                val rowSet = selectWorldQuery.execute(Tuple.of(randomIntBetween1And10000())).coAwait()
-                rowSet.single().toWorld()
-            }
+        get("/db").jsonResponseCoHandler(Serializers.world) {
+            val rowSet = selectWorldQuery.execute(Tuple.of(randomIntBetween1And10000())).coAwait()
+            rowSet.single().toWorld()
+        }
 
-            get("/queries").jsonResponseCoHandler(Serializers.worlds) {
-                val queries = it.request().getQueries()
-                selectRandomWorlds(queries)
-            }
+        get("/queries").jsonResponseCoHandler(Serializers.worlds) {
+            val queries = it.request().getQueries()
+            selectRandomWorlds(queries)
+        }
 
-            get("/fortunes").coHandlerUnconfined {
-                val fortunes = mutableListOf<Fortune>()
-                selectFortuneQuery.execute().coAwait()
-                    .mapTo(fortunes) { it.toFortune() }
+        get("/fortunes").coHandlerUnconfined {
+            val fortunes = mutableListOf<Fortune>()
+            selectFortuneQuery.execute().coAwait()
+                .mapTo(fortunes) { it.toFortune() }
 
-                fortunes.add(Fortune(0, "Additional fortune added at request time."))
-                fortunes.sortBy { it.message }
+            fortunes.add(Fortune(0, "Additional fortune added at request time."))
+            fortunes.sortBy { it.message }
 
-                val htmlString = buildString {
-                    append("<!DOCTYPE html>")
-                    appendHTML(false).html {
-                        head {
-                            title("Fortunes")
-                        }
-                        body {
-                            table {
-                                tr {
-                                    th { +"id" }
-                                    th { +"message" }
-                                }
-                                for (fortune in fortunes)
-                                    tr {
-                                        td { +fortune.id.toString() }
-                                        td { +fortune.message }
-                                    }
+            val htmlString = buildString {
+                append("<!DOCTYPE html>")
+                appendHTML(false).html {
+                    head {
+                        title("Fortunes")
+                    }
+                    body {
+                        table {
+                            tr {
+                                th { +"id" }
+                                th { +"message" }
                             }
+                            for (fortune in fortunes)
+                                tr {
+                                    td { +fortune.id.toString() }
+                                    td { +fortune.message }
+                                }
                         }
                     }
                 }
-
-                it.response().run {
-                    putCommonHeaders()
-                    putHeader(HttpHeaders.CONTENT_TYPE, "text/html; charset=utf-8")
-                    end(htmlString)/*.coAwait()*/
-                }
             }
 
-            get("/updates").jsonResponseCoHandler(Serializers.worlds) {
-                val queries = it.request().getQueries()
-                val worlds = selectRandomWorlds(queries)
-                val updatedWorlds = worlds.map { it.copy(randomNumber = randomIntBetween1And10000()) }
-
-                // Approach 1
-                // The updated worlds need to be sorted first to avoid deadlocks.
-                updateWordQuery
-                    .executeBatch(updatedWorlds.sortedBy { it.id }.map { Tuple.of(it.randomNumber, it.id) }).coAwait()
-
-                /*
-                // Approach 2, worse performance
-                updatedWorlds.map {
-                    pgPool.preparedQuery(UPDATE_WORLD_SQL).execute(Tuple.of(it.randomNumber, it.id))
-                }.awaitAll()
-                */
-
-                updatedWorlds
+            it.response().run {
+                putCommonHeaders()
+                putHeader(HttpHeaders.CONTENT_TYPE, "text/html; charset=utf-8")
+                end(htmlString)/*.coAwait()*/
             }
+        }
 
-            get("/plaintext").coHandlerUnconfined {
-                it.response().run {
-                    putCommonHeaders()
-                    putHeader(HttpHeaders.CONTENT_TYPE, "text/plain")
-                    end("Hello, World!")/*.coAwait()*/
-                }
+        get("/updates").jsonResponseCoHandler(Serializers.worlds) {
+            val queries = it.request().getQueries()
+            val worlds = selectRandomWorlds(queries)
+            val updatedWorlds = worlds.map { it.copy(randomNumber = randomIntBetween1And10000()) }
+
+            // Approach 1
+            // The updated worlds need to be sorted first to avoid deadlocks.
+            updateWordQuery
+                .executeBatch(updatedWorlds.sortedBy { it.id }.map { Tuple.of(it.randomNumber, it.id) }).coAwait()
+
+            /*
+            // Approach 2, worse performance
+            updatedWorlds.map {
+                pgPool.preparedQuery(UPDATE_WORLD_SQL).execute(Tuple.of(it.randomNumber, it.id))
+            }.awaitAll()
+            */
+
+            updatedWorlds
+        }
+
+        get("/plaintext").coHandlerUnconfined {
+            it.response().run {
+                putCommonHeaders()
+                putHeader(HttpHeaders.CONTENT_TYPE, "text/plain")
+                end("Hello, World!")/*.coAwait()*/
             }
         }
     }
