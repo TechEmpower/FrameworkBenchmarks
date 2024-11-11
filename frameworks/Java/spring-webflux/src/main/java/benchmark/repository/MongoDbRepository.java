1f3fb16bc37c1bb0f691c89bf12599115870bb35
Sébastien Deleuze <sdeleuze@users.noreply.github.com>
2024-11-11 17:39:46 +0100

    [spring] Batch update and Java 23 (#9370)
    The commit:
     - Use batch update in spring-webflux
     - Upgrade Java 21 to Java 23
     - Increase the connection pool from 256 to 512
     - Upgrade to Spring Boot 3.3.5

--- frameworks/Java/spring-webflux/src/main/java/benchmark/repository/MongoDbRepository.java
+++ frameworks/Java/spring-webflux/src/main/java/benchmark/repository/MongoDbRepository.java
@@ -1,16 +1,16 @@
 package benchmark.repository;
 
+import java.util.List;
+
 import benchmark.model.Fortune;
 import benchmark.model.World;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.context.annotation.Profile;
 import org.springframework.data.mongodb.core.ReactiveMongoOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import static org.springframework.data.mongodb.core.FindAndModifyOptions.options;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
 import static org.springframework.data.mongodb.core.query.Query.query;
 import static org.springframework.data.mongodb.core.query.Update.update;
@@ -31,12 +31,11 @@ public class MongoDbRepository implements DbRepository {
     }
 
     @Override
-    public Mono<World> findAndUpdateWorld(int id, int randomNumber) {
-        return operations.findAndModify(
-                query(where("id").is(id)),
-                update("randomNumber", randomNumber),
-                options().returnNew(true),
-                World.class);
+    public Mono<Void> updateWorlds(List<World> worlds) {
+        return Flux.fromIterable(worlds).flatMap(world -> operations.findAndModify(
+                query(where("id").is(world.id)),
+                update("randomNumber", world.randomnumber),
+                World.class)).then();
     }
 
     @Override
