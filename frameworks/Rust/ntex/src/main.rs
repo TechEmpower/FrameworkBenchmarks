d078293effd0c3df5ce321a26482e8c8c807e6a0
Nikolay Kim <fafhrd91@gmail.com>
2024-11-25 09:03:04 -0800

    ntex optimizations (#9406)
    * cpu affinity
    
    * ntex optimizations
    
    * remove smallvec
    
    * wip

--- frameworks/Rust/ntex/src/main.rs
+++ frameworks/Rust/ntex/src/main.rs
@@ -2,8 +2,8 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use ntex::http::header::{CONTENT_TYPE, SERVER};
-use ntex::{http, time::Seconds, util::BytesMut, util::PoolId, web};
-use yarte::Serialize;
+use ntex::{http, time::Seconds, util::BytesMut, util::PoolId, util::Ready, web};
+use sonic_rs::Serialize;
 
 mod utils;
 
@@ -15,10 +15,13 @@ pub struct Message {
 #[web::get("/json")]
 async fn json() -> web::HttpResponse {
     let mut body = BytesMut::with_capacity(utils::SIZE);
-    Message {
-        message: "Hello, World!",
-    }
-    .to_bytes_mut(&mut body);
+    sonic_rs::to_writer(
+        utils::BytesWriter(&mut body),
+        &Message {
+            message: "Hello, World!",
+        },
+    )
+    .unwrap();
 
     let mut response = web::HttpResponse::with_body(http::StatusCode::OK, body.into());
     response.headers_mut().insert(SERVER, utils::HDR_SERVER);
@@ -45,6 +48,10 @@ async fn plaintext() -> web::HttpResponse {
 async fn main() -> std::io::Result<()> {
     println!("Started http server: 127.0.0.1:8080");
 
+    let cores = core_affinity::get_core_ids().unwrap();
+    let total_cores = cores.len();
+    let cores = std::sync::Arc::new(std::sync::Mutex::new(cores));
+
     // start http server
     ntex::server::build()
         .backlog(1024)
@@ -60,7 +67,17 @@ async fn main() -> std::io::Result<()> {
                 .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
                 .h1(web::App::new().service(json).service(plaintext).finish())
         })?
-        .workers(num_cpus::get())
+        .configure(move |cfg| {
+            let cores = cores.clone();
+            cfg.on_worker_start(move |_| {
+                if let Some(core) = cores.lock().unwrap().pop() {
+                    core_affinity::set_for_current(core);
+                }
+                Ready::<_, &str>::Ok(())
+            });
+            Ok(())
+        })?
+        .workers(total_cores)
         .run()
         .await
 }
--- frameworks/Rust/ntex/src/utils.rs
+++ frameworks/Rust/ntex/src/utils.rs
@@ -1,8 +1,9 @@
 #![allow(dead_code)]
-use std::cmp;
+use std::{cmp, io, io::Write, mem::MaybeUninit, slice::from_raw_parts_mut};
 
 use atoi::FromRadix10;
 use ntex::{http::header::HeaderValue, util::BufMut, util::Bytes, util::BytesMut};
+use sonic_rs::writer::WriteExt;
 
 pub const HDR_SERVER: HeaderValue = HeaderValue::from_static("N");
 pub const HDR_JSON_CONTENT_TYPE: HeaderValue = HeaderValue::from_static("application/json");
@@ -28,5 +29,38 @@ pub fn reserve(buf: &mut BytesMut, lw: usize) {
     let remaining = buf.remaining_mut();
     if remaining < lw {
         buf.reserve(HW);
+    }
+}
+
+pub struct BytesWriter<'a>(pub &'a mut BytesMut);
+
+impl<'a> Write for BytesWriter<'a> {
+    fn write(&mut self, src: &[u8]) -> Result<usize, io::Error> {
+        self.0.extend_from_slice(src);
+        Ok(src.len())
+    }
+
+    fn flush(&mut self) -> Result<(), io::Error> {
+        Ok(())
+    }
+}
+
+impl<'a> WriteExt for BytesWriter<'a> {
+    #[inline(always)]
+    fn reserve_with(&mut self, additional: usize) -> Result<&mut [MaybeUninit<u8>], io::Error> {
+        self.0.reserve(additional);
+
+        unsafe {
+            let ptr = self.0.as_mut_ptr().add(self.0.len()) as *mut MaybeUninit<u8>;
+            Ok(from_raw_parts_mut(ptr, additional))
+        }
+    }
+
+    #[inline(always)]
+    unsafe fn flush_len(&mut self, additional: usize) {
+        unsafe {
+            let new_len = self.0.len() + additional;
+            self.0.set_len(new_len);
+        }
     }
 }
