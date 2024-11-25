771117c1d6aca021d8ede7d2327a19970c573a17
Andreas Nägeli <andr.naegeli@gmail.com>
2024-11-25 18:03:21 +0100

    [genhttp] Update to Version 9 (#9404)
    * Switch to .NET 9 but still on old GenHTTP version
    
    * Actually also switch the docker version
    
    * Migrate to GenHTTP 9
    
    * Re-enable real data on fortunes
    
    * Switch to ASP.NET framework
    
    * one should compile before pushing ..
    
    * Another try
    
    * Use other docker file style
    
    * Encode HTML in fortunes
    
    * Assign ID 0 to additional cookie

--- frameworks/CSharp/genhttp/Benchmarks/Model/DatabaseContext.cs
+++ frameworks/CSharp/genhttp/Benchmarks/Model/DatabaseContext.cs
@@ -1,52 +1,43 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace Benchmarks.Model
+namespace Benchmarks.Model;
+
+public sealed class DatabaseContext : DbContext
 {
+    private static DbContextOptions<DatabaseContext> _options;
 
-    public sealed class DatabaseContext : DbContext
+    private static DbContextOptions<DatabaseContext> _noTrackingOptions;
+
+    #region Factory
+
+    public static DatabaseContext Create() => new(_options ??= GetOptions(true));
+
+    public static DatabaseContext CreateNoTracking() => new(_noTrackingOptions ??= GetOptions(false));
+
+    private static DbContextOptions<DatabaseContext> GetOptions(bool tracking)
     {
-        private static DbContextOptions<DatabaseContext> _Options;
+        var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
 
-        private static DbContextOptions<DatabaseContext> _NoTrackingOptions;
+        optionsBuilder.UseNpgsql("Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;SSL Mode=Disable;Maximum Pool Size=512;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4");
 
-        #region Factory
-
-        public static DatabaseContext Create()
+        if (!tracking)
         {
-            return new DatabaseContext(_Options ??= GetOptions(true));
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
         }
 
-        public static DatabaseContext CreateNoTracking()
-        {
-            return new DatabaseContext(_NoTrackingOptions ??= GetOptions(false));
-        }
-
-        private static DbContextOptions<DatabaseContext> GetOptions(bool tracking)
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<DatabaseContext>();
-
-            optionsBuilder.UseNpgsql("Server=tfb-database;Database=hello_world;User Id=benchmarkdbuser;Password=benchmarkdbpass;Maximum Pool Size=256;NoResetOnClose=true;Enlist=false;Max Auto Prepare=4");
-
-            if (!tracking)
-            {
-                optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
-            }
-
-            return optionsBuilder.Options;
-        }
-
-        private DatabaseContext(DbContextOptions options) : base(options) { }
-
-        #endregion
-
-        #region Entities
-
-        public DbSet<World> World { get; set; }
-
-        public DbSet<Fortune> Fortune { get; set; }
-
-        #endregion
-
+        return optionsBuilder.Options;
     }
+
+    private DatabaseContext(DbContextOptions options) : base(options) { }
+
+    #endregion
+
+    #region Entities
+
+    public DbSet<World> World { get; set; }
+
+    public DbSet<Fortune> Fortune { get; set; }
+
+    #endregion
 
 }
