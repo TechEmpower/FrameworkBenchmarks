cda709d99efb483fb05deed5248873a8253047e6
三刀 <zhengjunweimail@163.com>
2024-12-09 23:58:33 +0800

    upgrade smart-servlet to 2.4 (#9445)
    * update smart-servlet to 0.1.3-SNAPSHOT
    
    * update aio-enhance to 1.0.3-SNAPSHOT
    
    * smart-servlet bugfix
    
    * bugfix
    
    * update smart-socket to 1.5.6-SNAPSHOT
    
    * remove file
    
    * update aio-enhance to 1.0.4-SNAPSHOT
    
    * 优化代码
    
    * 优化代码
    
    * update smart-socket to 1.5.6
    
    * config threadNum
    
    * update smart-socket to 1.5.7-SNAPSHOT
    
    * 优化代码
    
    * update smart-socket to 1.5.10-SNAPSHOT
    
    * 优化代码
    
    * 优化代码
    
    * 优化代码
    
    * 异常aio-enhance
    
    * 优化代码
    
    * 优化代码
    
    * 优化代码
    
    * remove aio-pro
    
    * remove headerLimiter
    
    * update hikaricp version
    
    * replace json util
    
    * 更新线程模型
    
    * upgrade smart-servlet to 0.1.9-SNAPSHOT
    
    * config thread num
    
    * config thread num
    
    * revert code
    
    * revert code
    
    * upgrade smart-servlet to 0.2.1-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 0.6-SNAPSHOT
    
    * upgrade smart-servlet to 1.0-SNAPSHOT
    
    * upgrade smart-servlet to 1.4
    
    * upgrade smart-servlet to 1.5-SNAPSHOT
    
    * 启用虚拟线程
    
    * 启用虚拟线程
    
    * 适配最新版
    
    * 适配最新版
    
    * 适配最新版
    
    * 适配最新版
    
    * 适配最新版
    
    * 适配最新版

--- frameworks/Java/smart-socket/src/main/java/org/smartboot/servlet/Bootstrap.java
+++ frameworks/Java/smart-socket/src/main/java/org/smartboot/servlet/Bootstrap.java
@@ -1,16 +1,10 @@
 package org.smartboot.servlet;
 
 
-import org.smartboot.http.server.HttpBootstrap;
-import org.smartboot.http.server.HttpRequest;
-import org.smartboot.http.server.HttpResponse;
-import org.smartboot.http.server.HttpServerHandler;
 import tech.smartboot.servlet.Container;
 import tech.smartboot.servlet.ServletContextRuntime;
 import tech.smartboot.servlet.conf.ServletInfo;
 import tech.smartboot.servlet.conf.ServletMappingInfo;
-
-import java.util.concurrent.CompletableFuture;
 
 /**
  * @author 三刀（zhengjunweimail@163.com）
@@ -19,6 +13,7 @@ import java.util.concurrent.CompletableFuture;
 public class Bootstrap {
 
     public static void main(String[] args) throws Throwable {
+        System.setProperty("smart-servlet-spring-boot-starter","true");
         Container containerRuntime = new Container();
         // plaintext
         ServletContextRuntime applicationRuntime = new ServletContextRuntime(null, Thread.currentThread().getContextClassLoader(), "/");
@@ -38,26 +33,13 @@ public class Bootstrap {
         applicationRuntime.getDeploymentInfo().addServlet(jsonServletInfo);
         applicationRuntime.getDeploymentInfo().addServletMapping(new ServletMappingInfo(jsonServletInfo.getServletName(), "/json"));
         containerRuntime.addRuntime(applicationRuntime);
-
         int cpuNum = Runtime.getRuntime().availableProcessors();
         // 定义服务器接受的消息类型以及各类消息对应的处理器
-        HttpBootstrap bootstrap = new HttpBootstrap();
-        bootstrap.configuration()
-                .threadNum(cpuNum)
-                .bannerEnabled(false)
-                .headerLimiter(0)
-                .readBufferSize(1024 * 4)
-                .writeBufferSize(1024 * 4)
-                .readMemoryPool(16384 * 1024 * 4)
-                .writeMemoryPool(10 * 1024 * 1024 * cpuNum, cpuNum);
-        containerRuntime.start(bootstrap.configuration());
-        bootstrap.setPort(8080)
-                .httpHandler(new HttpServerHandler() {
-                    @Override
-                    public void handle(HttpRequest request, HttpResponse response, CompletableFuture<Object> completableFuture) throws Throwable {
-                        containerRuntime.doHandle(request, response, completableFuture);
-                    }
-                })
-                .start();
+        containerRuntime.getConfiguration()
+                .setThreadNum(cpuNum)
+                .setReadBufferSize(1024 * 4);
+        containerRuntime.initialize();
+        containerRuntime.start();
+        
     }
 }
