5e6cb1ba4434cbff88c2fd0d1ee6db5078e2ba31
Andrew James <59655451+andrew-james-dev@users.noreply.github.com>
2024-12-24 22:55:35 +0000

    perf: simplification of mass update query

--- frameworks/Rust/axum/src/common/mod.rs
+++ frameworks/Rust/axum/src/common/mod.rs
@@ -17,10 +17,9 @@ pub const SELECT_WORLD_BY_ID: &str =
 pub const SELECT_ALL_CACHED_WORLDS: &str =
     "SELECT id, randomnumber FROM world ORDER BY id";
 #[allow(dead_code)]
-pub const UPDATE_WORLDS: &str = "WITH vals AS (SELECT * FROM UNNEST($1::int[], $2::int[]) AS v(id, rnum))
-    UPDATE world SET randomnumber = new.rnum FROM
-  (SELECT w.id, v.rnum FROM world w INNER JOIN vals v ON v.id = w.id ORDER BY w.id FOR UPDATE) AS new
-  WHERE world.id = new.id";
+pub const UPDATE_WORLDS: &str = r#"UPDATE world SET randomnumber = new.rnum FROM
+    (SELECT * FROM UNNEST($1::int[], $2::int[]) AS v(id, rnum)) AS new
+WHERE world.id = new.id"#;
 
 /// Return the value of an environment variable.
 #[allow(dead_code)]
