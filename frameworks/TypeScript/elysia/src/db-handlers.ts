a4d0334dfb76073af6bd049a64b26ec7a0241c3e
saltyaom <saltyaom@gmail.com>
2024-12-25 19:46:02 +0700

    :broom: chore(elysia) update Elysia to 1.2

--- frameworks/TypeScript/elysia/src/db-handlers.ts
+++ frameworks/TypeScript/elysia/src/db-handlers.ts
@@ -2,74 +2,66 @@ import { Elysia, t } from "elysia";
 import * as db from "./postgres";
 import { Fortune } from "./types";
 
-function rand() {
-  return Math.ceil(Math.random() * 10000);
+export function rand() {
+	return Math.ceil(Math.random() * 10000);
 }
 
 function parseQueriesNumber(q?: string) {
-  return Math.min(parseInt(q || "1") || 1, 500);
-}
-
-function renderTemplate(fortunes: Fortune[]) {
-  const n = fortunes.length;
-
-  let html = "";
-  for (let i = 0; i < n; i++) {
-    html += `<tr><td>${fortunes[i].id}</td><td>${Bun.escapeHTML(
-      fortunes[i].message,
-    )}</td></tr>`;
-  }
-
-  return `<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>${html}</table></body></html>`;
+	// NaN is falsy, fallback to one.
+	return Math.min(+(q as string) || 1, 500);
 }
 
 export const dbHandlers = new Elysia()
-  .headers({
-    server: "Elysia",
-  })
-  .get("/db", () => db.find(rand()))
-  .get("/fortunes", async (c) => {
-    const fortunes = await db.fortunes();
+	.headers({
+		server: "Elysia",
+	})
+	// ? Mark as async for Promise result to prevent double Elysia's mapResponse execution
+	.get("/db", async () => db.find(rand()))
+	.get("/fortunes", async (c) => {
+		const fortunes = await db.fortunes();
 
-    fortunes.push({
-      id: 0,
-      message: "Additional fortune added at request time.",
-    });
+		fortunes.push({
+			id: 0,
+			message: "Additional fortune added at request time.",
+		});
 
-    fortunes.sort((a, b) => {
-      if (a.message < b.message) return -1;
+		fortunes.sort((a, b) => {
+			if (a.message < b.message) return -1;
 
-      return 1;
-    });
+			return 1;
+		});
 
-    c.set.headers["content-type"] = "text/html; charset=utf-8";
+		c.set.headers["content-type"] = "text/html; charset=utf-8";
 
-    return renderTemplate(fortunes);
-  })
-  .get("/queries", (c) => {
-    const num = parseQueriesNumber(c.query.queries);
-    const worldPromises = new Array(num);
+		const n = fortunes.length;
 
-    for (let i = 0; i < num; i++) {
-      worldPromises[i] = db.find(rand());
-    }
+		let html = "";
+		for (let i = 0; i < n; i++) {
+			html += `<tr><td>${fortunes[i].id}</td><td>${Bun.escapeHTML(
+				fortunes[i].message,
+			)}</td></tr>`;
+		}
 
-    return Promise.all(worldPromises);
-  })
-  .get("/updates", async (c) => {
-    const num = parseQueriesNumber(c.query.queries);
-    const worldPromises = new Array(num);
+		return `<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>${html}</table></body></html>`;
+	})
+	// ? Mark as async for Promise result to prevent double Elysia's mapResponse execution
+	.get("/queries", async (c) => {
+		const num = parseQueriesNumber(c.query.queries);
+		const worldPromises = new Array(num);
 
-    for (let i = 0; i < num; i++) {
-      worldPromises[i] = db.find(rand());
-    }
+		for (let i = 0; i < num; i++) worldPromises[i] = db.find(rand());
 
-    const worlds = await Promise.all(worldPromises);
+		return Promise.all(worldPromises);
+	})
+	.get("/updates", async (c) => {
+		const num = parseQueriesNumber(c.query.queries);
+		const worldPromises = new Array(num);
 
-    for (let i = 0; i < num; i++) {
-      worlds[i].randomNumber = rand();
-    }
+		for (let i = 0; i < num; i++)
+			worldPromises[i] = db.findThenRand(rand());
 
-    await db.bulkUpdate(worlds);
-    return worlds;
-  });
+		const worlds = await Promise.all(worldPromises);
+
+		await db.bulkUpdate(worlds);
+		return worlds;
+	});
--- frameworks/TypeScript/elysia/src/postgres.ts
+++ frameworks/TypeScript/elysia/src/postgres.ts
@@ -1,30 +1,36 @@
 import postgres from "postgres";
-import { Fortune, World } from "./types";
+import { rand } from "./db-handlers";
+import type { Fortune, World } from "./types";
 
 const sql = postgres({
-  host: "tfb-database",
-  user: "benchmarkdbuser",
-  password: "benchmarkdbpass",
-  database: "hello_world",
-  max: 1,
+	host: "tfb-database",
+	user: "benchmarkdbuser",
+	password: "benchmarkdbpass",
+	database: "hello_world",
+	max: 1,
 });
 
 export const fortunes = () => sql<Fortune[]>`SELECT id, message FROM fortune`;
 
 export const find = (id: number) =>
-  sql<World[]>`SELECT id, randomNumber FROM world WHERE id = ${id}`.then(
-    (arr) => arr[0],
-  );
+	sql<World[]>`SELECT id, randomNumber FROM world WHERE id = ${id}`.then(
+		(arr) => arr[0],
+	);
 
-export const bulkUpdate = (worlds: World[]) => {
-  worlds = worlds.toSorted((a, b) => a.id - b.id);
+export const findThenRand = (id: number) =>
+	sql<World[]>`SELECT id, randomNumber FROM world WHERE id = ${id}`.then(
+		(arr) => {
+			arr[0].randomNumber = rand();
+			return arr[0];
+		},
+	);
 
-  const values = new Array(worlds.length);
-  for (let i = 0; i < worlds.length; i++) {
-    values[i] = [worlds[i].id, worlds[i].randomNumber];
-  }
-
-  return sql`UPDATE world SET randomNumber = (update_data.randomNumber)::int
-	FROM (VALUES ${sql(values)}) AS update_data (id, randomNumber)
+export const bulkUpdate = (worlds: World[]) =>
+	sql`UPDATE world SET randomNumber = (update_data.randomNumber)::int
+	FROM (VALUES ${worlds
+		.sort((a, b) => (a.id < b.id ? -1 : 1))
+		.map((world) => [
+			world.id,
+			world.randomNumber,
+		])}) AS update_data (id, randomNumber)
 	WHERE world.id = (update_data.id)::int`;
-};
