429f4b3534d70a02abe4140fb36d0ae99ef018e3
cypherpotato <gscatolin@outlook.com>
2025-01-22 00:58:32 -0300

    update sisk benchmarks

---
+++ frameworks/CSharp/sisk-framework/sisk-cadente/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using System.Text.Json;
+using Sisk.Cadente;
+
+var host = new HttpHost ( 8080, session => {
+    var request = session.Request;
+
+    if (request.Path == "/plaintext") {
+        SerializePlainTextResponse ( session.Response );
+    }
+    else if (request.Path == "/json") {
+        SerializeJsonResponse ( session.Response );
+    }
+    else {
+        session.Response.StatusCode = 404;
+    }
+} );
+
+host.Start ();
+Thread.Sleep ( Timeout.Infinite );
+
+static void SerializePlainTextResponse ( HttpResponse response ) {
+    var contentBytes = Encoding.UTF8.GetBytes ( "Hello, world!" );
+
+    response.Headers.Add ( new HttpHeader ( "Content-Type", "text/plain" ) );
+    response.ResponseStream = new MemoryStream ( contentBytes );
+}
+
+static void SerializeJsonResponse ( HttpResponse response ) {
+    var contentBytes = JsonSerializer.SerializeToUtf8Bytes ( new {
+        message = "Hello, world!"
+    } );
+
+    response.Headers.Add ( new HttpHeader ( "Content-Type", "application/json; charset=utf-8" ) );
+    response.ResponseStream = new MemoryStream ( contentBytes );
+}
---
+++ frameworks/CSharp/sisk-framework/sisk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http.Json;
+using Sisk.Core.Http;
+using Sisk.Core.Routing;
+
+var app = HttpServer.CreateBuilder ( host => {
+    host.UseListeningPort ( "http://+:8080/" );
+} ).Build ();
+
+app.Router.SetRoute ( RouteMethod.Get, "/plaintext", PlainText );
+app.Router.SetRoute ( RouteMethod.Get, "/json", Json );
+
+app.Start ();
+
+static HttpResponse PlainText ( HttpRequest request ) {
+    return new HttpResponse ( "Hello, world!" );
+}
+
+static HttpResponse Json ( HttpRequest request ) {
+    return new HttpResponse ( JsonContent.Create ( new {
+        message = "Hello, world!"
+    } ) );
+}
