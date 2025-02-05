87fa0bf296e63f6f1cff397202aec7d5a9de18ef
Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>
2025-02-05 13:25:18 -0500

    Sets size-factor to 1.0.
    Signed-off-by: Santiago Pericas-Geertsen <santiago.pericasgeertsen@oracle.com>

--- frameworks/Java/helidon/nima/src/main/java/io/helidon/benchmark/nima/models/PgClientConnectionPoolArray.java
+++ frameworks/Java/helidon/nima/src/main/java/io/helidon/benchmark/nima/models/PgClientConnectionPoolArray.java
@@ -2,12 +2,14 @@
 package io.helidon.benchmark.nima.models;
 
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 import io.helidon.config.Config;
 import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgConnectOptions;
 
 class PgClientConnectionPoolArray extends PgClientConnectionPool {
+    private static final Logger LOGGER = Logger.getLogger(PgClientConnectionPoolArray.class.getName());
 
     private final int connections;
     private final PgClientConnection[] connectionArray;
@@ -20,6 +22,8 @@ class PgClientConnectionPoolArray extends PgClientConnectionPool {
                 .orElse(1.0);
         connections = (int) (Runtime.getRuntime().availableProcessors() * sizeFactor);
         connectionArray = new PgClientConnection[connections];
+        LOGGER.info("Connection pool is " + getClass().getSimpleName());
+        LOGGER.info("Size of connection pool is " + connections);
     }
 
     @Override
--- frameworks/Java/helidon/nima/src/main/java/io/helidon/benchmark/nima/models/PgClientConnectionPoolMap.java
+++ frameworks/Java/helidon/nima/src/main/java/io/helidon/benchmark/nima/models/PgClientConnectionPoolMap.java
@@ -4,18 +4,21 @@ package io.helidon.benchmark.nima.models;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.locks.ReentrantLock;
+import java.util.logging.Logger;
 
 import io.helidon.config.Config;
 import io.vertx.core.Vertx;
 import io.vertx.pgclient.PgConnectOptions;
 
 class PgClientConnectionPoolMap extends PgClientConnectionPool {
+    private static final Logger LOGGER = Logger.getLogger(PgClientConnectionPoolMap.class.getName());
 
     private final ReentrantLock lock = new ReentrantLock();
     private final Map<String, PgClientConnection> connectionMap = new HashMap<>();
 
     PgClientConnectionPoolMap(Vertx vertx, PgConnectOptions options, Config config) {
         super(vertx, options, config);
+        LOGGER.info("Connection pool is " + getClass().getSimpleName());
     }
 
     @Override
