051603e3b5ece1a419e949a282bfbee3d996e112
Joan Miquel <joanhey@kumbiaphp.com>
2025-02-09 11:40:48 +0100

    Remove unnecessary services configs

--- frameworks/PHP/fomo/config/mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'host'     => env('MAIL_HOST' , 'smtp.mailtrap.io'),
-    'username' => env('MAIL_USERNAME'),
-    'password' => env('MAIL_PASSWORD'),
-    'port'     => env('MAIL_PORT' , 2525)
-];
--- frameworks/PHP/fomo/config/redis.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'host'     => env('REDIS_HOST' , '127.0.0.1'),
-    'username' => env('REDIS_USERNAME'),
-    'password' => env('REDIS_PASSWORD'),
-    'port'     => env('REDIS_PORT' , 6379),
-    'database' => env('REDIS_DATABASE' , 0),
-];
