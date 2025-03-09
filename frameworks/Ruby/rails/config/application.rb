c44a4a8920f534a5d97c3d81c98254347fec9fab
Petrik <petrik@deheus.net>
2025-03-09 18:22:01 +0100

    [rails] Update dependencies

--- frameworks/Ruby/rails/config/application.rb
+++ frameworks/Ruby/rails/config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 module Hello
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults Rails::VERSION::STRING.to_f
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
