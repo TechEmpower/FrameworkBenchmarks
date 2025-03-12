f380b8bb34ff12e923b6f5076c27831c89811767
尤雨东 <root@ltpp.vip>
2025-03-12 23:08:25 +0800

    feat: server header

--- frameworks/Rust/hyperlane/src/request_middleware.rs
+++ frameworks/Rust/hyperlane/src/request_middleware.rs
@@ -4,7 +4,7 @@ pub async fn request(controller_data: ControllerData) {
     let _ = controller_data
         .set_response_header(CONNECTION, CONNECTION_KEEP_ALIVE)
         .await
-        .set_request_header(
+        .set_response_header(
             CONTENT_TYPE,
             format!("{}; {}", APPLICATION_JSON, CHARSET_UTF_8),
         )
