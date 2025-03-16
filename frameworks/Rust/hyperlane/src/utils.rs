287235214ba9156738281360f6a8bc6448c829e4
尤雨东 <root@ltpp.vip>
2025-03-16 23:30:02 +0800

    feat: rand

--- frameworks/Rust/hyperlane/src/utils.rs
+++ frameworks/Rust/hyperlane/src/utils.rs
@@ -19,17 +19,17 @@ pub fn escape_html(input: &str) -> String {
 #[inline]
 pub fn get_random_id() -> Queries {
     let mut rand: WyRand = WyRand::new();
-    let random_id: Queries = rand.generate::<Queries>() % (RANDOM_MAX as Queries + 1);
-    random_id
+    let random_id: u32 = rand.generate::<u32>() % RANDOM_MAX as u32 + 1;
+    random_id as Queries
 }
 
 #[inline]
 pub fn get_random_id_list(limit: Queries) -> Vec<i32> {
     let mut id_list: Vec<i32> = (1..=limit).collect();
-    let mut rng: WyRand = WyRand::new();
+    let mut rand: WyRand = WyRand::new();
     let len: usize = id_list.len();
     for i in (1..len).rev() {
-        let j = rng.generate_range(0..=i as u32) as usize;
+        let j: usize = (rand.generate::<u32>() % RANDOM_MAX as u32 + 1) as usize;
         id_list.swap(i, j);
     }
     id_list.truncate(limit as usize);
