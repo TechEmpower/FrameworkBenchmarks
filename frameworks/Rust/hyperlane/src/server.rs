9e0cda584114dded8dc156749daebc951673665f
尤雨东 <root@ltpp.vip>
2025-03-23 20:02:17 +0800

    remove: log

--- frameworks/Rust/hyperlane/src/server.rs
+++ frameworks/Rust/hyperlane/src/server.rs
@@ -5,12 +5,12 @@ pub async fn run_server() {
     let server: Server = Server::new();
     server.host("0.0.0.0").await;
     server.port(8080).await;
-    server.log_dir("./logs").await;
+    server.disable_log().await;
+    server.disable_inner_log().await;
+    server.disable_inner_print().await;
     server.http_line_buffer_size(512).await;
     server.websocket_buffer_size(512).await;
     server.log_interval_millis(1_000_000_000).await;
-    server.disable_inner_log().await;
-    server.disable_inner_print().await;
     server.route("/plaintext", plaintext).await;
     server.route("/json", json).await;
     server.route("/cached-quer", cached_queries).await;
