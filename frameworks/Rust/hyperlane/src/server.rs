519aa5bbc665d91a31f5b0f4d5906732f3f35ea2
尤雨东 <root@ltpp.vip>
2025-03-24 23:10:14 +0800

    feat: log

--- frameworks/Rust/hyperlane/src/server.rs
+++ frameworks/Rust/hyperlane/src/server.rs
@@ -9,7 +9,6 @@ pub async fn run_server() {
     server.disable_inner_print().await;
     server.http_line_buffer_size(512).await;
     server.websocket_buffer_size(512).await;
-    server.log_interval_millis(1_000_000_000).await;
     server.route("/plaintext", plaintext).await;
     server.route("/json", json).await;
     server.route("/cached-quer", cached_queries).await;
