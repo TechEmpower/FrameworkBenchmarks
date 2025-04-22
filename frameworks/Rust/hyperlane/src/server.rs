49e93a1fe56be9730f877de9b7ceb190198452b8
尤雨东 <83822098+ltpp-universe@users.noreply.github.com>
2025-04-22 23:40:23 +0800

    Upgrade hyperlane (#9843)
    * feat: update & fortune
    
    * feat: randomNumber
    
    * feat: max row 500
    
    * feat: update
    
    * feat: Fortune
    
    * feat: fortunes
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: cache
    
    * feat: cache
    
    * feat: config
    
    * feat: config
    
    * feat: config
    
    * feat: v4.36.1
    
    * docs: readme
    
    * feat: remove dyn
    
    * docs: readme
    
    * feat: lock
    
    * feat: lock
    
    * feat: lock
    
    * feat: lock
    
    * feat: db pool
    
    * feat: db pool
    
    * feat: lock
    
    * feat: lock
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: port
    
    * feat: port
    
    * feat: port
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: lock
    
    * feat: lock
    
    * feat: v4.41.0
    
    * feat: v4.42.0
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: inline
    
    * feat: dockerfile
    
    * feat: v4.52.1
    
    * feat: remove key
    
    * remove: log
    
    * remove: log
    
    * feat: async
    
    * remove: empty loop
    
    * feat: utf8
    
    * change: pool_size
    
    * remove: utf8
    
    * feat: log
    
    * feat: log
    
    * feat: v3.14.1
    
    * feat: 4.56.3
    
    * feat: 4.56.4
    
    * feat: 4.56.5
    
    * feat:  rename
    
    * Merge branch 'master' of github.com:TechEmpower/FrameworkBenchmarks
    
    * feat: speed
    
    * feat: speed
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * feat: runtime
    
    * feat: runtime
    
    * feat: runtime
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'

--- frameworks/Rust/hyperlane/src/server.rs
+++ frameworks/Rust/hyperlane/src/server.rs
@@ -5,6 +5,8 @@ fn runtime() -> Runtime {
     Builder::new_multi_thread()
         .worker_threads(get_thread_count() >> 1)
         .thread_stack_size(1_048_576)
+        .worker_threads(get_thread_count() >> 1)
+        .thread_stack_size(1_048_576)
         .max_blocking_threads(5120)
         .max_io_events_per_tick(5120)
         .enable_all()
@@ -23,18 +25,33 @@ async fn init_server() {
     server.disable_inner_print().await;
     server.http_line_buffer_size(256).await;
     server.websocket_buffer_size(256).await;
-    server.request_middleware(request).await;
-    server.route("/plaintext", plaintext).await;
-    server.route("/json", json).await;
-    server.route("/cached-quer", cached_queries).await;
-    server.route("/db", db).await;
-    server.route("/query", queries).await;
-    server.route("/fortunes", fortunes).await;
-    server.route("/upda", updates).await;
+    server.request_middleware(request_middleware::request).await;
+    #[cfg(any(feature = "dev", feature = "plaintext"))]
+    server.route("/plaintext", route::plaintext).await;
+    #[cfg(any(feature = "dev", feature = "json"))]
+    server.route("/json", route::json).await;
+    #[cfg(any(feature = "dev", feature = "cached_query"))]
+    server.route("/cached-quer", route::cached_query).await;
+    #[cfg(any(feature = "dev", feature = "db"))]
+    server.route("/db", route::db).await;
+    #[cfg(any(feature = "dev", feature = "query"))]
+    server.route("/query", route::query).await;
+    #[cfg(any(feature = "dev", feature = "fortunes"))]
+    server.route("/fortunes", route::fortunes).await;
+    #[cfg(any(feature = "dev", feature = "update"))]
+    server.route("/upda", route::update).await;
     server.listen().await.unwrap();
 }
 
 async fn init() {
+    #[cfg(any(
+        feature = "dev",
+        feature = "db",
+        feature = "query",
+        feature = "update",
+        feature = "fortunes",
+        feature = "cached_query",
+    ))]
     init_db().await;
     init_server().await;
 }
