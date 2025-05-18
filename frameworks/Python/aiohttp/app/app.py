535bf1f9694511dc252370c75c39227fb9ca199e
Sam Bull <git@sambull.org>
2025-05-18 23:43:28 +0100

    Update app.py

--- frameworks/Python/aiohttp/app/app.py
+++ frameworks/Python/aiohttp/app/app.py
@@ -1,11 +1,15 @@
-import uvloop
-from aiohttp import web
 import argparse
+import platform
+
+from aiohttp import web
+
 from .main import create_app
 
 
 if __name__ == '__main__':
-    uvloop.install()
+    if platform.python_implementation() != "PyPy":
+        import uvloop
+        uvloop.install()
     parser = argparse.ArgumentParser()
     parser.add_argument('--socket', type=str, required=True)
     args = parser.parse_args()
