1b6c4a0eeef1681d2e1b3fb38debabfce6782482
Vetrichelvan <pythonhubdev@gmail.com>
2025-05-19 16:05:48 +0530

    feat: add support for litestar

---
+++ frameworks/Python/litestar/app-socketify-asgi.py
@@ -0,0 +1,42 @@
+import os
+import multiprocessing
+import logging
+
+import orjson
+from litestar import Litestar, get, MediaType
+from socketify import ASGI
+
+
+app = Litestar()
+
+@get("/json")
+async def json_serialization():
+    return orjson.dumps({"message": "Hello, world!"})
+
+@get("/plaintext", media_type=MediaType.TEXT)
+async def plaintext():
+    return b"Hello, world!"
+
+
+_is_travis = os.environ.get('TRAVIS') == 'true'
+
+workers = int(multiprocessing.cpu_count())
+if _is_travis:
+    workers = 2
+
+def run_app():
+    ASGI(app).listen(8080, lambda config: logging.info(f"Listening on port http://localhost:{config.port} now\n")).run()
+
+
+def create_fork():
+    n = os.fork()
+    # n greater than 0 means parent process
+    if not n > 0:
+        run_app()
+
+
+# fork limiting the cpu count - 1
+for i in range(1, workers):
+    create_fork()
+
+run_app()  # run app on the main process too :)
