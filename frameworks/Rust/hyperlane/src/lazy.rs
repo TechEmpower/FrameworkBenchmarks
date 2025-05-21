0737238566e0f6f2d86edfba7de5cb995f457a45
尤雨东 <83822098+eastspire@users.noreply.github.com>
2025-05-22 01:48:01 +0800

    Upgrade hyperlane (#9908)
    * feat: update & fortune
    
    * feat: randomNumber
    
    * feat: max row 500
    
    * feat: update
    
    * feat: Fortune
    
    * feat: fortunes
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: fortunes
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: cache
    
    * feat: cache
    
    * feat: config
    
    * feat: config
    
    * feat: config
    
    * feat: v4.36.1
    
    * docs: readme
    
    * feat: remove dyn
    
    * docs: readme
    
    * feat: lock
    
    * feat: lock
    
    * feat: lock
    
    * feat: lock
    
    * feat: db pool
    
    * feat: db pool
    
    * feat: lock
    
    * feat: lock
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: rand
    
    * feat: port
    
    * feat: port
    
    * feat: port
    
    * feat: db
    
    * feat: db
    
    * feat: db
    
    * feat: lock
    
    * feat: lock
    
    * feat: v4.41.0
    
    * feat: v4.42.0
    
    * Merge remote-tracking branch 'upstream/master'
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: inline
    
    * feat: dockerfile
    
    * feat: v4.52.1
    
    * feat: remove key
    
    * remove: log
    
    * remove: log
    
    * feat: async
    
    * remove: empty loop
    
    * feat: utf8
    
    * change: pool_size
    
    * remove: utf8
    
    * feat: log
    
    * feat: log
    
    * feat: v3.14.1
    
    * feat: 4.56.3
    
    * feat: 4.56.4
    
    * feat: 4.56.5
    
    * feat:  rename
    
    * Merge branch 'master' of github.com:TechEmpower/FrameworkBenchmarks
    
    * feat: speed
    
    * feat: speed
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * feat: runtime
    
    * feat: runtime
    
    * feat: runtime
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * lock: toml
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: hyperlane
    
    * feat: update
    
    * docs: readme
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * Merge remote-tracking branch 'upstream/master'
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: update
    
    * feat: lock
    
    * feat: use super
    
    ---------
    
    Co-authored-by: 尤雨东 <83822098+ltpp-universe@users.noreply.github.com>

--- frameworks/Rust/hyperlane/src/lazy.rs
+++ frameworks/Rust/hyperlane/src/lazy.rs
@@ -1,4 +1,4 @@
-use crate::*;
+use super::*;
 
 pub static DB: Lazy<DbPoolConnection> = Lazy::new(|| block_on(async { connection_db().await }));
 pub static CACHE: Lazy<Vec<QueryRow>> = Lazy::new(|| block_on(async { init_cache().await }));
