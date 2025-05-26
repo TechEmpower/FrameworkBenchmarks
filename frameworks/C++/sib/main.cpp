0c4e9c2c9763fa6fdbbe430af9d59b958d4fe900
Pooya Eimandar <Pooya.Eimandar@gmail.com>
2025-05-26 17:05:02 +0400

    update: sib

--- frameworks/C++/sib/main.cpp
+++ frameworks/C++/sib/main.cpp
@@ -1,177 +1,61 @@
-#include <folly/MPMCQueue.h>
-#include <folly/io/IOBuf.h>
-#include <folly/net/NetworkSocket.h>
-#include <folly/system/ThreadId.h>
-#include <folly/system/ThreadName.h>
+/*
+  Mozilla Public License 2.0 (MPL-2.0)
 
-#include <proxygen/lib/http/HTTPMessage.h>
-#include <proxygen/lib/http/session/HTTPTransaction.h>
+  This file is part of the "https://github.com/PooyaEimandar/sib" project, which is licensed under
+  the MPL-2.0. You can obtain a copy of the license at: https://opensource.org/licenses/MPL-2.0
 
-#include <sib/network/s_proxygen_server.hpp>
-#include <sib/sib.hpp>
+  The MPL-2.0 allows you to use, modify, and distribute this file under certain conditions.
+  The source code may be modified and distributed, provided that you retain the same license
+  and include a copy of this notice when redistributing the code.
 
-using namespace sib::network::http;
+  This project was created by Pooya Eimandar.
 
-// Constants
-const size_t s_compute_pool_size() {
-  constexpr size_t min_size = 512;
-  constexpr size_t max_size = 4096;
-  constexpr size_t per_thread = 128;
-  size_t total = per_thread * std::max(1u, std::thread::hardware_concurrency());
-  return std::min(std::max(total, min_size), max_size);
-}
+  For more information on the terms of this license, please refer to the official MPL-2.0
+  documentation.
 
-const size_t HANDLER_POOL_SIZE = s_compute_pool_size();
+  SPDX-License-Identifier: MPL-2.0
+*/
 
-constexpr auto MAX_BUFFER_SIZE = 4 * 1024 * 1024; // 4MB
+#pragma once
 
-// forward declaration
-struct handler;
+#include <seastar/core/app-template.hh>
+#include <seastar/http/httpd.hh>
+#include <seastar/http/function_handlers.hh>
+#include <seastar/core/sleep.hh>
+#include <seastar/core/reactor.hh>
+#include <iostream>
 
-// Thread-local handler Pool
-thread_local static folly::MPMCQueue<handler*> t_handler_pool(HANDLER_POOL_SIZE);
+using namespace seastar;
+using namespace seastar::httpd;
 
-struct handler : public proxygen::HTTPTransaction::Handler {
-  handler() = default;
-
-  void onHeadersComplete(std::unique_ptr<proxygen::HTTPMessage> /*req*/) noexcept override {
-    proxygen::HTTPMessage response;
-    response.setStatusCode(200);
-    response.setStatusMessage("OK");
-    response.setIsChunked(false);
-    response.setWantsKeepalive(true);
-
-    constexpr std::string_view SERVER_NAME = "SIB";
-    constexpr std::string_view CONTENT_TYPE = "text/plain";
-    constexpr std::string_view PAYLOAD_LEN = "13";
-    constexpr std::string_view PAYLOAD = "Hello, World!";
-    auto buffer = folly::IOBuf::wrapBuffer(PAYLOAD.data(), PAYLOAD.size());
-
-    auto& headers = response.getHeaders();
-    headers.add(proxygen::HTTPHeaderCode::HTTP_HEADER_SERVER, SERVER_NAME);
-    headers.add(proxygen::HTTPHeaderCode::HTTP_HEADER_CONTENT_TYPE, CONTENT_TYPE.data());
-    headers.add(proxygen::HTTPHeaderCode::HTTP_HEADER_CONTENT_LENGTH, PAYLOAD_LEN.data());
-
-    txn_->sendHeaders(response);
-    txn_->sendBody(std::move(buffer));
-    txn_->sendEOM();
-  }
-
-  void onBody(std::unique_ptr<folly::IOBuf>) noexcept override {}
-  void onTrailers(std::unique_ptr<proxygen::HTTPHeaders>) noexcept override {}
-  void onUpgrade(proxygen::UpgradeProtocol) noexcept override {}
-  void onError(const proxygen::HTTPException&) noexcept override {}
-  void onEgressPaused() noexcept override {}
-  void onEgressResumed() noexcept override {}
-
-  void setTransaction(proxygen::HTTPTransaction* txn) noexcept override { txn_ = txn; }
-
-  void detachTransaction() noexcept override {
-    txn_ = nullptr;
-    if (!t_handler_pool.write(this)) {
-      delete this; // Failed to return to pool, delete instead
-    }
-  }
-
-  void onEOM() noexcept override {}
-
- private:
-  proxygen::HTTPTransaction* txn_{nullptr};
-};
-
-auto create_socket_opt() -> folly::SocketOptionMap {
-  using ApplyPos = folly::SocketOptionKey::ApplyPos;
-  folly::SocketOptionMap socket_opt{};
-  // Enable SO_REUSEADDR, this is important for the server to be able to restart quickly
-  socket_opt.emplace(
-    folly::SocketOptionKey{SOL_SOCKET, SO_REUSEADDR, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{1});
-
-  // Disable Nagle (reduce latency)
-  socket_opt.emplace(
-    folly::SocketOptionKey{IPPROTO_TCP, TCP_NODELAY, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{1});
-  socket_opt.emplace(
-    folly::SocketOptionKey{IPPROTO_TCP, TCP_NODELAY, ApplyPos::POST_BIND},
-    folly::SocketOptionValue{1});
-  socket_opt.emplace(
-    folly::SocketOptionKey{
-      IPPROTO_TCP, TCP_NOTSENT_LOWAT, folly::SocketOptionKey::ApplyPos::POST_BIND},
-    folly::SocketOptionValue{1});
-
-  // Increase socket buffers (avoid drops under load)
-  socket_opt.emplace(
-    folly::SocketOptionKey{SOL_SOCKET, SO_RCVBUF, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{MAX_BUFFER_SIZE});
-  socket_opt.emplace(
-    folly::SocketOptionKey{SOL_SOCKET, SO_SNDBUF, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{MAX_BUFFER_SIZE});
-
-// Linux only TCP fast open for reducing handshake overhead
-#ifdef __linux__
-  socket_opt.emplace(
-    folly::SocketOptionKey{SOL_SOCKET, SO_REUSEPORT, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{1});
-  socket_opt.emplace(
-    folly::SocketOptionKey{IPPROTO_TCP, TCP_FASTOPEN, ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{1000} // Queue length for TFO
-  );
-#else
-  socket_opt.emplace(
-    folly::SocketOptionKey{IPPROTO_TCP, TCP_NOOPT, folly::SocketOptionKey::ApplyPos::PRE_BIND},
-    folly::SocketOptionValue{1});
-#endif
-
-  return socket_opt;
-}
-
-// === Main ===
 int main(int argc, char** argv) {
-  std::span<char*> argvSpan(argv, argc);
-  std::ignore = sib::init(argc, argvSpan);
+    app_template app;
+    auto server = std::make_shared<http_server_control>();
 
-  proxygen::HTTPServerOptions opts;
-  opts.threads = 0;
-  opts.shutdownOn = {SIGINT};
-  opts.idleTimeout = std::chrono::milliseconds(30000);
-  opts.enableContentCompression = false;
-  opts.h2cEnabled = false;
-  opts.listenBacklog = 65535;
-  opts.maxConcurrentIncomingStreams = 10000;
-  opts.initialReceiveWindow = 128 * 1024;
-  opts.receiveStreamWindowSize = 128 * 1024;
-  opts.receiveSessionWindowSize = MAX_BUFFER_SIZE;
-  opts.useZeroCopy = true;
-  opts.enableExHeaders = false;
+    return app.run(argc, argv, [server] () -> future<> {
+        constexpr auto port = 8080;
+        constexpr auto server_name = "Sib HTTP Server";
 
-  std::vector<proxygen::HTTPServer::IPConfig> ipConfigs = {
-    {folly::SocketAddress("0.0.0.0", 8080), proxygen::HTTPServer::Protocol::HTTP, nullptr}};
-  ipConfigs[0].enableTCPFastOpen = true;
-  ipConfigs[0].acceptorSocketOptions = create_socket_opt();
-
-  s_h_server httpServer(std::move(opts));
-  httpServer.set_domains({"localhost"})
-    .set_alpn_protocols({"http/1.1"})
-    .set_ips(std::move(ipConfigs));
-
-  auto server = s_proxygen_server::make()->set_h(std::move(httpServer));
-
-  server->run_forever(
-    []([[maybe_unused]] proxygen::HTTPMessage* req) -> proxygen::HTTPTransactionHandler* {
-      static thread_local bool pool_initialized = false;
-      if (!pool_initialized) {
-        pool_initialized = true;
-        auto fill_count = HANDLER_POOL_SIZE /
-          std::max(1u, static_cast<unsigned>(std::thread::hardware_concurrency()));
-        for (auto i = 0; i < fill_count; ++i) {
-          t_handler_pool.write(new handler());
-        }
-      }
-
-      handler* h = nullptr;
-      if (!t_handler_pool.read(h)) {
-        h = new handler();
-      }
-      return h;
+        return server->start(server_name).then([server] {
+            return server->set_routes([] (routes& p_routes) {
+                p_routes.add(operation_type::GET, url("/plaintext"),
+                    new function_handler(
+                        [] (std::unique_ptr<http::request> p_req,
+                            std::unique_ptr<http::reply> p_rep)
+                        -> future<std::unique_ptr<http::reply>> {
+                            p_rep->set_status(http::reply::status_type::ok);
+                            p_rep->write_body("text/plain", "Hello, World!");
+                            return make_ready_future<std::unique_ptr<http::reply>>(std::move(p_rep));
+                        },
+                        "text"
+                    )
+                );
+            });
+        }).then([server, port] {
+            return server->listen(socket_address{ipv4_addr{port}});
+        }).then([port]() -> future<> {
+            std::cout << "Seastar HTTP server listening on port " << port << " ...\n";
+            return sleep_abortable(std::chrono::hours(24)); 
+        });
     });
 }
--- frameworks/C++/sib/sib.dockerfile
+++ frameworks/C++/sib/sib.dockerfile
@@ -4,64 +4,32 @@ FROM ubuntu:24.04
 RUN apt-get update && apt-get install -y \
     git \
     cmake \
-    m4 \
     gcc \
     g++ \
-    clang-format \
-    clang-tidy \
-    ninja-build \
-    flex \
-    bison \
-    libunwind-dev \
-    libgflags-dev \
-    libkrb5-dev \
-    libsasl2-dev \
-    libnuma-dev \
-    pkg-config \
-    libssl-dev \
-    libcap-dev \
-    gperf \
-    libevent-dev \
-    libtool \
-    libboost-all-dev \
-    libjemalloc-dev \
-    libsnappy-dev \
-    wget \
-    unzip \
-    libiberty-dev \
-    liblz4-dev \
-    liblzma-dev \
-    make \
-    zlib1g-dev \
-    binutils-dev \
-    libsodium-dev \
-    libdouble-conversion-dev \
-    python3-venv \
-    libsecret-1-dev \
-    ccache
-
+    ninja-build 
+    
 # Set compiler environment
 ENV CC=gcc
 ENV CXX=g++
 ENV AR=gcc-ar
 ENV RANLIB=gcc-ranlib
-ENV CMAKE_C_COMPILER_LAUNCHER=ccache
-ENV CMAKE_CXX_COMPILER_LAUNCHER=ccache
 
 # Clone SIB repository with submodules
 WORKDIR /sib
-RUN git clone --depth 1 https://github.com/PooyaEimandar/sib.git .
+RUN git clone --depth 1 https://github.com/PooyaEimandar/sib.git . -b sib-seastar
+
+# Build Seastar
+RUN cd ./dep && ./seastar.sh release && cd ../
 
 # Copy TechEmpower-style entry point
-COPY ./main.cpp ./techempower/main.cpp
+#COPY ./main.cpp ./techempower/main.cpp
 
 # Run Proxygen/Folly build via your CMake script
 RUN cmake -B build -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
-    -DSIB_TECHEMPOWER=ON \
-    -DSIB_NET_PROXYGEN=ON \
- && cmake --build build 
+    -DCMAKE_CXX_FLAGS=-flto \
+ && cmake --build build
 
 # Run the sib server 
 EXPOSE 8080
-CMD ["./build/sib_techempower"]
+CMD ["./build/sib"]
