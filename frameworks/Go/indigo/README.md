34ca8b11c25adce5ae8c06b9dd10cbe5c7114e96
Alexander Kosachev <kosachev_alex@mail.ru>
2025-06-24 16:16:03 +0300

    Add indigo

---
+++ frameworks/Go/indigo/README.md
@@ -0,0 +1,14 @@
+# [Indigo](https://github.com/indigo-web/indigo) (Go) Benchmarking Test
+
+This is the go portion of a [benchmarking test suite](https://www.techempower.com/benchmarks/) comparing a variety of web development platforms.
+
+> Indigo is a web-framework focusing at performance, elegancy and robustness.
+
+## Test URLs
+* http://localhost:8080/json
+* http://localhost:8080/db
+* http://localhost:8080/queries?queries=[1-500]
+* http://localhost:8080/cached-worlds?count=[1-500]
+* http://localhost:8080/fortune
+* http://localhost:8080/update?queries=[1-500]
+* http://localhost:8080/plaintext
---
+++ frameworks/Go/indigo/src/app.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand/v2"
+	"slices"
+	"strconv"
+
+	"github.com/indigo-web/indigo/http"
+	"github.com/indigo-web/indigo/http/mime"
+
+	"indigo/app/models"
+	"indigo/app/templates"
+)
+
+type (
+	App struct {
+		DB    *DB
+		Cache models.Worlds
+	}
+)
+
+func NewApp(DB *DB) *App {
+	return &App{DB: DB}
+}
+
+func (app *App) PopulateCache(ctx context.Context) error {
+	cache := make(models.Worlds, 10000)
+
+	err := app.DB.FillWorlds(context.Background(), cache)
+	if err != nil {
+		return err
+	}
+
+	app.Cache = cache
+
+	return nil
+}
+
+func (app *App) HandleJSON(request *http.Request) *http.Response {
+	return request.Respond().JSON(&models.Message{
+		Message: "Hello, World!",
+	})
+}
+
+func (app *App) HandleDB(request *http.Request) *http.Response {
+	world := &models.World{
+		ID: rand.IntN(10000) + 1,
+	}
+
+	err := app.DB.FillWorldByID(context.Background(), world)
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	return request.Respond().JSON(world)
+}
+
+func (app *App) HandleQueries(request *http.Request) *http.Response {
+	n, err := normalizeNumber(request.Params.Get("n"))
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	i, worlds := 0, make(models.Worlds, n)
+
+	for i = range worlds {
+		worlds[i].ID = rand.IntN(10000) + 1
+	}
+
+	err = app.DB.FillWorldsByID(context.Background(), worlds)
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	return request.Respond().JSON(&worlds)
+}
+
+func (app *App) HandleUpdate(request *http.Request) *http.Response {
+	n, err := normalizeNumber(request.Params.Get("n"))
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	i, worlds := 0, make(models.Worlds, n)
+
+	for i = range worlds {
+		worlds[i].ID = rand.IntN(10000) + 1
+	}
+
+	err = app.DB.FillWorldsByID(context.Background(), worlds)
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	slices.SortFunc(worlds, func(a, b models.World) int {
+		return a.ID - b.ID
+	})
+
+	err = app.DB.UpdateWorlds(context.Background(), worlds)
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	return request.Respond().JSON(&worlds)
+}
+
+func (app *App) HandleCachedWorlds(request *http.Request) *http.Response {
+	n, err := normalizeNumber(request.Params.Get("n"))
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	i, worlds := 0, make(models.Worlds, n)
+
+	for i = range worlds {
+		worlds[i] = app.Cache[rand.Int32N(10000)]
+	}
+
+	return request.Respond().JSON(&worlds)
+}
+
+func (app *App) HandleFortunes(request *http.Request) *http.Response {
+	fortunes, err := app.DB.GetFortunes(context.Background())
+	if err != nil {
+		return http.Error(request, err)
+	}
+
+	fortunes = append(fortunes, models.Fortune{
+		Message: "Additional fortune added at request time.",
+	})
+
+	slices.SortFunc(fortunes, func(a, b models.Fortune) int {
+		if a.Message < b.Message {
+			return -1
+		} else if a.Message > b.Message {
+			return 1
+		}
+
+		return 0
+	})
+
+	return request.Respond().ContentType(mime.HTML).String(templates.HTMLFortunes(fortunes))
+}
+
+func (app *App) HandlePlaintext(request *http.Request) *http.Response {
+	return request.Respond().String("Hello, World!")
+}
+
+func normalizeNumber(nString string, found bool) (int, error) {
+	if !found {
+		return 0, fmt.Errorf("not found")
+	}
+
+	n, err := strconv.Atoi(nString)
+	if err != nil {
+		return 0, err
+	}
+
+	if n < 1 {
+		n = 1
+	} else if n > 500 {
+		n = 500
+	}
+
+	return n, nil
+}
