3f4b333e9db4045f8ba17c7897b0efb7f7657c7c
nazo <s3moye@gmail.com>
2025-06-28 15:07:54 +0800

    success fix

--- frameworks/Python/blacksheep/app-socketify.py
+++ frameworks/Python/blacksheep/app-socketify.py
@@ -115,7 +115,7 @@ async def db_updates_test(request):
             await connection.fetch_val(READ_ROW_SQL, [row_id])
         # await db_conn.executemany(WRITE_ROW_SQL, updates)
         await connection.execute_many(WRITE_ROW_SQL, updates)
-    return jsonify(worlds)
+    return bs.json(worlds)
 
 @bs.get('/plaintext')
 async def plaintext_test(request):
