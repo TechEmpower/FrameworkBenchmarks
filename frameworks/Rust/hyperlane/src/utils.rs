de538cf688ca058144a12f29182430f9fd7c6afe
尤雨东 <root@ltpp.vip>
2025-06-29 09:22:15 +0800

    feat: dep

--- frameworks/Rust/hyperlane/src/utils.rs
+++ frameworks/Rust/hyperlane/src/utils.rs
@@ -1,5 +1,16 @@
 use super::*;
 
+pub fn get_thread_count() -> usize {
+    let cpu_count: usize = num_cpus::get_physical();
+    let total_threads: usize = num_cpus::get();
+    let threads_per_cpu: usize = if cpu_count > 0 {
+        total_threads / cpu_count
+    } else {
+        1
+    };
+    cpu_count * threads_per_cpu
+}
+
 pub fn escape_html(input: &str) -> String {
     let mut result: String = String::new();
     for ch in input.chars() {
