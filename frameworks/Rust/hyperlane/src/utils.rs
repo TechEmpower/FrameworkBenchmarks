01f3a7bff01bd3bdbf3f263c0178db7f408ecbeb
尤雨东 <root@ltpp.vip>
2025-06-29 12:06:12 +0800

    feat: get_thread_count

--- frameworks/Rust/hyperlane/src/utils.rs
+++ frameworks/Rust/hyperlane/src/utils.rs
@@ -1,14 +1,7 @@
 use super::*;
 
 pub fn get_thread_count() -> usize {
-    let cpu_count: usize = num_cpus::get_physical();
-    let total_threads: usize = num_cpus::get();
-    let threads_per_cpu: usize = if cpu_count > 0 {
-        total_threads / cpu_count
-    } else {
-        1
-    };
-    cpu_count * threads_per_cpu
+    num_cpus::get().max(1)
 }
 
 pub fn escape_html(input: &str) -> String {
