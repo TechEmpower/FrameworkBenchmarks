2f8743823e70464be54f7f98e97216f050beb877
Andrew James <59655451+andrew-james-dev@users.noreply.github.com>
2025-07-21 01:41:49 +0100

    feat: dependency updates and performance tweaks

--- frameworks/Rust/axum/src/sqlx/models.rs
+++ frameworks/Rust/axum/src/sqlx/models.rs
@@ -1,10 +1,22 @@
-use serde::{Deserialize, Serialize};
-use sqlx::FromRow;
+use std::borrow::Cow;
 
-#[derive(Clone, Debug, PartialEq, Deserialize, Serialize, FromRow)]
+use serde::{Deserialize, Serialize};
+use sqlx::{FromRow, Row};
+use sqlx::postgres::PgRow;
+
+#[derive(Clone, Debug, PartialEq, Deserialize, Serialize)]
 pub struct Fortune {
     pub id: i32,
-    pub message: String,
+    pub message: Cow<'static, str>,
+}
+
+impl FromRow<'_, PgRow> for Fortune {
+  fn from_row(row: &PgRow) -> Result<Self, sqlx::Error> {
+    Ok(Fortune {
+        id: row.try_get(0usize)?,
+        message: Cow::Owned(row.try_get(1usize)?)
+      })
+  }
 }
 
 #[derive(Clone, Debug, PartialEq, Deserialize, Serialize, FromRow)]
