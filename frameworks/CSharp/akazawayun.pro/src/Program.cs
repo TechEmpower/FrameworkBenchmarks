625f00aecae59d23b7cd617d3ff13efeadcf456b
AkazawaYun <408193813@qq.com>
2025-07-27 19:30:46 +0800

    add new test : CSharp/akazawayun.pro

---
+++ frameworks/CSharp/akazawayun.pro/src/Program.cs
@@ -0,0 +1,36 @@
+﻿using AkazawaYun.PRO7;
+using System.Text.Json.Serialization;
+
+namespace AkazawaYun.FrameworkBenchmarks;
+
+public class Program : IPostFunction
+{
+    static async Task Main()
+    {
+        akzJson.Config(null, Json.Default);
+        var server = await akzWebBuilder.Shared.Load().SetDefault().Build().Launch();
+        akzLog.Default = akzLog.Output.None;
+        await Task.Delay(-1);
+    }
+
+
+    public static async ValueTask<HttpRes> plaintext(HttpReq _)
+    {
+        return "Hello, World!";
+    }
+    public static async ValueTask<HttpRes> json(HttpReq _)
+    {
+        return HttpRes.HttpJson(new JsonResponse
+        {
+            Message = "Hello, World!"
+        });
+    }
+}
+
+public class JsonResponse
+{
+    public string? Message { get; set; }
+}
+
+[JsonSerializable(typeof(JsonResponse))]
+public partial class Json : JsonSerializerContext { }
