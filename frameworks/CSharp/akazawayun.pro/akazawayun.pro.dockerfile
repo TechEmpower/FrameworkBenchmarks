18be8bb6e8c7f612c5a52dada469bbacf351914e
AkazawaYun <62974697+AkazawaYun@users.noreply.github.com>
2025-07-30 02:20:32 +0800

    modify and add new test : CSharp/akazawayun.pro (#10017)
    * add new test : CSharp/akazawayun.pro
    
    * modify config and code
    
    * add content-type
    
    * update url
    
    * update package version

---
+++ frameworks/CSharp/akazawayun.pro/akazawayun.pro.dockerfile
@@ -0,0 +1,13 @@
+﻿# 此阶段用于生成项目
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS publish
+WORKDIR /app
+COPY src .
+RUN dotnet publish -c Release -o /app/publish
+
+# 此阶段用于运行
+FROM mcr.microsoft.com/dotnet/sdk:9.0 AS runtime
+WORKDIR /app
+COPY --from=publish /app/publish .
+
+EXPOSE 2022
+ENTRYPOINT ["dotnet", "AkazawaYun.FrameworkBenchmarks.dll"]
---
+++ frameworks/CSharp/akazawayun.pro/src/Program.cs
@@ -0,0 +1,41 @@
+﻿using AkazawaYun.PRO7;
+using AkazawaYun.PRO7.AkazawaYunWebInterceptor;
+using System.Text.Json.Serialization;
+
+namespace AkazawaYun.FrameworkBenchmarks;
+
+public class Program : IPostFunction
+{
+    static async Task Main()
+    {
+        akzJson.Config(null, Json.Default);
+        var server = await akzWebBuilder.Shared.Load().SetDefault().Build()
+            .Config<IWebReceptor, akzWebInterceptor>(itc =>
+            {
+                itc.AddInterceptor(new akzWebInterceptorNotOnlyPost());
+            }).Launch();
+        akzLog.Default = akzLog.Output.None;
+        await Task.Delay(-1);
+    }
+
+
+    public static ValueTask<HttpRes> plaintext(HttpReq _)
+    {
+        return HttpRes.HttpOK("Hello, World!", ".txt");
+    }
+    public static ValueTask<HttpRes> json(HttpReq _)
+    {
+        return HttpRes.HttpJson(new JsonResponse
+        {
+            message = "Hello, World!"
+        });
+    }
+}
+
+public class JsonResponse
+{
+    public string? message { get; set; }
+}
+
+[JsonSerializable(typeof(JsonResponse))]
+public partial class Json : JsonSerializerContext { }
