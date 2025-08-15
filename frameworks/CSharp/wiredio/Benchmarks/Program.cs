d973e2b334eb2b70cfffa8af08a80dab0d0908cc
Diogo Martins <165835485+MDA2AV@users.noreply.github.com>
2025-08-15 19:45:50 +0100

    Add Wired.IO framework (#10022)
    * Add Wired.IO
    
    * fix plain text header
    
    * fix json and plaintext responses
    
    * Fix plaintext response
    
    * Fix runtime identifier
    
    * Add missing headers

---
+++ frameworks/CSharp/wiredio/Benchmarks/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Text.Json;
+using Wired.IO.App;
+using Wired.IO.Http11.Response.Content;
+using Wired.IO.Protocol.Response;
+using StringContent = Wired.IO.Http11.Response.Content.StringContent;
+
+var builder = WiredApp.CreateBuilder();
+
+await builder
+    .Endpoint(IPAddress.Any, 8080)
+    .MapGet("/plaintext", scope => context =>
+    {
+        context
+            .Respond()
+            .Status(ResponseStatus.Ok)
+            .Content(new StringContent("Hello, World!"))
+            .Type("text/plain");
+    })
+    .MapGet("/json", scope => context =>
+    {
+        context
+            .Respond()
+            .Status(ResponseStatus.Ok)
+            .Content(new JsonContent(new
+            {
+                Message = "Hello, World!"
+            }, JsonSerializerOptions.Default))
+            .Type("application/json");
+    })
+    .Build()
+    .RunAsync();
