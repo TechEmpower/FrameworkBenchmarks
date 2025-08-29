2987f77046ec67e99695b041f3ae3216f02358f4
kanarus <kanarus786@gmail.com>
2025-08-30 08:39:00 +0900

    update rt_tokio impl

--- frameworks/Rust/ohkami/rt_tokio/src/main.rs
+++ frameworks/Rust/ohkami/rt_tokio/src/main.rs
@@ -1,12 +1,39 @@
-fn main() {
-    tokio::runtime::Builder::new_multi_thread()
+fn runtime() -> tokio::runtime::Runtime {
+    tokio::runtime::Builder::new_current_thread()
         .enable_all()
-        .event_interval(11)
-        .global_queue_interval(31)
         .build()
         .unwrap()
-        .block_on(async {
-            framework_benchmarks::ohkami().await
-                .howl("0.0.0.0:8000").await
+}
+
+async fn serve<ServeFuture: Future>(
+    server: impl FnOnce(tokio::net::TcpListener) -> ServeFuture,
+) -> std::io::Result<()> {
+    println!("start serving !");
+
+    let socket = tokio::net::TcpSocket::new_v4()?;
+    socket.set_reuseport(true)?;
+    socket.set_reuseaddr(true)?;
+    socket.set_nodelay(true)?;
+
+    socket.bind("0.0.0.0:8000".parse().unwrap())?;
+    server(socket.listen(4096)?).await;
+
+    Ok(())
+}
+
+fn main() {
+    for _ in 0..(num_cpus::get() - 1/*for main thread*/) {
+        std::thread::spawn(|| {
+            runtime().block_on(async {
+                serve(|listener| async {
+                    framework_benchmarks::ohkami().await.howl(listener).await
+                }).await.expect("serving error")
+            })
         });
+    }
+    runtime().block_on(async {
+        serve(|listener| async {
+            framework_benchmarks::ohkami().await.howl(listener).await
+        }).await.expect("serving error")
+    });
 }
