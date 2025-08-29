3343df4100531c589cfd0c305ffe83fb7e2700ef
kanarus <kanarus786@gmail.com>
2025-08-29 21:35:33 +0900

    rust: bump ohkami to v0.24

--- frameworks/Rust/ohkami/rt_tokio/src/main.rs
+++ frameworks/Rust/ohkami/rt_tokio/src/main.rs
@@ -1,5 +1,12 @@
-#[tokio::main]
-async fn main() {
-    framework_benchmarks::ohkami().await
-        .howl("0.0.0.0:8000").await
+fn main() {
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .event_interval(11)
+        .global_queue_interval(31)
+        .build()
+        .unwrap()
+        .block_on(async {
+            framework_benchmarks::ohkami().await
+                .howl("0.0.0.0:8000").await
+        });
 }
