3f661bf2d8b952cdfe996b6d8c5cc27b3dd8bb56
Oxule <ribb2017@mail.ru>
2025-09-25 20:05:53 +0300

    Add Celerio C# framework benchmark

---
+++ frameworks/CSharp/celerio/README.md
@@ -0,0 +1,10 @@
+# [Celerio](https://github.com/Oxule/Celerio) benchmarks
+
+* [Github](https://github.com/Oxule/Celerio)
+* [NuGet](https://www.nuget.org/packages/Celerio)
+
+## Infrastructure Software Versions
+
+* C# 13.0
+* .NET 8
+
---
+++ frameworks/CSharp/celerio/src/Endpoints.cs
@@ -0,0 +1,25 @@
+﻿namespace src;
+
+using Celerio;
+using static Celerio.Result;
+
+public static class Endpoints
+{
+    [Get("/plaintext")]
+    public static Result Plaintext()
+    {
+        return Ok().Text("Hello, World!");
+    }
+
+    [Serializable]
+    public class SampleResponse
+    {
+        public string message { get; set; } = "Hello, World!";
+    }
+    
+    [Get("/json")]
+    public static Result Json()
+    {
+        return Ok().Json(new SampleResponse());
+    }
+}
