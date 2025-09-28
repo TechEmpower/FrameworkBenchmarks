4cbd8beb95efed7140b3f7ade5f1475238afd943
adamghill <adamghill@yahoo.com>
2025-09-28 16:51:38 -0400

    Bump all dependencies. Fix legacy regex url paths.

--- frameworks/Python/django/hello/hello/urls.py
+++ frameworks/Python/django/hello/hello/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import url
+from django.urls import path
 from world.views import plaintext, json, db, dbs, fortunes, update
 
 urlpatterns = [
-    url(r"^plaintext$", plaintext),
-    url(r"^json$", json),
-    url(r"^db$", db),
-    url(r"^dbs$", dbs),
-    url(r"^fortunes$", fortunes),
-    url(r"^update$", update),
+    path("plaintext", plaintext),
+    path("json", json),
+    path("db", db),
+    path("dbs", dbs),
+    path("fortunes", fortunes),
+    path("update", update),
 ]
