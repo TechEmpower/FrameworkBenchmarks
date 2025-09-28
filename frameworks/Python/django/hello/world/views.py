9a4e4bbb2933f3fed62e3df2e1e0915e4153cac4
adamghill <adamghill@yahoo.com>
2025-09-28 16:44:01 -0400

    Format with `ruff`.

--- frameworks/Python/django/hello/world/views.py
+++ frameworks/Python/django/hello/world/views.py
@@ -14,7 +14,7 @@ _random_int = partial(random.randint, 1, 10000)
 
 def _get_queries(request):
     try:
-        queries = int(request.GET.get('queries', 1))
+        queries = int(request.GET.get("queries", 1))
     except Exception:
         queries = 1
     if queries < 1:
@@ -30,17 +30,13 @@ def plaintext(request):
 
 def json(request):
     return HttpResponse(
-            uj_dumps({"message": "Hello, World!"}),
-            content_type="application/json"
-        )
+        uj_dumps({"message": "Hello, World!"}), content_type="application/json"
+    )
 
 
 def db(request):
     r = _random_int()
-    world = uj_dumps({
-        'id': r,
-        'randomNumber': World.objects.get(id=r).randomnumber
-    })
+    world = uj_dumps({"id": r, "randomNumber": World.objects.get(id=r).randomnumber})
     return HttpResponse(world, content_type="application/json")
 
 
@@ -49,18 +45,19 @@ def dbs(request):
 
     def caller(input_):
         int_ = _random_int()
-        return {'id': int_, 'randomNumber': World.objects.get(id=int_).randomnumber}
+        return {"id": int_, "randomNumber": World.objects.get(id=int_).randomnumber}
+
     worlds = tuple(map(caller, range(queries)))
 
     return HttpResponse(uj_dumps(worlds), content_type="application/json")
 
 
 def fortunes(request):
-    fortunes = list(Fortune.objects.values('id', 'message'))
-    fortunes.append({"id": 0, 'message': "Additional fortune added at request time."})
-    fortunes.sort(key=itemgetter('message'))
+    fortunes = list(Fortune.objects.values("id", "message"))
+    fortunes.append({"id": 0, "message": "Additional fortune added at request time."})
+    fortunes.sort(key=itemgetter("message"))
 
-    return render(request, 'fortunes.html', {'fortunes': fortunes})
+    return render(request, "fortunes.html", {"fortunes": fortunes})
 
 
 def update(request):
@@ -70,7 +67,8 @@ def update(request):
         w = World.objects.get(id=_random_int())
         w.randomnumber = _random_int()
         w.save()
-        return {'id': w.id, 'randomNumber': w.randomnumber}
+        return {"id": w.id, "randomNumber": w.randomnumber}
+
     worlds = tuple(map(caller, range(queries)))
 
     return HttpResponse(uj_dumps(worlds), content_type="application/json")
