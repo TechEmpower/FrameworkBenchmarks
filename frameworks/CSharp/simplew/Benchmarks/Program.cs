15284dd448d9792ccb11fc4cb421475305480b5f
Diogo Martins <diogoalves@ua.pt>
2025-11-01 11:45:21 +0000

    Add SimpleW frameowork

---
+++ frameworks/CSharp/simplew/Benchmarks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using SimpleW;
+
+namespace Benchmarks;
+
+internal static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var server = new SimpleWServer(IPAddress.Any, 8080);
+        server.AddDynamicContent("/api");
+        server.Start();
+        
+        await Task.Delay(-1);
+    }
+    
+    public class JsonController : Controller {
+        [Route("GET", "/json")]
+        public object Json() {
+            return new { message = "Hello World !" };
+        }
+        
+        [Route("GET", "/plaintext")]
+        public object Plaintext() {
+            return "Hello World !" ;
+        }
+    }
+}
---
+++ frameworks/CSharp/simplew/README.md
@@ -0,0 +1,26 @@
+# GenHTTP Tests on Linux
+
+See the [GenHTTP website](https://genhttp.org) for more information.
+
+## Infrastructure Software Versions
+
+**Language**
+
+* C# 13.0
+
+**Platforms**
+
+* .NET 8/9
+
+**Web Servers**
+
+* [GenHTTP](https://github.com/Kaliumhexacyanoferrat/GenHTTP)
+
+## Paths & Source for Tests
+
+* [Plaintext](Benchmarks/Program.cs): "/plaintext"
+* [JSON](Benchmarks/Tests/JsonResource.cs): "/json"
+* [DB](Benchmarks/Tests/DbResource.cs): "/db"
+* [Queries](Benchmarks/Tests/QueryResource.cs): "/queries"
+* [Updates](Benchmarks/Tests/UpdateResource.cs): "/updates"
+* [Fortunes](Benchmarks/Tests/FortuneHandler.cs): "/fortunes"
