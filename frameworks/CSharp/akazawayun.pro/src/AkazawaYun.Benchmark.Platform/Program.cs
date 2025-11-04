0516ab82262e15f08d11f9a703ebcdd55b6ddaef
AkazawaYun <408193813@qq.com>
2025-11-04 09:34:30 +0800

    update nupkg version

--- frameworks/CSharp/akazawayun.pro/src/AkazawaYun.Benchmark.Platform/Program.cs
+++ frameworks/CSharp/akazawayun.pro/src/AkazawaYun.Benchmark.Platform/Program.cs
@@ -9,27 +9,25 @@ class Program
 
     static Program()
     {
-        akzLog.War("AkazawaYun.PRO 压力测试特供版 ver2025.11.3, 只支持 /plaintext 和 /json");
+        akzLog.War("AkazawaYun.PRO 平台压力测试特供版 ver2025.11.3, 只支持 /plaintext 和 /json");
         akzJson.Config(null, AotJsonContext.Default);
         builder = akzWebBuilder.Shared.SetPort(port).SetDev(true)
             .Add<akzXmlSummary, akzXmlSummary>(() => null)
             .Add<IWebReceptor, MyBenchmarkReceptor>(() => new MyBenchmarkReceptor())
-            .Add<IWebListener, akzHttpListenerVIBenchmark>(() => new(port)
-            {
-                LogLevel = 0
-            }).Build();
+            .Add<IWebListener, akzHttpListenerVIBenchmark>(() => new(port))
+            .Build();
     }
     static async Task Main()
     {
         await builder.Launch();
 
-        akzLog.Inf("[API SELF-TEST]");
+        Console.WriteLine("[API SELF-TEST]");
         string url = $"http://localhost:{port}/plaintext";
-        akzLog.Inf(" REQ URL :" + url);
+        Console.WriteLine(" REQ URL :" + url);
         string res = await akzHttpClient.Shared.Get(url).FetchString();
-        akzLog.Inf(" RES LEN :" + res.Length);
-        akzLog.Inf(" RES BODY:" + res);
-        akzLog.Inf("[OK, I WORK FINE]");
+        Console.WriteLine(" RES LEN :" + res.Length);
+        Console.WriteLine(" RES BODY:" + res);
+        Console.WriteLine("[OK, I WORK FINE]");
 
         akzLog.Default = akzLog.Output.NoneButWar;
         await Task.Delay(-1);
