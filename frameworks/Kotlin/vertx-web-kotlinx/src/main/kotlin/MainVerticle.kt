9cf28f15b6f1806b3ed75260adee8acd822cac91
Yongshun Ye <ShreckYe@gmail.com>
2025-12-08 02:24:18 +0800

    Try setting `date` with kotlinx-datetime but it doesn't work as expected
    Got `java.lang.IllegalStateException: Field offsetHours is not set`.

--- frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/MainVerticle.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/MainVerticle.kt
@@ -18,6 +18,10 @@ import io.vertx.sqlclient.Row
 import io.vertx.sqlclient.RowSet
 import io.vertx.sqlclient.Tuple
 import kotlinx.coroutines.Dispatchers
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.format.DateTimeComponents
+import kotlinx.datetime.format.format
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.html.*
 import kotlinx.html.stream.appendHTML
 import kotlinx.io.buffered
@@ -26,10 +30,13 @@ import kotlinx.serialization.Serializable
 import kotlinx.serialization.SerializationStrategy
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.io.encodeToSink
-import java.time.ZonedDateTime
-import java.time.format.DateTimeFormatter
+import kotlin.time.Clock
 
 class MainVerticle(val hasDb: Boolean) : CoroutineVerticle(), CoroutineRouterSupport {
+    companion object {
+        val timeZone = TimeZone.currentSystemDefault()
+    }
+
     // `PgConnection`s as used in the "vertx" portion offers better performance than `PgPool`s.
     lateinit var pgConnection: PgConnection
     lateinit var date: String
@@ -40,9 +47,9 @@ class MainVerticle(val hasDb: Boolean) : CoroutineVerticle(), CoroutineRouterSup
     lateinit var updateWordQuery: PreparedQuery<RowSet<Row>>
 
     fun setCurrentDate() {
-        // kotlinx-datetime doesn't support the format yet.
-        //date = Clock.System.now().toString()
-        date = DateTimeFormatter.RFC_1123_DATE_TIME.format(ZonedDateTime.now())
+        date = DateTimeComponents.Formats.RFC_1123.format {
+            setDateTime(Clock.System.now().toLocalDateTime(timeZone))
+        }
     }
 
     override suspend fun start() {
