e54375606bc11e4fca896b096a9b8a19794011b4
Nikolay Kim <fafhrd91@gmail.com>
2025-12-08 18:40:20 +0100

    [rust/ntex] update to ntex-3.0 (#10357)
    * ntex: update to ntex-3.0
    
    * wip
    
    * wip
    
    * wip

--- frameworks/Rust/ntex/src/main.rs
+++ frameworks/Rust/ntex/src/main.rs
@@ -2,7 +2,7 @@
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 use ntex::http::header::{CONTENT_TYPE, SERVER};
-use ntex::{http, time::Seconds, util::BytesMut, util::PoolId, web};
+use ntex::{http, util::BytesMut, web};
 use sonic_rs::Serialize;
 
 mod utils;
@@ -52,18 +52,10 @@ async fn main() -> std::io::Result<()> {
     ntex::server::build()
         .backlog(1024)
         .enable_affinity()
-        .bind("techempower", "0.0.0.0:8080", |cfg| {
-            cfg.memory_pool(PoolId::P1);
-            PoolId::P1.set_read_params(65535, 2048);
-            PoolId::P1.set_write_params(65535, 2048);
-
-            http::HttpService::build()
-                .keep_alive(http::KeepAlive::Os)
-                .client_timeout(Seconds::ZERO)
-                .headers_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                .payload_read_rate(Seconds::ZERO, Seconds::ZERO, 0)
-                .h1(web::App::new().service(json).service(plaintext).finish())
+        .bind("tfb", "0.0.0.0:8080", async |_| {
+            http::HttpService::h1(web::App::new().service(json).service(plaintext).finish())
         })?
+        .config("tfb", utils::config())
         .run()
         .await
 }
