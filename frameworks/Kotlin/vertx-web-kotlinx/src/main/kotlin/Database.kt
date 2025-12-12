7e7d683b5e7f5f483d10ba8a111646ab58ad9491
Yongshun Ye <ShreckYe@gmail.com>
2025-12-12 11:53:02 +0800

    Add spaces around the equal sign to make the format consistent

--- frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/Database.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/src/main/kotlin/Database.kt
@@ -1,7 +1,7 @@
 import io.vertx.sqlclient.Row
 
 const val SELECT_WORLD_SQL = "SELECT id, randomnumber FROM world WHERE id = $1"
-const val UPDATE_WORLD_SQL = "UPDATE world SET randomnumber=$1 WHERE id = $2"
+const val UPDATE_WORLD_SQL = "UPDATE world SET randomnumber = $1 WHERE id = $2"
 const val SELECT_FORTUNE_SQL = "SELECT id, message FROM fortune"
 
 
