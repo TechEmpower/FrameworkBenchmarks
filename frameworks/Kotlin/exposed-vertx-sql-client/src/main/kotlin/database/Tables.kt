d8e0a8188182538332cfbb5c8010fe92e39228f6
Yongshun Ye <ShreckYe@gmail.com>
2025-12-12 09:54:46 +0800

    Use lower case table names
    PostgreSQL table names are case-insensitive.

--- frameworks/Kotlin/exposed-vertx-sql-client/src/main/kotlin/database/Tables.kt
+++ frameworks/Kotlin/exposed-vertx-sql-client/src/main/kotlin/database/Tables.kt
@@ -4,14 +4,14 @@ import org.jetbrains.exposed.v1.core.dao.id.IdTable
 
 // see "toolset/databases/postgres/create-postgres.sql"
 
-object WorldTable : IdTable<Int>("World") {
+object WorldTable : IdTable<Int>("world") {
     override val id = integer("id").entityId()
 
     // The name is "randomNumber" in "create-postgres.sql" but it's actually "randomnumber" in the test database.
     val randomNumber = integer("randomnumber").default(0)
 }
 
-object FortuneTable : IdTable<Int>("Fortune") {
+object FortuneTable : IdTable<Int>("fortune") {
     override val id = integer("id").entityId()
     val message = varchar("message", 2048)
 }
