e69dcc72ef73adfcde4639dfa611f92ce5fd7c26
Yongshun Ye <ShreckYe@gmail.com>
2025-12-13 16:39:18 +0800

    Bump all dependencies and toolchains to the latest

--- frameworks/Kotlin/ktor/ktor-exposed/app/build.gradle.kts
+++ frameworks/Kotlin/ktor/ktor-exposed/app/build.gradle.kts
@@ -3,9 +3,9 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "2.1.21"
-    kotlin("plugin.serialization") version "2.1.21"
-    id("com.gradleup.shadow") version "8.3.9"
+    kotlin("jvm") version "2.3.0-RC3"
+    kotlin("plugin.serialization") version "2.3.0-RC3"
+    id("com.gradleup.shadow") version "9.3.0"
 }
 
 repositories {
@@ -13,8 +13,8 @@ repositories {
 }
 
 val ktorVersion = "3.3.3"
-val kotlinxSerializationVersion = "1.8.1"
-val exposedVersion = "0.61.0"
+val kotlinxSerializationVersion = "1.9.0"
+val exposedVersion = "1.0.0-rc-4"
 
 dependencies {
     implementation("io.ktor:ktor-server-core:$ktorVersion")
@@ -28,19 +28,11 @@ dependencies {
     implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
     implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
 
-    implementation("org.postgresql:postgresql:42.7.5")
-    implementation("com.zaxxer:HikariCP:5.1.0")
-    runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
+    implementation("org.postgresql:postgresql:42.7.8")
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    runtimeOnly("org.slf4j:slf4j-simple:2.0.7")
 }
 
 application.mainClass.set("AppKt")
 
-kotlin {
-    jvmToolchain(21)
-}
-
-tasks.withType<KotlinCompile>().configureEach {
-    compilerOptions {
-        jvmTarget.set(JvmTarget.JVM_21)
-    }
-}
+kotlin. jvmToolchain(25)
