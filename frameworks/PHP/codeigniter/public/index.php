539939a5669c55861ee5b1625ea643bcc036e12b
Sergey Kuznetsov <sk757a@ik.me>
2025-12-16 15:36:14 +0500

    Remove PHP version check

--- frameworks/PHP/codeigniter/public/index.php
+++ frameworks/PHP/codeigniter/public/index.php
@@ -5,26 +5,6 @@ use Config\Paths;
 
 /*
  *---------------------------------------------------------------
- * CHECK PHP VERSION
- *---------------------------------------------------------------
- */
-
-$minPhpVersion = '8.1'; // If you update this, don't forget to update `spark`.
-if (version_compare(PHP_VERSION, $minPhpVersion, '<')) {
-    $message = sprintf(
-        'Your PHP version must be %s or higher to run CodeIgniter. Current version: %s',
-        $minPhpVersion,
-        PHP_VERSION,
-    );
-
-    header('HTTP/1.1 503 Service Unavailable.', true, 503);
-    echo $message;
-
-    exit(1);
-}
-
-/*
- *---------------------------------------------------------------
  * SET THE CURRENT DIRECTORY
  *---------------------------------------------------------------
  */
