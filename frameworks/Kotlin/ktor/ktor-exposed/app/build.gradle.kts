34532d12439d95c939bde1044a5f11afd07927d1
Yongshun Ye <ShreckYe@gmail.com>
2025-12-18 21:33:39 +0800

    Bump the Kotlin version to 2.3.0 in the `ktor-exposed` portion

--- frameworks/Kotlin/ktor/ktor-exposed/app/build.gradle.kts
+++ frameworks/Kotlin/ktor/ktor-exposed/app/build.gradle.kts
@@ -3,8 +3,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     application
-    kotlin("jvm") version "2.3.0-RC3"
-    kotlin("plugin.serialization") version "2.3.0-RC3"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.serialization") version "2.3.0"
     id("com.gradleup.shadow") version "9.3.0"
 }
 
