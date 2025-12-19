00ff40648037b49347015a147d4d2cd3cba4507e
Yongshun Ye <ShreckYe@gmail.com>
2025-12-19 19:37:01 +0800

    Call `suspendTransaction` in the `exposed-r2dbc` portion

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/MainVerticle.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/MainVerticle.kt
@@ -8,6 +8,7 @@ import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 import org.jetbrains.exposed.v1.r2dbc.select
 import org.jetbrains.exposed.v1.r2dbc.statements.toExecutable
 import org.jetbrains.exposed.v1.r2dbc.transactions.TransactionManager
+import org.jetbrains.exposed.v1.r2dbc.transactions.suspendTransaction
 
 // copied and adapted from https://github.com/huanshankeji/FrameworkBenchmarks/blob/34532d12439d95c939bde1044a5f11afd07927d1/frameworks/Kotlin/ktor/ktor-exposed/app/src/main/kotlin/App.kt#L148-L185
 class MainVerticle : CommonWithDbVerticle<R2dbcDatabase>() {
@@ -15,20 +16,26 @@ class MainVerticle : CommonWithDbVerticle<R2dbcDatabase>() {
         r2DbcDatabaseConnect()
 
     override suspend fun selectWorld(id: Int): World =
-        r2dbcSelectWorldWithIdQuery(id).single().toWorld()
+        suspendTransaction(dbClient) {
+            r2dbcSelectWorldWithIdQuery(id).single().toWorld()
+        }
 
     override suspend fun updateSortedWorlds(sortedWorlds: List<World>) {
-        val batch = BatchUpdateStatement(WorldTable)
-        sortedWorlds.forEach { world ->
-            batch.addBatch(EntityID(world.id, WorldTable))
-            batch[WorldTable.randomNumber] = world.randomNumber
+        suspendTransaction(dbClient) {
+            val batch = BatchUpdateStatement(WorldTable)
+            sortedWorlds.forEach { world ->
+                batch.addBatch(EntityID(world.id, WorldTable))
+                batch[WorldTable.randomNumber] = world.randomNumber
+            }
+            // TODO also consider passing the transaction explicitly
+            batch.toExecutable().execute(TransactionManager.current())
         }
-        // TODO also consider passing the transaction explicitly
-        batch.toExecutable().execute(TransactionManager.current())
     }
 
     override suspend fun selectFortunesInto(fortunes: MutableList<Fortune>) {
-        FortuneTable.select(FortuneTable.id, FortuneTable.message)
-            .map { it.toFortune() }.toList(fortunes)
+        suspendTransaction(dbClient) {
+            FortuneTable.select(FortuneTable.id, FortuneTable.message)
+                .map { it.toFortune() }.toList(fortunes)
+        }
     }
 }
