1c57bca4a7c1d04c499727ee2667ea56e5c698fe
Yongshun Ye <ShreckYe@gmail.com>
2025-12-21 03:29:18 +0800

    Fix some warnings

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/database/Row.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/database/Row.kt
@@ -5,7 +5,7 @@ import World
 import io.r2dbc.spi.Readable
 
 fun Readable.toWorld() =
-    World(get(0, Int::class.java), get(1, Int::class.java))
+    World(get(0, Int::class.java)!!, get(1, Int::class.java)!!)
 
 fun Readable.toFortune() =
-    Fortune(get(0, Int::class.java), get(1, String::class.java))
+    Fortune(get(0, Int::class.java)!!, get(1, String::class.java)!!)
