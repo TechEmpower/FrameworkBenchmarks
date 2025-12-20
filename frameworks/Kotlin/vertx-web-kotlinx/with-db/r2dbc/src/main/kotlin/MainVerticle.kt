70939d5bc6e9248e7445965a9cd66a8447871d81
Yongshun Ye <ShreckYe@gmail.com>
2025-12-20 18:17:48 +0800

    Remove the TODO as it's tried in commit 4602312f7c9e6575a5c3dfb75cb78e226aa4c9a1

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/MainVerticle.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/MainVerticle.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.reactive.awaitLast
 import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.reactive.collect
 
-// TODO also try getting new connections each time
 /*
 `ParallelOrPipelinedSelectWorlds` leads to `io.r2dbc.postgresql.client.ReactorNettyClient$RequestQueueException: [08006] Cannot exchange messages because the request queue limit is exceeded`.
 https://github.com/pgjdbc/r2dbc-postgresql/issues/360#issuecomment-869422327 offers a workaround, but it doesn't seem like the officially recommended approach.
