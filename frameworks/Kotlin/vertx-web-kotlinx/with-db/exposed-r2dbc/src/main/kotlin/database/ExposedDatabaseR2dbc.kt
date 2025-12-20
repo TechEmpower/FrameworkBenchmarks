c33e17dfc6001d0b7572bea8358823f799c06d3b
Yongshun Ye <ShreckYe@gmail.com>
2025-12-20 15:25:35 +0800

    Set the explicit dialect which is necessary

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/database/ExposedDatabaseR2dbc.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/exposed-r2dbc/src/main/kotlin/database/ExposedDatabaseR2dbc.kt
@@ -1,7 +1,10 @@
 package database
 
+import org.jetbrains.exposed.v1.core.vendors.PostgreSQLDialect
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabase
 import org.jetbrains.exposed.v1.r2dbc.R2dbcDatabaseConfig
 
 fun r2DbcDatabaseConnect() =
-    R2dbcDatabase.connect(connectionFactory, R2dbcDatabaseConfig())
+    R2dbcDatabase.connect(connectionFactory, R2dbcDatabaseConfig {
+        explicitDialect = PostgreSQLDialect()
+    })
