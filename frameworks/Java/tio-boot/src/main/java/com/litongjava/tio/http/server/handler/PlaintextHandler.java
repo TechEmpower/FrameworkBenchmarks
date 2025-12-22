3a69188fcabad1c35a65d235ca15d3b2318884e0
Tong Li <31761981+litongjava@users.noreply.github.com>
2025-12-22 09:51:39 -1000

    upgrade Tio boot to 2.0.5 (#10425)
    * update to tio-boot v1.9.7
    
    * upgrade to tio-boot v1.9.8
    
    * upgrade to 2.0.1
    
    * add PlaintextHandler
    
    * update CacheHandler

--- frameworks/Java/tio-boot/src/main/java/com/litongjava/tio/http/server/handler/PlaintextHandler.java
+++ frameworks/Java/tio-boot/src/main/java/com/litongjava/tio/http/server/handler/PlaintextHandler.java
@@ -2,25 +2,19 @@ package com.litongjava.tio.http.server.handler;
 
 import java.nio.charset.StandardCharsets;
 
-import com.alibaba.fastjson2.JSON;
 import com.litongjava.tio.boot.http.TioRequestContext;
 import com.litongjava.tio.http.common.HttpRequest;
 import com.litongjava.tio.http.common.HttpResponse;
 import com.litongjava.tio.http.common.utils.MimeTypeUtils;
-import com.litongjava.tio.http.server.model.Message;
 
-/**
- * ab -k -n1000000 -c10 http://127.0.0.1:8080/json ab -k -n1000000 -c10
- * http://127.0.0.1:8080/plaintext
- */
-public class IndexHandler {
+public class PlaintextHandler implements HttpRequestHandler {
 
   private static final String HELLO_WORLD = "Hello, World!";
 
   private static final byte[] HELLO_WORLD_BYTES = HELLO_WORLD.getBytes(StandardCharsets.UTF_8);
-  private static final byte[] JSON_BYTES = JSON.toJSONBytes(new Message(HELLO_WORLD));
 
-  public HttpResponse plaintext(HttpRequest request) {
+  @Override
+  public HttpResponse handle(HttpRequest httpRequest) throws Exception {
     HttpResponse response = TioRequestContext.getResponse();
     response.setBody(HELLO_WORLD_BYTES);
     String mimeTypeStr = MimeTypeUtils.getTextUTF8();
@@ -28,11 +22,4 @@ public class IndexHandler {
     return response;
   }
 
-  public HttpResponse json(HttpRequest request) {
-    HttpResponse response = TioRequestContext.getResponse();
-    response.setBody(JSON_BYTES);
-    String mimeTypeStr = MimeTypeUtils.getJsonUTF8();
-    response.setContentType(mimeTypeStr);
-    return response;
-  }
 }
