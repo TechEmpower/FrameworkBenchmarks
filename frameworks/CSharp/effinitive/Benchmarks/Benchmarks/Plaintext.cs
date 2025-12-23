af7bfec5ae43249b7a4f0d073d89881a021cf10c
Andreas Nägeli <andr.naegeli@gmail.com>
2025-12-23 08:16:43 +0100

    [csharp] Draft for Effinitive

---
+++ frameworks/CSharp/effinitive/Benchmarks/Benchmarks/Plaintext.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+using EffinitiveFramework.Core;
+
+namespace Benchmarks.Benchmarks;
+
+public class Plaintext : NoRequestEndpointBase<string>
+{
+    protected override string Method => "GET";
+
+    protected override string Route => "/plaintext";
+
+    public override ValueTask<string> HandleAsync(CancellationToken cancellationToken = default)
+    {
+        return ValueTask.FromResult("Hello, World!");
+    }
+
+}
---
+++ frameworks/CSharp/effinitive/Benchmarks/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using EffinitiveFramework.Core;
+
+namespace Benchmarks
+{
+
+    public static class Program
+    {
+        private static readonly ManualResetEvent WaitEvent = new(false);
+
+        public static async Task<int> Main(string[] args)
+        {
+            var app = EffinitiveApp.Create()
+                                   .UsePort(8080)
+                                   .MapEndpoints()
+                                   .Build();
+
+            try
+            {
+                AppDomain.CurrentDomain.ProcessExit += (_, __) =>
+                {
+                    WaitEvent.Set();
+                };
+
+                await app.RunAsync();
+
+                WaitEvent.WaitOne();
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+                return -1;
+            }
+        }
+
+    }
+
+}
