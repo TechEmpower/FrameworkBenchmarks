28740d83cb2783885c9d9482996752288ad9450a
Nigro Simone <nigro.simone@gmail.com>
2025-12-30 18:42:42 +0100

    chore: update dependencies and improve logic

--- frameworks/JavaScript/ultimate-express/app.js
+++ frameworks/JavaScript/ultimate-express/app.js
@@ -17,15 +17,15 @@ const jsonSerializer = fjs({
   }
 });
 
-const generateRandomNumber = () => Math.floor(Math.random() * maxRows) + 1;
+const generateRandomNumber = () => ((Math.random() * maxRows) | 0) + 1;
 
-const parseQueries = (i) => Math.min(parseInt(i) || 1, maxQuery);
+const parseQueries = (i) => i > maxQuery ? maxQuery : (i | 0) || 1;
 
 const escapeHTMLRules = { '&': '&#38;', '<': '&#60;', '>': '&#62;', '"': '&#34;', "'": '&#39;', '/': '&#47;' };
 
 const unsafeHTMLMatcher = /[&<>"'\/]/g;
 
-const escapeHTMLCode = (text) => unsafeHTMLMatcher.test(text) ? text.replace(unsafeHTMLMatcher, function (m) { return escapeHTMLRules[m] || m; }) : text;
+const escapeHTMLCode = (text) => unsafeHTMLMatcher.test(text) ? text.replace(unsafeHTMLMatcher,  m => escapeHTMLRules[m] || m) : text;
 
 const cache = new LRUCache({
   max: maxRows
@@ -50,105 +50,84 @@ app.get('/json', (req, res) => {
 if (db) {
   app.get('/db', async (req, res) => {
     res.setHeader('Server', 'UltimateExpress');
-
-    try {
-      const world = await db.find(generateRandomNumber());
-      res.json(world);
-    } catch (error) {
-      throw error;
-    }
+    const world = await db.find(generateRandomNumber());
+    res.json(world);
   });
 
   app.get('/queries', async (req, res) => {
     res.setHeader('Server', 'UltimateExpress');
 
-    try {
-      const queries = parseQueries(req.query.queries);
-      const worldPromises = new Array(queries);
+    const queries = parseQueries(req.query.queries);
+    const worldPromises = new Array(queries);
 
-      for (let i = 0; i < queries; i++) {
-        worldPromises[i] = db.find(generateRandomNumber());
-      }
-
-      const worlds = await Promise.all(worldPromises);
-
-      res.json(worlds);
-    } catch (error) {
-      throw error;
+    for (let i = 0; i < queries; i++) {
+      worldPromises[i] = db.find(generateRandomNumber());
     }
+
+    const worlds = await Promise.all(worldPromises);
+
+    res.json(worlds);
   })
 
   app.get('/updates', async (req, res) => {
     res.setHeader('Server', 'UltimateExpress');
 
-    try {
-      const queries = parseQueries(req.query.queries);
-      const worldPromises = new Array(queries);
+    const queries = parseQueries(req.query.queries);
+    const worldPromises = new Array(queries);
 
-      for (let i = 0; i < queries; i++) {
-        worldPromises[i] = db.find(generateRandomNumber());
-      }
-
-      const worlds = await Promise.all(worldPromises);
-
-      for (let i = 0; i < queries; i++) {
-        worlds[i].randomNumber = generateRandomNumber();
-      }
-
-      await db.bulkUpdate(worlds);
-
-      res.json(worlds);
-    } catch (error) {
-      throw error;
+    for (let i = 0; i < queries; i++) {
+      worldPromises[i] = db.find(generateRandomNumber());
     }
+
+    const worlds = await Promise.all(worldPromises);
+
+    for (let i = 0; i < queries; i++) {
+      worlds[i].randomNumber = generateRandomNumber();
+    }
+
+    await db.bulkUpdate(worlds);
+
+    res.json(worlds);
   })
 
   app.get('/fortunes', async (req, res) => {
     res.setHeader('Server', 'UltimateExpress');
 
-    try {
-      const fortunes = await db.fortunes()
+    const fortunes = await db.fortunes()
 
-      fortunes.push({ id: 0, message: 'Additional fortune added at request time.' });
+    fortunes.push({ id: 0, message: 'Additional fortune added at request time.' });
 
-      fortunes.sort((a, b) => (a.message < b.message) ? -1 : 1);
+    fortunes.sort((a, b) => (a.message < b.message) ? -1 : 1);
 
-      const n = fortunes.length
+    const n = fortunes.length
 
-      let i = 0, html = ''
-      for (; i < n; i++) html += `<tr><td>${fortunes[i].id}</td><td>${escapeHTMLCode(fortunes[i].message)}</td></tr>`
+    let i = 0, html = ''
+    for (; i < n; i++) html += `<tr><td>${fortunes[i].id}</td><td>${escapeHTMLCode(fortunes[i].message)}</td></tr>`
 
-      res
-        .header('Content-Type', 'text/html; charset=utf-8')
-        .end(`<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>${html}</table></body></html>`);
-    } catch (error) {
-      throw error;
-    }
+    res
+      .header('Content-Type', 'text/html; charset=utf-8')
+      .end(`<!DOCTYPE html><html><head><title>Fortunes</title></head><body><table><tr><th>id</th><th>message</th></tr>${html}</table></body></html>`);
   })
 
   let isCachePopulated = false
   app.get('/cached-worlds', async (req, res) => {
     res.setHeader('Server', 'UltimateExpress');
 
-    try {
-      if (!isCachePopulated) {
-        const worlds = await db.getAllWorlds();
-        for (let i = 0; i < worlds.length; i++) {
-          cache.set(worlds[i].id, worlds[i]);
-        }
-        isCachePopulated = true;
+    if (!isCachePopulated) {
+      const worlds = await db.getAllWorlds();
+      for (let i = 0; i < worlds.length; i++) {
+        cache.set(worlds[i].id, worlds[i]);
       }
-      const count = parseQueries(req.query.count);
-      const worlds = new Array(count);
-
-      for (let i = 0; i < count; i++) {
-        worlds[i] = cache.get(generateRandomNumber());
-      }
-
-      res.json(worlds);
-    } catch (error) {
-      throw error;
+      isCachePopulated = true;
     }
+    const count = parseQueries(req.query.count);
+    const worlds = new Array(count);
+
+    for (let i = 0; i < count; i++) {
+      worlds[i] = cache.get(generateRandomNumber());
+    }
+
+    res.json(worlds);
   });
 }
 
