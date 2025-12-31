40be1817f234a86fbcd7a69666a74a648f884a38
Nigro Simone <nigro.simone@gmail.com>
2025-12-31 05:11:29 +0100

    fix: force lower

--- frameworks/JavaScript/ultimate-express/database/mysql.js
+++ frameworks/JavaScript/ultimate-express/database/mysql.js
@@ -9,10 +9,10 @@ const execute = (text, values) => pool.execute(text, values || undefined)
 export const fortunes = () => execute('SELECT id, message FROM fortune')
 
 export const find = async(id) => {
-    const arr = await execute('SELECT id, randomNumber FROM world WHERE id = ?', [id]);
+    const arr = await execute('SELECT id, randomNumber AS randomnumber FROM world WHERE id = ?', [id]);
     return arr[0];
 }
 
-export const getAllWorlds = () => execute('SELECT id, randomNumber FROM world')
+export const getAllWorlds = () => execute('SELECT id, randomNumber AS randomnumber FROM world')
 
 export const bulkUpdate = (worlds) => pool.batch('UPDATE world SET randomNumber = ? WHERE id = ?', worlds.map(world => [world.randomNumber, world.id]).sort((a, b) => (a[1] < b[1]) ? -1 : 1))
