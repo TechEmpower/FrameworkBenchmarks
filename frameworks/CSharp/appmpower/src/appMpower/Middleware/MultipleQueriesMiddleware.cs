55a62f5c1d3cc4a418b616594d96e4e91c0e081d
“llt” <luc.lefever@fidesoft.com>
2026-01-03 20:49:05 +0100

    Correction unsafe block

--- frameworks/CSharp/appmpower/src/appMpower/Middleware/MultipleQueriesMiddleware.cs
+++ frameworks/CSharp/appmpower/src/appMpower/Middleware/MultipleQueriesMiddleware.cs
@@ -9,46 +9,49 @@ using Microsoft.Extensions.Primitives;
 using appMpower.Orm;
 #endif
 
-namespace appMpower; 
+namespace appMpower;
 
 public class MultipleQueriesMiddleware
 {
-   private readonly static KeyValuePair<string, StringValues> _headerServer =
-         new("Server", new StringValues("k"));
-   private readonly static KeyValuePair<string, StringValues> _headerContentType =
-         new("Content-Type", new StringValues("application/json"));
+    private readonly static KeyValuePair<string, StringValues> _headerServer =
+          new("Server", new StringValues("k"));
+    private readonly static KeyValuePair<string, StringValues> _headerContentType =
+          new("Content-Type", new StringValues("application/json"));
 
-   public static async Task Invoke(HttpContext httpContext)
-   {
-      var queryString = httpContext.Request.QueryString.ToString(); 
-      int queries; 
-      Int32.TryParse(queryString.Substring(queryString.LastIndexOf("=") + 1), out queries); 
-      queries = queries > 500 ? 500 : (queries > 0 ? queries : 1);
+    public static async Task Invoke(HttpContext httpContext)
+    {
+        var queryString = httpContext.Request.QueryString.ToString();
+        int queries;
+        Int32.TryParse(queryString.Substring(queryString.LastIndexOf("=") + 1), out queries);
+        queries = queries > 500 ? 500 : (queries > 0 ? queries : 1);
 
-      var response = httpContext.Response;
-      response.Headers.Add(_headerServer);
-      response.Headers.Add(_headerContentType);
+        var response = httpContext.Response;
+        response.Headers.Add(_headerServer);
+        response.Headers.Add(_headerContentType);
 
 #if AOTDLL
-      int payloadLength;
-      IntPtr handlePointer;
+        int payloadLength;
+        IntPtr handlePointer;
 
-      IntPtr bytePointer = NativeMethods.Query(queries, out payloadLength, out handlePointer);
-      byte[] json = new byte[payloadLength];
-      //Marshal.Copy(bytePointer, json, 0, payloadLength);
+        IntPtr bytePointer = NativeMethods.Query(queries, out payloadLength, out handlePointer);
+        byte[] json = new byte[payloadLength];
+        //Marshal.Copy(bytePointer, json, 0, payloadLength);
 
-      fixed (byte* dest = json)
-      {
-            Buffer.MemoryCopy((void*)bytePointer, dest, payloadLength, payloadLength);
-      }
+        unsafe
+        {
+            fixed (byte* dest = json)
+            {
+                Buffer.MemoryCopy((void*)bytePointer, dest, payloadLength, payloadLength);
+            }
+        }
 
-      NativeMethods.FreeHandlePointer(handlePointer);
+        NativeMethods.FreeHandlePointer(handlePointer);
 #else
-      byte[] json = await DotnetMethods.Query(queries);
+        byte[] json = await DotnetMethods.Query(queries);
 #endif
-      response.Headers.Add(
-            new KeyValuePair<string, StringValues>("Content-Length", json.Length.ToString()));
+        response.Headers.Add(
+              new KeyValuePair<string, StringValues>("Content-Length", json.Length.ToString()));
 
-      await response.Body.WriteAsync(json);
-   }
+        await response.Body.WriteAsync(json);
+    }
 }
