67767ce6c7cf19ad665a5a76e7d7fbe486b0d2ce
yn-tadpole <25441950@qq.com>
2026-01-06 06:09:39 +0800

    Create Tad.x Framework test (#10512)
    * [Java/Tad.x] Create Tad.x Framework test
    
    * Add Forwarded Port. Used for debugging during the development and testing of code
    
    * [Java/Tad.x] Build the test package (as the relevant dependencies of Tad.x have not yet been uploaded to the Maven repository, local packaging is temporarily used. In the future, it will be changed to rely on the Maven repository)
    
    * [Java/Tad.x] Update README.md
    
    ---------
    
    Co-authored-by: tadpole@home <tadpole@home>

---
+++ frameworks/Java/tadx/src/main/java/io/tadx/benchmark/controller/Db1.java
@@ -0,0 +1,46 @@
+package io.tadx.benchmark.controller;
+
+import io.tadx.benchmark.entity.World;
+import io.tadx.core.data.DataMap;
+import io.tadx.data.DbStorage;
+import io.tadx.web.*;
+import io.tadx.web.annotation.*;
+import io.vertx.core.Future;
+import io.vertx.core.Promise;
+
+import java.util.SplittableRandom;
+
+/**
+ * EN: The entry point of the application.
+ */
+
+@RestController(mapping = "/db_rest_druid")
+public class Db1 {
+
+
+    private static final SplittableRandom RANDOM = new SplittableRandom();
+    private final DbStorage dbStorage;
+
+    public Db1(DbStorage dbStorage) {
+        this.dbStorage = dbStorage;
+    }
+
+    //@RestFunction(method = HttpMethod.GET)
+    public Future<World> execute() {
+        Promise<World> promise = Promise.promise();
+        World world = dbStorage.findEntity(World.class, randomWorld());
+        promise.complete(world);
+        return promise.future();
+    }
+
+    @RestFunction(method = HttpMethod.GET)
+    public DataMap executeSQL() {
+        return dbStorage.queryRow("SELECT id, randomnumber FROM world WHERE id = ?", randomWorld());
+    }
+
+
+    static int randomWorld() {
+        return 1 + RANDOM.nextInt(10000);
+    }
+
+}
