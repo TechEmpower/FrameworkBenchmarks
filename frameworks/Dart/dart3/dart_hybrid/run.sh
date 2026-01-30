b4c2ddcf4afe786dac1d7d62cebe9d0fd4edd741
iapicca <francesco@yakforward.com>
2026-01-30 22:42:03 +0200

    comments added to run.sh

--- frameworks/Dart/dart3/dart_hybrid/run.sh
+++ frameworks/Dart/dart3/dart_hybrid/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+# --- 1. Environment & Hardware Detection ---
 TOTAL_PROCESSES=$(grep -c ^processor /proc/cpuinfo)
 MAX_ISO=${MAX_ISOLATES:-8}
 RESERVED=${MIN_TRAEFIK_PROCESSES:-4}
 
+# --- 2. Scaling Logic ---
 if [ "$TOTAL_PROCESSES" -le "$((MAX_ISO * 2))" ]; then
     exec /app/server
 else
@@ -14,6 +16,7 @@ else
 
     export GOMAXPROCS=$TRAEFIK_PROCESSES
 
+# --- 3. Generate Backend List ---
     TMP_URLS="/tmp/urls.yaml"
     true > "$TMP_URLS"
 
@@ -24,13 +27,14 @@ else
         /app/server --port=$PORT &
     done
 
+# --- 4. Traefik Configuration ---
     sed -i "/# DART_WORKERS_PLACEHOLDER/ {
       r $TMP_URLS
       d
     }" /etc/traefik/traefik_dynamic.yaml
-
     rm "$TMP_URLS"
     
+# --- 5. Readiness & Execution ---
     until nc -z 127.0.0.1 9001; do sleep 0.1; done
 
     exec traefik --configfile=/etc/traefik/traefik.yaml
