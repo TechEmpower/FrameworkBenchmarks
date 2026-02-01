0456604cb3d833664d18011698b34227c2692fc2
尤雨东 <root@ltpp.vip>
2026-02-01 10:27:38 +0800

    feat: v0.1.1

--- frameworks/Rust/hyperlane/src/route/struct.rs
+++ frameworks/Rust/hyperlane/src/route/struct.rs
@@ -1,7 +1,20 @@
+#[derive(Clone, Copy, Default)]
 pub(crate) struct JsonRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct PlaintextRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct DbRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct QueryRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct FortunesRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct UpdateRoute;
+
+#[derive(Clone, Copy, Default)]
 pub(crate) struct CachedQueryRoute;
