d0abb0d5883c2505b6972487c2350f24210231ec
Christophe CHATEAU <c.chateau@stratdev.fr>
2026-02-13 02:03:39 +0100

    Update SimpleW Program.cs

--- frameworks/CSharp/simplew/Benchmarks/Program.cs
+++ frameworks/CSharp/simplew/Benchmarks/Program.cs
@@ -10,39 +10,25 @@ internal static class Program
 {
     public static async Task Main(string[] args)
     {
-        var server = new SimpleWServer(IPAddress.Any, 8080);
-        server.AddDynamicContent("/api");
-        server.Start();
+        SimpleWServer server = new(IPAddress.Any, 8080);
         
-        await Task.Delay(-1);
+        // minimal api
+        server.MapGet("/json", (HttpSession session) => {
+            return session.Response
+                          .Json(new { message = "Hello, World!" })
+                          .AddHeader("Server", "SimpleW")
+                          .AddHeader("Date", DateTime.Now.ToString("R"))
+                          .NoCompression();
+        });
+        server.MapGet("/plaintext", (HttpSession session) => {
+            return session.Response
+                      .Text("Hello, World!")
+                      .AddHeader("Server", "SimpleW")
+                      .AddHeader("Date", DateTime.Now.ToString("R"))
+                      .NoCompression();
+        });
+        
+        await server.RunAsync();
     }
-    
-    public class BenchmarksController : Controller {
-        [Route("GET", "/json")]
-        public object Json() {
-            return Response.MakeResponse(
-                new { message = "Hello, World!" }, // object will be serialized
-                addHeaders: new Dictionary<string, string>()
-                {
-                    { "Server", "SimpleW" },
-                    { "Date", DateTime.Now.ToString("R") }
-                }
-                // compress parameter is default to null, so no compression
-            );
-        }
 
-        [Route("GET", "/plaintext")]
-        public object Plaintext() {
-            return Response.MakeResponse(
-                "Hello, World!",
-                "text/plain",
-                addHeaders: new Dictionary<string, string>()
-                {
-                    { "Server", "SimpleW" },
-                    { "Date", DateTime.Now.ToString("R") }
-                }
-                // compress parameter is default to null, so no compression
-            );
-        }
-    }
 }
