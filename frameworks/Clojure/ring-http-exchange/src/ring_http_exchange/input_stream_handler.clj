3ff4e6fec28c0a85fd57b19f632bc3d4145b6f52
jj <keistaslogairkosulys@gmail.com>
2026-02-16 18:41:20 +0100

    Update dependencies in ring-http-exchange
    Also updated executor and hikaricp connection settings

--- frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/input_stream_handler.clj
+++ frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/input_stream_handler.clj
@@ -2,32 +2,22 @@
   (:require
     [jj.majavat :as majavat]
     [jj.majavat.renderer :refer [->InputStreamRenderer]]
-    [jj.sql.boa :as boa])
+    [ring-http-exchange.model :as model])
   (:import (java.io ByteArrayInputStream)))
 
 (defrecord Response [body status headers])
 
-(def query-fortunes (boa/build-query (boa/->NextJdbcAdapter) "fortune.sql"))
-
 (def ^:private hello-world-bytes (.getBytes "Hello, World!"))
-
 (def ^:private ^:const fortune-headers {"Server"       "ring-http-exchange"
                                         "Content-Type" "text/html; charset=UTF-8"})
 
 (def ^:private render-fortune (majavat/build-html-renderer "fortune.html"
                                                            {:renderer (->InputStreamRenderer)}))
 
-(defn- get-body [datasource]
-  (let [context (as-> (query-fortunes datasource) fortunes
-                      (conj fortunes {:id      0
-                                      :message "Additional fortune added at request time."})
-                      (sort-by :message fortunes))]
-    (render-fortune {:messages context})))
-
 (defn get-handler [data-source]
   (fn [req]
     (case (req :uri)
-      "/fortunes" (Response. (get-body data-source) 200 fortune-headers)
+      "/fortunes" (Response. (render-fortune (model/fortunes-body data-source)) 200 fortune-headers)
       (Response. (ByteArrayInputStream. hello-world-bytes) 200 {"Server"       "ring-http-exchange"
                                                                 "Content-Type" "text/plain"}))))
 
