0a1f30912e17d68ec57483ab418075db9786d817
Thomas Segismont <tsegismo@ibm.com>
2026-02-17 15:03:25 +0100

    Don't precompute content length headers
    Frameworks doing this may be considered as stripped

--- frameworks/Java/quarkus/reactive-routes-pgclient/src/main/java/io/quarkus/benchmark/resource/PlaintextResource.java
+++ frameworks/Java/quarkus/reactive-routes-pgclient/src/main/java/io/quarkus/benchmark/resource/PlaintextResource.java
@@ -11,14 +11,12 @@ import jakarta.inject.Singleton;
 public class PlaintextResource {
     private static final String HELLO_WORLD = "Hello, world!";
     private static final Buffer HELLO_WORLD_BUFFER = Buffer.buffer(HELLO_WORLD, "UTF-8");
-    private static final CharSequence HELLO_WORLD_LENGTH = HttpHeaders.createOptimized("" + HELLO_WORLD.length());
 
     @Route(path = "plaintext")
     public void plaintext(final RoutingContext rc) {
         final var response = rc.response();
         final var headers = response.headers();
         headers.add(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN);
-        headers.add(HttpHeaders.CONTENT_LENGTH, HELLO_WORLD_LENGTH);
         response.end(HELLO_WORLD_BUFFER, null);
     }
 }
--- frameworks/Java/quarkus/vertx/src/main/java/io/quarkus/benchmark/resource/PlaintextHttpHandler.java
+++ frameworks/Java/quarkus/vertx/src/main/java/io/quarkus/benchmark/resource/PlaintextHttpHandler.java
@@ -12,12 +12,10 @@ import jakarta.inject.Singleton;
 public class PlaintextHttpHandler {
     private static final String HELLO_WORLD = "Hello, world!";
     private static final Buffer HELLO_WORLD_BUFFER = Buffer.buffer(HELLO_WORLD, "UTF-8");
-    private static final CharSequence HELLO_WORLD_LENGTH = HttpHeaders.createOptimized("" + HELLO_WORLD.length());
 
     public void handle(final HttpServerRequest request) {
         final HttpServerResponse response = request.response();
         final MultiMap headers = response.headers();
-        headers.add(HttpHeaders.CONTENT_LENGTH, HELLO_WORLD_LENGTH);
         headers.add(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.TEXT_PLAIN);
         response.end(HELLO_WORLD_BUFFER, null);
     }
