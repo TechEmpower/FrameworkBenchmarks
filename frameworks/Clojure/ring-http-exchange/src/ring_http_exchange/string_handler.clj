e4eadb4659c7fcde501ea6b2a20e2c6668eda8f2
ruroru <111705692+ruroru@users.noreply.github.com>
2026-02-23 22:50:05 +0100

    Remove ring-http-exchange-graalvm and ring-http-exchange-robaho-graalvm (#10807)
    Adding temporarily ring-http-exchange-robaho-async instead
    
    Co-authored-by: jj <keistaslogairkosulys@gmail.com>

--- frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/string_handler.clj
+++ frameworks/Clojure/ring-http-exchange/src/ring_http_exchange/string_handler.clj
@@ -1,7 +1,8 @@
 (ns ring-http-exchange.string-handler
   (:require
     [jj.majavat :as majavat]
-    [ring-http-exchange.model :as model]))
+    [ring-http-exchange.model :as model])
+  (:import (java.util.function Consumer)))
 
 (defrecord Response [body status headers])
 (def ^:private ^:const fortune-headers {"Server"       "ring-http-exchange"
@@ -13,12 +14,27 @@
 
 (def ^:private render-fortune (majavat/build-html-renderer "fortune.html"))
 
+(deftype FortuneCallback [respond]
+  Consumer
+  (accept [_ fortune-data]
+    (respond
+      (Response.
+        (render-fortune {:messages (sort-by :message (conj fortune-data {:id      0
+                                                                         :message "Additional fortune added at request time."}))})
+        200
+        fortune-headers))))
+
 (defn get-handler [data-source]
   (fn [req]
     (case (req :uri)
       "/plaintext" (Response. model/hello-world 200 plain-text-headers)
       "/json" (Response. (model/json-body) 200 json-headers)
-      "/fortunes" (Response. (render-fortune (model/fortunes-body  data-source)) 200 fortune-headers)
+      "/fortunes" (Response. (render-fortune (model/fortunes-body data-source)) 200 fortune-headers)
       (Response. model/hello-world 200 {"Server"       "ring-http-exchange"
                                         "Content-Type" "text/plain"}))))
 
+(defn get-async-handler [data-source]
+  (fn [req respond raise]
+    (if (.equals "/fortunes" (req :uri))
+      (model/async-query-fortunes data-source (FortuneCallback. respond) raise)
+      (Response. model/hello-world 200 plain-text-headers))))
