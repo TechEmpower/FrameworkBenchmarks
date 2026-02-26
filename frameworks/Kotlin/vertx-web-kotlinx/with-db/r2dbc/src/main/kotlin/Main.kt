cfd08e5f5fdaea55a92fa0086313c465c81dd460
Yongshun Ye <ShreckYe@gmail.com>
2026-02-26 20:31:36 +0800

    Replace usages of the word "optimized" for the R2DBC configs with "ktor-r2dbc"

--- frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/Main.kt
+++ frameworks/Kotlin/vertx-web-kotlinx/with-db/r2dbc/src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-import database.connectionPoolOptimized
+import database.connectionPoolKtorR2dbc
 import database.connectionPoolOriginal
 import io.r2dbc.spi.ConnectionFactory
 
@@ -7,27 +7,27 @@ suspend fun main(args: Array<String>) {
     val isSharedPool = args.getOrNull(0)?.toBooleanStrictOrNull() ?: false
     val poolSize = args.getOrNull(1)?.toIntOrNull()
         ?: 8 // TODO `1` might produce equivalent results because there is no transaction in this portion compared to `exposed-r2dbc`.
-    val useOptimizedConfig = args.getOrNull(2)?.toBooleanStrictOrNull() ?: false
+    val useKtorR2dbcConfig = args.getOrNull(2)?.toBooleanStrictOrNull() ?: false
 
     val benchmarkName = buildString {
         append("Vert.x-Web Kotlinx with R2DBC (and PostgreSQL)")
-        if (!isSharedPool || poolSize != 512 || !useOptimizedConfig) {
+        if (!isSharedPool || poolSize != 512 || !useKtorR2dbcConfig) {
             append(" - ")
             if (isSharedPool) {
                 append("Shared Pool Size $poolSize")
             } else {
                 append("Separate Pool Size $poolSize")
             }
-            if (useOptimizedConfig) {
-                append(" Optimized")
+            if (useKtorR2dbcConfig) {
+                append(" Ktor R2DBC portion config")
             }
         }
     }
 
     if (isSharedPool) {
         // Shared pool: create one ConnectionPool that all verticles will share
-        val connectionFactory: ConnectionFactory = if (useOptimizedConfig) {
-            connectionPoolOptimized(poolSize)
+        val connectionFactory: ConnectionFactory = if (useKtorR2dbcConfig) {
+            connectionPoolKtorR2dbc(poolSize)
         } else {
             connectionPoolOriginal(poolSize)
         }
@@ -42,7 +42,7 @@ suspend fun main(args: Array<String>) {
         commonRunVertxServer(
             benchmarkName,
             {},
-            { MainVerticleWithSeparatePool(poolSize, useOptimizedConfig) }
+            { MainVerticleWithSeparatePool(poolSize, useKtorR2dbcConfig) }
         )
     }
 }
