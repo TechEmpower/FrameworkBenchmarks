eba0c7cd6bb8ba1f6f5cc82385e2fe1aac10bf0a
kit rio <mario64aq@gmail.com>
2026-03-07 04:50:11 +0900

    [JS/nestjs] add typeorm-options.ts

---
+++ frameworks/TypeScript/nest/eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import globals from 'globals';
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      sourceType: 'module',
+      globals: globals.node,
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  eslintConfigPrettier,
+];
