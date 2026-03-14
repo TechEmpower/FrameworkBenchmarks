47e4d985ee6424240a61532720b12cb894b9b585
zhengjw22 <zhengjunweimail@163.com>
2026-03-14 18:01:57 +0800

    refactor(framework): 移除 smart-servlet 相关代码并升级依赖
    - 移除了 JsonUtil.java 中的全部实现代码
    - 移除了 MultipleQueriesHandler.java 中的全部实现代码
    - 移除了 SingleQueryHandler.java 中的全部实现代码
    - 移除了 UpdateHandler.java 中的全部实现代码
    - 移除了 Bootstrap.java 中的全部实现代码
    - 移除了 HelloWorldServlet.java 中的全部实现代码
    - 移除了 JsonServlet.java 中的全部实现代码
    - 从 benchmark_config.json 中移除了 smart-servlet 配置
    - 从 config.toml 中注释掉 smart-servlet 配置
    - 将 feat-cloud-starter 依赖版本从 1.4.0 升级到 1.5.2
    - 注释掉 servlet-core 依赖配置
    - 修改 shade 插件中的服务文件路径配置

--- frameworks/Java/smart-socket/src/main/java/org/smartboot/http/SingleQueryHandler.java
+++ frameworks/Java/smart-socket/src/main/java/org/smartboot/http/SingleQueryHandler.java
@@ -1,60 +1,60 @@
-package org.smartboot.http;
-
-
-import tech.smartboot.feat.core.server.HttpHandler;
-import tech.smartboot.feat.core.server.HttpRequest;
-import tech.smartboot.feat.core.server.HttpResponse;
-
-import javax.sql.DataSource;
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadLocalRandom;
-
-/**
- * @author 三刀
- * @version V1.0 , 2020/6/16
- */
-public class SingleQueryHandler implements HttpHandler {
-    private DataSource dataSource;
-
-    public SingleQueryHandler(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
-    @Override
-    public void handle(HttpRequest httpRequest, CompletableFuture<Void> completableFuture) throws IOException {
-        HttpResponse response = httpRequest.getResponse();
-        Thread.startVirtualThread(() -> {
-            try {
-                World world = new World();
-                try (Connection connection = dataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM World WHERE id=?");) {
-                    preparedStatement.setInt(1, getRandomNumber());
-                    ResultSet resultSet = preparedStatement.executeQuery();
-                    resultSet.next();
-                    world.setId(resultSet.getInt(1));
-                    world.setRandomNumber(resultSet.getInt(2));
-                } catch (SQLException throwables) {
-                    throwables.printStackTrace();
-                }
-                response.setContentType("application/json");
-                JsonUtil.writeJsonBytes(response, world);
-            } finally {
-                completableFuture.complete(null);
-            }
-        });
-
-    }
-
-    @Override
-    public void handle(HttpRequest request) throws Throwable {
-
-    }
-
-    protected int getRandomNumber() {
-        return 1 + ThreadLocalRandom.current().nextInt(10000);
-    }
-}
+//package org.smartboot.http;
+//
+//
+//import tech.smartboot.feat.core.server.HttpHandler;
+//import tech.smartboot.feat.core.server.HttpRequest;
+//import tech.smartboot.feat.core.server.HttpResponse;
+//
+//import javax.sql.DataSource;
+//import java.io.IOException;
+//import java.sql.Connection;
+//import java.sql.PreparedStatement;
+//import java.sql.ResultSet;
+//import java.sql.SQLException;
+//import java.util.concurrent.CompletableFuture;
+//import java.util.concurrent.ThreadLocalRandom;
+//
+///**
+// * @author 三刀
+// * @version V1.0 , 2020/6/16
+// */
+//public class SingleQueryHandler implements HttpHandler {
+//    private DataSource dataSource;
+//
+//    public SingleQueryHandler(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+//
+//    @Override
+//    public void handle(HttpRequest httpRequest, CompletableFuture<Void> completableFuture) throws IOException {
+//        HttpResponse response = httpRequest.getResponse();
+//        Thread.startVirtualThread(() -> {
+//            try {
+//                World world = new World();
+//                try (Connection connection = dataSource.getConnection(); PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM World WHERE id=?");) {
+//                    preparedStatement.setInt(1, getRandomNumber());
+//                    ResultSet resultSet = preparedStatement.executeQuery();
+//                    resultSet.next();
+//                    world.setId(resultSet.getInt(1));
+//                    world.setRandomNumber(resultSet.getInt(2));
+//                } catch (SQLException throwables) {
+//                    throwables.printStackTrace();
+//                }
+//                response.setContentType("application/json");
+//                JsonUtil.writeJsonBytes(response, world);
+//            } finally {
+//                completableFuture.complete(null);
+//            }
+//        });
+//
+//    }
+//
+//    @Override
+//    public void handle(HttpRequest request) throws Throwable {
+//
+//    }
+//
+//    protected int getRandomNumber() {
+//        return 1 + ThreadLocalRandom.current().nextInt(10000);
+//    }
+//}
